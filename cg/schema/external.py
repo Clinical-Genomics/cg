5fbfdd5d196b00dc83718b50b68403b2f213a6c2
Robin Andeer <robin.andeer@gmail.com>
2017-06-15 10:59:21 +0200

    remove unused options from external sample

--- cg/schema/external.py
+++ cg/schema/external.py
@@ -8,6 +8,12 @@ schema_family = schema_project["properties"]["families"]["items"]
 del schema_family["properties"]["require_qcok"]
 del schema_family["properties"]["priority"]
 schema_family["required"] = ["name", "panels"]
-schema_family["properties"]["samples"]["items"]["properties"]["capture_kit"] = {
+schema_sample = schema_family["properties"]["samples"]["items"]
+del schema_sample["properties"]["container"]
+del schema_sample["properties"]["container_name"]
+del schema_sample["properties"]["quantity"]
+del schema_sample["properties"]["source"]
+del schema_sample["properties"]["well_position"]
+schema_sample["properties"]["capture_kit"] = {
     "enum": constants.CAPTURE_KITS
 }
