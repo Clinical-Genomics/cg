71817ec6363865fe3cfde4793e91b5a0807c6c83
Robin Andeer <robin.andeer@gmail.com>
2017-07-19 12:51:58 +0200

    add cli to display status of famlies

--- cg/cli/status.py
+++ cg/cli/status.py
@@ -18,3 +18,21 @@ def analysis(context):
     records = context.obj['db'].families_to_analyze()
     for family_obj in records:
         click.echo(family_obj)
+
+
+@status.command()
+@click.option('-s', '--skip', default=0, help='skip initial families')
+@click.pass_context
+def families(context, skip):
+    """View status of families."""
+    records = context.obj['db'].families().offset(skip).limit(30)
+    for family_obj in records:
+        color = 'red' if family_obj.priority > 1 else 'blue'
+        message = f"{family_obj.internal_id} ({family_obj.priority})"
+        if family_obj.analyses:
+            message += f" {family_obj.analyses[0].analyzed_at.date()}"
+            color = 'green'
+        if family_obj.analyze:
+            message += ' [ANALYZE]'
+            color = 'orange'
+        click.echo(click.style(message, fg=color))
