8868031038d008ec0a04220bc1017c9d006eed91
Robin Andeer <robin.andeer@gmail.com>
2017-08-28 10:08:14 +0200

    update capture kit keys to MIP 5

--- cg/meta/analysis.py
+++ cg/meta/analysis.py
@@ -16,11 +16,11 @@ COMBOS = {
     'CM': ('CNM', 'CM'),
     'Horsel': ('Horsel', '141217', '141201'),
 }
-CAPTUREKIT_MAP = {'Agilent Sureselect CRE': 'Agilent_SureSelectCRE.V1',
-                  'SureSelect CRE': 'Agilent_SureSelectCRE.V1',
-                  'Agilent Sureselect V5': 'Agilent_SureSelect.V5',
-                  'SureSelect Focused Exome': 'Agilent_SureSelectFocusedExome.V1',
-                  'other': 'Agilent_SureSelectCRE.V1'}
+CAPTUREKIT_MAP = {'Agilent Sureselect CRE': 'agilent_sureselect_cre.v1',
+                  'SureSelect CRE': 'agilent_sureselect_cre.v1',
+                  'Agilent Sureselect V5': 'agilent_sureselect.v5',
+                  'SureSelect Focused Exome': 'agilent_sureselect_focusedexome.v1',
+                  'other': 'agilent_sureselect_cre.v1'}
 
 
 class AnalysisAPI():
