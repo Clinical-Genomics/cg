bb5f7c9a6092d7e3b37f359c963ff3073cf5a1b9
Robin Andeer <robin.andeer@gmail.com>
2017-09-04 10:08:03 +0200

    clean up install scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,4 +9,15 @@ pip install --upgrade --no-deps git+https://github.com/Clinical-Genomics/genotyp
 pip install --upgrade --no-deps git+https://github.com/Clinical-Genomics/cg@cg2
 
 # API server
+
 conda install gunicorn
+
+# Setup databases
+
+chanjo-stage db setup --reset
+chanjo-stage link /mnt/hds/proj/cust000/STAGE/resources/scout.exons.bed
+hk-stage init --reset --force
+tb-stage init --reset --force
+tb-stage user --name "Robin Andeer" robin.andeer@scilifelab.se
+gt-stage init --reset resources/genotype.snps.grch37.txt
+cg-stage init --reset --force
