d4d340946e66773402a20666f6b61d8b28512dc5
Kenny Billiau <kenny.billiau@scilifelab.se>
2017-12-07 13:59:10 +0100

    Make sure dirs exist and link do not before delivering post-analysis files

--- cg/cli/deliver.py
+++ cg/cli/deliver.py
@@ -6,6 +6,7 @@ import click
 import logging
 from pathlib import Path
 
+from cg.apps import tb, hk, lims
 from cg.store import Store
 from cg.meta.deliver.api import DeliverAPI
 
@@ -28,36 +29,62 @@ def deliver(context):
 def inbox(context, dry, family, version, tag, inbox):
     """Link files from HK to cust inbox."""
 
-    deliver_api = DeliverAPI(context.obj)
     db = Store(context.obj['database'])
-
-    family_files = deliver_api.get_post_analysis_family_files(family=family, version=version, tag=tag)
-    link_obj = db.family_samples(family)
+    deliver_api = DeliverAPI(
+        db=db,
+        hk_api=hk.HousekeeperAPI(context.obj),
+        lims_api=lims.LimsAPI(context.obj)
+    )
 
     family_obj = db.family(family)
-    for file_obj in family_files:
-        out_path = Path(inbox.format(family=family_obj.name, customer=family_obj.customer.internal_id))
+    if family_obj is None:
+        LOG.error(f"Family '{family}' not found.")
+        context.abort()
 
+    family_files = deliver_api.get_post_analysis_family_files(family=family, version=version, tag=tag)
+    if not family_files:
+        LOG.warning(f"No family files found.")
+
+    for file_obj in family_files:
+        out_dir = Path(inbox.format(family=family_obj.name, customer=family_obj.customer.internal_id))
+        out_dir.mkdir(parents=True, exist_ok=True)
+
+        # might be fun to name exit files according to tags instead of MIP's long name
         #file_name = Path('_'.join([ tag.name for tag in file_obj.tags ]))
         #file_ext = ''.join(Path(file_obj.path).suffixes)
-        file_name = Path(file_obj.path).name.replace(family, family_obj.name)
-        new_path = f"{out_path / file_name}"
+        out_file_name = Path(file_obj.path).name.replace(family, family_obj.name)
+        out_path = Path(f"{out_dir/ out_file_name}")
+        in_path = Path(file_obj.full_path)
 
-        os.link(file_obj.full_path, new_path)
-        LOG.info(f"linked file: {file_obj.full_path} -> {new_path}")
+        if not out_path.exists():
+            os.link(in_path, out_path)
+            LOG.info(f"linked file: {in_path} -> {out_path}")
+        else:
+            LOG.info(f"Target file exists: {out_path}")
 
-    sample_files = {}
+    link_obj = db.family_samples(family)
+    if not link_obj:
+        LOG.warning(f"No sample files found.")
+
     for family_sample in link_obj:
         sample_obj = family_sample.sample
         sample_files = deliver_api.get_post_analysis_sample_files(family=family, sample=sample_obj.internal_id, version=version, tag=tag)
 
+        if not sample_files:
+            LOG.warning(f"No sample files found for '{sample_obj.internal_id}'.")
+
         for file_obj in sample_files:
-            out_path = Path(inbox.format(family=family_obj.name, customer=family_obj.customer.internal_id))
-            out_path = out_path.joinpath(sample_obj.name)
+            out_dir = Path(inbox.format(family=family_obj.name, customer=family_obj.customer.internal_id))
+            out_dir = out_dir.joinpath(sample_obj.name)
+            out_dir.mkdir(parents=True, exist_ok=True)
 
-            file_name = Path(file_obj.path).name.replace(sample_obj.internal_id, sample_obj.name)
-            new_path = f"{out_path / file_name}"
+            out_file_name = Path(file_obj.path).name.replace(sample_obj.internal_id, sample_obj.name)
+            out_path = Path(f"{out_dir / out_file_name}")
+            in_path = Path(file_obj.full_path)
 
-            os.link(file_obj.full_path, new_path)
-            LOG.info(f"linked file: {file_obj.full_path} -> {new_path}")
+            if not out_path.exists():
+                os.link(in_path, out_path)
+                LOG.info(f"linked file: {in_path} -> {out_path}")
+            else:
+                LOG.info(f"Target file exists: {out_path}")
 
