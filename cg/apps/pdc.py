0df6bdae7df73351d91ae95e65a7c5417100cea0
Robin Andeer <robin.andeer@gmail.com>
2017-12-13 15:09:32 +0100

    add skeleton to pdc/backup functionality

---
+++ cg/apps/pdc.py
@@ -0,0 +1,10 @@
+
+
+class PdcApi():
+
+    def __init__(self, config: dict):
+        pass
+
+    def retrieve_flowcell(self, flowcell_id: str):
+        """Fetch a flowcell back from the backup solution."""
+        pass
