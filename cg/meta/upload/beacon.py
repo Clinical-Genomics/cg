6ef7b50966ab481929d6b0f099cfa59d69e4e9c8
northwestwitch <rasi.chiara@gmail.com>
2018-01-26 14:31:51 +0100

    do some printing

--- cg/meta/upload/beacon.py
+++ cg/meta/upload/beacon.py
@@ -63,7 +63,7 @@ class UploadBeaconApi():
                     while n_panels:
                         for line in ins:
                             if line.startswith("##gene_panel="):
-                                print(line.strip().lstrip(12))
+                                print(lstrip(line.strip()))
 
                                 n_panels -= 1
                 temp_panel.close()
