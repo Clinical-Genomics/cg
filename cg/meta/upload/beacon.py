fd58c06372cf9289c2f67c5b92bff30a261de382
northwestwitch <rasi.chiara@gmail.com>
2018-01-30 08:51:07 +0100

    checking code

--- cg/meta/upload/beacon.py
+++ cg/meta/upload/beacon.py
@@ -147,9 +147,9 @@ class UploadBeaconApi():
                         # Chech that path to VCF file with vars that went into beacon exists and get samples contained in that VCF file:
                         vcf_samples = vcfparser.get_samples(beacon_info[1])
                         if sample.internal_id in vcf_samples:
-                            print("here:#",beacon_info[2][1:-1],"#")
+                            print("here:#",beacon_info[3][1:-1],"#", sep="")
 
-                            panel_list = list(ast.literal_eval(beacon_info[2]))
+                            panel_list = list(ast.literal_eval(beacon_info[3]))
                             print("here2")
 
                             for panel in panel_list:
