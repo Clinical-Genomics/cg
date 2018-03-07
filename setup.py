1657b752f8ac7e302efe2136cd3663cd35ee7dfa
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2018-03-07 15:23:45 +0100

    Added PyTest plugin as per Chanjo

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 import os
 import sys
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist bdist_wheel upload')
@@ -25,6 +26,25 @@ def parse_reqs(req_path='./requirements.txt'):
                 # add the line as a new requirement
                 install_requires.append(line)
     return install_requires
+
+
+# This is a plug-in for setuptools that will invoke py.test
+# when you run python setup.py test
+class PyTest(TestCommand):
+
+    """Set up the py.test test runner."""
+
+    def finalize_options(self):
+        """Set options for the command line."""
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        """Execute the test runner command."""
+        # Import here, because outside the required eggs aren't loaded yet
+        import pytest
+        sys.exit(pytest.main(self.test_args))
 
 
 setup(
