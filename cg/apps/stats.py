83edd46b9007329dd86dc79309f27984153a6d60
Kenny Billiau <kenny.billiau@scilifelab.se>
2018-03-11 23:05:24 +0100

    Fix sampleid/samplename in cgstats app

--- cg/apps/stats.py
+++ cg/apps/stats.py
@@ -94,6 +94,6 @@ class StatsAPI(alchy.Manager):
         base_pattern = "*{}/Unaligned*/Project_*/Sample_{}/*.fastq.gz"
         alt_pattern = "*{}/Unaligned*/Project_*/Sample_{}_*/*.fastq.gz"
         for fastq_pattern in (base_pattern, alt_pattern):
-            pattern = fastq_pattern.format(flowcell, sample_obj.sampleid)
+            pattern = fastq_pattern.format(flowcell, sample_obj.samplename)
             files = self.root_dir.glob(pattern)
             yield from files
