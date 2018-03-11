f6ce20652b3293beb586651b7a02a31669d18999
Kenny Billiau <kenny.billiau@scilifelab.se>
2018-03-11 23:05:44 +0100

    Bump version: 1.4.3 → 1.4.4

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def parse_reqs(req_path='./requirements.txt'):
 
 setup(
     name='cg',
-    version='1.4.3',
+    version='1.4.4',
     description='Clinical Genomics command center.',
     author='Robin Andeer',
     author_email='robin.andeer@scilifelab.se',
