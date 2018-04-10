1d23d98a2a4df3b463dccab5c93b051e2887063e
Kenny Billiau <kenny.billiau@scilifelab.se>
2018-04-10 11:18:57 +0200

    Bump version: 1.4.6 → 1.4.7

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class PyTest(TestCommand):
 
 setup(
     name='cg',
-    version='1.4.6',
+    version='1.4.7',
     description='Clinical Genomics command center.',
     author='Robin Andeer',
     author_email='robin.andeer@scilifelab.se',
