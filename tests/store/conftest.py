0f297164cb72219639cee100a4cfe5b590f1fcf7
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2018-06-05 10:13:01 +0200

    Removed empty fixture

--- tests/store/conftest.py
+++ tests/store/conftest.py
@@ -3,10 +3,3 @@ import datetime as dt
 import pytest
 
 
-@pytest.fixture(scope='function')
-def store_families(base_store):
-    """Populate store with families in different states."""
-    # just added family
-    # family read to be analyzed
-    # family partly sequenced
-    pass
