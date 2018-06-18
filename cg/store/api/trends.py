164e3ec001415e31bd7f87370bd9c272549150a5
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2018-06-18 10:10:24 +0200

    view trends by year

--- cg/store/api/trends.py
+++ cg/store/api/trends.py
@@ -1,6 +1,6 @@
 import datetime as dt
 from itertools import groupby
-
+from dateutil.relativedelta import relativedelta
 from sqlalchemy import func, text
 
 from cg.store import models
@@ -8,23 +8,44 @@ from cg.store import models
 MONTHS = {1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May', 6: 'June',
           7: 'July', 8: 'August', 9: 'September', 10: 'October', 11: 'November', 12: 'December'}
 
-SAMPLES_FROM_DATE = dt.datetime(2017, 12, 31)
-
 
 class TrendsHandler:
 
-    def samples_per_month(self):
+    def get_last_day_of_previous_year(self, year: int):
+        return dt.date(year, 1, 1) - relativedelta(days=1)
+
+    def get_from_date(self, year):
+        print(self.get_last_day_of_previous_year(int(year)))
+
+        return self.get_last_day_of_previous_year(int(year))
+
+    def get_tom_date(self, year):
+        year = int(year)
+        today = dt.date.today()
+        month = today.month if today.year == year else 12
+
+        if today.year == year:
+            return dt.date(year, month, 1) - relativedelta(days=1)
+        else:
+            return dt.date(year, 12, 31) + relativedelta(days=1)
+
+    def samples_per_month(self, year):
         """Fetch samples per month."""
+
+        print( self.get_tom_date(year))
+
         query = (
             self.session.query(
                 models.Customer.priority.label('priority'),
                 func.month(models.Sample.received_at).label('month_no'),
                 func.count(models.Sample.id).label('count'),
             )
-            .join(models.Sample.customer)
-            .filter(models.Sample.received_at > SAMPLES_FROM_DATE)
-            .group_by(models.Customer.priority, func.month(models.Sample.received_at))
+                .join(models.Sample.customer)
+                .filter(self.get_from_date(year) < models.Sample.received_at,
+                        models.Sample.received_at < self.get_tom_date(year))
+                .group_by(models.Customer.priority, func.month(models.Sample.received_at))
         )
+
         for cust_priority, results in groupby(query, key=lambda result: result.priority):
             counts = {MONTHS[result.month_no]: result.count for result in results}
             data = {
@@ -36,26 +57,27 @@ class TrendsHandler:
             }
             yield data
 
-    def received_to_delivered(self):
+    def received_to_delivered(self, year):
         """Calculate averages from received to delivered."""
         query = (
             self.session.query(
                 models.Application.category.label('category'),
                 func.month(models.Sample.received_at).label('month_no'),
                 (func.avg(func.datediff(models.Sample.delivered_at, models.Sample.received_at))
-                     .label('average')),
+                 .label('average')),
             )
-            .join(
+                .join(
                 models.Sample.customer,
                 models.Sample.application_version,
                 models.ApplicationVersion.application,
             )
-            .filter(
+                .filter(
                 models.Customer.priority == 'diagnostic',
-                models.Sample.received_at > SAMPLES_FROM_DATE,
-                models.Sample.delivered_at != None,
+                self.get_from_date(year) < models.Sample.received_at,
+                models.Sample.received_at < self.get_tom_date(year),
+                models.Sample.delivered_at is not None,
             )
-            .group_by(
+                .group_by(
                 models.Application.category,
                 func.month(models.Sample.received_at),
             )
@@ -71,25 +93,26 @@ class TrendsHandler:
                 } for month in MONTHS.values()]
             }
 
-    def received_to_prepped(self):
+    def received_to_prepped(self, year):
         """Calculate averages to prepp samples."""
         query = (
             self.session.query(
                 models.Application.category.label('category'),
                 func.month(models.Sample.received_at).label('month_no'),
                 (func.avg(func.datediff(models.Sample.prepared_at, models.Sample.received_at))
-                     .label('average')),
+                 .label('average')),
             )
-            .join(
+                .join(
                 models.Sample.customer,
                 models.Sample.application_version,
                 models.ApplicationVersion.application,
             )
-            .filter(
+                .filter(
                 models.Customer.priority == 'diagnostic',
-                models.Sample.received_at > SAMPLES_FROM_DATE
+                self.get_from_date(year) < models.Sample.received_at,
+                models.Sample.received_at < self.get_tom_date(year),
             )
-            .group_by(
+                .group_by(
                 models.Application.category,
                 func.month(models.Sample.received_at),
             )
@@ -105,24 +128,27 @@ class TrendsHandler:
                 } for month in MONTHS.values()]
             }
 
-    def prepped_to_sequenced(self):
+    def prepped_to_sequenced(self, year):
         """Calculate average to sequence samples."""
         query = (
             self.session.query(
                 models.Application.category.label('category'),
                 func.month(models.Sample.received_at).label('month_no'),
-                func.avg(func.datediff(models.Sample.sequenced_at, models.Sample.prepared_at)).label('average'),
+                func.avg(
+                    func.datediff(models.Sample.sequenced_at, models.Sample.prepared_at)).label(
+                    'average'),
             )
-            .join(
+                .join(
                 models.Sample.customer,
                 models.Sample.application_version,
                 models.ApplicationVersion.application,
             )
-            .filter(
+                .filter(
                 models.Customer.priority == 'diagnostic',
-                models.Sample.received_at > SAMPLES_FROM_DATE
+                self.get_from_date(year) < models.Sample.received_at,
+                models.Sample.received_at < self.get_tom_date(year),
             )
-            .group_by(
+                .group_by(
                 models.Application.category,
                 func.month(models.Sample.received_at),
             )
@@ -137,24 +163,44 @@ class TrendsHandler:
                     'average': averages.get(month) or None,
                 } for month in MONTHS.values()]
             }
-    
-    def sequenced_to_delivered(self):
+
+    def sequenced_to_delivered(self, year):
         """Calculate average to deliver samples."""
         query = (
             self.session.query(
                 models.Application.category.label('category'),
                 func.month(models.Sample.received_at).label('month_no'),
-                func.avg(func.datediff(models.Sample.delivered_at, models.Sample.sequenced_at)).label('average'),
+                func.avg(
+                    func.datediff(models.Sample.delivered_at, models.Sample.sequenced_at)).label(
+                    'average'),
             )
-            .join(
+                .join(
                 models.Sample.customer,
                 models.Sample.application_version,
                 models.ApplicationVersion.application,
             )
-            .filter(
+                .filter(
                 models.Customer.priority == 'diagnostic',
-                models.Sample.received_at > SAMPLES_FROM_DATE,
-                models.Sample.delivered_at != None,
+                self.get_from_date(year) < models.Sample.received_at,
+                models.Sample.received_at < self.get_tom_date(year),
+                models.Sample.delivered_at is not None,
+            )
+                .group_by(
+                models.Application.category,
+                func.month(models.Sample.received_at),
+            )
+        )
+        for category, results in groupby(query, key=lambda result: result.category):
+            averages = {MONTHS[result.month_no]: float(result.average) if result.average else None
+                        for result in results}
+            yield {
+                'category': category,
+                'results': [{
+                    'month': month,
+                    'average': averages.get(month) or None,
+                } for month in MONTHS.values()]
+            }
+
             )
             .group_by(
                 models.Application.category,
