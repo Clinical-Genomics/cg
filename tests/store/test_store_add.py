5d554cbb7a1990d32dc896943ad637cb81631d45
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2018-06-28 15:48:43 +0200

    Added method to add customer group, required for new customer

--- tests/store/test_store_add.py
+++ tests/store/test_store_add.py
@@ -7,9 +7,11 @@ def test_add_customer(store: Store):
     # GIVEN an empty database
     assert store.Customer.query.first() is None
     internal_id, name, scout_access = 'cust000', 'Test customer', True
+    customer_group = store.add_customer_group('dummy_group', 'dummy group')
 
-    # WHEN adding a new user
-    new_customer = store.add_customer(internal_id=internal_id, name=name, scout_access=scout_access)
+    # WHEN adding a new customer
+    new_customer = store.add_customer(internal_id=internal_id, name=name,
+                                      scout_access=scout_access, customer_group=customer_group)
     store.add_commit(new_customer)
 
     # THEN it should be stored in the database
@@ -18,7 +20,9 @@ def test_add_customer(store: Store):
 
 def test_add_user(store: Store):
     # GIVEN a database with a customer in it that we can connect the user to
-    customer = store.add_customer(internal_id='custtest', name="Test Customer", scout_access=False)
+    customer_group = store.add_customer_group('dummy_group', 'dummy group')
+    customer = store.add_customer(internal_id='custtest', name="Test Customer",
+                                  scout_access=False, customer_group=customer_group)
     store.add_commit(customer)
 
     # WHEN adding a new user
