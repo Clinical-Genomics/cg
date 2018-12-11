a42118d028b284a2803332ad470738fc9d86bdb6
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2018-12-11 14:50:24 +0100

    Revert "test lint fail"
    This reverts commit 298cc5f29fd737929d4327c24e10f42ff17b7897.

--- cg/constants.py
+++ cg/constants.py
@@ -16,36 +16,10 @@ CAPTUREKIT_OPTIONS = ('Agilent Sureselect CRE',
 CAPTUREKIT_CANCER_OPTIONS = ('Twist exome v1.3',
                              'Twist panel CG001',
                              'Nimblegen MSK-IMPACT')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-FLOWCELL_STATUS = ('ondisk', 'removed', 'requested',
-
-
-
-
-
-                   'processing')
-
-
-
+FLOWCELL_STATUS = ('ondisk', 'removed', 'requested', 'processing')
 METAGENOME_SOURCES = (
     'blood',
     'skin',
-
-
     'respiratory',
     'urine',
     'CSF',
@@ -53,14 +27,6 @@ METAGENOME_SOURCES = (
     'environmental',
     'unknown',
     'other')
-
-
-
-
-
-
-
-
 ANALYSIS_SOURCES = (
     'blood',
     'buccal swab',
