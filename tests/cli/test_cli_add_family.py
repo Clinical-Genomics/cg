bf9390bef35e4628a8ea72aeebc08d9bffafd388
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2018-12-11 14:24:54 +0100

    documentet the tests

--- tests/cli/test_cli_add_family.py
+++ tests/cli/test_cli_add_family.py
@@ -1,9 +1,11 @@
-import datetime
+"""This script tests the cli methods to add families to status-db"""
+from datetime import datetime
 
 from cg.store import Store
 
 
 def test_add_family_required(invoke_cli, disk_store: Store):
+    """Test to add a family using only the required arguments"""
     # GIVEN a database with a customer and an panel
 
     # WHEN adding a family
@@ -24,6 +26,7 @@ def test_add_family_required(invoke_cli, disk_store: Store):
 
 
 def test_add_family_bad_customer(invoke_cli, disk_store: Store):
+    """Test to add a family using a non-existing customer"""
     # GIVEN an empty database
 
     # WHEN adding a family
@@ -40,6 +43,7 @@ def test_add_family_bad_customer(invoke_cli, disk_store: Store):
 
 
 def test_add_family_bad_panel(invoke_cli, disk_store: Store):
+    """Test to add a family using a non-existing panel"""
     # GIVEN a database with a customer
 
     # WHEN adding a family
@@ -56,6 +60,7 @@ def test_add_family_bad_panel(invoke_cli, disk_store: Store):
 
 
 def test_add_family_priority(invoke_cli, disk_store: Store):
+    """Test that the added family get the priority we send in"""
     # GIVEN a database with a customer and an panel
 
     # WHEN adding a family
@@ -77,6 +82,7 @@ def test_add_family_priority(invoke_cli, disk_store: Store):
 
 
 def add_customer(disk_store, customer_id='cust_test'):
+    """utility function to add a customer to use in tests"""
     customer_group = disk_store.add_customer_group('dummy_group', 'dummy group')
     customer = disk_store.add_customer(internal_id=customer_id, name="Test Customer",
                                        scout_access=False, customer_group=customer_group)
@@ -85,21 +91,22 @@ def add_customer(disk_store, customer_id='cust_test'):
 
 
 def add_panel(disk_store, panel_id='panel_test', customer_id='cust_test'):
+    """utility function to add a panel to use in tests"""
     customer = disk_store.customer(customer_id)
     panel = disk_store.add_panel(customer=customer, name=panel_id, abbrev=panel_id,
-        version=1.0,
-                                 date=datetime.datetime.now(), genes=1)
+                                 version=1.0,
+                                 date=datetime.now(), genes=1)
     disk_store.add_commit(panel)
     return panel_id
 
 
 def add_application(disk_store, application_tag='dummy_tag'):
-
+    """utility function to add an application to use in tests"""
     application = disk_store.add_application(tag=application_tag, category='wgs',
                                              description='dummy_description')
     disk_store.add_commit(application)
     prices = {'standard': 10, 'priority': 20, 'express': 30, 'research': 5}
-    version = disk_store.add_version(application, 1, valid_from=datetime.datetime.now(),
+    version = disk_store.add_version(application, 1, valid_from=datetime.now(),
                                      prices=prices)
 
     disk_store.add_commit(version)
