6d8aa0fce175d8e50a456688be1cea5f8781c8f0
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-01-15 10:31:14 +0100

    fixes pep8 formatting

--- cg/cli/analysis.py
+++ cg/cli/analysis.py
@@ -1,11 +1,10 @@
 # -*- coding: utf-8 -*-
 import logging
-import sys
 import shutil
+import sys
 from pathlib import Path
 
 import click
-
 from cg.apps import hk, tb, scoutapi, lims
 from cg.apps.balsamic.fastq import FastqHandler
 from cg.exc import LimsDataError
@@ -143,8 +142,8 @@ def panel(context, print_output, family_id):
 @START_WITH_PROGRAM
 @click.argument('family_id')
 @click.pass_context
-def start(context: click.Context, family_id: str, priority: str=None, email: str=None,
-          start_with: str=None):
+def start(context: click.Context, family_id: str, priority: str = None, email: str = None,
+          start_with: str = None):
     """Start the analysis pipeline for a family."""
     family_obj = context.obj['db'].family(family_id)
     if family_obj is None:
