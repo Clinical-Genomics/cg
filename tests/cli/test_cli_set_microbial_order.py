28a6bd699e6426246f0c5bf2a77ed37b0df1587b
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-02-07 11:36:02 +0100

    adds missing parameters for add_microbial_sample

--- tests/cli/test_cli_set_microbial_order.py
+++ tests/cli/test_cli_set_microbial_order.py
@@ -56,53 +56,62 @@ def test_application(invoke_cli, disk_store: Store):
     assert signature in disk_store.MicrobialSample.query.first().comment
 
 
-def ensure_application_version(disk_store, application_tag='dummy_tag'):
+def ensure_application_version(store, application_tag='dummy_tag'):
     """utility function to return existing or create application version for tests"""
-    application = disk_store.application(tag=application_tag)
+    application = store.application(tag=application_tag)
     if not application:
-        application = disk_store.add_application(tag=application_tag, category='wgs',
+        application = store.add_application(tag=application_tag, category='wgs',
                                                  description='dummy_description')
-        disk_store.add_commit(application)
+        store.add_commit(application)
 
     prices = {'standard': 10, 'priority': 20, 'express': 30, 'research': 5}
-    version = disk_store.application_version(application, 1)
+    version = store.application_version(application, 1)
     if not version:
-        version = disk_store.add_version(application, 1, valid_from=datetime.now(), prices=prices)
-        disk_store.add_commit(version)
+        version = store.add_version(application, 1, valid_from=datetime.now(), prices=prices)
+        store.add_commit(version)
     return version
 
 
-def ensure_customer(disk_store, customer_id='cust_test'):
+def ensure_customer(store, customer_id='cust_test'):
     """utility function to return existing or create customer for tests"""
     customer_group_id = customer_id + '_group'
-    customer_group = disk_store.customer_group(customer_group_id)
+    customer_group = store.customer_group(customer_group_id)
     if not customer_group:
-        customer_group = disk_store.add_customer_group(customer_group_id, customer_group_id)
+        customer_group = store.add_customer_group(customer_group_id, customer_group_id)
 
-        customer = disk_store.add_customer(internal_id=customer_id, name="Test Customer",
-                                           scout_access=False, customer_group=customer_group,
-                                           invoice_address='dummy_address',
-                                           invoice_reference='dummy_reference')
-        disk_store.add_commit(customer)
-    customer = disk_store.customer(customer_id)
+        customer = store.add_customer(internal_id=customer_id, name="Test Customer",
+                                      scout_access=False, customer_group=customer_group,
+                                      invoice_address='dummy_address',
+                                      invoice_reference='dummy_reference')
+        store.add_commit(customer)
+    customer = store.customer(customer_id)
     return customer
 
 
-def add_microbial_sample(disk_store, order_id='sample_test', sex='female'):
+def add_organism(store):
+    organism = store.add_organism(internal_id='organism_id', name= 'organism_name')
+    return organism
+
+
+def add_microbial_sample(store, sample_id='sample_test'):
     """utility function to add a sample to use in tests"""
-    customer = ensure_customer(disk_store)
-    application_version_id = ensure_application_version(disk_store).id
-    sample = disk_store.add_microbial_sample(name=order_id, sex=sex)
-    sample.application_version_id = application_version_id
+    customer = ensure_customer(store)
+    application_version = ensure_application_version(store)
+    organism = add_organism(store)
+    sample = store.add_microbial_sample(name=sample_id,
+                                        organism=organism,
+                                        internal_id=sample_id,
+                                        reference_genome='test',
+                                        application_version=application_version)
     sample.customer = customer
-    disk_store.add_commit(sample)
+    store.add_commit(sample)
     return sample
 
 
-def add_microbial_order(disk_store, order_id='order_test', customer_id='cust_test'):
+def add_microbial_order(store, order_id='order_test', customer_id='cust_test'):
     """utility function to set a family to use in tests"""
-    customer = ensure_customer(disk_store, customer_id)
-    order = disk_store.add_microbial_order(name=order_id, customer=customer, ordered=datetime.now())
+    customer = ensure_customer(store, customer_id)
+    order = store.add_microbial_order(name=order_id, customer=customer, ordered=datetime.now())
     order.customer = customer
-    disk_store.add_commit(order)
+    store.add_commit(order)
     return order
