07e0173bacc5d832e8cbada44e82a52f9b045597
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-05-08 17:21:43 +0200

    uses different different analysis types

--- tests/apps/lims/conftest.py
+++ tests/apps/lims/conftest.py
@@ -5,7 +5,7 @@ from cg.apps.lims.api import LimsAPI
 @pytest.fixture
 def balsamic_orderform():
     """Orderform fixture for Balsamic samples"""
-    return 'tests/fixtures/orderforms/1508.17.mip_balsamic.xlsx'
+    return 'tests/fixtures/orderforms/1508.17.balsamic.xlsx'
 
 
 @pytest.fixture
@@ -17,7 +17,7 @@ def external_orderform():
 @pytest.fixture
 def fastq_orderform():
     """Orderform fixture for fastq samples"""
-    return 'tests/fixtures/orderforms/1508.17.mip_balsamic.xlsx'
+    return 'tests/fixtures/orderforms/1508.17.fastq.xlsx'
 
 
 @pytest.fixture
@@ -35,7 +35,7 @@ def microbial_orderform():
 @pytest.fixture
 def mip_orderform():
     """Orderform fixture for MIP samples"""
-    return 'tests/fixtures/orderforms/1508.17.mip_balsamic.xlsx'
+    return 'tests/fixtures/orderforms/1508.17.mip.xlsx'
 
 
 @pytest.fixture
