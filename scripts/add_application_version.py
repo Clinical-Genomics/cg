7e054aed548c742d2fdc128f04e2f397149eb8cb
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-05-21 14:55:24 +0200

    fix some pep8 issues

--- scripts/add_application_version.py
+++ scripts/add_application_version.py
@@ -1,47 +1,49 @@
-"""Scrpt to load new application versions to status-db"""
+"""Script to load new application versions to status-db"""
 import csv
-from datetime import datetime
-from argparse import ArgumentParser
 import logging
 import sys
+from argparse import ArgumentParser
+from datetime import datetime
+
 from sqlalchemy import create_engine, MetaData
 from sqlalchemy.exc import SQLAlchemyError
 
 logging.basicConfig(level=logging.INFO)
 
-DESC= """Scrpt to load new application versions to status-db"""
+DESC = """Script to load new application versions to status-db"""
 
-VALID_HEADERS = ['App tag', 'Version', 'Valid from', 'Standard', 
-                'Priority', 'Express', 'Research', 'Comment']
+VALID_HEADERS = ['App tag', 'Version', 'Valid from', 'Standard',
+                 'Priority', 'Express', 'Research', 'Comment']
 
-def add_application_version(file_path, conection_string, sign):
+
+def add_application_version(file_path, connection_string, sign):
     """
     Args:
-        file_path:          Path to csv file with heathers: 'App tag', 'Valid from', 'Standard', 
-                            'Priority', 'Express', 'Research'
-        connection_string: 'mysql+pymysql://<user>:<password>@<hoast>:<port>/cg'
+        :param file_path:          Path to csv file with heathers: 'App tag', 'Valid from',
+                                   'Standard', 'Priority', 'Express', 'Research'
+        :param connection_string:  'mysql+pymysql://<user>:<password>@<host>:<port>/cg'
+        :param sign:               Signature of user running the script
         """
     try:
-        engine = create_engine(conection_string)
-        connection = engine.connect() 
+        engine = create_engine(connection_string)
+        connection = engine.connect()
         metadata = MetaData()
-        metadata.reflect(bind=engine)   
+        metadata.reflect(bind=engine)
         table = metadata.tables['application_version']
     except SQLAlchemyError as error:
-        sys.exit('Failed to connect: %s' % (error))
+        sys.exit('Failed to connect: %s' % error)
 
     try:
-        f = open(file_path)
-        rows=csv.DictReader(f, delimiter=',')
+        file_handle = open(file_path)
+        rows = csv.DictReader(file_handle, delimiter=',')
     except IOError as error:
-        sys.exit('Failed read file: %s' % (error))
+        sys.exit('Failed read file: %s' % error)
 
     if not len(set(rows.fieldnames)) == len(rows.fieldnames):
-        sys.exit('Heades in file are not unique')
+        sys.exit('Headers in file are not unique')
 
     if not set(rows.fieldnames) < set(VALID_HEADERS):
         sys.exit('Headers in file are not valid. Should contain: %s' % (str(VALID_HEADERS)))
-    
 
     for row in rows:
         tag = row['App tag']
@@ -51,40 +53,40 @@ def add_application_version(file_path, conection_string, sign):
                 where application.tag="{tag}"'
         latest_version, app_tag, app_id = engine.execute(query).first()
         if not latest_version:
-            latest_version=0
+            latest_version = 0
         if app_tag:
             ins = table.insert().values(
-                application_id = app_id, 
-                version = latest_version + 1, 
-                valid_from = datetime.strptime(row['Valid from'], '%Y-%m-%d'),  
-                price_standard = row['Standard'], 
-                price_priority = row['Priority'], 
-                price_express = row['Express'], 
-                price_research = row['Research'], 
-                comment = 'Added by %s' % (sign),
-                created_at = datetime.today())
-            try: 
+                application_id=app_id,
+                version=latest_version + 1,
+                valid_from=datetime.strptime(row['Valid from'], '%Y-%m-%d'),
+                price_standard=row['Standard'],
+                price_priority=row['Priority'],
+                price_express=row['Express'],
+                price_research=row['Research'],
+                comment='Added by %s' % sign,
+                created_at=datetime.today())
+            try:
                 connection.execute(ins)
-                logging.info('adding new version for app tag %s' % (app_tag))
+                logging.info('adding new version for app tag %s', app_tag)
             except SQLAlchemyError as error:
                 logging.error(error)
-    f.close()
+    file_handle.close()
 
 
-def main(file, connection_string, sign):
-    """Running add_application_version to load applicaiton veriosn"""
-    add_application_version(file, connection_string, sign)
+def main(file_path, connection_string, sign):
+    """Running add_application_version to load application version"""
+    add_application_version(file_path, connection_string, sign)
 
 
 if __name__ == "__main__":
     parser = ArgumentParser(description=DESC)
-    parser.add_argument('-f', dest = 'file', required=True,
+    parser.add_argument('-f', dest='file_path', required=True,
                         help='Path to csv with apptags.')
-    parser.add_argument('-c', dest = 'connection_string', required=True,
-                        help='Connection string to connect with database.') 
-    parser.add_argument('-s', dest = 'sign', required=True,
-                        help = 'Signature.')
-                          
+    parser.add_argument('-c', dest='connection_string', required=True,
+                        help='Connection string to connect with database.')
+    parser.add_argument('-s', dest='sign', required=True,
+                        help='Signature.')
+
     args = parser.parse_args()
 
-    main(args.file, args.connection_string, args.sign)     
+    main(args.file_path, args.connection_string, args.sign)
