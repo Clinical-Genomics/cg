d814dc7ee327de56ac514999e71030b14be7d9b8
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-05-28 13:49:14 +0200

    fix pep8

--- cg/apps/lims/orderform.py
+++ cg/apps/lims/orderform.py
@@ -181,6 +181,7 @@ def parse_sample(raw_sample):
 
     if raw_sample['UDF/priority'].lower() == 'förtur':
         raw_sample['UDF/priority'] = 'priority'
+    raw_source = raw_sample['UDF/Source'] if raw_sample.get('UDF/Source') in SOURCE_TYPES else None,
     sample = {
         'application': raw_sample['UDF/Sequencing Analysis'],
         'capture_kit': raw_sample.get('UDF/Capture Library version'),
@@ -209,8 +210,7 @@ def parse_sample(raw_sample):
         'require_qcok': raw_sample.get('UDF/Process only if QC OK') == 'yes',
         'rml_plate_name': raw_sample.get('UDF/RML plate name'),
         'sex': REV_SEX_MAP.get(raw_sample.get('UDF/Gender', '').strip()),
-        'source': raw_sample.get('UDF/Source') if raw_sample.get('UDF/Source') in SOURCE_TYPES
-                  else None,
+        'source': raw_source,
         'status': raw_sample['UDF/Status'].lower() if raw_sample.get('UDF/Status') else None,
         'tissue_block_size': raw_sample.get('UDF/Tissue Block Size'),
         'tumour': raw_sample.get('UDF/tumor') == 'yes',
