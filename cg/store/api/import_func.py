10a4ab97cea01a246e0799c1dc068ae7b2f21061
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-06-03 08:06:16 +0200

    remove import of created_at to allow it to be set by database

--- cg/store/api/import_func.py
+++ cg/store/api/import_func.py
@@ -192,6 +192,5 @@ def add_application_from_raw(raw_application, sign, store: Store):
         comment=raw_application['comment'] + ' Added by %s' % sign,
         is_archived=raw_application['is_archived'] == 1.0,
         is_external=raw_application['is_external'] == 1.0,
-        created_at=raw_application['created_at'],
     )
     return new_application
