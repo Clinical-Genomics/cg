7075c6b9d01f75487293376ecc1331721fb840fe
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-06-14 08:01:14 +0200

    remove unused fixtures

--- tests/store/api/conftest.py
+++ tests/store/api/conftest.py
@@ -1,8 +1,6 @@
 """Pytest fixtures for testing the store/api"""
 
 import pytest
-from cg.store import Store
-from tests.store.api.test_store_import_func import add_applications
 
 
 @pytest.fixture
@@ -18,48 +16,6 @@ def applications_file():
 
 
 @pytest.fixture
-def balsamic_orderform():
-    """Orderform fixture for Balsamic samples"""
-    return 'tests/fixtures/orderforms/1508.17.balsamic.xlsx'
-
-
-@pytest.fixture
-def external_orderform():
-    """Orderform fixture for external samples"""
-    return 'tests/fixtures/orderforms/1541.6.external.xlsx'
-
-
-@pytest.fixture
-def fastq_orderform():
-    """Orderform fixture for fastq samples"""
-    return 'tests/fixtures/orderforms/1508.17.fastq.xlsx'
-
-
-@pytest.fixture
-def metagenome_orderform():
-    """Orderform fixture for metagenome samples"""
-    return 'tests/fixtures/orderforms/1605.6.metagenome.xlsx'
-
-
-@pytest.fixture
 def microbial_orderform():
     """Orderform fixture for microbial samples"""
     return 'tests/fixtures/orderforms/1603.8.microbial.xlsx'
-
-
-@pytest.fixture
-def mip_orderform():
-    """Orderform fixture for MIP samples"""
-    return 'tests/fixtures/orderforms/1508.17.mip.xlsx'
-
-
-@pytest.fixture
-def mip_balsamic_orderform():
-    """Orderform fixture for MIP and Balsamic samples"""
-    return 'tests/fixtures/orderforms/1508.17.mip_balsamic.xlsx'
-
-
-@pytest.fixture
-def rml_orderform():
-    """Orderform fixture for RML samples"""
-    return 'tests/fixtures/orderforms/1604.9.rml.xlsx'
