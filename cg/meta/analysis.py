3f44667c88da053cb7e0b94d3bc310ec3a2caa34
barrystokman <barry.stokman@gmail.com>
2019-07-02 12:27:01 +0200

    add twist capture kit to CAPTUREKIT_MAP. WARNING: THIS VALUE Is A PLACEHOLDER FOR TESTING PURPOSES ONLY!

--- cg/meta/analysis.py
+++ cg/meta/analysis.py
@@ -26,6 +26,8 @@ CAPTUREKIT_MAP = {'Agilent Sureselect CRE': 'agilent_sureselect_cre.v1',
                   'SureSelect CRE': 'agilent_sureselect_cre.v1',
                   'Agilent Sureselect V5': 'agilent_sureselect.v5',
                   'SureSelect Focused Exome': 'agilent_sureselect_focusedexome.v1',
+                  'Twist Human core exome v1.3 + Twist Human RefSeq Panel':
+                  'PLACEHOLDER_TWIST_CAPKIT',
                   'other': 'agilent_sureselect_cre.v1'}
 
 
