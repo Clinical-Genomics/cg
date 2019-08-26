41c76cca965a31318b453b6c1d17791f5f5afa50
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-08-26 15:59:27 +0200

    Bump version: 3.15.1 → 3.16.0

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class PyTest(TestCommand):
 
 setup(
     name='cg',
-    version='3.15.1',
+    version='3.16.0',
     description='Clinical Genomics command center.',
     author='Patrik Grenfeldt',
     author_email='patrik.grenfeldt@scilifelab.se',
