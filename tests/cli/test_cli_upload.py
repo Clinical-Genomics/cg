8109bd247704eca2247f87d280b14da3a672cb27
adrosenbaum <42931633+adrosenbaum@users.noreply.github.com>
2019-09-02 13:19:44 +0200

    feat: Add filter to wes samples (#384)
    * Add filter to wes samples
    
    Add filter so that no observations from wes analysis are uploaded
    
    * Check if there are missing files for family
    
    * Change from Path.is_file to Path.exists() to see if file exists
    
    * Improve checking if files exist, and improve logging
    
    * Added option to limit number of cases to upload when uploading observations
    
    * Add unittests

---
+++ tests/cli/test_cli_upload.py
@@ -0,0 +1,21 @@
+""" Test cg.cli.upload module """
+
+from cg.cli.upload import LinkHelper
+
+
+def test_all_samples_are_non_tumor(analysis_store):
+
+    family_obj = analysis_store.family('yellowhog')
+    assert LinkHelper.all_samples_are_non_tumour(family_obj.links)
+
+
+def test_all_samples_data_analysis(analysis_store):
+
+    family_obj = analysis_store.family('yellowhog')
+    assert LinkHelper.all_samples_data_analysis(family_obj.links, ['mip'])
+
+
+def test_all_samples_are_wgs(analysis_store):
+
+    family_obj = analysis_store.family('yellowhog')
+    assert LinkHelper.all_samples_are_wgs(family_obj.links)
