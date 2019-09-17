fe5bef8479885324063b0e3ffc388d43caac3777
Maya <maya.brandi@scilifelab.se>
2019-09-17 14:01:04 +0200

    adding some tests

--- tests/apps/gt/test_gt_api.py
+++ tests/apps/gt/test_gt_api.py
@@ -1,5 +1,5 @@
 """
-    Tests for loqusdbAPI
+    Tests for GenotypeAPI
 """
 
 import subprocess
@@ -14,9 +14,10 @@ from cg.exc import CaseNotFoundError
 def test_instatiate(genotype_config):
 
     """Test to instantiate a genotype api"""
-    # GIVEN a genotype api with some configs
 
-    # WHEN instantiating a loqusdb api
+    # GIVEN a genotype api with a config
+
+    # WHEN instantiating a genotype api
     genotype_api = GenotypeAPI(genotype_config)
 
     # THEN assert that the adapter was properly instantiated
@@ -24,122 +25,47 @@ def test_instatiate(genotype_config):
     assert genotype_api.genotype_binary == genotype_config['genotype']['binary_path']
 
 
-def test_get_trending(genotypeapi, genotype_trending_output, mocker):
+def test_get_trending(genotypeapi, mocker):
 
-    """Test to get a case via the api"""
-    # GIVEN a loqusdb api
-    days = '4'
-    # WHEN fetching a case with the adapter
+    """Test that get_trending calls the genotype API with correct command."""
+
+    # GIVEN a genotypeapi api and argument days
+    days = '20'
+
+    # WHEN running get_trending
     mocker.patch.object(subprocess, 'check_output')
-    subprocess.check_output.return_value = genotype_trending_output
-    case_obj = genotypeapi.get_trending(days=days)
-    # THEN assert that the correct case id is returned
-    assert case_obj['case_id'] == case_id
+    genotype_obj = genotypeapi.get_trending(days=days)
+
+    # THEN assert subprocess is running the GenotypeAPI with correct command
+    call = f"gtdb --database database prepare-trending -d {days}"
+    subprocess.check_output.assert_called_with(call, shell=True)
 
 
-# def test_get_case_non_existing(loqusdbapi, mocker):
+def test_get_trending_empty_inp(genotypeapi, mocker):
 
-#     """Test to get a case via the api"""
+    """Test that get_trending calls the genotype API with correct command."""
 
-#     # GIVEN a loqusdb api and a case id
-#     case_id = 'a_case'
+    # GIVEN a genotypeapi api and argument days is empty string
+    days = ''
 
-#     # WHEN loqusdb output is empty string
-#     mocker.patch.object(subprocess, 'check_output')
-#     subprocess.check_output.return_value = b""
+    # WHEN running get_trending
+    mocker.patch.object(subprocess, 'check_output')
+    genotype_obj = genotypeapi.get_trending(days=days)
 
-#     # THEN CaseNotFoundError should be raised
-#     with pytest.raises(CaseNotFoundError):
-#         loqusdbapi.get_case(case_id)
-
-
-# def test_get_case_command_fail(loqusdbapi, mocker):
-
-#     """Test to see if exception is raised"""
-#     # GIVEN a loqusdb api and a case id
-#     case_id = 'a_case'
-#     # WHEN an error occurs during fetching a case with the adapter
-#     mocker.patch.object(subprocess, 'check_output')
-#     subprocess.check_output.side_effect = subprocess.CalledProcessError(1, 'error')
-
-#     # THEN assert that the error is raised
-#     with pytest.raises(subprocess.CalledProcessError):
-#         loqusdbapi.get_case(case_id)
+    # THEN assert subprocess is running the GenotypeAPI with correct command
+    subprocess.check_output.assert_called_with(f'gtdb --database database', shell=True)
 
 
-# def test_get_duplicate(loqusdbapi, mocker, loqusdb_duplicate_output):
-#     """Test when duplicate exists"""
+def test_get_trending_no_output(genotypeapi, mocker):
 
-#     # GIVEN a loqusdb api and a duplicate output
-#     mocker.patch.object(subprocess, 'check_output')
-#     subprocess.check_output.return_value = loqusdb_duplicate_output
-#     dup_json = json.loads(loqusdb_duplicate_output.decode('utf-8'))
+    """Test to get genotype via the api"""
 
-#     # WHEN a duplicate exists
-#     duplicate = loqusdbapi.get_duplicate('vcf_path')
+    # GIVEN a genotypeapi api
 
-#     # THEN assert that the duplicate individual is returned
-#     assert dup_json == duplicate
+    # WHEN get_trending is calling the GenotypeAPI witch is returning a empty string
+    mocker.patch.object(subprocess, 'check_output')
+    subprocess.check_output.return_value = b''
 
-
-# def test_get_duplicate_non_existing(loqusdbapi, mocker):
-#     """Test when no duplicate exists"""
-
-#     # GIVEN a loqusdb api
-#     mocker.patch.object(subprocess, 'check_output')
-#     subprocess.check_output.return_value = b''
-
-#     # WHEN duplicate does not exist
-#     duplicate = loqusdbapi.get_duplicate('vcf_path')
-
-#     # THEN assert that an empty dict is returned
-#     assert duplicate == {}
-
-
-# def test_load(loqusdbapi, mocker, loqusdb_output):
-
-#     """Test to load a case"""
-#     # GIVEN a loqusdb api and some info about a case
-#     family_id = 'test'
-#     ped_path = 'a ped path'
-#     vcf_path = 'a vcf path'
-#     vcf_sv_path = 'a sv_vcf path'
-#     gbcf_path = 'a bcf path'
-
-#     # WHEN uploading a case with 15 variants to loqusdb
-#     mocker.patch('cg.apps.loqus.execute_command',
-#                  return_value=loqusdb_output.decode('utf-8').split('\n'))
-
-#     data = loqusdbapi.load(family_id, ped_path, vcf_path, vcf_sv_path, gbcf_path)
-
-#     # THEN assert that the number of variants is 15
-
-#     assert data['variants'] == 15
-
-
-# def test_repr_string(loqus_config):
-
-#     """Test __repr__ of loqusdbAPI"""
-
-#     loqusdb = LoqusdbAPI(loqus_config)
-
-#     repr_string = repr(loqusdb)
-
-#     correct_string = (f"LoqusdbAPI(binary={loqus_config['loqusdb']['binary_path']},"
-#                       f"config={loqus_config['loqusdb']['config_path']})")
-
-#     assert repr_string == correct_string
-
-
-# def test_execute_command(mocker, loqusdb_output, popen_obj_mock):
-
-#     """
-#         Test execute_command function
-#     """
-
-#     # Mock the Popen class
-#     popen_mock = mocker.patch('subprocess.Popen')
-#     popen_mock.return_value = popen_obj_mock
-#     command_output = [line for line in execute_command(['_loqus_', '_command_'])]
-
-#     assert command_output == loqusdb_output.decode('utf-8').split('\n')
+    # THEN assert CaseNotFoundError
+    with pytest.raises(CaseNotFoundError):
+        genotype_obj = genotypeapi.get_trending(days='')
---
+++ tests/meta/upload/vogue/test_upload_vogue.py
@@ -0,0 +1,28 @@
+"""Test for UploadVogueAPI"""
+
+import json
+
+from cg.meta.upload.vogue import UploadVogueAPI
+
+
+def test_load_genotype(genotypeapi, vogueapi, genotype_return_value, mocker):
+
+    """Test load_genotype"""
+
+    # GIVEN UploadVogueAPI and a genotype_return_value
+
+    # WHEN running load_genotype
+    mocker.patch.object(vogueapi, 'load_genotype')
+    mocker.patch.object(genotypeapi, 'get_trending')
+
+    genotypeapi.get_trending.return_value = genotype_return_value
+    upploadvogueapi = UploadVogueAPI(genotype_api=genotypeapi, vogue_api=vogueapi)
+    upploadvogueapi.load_genotype(days='1')
+
+    # THEN vogueapi.load_genotype will be called once for each sample in genotype_return_value
+    samples = json.loads(genotype_return_value)
+    call_list = vogueapi.load_genotype.call_args_list
+
+    assert vogueapi.load_genotype.call_count == 4
+    for call in call_list:
+        assert call[0][0]['_id'] in samples.keys()
