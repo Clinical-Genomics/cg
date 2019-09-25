8d82c1012e09df49704eebaa1bc5cab58056d0fa
Kenny Billiau <kenny.billiau@scilifelab.se>
2019-09-25 21:13:51 +0200

    Add json dump

--- cg/cli/microsalt.py
+++ cg/cli/microsalt.py
@@ -1,14 +1,12 @@
 """ Add CLI support to start microSALT """
+import json
 import logging
 #import subprocess
 from pathlib import Path
 
 import click
 from cg.apps import hk, lims
-from cg.meta.analysis import AnalysisAPI
 from cg.store import Store
-from cg.apps.usalt.fastq import USaltFastqHandler
-from cg.exc import LimsDataError
 
 LOGGER = logging.getLogger(__name__)
 
@@ -23,15 +21,17 @@ def microsalt(context):
 
 
 @microsalt.command()
+@click.option('-d', '--dry', is_flag=True, help='print parameters to console')
 @click.option('--project', 'project_id', help='link all samples for a family')
 @click.argument('sample_id', required=False)
 @click.pass_context
-def parameter(context, project_id, sample_id):
+def parameter(context, dry, project_id, sample_id):
     """ Get all samples for a project """
     if project_id and (sample_id is None):
         sample_objs = context.obj['db'].microbial_order(project_id).microbial_samples
     elif sample_id and (project_id is None):
         sample_objs = [context.obj['db'].microbial_sample(sample_id)]
+        project_id = sample_objs[0].microbial_order.internal_id
     elif sample_id and project_id:
         sample_objs = [
             sample_obj for sample_obj
@@ -42,11 +42,11 @@ def parameter(context, project_id, sample_id):
         LOGGER.error('provide project and/or sample')
         context.abort()
 
-    parameter = {}
+    parameters = []
     for sample_obj in sample_objs:
         method_library_prep = context.obj['lims'].get_prep_method(sample_obj.internal_id)
         method_sequencing = context.obj['lims'].get_sequencing_method(sample_obj.internal_id)
-        parameter = {
+        parameter_dict = {
             'CG_ID_project': sample_obj.microbial_order.internal_id,
             'CG_ID_sample': sample_obj.internal_id,
             'Customer_ID_sample': sample_obj.name,
@@ -61,14 +61,23 @@ def parameter(context, project_id, sample_id):
             'method_libprep': method_library_prep,
             'method_sequencing': method_sequencing
         }
-    print(parameter)
+        parameters.append(parameter_dict)
 
-@microsalt.command()
-@click.option('-d', '--dry', is_flag=True, help='print command to console')
-@click.option('-p', '--parameters', required=False, help='Optional')
-#@click.option('-p', '--priority', default='low', type=click.Choice(['low', 'normal', 'high']))
-#@click.option('-e', '--email', help='email to send errors to')
-@click.argument('project_id')
-@click.pass_context
-def start(context, dry, parameters, project_id):
-    pass
+    if dry:
+        outfilename = Path(context.obj['config']['usalt']['root']) / 'config' / project_id + '.json'
+        print(outfilename)
+        json.dumps(parameters)
+    else:
+        outfilename = Path(context.obj['config']['usalt']['root']) / 'config' / project_id + '.json'
+        with open(outfilename, 'w') as outfile:
+            json.dump(parameters, outfile)
+
+#@microsalt.command()
+#@click.option('-d', '--dry', is_flag=True, help='print command to console')
+#@click.option('-p', '--parameters', required=False, help='Optional')
+##@click.option('-p', '--priority', default='low', type=click.Choice(['low', 'normal', 'high']))
+##@click.option('-e', '--email', help='email to send errors to')
+#@click.argument('project_id')
+#@click.pass_context
+#def start(context, dry, parameters, project_id):
+#    pass
