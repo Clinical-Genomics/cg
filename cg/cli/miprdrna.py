292bf42dd3f832ecaecb3c5b242a2e922e11989c
Kenny Billiau <kenny.billiau@scilifelab.se>
2019-10-01 14:24:38 +0200

    Add mark_analysis_pending in tb

--- cg/cli/miprdrna.py
+++ cg/cli/miprdrna.py
@@ -25,10 +25,12 @@ def rna(context):
 @click.option('-e', '--email', help='email to send errors to')
 @click.option('-p', '--priority', type=click.Choice(['low', 'normal', 'high']))
 @click.option('-sw', '--start-with', help='start mip from this program.')
+@click.option('--skip-evaluation', is_flag=True, default=False, help='skip QC Collect')
 @click.argument('case_id')
 @click.pass_context
 def start(context: click.Context, case_id: str, dry: bool = False,
-          priority: str = None, email: str = None, start_with: str = None):
+          priority: str = None, email: str = None, start_with: str = None,
+          skip_evaluation: bool = False):
     """Start the analysis pipeline for a case."""
     case_obj = context.obj['db'].family(case_id)
     if case_obj is None:
@@ -41,10 +43,13 @@ def start(context: click.Context, case_id: str, dry: bool = False,
         # TODO move mip_config to it's own mip section in cg.yaml
         kwargs = dict(config=context.obj['trailblazer']['mip_config'], case=case_id,
                       priority=priority, email=email, dryrun=dry, start_with=start_with)
+        if skip_evaluation:
+            kwargs['skip_evaluation'] = True
         if dry:
             command = context.obj['rna_api'].build_command(**kwargs)
             print(command)
         else:
+            context.obj['tb_api'].mark_analysis_pending(case_id=case_id, email=email)
             context.obj['rna_api'].start(**kwargs)
 
 
