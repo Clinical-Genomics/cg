0df57a5b879242233f70de5d94cebb34ea49857c
Kenny Billiau <kenny.billiau@scilifelab.se>
2019-10-02 21:10:05 +0200

    Add test for the environ app

---
+++ tests/apps/test_apps_environ.py
@@ -0,0 +1,29 @@
+""" Test the environ app """
+
+import getpass
+import os
+
+from cg.apps.environ import environ_email
+
+def test_environ_email_sudo():
+    # GIVEN $SUDO_USER is set to a diplomat
+    sudo_user = 'chrisjen.avasarala'
+    os.environ['SUDO_USER'] = sudo_user
+
+    # WHEN asking for the email
+    email = environ_email()
+
+    # THEN it should provide us with the email
+    assert email == f"{sudo_user}@scilifelab.se"
+
+
+def test_environ_email_env(monkeypatch):
+    # GIVEN $SUDO_USER is not set
+    user = 'chrisjen.avasarala'
+    monkeypatch.setattr(getpass, 'getuser', user)
+
+    # WHEN asking for the email
+    email = environ_email()
+
+    # THEN it should provide us with the email
+    assert email == f"{user}@scilifelab.se"
