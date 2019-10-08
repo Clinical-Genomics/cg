be296706340af7c3384287246a96a78c380bf13a
Kenny Billiau <kenny.billiau@scilifelab.se>
2019-10-08 09:52:54 +0200

    ... a linter in the morning ...

--- tests/cli/analysis/rna/conftest.py
+++ tests/cli/analysis/rna/conftest.py
@@ -61,13 +61,27 @@ class MockStore():
     """We need to call the family function from the store
     without accessing the database. So here we go"""
 
+    def __init__(self):
+        """Constructor"""
+        self._case_obj = None
+        self._case_id = None
+        self._family_was_called = False
+
     def family(self, case_id: str):
         """Mock the family call"""
 
         case_obj = namedtuple('Case', 'internal_id')
         case_obj.internal_id = 'fake case'
+        self._case_id = case_id
+        self._case_obj = case_obj
+
+        self._family_was_called = True
 
         return case_obj
+
+    def family_was_called(self):
+        """Check if family was called"""
+        return self._family_was_called
 
 
 @pytest.fixture(scope='function')
