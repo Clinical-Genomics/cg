42b0d4a9e960fcd31f961498e3605025ed146325
barrystokman <barry.stokman@gmail.com>
2019-10-22 14:07:09 +0200

    change adapter_trim to adapter-trim

--- cg/cli/balsamic.py
+++ cg/cli/balsamic.py
@@ -143,7 +143,7 @@ def config(context, dry, target_bed, singularity, umi_trim_length, quality_trim,
     if quality_trim:
         command_str += f" --quality_trim "
     if adapter_trim:
-        command_str += f" --adapter_trim "
+        command_str += f" --adapter-trim "
 
     command = ["bash -c 'source activate P_BALSAMIC-base_3.2.0; balsamic"]
     command_str += "'"  # add ending quote from above line
