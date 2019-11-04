816ec184f77c664866d72acf02b21bcf5226f833
Kenny Billiau <kenny.billiau@scilifelab.se>
2019-11-04 20:37:55 +0100

    Bump version: 4.16.2 → 4.16.3

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ class PyTest(TestCommand):
 
 setup(
     name='cg',
-    version='4.16.2',
+    version='4.16.3',
     description='Clinical Genomics command center.',
     author='Patrik Grenfeldt',
     author_email='patrik.grenfeldt@scilifelab.se',
