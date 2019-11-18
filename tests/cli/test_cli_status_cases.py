1db68166d94381be13673f1b30b41a886a85e134
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-11-18 15:25:29 +0100

    use store as generic store name

--- tests/cli/test_cli_status_cases.py
+++ tests/cli/test_cli_status_cases.py
@@ -57,65 +57,65 @@ def test_lists_family(cli_runner, base_context, base_store: Store):
     assert family.internal_id in result.output
 
 
-def ensure_application_version(base_store, application_tag='dummy_tag'):
+def ensure_application_version(store, application_tag='dummy_tag'):
     """utility function to return existing or create application version for tests"""
-    application = base_store.application(tag=application_tag)
+    application = store.application(tag=application_tag)
     if not application:
-        application = base_store.add_application(tag=application_tag, category='wgs',
-                                                 description='dummy_description')
-        base_store.add_commit(application)
+        application = store.add_application(tag=application_tag, category='wgs',
+                                            description='dummy_description')
+        store.add_commit(application)
 
     prices = {'standard': 10, 'priority': 20, 'express': 30, 'research': 5}
-    version = base_store.application_version(application, 1)
+    version = store.application_version(application, 1)
     if not version:
-        version = base_store.add_version(application, 1, valid_from=datetime.now(),
-                                         prices=prices)
+        version = store.add_version(application, 1, valid_from=datetime.now(),
+                                    prices=prices)
 
-        base_store.add_commit(version)
+        store.add_commit(version)
     return version
 
 
-def ensure_customer(base_store, customer_id='cust_test'):
+def ensure_customer(store, customer_id='cust_test'):
     """utility function to return existing or create customer for tests"""
-    customer_group = base_store.customer_group('dummy_group')
+    customer_group = store.customer_group('dummy_group')
     if not customer_group:
-        customer_group = base_store.add_customer_group('dummy_group', 'dummy group')
+        customer_group = store.add_customer_group('dummy_group', 'dummy group')
 
-        customer = base_store.add_customer(internal_id=customer_id, name="Test Customer",
-                                           scout_access=False, customer_group=customer_group,
-                                           invoice_address='dummy_address',
-                                           invoice_reference='dummy_reference')
-        base_store.add_commit(customer)
-    customer = base_store.customer(customer_id)
+        customer = store.add_customer(internal_id=customer_id, name="Test Customer",
+                                      scout_access=False, customer_group=customer_group,
+                                      invoice_address='dummy_address',
+                                      invoice_reference='dummy_reference')
+        store.add_commit(customer)
+    customer = store.customer(customer_id)
     return customer
 
 
-def add_sample(base_store, sample_id='sample_test', gender='female'):
+def add_sample(store, sample_id='sample_test', gender='female'):
     """utility function to add a sample to use in tests"""
-    customer = ensure_customer(base_store)
-    application_version_id = ensure_application_version(base_store).id
-    sample = base_store.add_sample(name=sample_id, sex=gender)
+    customer = ensure_customer(store)
+    application_version_id = ensure_application_version(store).id
+    sample = store.add_sample(name=sample_id, sex=gender)
     sample.application_version_id = application_version_id
     sample.customer = customer
-    base_store.add_commit(sample)
+    store.add_commit(sample)
     return sample
 
 
-def add_panel(base_store, panel_id='panel_test', customer_id='cust_test'):
+def add_panel(store, panel_id='panel_test', customer_id='cust_test'):
     """utility function to add a panel to use in tests"""
-    customer = ensure_customer(base_store, customer_id)
-    panel = base_store.add_panel(customer=customer, name=panel_id, abbrev=panel_id,
-                                 version=1.0,
-                                 date=datetime.now(), genes=1)
-    base_store.add_commit(panel)
+    customer = ensure_customer(store, customer_id)
+    panel = store.add_panel(customer=customer, name=panel_id, abbrev=panel_id,
+                            version=1.0,
+                            date=datetime.now(), genes=1)
+    store.add_commit(panel)
     return panel
 
 
-def add_family(base_store, family_id='family_test', customer_id='cust_test'):
+def add_family(store, family_id='family_test', customer_id='cust_test'):
     """utility function to add a family to use in tests"""
-    panel = add_panel(base_store)
-    customer = ensure_customer(base_store, customer_id)
-    family = base_store.add_family(name=family_id, panels=panel.name)
+    panel = add_panel(store)
+    customer = ensure_customer(store, customer_id)
+    family = store.add_family(name=family_id, panels=panel.name)
     family.customer = customer
-    base_store.add_commit(family)
+    store.add_commit(family)
     return family
