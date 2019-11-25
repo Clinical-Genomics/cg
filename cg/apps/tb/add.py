468f16435a8a51ca12b5aa581e50e3d5c3ae320c
barrystokman <barry.stokman@gmail.com>
2019-11-25 11:01:29 +0100

    get rna files and bundle

--- cg/apps/tb/add.py
+++ cg/apps/tb/add.py
@@ -18,10 +18,19 @@ class AddHandler:
         config_raw = ruamel.yaml.safe_load(config_stream)
         config_data = mip_files.parse_config(config_raw)
         sampleinfo_raw = ruamel.yaml.safe_load(Path(config_data['sampleinfo_path']).open())
-        sampleinfo_data = mip_files.parse_sampleinfo(sampleinfo_raw)
+        rna_analysis = cls._rna_analysis(sampleinfo_raw)
+        if rna_analysis:
+            sampleinfo_data = mip_files.parse_sampleinfo_rna(sampleinfo_raw)
+        else:
+            sampleinfo_data = mip_files.parse_sampleinfo(sampleinfo_raw)
+
         if sampleinfo_data['is_finished'] is False:
             raise AnalysisNotFinishedError('analysis not finished')
-        new_bundle = cls._build_bundle(config_data, sampleinfo_data)
+
+        if rna_analysis:
+            new_bundle = cls._build_bundle_rna(config_data, sampleinfo_data)
+        else:
+            new_bundle = cls._build_bundle(config_data, sampleinfo_data)
         return new_bundle
 
     @classmethod
@@ -34,6 +43,22 @@ class AddHandler:
             'files': cls._get_files(config_data, sampleinfo_data),
         }
         return data
+
+    @classmethod
+    def _build_bundle_rna(cls, config_data: dict, sampleinfo_data: dict) -> dict:
+        """Create a new bundle for RNA."""
+        data = {
+            'name': config_data['case'],
+            'created': sampleinfo_data['date'],
+            'pipeline_version': sampleinfo_data['version'],
+            'files': cls._get_files_rna(config_data, sampleinfo_data),
+        }
+        return data
+
+    @staticmethod
+    def _rna_analysis(sampleinfo_raw: dict) -> bool:
+        """checks if all samples are RNA samples based on analysis type """
+        return all([analysis == 'wts' for analysis in sampleinfo_raw['analysis_type'].values()])
 
     @staticmethod
     def _get_files(config_data: dict, sampleinfo_data: dict) -> dict:
@@ -161,6 +186,103 @@ class AddHandler:
             data.append({
                 'path': cytosure_path,
                 'tags': ['vcf2cytosure', sample_data['id']],
+                'archive': False,
+            })
+
+        return data
+
+    @staticmethod
+    def _get_files_rna(config_data: dict, sampleinfo_data: dict) -> dict:
+        """Get all the files from the MIP RNA files."""
+
+        data = [{
+            'path': config_data['config_path'],
+            'tags': ['mip-config', 'rd-rna'],
+            'archive': True,
+        }, {
+            'path': config_data['log_path'],
+            'tags': ['mip-log', 'rd-rna'],
+            'archive': True,
+        }, {
+            'path': config_data['sampleinfo_path'],
+            'tags': ['sampleinfo', 'rd-rna'],
+            'archive': True,
+        }, {
+            'path': sampleinfo_data['bcftools_merge'],
+            'tags': ['bcftools-combined-vcf', 'rd-rna'],
+            'archive': True,
+        }, {
+            'path': sampleinfo_data['multiqc_html'],
+            'tags': ['multiqc-html', 'rd-rna'],
+            'archive': True,
+        }, {
+            'path': sampleinfo_data['multiqc_json'],
+            'tags': ['multiqc-json', 'rd-rna'],
+            'archive': True,
+        }, {
+            'path': sampleinfo_data['pedigree_path'],
+            'tags': ['pedigree', 'rd-rna'],
+            'archive': True,
+        }, {
+            'path': sampleinfo_data['qcmetrics_path'],
+            'tags': ['qcmetrics', 'rd-rna'],
+            'archive': True,
+        }, {
+            'path': sampleinfo_data['vep_path'],
+            'tags': ['vep-vcf', 'rd-rna'],
+            'archive': True,
+        }, {
+            'path': sampleinfo_data['version_collect_ar_path'],
+            'tags': ['versions', 'rd-rna'],
+            'archive': True,
+        }]
+
+        if sampleinfo_data['vp_stderr_file'] is not None:
+            data.append({
+                'path': sampleinfo_data['vp_stderr_file'],
+                'tags': ['vp-stderr', 'rd-rna'],
+                'archive': True,
+            })
+
+        for sample_data in sampleinfo_data['samples']:
+            data.append({
+                'path': sample_data['bootstrap_vcf'],
+                'tags': ['bootstrap-vcf', sample_data['id'], 'rd-rna'],
+                'archive': False,
+            })
+            data.append({
+                'path': sample_data['gatk_asereadcounter'],
+                'tags': ['ase-readcounts', sample_data['id'], 'rd-rna'],
+                'archive': False,
+            })
+            data.append({
+                'path': sample_data['gatk_baserecalibration'],
+                'tags': ['bam', 'baserecalibration', sample_data['id'], 'rd-rna'],
+                'archive': False,
+            })
+            data.append({
+                'path': sample_data['gffcompare_ar'],
+                'tags': ['gff-compare-ar', sample_data['id'], 'rd-rna'],
+                'archive': False,
+            })
+            data.append({
+                'path': sample_data['mark_duplicates'],
+                'tags': ['mark-duplicates', sample_data['id'], 'rd-rna'],
+                'archive': False,
+            })
+            data.append({
+                'path': sample_data['salmon_quant'],
+                'tags': ['salmon-quant', sample_data['id'], 'rd-rna'],
+                'archive': False,
+            })
+            data.append({
+                'path': sample_data['star_fusion'],
+                'tags': ['star-fusion', sample_data['id'], 'rd-rna'],
+                'archive': False,
+            })
+            data.append({
+                'path': sample_data['stringtie_ar'],
+                'tags': ['stringtie-ar', sample_data['id'], 'rd-rna'],
                 'archive': False,
             })
 
