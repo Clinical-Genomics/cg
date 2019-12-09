8628d5edb389e815ba8d237548786af2d1e58fad
Måns Magnusson <mans.magnusson@scilifelab.se>
2019-12-09 10:20:01 +0100

    Fixes linting

--- cg/utils/commands.py
+++ cg/utils/commands.py
@@ -1,7 +1,10 @@
+"""
+Code to handle communications to the shell from CG
+"""
+
+import copy
 import logging
 import subprocess
-import copy
-
 from subprocess import CalledProcessError
 
 LOG = logging.getLogger(__name__)
@@ -9,12 +12,12 @@ LOG = logging.getLogger(__name__)
 
 class Process(object):
     """Class to handle communication with other programs via the shell
-    
+
     The other parts of the code should not need to have any knowledge about how the processes are
     called, that will be handled in this module.Output form stdout and stdin will be handeld here.
-    
     """
-    def __init__(self, binary, config=None, config_parameter='--config', base_call=None):
+
+    def __init__(self, binary, config=None, config_parameter="--config"):
         """
         Args:
             binary(str): Path to binary for the process to use
@@ -29,10 +32,10 @@ class Process(object):
         LOG.info("Use base call %s", self.base_call)
         self._stdout = ""
         self._stderr = ""
-    
+
     def run_command(self, parameters=None):
         """Execute a command in the shell
-        
+
         Args:
             parameters(list)
         """
@@ -42,24 +45,21 @@ class Process(object):
         try:
             LOG.info("Running command %s", command)
             res = subprocess.run(
-                command, 
-                check=True,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
+                command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
             )
         except CalledProcessError as err:
             LOG.warning(err)
             raise err
-        
-        self.stdout = res.stdout.decode('utf-8').rstrip()
-        self.stderr = res.stdout.decode('utf-8').rstrip()
+
+        self.stdout = res.stdout.decode("utf-8").rstrip()
+        self.stderr = res.stdout.decode("utf-8").rstrip()
 
         return res.returncode
-    
+
     @property
     def stdout(self):
         return self._stdout
-    
+
     @stdout.setter
     def stdout(self, text):
         self._stdout = text
@@ -71,7 +71,7 @@ class Process(object):
     @property
     def stderr(self):
         return self._stderr
-    
+
     @stderr.setter
     def stderr(self, text):
         self._stderr = text
@@ -79,17 +79,16 @@ class Process(object):
     @stderr.deleter
     def stderr(self):
         del self._stderr
-    
+
     def stdout_lines(self):
         """Iterate over the lines in self.stdout"""
-        for line in self.stdout.split('\n'):
+        for line in self.stdout.split("\n"):
             yield line
 
     def stderr_lines(self):
         """Iterate over the lines in self.stderr"""
-        for line in self.stderr.split('\n'):
+        for line in self.stderr.split("\n"):
             yield line
-    
-    def __repr__(self):
-        return (f"Process:base_call:{self.base_call}")
 
+    def __repr__(self):
+        return f"Process:base_call:{self.base_call}"
--- tests/utils/conftest.py
+++ tests/utils/conftest.py
@@ -1,33 +1,38 @@
 import pytest
 from cg.utils import Process
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope="function")
 def ls_process():
     """
         list files process
     """
-    binary = 'ls'
+    binary = "ls"
     process = Process(binary=binary)
     return process
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope="function")
 def echo_process():
     """
         list files process
     """
-    binary = 'echo'
+    binary = "echo"
     process = Process(binary=binary)
     return process
 
-@pytest.fixture(scope='function')
+
+@pytest.fixture(scope="function")
 def stderr_output():
     """
         list files process
     """
-    lines = ('2018-11-29 08:41:38 130-229-8-20-dhcp.local '
-             'mongo_adapter.client[77135] INFO Connecting to '
-             'uri:mongodb://None:None@localhost:27017\n'
-             '2018-11-29 08:41:38 130-229-8-20-dhcp.local '
-             'mongo_adapter.client[77135] INFO Connection '
-             'established\n')
+    lines = (
+        "2018-11-29 08:41:38 130-229-8-20-dhcp.local "
+        "mongo_adapter.client[77135] INFO Connecting to "
+        "uri:mongodb://None:None@localhost:27017\n"
+        "2018-11-29 08:41:38 130-229-8-20-dhcp.local "
+        "mongo_adapter.client[77135] INFO Connection "
+        "established\n"
+    )
     return lines
