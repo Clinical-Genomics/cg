2dd493fcb8fc9df2202955fe576082eae21baa2e
northwestwitch <rasi.chiara@gmail.com>
2019-12-10 22:34:14 +0100

    remove unused param

--- tests/cli/upload/test_cli_upload_scout.py
+++ tests/cli/upload/test_cli_upload_scout.py
@@ -3,7 +3,7 @@ import logging
 
 from cg.cli.upload import scout
 
-def test_produce_load_config(base_context, cli_runner, analysis_family_single_case, lims_samples):
+def test_produce_load_config(base_context, cli_runner, analysis_family_single_case):
     # GIVEN a singleton WGS case
 
     base_context['scout_upload_api'].mock_generate_config = False
