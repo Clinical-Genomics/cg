2616299896ee18f1a705541515567adcf139a955
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-12-13 14:44:49 +0100

    Test base commands for analysis, store and deliver

---
+++ tests/cli/analysis/test_cli_analysis.py
@@ -0,0 +1,20 @@
+"""This script tests the cli method to do analysis"""
+from cg.cli.analysis import analysis
+
+EXIT_SUCCESS = 0
+
+
+def test_no_options(cli_runner, base_context):
+    """Test command with no options"""
+
+    # GIVEN
+
+    # WHEN dry running
+    result = cli_runner.invoke(analysis, obj=base_context)
+
+    # THEN command should have returned all pipelines that is supported
+    assert result.exit_code == EXIT_SUCCESS
+    assert "balsamic" in result.output
+    assert "microsalt" in result.output
+    assert "mip-dna" in result.output
+    assert "mip-rna" in result.output
