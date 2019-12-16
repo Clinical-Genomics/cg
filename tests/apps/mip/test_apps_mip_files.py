7f4e943d5a6bfefef91bff4a7f499a959a7d5cb1
Barry Stokman <26384108+barrystokman@users.noreply.github.com>
2019-12-16 12:59:08 +0100

    Update tests/apps/mip/test_apps_mip_files.py

--- tests/apps/mip/test_apps_mip_files.py
+++ tests/apps/mip/test_apps_mip_files.py
@@ -22,5 +22,5 @@ def test_parse_sampleinfo_rna_result_contents(snapshot: Snapshot, files_raw: dic
     # WHEN parsing the file
     sampleinfo_data = files.parse_sampleinfo_rna(sampleinfo_raw)
 
-    # THEN the result should contains the correct data
+    # THEN the result should contains the data as specified by the MIP team
     snapshot.assert_match(sampleinfo_data)
