321db2a06cbe1f78f75d998cde09badc9352e854
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2019-12-16 15:54:24 +0100

    log more information on ticket creation fails

--- cg/apps/osticket.py
+++ cg/apps/osticket.py
@@ -1,10 +1,13 @@
 # -*- coding: utf-8 -*-
+import logging
 import os.path
 
 from flask import Flask
 import requests
 
 from cg.exc import TicketCreationError
+
+LOG = logging.getLogger(__name__)
 
 
 class OsTicket(object):
@@ -17,12 +20,14 @@ class OsTicket(object):
 
     def init_app(self, app: Flask):
         """Initialize the API in Flask."""
-        self.setup(api_key=app.config['OSTICKET_API_KEY'], domain=app.config['OSTICKET_DOMAIN'])
+        self.setup(
+            api_key=app.config["OSTICKET_API_KEY"], domain=app.config["OSTICKET_DOMAIN"]
+        )
 
-    def setup(self, api_key: str=None, domain: str=None):
+    def setup(self, api_key: str = None, domain: str = None):
         """Initialize the API."""
-        self.headers = {'X-API-Key': api_key}
-        self.url = os.path.join(domain, 'api/tickets.json')
+        self.headers = {"X-API-Key": api_key}
+        self.url = os.path.join(domain, "api/tickets.json")
 
     def open_ticket(self, name: str, email: str, subject: str, message: str) -> str:
         """Open a new ticket through the REST API."""
@@ -31,4 +36,6 @@ class OsTicket(object):
         if res.ok:
             return res.text
         else:
+            LOG.error("res.text: %s, reason: %s", res.text, res.reason)
+
             raise TicketCreationError(res)
