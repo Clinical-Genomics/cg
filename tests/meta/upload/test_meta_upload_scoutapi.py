fa34e645e7fc91c6c7e664e850ffc1abdc8d818d
Måns Magnusson <mans.magnusson@scilifelab.se>
2019-12-20 11:37:13 +0100

    Fix linting in tests

--- tests/meta/upload/test_meta_upload_scoutapi.py
+++ tests/meta/upload/test_meta_upload_scoutapi.py
@@ -1,15 +1,17 @@
 """Tests for the scout upload API"""
 from pathlib import Path
 
+import pytest
 import yaml
 
-import pytest
 from cg.apps.hk import HousekeeperAPI
 from cg.meta.upload.scoutapi import UploadScoutAPI
 from cg.store import Store
 
 
+# pylint: disable=protected-access
 def test_unlinked_family_is_linked(upload_scout_api):
+    """Test that is_family check fails when samples are not linked"""
     # GIVEN a upload scout api and case data for a family without linked individuals
     family_data = {
         "samples": [
@@ -23,7 +25,9 @@ def test_unlinked_family_is_linked(upload_scout_api):
     assert res is False
 
 
+# pylint: disable=protected-access
 def test_family_is_linked(upload_scout_api):
+    """Test that is_family returns true when samples are linked"""
     # GIVEN a upload scout api and case data for a linked family
     family_data = {
         "samples": [
@@ -41,6 +45,7 @@ def test_family_is_linked(upload_scout_api):
 def test_generate_config_adds_rank_model_version(
     analysis: Store.Analysis, upload_scout_api: UploadScoutAPI
 ):
+    """Test that generate config adds rank model version"""
     # GIVEN a status db and hk with an analysis
     assert analysis
 
@@ -55,6 +60,7 @@ def test_generate_config_adds_rank_model_version(
 def test_generate_config_adds_vcf2cytosure(
     analysis: Store.Analysis, upload_scout_api: UploadScoutAPI
 ):
+    """Test that generate config adds vcf2cytosure file"""
     # GIVEN a status db and hk with an analysis
     assert analysis
 
@@ -77,7 +83,7 @@ def _file_is_yaml(file_path):
     with open(file_path, "r") as stream:
         try:
             data = yaml.safe_load(stream)
-        except yaml.YAMLError as exc:
+        except yaml.YAMLError:
             return False
     return data is not None
 
@@ -89,9 +95,7 @@ def test_save_config_creates_file(upload_scout_api: UploadScoutAPI, tmp_file):
     scout_config = {"dummy": "data"}
 
     # WHEN calling method to save config
-    result_data = upload_scout_api.save_config_file(
-        upload_config=scout_config, file_path=tmp_file
-    )
+    upload_scout_api.save_config_file(upload_config=scout_config, file_path=tmp_file)
 
     # THEN the config should exist on disk
     assert _file_exists_on_disk(tmp_file)
@@ -104,9 +108,7 @@ def test_save_config_creates_yaml(upload_scout_api: UploadScoutAPI, tmp_file):
     scout_config = {"dummy": "data"}
 
     # WHEN calling method to save config
-    result_data = upload_scout_api.save_config_file(
-        upload_config=scout_config, file_path=tmp_file
-    )
+    upload_scout_api.save_config_file(upload_config=scout_config, file_path=tmp_file)
 
     # THEN the should be of yaml type
     assert _file_is_yaml(tmp_file)
@@ -115,9 +117,10 @@ def test_save_config_creates_yaml(upload_scout_api: UploadScoutAPI, tmp_file):
 def test_add_scout_config_to_hk(
     upload_scout_api: UploadScoutAPI, housekeeper_api: HousekeeperAPI, tmp_file
 ):
+    """Test that scout load config is added to housekeeper"""
     # GIVEN a hk_mock and a file path to scout load config
     # WHEN adding the file path to hk_api
-    file_obj = upload_scout_api.add_scout_config_to_hk(
+    upload_scout_api.add_scout_config_to_hk(
         config_file_path=tmp_file, hk_api=housekeeper_api, case_id="dummy"
     )
     # THEN assert that the file path is added to hk
@@ -130,12 +133,13 @@ def test_add_scout_config_to_hk(
 def test_add_scout_config_to_hk_existing_files(
     upload_scout_api: UploadScoutAPI, housekeeper_api: HousekeeperAPI, tmp_file
 ):
+    """Test that scout config is not updated in housekeeper if it already exists"""
     # GIVEN a hk_mock with an scout upload file and a file path to scout load config
     housekeeper_api._files = ["a file path"]
     # WHEN adding the file path to hk_api
     with pytest.raises(FileExistsError):
         # THEN assert File exists exception is raised
-        file_obj = upload_scout_api.add_scout_config_to_hk(
+        upload_scout_api.add_scout_config_to_hk(
             config_file_path=tmp_file, hk_api=housekeeper_api, case_id="dummy"
         )
     # THEN assert file is not added to hk-db
