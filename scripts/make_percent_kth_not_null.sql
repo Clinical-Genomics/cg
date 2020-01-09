cec454bb4701337fd1b02ebffe4ae5e014f8d848
mayabrandi <maya.brandi@scilifelab.se>
2020-01-09 09:08:51 +0100

    Update make_percent_kth_not_null.sql

--- scripts/make_percent_kth_not_null.sql
+++ scripts/make_percent_kth_not_null.sql
@@ -1,2 +1,2 @@
-UPDATE application SET percent_kth="" WHERE percent_kth IS NULL;
+UPDATE application SET percent_kth=-1 WHERE percent_kth IS NULL;
 ALTER TABLE application MODIFY COLUMN percent_kth int NOT NULL;
