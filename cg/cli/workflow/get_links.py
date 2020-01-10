b76f8c12315be1046e65734cd13f6d0efd3afed0
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-01-10 08:46:05 +0100

    abort directly if used without input, use not instead of comparing to None

--- cg/cli/workflow/get_links.py
+++ cg/cli/workflow/get_links.py
@@ -19,17 +19,21 @@ def get_links(
      """
     link_objs = None
 
-    if case_id and (sample_id is None):
+    if not case_id and not sample_id:
+        LOG.error("provide case and/or sample")
+        context.abort()
+    if case_id and not sample_id:
         LOG.info("link all samples in a case")
         case_obj = context.obj["db"].family(case_id)
         link_objs = case_obj.links
-    elif sample_id and (case_id is None):
+    elif sample_id and not case_id:
         LOG.info("link sample in all its families")
         sample_obj = context.obj["db"].sample(sample_id)
 
         if not sample_obj:
             LOG.error(
-                "Could not find sample %s, use -c if you intended it as a case-id",
+                "Could not find sample %s. Did you intend %s as a case-id?",
+                sample_id,
                 sample_id,
             )
             context.abort()
