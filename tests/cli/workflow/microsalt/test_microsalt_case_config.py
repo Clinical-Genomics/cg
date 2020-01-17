ac71017e2e8cd191119444a531736c590f4c940c
Kenny Billiau <kenny.billiau@scilifelab.se>
2020-01-17 09:54:13 +0100

    Add tests for missing params

--- tests/cli/workflow/microsalt/test_microsalt_case_config.py
+++ tests/cli/workflow/microsalt/test_microsalt_case_config.py
@@ -16,7 +16,7 @@ def test_no_arguments(cli_runner, base_context, caplog):
     # WHEN dry running without anything specified
     result = cli_runner.invoke(case_config, obj=base_context)
 
-    # THEN command should mention argument
+    # THEN command should mention missing arguments
     assert result.exit_code != EXIT_SUCCESS
     assert "Aborted!" in result.output
     with caplog.at_level(logging.ERROR):
@@ -24,7 +24,7 @@ def test_no_arguments(cli_runner, base_context, caplog):
 
 
 def test_dry_sample(cli_runner, base_context, microbial_sample_id, snapshot: Snapshot):
-    """Test command without any options"""
+    """Test working dry command for sample"""
 
     # GIVEN
 
@@ -34,5 +34,57 @@ def test_dry_sample(cli_runner, base_context, microbial_sample_id, snapshot: Sna
     )
 
     # THEN command should give us a json dump
-    # assert result.exit_code == EXIT_SUCCESS
+    assert result.exit_code == EXIT_SUCCESS
     snapshot.assert_match(result.output)
+
+
+def test_no_sample_found(cli_runner, base_context, caplog):
+    """Test missing sample command """
+
+    # GIVEN a not existing sample
+    microbial_sample_id = "not_existing_sample"
+
+    # WHEN dry running a sample name
+    result = cli_runner.invoke(
+        case_config, [microbial_sample_id], obj=base_context
+    )
+
+    # THEN command should mention missing sample
+    assert result.exit_code != EXIT_SUCCESS
+    with caplog.at_level(logging.ERROR):
+        assert f"Sample {microbial_sample_id} not found" in caplog.text
+
+
+def test_no_project_found(cli_runner, base_context, caplog):
+    """Test missing project command """
+
+    # GIVEN a not existing project 
+    microbial_project_id = "not_existing_project"
+
+    # WHEN dry running a project name
+    result = cli_runner.invoke(
+        case_config, ['--project', microbial_project_id], obj=base_context
+    )
+
+    # THEN command should mention missing project
+    assert result.exit_code != EXIT_SUCCESS
+    with caplog.at_level(logging.ERROR):
+        assert f"Project {microbial_project_id} not found" in caplog.text
+
+
+def test_no_sample_project_found(cli_runner, base_context, caplog):
+    """Test missing sample and project command """
+
+    # GIVEN a not existing project 
+    microbial_sample_id = "not_existing_sample"
+    microbial_project_id = "not_existing_project"
+
+    # WHEN dry running a project name
+    result = cli_runner.invoke(
+        case_config, ['--project', microbial_project_id, microbial_sample_id], obj=base_context
+    )
+
+    # THEN command should mention missing project
+    assert result.exit_code != EXIT_SUCCESS
+    with caplog.at_level(logging.ERROR):
+        assert f"Samples {microbial_sample_id} not found in {microbial_project_id}" in caplog.text
