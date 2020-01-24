ac0724872bbadca31ab1573be2910eaeaf02da33
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-01-24 13:43:56 +0100

    Test store

--- cg/cli/workflow/balsamic/store.py
+++ cg/cli/workflow/balsamic/store.py
@@ -88,9 +88,7 @@ def _include_files_in_housekeeper(bundle_obj, context, hk_api, version_obj):
 def _create_analysis(bundle_data, case_obj, status, version_obj):
     """Function to create and return a new analysis database record"""
     pipeline = case_obj.links[0].sample.data_analysis
-    pipeline = (
-        pipeline if pipeline else "balsamic"
-    )  # TODO remove this default from here
+    pipeline = pipeline if pipeline else "balsamic"
 
     if status.analysis(family=case_obj, started_at=version_obj.created_at):
         raise AnalysisDuplicationError(
