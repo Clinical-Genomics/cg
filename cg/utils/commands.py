b5516f7771bd813aa9a31e609f3b3794f321edbc
adrosenbaum <42931633+adrosenbaum@users.noreply.github.com>
2020-02-20 16:58:43 +0100

    Feat(integration): Decouple chanjo (#540)
    * Decouple chanjo
    
    * Added tests
    
    * Improve tests
    
    * lint stuff
    
    * Write gene list to tmp file and pass to chanjo CLI instead
    
    * Pass path to bed as str
    
    * change threshold from str to int
    
    * Use only sample id
    
    * Convert gene ids to str
    
    * Fix tests
    
    * change attribute names
    
    * Remove chanjo from requirements
    
    * Log runned command as a space-separated string not a list
    
    * Fix tests
    
    * Make fixture name usage specific
    
    * remove unused mocked methods
    
    Co-authored-by: Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>

--- cg/utils/commands.py
+++ cg/utils/commands.py
@@ -43,7 +43,7 @@ class Process:
         if parameters:
             command.extend(parameters)
 
-        LOG.info("Running command %s", command)
+        LOG.info("Running command %s", " ".join(command))
         res = subprocess.run(
             command, check=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE
         )
@@ -52,6 +52,7 @@ class Process:
         self.stderr = res.stderr.decode("utf-8").rstrip()
         if res.returncode != 0:
             LOG.critical("Call %s exit with a non zero exit code", command)
+            LOG.critical(self.stderr)
             raise CalledProcessError(command, res.returncode)
 
         return res.returncode
--- tests/meta/upload/conftest.py
+++ tests/meta/upload/conftest.py
@@ -2,9 +2,11 @@
 import pytest
 
 from cg.apps.hk import HousekeeperAPI
+from cg.apps.coverage.api import ChanjoAPI
 from cg.meta.upload.mutacc import UploadToMutaccAPI
 from cg.meta.upload.observations import UploadObservationsAPI
 from cg.meta.upload.scoutapi import UploadScoutAPI
+from cg.meta.upload.coverage import UploadCoverageApi
 
 
 class MockVersion:
@@ -127,6 +129,12 @@ class MockAnalysis:
     def convert_panels(customer_id, panels):
         """Mock convert_panels"""
         return ""
+
+
+class MockCoverage(ChanjoAPI):
+    """Mock chanjo coverage api"""
+
+    pass
 
 
 class MockMutaccAuto:
@@ -268,6 +276,19 @@ def mutacc_upload_api():
 
     _api = UploadToMutaccAPI(scout_api=scout_api, mutacc_auto_api=mutacc_auto_api)
 
+    return _api
+
+
+@pytest.yield_fixture(scope="function")
+def coverage_upload_api(chanjo_config_dict):
+    """Fixture for coverage upload API"""
+    hk_api = MockHouseKeeper()
+    hk_api.add_file(file="path", version_obj="", tag_name="")
+    status_api = None
+    coverage_api = MockCoverage(chanjo_config_dict)
+    _api = UploadCoverageApi(
+        status_api=status_api, hk_api=hk_api, chanjo_api=coverage_api
+    )
     return _api
 
 
