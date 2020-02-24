292c9c10abf27a813fd6d424ca08d58412240747
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-02-24 12:12:21 +0100

    remove data show since it's irrelevant
    all invokations of balsamic case-config has been with the -p flag and this data has not been used

--- scripts/alter-bed-version.sql
+++ scripts/alter-bed-version.sql
@@ -1,10 +1,3 @@
--- show data to be removed
-select sample.internal_id, sample.ordered_at, sample.sequenced_at, sample.delivered_at, bed.name, bed_version.description, bed_version.filename  from sample
-inner join bed_version on bed_version.id = bed_version_id
-inner join bed on bed.id = bed_version.bed_id
-where bed_version_id is not null
-order by sample.ordered_at
-
 -- remove unused columns in bed-version
 ALTER TABLE `bed_version` DROP COLUMN `genes`;
 ALTER TABLE `bed_version` DROP COLUMN `unique_transcripts`;
