31b83152f0f77707a386064e6a390f3358a6636f
hassanfa <hassan.foroughi@gmail.com>
2020-02-28 13:41:52 +0100

    add test for wgs from lims

--- tests/cli/workflow/balsamic/test_cli_balsamic_config_case.py
+++ tests/cli/workflow/balsamic/test_cli_balsamic_config_case.py
@@ -128,6 +128,26 @@ def test_target_bed_from_lims(
     assert str(bed_path) in result.output
 
 
+def test_wgs_from_lims(
+    cli_runner, balsamic_context, balsamic_case_wgs, lims_api, balsamic_store 
+):
+    """Test command without --target-bed option"""
+
+    # GIVEN case with wgs tag 
+    bed_key = "-p"
+    case_id = balsamic_case_wgs.internal_id
+
+    # WHEN dry running
+    result = cli_runner.invoke(
+        config_case, [case_id, "--dry-run"], obj=balsamic_context
+    )
+
+    # THEN dry-print should NOT include the bed_key and the bed_value including path
+    assert result.exit_code == EXIT_SUCCESS
+    assert bed_key not in result.output
+
+
+
 def test_umi_trim_length(cli_runner, balsamic_context, balsamic_case):
     """Test command with --umi-trim-length option"""
 
