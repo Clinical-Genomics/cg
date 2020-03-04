ee6c92adf62a8e7f5bfb98599fc33d946f1cf82e
jemten <jemten@users.noreply.github.com>
2020-03-04 14:58:26 +0100

    Set min sequencing depth for WGS 30x apptags

--- scripts/add-min_depth_to_application.sql
+++ scripts/add-min_depth_to_application.sql
@@ -1,7 +1,7 @@
 ALTER TABLE `application`
 ADD COLUMN `min_sequencing_depth` int(11) DEFAULT NULL;
 
-Update application SET min_sequencing_depth = sequencing_depth;
+UPDATE application SET min_sequencing_depth = sequencing_depth;
 
-Update application SET min_sequencing_depth = sequencing_depth * 0.87
-    where application.tag like 'WGS%'
+UPDATE application SET min_sequencing_depth = 26
+    WHERE application.tag LIKE 'WG%030';
