96f71b571ef6007174e733ff5c3b9adce09bc3bb
barrystokman <barry.stokman@gmail.com>
2020-03-11 13:53:01 +0100

    add snapshot unit test for get_files

--- tests/meta/store/snapshots/snap_test_mip_rna_build_bundle.py
+++ tests/meta/store/snapshots/snap_test_mip_rna_build_bundle.py
@@ -234,3 +234,226 @@ snapshots['test_build_bundle 1'] = {
     'name': 'case_id',
     'pipeline_version': 'v8.2.2'
 }
+
+snapshots['test_get_files 1'] = [
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/sample_id/salmon_quant/quant.sf',
+        'tags': [
+            'meta',
+            'rd-rna',
+            'salmon_quant',
+            'sample_id'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/sample_id/star_fusion/star-fusion.fusion_predictions.abridged.tsv',
+        'tags': [
+            'meta',
+            'rd-rna',
+            'sample_id',
+            'star_fusion'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/sample_id/arriba_ar/sample_id_lanes_1234_trim_arriba.pdf',
+        'tags': [
+            'arriba_ar',
+            'arriba_report',
+            'meta',
+            'rd-rna',
+            'sample_id'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/sample_id/arriba_ar/sample_id_lanes_1234_trim_arriba.tsv',
+        'tags': [
+            'arriba_ar',
+            'meta',
+            'rd-rna',
+            'sample_id'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/sample_id/stringtie_ar/sample_id_lanes_1234_trim_star_sorted_merged_strg.gtf',
+        'tags': [
+            'meta',
+            'rd-rna',
+            'sample_id',
+            'stringtie_ar'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/sample_id/gffcompare_ar/sample_id_lanes_1234_trim_star_sorted_merged_strg_gffcmp.gtf',
+        'tags': [
+            'gffcompare_ar',
+            'meta',
+            'rd-rna',
+            'sample_id'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/sample_id/markduplicates/sample_id_lanes_1234_trim_star_sorted_merged_md.cram',
+        'tags': [
+            'cram',
+            'markduplicates',
+            'rd-rna',
+            'sample_id'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/sample_id/gatk_asereadcounter/sample_id_lanes_1234_trim_star_sorted_merged_md_splitncigar_brecal_haptc_filtered_ase.csv',
+        'tags': [
+            'gatk_asereadcounter',
+            'meta',
+            'rd-rna',
+            'sample_id'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/sample_id/bootstrapann/sample_id_lanes_1234_trim_star_sorted_merged_md_splitncigar_brecal_haptc_filtered_ase_bootstr.vcf',
+        'tags': [
+            'bootstrapann',
+            'rd-rna',
+            'sample_id',
+            'vcf'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/case_id/bcftools_merge/case_id_comb.vcf',
+        'tags': [
+            'bcftools_merge',
+            'case_id',
+            'rd-rna',
+            'vcf'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/case_id/varianteffectpredictor/case_id_comb_vep.vcf',
+        'tags': [
+            'case_id',
+            'rd-rna',
+            'varianteffectpredictor',
+            'vcf'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/case_id/version_collect_ar/case_id_vcol.yaml',
+        'tags': [
+            'case_id',
+            'meta',
+            'rd-rna',
+            'version_collect_ar'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/case_id/multiqc_ar/multiqc_data/multiqc_data.json',
+        'tags': [
+            'case_id',
+            'json',
+            'meta',
+            'multiqc_ar',
+            'rd-rna'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/case_id/multiqc_ar/multiqc_report.html',
+        'tags': [
+            'case_id',
+            'html',
+            'meta',
+            'multiqc_ar',
+            'rd-rna'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/case_id_qc_sample_info.yaml',
+        'tags': [
+            'case_id',
+            'meta',
+            'mip_analyse',
+            'rd-rna',
+            'sample_info'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/reference_info.yaml',
+        'tags': [
+            'case_id',
+            'meta',
+            'mip_analyse',
+            'rd-rna',
+            'references_info'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/mip_log/2020-02-28/mip_2020-02-28T10:22:26.log',
+        'tags': [
+            'case_id',
+            'log',
+            'meta',
+            'mip_analyse',
+            'rd-rna'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/case_id_config.yaml',
+        'tags': [
+            'case_id',
+            'config_analysis',
+            'meta',
+            'mip_analyse',
+            'rd-rna'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/pedigree.yaml',
+        'tags': [
+            'case_id',
+            'meta',
+            'mip_analyse',
+            'pedigree',
+            'rd-rna'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/servers/config/cluster.ourplace.se/mip8.2-rna-stage.yaml',
+        'tags': [
+            'case_id',
+            'config',
+            'meta',
+            'mip_analyse',
+            'rd-rna'
+        ]
+    },
+    {
+        'archive': False,
+        'path': '/path/to/rare-disease/cases/case_id/analysis/case_id/case_id.fam',
+        'tags': [
+            'case_id',
+            'meta',
+            'mip_analyse',
+            'pedigree_fam',
+            'rd-rna'
+        ]
+    }
+]
--- tests/meta/store/test_mip_rna_build_bundle.py
+++ tests/meta/store/test_mip_rna_build_bundle.py
@@ -142,3 +142,13 @@ def test_build_bundle(
 
     # THEN the result should contain the data to be stored in Housekeeper
     snapshot.assert_match(mip_rna_bundle)
+
+
+def test_get_files(snapshot: Snapshot, deliverables_raw: dict):
+    # GIVEN the MIP analysis deliverables file
+
+    # WHEN getting the files used to build the bundle
+    mip_rna_files = mip_rna.get_files(deliverables_raw)
+
+    # THEN the result should contain the data to be stored in Housekeeper
+    snapshot.assert_match(mip_rna_files)
