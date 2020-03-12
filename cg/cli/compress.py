434cf6b9eb364ba2c3ffeac0ddc3d0780f7bc9ef
Adam Rosenbaum <adam.rosenbaum@scilifelab.se>
2020-03-12 12:14:23 +0100

    make sure that bam_files in scout exist in latest version of hk_bundle when assembling bam-files for a case

--- cg/cli/compress.py
+++ cg/cli/compress.py
@@ -21,54 +21,6 @@ def compress(context):
     context.obj["crunchy"] = crunchy.CrunchyAPI(context.obj)
 
 
-# @compress.command()
-# @click.option("--case-id", "-c", type=str)
-# @click.option("-n", "--number-of-conversions", type=int)
-# @click.option("-d", "--dry-run", is_flag=True)
-# @click.pass_context
-# def spring(context, case_id, number_of_conversions, dry_run):
-#     """Find cases with fastq-files and compress with spring"""
-#     compress_api = CompressAPI(hk_api=context.obj["hk"], crunchy_api=context.obj["crunchy"])
-#     conversion_count = 0
-#     if case_id:
-#         families = [context.obj["db"].family(case_id)]
-#     else:
-#         families = context.obj["db"].families()
-#     for case in families:
-#         if conversion_count == number_of_conversions:
-#             break
-#         case_id = case.internal_id
-#         fastq_dict = compress_api.get_fastq_files(bundle=case_id)
-#         if not fastq_dict:
-#             continue
-#         case_is_compressable = True
-#         for sample, fastq_files in fastq_dict.items():
-#             fastq_first = Path(fastq_files["fastq_first_path"])
-#             fastq_second = Path(fastq_files["fastq_second_path"])
-#             if not context.obj["crunchy"].fastq_compression_possible(
-#                 fastq_first_path=fastq_first, fastq_second_path=fastq_second
-#             ):
-#                 case_is_compressable = False
-#                 break
-#         if case_is_compressable:
-#             LOG.info("Compressing fastq-files for %s", case_id)
-#             for sample, fastq_files in fastq_dict.items():
-#                 fastq_first = Path(fastq_files["fastq_first_path"])
-#                 fastq_second = Path(fastq_files["fastq_second_path"])
-#                 LOG.info(
-#                     "compressing %s and %s for sample %s",
-#                     fastq_first,
-#                     fastq_second,
-#                     sample,
-#                 )
-#                 context.obj["crunchy"].spring(
-#                     fastq_first_path=fastq_first,
-#                     fastq_second_path=fastq_second,
-#                     dry_run=dry_run,
-#                 )
-#             conversion_count += 1
-
-
 @compress.command()
 @click.option("-c", "--case-id", type=str)
 @click.option("-n", "--number-of-conversions", type=int)
@@ -92,12 +44,14 @@ def cram(context, case_id, number_of_conversions, dry_run):
             LOG.info("compressed bam-files for %s cases", conversion_count)
             break
         case_id = case.internal_id
-        bam_dict = compress_api.get_bam_files_from_scout(case_id=case_id)
+        bam_dict = compress_api.get_bam_files(case_id=case_id)
         if not bam_dict:
             continue
         case_is_compressable = True
-        for sample, bam_file in bam_dict.items():
-            if not context.obj["crunchy"].bam_compression_possible(bam_path=bam_file):
+        for sample, bam_files in bam_dict.items():
+            bam_path = Path(bam_files["bam"].full_path)
+            if not context.obj["crunchy"].bam_compression_possible(bam_path=bam_path):
+                LOG.info("bam to cram compression not possible for %s", sample)
                 case_is_compressable = False
                 break
         if case_is_compressable:
--- cg/meta/compress.py
+++ cg/meta/compress.py
@@ -33,81 +33,48 @@ class CompressAPI:
     def get_nlinks(file_link: Path):
         return os.stat(file_link).st_nlink
 
-    # def get_fastq_files(self, bundle: str):
-    #     """Fetch all fastq-files for a bundle, and organize them into dictionary
-    #
-    #     Returns:
-    #         fastq_dict(dict)
-    #     """
-    #     fastq_files = self.hk_api.get_files(bundle=bundle, tags=["fastq"])
-    #     fastq_dict = {}
-    #     for fastq_file in fastq_files:
-    #         sample_name = Path(fastq_file.full_path).name.split("_")[0]
-    #         if sample_name not in fastq_dict.keys():
-    #             fastq_dict[sample_name] = {
-    #                 "fastq_first_path": None,
-    #                 "fastq_second_path": None,
-    #             }
-    #         if fastq_file.full_path.endswith(FASTQ_FIRST_SUFFIX):
-    #             fastq_dict[sample_name]["fastq_first_path"] = fastq_file.full_path
-    #         if fastq_file.full_path.endswith(FASTQ_SECOND_SUFFIX):
-    #             fastq_dict[sample_name]["fastq_second_path"] = fastq_file.full_path
-    #     return fastq_dict
+    def get_bam_files(self, case_id: str):
 
-    def get_bam_files_from_hk(self, case_id: str):
-        """Fetch all bam-files for a bundle, and organize them into dictionary
-
-        Returns:
-            fastq_dict(dict)
-        """
-        bam_files = self.hk_api.get_files(bundle=case_id, tags=["bam"])
-        bam_dict = {}
-        for bam_file in bam_files:
-            bam_path = Path(bam_file.full_path)
-            if not bam_path.exists():
-                LOG.debug("%s does not exist", bam_path)
-                return None
-            if self.get_nlinks(bam_path) > 1:
-                LOG.warning("%s has more than 1 links to same inode", bam_path)
-                return None
-            sample_name = bam_path.name.split("_")[0]
-            bam_dict[sample_name] = bam_file
         scout_case = self.scout_api.get_cases(case_id=case_id)[0]
-        scout_samples = [
-            sample["individual_id"] for sample in scout_case["individuals"]
-        ]
-        if set(bam_dict.keys()) != set(scout_samples):
-            LOG.debug("Did not find bam for all samples in %s", case_id)
-            return None
-        return bam_dict
-
-    def get_bam_files_from_scout(self, case_id: str):
-
-        case = self.scout_api.get_cases(case_id=case_id)[0]
-
+        last_version = self.hk_api.last_version(bundle=case_id)
+        hk_files = self.hk_api.get_files(
+            bundle=case_id, tags=["bam", "bai", "bam-index"], version=last_version.id
+        )
+        hk_files_dict = {Path(file.full_path): file for file in hk_files}
         bam_dict = {}
-        for sample in case["individuals"]:
-            bam_path = Path(sample["bam_file"])
+        for sample in scout_case["individuals"]:
             sample_id = sample["individual_id"]
+            bam_path = Path(sample["bam_file"])
             if not bam_path.exists():
                 LOG.debug("%s does not exist", bam_path)
                 return None
             if self.get_nlinks(bam_path) > 1:
-                LOG.debug("%s has more than 1 links to same inode")
+                LOG.debug("%s has more than 1 links to same inode", bam_path)
                 return None
-            bam_dict[sample_id] = bam_path
+            if bam_path not in hk_files_dict.keys():
+                LOG.debug("%s not in latest version of housekeeper bundle", bam_path)
+                return None
+            if bam_path.with_suffix(".bai") not in hk_files_dict.keys():
+                LOG.debug("%s has no index-file", bam_path)
+                return None
+            bam_dict[sample_id] = {
+                "bam": hk_files_dict[bam_path],
+                "bai": hk_files_dict[bam_path.with_suffix(".bai")],
+            }
         return bam_dict
 
     def compress_case(self, bam_dict: dict, dry_run: bool = False):
-        for sample, bam_path in bam_dict.items():
+        for sample, bam_files in bam_dict.items():
+            bam_path = Path(bam_files["bam"].full_path)
             LOG.info("Compressing %s for sample %s", bam_path, sample)
             self.crunchy_api.bam_to_cram(bam_path=bam_path, dry_run=dry_run)
 
     def scout_case_is_compressed(self, case_id):
-        bam_dict = self.get_bam_files_from_scout(case_id=case_id)
+        bam_dict = self.get_bam_files(case_id=case_id)
         if not bam_dict:
             return False
-        for _, bam_path in bam_dict.items():
+        for _, bam_files in bam_dict.items():
+            bam_path = Path(bam_files["bam"].full_path)
             if not self.crunchy_api.cram_compression_done(bam_path=bam_path):
                 LOG.debug("No cram compression for %s", bam_path)
                 return False
@@ -115,10 +82,11 @@ class CompressAPI:
 
     def update_scout(self, case_id: str, dry_run: bool = False):
 
-        bam_dict = self.get_bam_files_from_scout(case_id=case_id)
+        bam_dict = self.get_bam_files(case_id=case_id)
         original_case = self.scout_api.get_cases(case_id=case_id)[0]
         modified_case = deepcopy(original_case)
-        for sample_id, bam_path in bam_dict.items():
+        for sample_id, bam_files in bam_dict.items():
+            bam_path = Path(bam_files["bam"].full_path)
             if self.crunchy_api.cram_compression_done(bam_path=bam_path):
                 cram_path = self.crunchy_api.change_suffix_bam_to_cram(
                     bam_path=bam_path
@@ -134,13 +102,13 @@ class CompressAPI:
             pass
 
     def update_hk(self, case_id: str, dry_run: bool = False):
-        bam_dict = self.get_bam_files_from_hk(case_id=case_id)
+        bam_dict = self.get_bam_files(case_id=case_id)
         latest_hk_version = self.hk_api.last_version(bundle=case_id)
-        for sample_id, bam_file in bam_dict.items():
+        for sample_id, bam_files in bam_dict.items():
             cram_tags = [sample_id, "cram"]
             crai_tags = [sample_id, "cram-index"]
-            bam_path = Path(bam_file.full_path)
-            bai_path = bam_path.with_suffix(".bai")
+            bam_path = Path(bam_files["bam"].full_path)
+            bai_path = Path(bam_files["bai"].full_path)
             cram_path = self.crunchy_api.change_suffix_bam_to_cram(bam_path=bam_path)
             crai_path = cram_path.with_suffix(".crai")
             if self.crunchy_api.cram_compression_done(bam_path=bam_path):
@@ -156,17 +124,17 @@ class CompressAPI:
                     )
 
     def remove_bams(self, case_id: str, dry_run: bool = False):
-        bam_files = self.get_bam_files_from_hk(case_id=case_id)
-        latest_hk_version = self.hk_api.last_version(bundle=case_id)
-        for sample_id, bam_file in bam_files.items():
-            bam_path = Path(bam_file.full_path)
-            bai_path = bam_path.with_suffix(".bai")
+        bam_files = self.get_bam_files(case_id=case_id)
+        for sample_id, bam_files in bam_files.items():
+            bam_path = Path(bam_files["bam"].full_path)
+            bai_path = Path(bam_files["bai"].full_path)
             flag_path = self.crunchy_api.get_flag_path(file_path=bam_path)
             if self.crunchy_api.cram_compression_done(bam_path=bam_path):
                 LOG.debug("Will remove %s, %s, and %s", bam_path, bai_path, flag_path)
                 if not dry_run:
                     LOG.info("deleting files...")
-                    bam_file.delete()
+                    bam_files["bam"].delete()
+                    bam_files["bai"].delete()
                     self.hk_api.commit()
                     bam_path.unlink()
                     bai_path.unlink()
