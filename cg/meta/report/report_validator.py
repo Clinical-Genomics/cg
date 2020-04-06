0b132f61418566bb79c700c036e22382038ec998
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-04-06 17:04:20 +0200

    test assumed required fields

--- cg/meta/report/report_validator.py
+++ cg/meta/report/report_validator.py
@@ -3,47 +3,93 @@ from cg.store import Store
 
 
 class ReportValidator:
-
     def __init__(self, store: Store):
         self._sample_helper = SampleHelper(store)
 
     def has_required_data(self, report_data: dict):
 
-        if not self._has_all_values(report_data, ["report_version",
-                                                  "family",
-                                                  "customer_name",
-                                                  "today",
-                                                  "panels",
-                                                  "customer_invoice_address",
-                                                  "scout_access",
-                                                  "accredited"]):
+        if not self._has_all_values(
+            report_data,
+            [
+                "report_version",
+                "family",
+                "customer_name",
+                "today",
+                "panels",
+                "customer_invoice_address",
+                "scout_access",
+                "accredited",
+                "pipeline_version",
+                "genome_build",
+            ],
+        ):
             return False
 
         for sample in report_data["samples"]:
 
             if isinstance(sample, str):
                 print(report_data)
-                print('t(s)', type(sample))
+                print("t(s)", type(sample))
                 print(sample)
                 continue
 
-            if not self._sample_helper.is_ready_made_sample(sample["internal_id"]) and \
-                    not self._has_all_values(sample, ["prepared_at"]):
+            if not self._has_required_sample_values(sample):
                 return False
 
-            if self._sample_helper.is_sequence_sample(sample["internal_id"]):
-                if not self._has_all_values(sample, ["received_at", "prepared_at",
-                                                     "sequenced_at"]):
-                    return False
-
-            if not self._has_all_values(sample, ["delivered_at"]):
-                return False
         return True
 
     @staticmethod
     def _has_all_values(subscriptable, keys):
         for key in keys:
             if not subscriptable[key]:
+                return False
+
+        return True
+
+    def _has_required_values_for_all_samples(self, sample):
+        return self._has_all_values(
+            sample,
+            [
+                "name",
+                "internal_id",
+                "sex",
+                "status",
+                "ticket",
+                "application",
+                "ordered_at",
+                "delivery_method",
+                "delivered_at",
+                "processing_time",
+                "data_analysis",
+                "analysis_sex",
+            ],
+        )
+
+    def _has_required_sample_values(self, sample):
+        if not self._has_required_values_for_all_samples(sample):
+            return False
+
+        if not self._sample_helper.is_ready_made_sample(
+            sample["internal_id"]
+        ) and not self._has_all_values(sample, ["prepared_at", "prep_method"]):
+            return False
+
+        if self._sample_helper.is_sequence_sample(sample["internal_id"]):
+            if not self._has_all_values(
+                sample,
+                [
+                    "received_at",
+                    "prep_method",
+                    "prepared_at",
+                    "sequencing_method",
+                    "sequenced_at",
+                    "million_read_pairs",
+                    "mapped_reads",
+                    "target_coverage",
+                    "target_completeness",
+                    "duplicates",
+                ],
+            ):
                 return False
 
         return True
--- tests/meta/report/test_report_validator.py
+++ tests/meta/report/test_report_validator.py
@@ -5,15 +5,18 @@ from cg.meta.report.report_validator import ReportValidator
 
 @pytest.mark.parametrize(
     "required_report_value_key",
-    ["report_version",
-     "family",
-     "customer_name",
-     "today",
-     "panels",
-     "customer_invoice_address",
-     "scout_access",
-     "accredited",
-     ]
+    [
+        "report_version",
+        "family",
+        "customer_name",
+        "today",
+        "panels",
+        "customer_invoice_address",
+        "scout_access",
+        "accredited",
+        "pipeline_version",
+        "genome_build",
+    ],
 )
 def test_required_report_fields_raises(report_api, required_report_value_key, report_store):
     # GIVEN complete delivery data missing only the data we check for value
@@ -28,86 +31,84 @@ def test_required_report_fields_raises(report_api, required_report_value_key, re
     # THEN the indication on completeness should be false
     assert not has_required_data
 
-# @pytest.mark.parametrize(
-#     "required_report_value_key",
-#     ["report_version",
-#      "family",
-#      "customer_name",
-#      "today",
-#      "panels",
-#      "customer_invoice_address",
-#      "customer_name",
-#      "scout_access",
-#      "accredited",
-#     ]
-# )
-# def test_required_report_sample_fields_raises(report_api, required_report_value_key):
-#
-#     # GIVEN complete delivery data missing only the data we check for value
-#     delivery_data = report_api._get_delivery_data(family_id="yellowhog")
-#     assert report_api._has_required_data(delivery_data)
-#
-#     delivery_data[required_report_value_key] = None
-#
-#     # WHEN checking that the data set has the required value
-#     has_required_data = report_api._has_required_data(delivery_data)
-#
-#     # THEN the indication on completeness should be false
-#     assert not has_required_data
+
+@pytest.mark.parametrize(
+    "required_sample_value_key",
+    [
+        "name",
+        "internal_id",
+        "sex",
+        "status",
+        # "source",
+        "ticket",
+        "application",
+        # "received_at",
+        "ordered_at",
+        # "prep_method",
+        # "prepared_at",
+        # "capture_kit",
+        # "sequencing_method",
+        # "sequenced_at",
+        "delivery_method",
+        "delivered_at",
+        # "million_read_pairs",
+        # "mapped_reads",
+        # "target_coverage",
+        # "target_completeness",
+        # "duplicates",
+        "processing_time",
+        "data_analysis",
+        "analysis_sex",
+    ],
+)
+def test_required_report_sample_fields_raises(report_api, report_store, required_sample_value_key):
+
+    # GIVEN complete delivery data missing only the data we check for value on each sample
+    delivery_data = report_api._get_delivery_data(family_id="yellowhog")
+    assert ReportValidator(report_store).has_required_data(delivery_data)
+
+    for sample in delivery_data["samples"]:
+        sample[required_sample_value_key] = None
+
+    # WHEN checking that the data set has the required value
+    has_required_data = ReportValidator(report_store).has_required_data(delivery_data)
+
+    # THEN the indication on completeness should be false
+    assert not has_required_data
 
 
-# @pytest.mark.parametrize(
-#     "required_sample_value_key",
-#     ["values"]
-# )
-# def test_required_sample_fields_raises_on_rml(report_api, required_sample_value_key):
-#     # GIVEN complete delivery data missing only the data we check for value
-#     delivery_data = report_api._get_delivery_data(family_id="yellowhog")
-#     assert report_api._has_required_data(delivery_data)
-#
-#     for sample in delivery_data["samples"]:
-#         sample[required_sample_value_key] = None
-#
-#     # WHEN checking that the data set has the required value
-#     has_required_data = report_api._has_required_data(delivery_data)
-#
-#     # THEN the indication on completeness should be false
-#     assert not has_required_data
-#
-#
-# @pytest.mark.parametrize(
-#     "required_sample_value_key",
-#     ["values"]
-# )
-# def test_required_sample_fields_raises_on_external(report_api, required_sample_value_key):
-#     # GIVEN complete delivery data missing only the data we check for value
-#     delivery_data = report_api._get_delivery_data(family_id="yellowhog")
-#     assert report_api._has_required_data(delivery_data)
-#
-#     for sample in delivery_data["samples"]:
-#         sample[required_sample_value_key] = None
-#
-#     # WHEN checking that the data set has the required value
-#     has_required_data = report_api._has_required_data(delivery_data)
-#
-#     # THEN the indication on completeness should be false
-#     assert not has_required_data
-#
-#
-# @pytest.mark.parametrize(
-#     "required_sample_value_key",
-#     ["values"]
-# )
-# def test_required_sample_fields_raises_on_internal(report_api, required_sample_value_key):
-#     # GIVEN complete delivery data missing only the data we check for value
-#     delivery_data = report_api._get_delivery_data(family_id="yellowhog")
-#     assert report_api._has_required_data(delivery_data)
-#
-#     for sample in delivery_data["samples"]:
-#         sample[required_sample_value_key] = None
-#
-#     # WHEN checking that the data set has the required value
-#     has_required_data = report_api._has_required_data(delivery_data)
-#
-#     # THEN the indication on completeness should be false
-#     assert not has_required_data
+@pytest.mark.parametrize(
+    "required_sample_value_key",
+    [
+        "received_at",
+        "prep_method",
+        "prepared_at",
+        "sequencing_method",
+        "sequenced_at",
+        "million_read_pairs",
+        "mapped_reads",
+        "target_coverage",
+        "target_completeness",
+        "duplicates",
+    ],
+)
+def test_required_sequence_sample_fields_raises(
+    report_api, report_store, required_sample_value_key
+):
+
+    # GIVEN complete delivery data missing only the data we check for value on each sample
+    delivery_data = report_api._get_delivery_data(family_id="yellowhog")
+    assert ReportValidator(report_store).has_required_data(delivery_data)
+
+    for sample in delivery_data["samples"]:
+        assert not report_store.sample(
+            sample["internal_id"]
+        ).application_version.application.is_external
+
+        sample[required_sample_value_key] = None
+
+    # WHEN checking that the data set has the required value
+    has_required_data = ReportValidator(report_store).has_required_data(delivery_data)
+
+    # THEN the indication on completeness should be false
+    assert not has_required_data
