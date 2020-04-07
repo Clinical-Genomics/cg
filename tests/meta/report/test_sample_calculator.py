9cb5fc0319d4cfa96a87d182c59591db478087e4
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-04-07 09:40:51 +0200

    remove duplicated code

--- tests/meta/report/test_sample_calculator.py
+++ tests/meta/report/test_sample_calculator.py
@@ -2,6 +2,7 @@
 from datetime import timedelta, datetime
 
 from cg.meta.report.sample_calculator import SampleCalculator
+from tests.store_helpers import ensure_customer, ensure_application_version, add_sample
 
 
 def test_calculate_processing_days_invalid_delivered(sample_store):
@@ -51,56 +52,3 @@ def test_calculate_processing_days_valid_dates(sample_store):
 
     # THEN calculated processing days is 1
     assert processing_days == 1
-
-
-def add_sample(store, sample_id="sample_test", gender="female"):
-    """utility function to add a sample to use in tests"""
-    customer = ensure_customer(store)
-    application_version_id = ensure_application_version(store).id
-    sample = store.add_sample(name=sample_id, sex=gender)
-    sample.application_version_id = application_version_id
-    sample.customer = customer
-    store.add_commit(sample)
-    return sample
-
-
-def ensure_application_version(store, application_tag="dummy_tag"):
-    """utility function to return existing or create application version for tests"""
-    application = store.application(tag=application_tag)
-    if not application:
-        application = store.add_application(
-            tag=application_tag,
-            category="wgs",
-            description="dummy_description",
-            percent_kth=80,
-        )
-        store.add_commit(application)
-
-    prices = {"standard": 10, "priority": 20, "express": 30, "research": 5}
-    version = store.application_version(application, 1)
-    if not version:
-        version = store.add_version(
-            application, 1, valid_from=datetime.now(), prices=prices
-        )
-
-        store.add_commit(version)
-    return version
-
-
-def ensure_customer(store, customer_id="cust_test"):
-    """utility function to return existing or create customer for tests"""
-    customer_group = store.customer_group("dummy_group")
-    if not customer_group:
-        customer_group = store.add_customer_group("dummy_group", "dummy group")
-
-        customer = store.add_customer(
-            internal_id=customer_id,
-            name="Test Customer",
-            scout_access=False,
-            customer_group=customer_group,
-            invoice_address="dummy_address",
-            invoice_reference="dummy_reference",
-        )
-        store.add_commit(customer)
-    customer = store.customer(customer_id)
-    return customer
