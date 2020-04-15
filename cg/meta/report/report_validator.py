312e920193ee6e419e137054e342661dc847bf98
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-04-15 11:05:11 +0200

    add exception when running methods in wrong order

--- cg/meta/report/report_validator.py
+++ cg/meta/report/report_validator.py
@@ -1,3 +1,4 @@
+"""Module to validate that required delivery report data exists"""
 from cg.exc import ValidationError
 from cg.meta.report.sample_helper import SampleHelper
 from cg.store import Store
@@ -41,11 +42,14 @@ REQUIRED_ANALYSIS_SAMPLE_FIELDS = [
 
 
 class ReportValidator:
+    """API to validate report data"""
+
     def __init__(self, store: Store):
         self._sample_helper = SampleHelper(store)
-        self._attributes_missing_values = []
+        self._attributes_missing_values = None
 
     def has_required_data(self, report_data: dict) -> bool:
+        """Main method to validate report data"""
 
         self._attributes_missing_values = []
 
@@ -56,15 +60,27 @@ class ReportValidator:
 
         return self.get_missing_attributes() == []
 
+    def get_missing_attributes(self) -> [str]:
+        """Returns a list of all attributes that seems to be missing in the report data set"""
+        if self._attributes_missing_values is None:
+            raise ValidationError(
+                "get_missing_attributes() can only be run after " "has_required_data(...)"
+            )
+
+        return self._attributes_missing_values
+
     def _collect_missing_attributes(self, subscriptable, keys):
+        """Gathers all attributes that should exist"""
         for key in keys:
             if not subscriptable[key]:
                 self._attributes_missing_values.append(key)
 
     def _check_required_sample_attributes_for_all(self, sample):
+        """Checks attributes that should exist on all samples"""
         self._collect_missing_attributes(sample, REQUIRED_GENERIC_SAMPLE_FIELDS)
 
     def _check_required_sample_attributes(self, sample):
+        """Checks attributes for a sample"""
         self._check_required_sample_attributes_for_all(sample)
 
         if not sample.get("internal_id"):
@@ -91,7 +107,5 @@ class ReportValidator:
             self._collect_missing_attributes(sample, REQUIRED_ANALYSIS_SAMPLE_FIELDS)
 
     def _check_required_general_report_data(self, report_data):
+        """Check root-level attributes for a report data set"""
         self._collect_missing_attributes(report_data, REQUIRED_REPORT_FIELDS)
-
-    def get_missing_attributes(self) -> [str]:
-        return self._attributes_missing_values
