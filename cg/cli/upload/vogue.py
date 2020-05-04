ac01d73581d69d235864839ea2a577265659d509
hassanfa <hassan.foroughi@gmail.com>
2020-05-04 17:51:17 +0200

    feat, get workflow name and list of samples

--- cg/cli/upload/vogue.py
+++ cg/cli/upload/vogue.py
@@ -5,6 +5,8 @@ import click
 
 from cg.meta.upload.vogue import UploadVogueAPI
 from cg.apps import vogue as vogue_api, gt
+from cg.cli.workflow.get_links import get_links
+from cg.store import Store
 
 LOG = logging.getLogger(__name__)
 
@@ -16,6 +18,7 @@ def vogue(context):
 
     click.echo(click.style("----------------- TRENDING -----------------------"))
 
+    context.obj["db"] = Store(context.obj["database"])
     context.obj["vogue_api"] = vogue_api.VogueAPI(context.obj)
     context.obj["vogue_upload_api"] = UploadVogueAPI(
         genotype_api=gt.GenotypeAPI(context.obj),
@@ -102,5 +105,74 @@ def bioinfo(context, case_name, cleanup, target_load):
     """Load bioinfo case results to the trending database"""
 
     click.echo(click.style("----------------- BIOINFO -----------------------"))
-    if context.obj["vogue_upload_api"]:
-        click.echo(f"Options provided: {case_name}, {cleanup}, {target_load}")
+
+    load_bioinfo_raw_inputs = dict()
+    
+    
+    # Probably get samples for a case_name through statusdb api
+    load_bioinfo_raw_inputs["samples"] = _get_samples(context, case_name)
+
+    # Probably get analysis result file through housekeeper ai
+    load_bioinfo_raw_inputs["analysis_result_file"] = _get_analysis_result_file(case_name)
+
+#    # Probably get analysis_type [multiqc or microsalt or all] from cli
+#    # This might automated to some extend by checking if input multiqc json.
+#    # This tells us how the result was generated. If it is multiqc it will try to validate keys with 
+#    # an actual model.
+#    load_bioinfo_raw_inputs["analysis_type"] = get_analysis_type(case_name)
+#
+#    # case_name is the input
+#    load_bioinfo_raw_inputs["analysis_case_name"] = case_name
+#
+#    # Get workflow version from where!?
+#    load_bioinfo_raw_inputs["analysis_workflow_version"] = get_analysis_worfklow_version(case_name)
+#
+#    # Get case_analysis_type from cli a free text for an entry in trending database
+#    load_bioinfo_raw_inputs["case_analysis_type"] = get_case_analysis_type(case_name)
+#
+    # Workflow that generated these results
+    load_bioinfo_raw_inputs["analysis_workflow_name"] = _get_analysis_workflow_name(context, case_name)
+
+    print(load_bioinfo_raw_inputs)
+
+def _get_samples(context, case_name):
+    """Get a sample string for case_name
+       Args:
+           case_name(str): onemite
+       Returns:
+           sample_names(str): ACC12345,ACC45679
+    """
+    
+    link_objs = get_links(context, case_name, sample_id=False)
+    sample_ids = set()
+    for link_obj in link_objs:
+        if link_obj.sample.delivered_at is not None:
+            sample_ids.add(link_obj.sample.internal_id)
+    return ",".join(sample_ids)
+
+def _get_analysis_result_file(case_name):
+    """Get the analysis file for a tag and a case_name 
+       Args:
+           case_name(str): onemite
+       Returns:
+           result_file(str): /path/to/multiqc.json
+    """
+
+    return 'path/result/multiqc.json'
+
+
+def _get_analysis_workflow_name(context, case_name):
+    """Get lowercase workflow name for a case_name
+       Args:
+           case_name(str): onemite
+       Returns:
+           workflow_name(str): balsamic 
+    """
+    #TODO: check if sample has two data_analysis assigned
+    link_objs = get_links(context, case_name, sample_id=False)
+    data_analysis = set()
+    for link_obj in link_objs:
+        if link_obj.sample.delivered_at is not None:
+            data_analysis.add(link_obj.sample.data_analysis)
+    
+    return data_analysis
