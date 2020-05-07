df2738562ff97c8cca700ed088f1bf9c2d4c3f4b
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-05-07 15:51:40 +0200

    Use helpers in cli set microbial order test cli set microbial order

--- tests/cli/set/microbial_order/test_cli_set_microbial_order.py
+++ tests/cli/set/microbial_order/test_cli_set_microbial_order.py
@@ -1,10 +1,9 @@
 """Test methods for cg/cli/set/microbial_order"""
-from datetime import datetime
 
 import pytest
+
 from cg.cli.set import microbial_order
 from cg.store import Store
-
 
 SUCCESS = 0
 
@@ -23,9 +22,11 @@ def test_invalid_order_empty_db(cli_runner, base_context, base_store: Store):
     assert result.exit_code != SUCCESS
 
 
-def test_invalid_order_non_empty_db(cli_runner, base_context, base_store: Store):
+def test_invalid_order_non_empty_db(
+    cli_runner, base_context, base_store: Store, helpers
+):
     # GIVEN a non empty database
-    add_microbial_order(base_store)
+    helpers.add_microbial_order(base_store)
 
     # WHEN running set with an order that does not exist
     order_id = "dummy_order_id"
@@ -38,9 +39,11 @@ def test_invalid_order_non_empty_db(cli_runner, base_context, base_store: Store)
     assert result.exit_code != SUCCESS
 
 
-def test_valid_order_no_apptag_option(cli_runner, base_context, base_store: Store):
+def test_valid_order_no_apptag_option(
+    cli_runner, base_context, base_store: Store, helpers
+):
     # GIVEN a non empty database
-    order = add_microbial_order(base_store)
+    order = helpers.add_microbial_order(base_store)
 
     # WHEN running set with an order that does not exist
     order_id = order.internal_id
@@ -51,10 +54,10 @@ def test_valid_order_no_apptag_option(cli_runner, base_context, base_store: Stor
 
 
 @pytest.mark.parametrize("option_key", ["--name", "--ticket"])
-def test_set_option(cli_runner, base_context, base_store: Store, option_key):
+def test_set_option(cli_runner, base_context, base_store: Store, option_key, helpers):
 
     # GIVEN a database with an order
-    order = add_microbial_order(base_store)
+    order = helpers.add_microbial_order(base_store)
     option_value = "option_value"
 
     # WHEN calling set_microbial_order with option
@@ -73,10 +76,10 @@ def test_set_option(cli_runner, base_context, base_store: Store, option_key):
     assert signature in base_store.MicrobialOrder.query.first().comment
 
 
-def test_set_project_name_in_lims(cli_runner, base_context, base_store):
+def test_set_project_name_in_lims(cli_runner, base_context, base_store, helpers):
 
     # GIVEN a database with an order
-    order = add_microbial_order(base_store)
+    order = helpers.add_microbial_order(base_store)
     name = "a_name"
 
     # WHEN calling set_microbial_order with option
@@ -96,11 +99,11 @@ def test_set_project_name_in_lims(cli_runner, base_context, base_store):
 
 @pytest.mark.parametrize("option_key", ["--name", "--ticket"])
 def test_set_option_with_same_value(
-    cli_runner, base_context, base_store: Store, option_key
+    cli_runner, base_context, base_store: Store, option_key, helpers
 ):
 
     # GIVEN a database with an order
-    order = add_microbial_order(base_store)
+    order = helpers.add_microbial_order(base_store)
     option_value = "option_value"
     first_sign = "first_sign"
     second_sign = "second_sign"
@@ -126,10 +129,10 @@ def test_set_option_with_same_value(
     assert second_sign not in base_store.MicrobialOrder.query.first().comment
 
 
-def test_old_comment_preserved(cli_runner, base_context, base_store: Store):
+def test_old_comment_preserved(cli_runner, base_context, base_store: Store, helpers):
 
     # GIVEN a database with an order
-    order = add_microbial_order(base_store)
+    order = helpers.add_microbial_order(base_store)
     first_sign = "first_sign"
     second_sign = "second_sign"
 
@@ -149,84 +152,3 @@ def test_old_comment_preserved(cli_runner, base_context, base_store: Store):
     assert result.exit_code == SUCCESS
     assert first_sign in base_store.MicrobialOrder.query.first().comment
     assert second_sign in base_store.MicrobialOrder.query.first().comment
-
-
-def ensure_application_version(store, application_tag="dummy_tag"):
-    """utility function to return existing or create application version for tests"""
-    application = store.application(tag=application_tag)
-    if not application:
-        application = store.add_application(
-            tag=application_tag,
-            category="wgs",
-            description="dummy_description",
-            percent_kth=80,
-        )
-        store.add_commit(application)
-
-    prices = {"standard": 10, "priority": 20, "express": 30, "research": 5}
-    version = store.application_version(application, 1)
-    if not version:
-        version = store.add_version(
-            application, 1, valid_from=datetime.now(), prices=prices
-        )
-        store.add_commit(version)
-    return version
-
-
-def ensure_customer(store, customer_id="cust_test"):
-    """utility function to return existing or create customer for tests"""
-    customer_group_id = customer_id + "_group"
-    customer_group = store.customer_group(customer_group_id)
-    if not customer_group:
-        customer_group = store.add_customer_group(customer_group_id, customer_group_id)
-
-        customer = store.add_customer(
-            internal_id=customer_id,
-            name="Test Customer",
-            scout_access=False,
-            customer_group=customer_group,
-            invoice_address="dummy_address",
-            invoice_reference="dummy_reference",
-        )
-        store.add_commit(customer)
-    customer = store.customer(customer_id)
-    return customer
-
-
-def add_organism(store):
-    """utility function to add an organism to use in tests"""
-    organism = store.add_organism(internal_id="organism_id", name="organism_name")
-    return organism
-
-
-def add_microbial_sample(store, sample_id="sample_test"):
-    """utility function to add a sample to use in tests"""
-    customer = ensure_customer(store)
-    application_version = ensure_application_version(store)
-    organism = add_organism(store)
-    sample = store.add_microbial_sample(
-        name=sample_id,
-        organism=organism,
-        internal_id=sample_id,
-        reference_genome="test",
-        application_version=application_version,
-    )
-    sample.customer = customer
-    return sample
-
-
-def add_microbial_order(store, order_id="order_test", customer_id="cust_test"):
-    """utility function to set a family to use in tests"""
-    customer = ensure_customer(store, customer_id)
-    with store.session.no_autoflush:
-        order = store.add_microbial_order(
-            name=order_id,
-            internal_id=order_id,
-            customer=customer,
-            ordered=datetime.now(),
-        )
-        order.customer = customer
-        sample = add_microbial_sample(store)
-        order.microbial_samples.append(sample)
-    store.add_commit(sample)
-    return order
