96c29443e803319d4e7adbdfca3af233293ff5ca
hassanfa <hassan.foroughi@gmail.com>
2020-05-10 19:47:47 +0200

    test, cleanup a bit

--- tests/cli/upload/test_cli_upload_vogue.py
+++ tests/cli/upload/test_cli_upload_vogue.py
@@ -3,17 +3,20 @@
 """
 import logging
 
-def test_upload_vogue_bioinfo_dry(invoke_cli):
+from cg.cli.workflow.mip_dna.store import _add_new_complete_analysis_record
+from cg.store import Store
+
+def test_upload_vogue_bioinfo_dry(invoke_cli, base_context):
     """
     Test dry run with minimal options
     """
     
     # GIVEN a dummy case name 
-    case_name = "dummy_case"
+    case_name = base_context["status"].families().first().internal_id
 
     # WHEN trying to upload with a family that doesn't exist
     result = invoke_cli(["upload", "vogue", "bioinfo", "--case-name", case_name, "--dry"])
 
     # THEN it fails hard and reports that it is missing the family
-    assert result.exit_code != 0
+    assert result.exit_code == 0
 
