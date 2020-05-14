bb145b486be58c5e02e398c11c3ebcd429426e2d
barrystokman <barry.stokman@gmail.com>
2020-05-14 06:57:28 +0200

    addressing review comments

--- cg/meta/store/base.py
+++ cg/meta/store/base.py
@@ -8,11 +8,14 @@ from cg.exc import (
     MandatoryFilesMissing,
 )
 
+PIPELINE_TAGS = {
+    "wgs": MIP_DNA_TAGS,
+}
+
 
 def add_new_analysis(bundle_data, case_obj, status, version_obj):
     """Function to create and return a new analysis database record"""
 
-    breakpoint()
     pipeline = case_obj.links[0].sample.data_analysis
 
     if not pipeline:
@@ -40,16 +43,17 @@ def reset_case_action(case_obj):
     case_obj.action = None
 
 
-def get_files(deliverables: dict, pipeline: list) -> list:
+def get_files(deliverables: dict, pipeline: str) -> list:
     """Get all deliverable files from the pipeline"""
 
-    files = _get_files_non_index(deliverables, pipeline)
-    index_files = _get_files_index(deliverables, pipeline)
+    pipeline_tag = HK_TAGS[pipeline]
+    files = _get_files_non_index(deliverables, pipeline_tag)
+    index_files = _get_files_index(deliverables, pipeline_tag)
 
     files.extend(index_files)
 
-    _check_mandatory_tags(files)
-    _convert_tags(files)
+    _check_mandatory_tags(files, PIPELINE_TAGS[pipeline])
+    _convert_tags(files, PIPELINE_TAGS[pipeline])
 
     return files
 
@@ -63,11 +67,11 @@ def _get_files_non_index(deliverables: dict, pipeline: list) -> list:
     ]
 
 
-def _get_files_index(deliverables: dict, pipeline: list) -> list:
+def _get_files_index(deliverables: dict, pipeline_tags: list) -> list:
     """ Get all index files from the deliverables file """
 
     return [
-        {"path": file["path_index"], "tags": get_tags(file, pipeline_tags), "archive": False,}
+        {"path": file["path_index"], "tags": get_tags(file, pipeline_tags), "archive": False}
         for file in deliverables["files"]
         if file["path_index"]
     ]
@@ -89,21 +93,20 @@ def build_bundle(config_data: dict, analysisinfo_data: dict, deliverables: dict)
     """Create a new bundle to store in Housekeeper"""
 
     pipeline = config_data["samples"][0]["type"]
-    pipeline_tag = HK_TAGS[pipeline]
 
     data = {
         "name": config_data["case"],
         "created": analysisinfo_data["date"],
         "pipeline_version": analysisinfo_data["version"],
-        "files": get_files(deliverables, pipeline_tag),
+        "files": get_files(deliverables, pipeline),
     }
     return data
 
 
-def _convert_tags(data: list):
-    """ Convert tags from deliverables tags to MIP standard tags """
+def _convert_tags(data: list, standard_tags: dict):
+    """ Convert tags from deliverables tags to standard tags """
 
-    for deliverables_tags, mip_tags in MIP_TAGS.items():
+    for deliverables_tags, pipeline_tags in standard_tags.items():
         for file in data:
             if all(tag in file["tags"] for tag in deliverables_tags):
                 tags_filtered = list(filter(lambda x: x not in deliverables_tags, file["tags"]))
@@ -111,14 +114,14 @@ def _convert_tags(data: list):
                 file["tags"] = converted_tags
 
 
-def _check_mandatory_tags(files: list):
+def _check_mandatory_tags(files: list, pipeline_tags: dict):
     """
         Check if all the mandatory tags are present for the files to be added to Housekeeper.
         Raise an exception if not.
     """
 
     all_deliverable_tags = [file["tags"] for file in files]
-    all_mandatory_tags = [tag for tag in MIP_TAGS if MIP_TAGS[tag]["is_mandatory"]]
+    all_mandatory_tags = [tag for tag in pipeline_tags if pipeline_tags[tag]["is_mandatory"]]
     deliverable_tags = _flatten(all_deliverable_tags)
     mandatory_tags = _flatten(all_mandatory_tags)
 
