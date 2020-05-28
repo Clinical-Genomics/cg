e1bb9c2bed2c1873693e9e31ff13f794a944ead3
barrystokman <barry.stokman@gmail.com>
2020-05-28 14:16:31 +0200

    flatten conditions

--- cg/cli/backup.py
+++ cg/cli/backup.py
@@ -7,6 +7,7 @@ from cg.store import Store
 from cg.meta.backup import BackupApi
 
 LOG = logging.getLogger(__name__)
+MAX_FLOWCELLS = 1250  # Increased by 250 when we hit the previous limit of 1000...
 
 
 @click.group()
@@ -17,7 +18,7 @@ def backup(context: click.Context):
 
 
 @backup.command("fetch-flowcell")
-@click.option("-f", "--flowcell", help="Retreive a specific flowcell")
+@click.option("-f", "--flowcell", help="Retrieve a specific flowcell")
 @click.option(
     "--dry-run", is_flag=True, help="Don't retrieve from PDC or set flowcell's status"
 )
@@ -38,12 +39,16 @@ def fetch_flowcell(context: click.Context, dry_run: bool, flowcell: str):
 
     retrieval_time = backup_api.fetch_flowcell(flowcell_obj=flowcell_obj, dry_run=dry_run)
 
-    if retrieval_time is None:
-        if flowcell:
-            LOG.info(f"{flowcell}: updating flowcell status to requested")
-            if not dry_run:
-                flowcell_obj.status = "requested"
-                status_api.commit()
-    else:
+    if retrieval_time:
         hours = retrieval_time / 60 / 60
         LOG.info(f"Retrieval time: {hours:.1}h")
+        return
+
+    if not flowcell:
+        return
+
+    LOG.info(f"{flowcell}: updating flowcell status to requested")
+
+    if not dry_run:
+        flowcell_obj.status = "requested"
+        status_api.commit()
