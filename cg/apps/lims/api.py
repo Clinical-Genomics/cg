d2cfbe2cac28b331de6da1dbf8ff3b118ae47073
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-05-29 09:34:52 +0200

    Instantiate before return

--- cg/apps/lims/api.py
+++ cg/apps/lims/api.py
@@ -42,7 +42,9 @@ class LimsAPI(Lims, OrderHandler):
 
     def __init__(self, config):
         lconf = config["lims"]
-        super(LimsAPI, self).__init__(lconf["host"], lconf["username"], lconf["password"])
+        super(LimsAPI, self).__init__(
+            lconf["host"], lconf["username"], lconf["password"]
+        )
 
     def sample(self, lims_id: str):
         """Fetch a sample from the LIMS database."""
@@ -52,7 +54,9 @@ class LimsAPI(Lims, OrderHandler):
 
     def samples_in_pools(self, pool_name, projectname):
         """Fetch all samples from a pool"""
-        return self.get_samples(udf={"pool name": str(pool_name)}, projectname=projectname)
+        return self.get_samples(
+            udf={"pool name": str(pool_name)}, projectname=projectname
+        )
 
     @staticmethod
     def _export_project(lims_project) -> dict:
@@ -60,7 +64,9 @@ class LimsAPI(Lims, OrderHandler):
         return {
             "id": lims_project.id,
             "name": lims_project.name,
-            "date": parse_date(lims_project.open_date) if lims_project.open_date else None,
+            "date": parse_date(lims_project.open_date)
+            if lims_project.open_date
+            else None,
         }
 
     def _export_sample(self, lims_sample):
@@ -77,7 +83,9 @@ class LimsAPI(Lims, OrderHandler):
             "mother": udfs.get("motherID"),
             "source": udfs.get("Source"),
             "status": udfs.get("Status"),
-            "panels": udfs.get("Gene List").split(";") if udfs.get("Gene List") else None,
+            "panels": udfs.get("Gene List").split(";")
+            if udfs.get("Gene List")
+            else None,
             "priority": udfs.get("priority"),
             "received": self.get_received_date(lims_sample.id),
             "application": udfs.get("Sequencing Analysis"),
@@ -100,36 +108,40 @@ class LimsAPI(Lims, OrderHandler):
 
         sample = Sample(self, id=lims_id)
         try:
-            return sample.udf.get("Received at")
+            date = sample.udf.get("Received at")
         except HTTPError:
-            return None
+            date = None
+        return date
 
     def get_prepared_date(self, lims_id: str) -> dt.datetime:
         """Get the date when a sample was prepared in the lab."""
 
         sample = Sample(self, id=lims_id)
         try:
-            return sample.udf.get("Library Prep Finished")
+            date = sample.udf.get("Library Prep Finished")
         except HTTPError:
-            return None
+            date = None
+        return date
 
     def get_delivery_date(self, lims_id: str) -> dt.date:
         """Get delivery date for a sample."""
 
         sample = Sample(self, id=lims_id)
         try:
-            return sample.udf.get("Delivered at")
+            date = sample.udf.get("Delivered at")
         except HTTPError:
-            return None
+            date = None
+        return date
 
     def get_sequenced_date(self, lims_id: str) -> dt.date:
         """Get the date when a sample was sequenced."""
 
         sample = Sample(self, id=lims_id)
         try:
-            return sample.udf.get("Sequencing Finished")
+            date = sample.udf.get("Sequencing Finished")
         except HTTPError:
-            return None
+            date = None
+        return date
 
     def capture_kit(self, lims_id: str) -> str:
         """Get capture kit for a LIMS sample."""
@@ -166,7 +178,9 @@ class LimsAPI(Lims, OrderHandler):
         """Bypass to original method."""
         lims_samples = super(LimsAPI, self).get_samples(*args, **kwargs)
         if map_ids:
-            lims_map = {lims_sample.name: lims_sample.id for lims_sample in lims_samples}
+            lims_map = {
+                lims_sample.name: lims_sample.id for lims_sample in lims_samples
+            }
             return lims_map
 
         return lims_samples
@@ -175,7 +189,9 @@ class LimsAPI(Lims, OrderHandler):
         """Fetch information about a family of samples."""
         filters = {"customer": customer, "familyID": family}
         lims_samples = self.get_samples(udf=filters)
-        samples_data = [self._export_sample(lims_sample) for lims_sample in lims_samples]
+        samples_data = [
+            self._export_sample(lims_sample) for lims_sample in lims_samples
+        ]
         # get family level data
         family_data = {"family": family, "customer": customer, "samples": []}
         priorities = set()
@@ -213,7 +229,12 @@ class LimsAPI(Lims, OrderHandler):
                 yield lims_sample.id
 
     def update_sample(
-        self, lims_id: str, sex=None, target_reads: int = None, name: str = None, **kwargs,
+        self,
+        lims_id: str,
+        sex=None,
+        target_reads: int = None,
+        name: str = None,
+        **kwargs,
     ):
         """Update information about a sample."""
         lims_sample = Sample(self, id=lims_id)
@@ -230,7 +251,8 @@ class LimsAPI(Lims, OrderHandler):
         for key, value in kwargs.items():
             if not PROP2UDF.get(key):
                 raise LimsDataError(
-                    f"Unknown how to set {key} in LIMS since it is not defined in" f" {PROP2UDF}"
+                    f"Unknown how to set {key} in LIMS since it is not defined in"
+                    f" {PROP2UDF}"
                 )
             lims_sample.udf[PROP2UDF[key]] = value
 
@@ -309,7 +331,9 @@ class LimsAPI(Lims, OrderHandler):
         methods = []
 
         for process_name in step_names_udfs:
-            artifacts = self.get_artifacts(process_type=process_name, samplelimsid=lims_id)
+            artifacts = self.get_artifacts(
+                process_type=process_name, samplelimsid=lims_id
+            )
             if artifacts:
                 udf_key_number = step_names_udfs[process_name]["method_number"]
                 udf_key_version = step_names_udfs[process_name]["method_version"]
