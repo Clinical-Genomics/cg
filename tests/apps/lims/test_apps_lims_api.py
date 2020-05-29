1faf1a2bc371985d2b20c74c24bcffe8934e3fa1
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-05-29 10:23:47 +0200

    Adds tests for get received date

--- tests/apps/lims/test_apps_lims_api.py
+++ tests/apps/lims/test_apps_lims_api.py
@@ -1,7 +1,39 @@
 """Test the Lims api"""
 import datetime as dt
 
+from requests.exceptions import HTTPError
+
+import cg
 from cg.apps.lims.api import LimsAPI
+
+
+def test_get_received_date(lims_api, mocker):
+    """Test to get the received date"""
+    # GIVEN a lims api and a mocked sample that returns a received at date
+    mocked_sample = mocker.patch("cg.apps.lims.api.Sample")
+    mock_instance = mocked_sample.return_value
+    date = dt.datetime.today()
+    mock_instance.udf = {"Received at": date}
+
+    # WHEN fetching the date
+    res = lims_api.get_received_date(123)
+
+    # THEN asserrt the correct date is fetched
+    assert res == date
+
+
+def test_get_received_date_no_sample(lims_api, mocker):
+    """Test to get the received date when sample not exists"""
+    # GIVEN a lims api and a mocked sample that returns a received at date
+    mocked_sample = mocker.patch("cg.apps.lims.api.Sample")
+    mock_instance = mocked_sample.return_value
+    mock_instance.udf.get.side_effect = HTTPError()
+
+    # WHEN fetching the date
+    res = lims_api.get_received_date(123)
+
+    # THEN assert that None is returned since a exception was raised
+    assert res == None
 
 
 def test_get_processing_time_for_values(lims_api, mocker):
