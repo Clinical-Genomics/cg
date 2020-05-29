79ec7ac2830435026544ce9e0ea5981ea800c5c0
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-05-29 09:30:24 +0200

    Mock with mocker instead of mocking out api

--- tests/apps/lims/test_apps_lims_api.py
+++ tests/apps/lims/test_apps_lims_api.py
@@ -1,38 +1,54 @@
+"""Test the Lims api"""
 import datetime as dt
 
+from cg.apps.lims.api import LimsAPI
 
-def test_get_processing_time_for_values(lims_api):
 
+def test_get_processing_time_for_values(lims_api, mocker):
+    """Test to get the processing time when there are some existing dates"""
     # GIVEN there is a received_at date and a delivered date
     date = dt.datetime.today()
-    lims_api.set_received_date(date)
-    lims_api.set_delivery_date(date)
+    # GIVEN that received and delivered date is the same
+    mocker.patch.object(LimsAPI, "get_received_date")
+    LimsAPI.get_received_date.return_value = date
 
-    # WHEN calling get_processing_time
+    mocker.patch.object(LimsAPI, "get_delivery_date")
+    LimsAPI.get_delivery_date.return_value = date
+
+    # WHEN fetching the processing time
     processing_time = lims_api.get_processing_time(123)
 
     # THEN return value is the delivered - received == 0 for same datetime
     assert processing_time == dt.timedelta(0)
 
 
-def test_get_processing_time_for_no_received_time(lims_api):
-
-    # GIVEN there is no received_at date and a delivered date
+def test_get_processing_time_for_no_received_time(lims_api, mocker):
+    """Test to get the processing time when there is not received time"""
+    # GIVEN there is no received_at date
+    mocker.patch.object(LimsAPI, "get_received_date")
+    LimsAPI.get_received_date.return_value = None
+    # GIVEN there is a delivery date
     date = dt.datetime.today()
-    lims_api.set_delivery_date(date)
+    mocker.patch.object(LimsAPI, "get_delivery_date")
+    LimsAPI.get_delivery_date.return_value = date
 
     # WHEN calling get_processing_time
     processing_time = lims_api.get_processing_time(123)
 
-    # THEN return value is none
+    # THEN return value should be none since one time is missing
     assert processing_time is None
 
 
-def test_get_processing_time_for_no_delivered_time(lims_api):
+def test_get_processing_time_for_no_delivered_time(lims_api, mocker):
+    """Test to get the processing time when there is not delivered time"""
 
     # GIVEN there is a received_at date and no delivered date
     date = dt.datetime.today()
-    lims_api.set_received_date(date)
+    mocker.patch.object(LimsAPI, "get_received_date")
+    LimsAPI.get_received_date.return_value = date
+
+    mocker.patch.object(LimsAPI, "get_delivery_date")
+    LimsAPI.get_delivery_date.return_value = None
 
     # WHEN calling get_processing_time
     processing_time = lims_api.get_processing_time(123)
