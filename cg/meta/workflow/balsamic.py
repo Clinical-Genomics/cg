5b0bd1bb84f5f6529ea27cb1e7f60ffcc32b7384
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-06-04 13:02:09 +0200

    Update cg/meta/workflow/balsamic.py
    Co-authored-by: Måns Magnusson <mans.magnusson@scilifelab.se>

--- cg/meta/workflow/balsamic.py
+++ cg/meta/workflow/balsamic.py
@@ -21,7 +21,7 @@ class AnalysisAPI:
     def get_deliverables_file_path(case_id, root_dir):
         """Generates a path where the Balsamic deliverables file for the case_id should be
         located"""
-        return Path.joinpath(root_dir, case_id, "delivery_report", case_id + ".hk")
+        return root_dir / case_id / "delivery_report" / (case_id + ".hk")
 
     @staticmethod
     def get_config_path(root_dir: Path, case_id: str) -> Path:
