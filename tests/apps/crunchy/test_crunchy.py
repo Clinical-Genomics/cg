3d9c1f38c42265f69d2d7f8321f820634fc53d7d
Lint Action <lint-action@samuelmeuli.com>
2020-06-11 20:31:22 +0000

    Fix code style issues with Black

--- tests/apps/crunchy/test_crunchy.py
+++ tests/apps/crunchy/test_crunchy.py
@@ -6,8 +6,7 @@ from pathlib import Path
 import pytest
 
 from cg.apps.crunchy import CrunchyAPI
-from cg.constants import (CRAM_SUFFIX, FASTQ_FIRST_READ_SUFFIX,
-                          FASTQ_SECOND_READ_SUFFIX)
+from cg.constants import CRAM_SUFFIX, FASTQ_FIRST_READ_SUFFIX, FASTQ_SECOND_READ_SUFFIX
 
 
 def test_set_dry_run(crunchy_config_dict):
