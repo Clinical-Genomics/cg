0583f88b56f11dbc372da7061662b9fc0e696c0d
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-06-15 13:25:45 +0200

    Fix type in new find cases file

--- cg/store/get/cases.py
+++ cg/store/get/cases.py
@@ -1,6 +1,6 @@
 """Functions to get cases from the store database"""
 
-from typing import Iterable
+from typing import Iterator
 
 from sqlalchemy.orm import Query
 
@@ -11,10 +11,10 @@ from cg.store.api import Store
 def analysis_completed(store: Store) -> Query:
     """Return cases where analysis is finished"""
     cases = store.Family.query.join(models.Analysis)
-    return cases.filter_by(models.Analysis.completed_at.isnot(None)
+    return cases.filter_by(models.Analysis.completed_at.isnot(None))
 
 
-def ready_for_spring_compresssion(store: Store) -> Iterable[models.Family]:
+def ready_for_spring_compresssion(store: Store) -> Iterator[models.Family]:
     """Fetch all cases that are ready to be compressed by SPRING"""
     for case_obj in analysis_completed(store):
         if case_obj.ordered_at > case_obj.analyses[0].completed_at:
