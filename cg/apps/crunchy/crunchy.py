576b40598ef1254b0444b28c9ddb34723283dd51
Lint Action <lint-action@samuelmeuli.com>
2020-06-17 14:41:21 +0000

    Fix code style issues with Black

--- cg/apps/crunchy/crunchy.py
+++ cg/apps/crunchy/crunchy.py
@@ -10,15 +10,26 @@ from typing import List
 
 from marshmallow import ValidationError
 
-from cg.constants import (BAM_INDEX_SUFFIX, BAM_SUFFIX, CRAM_INDEX_SUFFIX,
-                          CRAM_SUFFIX, FASTQ_DELTA, FASTQ_FIRST_READ_SUFFIX,
-                          FASTQ_SECOND_READ_SUFFIX, SPRING_SUFFIX)
+from cg.constants import (
+    BAM_INDEX_SUFFIX,
+    BAM_SUFFIX,
+    CRAM_INDEX_SUFFIX,
+    CRAM_SUFFIX,
+    FASTQ_DELTA,
+    FASTQ_FIRST_READ_SUFFIX,
+    FASTQ_SECOND_READ_SUFFIX,
+    SPRING_SUFFIX,
+)
 from cg.utils import Process
 from cg.utils.date import get_date_str
 
 from .models import CrunchyFileSchema
-from .sbatch import (SBATCH_BAM_TO_CRAM, SBATCH_FASTQ_TO_SPRING,
-                     SBATCH_HEADER_TEMPLATE, SBATCH_SPRING_TO_FASTQ)
+from .sbatch import (
+    SBATCH_BAM_TO_CRAM,
+    SBATCH_FASTQ_TO_SPRING,
+    SBATCH_HEADER_TEMPLATE,
+    SBATCH_SPRING_TO_FASTQ,
+)
 
 LOG = logging.getLogger(__name__)
 
