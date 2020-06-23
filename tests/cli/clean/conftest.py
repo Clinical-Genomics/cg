2755dbfccae880f98da2dc13cc3fbfb9ce2b5a4d
Lint Action <lint-action@samuelmeuli.com>
2020-06-23 11:06:31 +0000

    Fix code style issues with Black

--- tests/cli/clean/conftest.py
+++ tests/cli/clean/conftest.py
@@ -4,15 +4,11 @@ import pytest
 
 
 @pytest.fixture
-def clean_context(
-    base_store, housekeeper_api, helpers, tmpdir
-) -> dict:
+def clean_context(base_store, housekeeper_api, helpers, tmpdir) -> dict:
     """context to use in cli"""
 
     return {
         "hk_api": housekeeper_api,
         "store_api": base_store,
-        "balsamic": {
-            "root": tmpdir,
-        },
+        "balsamic": {"root": tmpdir,},
     }
