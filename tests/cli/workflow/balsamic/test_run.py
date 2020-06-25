ac1dc7f467bd5d241c49ef25e6cbeaa0cd1b59f1
hassanfa <hassan.foroughi@gmail.com>
2020-06-25 15:57:07 +0200

    test, add mock balsamic run

--- tests/cli/workflow/balsamic/test_run.py
+++ tests/cli/workflow/balsamic/test_run.py
@@ -1,5 +1,7 @@
 """This script tests the cli methods to run balsamic"""
+import subprocess
 import logging
+from unittest import mock
 
 from cg.cli.workflow.balsamic.base import run
 
@@ -19,19 +21,29 @@ def test_without_options(cli_runner):
     assert "Missing argument" in result.output
 
 
-def test_with_case(cli_runner, balsamic_context):
+def test_with_case(cli_runner, balsamic_context, caplog):
     """Test command with case to start with"""
 
     # GIVEN case-id, and malconfigured pipeline
-    case_id = "soberelephant"
+    case_id = "case" 
+
+    dummy_stdout = "dummy_stdout"
+    dummy_stderr = "dummy_stderr"
+    dummy_returncode_success = 0
+
+    caplog.set_level(logging.INFO)
 
     context = balsamic_context
+    
+    # WHEN running balsamic on "case"
+    with mock.patch.object(subprocess, "run") as mocked:
+        mocked.return_value.stdout = dummy_stdout.encode("utf-8")
+        mocked.return_value.stderr = dummy_stderr.encode("utf-8")
+        mocked.return_value.returncode = dummy_returncode_success
+        result = cli_runner.invoke(run, [case_id], obj=context)
 
-    # WHEN running
-    result = cli_runner.invoke(run, [case_id], obj=context)
-
-    # THEN command should successfully call the command it creates
-    assert result.exit_code == EXIT_SUCCESS
+        # THEN assert that command is in log output
+        assert " ".join(["balsamic", "run", "analysis"]) in caplog.text
 
 
 def test_dry(cli_runner, balsamic_context, caplog):
