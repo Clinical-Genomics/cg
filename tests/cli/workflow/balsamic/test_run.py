1629e99c2d5aaad08fde844b7a2f8d42565481bb
hassanfa <hassan.foroughi@gmail.com>
2020-06-25 16:11:39 +0200

    test, set level caplog and use dummy case name2

--- tests/cli/workflow/balsamic/test_run.py
+++ tests/cli/workflow/balsamic/test_run.py
@@ -21,11 +21,11 @@ def test_without_options(cli_runner):
     assert "Missing argument" in result.output
 
 
-def test_with_case(cli_runner, balsamic_context, caplog):
+def test_with_case(cli_runner, balsamic_context, caplog, balsamic_dummy_case):
     """Test command with case to start with"""
 
     # GIVEN case-id, and malconfigured pipeline
-    case_id = "case" 
+    case_id = balsamic_dummy_case
 
     dummy_stdout = "dummy_stdout"
     dummy_stderr = "dummy_stderr"
@@ -46,11 +46,13 @@ def test_with_case(cli_runner, balsamic_context, caplog):
         assert " ".join(["balsamic", "run", "analysis"]) in caplog.text
 
 
-def test_dry(cli_runner, balsamic_context, caplog):
+def test_dry(cli_runner, balsamic_context, caplog, balsamic_dummy_case):
     """Test command with dry option"""
 
     # GIVEN case-id
-    case_id = "sillyshark"
+    case_id = balsamic_dummy_case
+
+    caplog.set_level(logging.INFO)
 
     context = balsamic_context
 
@@ -59,15 +61,16 @@ def test_dry(cli_runner, balsamic_context, caplog):
 
     # THEN command should print the balsamic command-string
     assert result.exit_code == EXIT_SUCCESS
-    with caplog.at_level(logging.INFO):
-        assert case_id in caplog.text
+    assert case_id in caplog.text
 
 
-def test_run_analysis(cli_runner, balsamic_context, caplog):
+def test_run_analysis(cli_runner, balsamic_context, caplog, balsamic_dummy_case):
     """Test command with run-analysis option"""
 
     # GIVEN case-id
-    case_id = "slimwhale"
+    case_id = balsamic_dummy_case
+
+    caplog.set_level(logging.INFO)
 
     context = balsamic_context
 
@@ -76,18 +79,18 @@ def test_run_analysis(cli_runner, balsamic_context, caplog):
 
     # THEN dry-print should include the option
     assert result.exit_code == EXIT_SUCCESS
-    with caplog.at_level(logging.INFO):
-        assert "--run-analysis" in caplog.text
+    assert "--run-analysis" in caplog.text
 
 
-def test_config(cli_runner, balsamic_context, caplog):
+def test_config(cli_runner, balsamic_context, caplog, balsamic_dummy_case):
     """Test command with config option"""
 
     # GIVEN case-id
-    case_id = "analogeel"
+    case_id = balsamic_dummy_case
     option_key = "--config"
     option_value = "config-path"
 
+    caplog.set_level(logging.INFO)
     context = balsamic_context
 
     # WHEN dry running with option specified
@@ -95,18 +98,19 @@ def test_config(cli_runner, balsamic_context, caplog):
 
     # THEN dry-print should include the the option-value but not the case-id
     assert result.exit_code == EXIT_SUCCESS
-    with caplog.at_level(logging.INFO):
-        assert option_value in caplog.text
-        assert case_id not in caplog.text
+    assert option_value in caplog.text
+    assert case_id not in caplog.text
 
 
-def test_email(cli_runner, balsamic_context, caplog):
+def test_email(cli_runner, balsamic_context, caplog, balsamic_dummy_case):
     """Test command with config option"""
 
     # GIVEN case-id
-    case_id = "mightymonkey"
+    case_id = balsamic_dummy_case
     option_key = "--email"
     option_value = "salmon.moose@test.com"
+
+    caplog.set_level(logging.INFO)
 
     context = balsamic_context
 
@@ -115,18 +119,19 @@ def test_email(cli_runner, balsamic_context, caplog):
 
     # THEN dry-print should include the the option-value but not the case-id
     assert result.exit_code == EXIT_SUCCESS
-    with caplog.at_level(logging.INFO):
-        assert "--mail-user" in caplog.text
-        assert option_value in caplog.text
+    assert "--mail-user" in caplog.text
+    assert option_value in caplog.text
 
 
-def test_priority(cli_runner, balsamic_context, caplog):
+def test_priority(cli_runner, balsamic_context, caplog, balsamic_dummy_case):
     """Test command with priority option"""
 
     # GIVEN case-id
-    case_id = "weakgorilla"
+    case_id = balsamic_dummy_case
     option_key = "--priority"
     option_value = "high"
+
+    caplog.set_level(logging.INFO)
 
     context = balsamic_context
 
@@ -135,6 +140,5 @@ def test_priority(cli_runner, balsamic_context, caplog):
 
     # THEN dry-print should include the the option-value
     assert result.exit_code == EXIT_SUCCESS
-    with caplog.at_level(logging.INFO):
-        assert "--qos" in caplog.text
-        assert option_value in caplog.text
+    assert "--qos" in caplog.text
+    assert option_value in caplog.text
