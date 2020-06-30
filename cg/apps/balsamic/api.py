6416df65d44238f25e3446fbbeef55ea8bb45206
Maria Ropat <35531751+Mropat@users.noreply.github.com>
2020-06-30 11:08:02 +0200

    Feat/balsamic mode select (#659)
    * Adding option to select analysis type for balsamic from cg workflow
    
    * Fix code style issues with Black
    
    * option added as ANALYSIS_TYPE_OPTION
    
    * Utilize Process for shell execution
    
    * merging conflict resolved
    
    * Fix code style issues with Black
    
    * subprocess revert
    
    * merge conflicts
    
    * Fix code style issues with Black
    
    * removed extra arguments
    
    * Fix code style issues with Black
    
    * draft BalsamicAPI
    
    * Fix code style issues with Black
    
    * metaAPI draft
    
    * refactor case config
    
    * Formatting
    
    * Fix code style issues with Black
    
    * Formatting
    
    * Fix code style issues with Black
    
    * removed store_cmd for testing
    
    * Fix code style issues with Black
    
    * Analysis_api removed for testing
    
    * conflicts
    
    * conflicts
    
    * brackets
    
    * Fix code style issues with Black
    
    * email_user
    
    * dict formatting
    
    * Fix code style issues with Black
    
    * fixed error tumor samples
    
    * fixed error in balsamic call
    
    * fixed error dict formatting
    
    * Fix code style issues with Black
    
    * path fiexd
    
    * path error fixed
    
    * Fix code style issues with Black
    
    * posixpath error
    
    * path error
    
    * formatting
    
    * Fix code style issues with Black
    
    * Edited dockstring
    
    * Fix code style issues with Black
    
    * Revert "Merge branch 'fix/balsamic-command-exectute-from-process' into feat/balsamic-mode-select"
    
    This reverts commit 0ee93c005d75537fc285631e57397185bc13ccc7, reversing
    changes made to 6880719b9499f1eeba5b9fc37175ea081e1167c0.
    
    * API added
    
    * Added BalsamicAPI and process
    
    * Fix code style issues with Black
    
    * Removed optional arguments
    
    * restored options
    
    * Fix code style issues with Black
    
    * dry run option
    
    * Fix code style issues with Black
    
    * synthax error fix
    
    * dry run
    
    * Fix code style issues with Black
    
    * config
    
    * Fix code style issues with Black
    
    * test
    
    * comma
    
    * Fix code style issues with Black
    
    * mail user fixture
    
    * Fix code style issues with Black
    
    * bed_path
    
    * Fix code style issues with Black
    
    * priority test
    
    * Fix code style issues with Black
    
    * formatting
    
    * analysis flag
    
    * analysis flag
    
    * flag fix
    
    * Fix code style issues with Black
    
    * fixture
    
    * fixture
    
    * balsamic no longer in command line
    
    * formatting
    
    * phrasing
    
    * test, add a fixture for balsamic config
    
    * test, add mock balsamic run
    
    * test, add dummy balsamic case fixture
    
    * test, set level caplog and use dummy case name2
    
    * Fix code style issues with Black
    
    * (minor) Minor refactoring to address  code review
    
    * (minor) BALSAMIC uppercase
    
    * Fix code style issues with Black
    
    * (minor) dicstring space added
    
    * Apply suggestions from code review
    
    Co-authored-by: Måns Magnusson <mans.magnusson@scilifelab.se>
    
    * Fix code style issues with Black
    
    * (minor) import modls from cg.store
    
    Co-authored-by: Lint Action <lint-action@samuelmeuli.com>
    Co-authored-by: Hassan Foroughi <hassanfa@users.noreply.github.com>
    Co-authored-by: hassanfa <hassan.foroughi@gmail.com>
    Co-authored-by: Måns Magnusson <mans.magnusson@scilifelab.se>

---
+++ cg/apps/balsamic/api.py
@@ -0,0 +1,79 @@
+import logging
+from cg.utils.commands import Process
+from pathlib import Path
+from typing import List, Dict
+
+LOG = logging.getLogger(__name__)
+
+
+class BalsamicAPI:
+    """Handles execution of BALSAMIC"""
+
+    EXIT_SUCCESS = 0
+
+    def __init__(self, config: dict):
+        self.binary = config["balsamic"]["binary_path"]
+        self.singularity = config["balsamic"]["singularity"]
+        self.reference_config = config["balsamic"]["reference_config"]
+        self.root_dir = config["balsamic"]["root"]
+        self.account = config["balsamic"]["slurm"]["account"]
+        self.email = config["balsamic"]["slurm"]["mail_user"]
+        self.qos = config["balsamic"]["slurm"]["qos"]
+        self.bed_path = config["bed_path"]
+        self.process = Process(self.binary)
+
+    def __build_command_str(self, options: dict) -> List[str]:
+        formatted_options = []
+        for key, val in options.items():
+            if val:
+                formatted_options.append(str(key))
+                formatted_options.append(str(val))
+        return formatted_options
+
+    def config_case(self, arguments: dict, dry: bool = False) -> int:
+        """Create config file for BALSAMIC analysis """
+
+        command = ["config", "case"]
+        options = {
+            "--analysis-dir": self.root_dir,
+            "--singularity": self.singularity,
+            "--reference-config": self.reference_config,
+            "--case-id": arguments.get("case_id"),
+            "--normal": arguments.get("normal"),
+            "--tumor": arguments.get("tumor"),
+            "--output-config": arguments.get("output_config"),
+            "--panel-bed": arguments.get("panel_bed"),
+            "--adapter-trim": arguments.get("adapter_trim"),
+            "--quality-trim": arguments.get("quality_trim"),
+            "--umi": arguments.get("umi"),
+            "--umi-trim-length": arguments.get("umi_trim_length"),
+        }
+
+        options = self.__build_command_str(options)
+
+        if dry:
+            LOG.info(f'Executing command balsamic{" ".join(command + options)}')
+            retcode = self.EXIT_SUCCESS
+        else:
+            retcode = self.process.run_command(command + options)
+        return retcode
+
+    def run_analysis(self, arguments: dict, run_analysis: bool, dry: bool = False) -> int:
+        """Execute BALSAMIC"""
+
+        command = ["run", "analysis"]
+        run_analysis = ["--run-analysis"] if run_analysis else []
+        options = {
+            "--account": self.account,
+            "--mail-user": arguments.get("email") or self.email,
+            "--qos": arguments.get("priority") or self.qos,
+            "--sample-config": arguments.get("sample_config"),
+            "--analysis-type": arguments.get("analysis_type"),
+        }
+        options = self.__build_command_str(options)
+        if dry:
+            LOG.info(f'Executing command balsamic{" ".join(command + options + run_analysis)}')
+            retcode = self.EXIT_SUCCESS
+        else:
+            retcode = self.process.run_command(command + options + run_analysis)
+        return retcode
