64912eeea92865414b0b30249cf8803ff9fc85ee
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-07-04 19:41:57 +0200

    Bump version: 9.25.2 → 9.25.3

--- setup.py
+++ setup.py
@@ -53,7 +53,7 @@ class PyTest(TestCommand):
 
 setup(
     name="cg",
-    version="9.25.2",
+    version="9.25.3",
     description="Clinical Genomics command center.",
     author="Patrik Grenfeldt",
     author_email="patrik.grenfeldt@scilifelab.se",
