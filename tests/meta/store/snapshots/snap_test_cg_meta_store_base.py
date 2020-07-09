66478e1f71b6584d9e7b236e314a73bfd984cfb1
Lint Action <lint-action@samuelmeuli.com>
2020-07-09 13:10:01 +0000

    Fix code style issues with Black

--- tests/meta/store/snapshots/snap_test_cg_meta_store_base.py
+++ tests/meta/store/snapshots/snap_test_cg_meta_store_base.py
@@ -7,11 +7,9 @@ from snapshottest import GenericRepr, Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_build_bundle 1'] = {
-    'created': GenericRepr('datetime.datetime(2020, 3, 1, 0, 0)'),
-    'files': {
-        'path': 'mock_path'
-    },
-    'name': 'case_id',
-    'pipeline_version': 'v8.2.2'
+snapshots["test_build_bundle 1"] = {
+    "created": GenericRepr("datetime.datetime(2020, 3, 1, 0, 0)"),
+    "files": {"path": "mock_path"},
+    "name": "case_id",
+    "pipeline_version": "v8.2.2",
 }
