2d715010cfbf488bbcb4cbf32dff738760c215fb
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-08-25 14:26:08 +0200

    Move tests for nlinks to models

--- tests/meta/compress/test_compress_common.py
+++ tests/meta/compress/test_compress_common.py
@@ -1,63 +1,6 @@
 """ Tests for common functions of the compress API """
-import os
 
 from cg.meta.compress import files
-
-
-def test_get_nlinks_one_link(project_dir):
-    """Test get_nlinks when there is one link to a file"""
-    # GIVEN a file with one link
-    file_path = project_dir / "file"
-    file_path.touch()
-
-    # WHEN getting number of links to inode
-    nlinks = files.get_nlinks(file_link=file_path)
-
-    # THEN number of links should be one
-    assert nlinks == 1
-
-
-def test_get_nlinks_two_links(project_dir):
-    """Test get_nlinks two links"""
-    # GIVEN a file with two links
-    file_path = project_dir / "file"
-    file_path.touch()
-
-    first_link = project_dir / "link-1"
-    os.link(file_path, first_link)
-
-    # WHEN fetching the number of links from the API
-    nlinks = files.get_nlinks(file_link=file_path)
-
-    # THEN number of links should be two
-    assert nlinks == 2
-
-    # WHEN adding yet another link
-    second_link = project_dir / "link-2"
-    os.link(file_path, second_link)
-    nlinks = files.get_nlinks(file_link=file_path)
-
-    # THEN number of links should be three
-    assert nlinks == 3
-
-
-def test_get_nlinks_three_links(project_dir):
-    """Test get_nlinks when three links"""
-    # GIVEN a file with two links
-    file_path = project_dir / "file"
-    file_path.touch()
-
-    first_link = project_dir / "link-1"
-    os.link(file_path, first_link)
-
-    second_link = project_dir / "link-2"
-    os.link(file_path, second_link)
-
-    # WHEN fetching the links from the api
-    nlinks = files.get_nlinks(file_link=file_path)
-
-    # THEN number of links should be three
-    assert nlinks == 3
 
 
 def test_get_latest_version_no_housekeeper_files(compress_api, caplog):
