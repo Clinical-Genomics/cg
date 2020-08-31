0a1ddf36f792e7d83fc549d3353f28d93cd9c0ed
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-08-31 15:23:10 +0200

    Use tmp dir where files are written to

--- cg/apps/crunchy/crunchy.py
+++ cg/apps/crunchy/crunchy.py
@@ -361,10 +361,10 @@ class CrunchyAPI:
         return sbatch_header
 
     @staticmethod
-    def _get_tmp_dir(prefix: str, suffix: str) -> str:
+    def _get_tmp_dir(prefix: str, suffix: str, base: str = None) -> str:
         """Create a temporary directory and return the path to it"""
 
-        with tempfile.TemporaryDirectory(prefix=prefix, suffix=suffix) as dir_name:
+        with tempfile.TemporaryDirectory(prefix=prefix, suffix=suffix, dir=base) as dir_name:
             tmp_dir_path = dir_name
 
         LOG.info("Created temporary dir %s", tmp_dir_path)
@@ -374,7 +374,9 @@ class CrunchyAPI:
     def _get_slurm_fastq_to_spring(compression_obj: CompressionData) -> str:
         """Create and return the body of a sbatch script that runs FASTQ to SPRING"""
         LOG.info("Generating FASTQ to SPRING sbatch body")
-        tmp_dir_path = CrunchyAPI._get_tmp_dir(prefix="spring_", suffix="_compress")
+        tmp_dir_path = CrunchyAPI._get_tmp_dir(
+            prefix="spring_", suffix="_compress", base=compression_obj.analysis_dir
+        )
         sbatch_body = SBATCH_FASTQ_TO_SPRING.format(
             fastq_first=compression_obj.fastq_first,
             fastq_second=compression_obj.fastq_second,
@@ -388,13 +390,13 @@ class CrunchyAPI:
 
     @staticmethod
     def _get_slurm_spring_to_fastq(
-        compression_obj: CompressionData,
-        checksum_first: str,
-        checksum_second: str,
+        compression_obj: CompressionData, checksum_first: str, checksum_second: str,
     ) -> str:
         """Create and return the body of a sbatch script that runs SPRING to FASTQ"""
         LOG.info("Generating SPRING to FASTQ sbatch body")
-        tmp_dir_path = CrunchyAPI._get_tmp_dir(prefix="spring_", suffix="_decompress")
+        tmp_dir_path = CrunchyAPI._get_tmp_dir(
+            prefix="spring_", suffix="_decompress", base=compression_obj.analysis_dir
+        )
         sbatch_body = SBATCH_SPRING_TO_FASTQ.format(
             spring_path=compression_obj.spring_path,
             fastq_first=compression_obj.fastq_first,
