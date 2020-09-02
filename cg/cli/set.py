4c9af99d924771c6db105e631b3765e307b5a464
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-09-02 10:16:21 +0200

    fix faulty option help

--- cg/cli/set.py
+++ cg/cli/set.py
@@ -132,7 +132,7 @@ def show_set_sample_help(sample_obj: models.Sample = "None"):
     click.echo(f"To set apptag use '{OPTION_SHORT_KEY_VALUE} application_version [APPTAG]")
     click.echo(f"To set customer use '{OPTION_SHORT_KEY_VALUE} customer [CUSTOMER]")
     show_option_help(option_long=OPTION_LONG_SKIP_LIMS, option_help=HELP_SKIP_LIMS)
-    show_option_help(OPTION_SHORT_KEY_VALUE, OPTION_LONG_KEY_VALUE, HELP_YES)
+    show_option_help(OPTION_SHORT_YES, OPTION_LONG_YES, HELP_YES)
 
 
 def show_option_help(option_short: str = None, option_long: str = None, option_help: str = None):
