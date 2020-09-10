03affa75547d8828d0174e2ebba48ff4abb5d916
Mropat <mariaropat@gmail.com>
2020-09-10 10:26:48 +0200

    better log message

--- cg/cli/workflow/mip_dna/base.py
+++ cg/cli/workflow/mip_dna/base.py
@@ -98,7 +98,9 @@ def link(context: click.Context, case_id: str, sample_id: str):
         )
         # This block is necessary to handle cases where data analysis is not set in ClinicalDB for old samples
         if not link_obj.sample.data_analysis:
-            LOG.warning(f"No analysis set for {link_obj.sample.internal_id}")
+            LOG.warning(
+                f"No analysis set for {link_obj.sample.internal_id}, file will still be linked"
+            )
             dna_api.link_sample(sample=link_obj.sample, case_id=case_id)
             return
 
