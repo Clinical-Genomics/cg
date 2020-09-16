53623511b9f3519e6d901c9609a4e1b031a802ee
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2020-09-16 07:23:12 +0200

    feat(mip): Added skip of TB also for rna

--- cg/cli/workflow/mip_rna/base.py
+++ cg/cli/workflow/mip_rna/base.py
@@ -103,6 +103,10 @@ def run(
         rna_api.run_command(dry_run=dry_run, **kwargs)
         return
 
+    if mip_dry_run:
+        rna_api.run_command(**kwargs)
+        return
+
     rna_api.run_command(**kwargs)
     rna_api.tb.mark_analyses_deleted(case_id=case_id)
     rna_api.tb.add_pending_analysis(case_id=case_id, email=email)
