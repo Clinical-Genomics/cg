e0ca92225a49d6fa92ac8b5c22f9b289e20faf2b
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-09-16 13:31:56 +0200

    make reference_genome text column

--- scripts/add-microbial-columns-to-sample.sql
+++ scripts/add-microbial-columns-to-sample.sql
@@ -1,6 +1,6 @@
 ALTER TABLE sample ADD COLUMN organism_id INTEGER NULL;
 
 ALTER TABLE `sample`
-ADD COLUMN `reference_genome` INT(11) DEFAULT NULL;
+ADD COLUMN  `reference_genome` varchar(255) DEFAULT NULL;
 
 ALTER TABLE sample ADD CONSTRAINT `organism_ibfk_2` FOREIGN KEY (`organism_id`) REFERENCES `organism` (`id`);
