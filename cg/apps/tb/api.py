f4e38f98fc7e787ba93b120782e45014ff0102e1
Mropat <mariaropat@gmail.com>
2020-09-24 15:37:59 +0200

    Adding first REST query

--- cg/apps/tb/api.py
+++ cg/apps/tb/api.py
@@ -2,6 +2,9 @@
 
 import logging
 from trailblazer.store import Store
+from google.auth.crypt import RSASigner
+from google.auth import jwt
+import requests
 
 LOG = logging.getLogger(__name__)
 
@@ -14,3 +17,18 @@ class TrailblazerAPI(Store):
             config["trailblazer"]["database"],
             families_dir=config["trailblazer"]["root"],
         )
+        self.service_account = config["trailblazer"]["service_account"]
+        self.service_account_auth_file = config["trailblazer"]["service_account_auth_file"]
+        self.host = config["trailblazer"]["host"]
+
+    @property
+    def auth_header(self):
+        signer = RSASigner.from_service_account_file(self.service_account_auth_file)
+        payload = {"email": self.service_account}
+        jwt_token = jwt.encode(signer=signer, payload=payload)
+        auth_header = {"Authorization": f"Bearer {jwt_token}"}
+        return auth_header
+
+    def get_analysis(self, analysis_id: int):
+        response = requests.get(self.host + f"/analyses/{analysis_id}", headers=self.auth_header)
+        LOG.info(response)
