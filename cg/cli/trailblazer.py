78b067dc806dd5785e2736d4d329be20a34c4366
Mropat <mariaropat@gmail.com>
2020-09-24 15:57:41 +0200

    cli command to test api

---
+++ cg/cli/trailblazer.py
@@ -0,0 +1,19 @@
+import logging
+import click
+from cg.apps.tb import TrailblazerAPI
+
+LOG = logging.getLogger(__name__)
+
+
+@click.group()
+@click.pass_context
+def trailblazer(context):
+    """Trailblazer context"""
+    context.obj["trailblazer_api"] = TrailblazerAPI(context.obj)
+
+
+@trailblazer.command("analysis")
+@click.argument("analysis_id", type=int)
+@click.pass_context
+def analysis(context, analysis_id: int):
+    context.obj["trailblazer_api"].get_analysis(analysis_id=analysis_id)
