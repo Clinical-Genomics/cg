037fc4912f92ba98d8bb8754372d10037cc004e7
barrystokman <barry.stokman@gmail.com>
2020-09-25 10:10:06 +0200

    order keys alphabetically

--- cg/constants.py
+++ cg/constants.py
@@ -223,28 +223,37 @@ MIP_RNA_TAGS = {
 }
 
 MICROSALT_TAGS = {
-    ("analysis", "sampleinfo"): {
-        "tags": ["analysis", "sampleinfo"],
+    ("alignment", "reference-alignment"): {
+        "tags": ["alignment", "reference-alignment"],
         "is_mandatory": True,
     },
-    ("result_aggregation", "microsalt-qc"): {
-        "tags": ["result-aggregration-qc"],
+    ("alignment", "reference-alignment-deduplicated"): {
+        "tags": ["alignment", "reference-alignment-deduplicated"],
+        "index_tags": ["reference-alignment-deduplicate-index"],
         "is_mandatory": True,
     },
-    ("result_aggregation", "microsalt-type"): {
-        "tags": ["result-aggregation-type"],
+    ("alignment", "reference-alignment-sorted"): {
+        "tags": ["alignment", "reference-alignment-sorted"],
         "is_mandatory": True,
     },
-    ("result_aggregation", "microsalt-json"): {
-        "tags": ["result-aggregation-json"],
+    ("analysis", "logfile"): {
+        "tags": ["analysis", "log"],
         "is_mandatory": True,
     },
     ("analysis", "runtime-settings"): {
         "tags": ["analysis", "runtime-settings"],
         "is_mandatory": True,
     },
+    ("analysis", "sampleinfo"): {
+        "tags": ["analysis", "sampleinfo"],
+        "is_mandatory": True,
+    },
     ("assembly", "assembly"): {
         "tags": ["assembly"],
+        "is_mandatory": True,
+    },
+    ("assembly", "quast-results"): {
+        "tags": ["assembly", "quast-results"],
         "is_mandatory": True,
     },
     ("concatination", "trimmed-forward-reads"): {
@@ -255,25 +264,16 @@ MICROSALT_TAGS = {
         "tags": ["concatination", "trimmed-reverse-reads"],
         "is_mandatory": True,
     },
-    ("analysis", "logfile"): {
-        "tags": ["analysis", "log"],
+    ("result_aggregation", "microsalt-json"): {
+        "tags": ["result-aggregation-json"],
         "is_mandatory": True,
     },
-    ("assembly", "quast-results"): {
-        "tags": ["assembly", "quast-results"],
+    ("result_aggregation", "microsalt-qc"): {
+        "tags": ["result-aggregration-qc"],
         "is_mandatory": True,
     },
-    ("alignment", "reference-alignment"): {
-        "tags": ["alignment", "reference-alignment"],
-        "is_mandatory": True,
-    },
-    ("alignment", "reference-alignment-sorted"): {
-        "tags": ["alignment", "reference-alignment-sorted"],
-        "is_mandatory": True,
-    },
-    ("alignment", "reference-alignment-deduplicated"): {
-        "tags": ["alignment", "reference-alignment-deduplicated"],
-        "index_tags": ["reference-alignment-deduplicate-index"],
+    ("result_aggregation", "microsalt-type"): {
+        "tags": ["result-aggregation-type"],
         "is_mandatory": True,
     },
     ("insertsize_calc", "picard-insertsize"): {
