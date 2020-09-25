e1db67a146f9a36e1a7d6f416eaec648bb930bcd
Barry Stokman <26384108+barrystokman@users.noreply.github.com>
2020-09-25 10:13:13 +0200

    Update cg/store/api/findbusinessdata.py
    Co-authored-by: Henrik Stranneheim <henrik.stranneheim@scilifelab.se>

--- cg/store/api/findbusinessdata.py
+++ cg/store/api/findbusinessdata.py
@@ -66,7 +66,7 @@ class FindBusinessDataHandler(BaseHandler):
     def microbial_analysis(
         self, microbial_order_id: models.MicrobialOrder, started_at: dt.datetime
     ) -> models.Analysis:
-        """Fetch a microSALT analysis."""
+        """Get a microbial analysis"""
         return self.Analysis.query.filter_by(
             microbial_order_id=microbial_order_id, started_at=started_at
         ).first()
