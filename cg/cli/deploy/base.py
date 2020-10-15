87dcdb1e8c1948c47d091bd6d27cfe22f39215d4
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-10-15 12:52:39 +0200

    Deploy Genotype with CG (#790)
    * Deploy genotype with CG and tests

--- cg/cli/deploy/base.py
+++ cg/cli/deploy/base.py
@@ -15,6 +15,7 @@ LOG = logging.getLogger(__name__)
 def deploy(context, dry_run):
     """Deploy tools with CG. Use --help to see what tools are available"""
     LOG.info("Running CG DEPLOY")
+    print(context.obj)
     shipping_api = ShippingAPI(context.obj["shipping"])
     shipping_api.set_dry_run(dry_run)
     context.obj["shipping_api"] = shipping_api
@@ -26,7 +27,17 @@ def deploy_shipping_cmd(context):
     """Deploy the shipping tool"""
     LOG.info("Deploying shipping with CG")
     shipping_api: ShippingAPI = context.obj["shipping_api"]
-    shipping_api.deploy_shipping()
+    shipping_api.deploy(app_name="shipping")
+
+
+@click.command(name="genotype")
+@click.pass_context
+def deploy_genotype_cmd(context):
+    """Deploy the shipping tool"""
+    LOG.info("Deploying genotype with CG")
+    shipping_api: ShippingAPI = context.obj["shipping_api"]
+    shipping_api.deploy(app_name="genotype")
 
 
 deploy.add_command(deploy_shipping_cmd)
+deploy.add_command(deploy_genotype_cmd)
