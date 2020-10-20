5c15387ad48943dbdc06aafa9b30bd3dfd386033
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-10-20 08:51:04 +0200

    make migration re-runnable

--- scripts/add-data_analysis_to_case.py
+++ scripts/add-data_analysis_to_case.py
@@ -3,11 +3,6 @@ from cg.store import Store, models
 from ruamel import yaml
 
 
-def dict_print(dict_):
-    for key in sorted(dict_):
-        print(f"{key}: {dict_.get(key)}")
-
-
 @click.command("add-data-analysis-to-case")
 @click.option("-c", "--config-file", type=click.File())
 def add_data_analysis(config_file: click.File):
@@ -15,11 +10,11 @@ def add_data_analysis(config_file: click.File):
     config = yaml.safe_load(config_file)
     store = Store(config["database"])
 
-    for sample in models.Sample.query.all():
+    for sample in models.Sample.query.outerjoin(models.Sample.links).filter(models.Sample.links
+                                                                            is None
+                                                                            ).all():
         # if sample has links then we deal with it later in case processing
-        # if sample does have empty data_analysis then its ok to put on case
-        # if sample is fastq then ??
-        if sample.links or not sample.data_analysis or sample.data_analysis == "fastq":
+        if sample.links:
             print(".", end="", flush=True)
             continue
 
@@ -37,19 +32,22 @@ def add_data_analysis(config_file: click.File):
                 fg="yellow",
             )
         )
-        # if sample.reads == 0 and click.confirm(click.style(
-        #             f"Delete sample: {sample.__str__()}?",
-        #             fg="red",
-        #         )):
-        #     store.delete(sample)
-        #     click.echo(
-        #         click.style(
-        #             f"Deleted!"
-        #         )
-        #     )
-        #     continue
+
+        if sample.name == "4321":
+            click.echo(click.style(
+                f"Deleting test sample: {sample.__str__()}?",
+                fg="red",
+            ))
+            store.delete(sample)
+            click.echo(
+                click.style(
+                    f"Deleted!"
+                )
+            )
+            continue
 
         case = store.add_family(data_analysis=sample.data_analysis, panels=None, name=sample.name)
+        sample.data_analysis = None
         case.customer_id = sample.customer.id
         click.echo(
             click.style(
@@ -64,39 +62,57 @@ def add_data_analysis(config_file: click.File):
             )
         )
         store.relate_sample(sample=sample, family=case, status="unknown")
+        store.commit()
 
     # pull data_analysis from sample to case
-    for case in models.Family.query.all():
+    for case in models.Family.query.filter(
+            models.Family.data_analysis.is_(None)
+    ).all():
 
         click.echo(click.style("processing case : " + case.__str__(), fg="white"))
 
-        data_analysis = None
-        for link_obj in case.links:
+        analysis_pipelines = set()
+        for analysis_obj in case.analyses:
+            if analysis_obj.pipeline:
+                analysis_pipelines.add(analysis_obj.pipeline.lower())
+
             if (
-                data_analysis
-                and link_obj.sample.data_analysis
-                and data_analysis != link_obj.sample.data_analysis
+                    len(analysis_pipelines) > 1
             ):
                 click.echo(
                     click.style(
-                        "Found case with mixed data_analysis: " + case.__str__(),
+                        f"Found case ({case.__str__()}) with multiple pipelines: {analysis_pipelines} ",
                         fg="red",
                     )
                 )
-                continue
 
-            data_analysis = data_analysis or link_obj.sample.data_analysis
+        data_analyses = set()
+        for link_obj in case.links:
+            if link_obj.sample.data_analysis:
+                data_analyses.add(link_obj.sample.data_analysis.lower())
 
-        if case.analyses:
-            case.data_analysis = case.analyses[0].pipeline
-        else:
-            click.echo(
-                click.style(
-                    "Found case without analysis: " + case.__str__(),
-                    fg="yellow",
+            if (
+                    len(data_analyses) > 1
+            ):
+                click.echo(
+                    click.style(
+                        f"Found case ({case.__str__()}) with multiple data_analyses: {data_analyses} ",
+                        fg="red",
+                    )
                 )
-            )
-            case.data_analysis = data_analysis
+
+        if analysis_pipelines:
+            if len(analysis_pipelines) == 1:
+                case.data_analysis = analysis_pipelines.pop()
+            else:
+                case.data_analysis = "|".join(analysis_pipelines)
+        elif data_analyses:
+            if len(data_analyses) == 1:
+                case.data_analysis = data_analyses.pop()
+            else:
+                case.data_analysis = "|".join(data_analyses)
+        else:
+            case.data_analysis = ""
 
         for link_obj in case.links:
             link_obj.sample.data_analysis = None
@@ -108,7 +124,6 @@ def add_data_analysis(config_file: click.File):
                     fg="yellow",
                 )
             )
-            continue
 
         click.echo(
             click.style(
@@ -117,7 +132,7 @@ def add_data_analysis(config_file: click.File):
             )
         )
 
-    store.commit()
+        store.commit()
 
 
 if __name__ == "__main__":
