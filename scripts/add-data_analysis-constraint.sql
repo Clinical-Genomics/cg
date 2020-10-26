d4f7412606b06c243f4b1f11ebddb502e2f55226
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-10-26 15:51:06 +0100

    add data delivery to keep delivery type

--- scripts/add-data_analysis-constraint.sql
+++ scripts/add-data_analysis-constraint.sql
@@ -1,3 +1,8 @@
+ALTER TABLE `family`
+ADD COLUMN `data_delivery` ENUM('fastq', 'custom')
+ CHARACTER SET latin1
+ COLLATE latin1_swedish_ci
+ NOT NULL;
 
 Update `family` SET data_analysis = 'mip-dna' where data_analysis like 'mip';
 Update `family` SET data_analysis = 'mip-rna' where data_analysis like 'mip + rna';
@@ -5,6 +10,8 @@ Update `family` SET data_analysis = 'balsamic' where data_analysis like 'balsami
 Update `family` SET data_analysis = 'fastq' where data_analysis like 'fastq';
 Update `family` SET data_analysis = 'mip-rna' where data_analysis like 'mip rna';
 Update `family` SET data_analysis = 'microsalt' where data_analysis like 'microbial';
+Update `family` SET data_delivery = 'fastq' where data_analysis like 'microbial|fastq';
+Update `family` SET data_delivery = 'custom' where data_analysis like 'microbial|custom';
 Update `family` SET data_analysis = 'microsalt' where data_analysis like 'microbial|fastq';
 Update `family` SET data_analysis = 'microsalt' where data_analysis like 'microbial|custom';
 
@@ -18,5 +25,5 @@ ALTER TABLE `analysis` CHANGE `pipeline` `pipeline` ENUM('balsamic', 'fastq', 'm
  COLLATE latin1_swedish_ci
  NOT NULL;
 
-
-select distinct data_analysis from family;
+-- select distinct data_analysis from family;
+-- select distinct data_delivery from family;
