422b2e3c9570c4fe00df098cad62a9dac2033fd0
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-10-27 10:01:54 +0100

    draft

--- scripts/add-data_analysis-constraint.sql
+++ scripts/add-data_analysis-constraint.sql
@@ -1,19 +1,24 @@
 ALTER TABLE `family`
-ADD COLUMN `data_delivery` ENUM('fastq', 'custom')
+ADD COLUMN `data_delivery` ENUM('fastq', 'qc', 'standard')
  CHARACTER SET latin1
  COLLATE latin1_swedish_ci
- NOT NULL;
+ NOT NULL
+ DEFAULT 'standard';
 
+Update `family` SET data_analysis = 'mip-dna' where data_analysis like 'mip';
 Update `family` SET data_analysis = 'mip-dna' where data_analysis like 'mip';
 Update `family` SET data_analysis = 'mip-rna' where data_analysis like 'mip + rna';
 Update `family` SET data_analysis = 'balsamic' where data_analysis like 'balsamic';
 Update `family` SET data_analysis = 'fastq' where data_analysis like 'fastq';
+Update `family` SET data_delivery = 'fastq' where data_analysis like 'fastq';
 Update `family` SET data_analysis = 'mip-rna' where data_analysis like 'mip rna';
 Update `family` SET data_analysis = 'microsalt' where data_analysis like 'microbial';
-Update `family` SET data_delivery = 'fastq' where data_analysis like 'microbial|fastq';
-Update `family` SET data_delivery = 'custom' where data_analysis like 'microbial|custom';
+Update `family` SET data_delivery = 'qc' where data_analysis like 'microbial|fastq';
+Update `family` SET data_delivery = 'standard' where data_analysis like 'microbial|custom';
 Update `family` SET data_analysis = 'microsalt' where data_analysis like 'microbial|fastq';
 Update `family` SET data_analysis = 'microsalt' where data_analysis like 'microbial|custom';
+
+
 
 ALTER TABLE `family` CHANGE `data_analysis` `data_analysis` ENUM('balsamic', 'fastq', 'microsalt', 'mip-dna', 'mip-rna')
  CHARACTER SET latin1
@@ -27,3 +32,4 @@ ALTER TABLE `analysis` CHANGE `pipeline` `pipeline` ENUM('balsamic', 'fastq', 'm
 
 -- select distinct data_analysis from family;
 -- select distinct data_delivery from family;
+
