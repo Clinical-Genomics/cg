9cad446fe21bdb0d98aa38c4ad751e6a7a43ca33
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-11-03 12:14:19 +0100

    make mip flags python nameable

--- scripts/add-data_analysis-constraint.sql
+++ scripts/add-data_analysis-constraint.sql
@@ -9,20 +9,20 @@ Update `family` SET data_delivery = 'qc' where data_analysis like 'microbial|fas
 Update `family` SET data_delivery = 'standard' where data_analysis like 'microbial|custom';
 Update `family` SET data_delivery = 'fastq' where data_analysis like 'fastq';
 
-Update `family` SET data_analysis = 'mip-dna' where data_analysis like 'mip';
-Update `family` SET data_analysis = 'mip-dna' where data_analysis like 'mip';
-Update `family` SET data_analysis = 'mip-rna' where data_analysis like 'mip + rna';
+Update `family` SET data_analysis = 'mip_dna' where data_analysis like 'mip';
+Update `family` SET data_analysis = 'mip_dna' where data_analysis like 'mip';
+Update `family` SET data_analysis = 'mip_rna' where data_analysis like 'mip + rna';
 Update `family` SET data_analysis = 'balsamic' where data_analysis like 'balsamic';
 Update `family` SET data_analysis = 'fastq' where data_analysis like 'fastq';
-Update `family` SET data_analysis = 'mip-rna' where data_analysis like 'mip rna';
+Update `family` SET data_analysis = 'mip_rna' where data_analysis like 'mip rna';
 Update `family` SET data_analysis = 'microsalt' where data_analysis like 'microbial';
 Update `family` SET data_analysis = 'microsalt' where data_analysis like 'microbial|fastq';
 Update `family` SET data_analysis = 'microsalt' where data_analysis like 'microbial|custom';
 -- Update `family` SET data_analysis = 'mip-dna' where data_analysis is null;
 
-select * from family where family.data_analysis not in ('balsamic', 'fastq', 'microsalt', 'mip-dna', 'mip-rna');
+select * from family where family.data_analysis not in ('balsamic', 'fastq', 'microsalt', 'mip_dna', 'mip_rna');
 
-ALTER TABLE `family` CHANGE `data_analysis` `data_analysis` ENUM('balsamic', 'fastq', 'microsalt', 'mip-dna', 'mip-rna')
+ALTER TABLE `family` CHANGE `data_analysis` `data_analysis` ENUM('balsamic', 'fastq', 'microsalt', 'mip_dna', 'mip_rna')
  CHARACTER SET latin1
  COLLATE latin1_swedish_ci
  NOT NULL;
@@ -30,9 +30,9 @@ ALTER TABLE `family` CHANGE `data_analysis` `data_analysis` ENUM('balsamic', 'fa
 Update `analysis` SET pipeline = 'balsamic' where pipeline like 'Balsamic';
 Update `analysis` SET pipeline = 'mip-dna' where pipeline like 'mip';
 
-select * from analysis where analysis.pipeline not in ('balsamic', 'fastq', 'microsalt', 'mip-dna', 'mip-rna');
+select * from analysis where analysis.pipeline not in ('balsamic', 'fastq', 'microsalt', 'mip_dna', 'mip_rna');
 
-ALTER TABLE `analysis` CHANGE `pipeline` `pipeline` ENUM('balsamic', 'fastq', 'microsalt', 'mip-dna', 'mip-rna')
+ALTER TABLE `analysis` CHANGE `pipeline` `pipeline` ENUM('balsamic', 'fastq', 'microsalt', 'mip_dna', 'mip_rna')
  CHARACTER SET latin1
  COLLATE latin1_swedish_ci
  NOT NULL;
