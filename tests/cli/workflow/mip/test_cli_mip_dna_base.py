2f07773976fb5c2206b276badafa2641ced5349d
henningonsbring <henning.onsbring@gmail.com>
2020-11-05 15:22:14 +0100

    Test for spring automation

--- tests/cli/workflow/mip/test_cli_mip_dna_base.py
+++ tests/cli/workflow/mip/test_cli_mip_dna_base.py
@@ -3,22 +3,22 @@ import logging
 import os
 from pathlib import Path
 
-#from cg.cli.workflow.mip_dna.base import mip_dna
-from cg.cli.workflow.mip_dna.base import run
+from cg.cli.workflow.mip_dna.base import mip_dna
+#from cg.cli.workflow.mip_dna.base import run
 
 CASE_ID = "yellowhog"
 EMAIL = "james.holden@scilifelab.se"
 
 def test_mip_dna(cli_runner, mip_context, caplog):
     # GIVEN fastqs are all decompressed and linked
-    pedigree_path = mip_context.get("dna_api").get_pedigree_config_path(case_id="yellowhog")
+    pedigree_path = mip_context.get("dna_api").get_pedigree_config_path(case_id=CASE_ID)
     Path.mkdir(pedigree_path.parent, parents=True, exist_ok=True)
     os.link("tests/fixtures/apps/mip/dna/store/pedigree.yaml", pedigree_path)
 
     caplog.set_level(logging.INFO)
     # WHEN calling mip_dna
-    #result = cli_runner.invoke(mip_dna, ["-c", CASE_ID, "--dry-run"], obj=mip_context, catch_exceptions=False)
-    result = cli_runner.invoke(run, ["--dry-run", CASE_ID], obj=mip_context, catch_exceptions=False)
+    result = cli_runner.invoke(mip_dna, ["-c", CASE_ID, "--dry-run"], obj=mip_context, catch_exceptions=False)
+    #result = cli_runner.invoke(run, ["--dry-run", CASE_ID], obj=mip_context, catch_exceptions=False)
 
     # THEN no error should be thrown
     assert result.exit_code == 0
