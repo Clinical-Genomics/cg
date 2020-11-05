7f0075905d24cef1e048c21f56d507db6dd95b8f
henningonsbring <henning.onsbring@gmail.com>
2020-11-05 14:27:11 +0100

    Added test for successful start of mip

--- tests/cli/workflow/mip/test_cli_mip_dna_base.py
+++ tests/cli/workflow/mip/test_cli_mip_dna_base.py
@@ -3,11 +3,13 @@ import logging
 import os
 from pathlib import Path
 
-from cg.cli.workflow.mip_dna.base import mip_dna
+#from cg.cli.workflow.mip_dna.base import mip_dna
+from cg.cli.workflow.mip_dna.base import run
 
 CASE_ID = "yellowhog"
+EMAIL = "james.holden@scilifelab.se"
 
-def test_mip_dna(cli_runner, mip_context, caplog, hk_config_dict):
+def test_mip_dna(cli_runner, mip_context, caplog):
     # GIVEN fastqs are all decompressed and linked
     pedigree_path = mip_context.get("dna_api").get_pedigree_config_path(case_id="yellowhog")
     Path.mkdir(pedigree_path.parent, parents=True, exist_ok=True)
@@ -15,14 +17,17 @@ def test_mip_dna(cli_runner, mip_context, caplog, hk_config_dict):
 
     caplog.set_level(logging.INFO)
     # WHEN calling mip_dna
-    result = cli_runner.invoke(mip_dna, ["-c", CASE_ID], obj=hk_config_dict, catch_exceptions=False)
+    #result = cli_runner.invoke(mip_dna, ["-c", CASE_ID, "--dry-run"], obj=mip_context, catch_exceptions=False)
+    result = cli_runner.invoke(run, ["--dry-run", CASE_ID], obj=mip_context, catch_exceptions=False)
 
     # THEN no error should be thrown
-    print(result.output)
     assert result.exit_code == 0
-
     # THEN should not output "Decompression is running"
+    assert "Decompression is running" not in caplog.text
     # THEN should not output "Started decompression"
+    assert "Started decompression" not in caplog.text
     # THEN should not output "Creating links"
-    assert False
+    assert "Creating links" not in caplog.text
+    # THEN mip should start
+    assert "MIP rd-dna run started" in caplog.text
 
