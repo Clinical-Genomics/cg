20782c5cd5bb7c577afda4f2be2df9db275619bf
Mropat <mariaropat@gmail.com>
2020-11-09 16:20:15 +0100

    Bump version: 13.18.0 → 14.0.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ except FileNotFoundError:
 
 setup(
     name=NAME,
-    version="13.18.0",
+    version="14.0.0",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
