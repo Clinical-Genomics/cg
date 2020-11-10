8f942f4c926a1d20667f939e81d8c6621f4f1d63
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-11-10 09:34:05 +0100

    format with black

--- cg/utils/StrEnum.py
+++ cg/utils/StrEnum.py
@@ -3,6 +3,5 @@ from enum import Enum
 
 
 class StrEnum(str, Enum):
-
     def __str__(self) -> str:
         return str.__str__(self)
--- tests/cli/upload/test_cli_upload_auto.py
+++ tests/cli/upload/test_cli_upload_auto.py
@@ -15,9 +15,7 @@ def test_upload_auto_with_pipeline_as_argument(
 
     # WHEN uploading all analysis from pipeline MIP
     caplog.set_level(logging.INFO)
-    cli_runner.invoke(
-        auto, ["--pipeline", str(pipeline)], obj=base_context, catch_exceptions=False
-    )
+    cli_runner.invoke(auto, ["--pipeline", str(pipeline)], obj=base_context, catch_exceptions=False)
 
     # THEN assert that the MIP analysis was successfully uploaded
     with caplog.at_level(logging.INFO):
