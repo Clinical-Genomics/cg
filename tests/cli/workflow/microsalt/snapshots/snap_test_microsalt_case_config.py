d656322b6832a7cd3b5bb9eaf9fdcb8aa66748be
Isak Sylvin <isak.sylvin@hasta.scilifelab.se>
2020-11-11 15:59:09 +0100

    Blacked everything?

--- tests/cli/workflow/microsalt/snapshots/snap_test_microsalt_case_config.py
+++ tests/cli/workflow/microsalt/snapshots/snap_test_microsalt_case_config.py
@@ -7,7 +7,9 @@ from snapshottest import Snapshot
 
 snapshots = Snapshot()
 
-snapshots['test_dry_order 1'] = '''[
+snapshots[
+    "test_dry_order 1"
+] = """[
     {
         "CG_ID_project": "microbial_order_test",
         "CG_ID_sample": "microbial_sample_id",
@@ -25,9 +27,11 @@ snapshots['test_dry_order 1'] = '''[
         "reference": null
     }
 ]
-'''
+"""
 
-snapshots['test_dry_sample 1'] = '''[
+snapshots[
+    "test_dry_sample 1"
+] = """[
     {
         "CG_ID_project": "microbial_order_test",
         "CG_ID_sample": "microbial_sample_id",
@@ -45,9 +49,11 @@ snapshots['test_dry_sample 1'] = '''[
         "reference": null
     }
 ]
-'''
+"""
 
-snapshots['test_dry_sample_order 1'] = '''[
+snapshots[
+    "test_dry_sample_order 1"
+] = """[
     {
         "CG_ID_project": "microbial_order_test",
         "CG_ID_sample": "microbial_sample_id",
@@ -65,42 +71,42 @@ snapshots['test_dry_sample_order 1'] = '''[
         "reference": null
     }
 ]
-'''
+"""
 
-snapshots['test_sample 1'] = [
-    '''[
-''',
-    '''    {
-''',
-    '''        "CG_ID_project": "microbial_order_test",
-''',
-    '''        "CG_ID_sample": "microbial_sample_id",
-''',
-    '''        "Customer_ID": "cust_test",
-''',
-    '''        "Customer_ID_project": 123456,
-''',
-    '''        "Customer_ID_sample": "microbial_name_test",
-''',
-    '''        "application_tag": "dummy_tag",
-''',
-    '''        "date_arrival": "0001-01-01 00:00:00",
-''',
-    '''        "date_libprep": "0001-01-01 00:00:00",
-''',
-    '''        "date_sequencing": "0001-01-01 00:00:00",
-''',
-    '''        "method_libprep": "1337:00",
-''',
-    '''        "method_sequencing": "1338:00",
-''',
-    '''        "organism": "organism_test",
-''',
-    '''        "priority": "research",
-''',
-    '''        "reference": null
-''',
-    '''    }
-''',
-    ']'
+snapshots["test_sample 1"] = [
+    """[
+""",
+    """    {
+""",
+    """        "CG_ID_project": "microbial_order_test",
+""",
+    """        "CG_ID_sample": "microbial_sample_id",
+""",
+    """        "Customer_ID": "cust_test",
+""",
+    """        "Customer_ID_project": 123456,
+""",
+    """        "Customer_ID_sample": "microbial_name_test",
+""",
+    """        "application_tag": "dummy_tag",
+""",
+    """        "date_arrival": "0001-01-01 00:00:00",
+""",
+    """        "date_libprep": "0001-01-01 00:00:00",
+""",
+    """        "date_sequencing": "0001-01-01 00:00:00",
+""",
+    """        "method_libprep": "1337:00",
+""",
+    """        "method_sequencing": "1338:00",
+""",
+    """        "organism": "organism_test",
+""",
+    """        "priority": "research",
+""",
+    """        "reference": null
+""",
+    """    }
+""",
+    "]",
 ]
