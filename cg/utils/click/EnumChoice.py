c1d95a8068b0eca03b672af6b027c4131a4c9c2a
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-11-17 13:51:44 +0100

    make use_value use values in EnumChoice

--- cg/utils/click/EnumChoice.py
+++ cg/utils/click/EnumChoice.py
@@ -11,12 +11,16 @@ class EnumChoice(click.Choice):
     """ class to use when the click option is an Enum """
 
     def __init__(self, enum, case_sensitive=False, use_value=True):
-        if use_value and isinstance(enum, tuple):
-            choices = (_.value for _ in enum)
-        elif isinstance(enum, tuple):
-            choices = (_.name for _ in enum)
+        if isinstance(enum, tuple):
+            if use_value:
+                choices = (_.value for _ in enum)
+            else:
+                choices = (_.name for _ in enum)
         elif isinstance(enum, EnumMeta):
-            choices = enum.__members__
+            if use_value:
+                choices = enum
+            else:
+                choices = enum.__members__
         else:
             raise TypeError("`enum` must be `tuple` or `Enum`")
 
@@ -25,6 +29,7 @@ class EnumChoice(click.Choice):
 
         self.__enum = enum
         self.__case_sensitive = case_sensitive
+        self.__use_value = use_value
 
         super().__init__(list(sorted(set(choices))))
 
@@ -34,10 +39,16 @@ class EnumChoice(click.Choice):
 
         value = super().convert(value, param, ctx)
 
-        if not self.__case_sensitive:
-            return next(_ for _ in self.__enum if _.name.lower() == value.lower())
+        if self.__use_value:
+            if not self.__case_sensitive:
+                return next(_ for _ in self.__enum if _.value.lower() == value.lower())
+            else:
+                return next(_ for _ in self.__enum if _.value == value)
         else:
-            return next(_ for _ in self.__enum if _.name == value)
+            if not self.__case_sensitive:
+                return next(_ for _ in self.__enum if _.name.lower() == value.lower())
+            else:
+                return next(_ for _ in self.__enum if _.name == value)
 
     def get_metavar(self, param):
         word = self.__enum.__name__
