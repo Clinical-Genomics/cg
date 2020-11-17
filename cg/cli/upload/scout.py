b816f00533d2db2fe4fefcbb74403d3a0381e610
mikaell <mikael.laaksonen@scilifelab.se>
2020-11-17 21:59:33 +0100

    Trian and Error

--- cg/cli/upload/scout.py
+++ cg/cli/upload/scout.py
@@ -38,7 +38,7 @@ def scout(context, re_upload, print_console, case_id):
     import pdb; pdb.set_trace()
     family_obj = status_db.family(case_id)
     analysis = status_db.analyses(family=family_obj)
-    LOG.info("analysis:{}".format(anaysis))
+    LOG.info("analysis:{}".format(analysis))
     LOG.info("family_obj:{}".format(family_obj))
     LOG.info("family_obj.analyses:{}".format(family_obj.analyses))
     
