e4c057f1dba5fdc76030d2eab0c97e3e324db5a7
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-11-26 15:43:15 +0100

    remove deprecated capture_kit from pool

--- scripts/add-fluffy-pipeline.sql
+++ scripts/add-fluffy-pipeline.sql
@@ -8,3 +8,6 @@ ALTER TABLE `analysis` CHANGE `pipeline` `pipeline` ENUM('balsamic', 'fastq', 'f
  CHARACTER SET latin1
  COLLATE latin1_swedish_ci
  NOT NULL;
+
+# remove deprecated capture-kit from pool table
+ALTER TABLE `pool` DROP COLUMN `capture_kit`;
