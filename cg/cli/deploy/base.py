a22cc2cb456386ccff9078a561eb620d0f5d5130
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-11-26 14:11:32 +0100

    Adds command to deploy scout

--- cg/cli/deploy/base.py
+++ cg/cli/deploy/base.py
@@ -32,8 +32,17 @@ def deploy_shipping_cmd(context):
 @click.command(name="genotype")
 @click.pass_context
 def deploy_genotype_cmd(context):
-    """Deploy the shipping tool"""
+    """Deploy the genotype tool"""
     LOG.info("Deploying genotype with CG")
+    shipping_api: ShippingAPI = context.obj["shipping_api"]
+    shipping_api.deploy(app_name="genotype")
+
+
+@click.command(name="scout")
+@click.pass_context
+def deploy_scout_cmd(context):
+    """Deploy the scout tool"""
+    LOG.info("Deploying scout-browser with CG")
     shipping_api: ShippingAPI = context.obj["shipping_api"]
     shipping_api.deploy(app_name="genotype")
 
