1424dec79a335da35a8847e3728181ccc6632017
Maria Ropat <35531751+Mropat@users.noreply.github.com>
2020-11-26 10:43:56 +0100

    Fix compression query (#856)
    * change query

--- cg/store/get/cases.py
+++ cg/store/get/cases.py
@@ -11,7 +11,7 @@ def ready_for_spring_compression(store: Store, date_threshold: dt.datetime) -> Q
     """Fetch all cases that are ready to be compressed by SPRING"""
     cases = (
         store.query(models.Family)
-        .filter(models.Family.action.isnot("running"), models.Family.action.isnot("analyze"))
+        .filter(models.Family.action not in ["running", "analyze"])
         .filter(models.Family.created_at < date_threshold)
         .order_by(models.Family.created_at.asc())
     )
