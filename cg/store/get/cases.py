07c88d1efb064795533f7173f28e97ee86b38b80
Mropat <mariaropat@gmail.com>
2020-11-26 10:13:11 +0100

    change query

--- cg/store/get/cases.py
+++ cg/store/get/cases.py
@@ -11,8 +11,7 @@ def ready_for_spring_compression(store: Store, date_threshold: dt.datetime) -> Q
     """Fetch all cases that are ready to be compressed by SPRING"""
     cases = (
         store.query(models.Family)
-        .filter(models.Family.action is not "running")
-        .filter(models.Family.action is not "analyze")
+        .filter(models.Family.action not in ["running", "analyze"])
         .filter(models.Family.created_at < date_threshold)
         .order_by(models.Family.created_at.asc())
     )
