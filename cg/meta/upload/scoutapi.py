301565426789a4bfdec2aa06d9d69c77e6e59dfd
mikaell <mikael.laaksonen@scilifelab.se>
2020-12-01 14:34:30 +0100

    BLACK

--- cg/meta/upload/scoutapi.py
+++ cg/meta/upload/scoutapi.py
@@ -40,7 +40,6 @@ class UploadScoutAPI:
         self.analysis = analysis_api
         self.lims = lims_api
 
-
     def fetch_file_path(self, tag: str, sample_id: str, hk_version_id: int = None) -> Optional[str]:
         """"Fetch files from housekeeper matchin one tag string"""
         return self.fetch_file_path_from_tags([tag], sample_id, hk_version_id)
