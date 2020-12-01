8b3a780e935a9350f29889e22dba4d2552792db2
jemten <jemten@users.noreply.github.com>
2020-12-01 16:35:22 +0100

    Adds new housekeeper tags for chromograph

--- cg/constants/tags.py
+++ cg/constants/tags.py
@@ -13,12 +13,14 @@ HK_FASTQ_TAGS = ["fastq"]
 # mandatory flag
 MIP_DNA_TAGS = {
     ("chanjo_sexcheck",): {"tags": ["chanjo", "sex-check"], "is_mandatory": False},
+    ("chromograph_cov", "tcov"): {"tags": ["chromograph", "tcov"], "is_mandatory": False},
+    ("chromograp_rhoviz", "autozyg"): {"tags": ["chromograph", "autozyg"], "is_mandatory": False},
+    ("chromograp_rhoviz", "fracsnp"): {"tags": ["chromograph", "fracsnp"], "is_mandatory": False},
     ("chromograph_upd", "sites"): {"tags": ["chromograph", "upd", "sites"], "is_mandatory": False},
     ("chromograph_upd", "regions"): {
         "tags": ["chromograph", "upd", "regions"],
         "is_mandatory": False,
     },
-    ("chromograph_cov", "tcov"): {"tags": ["chromograph", "tcov"], "is_mandatory": False},
     ("endvariantannotationblock", "clinical"): {
         "tags": ["vcf-snv-clinical"],
         "index_tags": ["vcf-snv-clinical-index"],
