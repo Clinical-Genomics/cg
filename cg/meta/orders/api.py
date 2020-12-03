38e860636be0e54e717b9bc03c595a155365eafd
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2020-12-03 13:19:19 +0100

    fetch sample ids from LIMS

--- cg/meta/orders/api.py
+++ cg/meta/orders/api.py
@@ -9,6 +9,7 @@ be validated and if passing all checks be accepted as new samples.
 import datetime as dt
 import logging
 import re
+from pprint import pprint
 from typing import List
 
 import typing
@@ -110,8 +111,10 @@ class OrdersAPI(LimsHandler, StatusHandler):
     def submit_rml(self, data: dict) -> dict:
         """Submit a batch of ready made libraries."""
         status_data = self.pools_to_status(data)
-        project_data, _ = self.process_lims(data, data["samples"])
-        new_records = self.store_pools(
+        project_data, lims_map = self.process_lims(data, data["samples"])
+        samples = [sample for pool in status_data["pools"] for sample in pool["samples"]]
+        self.fill_in_sample_ids(samples, lims_map, id_key="internal_id")
+        new_records = self.store_rml(
             customer=status_data["customer"],
             order=status_data["order"],
             ordered=project_data["date"],
