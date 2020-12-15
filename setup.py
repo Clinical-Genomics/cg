d2f7e73a7dec31d6516fc2dcc1c09f29c2235f73
Måns Magnusson <mans.magnusson@scilifelab.se>
2020-12-15 16:50:39 +0100

    Bump version: 16.15.1 → 16.16.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ except FileNotFoundError:
 
 setup(
     name=NAME,
-    version="16.15.1",
+    version="16.16.0",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
