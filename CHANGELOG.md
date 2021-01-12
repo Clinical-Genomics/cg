fe30655e0c6b5b39b7cf4a0ce6f51179ae9ab04f
Henrik Stranneheim <henrik.stranneheim@scilifelab.se>
2021-01-12 15:31:11 +0100

    Fix mip rna config case (#904)
    * fix(requirements): Added typing_extensions
    
    * fix(config-case): Add args for mip rna config case
    
    * feat(docs): Clarify help text
    
    * fix(review): remove use os.link and use Path instead
    
    * fix(link): Minor change
    
    * fix(revert): Path change
    
    * Update cg/cli/workflow/mip_rna/base.py
    
    * fix(review):
    
    * feat(docs): Update Changelog
    
    Co-authored-by: Måns Magnusson <mans.magnusson@scilifelab.se>

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -15,13 +15,19 @@ Try to use the following format:
 ### Fixed
 
 
+## [18.1.2]
+
+### Fixed
+
+- cg workflow mip-rna config-case command
+ 
 ## [18.1.1]
 
 ### Fixed
 
 - Updates balsamic deliver tags to align with the ones specified in hermes
 
-## 18.1.0
+## [18.1.0]
 
 ### Added
 - Customer in the ticket created for an order from the Orderportal
--- tests/cli/workflow/mip/test_cli_mip_dna_run.py
+++ tests/cli/workflow/mip/test_cli_mip_dna_run.py
@@ -5,26 +5,27 @@ from pathlib import Path
 
 from cg.cli.workflow.mip_dna.base import run
 
-CASE_ID = "yellowhog"
-CONDA_ENV = "S_mip_rd-dna"
-EMAIL = "james.holden@scilifelab.se"
 
-
-def test_cg_dry_run(cli_runner, mip_context, caplog):
+def test_cg_dry_run(cli_runner, mip_context, caplog, case_id, email_adress):
     """Test print the MIP run to console"""
-    pedigree_path = mip_context.get("dna_api").get_pedigree_config_path(case_id="yellowhog")
+
+    caplog.set_level(logging.INFO)
+
+    pedigree_path = mip_context.get("dna_api").get_pedigree_config_path(case_id=case_id)
     Path.mkdir(pedigree_path.parent, parents=True, exist_ok=True)
     os.link("tests/fixtures/apps/mip/dna/store/pedigree.yaml", pedigree_path)
 
     # GIVEN a cli function
-    with caplog.at_level(logging.INFO):
-        # WHEN we run a case in dry run mode
-        result = cli_runner.invoke(run, ["--dry-run", "--email", EMAIL, CASE_ID], obj=mip_context)
+    # WHEN we run a case in dry run mode
+    result = cli_runner.invoke(
+        run, ["--dry-run", "--email", email_adress, case_id], obj=mip_context
+    )
 
-        # THEN the command should be printed
-        assert (
-            "analyse rd_dna yellowhog --config config.yaml "
-            "--slurm_quality_of_service normal --email james.holden@scilifelab.se" in caplog.text
-        )
-        # THEN command is run successfully
-        assert result.exit_code == 0
+    # THEN the command should be printed
+    assert (
+        f"analyse rd_dna {case_id} --config config.yaml --slurm_quality_of_service normal --email {email_adress}"
+        in caplog.text
+    )
+
+    # THEN command is run successfully
+    assert result.exit_code == 0
