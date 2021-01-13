b390992fbc359b5f9b510cd5a0a798138b7e3a4f
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2021-01-13 10:44:22 +0100

    feat(tests): Add rna_mip_context and dna_mip_context
    - Add link test for dna and rna
    - Add new mip fixtures
    - Refactor tests

--- tests/cli/workflow/mip/test_cli_mip_dna_config_case.py
+++ tests/cli/workflow/mip/test_cli_mip_dna_config_case.py
@@ -4,7 +4,7 @@ import logging
 from cg.cli.workflow.mip_dna.base import config_case
 
 
-def test_cg_workflow_mip_dna_config_case_dry_run(cli_runner, caplog, case_id, mip_context):
+def test_cg_workflow_mip_dna_config_case_dry_run(cli_runner, caplog, case_id, dna_mip_context):
     """Test print the MIP dna case config command to console"""
 
     caplog.set_level(logging.INFO)
@@ -12,14 +12,13 @@ def test_cg_workflow_mip_dna_config_case_dry_run(cli_runner, caplog, case_id, mi
     # GIVEN a cli function
 
     # WHEN we run a case in dry run mode
-    result = cli_runner.invoke(config_case, ["--dry-run", case_id], obj=mip_context)
+    result = cli_runner.invoke(config_case, ["--dry-run", case_id], obj=dna_mip_context)
 
     # THEN the command should be printed
     assert result.exit_code == 0
-    assert f"config-case --dry {case_id}"
 
 
-def test_cg_workflow_mip_dna_config_case(cli_runner, caplog, case_id, mip_context):
+def test_cg_workflow_mip_dna_config_case(cli_runner, caplog, case_id, dna_mip_context):
     """Test print the MIP dna case config command to file"""
 
     caplog.set_level(logging.INFO)
@@ -27,8 +26,8 @@ def test_cg_workflow_mip_dna_config_case(cli_runner, caplog, case_id, mip_contex
     # GIVEN a cli function
 
     # WHEN we run a case in dry run mode
-    result = cli_runner.invoke(config_case, [case_id], obj=mip_context)
+    result = cli_runner.invoke(config_case, [case_id], obj=dna_mip_context)
 
     # THEN the command should be printed
     assert result.exit_code == 0
-    assert f"config-case --dry {case_id}"
+    assert f"Config file saved to" in caplog.text
