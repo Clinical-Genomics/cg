5e020459637ca5aa140253e3320a99e491446603
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-01-19 08:51:28 +0100

    skip cases with analyses and processed samples

--- cg/cli/delete/case.py
+++ cg/cli/delete/case.py
@@ -2,26 +2,11 @@
 
 import logging
 import click
+from cg.cli.get import family as print_case
 
 from cg.store import Store
 
 LOG = logging.getLogger(__name__)
-
-
-def _delete_analyses(case_obj, context, dry_run, status_db, yes):
-    """Delete all analyses of a case"""
-    for case_analysis in case_obj.analyses:
-        if not (
-            yes
-            or click.confirm(
-                f"Do you want to delete {case_analysis.pipeline} analysis of {case_analysis}?"
-            )
-        ):
-            context.abort()
-        else:
-            LOG.info("Deleting analysis: %s", case_analysis)
-            if not dry_run:
-                status_db.delete_commit(case_analysis)
 
 
 @click.command()
@@ -38,11 +23,20 @@ def case(context, case_id: str, dry_run: bool, yes: bool):
     status_db: Store = context.obj["status_db"]
     case_obj = status_db.family(case_id)
     if not case_obj:
-        LOG.warning("Could not find case %s", case_id)
+        LOG.error("Could not find case %s", case_id)
+        context.abort()
+
+    if case_obj.analyses:
+        LOG.error("Can't delete case with analyses %s", case_obj.analyses)
+        context.abort()
+
+    context.invoke(print_case, family_ids=[case_id])
+
+    if case_obj.links and not (yes or click.confirm(f"Case {case_id} has links: "
+                                                    f"{case_obj.links}, Continue?")):
         context.abort()
 
     _delete_links_and_samples(case_obj, context, dry_run, status_db, yes)
-    _delete_analyses(case_obj, context, dry_run, status_db, yes)
 
     if not (yes or click.confirm(f"Do you want to delete case: {case_obj}?")):
         context.abort()
@@ -55,13 +49,12 @@ def case(context, case_id: str, dry_run: bool, yes: bool):
 
 def _delete_links_and_samples(case_obj, context, dry_run, status_db, yes):
     """Delete all links from a case to samples"""
-    case_id = case_obj.internal_id
     for case_link in case_obj.links:
         sample = case_link.sample
         if not (
             yes
             or click.confirm(
-                f"Do you want to delete link {case_link}?"
+                f"Do you want to delete link: {case_link}?"
             )
         ):
             context.abort()
@@ -74,6 +67,16 @@ def _delete_links_and_samples(case_obj, context, dry_run, status_db, yes):
 
 
 def _delete_sample(dry_run, sample, status_db, yes):
+
+    if sample.received_at or sample.prepared_at or sample.sequenced_at or sample.delivered_at or \
+            sample.invoice_id:
+        LOG.info("Can't delete processed sample: %s", sample.internal_id)
+        LOG.info("sample was received: %s", sample.received_at)
+        LOG.info("sample was prepared: %s", sample.prepared_at)
+        LOG.info("sample was sequenced: %s", sample.sequenced_at)
+        LOG.info("sample was delivered: %s", sample.delivered_at)
+        LOG.info("sample has invoice: %s", sample.invoice_id)
+        return
 
     if not sample.links and not sample.father_links and not sample.mother_links:
 
