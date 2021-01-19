d3b3e352f03872792b8a5c4e94ad8afefcd89df3
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-01-19 09:54:13 +0100

    add command to delete multiple cases

--- cg/cli/delete/base.py
+++ cg/cli/delete/base.py
@@ -1,15 +1,11 @@
 """CLI for deleting records in statusDB """
 
 import logging
-from pathlib import Path
-from typing import List
-
 import click
 
-from cg.apps.housekeeper.hk import HousekeeperAPI
 from cg.cli.delete.case import case
+from cg.cli.delete.cases import cases
 from cg.store import Store
-from cg.store.models import Family
 
 LOG = logging.getLogger(__name__)
 
@@ -20,7 +16,7 @@ def delete(context):
     """delete database records in CG."""
     LOG.info("Running CG delete")
     context.obj["status_db"] = Store(context.obj["database"])
-    context.obj["housekeeper_api"] = HousekeeperAPI(context.obj)
 
 
 delete.add_command(case)
+delete.add_command(cases)
--- tests/cli/delete/test_cases.py
+++ tests/cli/delete/test_cases.py
@@ -1,19 +1,19 @@
-"""Test methods for cg/cli/set/families"""
+"""Test methods for cg/cli/delete/cases"""
 import logging
 
 import pytest
 
-from cg.cli.set.families import families
+from cg.cli.delete.cases import cases
 from cg.store import Store
 
 SUCCESS = 0
 
 
 @pytest.mark.parametrize("identifier_key", ["ticket_number", "order"])
-def test_set_families_by_sample_identifiers(
+def test_set_cases_by_sample_identifiers(
     cli_runner, base_context, base_store: Store, identifier_key, helpers, caplog
 ):
-    # GIVEN a database with a family with a sample
+    # GIVEN a database with a case with a sample
     sample_obj = helpers.add_sample(base_store)
     sample_obj.ticket_number = 123456
     sample_obj.order = "An order"
@@ -23,13 +23,13 @@ def test_set_families_by_sample_identifiers(
 
     caplog.set_level(logging.INFO)
 
-    # WHEN calling set families with valid sample identifiers
+    # WHEN calling delete cases with valid sample identifiers
     result = cli_runner.invoke(
-        families,
+        cases,
         ["--sample-identifier", identifier_key, identifier_value],
         obj=base_context,
     )
 
-    # THEN it should name the family to be changed
+    # THEN it should name the case to be changed
     assert case.internal_id in caplog.text
     assert case.name in caplog.text
