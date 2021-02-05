21e4d47a71a9b13690347805ef3aafe7b0372e8e
Mropat <mariaropat@gmail.com>
2021-02-05 14:44:53 +0100

    Bump version: 18.10.2 → 18.11.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ except FileNotFoundError:
 
 setup(
     name=NAME,
-    version="18.10.2",
+    version="18.11.0",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
