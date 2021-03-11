5d82d0e0884df6228dba04f686214581f23e4e95
Henrik Stranneheim <henrik.stranneheim@scilifelab.se>
2021-03-11 11:55:08 +0100

    Bump version: 20.6.0 → 20.7.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ except FileNotFoundError:
 
 setup(
     name=NAME,
-    version="20.6.0",
+    version="20.7.0",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
