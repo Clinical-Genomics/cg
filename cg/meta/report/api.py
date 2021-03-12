4a558601c73014958451b4a63f647bf364db0bad
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-03-12 18:15:31 +0100

    add missing return hint

--- cg/meta/report/api.py
+++ cg/meta/report/api.py
@@ -112,7 +112,7 @@ class ReportAPI:
         return None
 
     @staticmethod
-    def get_delivery_report_from_hk(hk_api: HousekeeperAPI, case_id: str) -> :
+    def get_delivery_report_from_hk(hk_api: HousekeeperAPI, case_id: str) -> str:
         delivery_report_tag_name = "delivery-report"
         version_obj = hk_api.last_version(case_id)
         uploaded_delivery_report_files = hk_api.get_files(
