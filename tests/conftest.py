2fe03699f8a6695626d1a4caf39e66846b335436
Mropat <mariaropat@gmail.com>
2021-03-16 08:46:51 +0100

    update tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1177,10 +1177,10 @@ def context_config(
         "loqusdb-wes": {"binary_path": "loqusdb", "config_path": "loqusdb-wes-stage.yaml"},
         "balsamic": {
             "root": balsamic_dir,
-            "singularity": "BALSAMIC_release_v6.0.1.sif",
             "reference_config": "reference.json",
             "binary_path": "echo",
             "conda_env": "S_BALSAMIC",
+            "balsamic_cache": "hello",
             "slurm": {
                 "mail_user": "test.email@scilifelab.se",
                 "account": "development",
