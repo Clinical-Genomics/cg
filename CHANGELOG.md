90fb84868376e11f473ffc07079daf106576395f
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-03-17 16:24:33 +0100

    Bump version: 20.9.8 → 20.9.9

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -13,7 +13,7 @@ Try to use the following format:
 ### Changed
 ### Fixed
 
-## [NG.NG.NG]
+## [20.9.9]
 ### Fixed
 - Fixed bug in upload delivery report automation
 
--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ except FileNotFoundError:
 
 setup(
     name=NAME,
-    version="20.9.8",
+    version="20.9.9",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
