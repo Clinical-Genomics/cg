62d6102faa6b700f9b234ea52b37180d1c72eb67
Mropat <mariaropat@gmail.com>
2021-03-17 07:54:27 +0100

    commit all

--- cg/models/workflow/mutant.py
+++ cg/models/workflow/mutant.py
@@ -1,17 +1,20 @@
 from pydantic import BaseModel
-import datetime as dt
 
 
 class MutantSampleConfig(BaseModel):
     cg_project_id: str = "MIC3109"  # Is this ticket number or case_id?
     cg_sample_id: str = "MIC3109A1"
+    case_id: str = "yellowhog"
+    region_code: str = "01"
+    lab_code: str = "SE100"
+    priority: str = "normal"
     customer_project_id: str = "AAA0000B1"  # Is this customers case name or ticket id?
     customer_sample_id: str = "MG-001"
     customer_id: str = "cust000"
     application_tag: str = "MWGNXTR003"
-    date_arrival: dt.datetime = "0001-01-01 00:00:00"
-    date_libprep: dt.datetime = "0001-01-01 00:00:00"
-    date_sequencing: dt.datetime = "0001-01-01 00:00:00"
+    date_arrival: str = "0001-01-01 00:00:00"
+    date_libprep: str = "0001-01-01 00:00:00"
+    date_sequencing: str = "0001-01-01 00:00:00"
     method_libprep: str = "1764:2"
     method_sequencing: str = "1036:15"
     cg_qc_pass: bool = True
