a4a4439c8322ada957bfa8ce5b2b500f5213f428
Mropat <mariaropat@gmail.com>
2021-03-23 13:19:53 +0100

    edit the fields

--- cg/meta/workflow/mutant.py
+++ cg/meta/workflow/mutant.py
@@ -63,13 +63,13 @@ class MutantAnalysisAPI(AnalysisAPI):
 
     def get_sample_parameters(self, sample_obj: models.Sample) -> MutantSampleConfig:
         return MutantSampleConfig(
-            cg_sample_id=sample_obj.internal_id,
-            case_id=sample_obj.links[0].family.internal_id,
-            customer_sample_id=sample_obj.name,
+            CG_ID_sample=sample_obj.internal_id,
+            case_ID=sample_obj.links[0].family.internal_id,
+            Customer_ID_sample=sample_obj.name,
             customer_id=sample_obj.customer.internal_id,
-            cg_qc_pass=sample_obj.reads > 0,
-            cg_project_id=sample_obj.links[0].family.name,
-            customer_project_id=sample_obj.ticket_number,
+            sequencing_qc_pass=sample_obj.reads > 0,
+            CG_ID_project=sample_obj.links[0].family.name,
+            Customer_ID_project=sample_obj.ticket_number,
             application_tag=sample_obj.application_version.application.tag,
             method_libprep=self.lims_api.get_sample_attribute(
                 lims_id=sample_obj.internal_id, key="pre_processing_method"
--- cg/models/workflow/mutant.py
+++ cg/models/workflow/mutant.py
@@ -2,21 +2,20 @@ from pydantic import BaseModel
 
 
 class MutantSampleConfig(BaseModel):
-    cg_project_id: str = "MIC3109"  # Is this ticket number or case_id?
-    cg_sample_id: str = "MIC3109A1"
-    case_id: str = "yellowhog"
-    region_code: str = "01"
-    lab_code: str = "SE100"
-    priority: str = "normal"
-    customer_project_id: int  # Is this customers case name or ticket id?
-    customer_sample_id: str = "MG-001"
-    customer_id: str = "cust000"
-    application_tag: str = "MWGNXTR003"
-    date_arrival: str = "0001-01-01 00:00:00"
-    date_libprep: str = "0001-01-01 00:00:00"
-    date_sequencing: str = "0001-01-01 00:00:00"
-    method_libprep: str = "1764:2"
-    method_sequencing: str = "1036:15"
-    cg_qc_pass: bool = True
-    selection_criteria: str = "zombie"
-    # please add more fields which wrapper of the  custs might want
+    CG_ID_project: str
+    CG_ID_sample: str
+    case_ID: str
+    region_code: str
+    lab_code: str
+    priority: str
+    Customer_ID_project: int
+    Customer_ID_sample: str
+    customer_id: str
+    application_tag: str
+    date_arrival: str
+    date_libprep: str
+    date_sequencing: str
+    method_libprep: str
+    method_sequencing: str
+    sequencing_qc_pass: bool
+    selection_criteria: str
