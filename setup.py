a852dad6fbffb5f9f2cc56399be7c0f4ca74b35e
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-03-25 12:11:34 +0100

    Bump version: 20.12.1 → 20.13.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ except FileNotFoundError:
 
 setup(
     name=NAME,
-    version="20.12.1",
+    version="20.13.0",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
