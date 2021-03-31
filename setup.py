4c51c79db3e37abc9f7e1f050dc67b42432739f8
Mropat <mariaropat@gmail.com>
2021-03-31 11:05:22 +0200

    Bump version: 20.14.2 → 20.15.0

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ except FileNotFoundError:
 
 setup(
     name=NAME,
-    version="20.14.2",
+    version="20.15.0",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
