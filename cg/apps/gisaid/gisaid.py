046f5a15f64f8d2a28b23609a1fb1737e4200e33
mayabrandi <maya.brandi@scilifelab.se>
2021-04-07 14:26:14 +0200

    gisaid load command

--- cg/apps/gisaid/gisaid.py
+++ cg/apps/gisaid/gisaid.py
@@ -6,6 +6,7 @@ from typing import List
 
 from cg.store import models
 from .constants import HEADERS
+from ...utils import Process
 
 LOG = logging.getLogger(__name__)
 
@@ -15,9 +16,12 @@ import csv
 class GisaidAPI:
     """Interface with Gisaid cli uppload"""
 
-    def __init__(self, case_obj: models.Family):
+    def __init__(self, case_obj: models.Family, config:dict):
         self.case_obj = case_obj
         self.dry_run = False
+        self.gisaid_config = config["gisaid"]["config_path"]
+        self.gisaid_binary = config["gisaid"]["binary_path"]
+        self.process = Process(binary=self.vogue_binary, config=self.vogue_config)
 
     def set_dry_run(self, dry_run: bool) -> None:
         """Set the dry run state"""
@@ -40,8 +44,15 @@ class GisaidAPI:
                     wr.writerow(sample_row)
         return file
 
-    def uppload(self):
-        return
+    def uppload(self, csv_file_path: str, fasta_file_path: str) -> None:
+        """Load genotype data from a dict."""
+
+        load_call = ["CoV", "upload", "--csv",csv_file_path,"--fasta", fasta_file_path]
+        self.process.run_command(parameters=load_call)
+
+        # Execute command and print its stdout+stderr as it executes
+        for line in self.process.stderr_lines():
+            LOG.info("gisaid output: %s", line)
 
     def __str__(self):
         return f"GisaidAPI(dry_run: {self.dry_run})"
