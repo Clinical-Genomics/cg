198ab703fb83e9fd9b1b5707229ab1556c612190
moonso <mans.magnusson@scilifelab.se>
2021-04-10 14:33:15 +0200

    Adds some type hints to store commands

--- cg/cli/store/fastq.py
+++ cg/cli/store/fastq.py
@@ -1,9 +1,11 @@
 import logging
+from typing import Iterable, List
 
 import click
-
 from cg.cli.compress.helpers import get_fastq_individuals, update_compress_api
 from cg.exc import CaseNotFoundError
+from cg.meta.compress import CompressAPI
+from cg.store import Store, models
 
 LOG = logging.getLogger(__name__)
 
@@ -12,13 +14,13 @@ LOG = logging.getLogger(__name__)
 @click.argument("sample-id", type=str)
 @click.option("-d", "--dry-run", is_flag=True)
 @click.pass_context
-def store_sample(context, sample_id, dry_run):
+def store_sample(context: click.Context, sample_id: str, dry_run: bool):
     """Include links to decompressed FASTQ files belonging to this sample in housekeeper"""
-    compress_api = context.obj["compress_api"]
+    compress_api: CompressAPI = context.obj["compress_api"]
     update_compress_api(compress_api, dry_run=dry_run)
 
-    was_decompressed = compress_api.add_decompressed_fastq(sample_id)
-    if was_decompressed is False:
+    was_decompressed: bool = compress_api.add_decompressed_fastq(sample_id)
+    if not was_decompressed:
         LOG.info(f"Skipping sample {sample_id}")
         return 0
     LOG.info(f"Stored fastq files for {sample_id}")
@@ -32,16 +34,16 @@ def store_sample(context, sample_id, dry_run):
 def store_case(context, case_id, dry_run):
     """Include links to decompressed FASTQ files belonging to this case in housekeeper"""
 
-    status_db = context.obj["status_db"]
+    status_db: Store = context.obj["status_db"]
     try:
-        samples = get_fastq_individuals(status_db, case_id)
-        stored_inds = 0
+        samples: Iterable[str] = get_fastq_individuals(status_db, case_id)
+        stored_individuals = 0
         for sample_id in samples:
-            stored_count = context.invoke(store_sample, sample_id=sample_id, dry_run=dry_run)
-            stored_inds += stored_count
+            stored_count: int = context.invoke(store_sample, sample_id=sample_id, dry_run=dry_run)
+            stored_individuals += stored_count
     except CaseNotFoundError:
         return
-    LOG.info(f"Stored fastq files for {stored_inds} samples")
+    LOG.info(f"Stored fastq files for {stored_individuals} samples")
 
 
 @click.command("flowcell")
@@ -51,13 +53,15 @@ def store_case(context, case_id, dry_run):
 def store_flowcell(context, flowcell_id, dry_run):
     """Include links to decompressed FASTQ files belonging to this flowcell in housekeeper"""
 
-    status_db = context.obj["status_db"]
-    samples = status_db.get_samples_from_flowcell(flowcell_id=flowcell_id)
-    stored_inds = 0
+    status_db: Store = context.obj["status_db"]
+    samples: List[models.Sample] = status_db.get_samples_from_flowcell(flowcell_id=flowcell_id)
+    stored_individuals = 0
     for sample in samples:
-        stored_count = context.invoke(store_sample, sample_id=sample.internal_id, dry_run=dry_run)
-        stored_inds += stored_count
-    LOG.info(f"Stored fastq files for {stored_inds} samples")
+        stored_count: int = context.invoke(
+            store_sample, sample_id=sample.internal_id, dry_run=dry_run
+        )
+        stored_individuals += stored_count
+    LOG.info(f"Stored fastq files for {stored_individuals} samples")
 
 
 @click.command("ticket")
@@ -66,10 +70,12 @@ def store_flowcell(context, flowcell_id, dry_run):
 @click.pass_context
 def store_ticket(context, ticket_id, dry_run):
     """Include links to decompressed FASTQ files belonging to this ticket in housekeeper"""
-    status_db = context.obj["status_db"]
-    samples = status_db.get_samples_from_ticket(ticket_id=ticket_id)
-    stored_inds = 0
+    status_db: Store = context.obj["status_db"]
+    samples: List[models.Sample] = status_db.get_samples_from_ticket(ticket_id=ticket_id)
+    stored_individuals = 0
     for sample in samples:
-        stored_count = context.invoke(store_sample, sample_id=sample.internal_id, dry_run=dry_run)
-        stored_inds += stored_count
-    LOG.info(f"Stored fastq files for {stored_inds} samples")
+        stored_count: int = context.invoke(
+            store_sample, sample_id=sample.internal_id, dry_run=dry_run
+        )
+        stored_individuals += stored_count
+    LOG.info(f"Stored fastq files for {stored_individuals} samples")
--- cg/cli/store/store.py
+++ cg/cli/store/store.py
@@ -3,7 +3,6 @@
 import logging
 
 import click
-
 from cg.apps.crunchy.crunchy import CrunchyAPI
 from cg.apps.housekeeper.hk import HousekeeperAPI
 from cg.meta.compress.compress import CompressAPI
@@ -16,15 +15,14 @@ LOG = logging.getLogger(__name__)
 
 @click.group()
 @click.pass_context
-def store(context):
+def store(context: click.Context):
     """Command for storing files"""
     LOG.info("Running CG store command")
-    housekeeper_api = HousekeeperAPI(context.obj)
-    crunchy_api = CrunchyAPI(context.obj)
+    housekeeper_api = context.obj["housekeeper_api"]
+    crunchy_api = context.obj["meta_api"].crunchy_api
 
     compress_api = CompressAPI(hk_api=housekeeper_api, crunchy_api=crunchy_api)
     context.obj["compress_api"] = compress_api
-    context.obj["status_db"] = Store(context.obj.get("database"))
 
 
 store.add_command(store_sample)
