28a8c21092968dde8cfe3a28a5b35d037b12ae53
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-04-12 09:19:45 +0200

    refactor

--- cg/apps/avatar/api.py
+++ cg/apps/avatar/api.py
@@ -5,10 +5,26 @@ import petname
 from flask_admin.form import thumbgen_filename
 from simple_image_download import simple_image_download as simp
 
+RANDOMIZING_WORDS = ['cute', 'cuddly', 'small', 'pet']
+
 response = simp.simple_image_download
 
 
 class Avatar:
+    @staticmethod
+    def get_avatar_url(internal_id):
+
+        adjective, animal = Avatar._split_petname(internal_id)
+        seed = random.choice(RANDOMIZING_WORDS)
+        urls = response().urls(keywords=f"{seed} {adjective} {animal} animal", limit=25, extensions={'.gif', '.jpg', '.jpeg', '.png', '.tiff'})
+        random.shuffle(urls)
+        for url in urls:
+            thumb_url = thumbgen_filename(url)
+            if Avatar._is_url_image(thumb_url):
+                return thumb_url
+
+        return None
+
     @staticmethod
     def _is_url_image(image_url):
         from PIL import Image
@@ -22,26 +38,8 @@ class Avatar:
             pass
         return False
 
-    @staticmethod
-    def get_avatar_url(internal_id):
-
-        adjective, animal = Avatar.split_pet(internal_id)
-
-        seeds = ['cute', 'cuddly', 'small', 'pet']
-        random_index = random.randint(0, len(seeds) - 1)
-        seed = seeds[random_index]
-
-        urls = response().urls(keywords=f"{seed} {adjective} {animal} animal", limit=25, extensions={'.gif', '.jpg', '.jpeg', '.png', '.tiff'})
-        random.shuffle(urls)
-        for url in urls:
-            thumb_url = thumbgen_filename(url)
-            if Avatar._is_url_image(thumb_url):
-                return thumb_url
-
-        return None
-
     @classmethod
-    def split_pet(cls, merged_name):
+    def _split_petname(cls, merged_name):
 
         found_adj = ""
         for adjective in petname.adjectives:
