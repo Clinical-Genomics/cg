b9ba40b0bfce350c3a1d52a747d0ff61b24644a4
mayabrandi <maya.brandi@scilifelab.se>
2021-04-12 12:02:22 +0200

    Update cg/apps/gisaid/gisaid.py
    Co-authored-by: Måns Magnusson <mans.magnusson@scilifelab.se>

--- cg/apps/gisaid/gisaid.py
+++ cg/apps/gisaid/gisaid.py
@@ -5,7 +5,7 @@ from pathlib import Path
 from typing import List
 
 from cg.store import models
-from ...meta.workflow.fastq import FastqHandler
+from cg.meta.workflow.fastq import FastqHandler
 from ...utils import Process
 from .models import GisaidSample, UpploadFiles
 
