b9c01922d74838f47b5fcac4e74a15815bd4f9de
mayabrandi <maya.brandi@scilifelab.se>
2021-04-12 12:45:43 +0200

    mmissing typehint

--- cg/store/api/findbusinessdata.py
+++ cg/store/api/findbusinessdata.py
@@ -135,7 +135,7 @@ class FindBusinessDataHandler(BaseHandler):
     def get_samples_by_family_id(self, family_id: str) -> List[models.Sample]:
         """Get samples on a given family_id"""
 
-        case = self.family(internal_id=family_id)
+        case: models.Family = self.family(internal_id=family_id)
         return [link.sample for link in case.links] if case else []
 
     def get_sequenced_samples(self, family_id: str) -> List[models.Sample]:
