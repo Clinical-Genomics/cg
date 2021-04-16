d05e5da538a5bb12ae6f116cbeabce004083d354
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-04-16 15:27:34 +0200

    refactor

--- cg/cli/clean.py
+++ cg/cli/clean.py
@@ -37,23 +37,35 @@ def hk_alignment_files(context: CGConfig, bundle: str, yes: bool = False, dry_ru
     housekeeper_api: HousekeeperAPI = context.housekeeper_api
     for tag in ["bam", "bai", "bam-index", "cram", "crai", "cram-index"]:
 
-        for file_obj in set(housekeeper_api.get_files(bundle=bundle, tags=[tag])):
-            if file_obj.is_included:
-                question = (
-                    f"{bundle}: remove file from file system and database: {file_obj.full_path}"
-                )
-            else:
-                question = f"{bundle}: remove file from database: {file_obj.full_path}"
+        tag_files = set(housekeeper_api.get_files(bundle=bundle, tags=[tag]))
 
-            if yes or click.confirm(question):
-                file_path: Path = Path(file_obj.full_path)
-                if file_obj.is_included and file_path.exists() and not dry_run:
+        if not tag_files:
+            LOG.warning("Could not find any files ready for cleaning for bundle %s and tag %s", bundle, tag)
+
+        for file_obj in tag_files:
+            if not (yes or click.confirm(_get_confirm_question(bundle, file_obj))):
+                continue
+
+            file_path: Path = Path(file_obj.full_path)
+            if file_obj.is_included and file_path.exists():
+                LOG.info("Unlinking %s", file_path)
+                if not dry_run:
                     file_path.unlink()
 
-                if not dry_run:
-                    file_obj.delete()
-                    housekeeper_api.commit()
-                click.echo(f"{file_path} deleted")
+            LOG.info("Deleting %s from database", file_path)
+            if not dry_run:
+                file_obj.delete()
+                housekeeper_api.commit()
+
+
+def _get_confirm_question(bundle, file_obj):
+    if file_obj.is_included:
+        question = (
+            f"{bundle}: remove file from file system and database: {file_obj.full_path}"
+        )
+    else:
+        question = f"{bundle}: remove file from database: {file_obj.full_path}"
+    return question
 
 
 @clean.command("scout-finished-cases")
