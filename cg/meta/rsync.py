a1e35570cef7aecaad91854145d793a070de46e7
Maria Ropat <35531751+Mropat@users.noreply.github.com>
2021-04-19 12:02:24 +0200

    Fix subprocess wildcard for deliver covid(#1116)

--- cg/meta/rsync.py
+++ cg/meta/rsync.py
@@ -1,4 +1,5 @@
 """Module for building the rsync command to send files to customer inbox on caesar"""
+import glob
 import logging
 from pathlib import Path
 from typing import List
@@ -17,11 +18,10 @@ LOG = logging.getLogger(__name__)
 class RsyncAPI(MetaAPI):
     def __init__(self, config: CGConfig):
         super().__init__(config)
-        self.config = config
-        self.delivery_path = config.delivery_path
-        self.destination_path = config.data_delivery.destination_path
-        self.covid_destination_path = config.data_delivery.covid_destination_path
-        self.covid_report_path = config.data_delivery.covid_report_path
+        self.delivery_path: str = config.delivery_path
+        self.destination_path: str = config.data_delivery.destination_path
+        self.covid_destination_path: str = config.data_delivery.covid_destination_path
+        self.covid_report_path: str = config.data_delivery.covid_report_path
         self._process = None
 
     @property
@@ -38,17 +38,26 @@ class RsyncAPI(MetaAPI):
             raise CgError()
 
         customer_id: str = cases[0].customer.internal_id
-        rsync_destination_path = self.destination_path % (customer_id, ticket_id)
-        delivery_source_path = (
+        rsync_destination_path: str = self.destination_path % (customer_id, ticket_id)
+        delivery_source_path: str = (
             Path(self.delivery_path, customer_id, "inbox", str(ticket_id)).as_posix() + "/"
         )
         rsync_options = "-rvL"
-        parameters = [rsync_options, delivery_source_path, rsync_destination_path]
+        parameters: List[str] = [rsync_options, delivery_source_path, rsync_destination_path]
         self.process.run_command(parameters=parameters, dry_run=dry_run)
 
         if cases[0].data_analysis == Pipeline.SARS_COV_2:
             LOG.info("Delivering report for SARS-COV-2 analysis")
-            covid_report_path = self.covid_report_path % (str(cases[0].internal_id), ticket_id)
-            covid_destination_path = self.covid_destination_path % customer_id
-            parameters = [rsync_options, covid_report_path, covid_destination_path]
+            covid_report_options: List[str] = glob.glob(
+                self.covid_report_path % (str(cases[0].internal_id), ticket_id)
+            )
+            if not covid_report_options:
+                LOG.error(
+                    f"No report file could be found with path"
+                    f" {self.covid_report_path % (str(cases[0].internal_id), ticket_id)}!"
+                )
+                return
+            covid_report_path: str = covid_report_options[0]
+            covid_destination_path: str = self.covid_destination_path % customer_id
+            parameters: List[str] = [rsync_options, covid_report_path, covid_destination_path]
             self.process.run_command(parameters=parameters, dry_run=dry_run)
