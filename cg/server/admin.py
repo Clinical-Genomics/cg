41f7624d106677720617cb41fec194e33d262a27
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-04-20 14:42:00 +0200

    format with black

--- cg/server/admin.py
+++ cg/server/admin.py
@@ -369,9 +369,9 @@ class UserView(BaseView):
     """Admin view for Model.User"""
 
     column_default_sort = "name"
-    column_filters = ['customers']
+    column_filters = ["customers"]
     column_hide_backrefs = False
-    column_list = ('name', 'is_admin', 'email', 'customers')
+    column_list = ("name", "is_admin", "email", "customers")
     column_searchable_list = ["name", "email"]
     create_modal = True
     edit_modal = True
