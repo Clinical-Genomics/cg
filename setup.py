45f6e0a76b1e5bfdbfa509d2a8349872cb392ee5
moonso <mans.magnusson@scilifelab.se>
2021-05-19 08:55:26 +0200

    Bump version: 21.6.8 → 21.6.9

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ except FileNotFoundError:
 
 setup(
     name=NAME,
-    version="21.6.8",
+    version="21.6.9",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
