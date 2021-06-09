dada82c119053f9ab74fd5b9cace1ded0885f2eb
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-06-09 11:37:47 +0200

    remove colorclass

--- cg/cli/status.py
+++ cg/cli/status.py
@@ -4,7 +4,6 @@ import click
 from cg.constants import CASE_ACTIONS, PRIORITY_OPTIONS, Pipeline
 from cg.models.cg_config import CGConfig
 from cg.store import Store, models
-from colorclass import Color
 from tabulate import tabulate
 
 STATUS_OPTIONS = ["pending", "running", "completed", "failed", "error"]
@@ -215,33 +214,17 @@ def cases(
         max_tat = case.get("max_tat")
 
         if (
-            case.get("samples_received_bool")
-            and case.get("samples_delivered_bool")
-            and tat_number <= max_tat
-        ):
-            tat_color = "green"
-        elif tat_number == max_tat:
-            tat_color = "yellow"
-        elif tat_number > max_tat:
-            tat_color = "red"
-        else:
-            tat_color = "white"
-
-        color_start = Color("{" + f"{tat_color}" + "}")
-        color_end = Color("{/" + f"{tat_color}" + "}")
-
-        if (
             not case.get("case_external_bool")
             and case.get("samples_received_bool")
             and case.get("samples_delivered_bool")
         ):
-            tat = f"{tat_number}/{max_tat}" + color_end
+            tat = f"{tat_number}/{max_tat}"
         elif case.get("case_external_bool") and case.get("analysis_uploaded_bool"):
-            tat = f"{tat_number}/{max_tat}" + color_end
+            tat = f"{tat_number}/{max_tat}"
         else:
-            tat = f"({tat_number})/{max_tat}" + color_end
+            tat = f"({tat_number})/{max_tat}"
 
-        title = color_start + f"{case.get('internal_id')}"
+        title = f"{case.get('internal_id')}"
 
         if name:
             title = f"{title} ({case.get('name')})"
