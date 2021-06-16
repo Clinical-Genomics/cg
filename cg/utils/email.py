c2cc7efd325f2c9827072238382098e9645e74ca
mayabrandi <maya.brandi@scilifelab.se>
2021-06-16 12:20:46 +0200

    debug

--- cg/utils/email.py
+++ cg/utils/email.py
@@ -9,13 +9,13 @@ from email.mime.multipart import MIMEMultipart
 def send_mail(email_info: EmailInfo):
     """Email handler. Sending new user request email to admin"""
 
-    try:
-        with SMTP(email_info.smtp_server) as server:
-            msg = MIMEMultipart()
-            msg["From"] = email_info.sender_email
-            msg["To"] = email_info.receiver_email
-            msg["Subject"] = email_info.subject
-            msg.attach(MIMEText(email_info.message, "plain"))
-            server.sendmail(email_info.sender_email, email_info.receiver_email, msg.as_string())
-    except:
-        raise EmailNotSentError(message=f"Email could not be sent")
+    print(email_info.json())
+    with SMTP(email_info.smtp_server) as server:
+        msg = MIMEMultipart()
+        msg["From"] = email_info.sender_email
+        msg["To"] = email_info.receiver_email
+        msg["Subject"] = email_info.subject
+        msg.attach(MIMEText(email_info.message, "plain"))
+        server.sendmail(email_info.sender_email, email_info.receiver_email, msg.as_string())
+    # except:
+    #   raise EmailNotSentError(message=f"Email could not be sent")
