27b306cf26d721e45c8349a87c662c67946b3166
mayabrandi <maya.brandi@scilifelab.se>
2021-06-16 11:27:56 +0200

    testing without auth

--- cg/utils/email.py
+++ cg/utils/email.py
@@ -1,5 +1,5 @@
-import smtplib
-import ssl
+from smtplib import SMTP
+
 from email.message import EmailMessage
 from cg.exc import EmailNotSentError
 from cg.models.email import EmailInfo
@@ -9,11 +9,7 @@ def send_mail(email_info: EmailInfo):
     """Email handler. Sending new user request email to admin"""
 
     try:
-        context = ssl.create_default_context()
-        with smtplib.SMTP_SSL(
-            email_info.smtp_server, email_info.sll_port, context=context
-        ) as server:
-            server.login(email_info.sender_email, email_info.sender_password)
+        with SMTP(email_info.smtp_server) as server:
             msg = EmailMessage()
             msg.set_content(email_info.message)
             msg["Subject"] = email_info.subject
