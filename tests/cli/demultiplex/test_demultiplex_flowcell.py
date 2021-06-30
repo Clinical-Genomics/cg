4952d8ee642df5a20de7d68fcf8289c0a07edf11
Barry Stokman <barry.stokman@gmail.com>
2021-06-30 15:19:00 +0200

    more tests

--- tests/cli/demultiplex/test_demultiplex_flowcell.py
+++ tests/cli/demultiplex/test_demultiplex_flowcell.py
@@ -10,36 +10,36 @@ from cg.models.cg_config import CGConfig
 from cg.models.demultiplex.flowcell import Flowcell
 
 
-# def test_demultiplex_flowcell_dry_run(
-#     cli_runner: testing.CliRunner,
-#     demultiplex_ready_flowcell: Path,
-#     demultiplex_context: CGConfig,
-#     caplog,
-# ):
-#     caplog.set_level(logging.INFO)
-#     # GIVEN that all files are present for demultiplexing
-#     flowcell: Flowcell = Flowcell(demultiplex_ready_flowcell)
-#     # GIVEN a out dir that does not exist
-#     demux_api: DemultiplexingAPI = demultiplex_context.demultiplex_api
-#     assert demux_api.is_demultiplexing_possible(flowcell=flowcell)
-#     demux_dir: Path = demux_api.flowcell_out_dir_path(flowcell)
-#     unaligned_dir: Path = demux_dir / "Unaligned"
-#     assert demux_dir.exists() is False
-#     assert unaligned_dir.exists() is False
-#
-#     # WHEN starting demultiplexing from the CLI with dry run flag
-#     result: testing.Result = cli_runner.invoke(
-#         demultiplex_flowcell,
-#         [str(demultiplex_ready_flowcell), "--dry-run"],
-#         obj=demultiplex_context,
-#     )
-#     # THEN assert the command exits without problems
-#     assert result.exit_code == 0
-#     # THEN assert no results folder was created since it is run in dry run mode
-#     assert demux_dir.exists() is False
-#     assert unaligned_dir.exists() is False
-#
-#
+def test_demultiplex_flowcell_dry_run(
+    cli_runner: testing.CliRunner,
+    demultiplex_ready_flowcell: Path,
+    demultiplex_context: CGConfig,
+    caplog,
+):
+    caplog.set_level(logging.INFO)
+    # GIVEN that all files are present for demultiplexing
+    flowcell: Flowcell = Flowcell(demultiplex_ready_flowcell)
+    # GIVEN a out dir that does not exist
+    demux_api: DemultiplexingAPI = demultiplex_context.demultiplex_api
+    assert demux_api.is_demultiplexing_possible(flowcell=flowcell)
+    demux_dir: Path = demux_api.flowcell_out_dir_path(flowcell)
+    unaligned_dir: Path = demux_dir / "Unaligned"
+    assert demux_dir.exists() is False
+    assert unaligned_dir.exists() is False
+
+    # WHEN starting demultiplexing from the CLI with dry run flag
+    result: testing.Result = cli_runner.invoke(
+        demultiplex_flowcell,
+        [str(demultiplex_ready_flowcell), "--dry-run"],
+        obj=demultiplex_context,
+    )
+    # THEN assert the command exits without problems
+    assert result.exit_code == 0
+    # THEN assert no results folder was created since it is run in dry run mode
+    assert demux_dir.exists() is False
+    assert unaligned_dir.exists() is False
+
+
 def test_demultiplex_bcl2fastq_flowcell(
     cli_runner: testing.CliRunner,
     demultiplex_ready_flowcell_bcl2fastq: Path,
