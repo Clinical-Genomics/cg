abdb3e66966f7cd135b6e0a473e1cb2ad2520e6d
Barry Stokman <26384108+barrystokman@users.noreply.github.com>
2021-07-06 10:31:38 +0200

    Update cg/apps/cgstats/crud/create.py
    Co-authored-by: mayabrandi <maya.brandi@scilifelab.se>

--- cg/apps/cgstats/crud/create.py
+++ cg/apps/cgstats/crud/create.py
@@ -199,7 +199,7 @@ def _create_samples(
     """handles sample objects creation for the table `Sample` in cgstats"""
 
     barcode = (
-        sample.index if not sample.second_index else "+".join([sample.index, sample.second_index])
+        sample.index if not sample.second_index else f"{sample.index}+{sample.second_index}"
     )
     sample_id: Optional[int] = find.get_sample_id(sample_id=sample.sample_id, barcode=barcode)
     if sample.project == "indexcheck":
