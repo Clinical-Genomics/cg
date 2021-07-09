3b0c5b548678a45d159285e121cc4c91f56c64ab
Barry Stokman <barry.stokman@gmail.com>
2021-07-09 15:13:20 +0200

    sbatch inheritance

--- cg/models/slurm/sbatch.py
+++ cg/models/slurm/sbatch.py
@@ -16,21 +16,14 @@ class Sbatch(BaseModel):
     commands: str
     error: Optional[str]
     exclude: Optional[str] = ""
+
+
+class SbatchBcl2Fastq(Sbatch):
     number_tasks: int
     memory: int
 
 
-class SbatchDragen(BaseModel):
-    job_name: str
-    account: str
-    log_dir: str
-    email: str
-    hours: int
-    minutes: str = "00"
-    priority: SlurmQos = SlurmQos.LOW
-    commands: str
-    error: Optional[str]
-    exclude: Optional[str] = ""
+class SbatchDragen(Sbatch):
     partition: str = "dragen"
     nodes: int = 1
     cpus_per_task: int = 24
