9d7368a45ada5248e1eaa6ea2533cfc317670380
karlnyr <kallejohan211@hotmail.com>
2021-09-23 15:53:36 +0200

    split housekeeper commands

--- cg/cli/demultiplex/wipe_flowcell.py
+++ cg/cli/demultiplex/wipe_flowcell.py
@@ -44,37 +44,47 @@ class WipeDemuxAPI:
         self.root_dir = root_dir
         self.status_db = status_db
 
+    def set_fail(self) -> None:
+        """Set exit code to EXIT_FAIL"""
+        self.exit_code = EXIT_FAIL
+
+    def set_success(self) -> None:
+        """Set exit code to EXIT_SUCCESS"""
+        self.exit_code = EXIT_SUCCESS
+
     def set_dry_run(self, dry_run: bool) -> None:
         """Set dry run flag for API"""
         self.dry_run = dry_run
 
     def wipe_flowcell_housekeeper(self, housekeeper_api: HousekeeperAPI, status_db: Store) -> None:
         """Wipe any presence of a flowcell in housekeeper. Including Sample sheets AND fastq-files"""
-        pass
+        self._wipe_flowcell_tag_housekeeper()
 
     def _wipe_flowcell_tag_housekeeper(self) -> None:
         """Wipe the presence of a flowcell, based on the flowcell id as a tag, in Housekeeper."""
+        self.set_success()
         if self.housekeeper_api.check_for_files(tags=[self.flowcell_id]):
             self.housekeeper_api.delete_files(dry_run=self.dry_run, tags=[self.flowcell_id])
             return
+        self.set_fail()
 
-    def _wipe_fastq_tags_housekeeper(
-        self, housekeeper_api: HousekeeperAPI, status_db: Store, samples: List[str]
-    ) -> None:
+    def _wipe_fastq_tags_housekeeper(self, samples: List[str]) -> None:
         """Wipe the presence of a flowcell, based on samples related to flowcell and the fastq-tags, in Housekeeper."""
+        self.set_success()
         samples_to_delete = []
 
         for sample in samples:
-            if housekeeper_api.check_for_files(tags=["fastq"]):
+            if self.housekeeper_api.check_for_files(tags=["fastq"]):
                 samples_to_delete.append(sample)
             else:
                 LOG.warning(f"No fastq-files for {sample} were found in Housekeeper.")
-                self.exit_code = EXIT_FAIL
+                self.set_fail()
 
         if samples_to_delete and self.exit_code == EXIT_SUCCESS:
             for sample in samples_to_delete:
-                housekeeper_api.delete_files(dry_run=self.dry_run, tags=["fastq"], bundle=sample)
-            return
+                self.housekeeper_api.delete_files(
+                    dry_run=self.dry_run, tags=["fastq"], bundle=sample
+                )
 
     def wipe_flowcell_statusdb(self, status_db: Store) -> None:
         """Wipe any prescense of a flowcell in status db"""
@@ -89,7 +99,7 @@ class WipeDemuxAPI:
         # Get path to out from demux, clean the path. Check there is a path for flowcell.
         pass
 
-    def _active_samples_on_flowcell(self) -> bool:
+    def active_samples_on_flowcell(self) -> bool:
         """Check if there are any active cases related to samples of a flowcell"""
         if any(
             [
