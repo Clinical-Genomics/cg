8e81855f117873af5a51cedde9c4a529b30d395a
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2021-10-16 16:45:20 +0200

    add new commands to CLI

--- cg/cli/upload/base.py
+++ cg/cli/upload/base.py
@@ -26,7 +26,8 @@ from .genotype import genotypes
 from .gisaid import gisaid
 from .mutacc import process_solved, processed_solved
 from .observations import observations
-from .scout import create_scout_load_config, scout, upload_case_to_scout
+from .scout import create_scout_load_config, scout, upload_case_to_scout, upload_fusion_report_to_scout, \
+    upload_splice_junctions_bed_to_scout, upload_rna_coverage_bigwig_to_scout, upload_rna_to_scout
 from .utils import suggest_cases_to_upload
 from .validate import validate
 
@@ -161,6 +162,10 @@ upload.add_command(processed_solved)
 upload.add_command(validate)
 upload.add_command(scout)
 upload.add_command(upload_case_to_scout)
+upload.add_command(upload_fusion_report_to_scout)
+upload.add_command(upload_splice_junctions_bed_to_scout)
+upload.add_command(upload_rna_coverage_bigwig_to_scout)
+upload.add_command(upload_rna_to_scout)
 upload.add_command(create_scout_load_config)
 upload.add_command(observations)
 upload.add_command(genotypes)
