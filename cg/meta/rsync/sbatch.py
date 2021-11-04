c63bfbd2ed2334a84bee5e09b76333c6df8af1f6
Vincent Janvid <69356202+Vince-janv@users.noreply.github.com>
2021-11-04 16:46:13 +0100

    Added md5sum ckecks (#1272) (minor)
    * Added md5sum ckecks
    
    * fixed syntax to work with current python
    
    * removed unnecessary arguments
    
    * Added and fixed tests
    
    * removed first idea
    
    * Removed debug printing and added extra log-info
    
    * added print for debugging
    
    * removed commit upon creating initial hk-bundle
    
    * Investigating potentially unnecessary hk-commit
    
    * Fixed error with adding existing files to hk-bundle
    
    * Still fixing error with adding existing files to hk-bundle
    
    * Setting case status to analyse for all cases
    
    * Added to Changelog and added logging for case status
    
    * Restructuring based on HSs comments, also addition and upgrade of pytests
    
    * Adding docstrings and fixing type hints
    
    * Removed redundant else and fixed type hints
    
    * Replaced variables with fixtures and simplified functions
    
    * Fixing comments and making functions more general
    
    * Fixed wrong function called in add.py
    
    * Added mkdir for customer directory on hasta
    
    * Clarified funktion names and made some more flexible
    
    * Fixed imports
    
    * Fixed issue with trailing slash and list appending
    
    * Added print for debugging
    
    * More debugging
    
    * Fixed bug with None-filled list
    
    * Removed print and added a for loop instead if map
    
    * Final touches
    
    * Fixed mkdir
    
    * Fixed sbatch log_dir

--- cg/meta/rsync/sbatch.py
+++ cg/meta/rsync/sbatch.py
@@ -4,6 +4,10 @@ RSYNC_COMMAND = """
 rsync -rvL {source_path} {destination_path}
 """
 
+RSYNC_CONTENTS_COMMAND = """
+rsync -rvL {source_path}/ {destination_path}
+"""
+
 COVID_RSYNC = """
 rsync -rvL {source_path} {destination_path}
 rsync -rvL {covid_report_path} {covid_destination_path}
