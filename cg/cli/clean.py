ad313f022e88f8566d17ad952ae1fbe94b611565
barrystokman <barry.stokman@gmail.com>
2021-12-13 10:19:24 +0100

    clearer log message

--- cg/cli/clean.py
+++ cg/cli/clean.py
@@ -263,7 +263,7 @@ def fix_flowcell_status(context: CGConfig, dry_run: bool):
     flowcells_in_statusdb = [
         flowcell for flowcell in status_db.flowcells() if flowcell.status in ["ondisk", "removed"]
     ]
-    LOG.info("Found %s flowcells in statusdb!", len(flowcells_in_statusdb))
+    LOG.info("Number of ondisk or removed flowcells in statusdb: %s", len(flowcells_in_statusdb))
     physical_ondisk_flowcell_names = [
         DemultiplexedRunsFlowcell(flowcell_dir, status_db, housekeeper_api).id
         for flowcell_dir in demux_api.out_dir.iterdir()
