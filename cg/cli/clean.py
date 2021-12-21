ee7211800f7579f2b15693d19242172bd6bef0e6
Barry Stokman <26384108+barrystokman@users.noreply.github.com>
2021-12-21 08:49:56 +0100

    better dry run handling
    Co-authored-by: Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>

--- cg/cli/clean.py
+++ cg/cli/clean.py
@@ -239,11 +239,12 @@ def remove_invalid_flowcell_directories(context: CGConfig, failed_only: bool, dr
 
     for flowcell in failed_flowcells:
         LOG.warning("Invalid flowcell directory found: %s", flowcell.path)
-        if not dry_run:
-            LOG.warning("Removing %s!", flowcell.path)
-            flowcell.remove_from_demultiplexed_runs()
-            if flowcell.fastq_files_exist_in_housekeeper and flowcell.is_correctly_named:
-                flowcell.remove_files_from_housekeeper()
+        if dry_run:
+            continue
+        LOG.warning("Removing %s!", flowcell.path)
+        flowcell.remove_from_demultiplexed_runs()
+        if flowcell.fastq_files_exist_in_housekeeper and flowcell.is_correctly_named:
+            flowcell.remove_files_from_housekeeper()
 
 
 @clean.command("fix-flowcell-status")
