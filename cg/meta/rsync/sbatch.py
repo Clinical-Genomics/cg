c61df16d178aa2a2a5930c28b7b69fbbdb53505f
henningonsbring <henning.onsbring@gmail.com>
2022-01-10 15:28:35 +0100

    rsync -a flag

--- cg/meta/rsync/sbatch.py
+++ cg/meta/rsync/sbatch.py
@@ -1,16 +1,16 @@
 """Sbatch templates for the rsync function"""
 
 RSYNC_COMMAND = """
-rsync -rvL {source_path} {destination_path}
+rsync -rvLa {source_path} {destination_path}
 """
 
 RSYNC_CONTENTS_COMMAND = """
-rsync -rvL {source_path}/ {destination_path}
+rsync -rvLa {source_path}/ {destination_path}
 """
 
 COVID_RSYNC = """
-rsync -rvL {source_path} {destination_path}
-rsync -rvL {covid_report_path} {covid_destination_path}
+rsync -rvLa {source_path} {destination_path}
+rsync -rvLa {covid_report_path} {covid_destination_path}
 """
 
 ERROR_RSYNC_FUNCTION = """
