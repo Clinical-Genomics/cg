e11542a1ce5c5957d3555ad3de8a437313e7ec87
henningonsbring <henning.onsbring@gmail.com>
2022-01-10 15:49:01 +0100

    replaced -r with -a

--- cg/meta/rsync/sbatch.py
+++ cg/meta/rsync/sbatch.py
@@ -1,16 +1,16 @@
 """Sbatch templates for the rsync function"""
 
 RSYNC_COMMAND = """
-rsync -rvLa {source_path} {destination_path}
+rsync -avL {source_path} {destination_path}
 """
 
 RSYNC_CONTENTS_COMMAND = """
-rsync -rvLa {source_path}/ {destination_path}
+rsync -avL {source_path}/ {destination_path}
 """
 
 COVID_RSYNC = """
-rsync -rvLa {source_path} {destination_path}
-rsync -rvLa {covid_report_path} {covid_destination_path}
+rsync -avL {source_path} {destination_path}
+rsync -avL {covid_report_path} {covid_destination_path}
 """
 
 ERROR_RSYNC_FUNCTION = """
