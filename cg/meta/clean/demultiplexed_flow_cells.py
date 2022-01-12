cd1c430ea8ce21da0c267fb61521edb8da21391b
Barry Stokman <26384108+barrystokman@users.noreply.github.com>
2022-01-12 09:26:23 +0100

    Update cg/meta/clean/demultiplexed_flow_cells.py
    Co-authored-by: Henrik Stranneheim <henrik.stranneheim@scilifelab.se>

--- cg/meta/clean/demultiplexed_flow_cells.py
+++ cg/meta/clean/demultiplexed_flow_cells.py
@@ -148,7 +148,7 @@ class DemultiplexedRunsFlowCell:
                 )
             else:
                 if self.fastq_files_exist_in_housekeeper:
-                    fastq_files_exist_on_disk = self._check_files_existence(self.hk_fastq_files)
+                    fastq_files_exist_on_disk: List[bool] = self._check_files_existence(self.hk_fastq_files)
                     self._files_exist_on_disk: bool = all(fastq_files_exist_on_disk)
                 if self.spring_files_exist_in_housekeeper:
                     spring_files_exist_on_disk: List[bool] = self._check_files_existence(self.hk_spring_files)
