dbdb68efd583545ebfdaeb596346348f86b5988f
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2022-02-14 15:23:26 +0100

    Revert "Bump version: 22.44.3 → 22.45.0 [skip ci]"
    This reverts commit 9463d899c85606d1070901643be268e0738df7a9.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ except FileNotFoundError:
 
 setup(
     name=NAME,
-    version="22.45.0",
+    version="22.44.3",
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
