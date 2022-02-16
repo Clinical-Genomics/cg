930bb6fbca1432924d04f98ea387bda913bced88
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2022-02-16 11:09:14 +0100

    rewrite for sonarcloud

--- cg/server/api.py
+++ cg/server/api.py
@@ -86,10 +86,11 @@ def submit_order(order_type):
     error_message: str
     try:
         request_json = request.get_json()
-        if not order_type.isalnum() or not request_json.isalnum():
-            raise OrderFormError("Could not process order since it contains non-alphanumeric data")
 
-        LOG.info("processing '%s' order: %s", order_type, request_json)
+        if order_type.isalnum() and request_json.isalnum():
+            LOG.info("processing '%s' order: %s", order_type, request_json)
+        else:
+            raise OrderFormError("Could not process order since it contains non-alphanumeric data")
 
         project: OrderType = OrderType(order_type)
         order_in: OrderIn = OrderIn.parse_obj(request_json, project=project)
