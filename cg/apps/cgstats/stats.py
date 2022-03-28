3cf390428472b67715d8bc86322c0a0ffdfc1385
karlnyr <kallejohan211@hotmail.com>
2022-03-28 13:16:18 +0200

    removing untouched variable

--- cg/apps/cgstats/stats.py
+++ cg/apps/cgstats/stats.py
@@ -41,7 +41,6 @@ class StatsAPI(alchy.Manager):
             curated_sample_name: str = self.get_curated_sample_name(sample_obj.samplename)
             sample_data = {"name": curated_sample_name, "reads": 0, "fastqs": []}
             for fc_data in self.sample_reads(sample_obj):
-                read_length: str = ""
                 if fc_data.q30 >= FLOWCELL_Q30_THRESHOLD[fc_data.type]:
                     sample_data["reads"] += fc_data.reads
                 else:
