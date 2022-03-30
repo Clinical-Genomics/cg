13fccc0b349fe0b1d300b68706e9ccfa938b927d
Vadym Ivanchuk <vadym.ivanchuk@scilifelab.se>
2022-03-30 15:51:42 +0200

    fix mock chanjo api pytest

--- tests/mocks/report.py
+++ tests/mocks/report.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from pathlib import Path
 from typing import Union
 
+from cg.apps.coverage import ChanjoAPI
 from cg.meta.workflow.mip_dna import MipDNAAnalysisAPI
 from cg.meta.report.mip_dna import MipDNAReportAPI
 from cg.meta.workflow.analysis import AnalysisAPI
@@ -78,10 +79,10 @@ class MockDB(Store):
         self.store = store
 
 
-class MockChanjo:
+class MockChanjo(ChanjoAPI):
     """Chanjo mock class"""
 
-    def sample_coverage(self, sample_id: str, _) -> Union[None, dict]:
+    def sample_coverage(self, sample_id: str, panel_genes: list) -> Union[None, dict]:
         """Calculates  for a specific panel"""
 
         sample_coverage = None
