6a30332792b8bbae56d0cbf64154b34da782eb70
barrystokman <barry.stokman@gmail.com>
2022-04-08 15:22:35 +0200

    mock backup path fixture

--- tests/meta/backup/conftest.py
+++ tests/meta/backup/conftest.py
@@ -9,6 +9,12 @@ def fixture_spring_file_path() -> Path:
     return Path("/path/to/spring_file.spring")
 
 
+@pytest.fixture(name="backup_file_path")
+def fixture_backup_file_path() -> str:
+    """Return path to a file used in the backup process"""
+    return "/path/to/backup_file.extension"
+
+
 @pytest.fixture(name="binary_path")
 def fixture_binary_path() -> str:
     """Return the string of a path to a binary"""
--- tests/meta/backup/test_meta_pdc.py
+++ tests/meta/backup/test_meta_pdc.py
@@ -5,48 +5,48 @@ from cg.meta.backup.pdc import PdcAPI
 
 
 @mock.patch("cg.meta.backup.pdc.Process")
-def test_archive_file_to_pdc(mock_process, binary_path):
+def test_archive_file_to_pdc(mock_process, binary_path, backup_file_path):
     """tests execution command to archive files to pdc"""
     # GIVEN
     pdc_api = PdcAPI(binary_path=binary_path)
     pdc_api.process = mock_process
 
     # WHEN
-    pdc_api.archive_file_to_pdc("path/to/file")
+    pdc_api.archive_file_to_pdc(backup_file_path)
 
     # THEN
     mock_process.run_command.assert_called_once_with(
-        parameters=["archive", "path/to/file"], dry_run=False
+        parameters=["archive", backup_file_path], dry_run=False
     )
 
 
 @mock.patch("cg.meta.backup.pdc.Process")
-def test_query_pdc(mock_process, binary_path):
+def test_query_pdc(mock_process, binary_path, backup_file_path):
     """tests execution command to archive files to pdc"""
     # GIVEN
     pdc_api = PdcAPI(binary_path=binary_path)
     pdc_api.process = mock_process
 
     # WHEN
-    pdc_api.query_pdc("path/to/file")
+    pdc_api.query_pdc(backup_file_path)
 
     # THEN
     mock_process.run_command.assert_called_once_with(
-        parameters=["q", "archive", "path/to/file"], dry_run=False
+        parameters=["q", "archive", backup_file_path], dry_run=False
     )
 
 
 @mock.patch("cg.meta.backup.pdc.Process")
-def test_retrieve_file_from_pdc(mock_process, binary_path):
+def test_retrieve_file_from_pdc(mock_process, binary_path, backup_file_path):
     """tests execution command to archive files to pdc"""
     # GIVEN
     pdc_api = PdcAPI(binary_path=binary_path)
     pdc_api.process = mock_process
 
     # WHEN
-    pdc_api.retrieve_file_from_pdc("path/to/file")
+    pdc_api.retrieve_file_from_pdc(backup_file_path)
 
     # THEN
     mock_process.run_command.assert_called_once_with(
-        parameters=["retrieve", "-replace=yes", "path/to/file"], dry_run=False
+        parameters=["retrieve", "-replace=yes", backup_file_path], dry_run=False
     )
