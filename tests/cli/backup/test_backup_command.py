818db28b383f445e6ffb1202ef28e78efb316001
barrystokman <barry.stokman@gmail.com>
2022-04-14 08:55:00 +0200

    review changes

--- tests/cli/backup/test_backup_command.py
+++ tests/cli/backup/test_backup_command.py
@@ -2,7 +2,7 @@ import logging
 
 from click.testing import CliRunner
 
-from cg.cli.backup import backup, fetch_flowcell
+from cg.cli.backup import backup, fetch_flow_cell
 from cg.meta.backup.backup import BackupApi
 from cg.models.cg_config import CGConfig
 
@@ -14,7 +14,7 @@ def test_run_backup_base_cmd(cli_runner: CliRunner, cg_context: CGConfig):
     assert "backup_api" not in cg_context.meta_apis
 
     # WHEN running the base command
-    result = cli_runner.invoke(backup, ["fetch-flowcell", "--help"], obj=cg_context)
+    result = cli_runner.invoke(backup, ["fetch-flow-cell", "--help"], obj=cg_context)
 
     # THEN assert that the command exits without any problems
     assert result.exit_code == 0
@@ -32,12 +32,12 @@ def test_run_fetch_flow_cell_dry_run_no_flow_cell_specified(
     assert not backup_context.status_db.flowcells(status="requested").first()
 
     # WHEN running the fetch flow cell command without specifying any flow cell in dry run mode
-    result = cli_runner.invoke(fetch_flowcell, ["--dry-run"], obj=backup_context)
+    result = cli_runner.invoke(fetch_flow_cell, ["--dry-run"], obj=backup_context)
 
     # THEN assert that it exits without any problems
     assert result.exit_code == 0
     # THEN assert that it is communicated that no flow cells are requested
-    assert "no flow cells requested" in caplog.text
+    assert "No flow cells requested" in caplog.text
 
 
 def test_run_fetch_flow_cell_dry_run_retrieval_time(
@@ -50,10 +50,10 @@ def test_run_fetch_flow_cell_dry_run_retrieval_time(
     assert not backup_context.status_db.flowcells(status="requested").first()
     # GIVEN that the backup api returns a retrieval time
     expected_time = 60
-    mocker.patch("cg.meta.backup.backup.BackupApi.fetch_flowcell", return_value=expected_time)
+    mocker.patch("cg.meta.backup.backup.BackupApi.fetch_flow_cell", return_value=expected_time)
 
     # WHEN running the fetch flow cell command without specifying any flow cell in dry run mode
-    result = cli_runner.invoke(fetch_flowcell, ["--dry-run"], obj=backup_context)
+    result = cli_runner.invoke(fetch_flow_cell, ["--dry-run"], obj=backup_context)
 
     # THEN assert that it exits without any problems
     assert result.exit_code == 0
@@ -66,13 +66,13 @@ def test_run_fetch_flow_cell_non_existing_flow_cell(
 ):
     # GIVEN a context with a backup api
     # GIVEN a non existing flow cell id
-    flowcell_id = "hello"
-    assert backup_context.status_db.flowcell(flowcell_id) is None
+    flow_cell_id = "hello"
+    assert backup_context.status_db.flowcell(flow_cell_id) is None
 
     # WHEN running the command with the non existing flow cell id
-    result = cli_runner.invoke(fetch_flowcell, ["--flowcell", flowcell_id], obj=backup_context)
+    result = cli_runner.invoke(fetch_flow_cell, ["--flow-cell", flow_cell_id], obj=backup_context)
 
     # THEN assert that it exits with a non zero exit code
     assert result.exit_code != 0
     # THEN assert that it was communicated that the flow cell does not exist
-    assert f"{flowcell_id}: not found" in caplog.text
+    assert f"{flow_cell_id}: not found" in caplog.text
