7aeb208d5e4a202a6f8653f0c3d26e3b318e4b74
barrystokman <barry.stokman@gmail.com>
2022-05-13 14:39:07 +0200

    cg_config fixture

--- tests/cli/backup/conftest.py
+++ tests/cli/backup/conftest.py
@@ -2,14 +2,18 @@ import pytest
 
 from cg.meta.backup.backup import BackupApi
 from cg.meta.backup.pdc import PdcAPI
+from cg.meta.encryption.encryption import EncryptionAPI
+from cg.meta.tar.tar import TarAPI
 from cg.models.cg_config import CGConfig
 
 
 @pytest.fixture(name="backup_context")
 def fixture_backup_context(cg_context: CGConfig) -> CGConfig:
     cg_context.meta_apis["backup_api"] = BackupApi(
+        encryption_api=EncryptionAPI(binary_path=cg_context.encryption.binary_path),
         status=cg_context.status_db,
-        pdc_api=PdcAPI(),
+        tar_api=TarAPI(binary_path=cg_context.tar.binary_path),
+        pdc_api=PdcAPI(binary_path=cg_context.pdc.binary_path),
         root_dir=cg_context.backup.root.dict(),
     )
     return cg_context
--- tests/conftest.py
+++ tests/conftest.py
@@ -1235,6 +1235,7 @@ def fixture_context_config(
         },
         "encryption": {"binary_path": "bin/gpg"},
         "pdc": {"binary_path": "/bin/dsmc"},
+        "tar": {"binary_path": "/bin/tar"},
         "shipping": {"host_config": "host_config_stage.yaml", "binary_path": "echo"},
         "housekeeper": {"database": fixture_hk_uri, "root": str(housekeeper_dir)},
         "trailblazer": {
