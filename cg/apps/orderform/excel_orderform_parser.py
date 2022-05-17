c2949644d9e7d4d48238fdd8862eb6a7fcfd139e
vincent.janvid <vincent.janvid@scilifelab.se>
2022-05-17 07:58:20 +0200

    Updated json and excel order, and accepted version

--- cg/apps/orderform/excel_orderform_parser.py
+++ cg/apps/orderform/excel_orderform_parser.py
@@ -28,7 +28,7 @@ class ExcelOrderformParser(OrderformParser):
         f"{Orderform.MICROSALT}:11",  # Microbial WGS
         f"{Orderform.RML}:14",  # Orderform Ready made libraries (RML)
         f"{Orderform.METAGENOME}:10",  # Microbial meta genomes
-        f"{Orderform.SARS_COV_2}:6",  # Orderform SARS-CoV-2
+        f"{Orderform.SARS_COV_2}:7",  # Orderform SARS-CoV-2
     ]
     samples: List[ExcelSample] = []
 
