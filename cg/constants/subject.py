3e61b4b6a2c9e9806f121c1e80a96ccfd0a54219
Vincent Janvid <69356202+Vince-janv@users.noreply.github.com>
2022-05-22 13:51:17 +0200

    fix(scout upload config) Change representation of new types (#1490)
    ### Changed
    
    - Representation of the types Gender and PhenotypeStatus

--- cg/constants/subject.py
+++ cg/constants/subject.py
@@ -8,12 +8,18 @@ class Gender(str, Enum):
     OTHER = "other"
     MISSING = None
 
+    def __repr__(self):
+        return self.value
+
 
 class PhenotypeStatus(str, Enum):
     UNKNOWN = "unknown"
     UNAFFECTED = "unaffected"
     AFFECTED = "affected"
     MISSING = None
+
+    def __repr__(self):
+        return self.value
 
 
 class PlinkPhenotypeStatus(IntEnum):
