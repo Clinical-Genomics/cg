6156a7c059dca21eb36a5b11bab94b4178b02919
henningonsbring <henning.onsbring@scilifelab.se>
2022-06-01 15:12:40 +0200

    removed unecessary variable

--- cg/meta/workflow/mutant.py
+++ cg/meta/workflow/mutant.py
@@ -54,9 +54,7 @@ class MutantAnalysisAPI(AnalysisAPI):
     def get_trailblazer_config_path(self, case_id: str) -> Path:
         return Path(self.get_case_output_path(case_id=case_id), "trailblazer_config.yaml")
 
-    def get_sample_fastq_destination_dir(
-        self, case_obj: models.Family, sample_obj: models.Sample, nanopore: bool
-    ):
+    def get_sample_fastq_destination_dir(self, case_obj: models.Family, sample_obj: models.Sample):
         application_tag = sample_obj.application_version.application.tag
         seq_info = application_tag[3:6]
         if seq_info == "ONT":
