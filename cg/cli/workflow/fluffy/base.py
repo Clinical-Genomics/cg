7e99a7c7099f17d21f1c46810f313ce41f68b1a8
henningonsbring <henning.onsbring@scilifelab.se>
2022-06-03 13:13:48 +0200

    blacked

--- cg/cli/workflow/fluffy/base.py
+++ cg/cli/workflow/fluffy/base.py
@@ -83,7 +83,11 @@ def run(context: CGConfig, case_id: str, dry_run: bool, config: str, external_re
 @OPTION_EXTERNAL_REF
 @click.pass_context
 def start(
-    context: click.Context, case_id: str, dry_run: bool, external_ref: bool = False, config: str = None
+    context: click.Context,
+    case_id: str,
+    dry_run: bool,
+    external_ref: bool = False,
+    config: str = None,
 ):
     """
     Starts full Fluffy analysis workflow
