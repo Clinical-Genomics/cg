2bd964cfd94f276d90d34020f91d93646891c876
Annick Renevey <47788523+rannick@users.noreply.github.com>
2022-06-09 15:38:08 +0200

    adapt to balsamic umi changes

--- cg/meta/upload/scout/balsamic_umi_config_builder.py
+++ cg/meta/upload/scout/balsamic_umi_config_builder.py
@@ -2,7 +2,7 @@ import logging
 
 from cg.meta.upload.scout.balsamic_config_builder import BalsamicConfigBuilder
 from cg.apps.lims import LimsAPI
-from cg.models.scout.scout_load_config import BalsamicUmiLoadConfig
+from cg.models.scout.scout_load_config import BalsamicUmiLoadConfig, ScoutBalsamicIndividual
 from cg.store import models
 
 from housekeeper.store import models as hk_models
@@ -17,4 +17,14 @@ class BalsamicUmiConfigBuilder(BalsamicConfigBuilder):
         super().__init__(
             hk_version_obj=hk_version_obj, analysis_obj=analysis_obj, lims_api=lims_api
         )
+        self.case_tags: CaseTags = CaseTags(**BALSAMIC_UMI_CASE_TAGS)
+        self.sample_tags: SampleTags = SampleTags(**BALSAMIC_UMI_SAMPLE_TAGS)
         self.load_config: BalsamicUmiLoadConfig = BalsamicUmiLoadConfig(track="cancer")
+
+    def include_sample_files(self, config_sample: ScoutBalsamicIndividual) -> None:
+        LOG.info("Including BALSAMIC specific sample level files")
+
+    def get_balsamic_analysis_type(self, sample: models.Sample) -> str:
+        """Returns a formatted balsamic analysis type"""
+
+        return "panel-umi"
