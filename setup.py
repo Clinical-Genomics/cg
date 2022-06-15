7967e565057a63e43dfae944938c79d4a90ac35f
Patrik Grenfeldt <patrik.grenfeldt@scilifelab.se>
2022-06-15 13:16:59 +0200

    remove/replace pg

--- setup.py
+++ setup.py
@@ -8,8 +8,8 @@ from setuptools import find_packages, setup
 NAME = "cg"
 DESCRIPTION = "Clinical Genomics command center"
 URL = "https://github.com/Clinical-Genomics/cg"
-EMAIL = "patrik.grenfeldt@scilifelab.se"
-AUTHOR = "Patrik Grenfeldt"
+EMAIL = "support@clinicalgenomics.se"
+AUTHOR = "Clinical Genomics"
 REQUIRES_PYTHON = ">=3.6.0"
 
 HERE = os.path.abspath(os.path.dirname(__file__))
