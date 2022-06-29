e6920845da65df2243583c732b65fcf1e9f776d4
Annick Renevey <47788523+rannick@users.noreply.github.com>
2022-06-29 12:41:14 +0200

    remove import of unused modules

--- cg/meta/workflow/balsamic_qc.py
+++ cg/meta/workflow/balsamic_qc.py
@@ -1,18 +1,10 @@
 """Module for Balsamic Analysis API"""
 
-import json
 import logging
-from pathlib import Path
-from typing import List, Optional
 
-from cg.constants import DataDelivery, Pipeline
-from cg.exc import BalsamicStartError
-from cg.meta.workflow.analysis import AnalysisAPI
+from cg.constants import Pipeline
 from cg.meta.workflow.balsamic import BalsamicAnalysisAPI
-from cg.meta.workflow.fastq import BalsamicFastqHandler
 from cg.models.cg_config import CGConfig
-from cg.store import models
-from cg.utils import Process
 
 LOG = logging.getLogger(__name__)
 
