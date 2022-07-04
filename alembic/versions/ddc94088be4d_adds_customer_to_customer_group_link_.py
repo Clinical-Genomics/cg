3b39a3b1dbb3d7b9d50f54aa9c68682ba1ae841e
vincent.janvid <vincent.janvid@scilifelab.se>
2022-07-04 15:18:32 +0200

    Work in progress

--- alembic/versions/ddc94088be4d_adds_customer_to_customer_group_link_.py
+++ alembic/versions/ddc94088be4d_adds_customer_to_customer_group_link_.py
@@ -8,9 +8,9 @@ Create Date: 2022-06-29 15:11:08.142445
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
-from sqlalchemy import Column, types
+from sqlalchemy.dialects import mysql
+from sqlalchemy import Column, types, orm
 from sqlalchemy.ext.declarative import declarative_base
 
 revision = "ddc94088be4d"
@@ -20,32 +20,92 @@ depends_on = None
 
 Base = declarative_base()
 
+customer_group_links = sa.Table(
+    "customer_group_links",
+    Base.metadata,
+    Column("customer_id", types.Integer, sa.ForeignKey("customer.id"), nullable=False),
+    Column("customer_group_id", types.Integer, sa.ForeignKey("customer_group.id"), nullable=False),
+    sa.UniqueConstraint("customer_id", "customer_group_id", name="_customer_group_link_uc"),
+)
+
+
+class Customer(Base):
+    __tablename__ = "customer"
+    id = Column(types.Integer, primary_key=True)
+    customer_groups = orm.relationship("CustomerGroup", secondary=customer_group_links)
+    customer_group_id = Column(sa.ForeignKey("customer_group.id"), nullable=False)
+
 
 class CustomerGroup(Base):
+    __tablename__ = "customer_group"
     id = Column(types.Integer, primary_key=True)
     internal_id = Column(types.String(32), unique=True, nullable=False)
     name = Column(types.String(128), nullable=False)
-
-    customers = sa.orm.relationship(Customer, backref="customer_group", order_by="-Customer.id")
-
-
-class CustomerLink(Base):
-    __table_args__ = (sa.UniqueConstraint("viewer_id", "owner_id", name="_cust_link_uc"),)
-    id = Column(types.Integer, primary_key=True)
-    viewer_id = Column(sa.ForeignKey("customer.id"))
-    owner_id = Column(sa.ForeignKey("customer.id"))
-    viewer = sa.orm.relationship("Customer")
-    owner = sa.orm.relationship("Customer")
+    customers = orm.relationship("Customer", secondary=customer_group_links)
 
 
 def upgrade():
-    op.create_table("customer_link", Column("id", types.Integer, primary_key=True))
-    op.create_foreign_key(
-        source_table="customer",
-        referent_table="customer_link",
+    bind = op.get_bind()
+    session = sa.orm.Session(bind=bind)
+
+    for customer_group in session.query(CustomerGroup):
+        print(customer_group)
+
+    op.create_table(
+        "customer_group_links",
+        sa.Column("customer_id", sa.Integer(), nullable=False),
+        sa.Column("customer_group_id", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ("customer_id",),
+            ["customer.id"],
+        ),
+        sa.ForeignKeyConstraint(
+            ("customer_group_id",),
+            ["customer_group.id"],
+        ),
+        sa.UniqueConstraint("customer_id", "customer_group_id", name="_customer_custg_uc"),
     )
-    pass
+
+    for customer_group in session.query(CustomerGroup):
+        if len(customer_group) > 1:
+            print("Got here!")
+            for customer in customer_group.customers:
+                customer.customer_group.append(customer)
+        else:
+            sa.delete(customer_group)
+
+    op.drop_column(
+        table_name="customer",
+        column_name="customer_group_id",
+    )
+    session.commit()
 
 
 def downgrade():
-    pass
+    bind = op.get_bind()
+    session = sa.orm.Session(bind=bind)
+
+    op.add_column(
+        "customer",
+        sa.Column(
+            "customer_group_id",
+            mysql.INTEGER(display_width=11),
+            autoincrement=False,
+            nullable=False,
+        ),
+    )
+    for customer in session.query(Customer):
+        if not Customer.customer_groups:
+            customer_group = CustomerGroup(internal_id=customer.internal_id, name=customer.name)
+            session.add_commit(customer_group)
+            session.refresh(customer_group)
+            customer.customer_group_id = customer_group.id
+        else:
+            customer.customer_group_id = customer.customer_groups[0].id
+            print(
+                f"Customer {customer.internal_id} is added to the group "
+                f"{customer.customer_groups[0].name} "
+            )
+
+    op.drop_table("customer_group_links")
+    session.commit()
