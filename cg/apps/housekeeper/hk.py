a9817f6cb88fefb647dfccb1d5d2d86d6ed79cec
karlnyr <kallejohan211@hotmail.com>
2022-07-06 15:02:11 +0200

    stack info

--- cg/apps/housekeeper/hk.py
+++ cg/apps/housekeeper/hk.py
@@ -24,7 +24,8 @@ class HousekeeperAPI:
         self.root_dir = config["housekeeper"]["root"]
 
     def __getattr__(self, name):
-        raise LOG.exception("Called undefined %s on %s, please wrap", name, self.__class__.__name__)
+        LOG.info("This prints the stack", stack_info=True)
+        LOG.warning("Called undefined %s on %s, please wrap", name, self.__class__.__name__)
         # return getattr(self._store, name)
 
     def new_bundle(self, name: str, created_at: dt.datetime = None) -> models.Bundle:
