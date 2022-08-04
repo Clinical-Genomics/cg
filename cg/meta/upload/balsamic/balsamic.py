f056242001213bb690fe4aa516b39ee68e758248
Vadym Ivanchuk <vadym.ivanchuk@scilifelab.se>
2022-08-04 17:29:10 +0200

    Revert "add delivery report to upload command"
    This reverts commit 3d91b1ce5d1cf098c420f9cea4b90cb1366666df.

--- cg/meta/upload/balsamic/balsamic.py
+++ cg/meta/upload/balsamic/balsamic.py
@@ -3,8 +3,6 @@
 import logging
 
 import click
-
-from cg.cli.upload.delivery_report import upload_delivery_report_to_scout
 from cg.cli.upload.scout import scout
 from cg.constants import DataDelivery
 from cg.meta.workflow.balsamic import BalsamicAnalysisAPI
@@ -31,7 +29,6 @@ class BalsamicUploadAPI(UploadAPI):
         if DataDelivery.SCOUT in case_obj.data_delivery:
             self.update_upload_started_at(analysis_obj)
             ctx.invoke(scout, case_id=case_obj.internal_id, re_upload=restart)
-            ctx.invoke(upload_delivery_report_to_scout, case_id=case_obj.internal_id, dry_run=False)
             self.update_uploaded_at(analysis_obj)
         else:
             LOG.warning(
