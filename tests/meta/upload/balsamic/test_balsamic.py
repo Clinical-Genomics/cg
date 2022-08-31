b6c02eb892ac882a19c0dea0a4f15dde875e929a
GustavLagneborgScilifelab <107848458+GustavLagneborgScilifelab@users.noreply.github.com>
2022-08-31 09:31:30 +0200

    adding fixtures to pytests

--- tests/meta/upload/balsamic/test_balsamic.py
+++ tests/meta/upload/balsamic/test_balsamic.py
@@ -1,12 +1,14 @@
 """Tests API upload methods for balsamic cases"""
 
 from cg.models.cg_config import CGConfig
+from cg.store import models
 
 
 def test_genotype_check_wgs_normal(balsamic_context: CGConfig):
     """Test a balsamic case with WGS and Normal sample that is genotype compatible"""
     # GIVEN a balsamic case with WGS tag and a normal sample
-    case_obj = balsamic_context.status_db.
+    internal_id = "balsamic_case_wgs_paired_enough_reads"
+    case_obj: models.Family = balsamic_context.status_db.family(internal_id=internal_id)
 
     # WHEN checking if genotype upload compatible
     genotype_check = case_obj.genotype_check
@@ -18,7 +20,8 @@ def test_genotype_check_wgs_normal(balsamic_context: CGConfig):
 def test_genotype_check_non_wgs_normal(balsamic_context: CGConfig):
     """Test a balsamic case with no WGS sample that is not genotype compatible"""
     # GIVEN a balsamic case with a normal sample but no WGS tag
-    case_obj = balsamic_context.status_db.
+    internal_id = "balsamic_case_tgs_paired"
+    case_obj: models.Family = balsamic_context.status_db.family(internal_id=internal_id)
 
     # WHEN checking if genotype upload compatible
     genotype_check = case_obj.genotype_check
@@ -30,7 +33,8 @@ def test_genotype_check_non_wgs_normal(balsamic_context: CGConfig):
 def test_genotype_check_only_tumour(balsamic_context: CGConfig):
     """Test a balsamic case with only a tumour sample that is not genotype compatible"""
     # GIVEN a balsamic case with only tumour sample
-    case_obj = balsamic_context.status_db.
+    internal_id = "balsamic_case_wgs_single"
+    case_obj: models.Family = balsamic_context.status_db.family(internal_id=internal_id)
 
     # WHEN checking if genotype upload compatible
     genotype_check = case_obj.genotype_check
