a9a9aa7a88ea40556a4e97a1c242e2fd8243d917
Annick Renevey <47788523+rannick@users.noreply.github.com>
2022-08-31 13:53:14 +0200

    fix typo

--- cg/resources/__init__.py
+++ cg/resources/__init__.py
@@ -6,7 +6,7 @@ valid_indexes_file = "resources/20181012_Indices.csv"
 
 valid_indexes_path = Path(pkg_resources.resource_filename("cg", valid_indexes_file))
 
-rnafusion_bundle_filenames: str = Path("resources, "rnafusion_bundle_filenames.csv").as_posix()
+rnafusion_bundle_filenames: str = Path("resources", "rnafusion_bundle_filenames.csv").as_posix()
 
 rnafusion_bundle_filenames_path = Path(
     pkg_resources.resource_filename("cg", rnafusion_bundle_filenames)
