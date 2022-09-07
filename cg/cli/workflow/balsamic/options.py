1f86d3621685be1d108732e6cf6768e2787a37a5
Vadym Ivanchuk <vadym.ivanchuk@scilifelab.se>
2022-09-07 09:25:58 +0200

    update CLI help field

--- cg/cli/workflow/balsamic/options.py
+++ cg/cli/workflow/balsamic/options.py
@@ -42,3 +42,40 @@ OPTION_PON_CNN = click.option(
     required=False,
     help="Panel of normal reference (.cnn) for CNVkit",
 )
+OPTION_CLINICAL_SNV_OBSERVATIONS = click.option(
+    "--clinical-snv-observations",
+    type=click.Path(exists=True),
+    required=False,
+    help="VCF path of clinical SNV observations (WGS analysis workflow). Set this option to override the latest "
+    "loqusdb dump.",
+)
+OPTION_CLINICAL_SV_OBSERVATIONS = click.option(
+    "--clinical-sv-observations",
+    type=click.Path(exists=True),
+    required=False,
+    help="VCF path of clinical SV observations (WGS analysis workflow)",
+)
+OPTION_CANCER_SNV_NORMAL_OBSERVATIONS = click.option(
+    "--cancer-snv-normal-observations",
+    type=click.Path(exists=True),
+    required=False,
+    help="VCF path of cancer SNV normal observations (WGS analysis workflow)",
+)
+OPTION_CANCER_SNV_TUMOR_OBSERVATIONS = click.option(
+    "--cancer-snv-tumor-observations",
+    type=click.Path(exists=True),
+    required=False,
+    help="VCF path of cancer SNV tumor observations (WGS analysis workflow)",
+)
+OPTION_CANCER_SV_NORMAL_OBSERVATIONS = click.option(
+    "--cancer-sv-normal-observations",
+    type=click.Path(exists=True),
+    required=False,
+    help="VCF path of cancer SV normal observations (WGS analysis workflow)",
+)
+OPTION_CANCER_SV_TUMOR_OBSERVATIONS = click.option(
+    "--cancer-sv-tumor-observations",
+    type=click.Path(exists=True),
+    required=False,
+    help="VCF path of cancer SV tumor observations (WGS analysis workflow)",
+)
