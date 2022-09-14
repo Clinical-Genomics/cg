a2afbb0fe91c3957b13f4bcfa91eee77f9795382
Annick Renevey <47788523+rannick@users.noreply.github.com>
2022-09-14 16:37:05 +0200

    test

--- cg/models/cg_config.py
+++ cg/models/cg_config.py
@@ -138,7 +138,7 @@ class MipConfig(BaseModel):
     script: str
 
 
-class RnafusionConfig(BaseModel):
+class RnafusionConfig(CommonAppConfig):
     root: str
     references: str
     binary_path: str
@@ -264,7 +264,7 @@ class CGConfig(BaseModel):
     mip_rd_dna: MipConfig = Field(None, alias="mip-rd-dna")
     mip_rd_rna: MipConfig = Field(None, alias="mip-rd-rna")
     mutant: MutantConfig = None
-    rnafusion: RnafusionConfig
+    rnafusion: RnafusionConfig = None
 
     # These are meta APIs that gets instantiated in the code
     meta_apis: dict = {}
