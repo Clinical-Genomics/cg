7ed085bd8775c59b668f141744123788b33adff1
Eva Caceres <eva.caceres@scilifelab.se>
2022-09-23 17:16:41 +0200

    Added dry-run and yes options

--- cg/cli/delete/observations.py
+++ cg/cli/delete/observations.py
@@ -4,6 +4,7 @@ import click
 from cg.apps.loqus import LoqusdbAPI
 from cg.models.cg_config import CGConfig
 from cg.store import Store, models
+from cg.constants.constants import DRY_RUN, SKIP_CONFIRMATION
 
 LOG = logging.getLogger(__name__)
 
@@ -16,18 +17,26 @@ def delete_cmd():
 
 @delete_cmd.command()
 @click.option("-c", "--case_id", help="internal case id")
+@DRY_RUN
+@SKIP_CONFIRMATION
 @click.pass_obj
-def observations(context: CGConfig, case_id: str):
+def observations(context: CGConfig, case_id: str, dry_run: bool, yes: bool):
     """Delete case from LoqusDB and reset loqusdb_id in statusDB."""
 
     status_db: Store = context.status_db
     loqusdb_api: LoqusdbAPI = context.loqusdb_api
 
-    LOG.info("This will delete all variants in LoqusDB for case: %s", case_id)
-    click.confirm("Do you want to continue?", abort=True)
-    LOG.info("Delete variants from LoqusDB for case: %s", case_id)
-    loqusdb_api.delete_case(case_id=case_id)
+    if dry_run:
+        LOG.info("DRY-RUN MODE. Variants will not be deleted.")
+        LOG.info("This would delete all variants in LoqusDB for case: %s", case_id)
 
-    status_db.reset_loqusdb_observation_ids(case_id)
-    LOG.info("Reset loqus_id in StatusDB for: %s", case_id)
-    status_db.commit()
+    else:
+        LOG.info("This will delete all variants in LoqusDB for case: %s", case_id)
+
+        if yes or click.confirm("Do you want to continue?", abort=True):
+            LOG.info("Delete variants from LoqusDB for case: %s", case_id)
+            loqusdb_api.delete_case(case_id=case_id)
+
+            LOG.info("Reset loqus_id in StatusDB for: %s", case_id)
+            status_db.reset_loqusdb_observation_ids(case_id)
+            status_db.commit()
