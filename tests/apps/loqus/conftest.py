0a2613627e195e464e8e5ac9eddbe29fe0a8aaf4
Eva Caceres <eva.caceres@scilifelab.se>
2022-09-28 16:15:39 +0200

    Exclude username from output fixture

--- tests/apps/loqus/conftest.py
+++ tests/apps/loqus/conftest.py
@@ -183,13 +183,13 @@ def fixture_loqusdb_delete_stderr() -> bytes:
     """
     Loqusdb STDERR for a successful delete.
     """
-    return b"""2022-09-22 12:30:07 evacmbp loqusdb.commands.cli[20689] INFO Running loqusdb version 2.6.9
-2022-09-22 12:30:07 evacmbp mongo_adapter.client[20689] INFO Connecting to uri:mongodb://None:None@localhost:27017
-2022-09-22 12:30:07 evacmbp mongo_adapter.client[20689] INFO Connection established
-2022-09-22 12:30:07 evacmbp mongo_adapter.adapter[20689] INFO Use database loqusdb
-2022-09-22 12:30:07 evacmbp loqusdb.plugins.mongo.case[20689] INFO Removing case yellowhog from database
-2022-09-22 12:30:07 evacmbp loqusdb.utils.delete[20689] INFO deleting variants
-2022-09-22 12:30:07 evacmbp loqusdb.utils.delete[20689] INFO Start deleting chromosome 1"""
+    return b"""2022-09-22 12:30:07 usernam loqusdb.commands.cli[20689] INFO Running loqusdb version 2.6.9
+2022-09-22 12:30:07 usernam mongo_adapter.client[20689] INFO Connecting to uri:mongodb://None:None@localhost:27017
+2022-09-22 12:30:07 usernam mongo_adapter.client[20689] INFO Connection established
+2022-09-22 12:30:07 usernam mongo_adapter.adapter[20689] INFO Use database loqusdb
+2022-09-22 12:30:07 usernam loqusdb.plugins.mongo.case[20689] INFO Removing case yellowhog from database
+2022-09-22 12:30:07 usernam loqusdb.utils.delete[20689] INFO deleting variants
+2022-09-22 12:30:07 usernam loqusdb.utils.delete[20689] INFO Start deleting chromosome 1"""
 
 
 @pytest.fixture(name="loqusdb_delete_nonexisting_stderr")
@@ -197,8 +197,8 @@ def fixture_loqusdb_delete_nonexisting_stderr() -> bytes:
     """
     Loqusdb delete STDERR for non existing case.
     """
-    return b"""2022-09-22 11:40:04 evacmbp loqusdb.commands.cli[19944] INFO Running loqusdb version 2.6.9
-2022-09-22 11:40:04 evacmbp mongo_adapter.client[19944] INFO Connecting to uri:mongodb://None:None@localhost:27017
-2022-09-22 11:40:04 evacmbp mongo_adapter.client[19944] INFO Connection established
-2022-09-22 11:40:04 evacmbp mongo_adapter.adapter[19944] INFO Use database loqusdb
-2022-09-22 11:40:04 evacmbp loqusdb.commands.delete[19944] WARNING Case yellowhog does not exist in database"""
+    return b"""2022-09-22 11:40:04 usernam loqusdb.commands.cli[19944] INFO Running loqusdb version 2.6.9
+2022-09-22 11:40:04 usernam mongo_adapter.client[19944] INFO Connecting to uri:mongodb://None:None@localhost:27017
+2022-09-22 11:40:04 usernam mongo_adapter.client[19944] INFO Connection established
+2022-09-22 11:40:04 usernam mongo_adapter.adapter[19944] INFO Use database loqusdb
+2022-09-22 11:40:04 usernam loqusdb.commands.delete[19944] WARNING Case yellowhog does not exist in database"""
