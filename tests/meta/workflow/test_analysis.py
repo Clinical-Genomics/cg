dcba38e751e2219f4ca9e1080f47e2105d8554ec
peterpru <peter.pruisscher@scilifelab.se>
2022-10-25 11:43:36 +0200

    add test for correct gene panel list

--- tests/meta/workflow/test_analysis.py
+++ tests/meta/workflow/test_analysis.py
@@ -1,10 +1,12 @@
 """Test for analysis"""
+from typing import List
 
 import pytest
 
-from cg.constants import Priority
+from cg.constants import Priority, GenePanelMasterList
 from cg.constants.priority import SlurmQos
 from cg.meta.workflow.analysis import AnalysisAPI
+from cg.meta.workflow.mip import MipAnalysisAPI
 
 
 @pytest.mark.parametrize(
@@ -29,3 +31,25 @@ def test_get_slurm_qos_for_case(mocker, case_id: str, priority, expected_slurm_q
 
     # THEN the expected slurm QOS should be returned
     assert slurm_qos is expected_slurm_qos
+
+
+def test_gene_panels_correctly_added():
+    """Test get correct gene panel list"""
+
+    # GIVEN a case that has a gene panel included in the master list:
+    default_panels_included: List[str] = [GenePanelMasterList.get_panel_names()[0]]
+    master_list = GenePanelMasterList.get_panel_names()
+    # WHEN converting the gene panels between the default and the gene_panel_master_list:
+    list_of_gene_panels_used = MipAnalysisAPI.convert_panels("cust002", default_panels_included)
+
+    # THEN the list_of_gene_panels_used should return all gene panels:
+    assert set(list_of_gene_panels_used) == set(master_list)
+
+    # GIVEN a case that has a gene panel that is NOT included in the master list:
+    default_panels_not_included: List[str] = ["ANOTHER_PANEL"]
+
+    # WHEN converting the gene panels between the default and the gene_panel_master_list:
+    list_of_gene_panels_used = MipAnalysisAPI.convert_panels("cust002", default_panels_not_included)
+
+    # THEN the list_of_gene_panels_used should return the custom panel and OMIM-AUTO:
+    assert set(list_of_gene_panels_used) == set(default_panels_not_included + ["OMIM-AUTO"])
