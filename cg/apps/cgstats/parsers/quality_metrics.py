e9d6c2b638ccf36c8dbd9c63319a78c7688ff7ee
GustavLagneborgScilifelab <107848458+GustavLagneborgScilifelab@users.noreply.github.com>
2022-11-01 08:16:51 +0100

    fixing parsing metrics that was moved during software dragen update

---
+++ cg/apps/cgstats/parsers/quality_metrics.py
@@ -0,0 +1,31 @@
+"""Parse statistics from the dragen quality metrics file"""
+import csv
+import logging
+from pathlib import Path
+from typing import Dict
+
+LOG = logging.getLogger(__name__)
+
+class QualityMetrics:
+    def __init__(self, quality_metrics_path: Path):
+        self.quality_metrics_path = quality_metrics_path
+        self.quality_metrics = self.quality_metrics_file()
+
+    def quality_metrics_file(
+        self,
+
+    )-> Dict[int, dict]:
+        """Parse the Dragen quality metrics file"""
+        LOG.info("Parsing Dragen demultiplexing quality metrics file %s", self.quality_metrics_file)
+
+        parsed_metrics = {}
+
+        with open(self.quality_metrics_path, mode="r") as metrics_file:
+            metrics_reader = csv.DictReader(metrics_file)
+            for row in metrics_reader:
+                lane = int(row["Lane"])
+                sample_id = row["Sample_ID"]
+                parsed_metrics[lane] = parsed_metrics.get(lane, {})
+                parsed_metrics[lane][sample_id] = row
+
+        return parsed_metrics
