9dfcef840d408a48133925ecdd8f8709c8d842a5
Henrik Stranneheim <henrik.stranneheim@scilifelab.se>
2022-11-22 08:53:39 +0100

    Check for new demux (#1621) (major)
    ### Added
    
    - New command to post-process Hiseq X flow cells
    - Add cg transfer flow cell call to post processing of Novaseq and Hiseq X flow cells
    - Refactor flowcell to flow_cell
    
    ### Changed
    
    - Use cg transfer cmd directly from cg for Novaseq flow cells

--- cg/cli/demultiplex/base.py
+++ cg/cli/demultiplex/base.py
@@ -1,8 +1,9 @@
+"""Module for demultiplexing CLI."""
 import logging
 
 import click
-from cg.cli.demultiplex.add import add_flowcell_cmd, select_project_cmd
-from cg.cli.demultiplex.demux import demultiplex_all, demultiplex_flowcell, delete_flow_cell
+from cg.cli.demultiplex.add import add_flow_cell_cmd, select_project_cmd
+from cg.cli.demultiplex.demux import demultiplex_all, demultiplex_flow_cell, delete_flow_cell
 from cg.cli.demultiplex.finish import finish_group
 from cg.cli.demultiplex.report import create_report_cmd
 from cg.cli.demultiplex.sample_sheet import sample_sheet_commands
@@ -12,16 +13,19 @@ LOG = logging.getLogger(__name__)
 
 @click.group(name="demultiplex")
 def demultiplex_cmd_group():
-    """Command group for the demultiplex commands"""
-    LOG.info("Running CG demultiplex")
+    """Command group for the demultiplex CLI."""
+    LOG.info("Running cg demultiplex.")
 
 
 demultiplex_cmd_group: click.Group
-demultiplex_cmd_group.add_command(demultiplex_flowcell)
-demultiplex_cmd_group.add_command(demultiplex_all)
-demultiplex_cmd_group.add_command(sample_sheet_commands)
-demultiplex_cmd_group.add_command(add_flowcell_cmd)
-demultiplex_cmd_group.add_command(select_project_cmd)
-demultiplex_cmd_group.add_command(finish_group)
-demultiplex_cmd_group.add_command(create_report_cmd)
-demultiplex_cmd_group.add_command(delete_flow_cell)
+for sub_cmd in [
+    add_flow_cell_cmd,
+    create_report_cmd,
+    delete_flow_cell,
+    demultiplex_flow_cell,
+    demultiplex_all,
+    finish_group,
+    sample_sheet_commands,
+    select_project_cmd,
+]:
+    demultiplex_cmd_group.add_command(sub_cmd)
