a4b781fbb5b02922ca618d1f9a67809d55e5766a
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2022-12-01 10:41:14 +0100

    feat(refactor): Add test

--- cg/store/status_flow_cell_filters.py
+++ cg/store/status_flow_cell_filters.py
@@ -5,12 +5,14 @@ from alchy import Query
 from cg.store.models import Flowcell
 
 
-def filter_flow_cell_has_id(flow_cells: Query, flow_cell_id: str, **kwargs) -> Query:
+def filter_flow_cell_has_id(flow_cells: Query, flow_cell_id: str, **kwargs) -> Flowcell:
     """Extracts flow cell by flow cell id."""
     return flow_cells.filter(Flowcell.name == flow_cell_id).first()
 
 
-def apply_flow_cell_filter(function: str, flow_cells: Query, flow_cell_id: Optional[str] = None):
+def apply_flow_cell_filter(
+    function: str, flow_cells: Query, flow_cell_id: Optional[str] = None
+) -> Flowcell:
     """Apply filtering functions and return filtered results."""
     filter_map = {
         "flow_cell_has_id": filter_flow_cell_has_id,
