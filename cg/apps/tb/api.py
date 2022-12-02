9dca78b32cbe4ac19a937cedac36a75360022fe3
GustavLagneborgScilifelab <107848458+GustavLagneborgScilifelab@users.noreply.github.com>
2022-12-02 10:14:54 +0100

    updating api adress

--- cg/apps/tb/api.py
+++ cg/apps/tb/api.py
@@ -173,4 +173,4 @@ class TrailblazerAPI:
 
         LOG.debug(f"Setting analysis uploaded at for {request_body}")
         LOG.info(f"{case_id} - uploaded at set to {uploaded_at}")
-        self.query_trailblazer(command="set_analysis_uploaded", request_body=request_body)
+        self.query_trailblazer(command="set-analysis-uploaded", request_body=request_body)
