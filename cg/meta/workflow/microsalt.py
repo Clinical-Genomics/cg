4f68abc2df997759bb13a99f0a57fbca6d5e9c78
GustavLagneborgScilifelab <107848458+GustavLagneborgScilifelab@users.noreply.github.com>
2023-01-12 14:21:30 +0100

    black

--- cg/meta/workflow/microsalt.py
+++ cg/meta/workflow/microsalt.py
@@ -85,7 +85,11 @@ class MicrosaltAnalysisAPI(AnalysisAPI):
         )
 
         return next(
-            (path for path in self.get_case_path(case_id=case_id) if lims_project + "_" in str(path)),
+            (
+                path
+                for path in self.get_case_path(case_id=case_id)
+                if lims_project + "_" in str(path)
+            ),
             None,
         )
 
