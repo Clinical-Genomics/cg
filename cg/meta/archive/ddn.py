a5d5e8ace8054722675dab1946c0079ffa8b1723
Vincent Janvid <vincent.janvid@scilifelab.se>
2023-02-09 11:29:47 +0100

    Added json construction

--- cg/meta/archive/ddn.py
+++ cg/meta/archive/ddn.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Dict
 from urllib.parse import urljoin
 
 from cg.constants.constants import APIMethods, FileFormat
@@ -6,9 +7,11 @@ from cg.io.controller import APIRequest, ReadStream
 
 
 class DDNApi:
-    def __int__(self, config: Path):
+    def __init__(self, config: Path):
         self.config = config
         self.url: str = "some/url"
+        self.archive_repo = "archive_name"
+        self.source_repo = "probably the HPC?"
 
     @property
     def auth_header(self) -> dict:
@@ -29,18 +32,35 @@ class DDNApi:
         return {"Authorization": f"Bearer {auth_token}"}
 
     # Todo add json construction logic here
-    def archive_folder(self, folder: Path):
+    def archive_folder(self, destination: Path, source: Path):
+        payload: dict = {
+            "pathInfo": [self._construct_path_dict(destination=destination, source=source)],
+            "osType": "linux",
+            "createDirectory": "true",
+            "metadataList": [{}],
+        }
         response = APIRequest.api_request_from_content(
             api_method=APIMethods.POST,
             url=urljoin(base=self.url, url="files/archive"),
             headers=self.auth_header,
-            json={},
+            json=payload,
         )
 
-    def retrieve_folder(self, folder: Path):
+    def retrieve_folder(self, destination: Path, source: Path):
+        payload: dict = {
+            "pathInfo": [self._construct_path_dict(destination=destination, source=source)],
+            "osType": "linux",
+            "createDirectory": "true",
+        }
         response = APIRequest.api_request_from_content(
-            api_method=APIMethods.GET,
+            api_method=APIMethods.POST,
             url=urljoin(base=self.url, url="files/retrieve"),
             headers=self.auth_header,
-            json={},
+            json=payload,
         )
+
+    def _construct_path_dict(self, source, destination) -> Dict[str, str]:
+        return {
+            "source": "archive@OneOne:/Test./ada_event",
+            "destination": "nfs@Nas_1:/Miria/Binary/Adm",
+        }
