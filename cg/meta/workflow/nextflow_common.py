4bbca808a80fd588f8cef90cac2130cc39190b3d
Eva Caceres <eva.caceres@scilifelab.se>
2023-03-16 10:58:09 +0100

    Capture stdout and stderr using proceess for both tower and nextflow

--- cg/meta/workflow/nextflow_common.py
+++ cg/meta/workflow/nextflow_common.py
@@ -163,12 +163,7 @@ class NextflowAnalysisAPI:
             ),
             exclude_true=True,
         )
-        parameters = (
-            nextflow_options
-            + ["run", pipeline_path]
-            + run_options
-            + NextflowAnalysisAPI.get_nextflow_stdout_stderr(case_id=case_id, root_dir=root_dir)
-        )
+        parameters = nextflow_options + ["run", pipeline_path] + run_options
         return parameters
 
     @classmethod
