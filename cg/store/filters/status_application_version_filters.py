632c591196170ef2c613c8934753f1bf2e84f2c2
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-03-22 09:26:55 +0100

    Complete app_version filter script

--- cg/store/filters/status_application_version_filters.py
+++ cg/store/filters/status_application_version_filters.py
@@ -8,30 +8,43 @@ from cg.store.models import Application, ApplicationVersion
 def filter_application_version_by_application(
     application_versions: Query, application: Application
 ) -> Query:
-    """Return the application versions of a given application."""
+    """Return the application versions given an application."""
     return application_versions.filter(ApplicationVersion.application == application)
 
 
 def filter_application_version_by_application_id(
     application_versions: Query, application_id: int
 ) -> Query:
-    """Return the application versions of a given application id."""
+    """Return the application versions given an application id."""
     return application_versions.filter(ApplicationVersion.application_id == application_id)
 
 
 def filter_application_version_by_version(application_versions: Query, version: int) -> Query:
-    """Return the application versions of a given version."""
+    """Return the application versions given a version."""
     return application_versions.filter(ApplicationVersion.version == version)
 
 
 def apply_application_version_filter(
     filter_functions: List[Callable],
+    application_versions: Query,
+    application_id: int = None,
+    application: Application = None,
+    version: int = None,
 ) -> Query:
     """Apply filtering functions to the sample queries and return filtered results."""
-    pass
+    for filter_function in filter_functions:
+        application_versions: Query = filter_function(
+            application_versions=application_versions,
+            application_id=application_id,
+            applicatiomn=application,
+            version=version,
+        )
+    return application_versions
 
 
 class ApplicationVersionFilter(Enum):
     """Define Application Version filter functions."""
 
-    pass
+    FILTER_BY_APPLICATION = filter_application_version_by_application
+    FILTER_BY_APPLICATION_ID = filter_application_version_by_application_id
+    FILTER_BY_VERSION = filter_application_version_by_version
