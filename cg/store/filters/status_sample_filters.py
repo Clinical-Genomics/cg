7c5c4dfaf5f3f27e583ff9d322958f80f6fe8267
ChristianOertlin <christian.oertlin@scilifelab.se>
2023-03-23 11:44:28 +0100

    refactor(sample add filters and tests) (#1835)
    ## Description
    Adding filters and filter tests for the sample model

--- cg/store/filters/status_sample_filters.py
+++ cg/store/filters/status_sample_filters.py
@@ -3,7 +3,7 @@ from enum import Enum
 from sqlalchemy.orm import Query
 
 from cg.constants.constants import SampleType
-from cg.store.models import Sample
+from cg.store.models import Sample, Customer
 
 
 def filter_samples_by_internal_id(internal_id: str, samples: Query, **kwargs) -> Query:
@@ -87,9 +87,9 @@ def filter_samples_do_not_invoice(samples: Query, **kwargs) -> Query:
     return samples.filter(Sample.no_invoice.is_(True))
 
 
-def filter_samples_by_customer_id(samples: Query, customer_id: str, **kwargs) -> Query:
+def filter_samples_by_customer_id(samples: Query, customer_ids: List[int], **kwargs) -> Query:
     """Return samples by customer id."""
-    return samples.filter(Sample.customer_id.in_(customer_id))
+    return samples.filter(Sample.customer_id.in_(customer_ids))
 
 
 def filter_samples_by_customer_name(samples: Query, customer_name: str, **kwargs) -> Query:
@@ -132,6 +132,17 @@ def filter_samples_is_not_tumour(samples: Query, **kwargs) -> Query:
     return samples.filter(Sample.is_tumour.is_(False))
 
 
+def filter_samples_by_name_pattern(samples: Query, name_pattern: str, **kwargs) -> Query:
+    """Return samples matching the name pattern."""
+    filtered_samples = samples.filter(Sample.name.like(f"%{name_pattern}%"))
+    return filtered_samples if filtered_samples.all() else samples
+
+
+def filter_samples_by_customer(samples: Query, customer: Customer, **kwargs) -> Query:
+    """Return samples by customer."""
+    return samples.filter(Sample.customer == customer)
+
+
 def apply_sample_filter(
     filter_functions: List[Callable],
     samples: Query,
@@ -140,23 +151,27 @@ def apply_sample_filter(
     tissue_type: Optional[SampleType] = None,
     data_analysis: Optional[str] = None,
     invoice_id: Optional[int] = None,
-    customer_id: Optional[str] = None,
+    customer_ids: Optional[List[int]] = None,
     subject_id: Optional[str] = None,
     name: Optional[str] = None,
+    customer: Optional[Customer] = None,
+    name_pattern: Optional[str] = None,
 ) -> Query:
     """Apply filtering functions to the sample queries and return filtered results."""
 
-    for function in filter_functions:
-        samples: Query = function(
+    for filter_function in filter_functions:
+        samples: Query = filter_function(
             samples=samples,
             entry_id=entry_id,
             internal_id=internal_id,
             tissue_type=tissue_type,
             data_analysis=data_analysis,
             invoice_id=invoice_id,
-            customer_id=customer_id,
+            customer_ids=customer_ids,
             subject_id=subject_id,
             name=name,
+            customer=customer,
+            name_pattern=name_pattern,
         )
     return samples
 
@@ -189,3 +204,5 @@ class SampleFilter(Enum):
     FILTER_BY_SUBJECT_ID: Callable = filter_samples_by_subject_id
     FILTER_IS_TUMOUR: Callable = filter_samples_is_tumour
     FILTER_IS_NOT_TUMOUR: Callable = filter_samples_is_not_tumour
+    FILTER_BY_NAME_PATTERN: Callable = filter_samples_by_name_pattern
+    FILTER_BY_CUSTOMER: Callable = filter_samples_by_customer
