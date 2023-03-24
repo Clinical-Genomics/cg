c00e11b3481944810b1e3fac560ec166f16a9a61
Christian Oertlin <christian@n153-p159.eduroam.kth.se>
2023-03-24 12:43:44 +0100

    fix function call

--- cg/server/invoices/views.py
+++ cg/server/invoices/views.py
@@ -141,12 +141,12 @@ def new(record_type):
     if record_type == "Sample":
         records: List[Pool or Sample] = db.get_samples_to_invoice_for_customer(customer=customer)
         customers_to_invoice: List[Customer] = db.get_customers_to_invoice(
-            records=db.get_samples_to_invoice()
+            records=db.get_samples_to_invoice_query()
         )
     elif record_type == "Pool":
         records: List[Pool or Sample] = db.get_pools_to_invoice_for_customer(customer=customer)
         customers_to_invoice: List[Customer] = db.get_customers_to_invoice(
-            records=db.get_pools_to_invoice()
+            records=db.get_pools_to_invoice_query()
         )
     return render_template(
         "invoices/new.html",
