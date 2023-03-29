f3d53edb9b33a10794f263a4a1692d4abc6fce63
seallard <sebastian.allard@scilifelab.se>
2023-03-29 16:38:05 +0200

    Remove remaining test

--- tests/store/api/add/test_store_add_base.py
+++ tests/store/api/add/test_store_add_base.py
@@ -4,19 +4,6 @@ from cg.store import Store
 from cg.store.models import Customer
 
 
-def test_add_collaboration(store: Store):
-    # GIVEN an empty database
-    assert store.Collaboration.query.first() is None
-    internal_id, name = "cust_group", "Test customer group"
-
-    # WHEN adding a new customer group
-    new_collaboration = store.add_collaboration(internal_id=internal_id, name=name)
-    store.add_commit(new_collaboration)
-
-    # THEN it should be stored in the database
-    assert store.Collaboration.query.first() == new_collaboration
-
-
 def test_add_user(store: Store):
     # GIVEN a database with a customer in it that we can connect the user to
     customer = store.add_customer(
