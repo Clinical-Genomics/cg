4c3ff8aff3995c162e14e326133eb93f18a1bbd9
Christian Oertlin <c.oertlin@gmail.com>
2023-04-03 14:34:04 +0200

    test

--- cg/store/api/find_business_data.py
+++ cg/store/api/find_business_data.py
@@ -594,8 +594,6 @@ class FindBusinessDataHandler(BaseHandler):
         customer_entry_ids: List[int] = []
         filter_functions: List[SampleFilter] = []
         if customers:
-            if not isinstance(customers, list):
-                customers = [customers]
             customer_entry_ids = [customer.id for customer in customers]
             filter_functions.append(SampleFilter.FILTER_BY_CUSTOMER_ENTRY_IDS)
         if pattern:
