35118e1c78d3495a1b69af8a132eeec55dbec008
ChristianOertlin <christian.oertlin@scilifelab.se>
2023-04-03 09:53:30 +0200

    Apply suggestions from code review
    Co-authored-by: Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>

--- cg/utils/dispatcher.py
+++ cg/utils/dispatcher.py
@@ -5,7 +5,7 @@ class Dispatcher:
     def __init__(self, functions: List[Callable], input_dict: Dict[str, Any]):
         self.functions: List[Callable] = functions
         self.input_dict: Dict[str, any] = input_dict
-        self.dispatch_table = self._generate_dispatch_table()
+        self.dispatch_table: Dict[Tuple[str], Callable] = self._generate_dispatch_table()
 
     def __call__(self, input_dict: Dict[str, Any]):
         key, values = self._parse_input_dict(input_dict)
@@ -13,7 +13,7 @@ class Dispatcher:
             raise ValueError(f"No matching function found for arguments: {input_dict.keys()}")
         return self.dispatch_table[key](*values)
 
-    def _parse_input_dict(self, input_dict: Dict[str, Any]) -> [Tuple[str], Tuple[Any]]:
+    def _parse_input_dict(self, input_dict: Dict[str, Any]) -> Tuple[str], Tuple[Any]:
         parameters_not_none: List[str] = []
         values_not_none: List[Any] = []
         for parameter in input_dict.keys():
@@ -23,7 +23,7 @@ class Dispatcher:
                 values_not_none.append(value)
         return tuple(parameters_not_none), tuple(values_not_none)
 
-    def _generate_dispatch_table(self):
+    def _generate_dispatch_table(self) -> Dict[Tuple[str], Callable]:
         dispatch_table = {}
         for func in self.functions:
             func_arg_names = func.__code__.co_varnames[: func.__code__.co_argcount]
