bb90220f627d32ca291aa079ef432b7852063bc5
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-04-04 17:09:34 +0200

    Update cg/meta/orders/case_submitter.py
    Co-authored-by: ChristianOertlin <christian.oertlin@scilifelab.se>

--- cg/meta/orders/case_submitter.py
+++ cg/meta/orders/case_submitter.py
@@ -345,7 +345,7 @@ class CaseSubmitter(Submitter):
         sample_obj.customer = customer_obj
         with self.status.session.no_autoflush:
             application_tag = sample["application"]
-            sample_obj.application_version = self.status.get_current_application_version_by_tag(
+            sample_obj.application_version: ApplicationVersion = self.status.get_current_application_version_by_tag(
                 tag=application_tag
             )
         self.status.add(sample_obj)
