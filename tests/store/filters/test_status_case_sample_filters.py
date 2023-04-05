20db72b561ed6aef70ef2eb7742a0a6bd8a7a3fc
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-04-05 17:25:30 +0200

    Add new test function and rename parameters

--- tests/store/filters/test_status_case_sample_filters.py
+++ tests/store/filters/test_status_case_sample_filters.py
@@ -14,30 +14,32 @@ from tests.store_helpers import StoreHelpers
 
 
 def test_get_samples_associated_with_case_valid_id(store_with_analyses_for_cases: Store):
-    """Test that the case id of the filtered case_sample is the correct one."""
-    # GIVEN a store with case_samples
+    """Test that the case id of the filtered case-sample is the correct one."""
+    # GIVEN a store with case-samples
     case_sample_query: Query = store_with_analyses_for_cases._get_join_case_sample_query()
     assert case_sample_query.count() > 0
     # GIVEN a case_id from the store
-    case_id: str = case_sample_query.first().family_id
-    assert case_id
+    case_internal_id: str = case_sample_query.first().family.internal_id
+    assert case_internal_id
 
     # WHEN filtering by the chosen case id
     filtered_query: Query = get_samples_associated_with_case(
-        case_samples=case_sample_query, case_id=case_id
+        case_samples=case_sample_query, case_id=case_internal_id
     )
 
     # THEN the filtered query has fewer elements than the original query
     assert filtered_query.count() < case_sample_query.count()
     # THEN the case_samples in the query have the correct case id
-    assert all(case_sample.family_id == case_id for case_sample in filtered_query.all())
+    assert all(
+        case_sample.family.internal_id == case_internal_id for case_sample in filtered_query.all()
+    )
 
 
 def test_get_samples_associated_with_case_unexistent_id(
     store_with_analyses_for_cases: Store, case_id_does_not_exist: str
 ):
     """Test that an empty query is returned when filtering using an unexistent case id."""
-    # GIVEN a store with case samples
+    # GIVEN a store with case-samples
     case_sample_query: Query = store_with_analyses_for_cases._get_join_case_sample_query()
     assert case_sample_query.count() > 0
 
@@ -50,7 +52,31 @@ def test_get_samples_associated_with_case_unexistent_id(
     assert filtered_query.count() == 0
 
 
-def test_get_cases_associated_with_sample_by_entry_id():
+def test_get_cases_associated_with_sample_by_entry_id_valid_id(
+    store_with_analyses_for_cases: Store,
+):
+    """."""
+    # GIVEN a store with case-samples
+    case_sample_query: Query = store_with_analyses_for_cases._get_join_case_sample_query()
+    assert case_sample_query.count() > 0
+    # GIVEN one sample entry id in the store
+    sample_entry_id: int = case_sample_query.first().sample_id
+    assert sample_entry_id
+
+    # WHEN filtering the query by the chosen sample entry id
+    filtered_query: Query = get_cases_associated_with_sample_by_entry_id(
+        case_samples=case_sample_query, sample_entry_id=sample_entry_id
+    )
+
+    # THEN the filtered query has fewer elements than the original query
+    assert filtered_query.count() < case_sample_query.count()
+    # THEN the case_samples in the query have the correct case id
+    assert all(case_sample.sample_id == sample_entry_id for case_sample in filtered_query.all())
+
+
+def test_get_cases_associated_with_sample_by_entry_id_invalid_id(
+    store_with_analyses_for_cases: Store,
+):
     """."""
     # GIVEN
 
