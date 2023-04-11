83d0b9fbb128db194ad2d272976ebc799388160c
Henrik Stranneheim <henrik.stranneheim@scilifelab.se>
2023-04-11 15:08:45 +0200

    Update tests/store/api/delete/test_store_api_delete.py
    Co-authored-by: Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>

--- tests/store/api/delete/test_store_api_delete.py
+++ tests/store/api/delete/test_store_api_delete.py
@@ -4,7 +4,8 @@ from cg.store.models import Flowcell, Family, FamilySample, Sample
 
 
 def test_delete_flow_cell(flow_cell_id: str, populated_flow_cell_store: Store):
-    """Test deleting a flow cell in Store"""
+    """Test deleting a flow cell in Store."""
+
 
     # GIVEN a database containing a flow cell
     flow_cell: Flowcell = populated_flow_cell_store.get_flow_cell(flow_cell_id=flow_cell_id)
