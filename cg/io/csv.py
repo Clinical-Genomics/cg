e842cc5074aff109cbfba51a837312e30cdcc469
ChristianOertlin <christian.oertlin@scilifelab.se>
2023-04-12 13:13:10 +0200

    Apply suggestions from code review
    Co-authored-by: Henrik Stranneheim <henrik.stranneheim@scilifelab.se>

--- cg/io/csv.py
+++ cg/io/csv.py
@@ -1,4 +1,4 @@
-"""Module for reading and writing csv files."""
+"""Module for reading and writing comma separated values (CSV) formatted files."""
 import csv
 import io
 from typing import Any, List, Tuple
@@ -6,20 +6,20 @@ from pathlib import Path
 
 
 def read_csv(file_path: Path) -> List[List[str]]:
-    """Read content in a csv file."""
+    """Read content in a CSV file."""
     with open(file_path, "r") as file:
         csv_reader = csv.reader(file)
         return [row for row in csv_reader]
 
 
 def read_csv_stream(stream: str) -> List[List[str]]:
-    """Read csv formatted stream."""
+    """Read CSV formatted stream."""
     csv_reader = csv.reader(stream.splitlines())
     return [row for row in csv_reader]
 
 
 def write_csv(content: List[List[Any]], file_path: Path) -> None:
-    """Write content to a csv file."""
+    """Write content to a CSV file."""
     with open(file_path, "w", newline="") as file:
         csv_writer = csv.writer(file)
         for row in content:
@@ -27,7 +27,7 @@ def write_csv(content: List[List[Any]], file_path: Path) -> None:
 
 
 def write_csv_stream(content: List[List[Any]]) -> str:
-    """Write content to a csv stream."""
+    """Write content to a CSV stream."""
     csv_stream = io.StringIO()
     csv_writer = csv.writer(csv_stream, lineterminator="\n")
     for row in content:
