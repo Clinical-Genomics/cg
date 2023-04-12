3ccff18bf6b2371a56707c405deae43f9ea42017
Vincent Janvid <vincent.janvid@scilifelab.se>
2023-04-12 15:15:28 +0200

    Finished alembic script

--- alembic/versions/df1b3dd317d0_add_trusted_customer_field.py
+++ alembic/versions/df1b3dd317d0_add_trusted_customer_field.py
@@ -10,10 +10,10 @@ Create Date: 2023-04-12 11:13:37.585551
 from alembic import op
 import sqlalchemy as sa
 
-from sqlalchemy.dialects import mysql
 from sqlalchemy import Column, types, orm
+from sqlalchemy.engine import Connection
 from sqlalchemy.ext.declarative import declarative_base
-
+from sqlalchemy.orm import Session
 
 # revision identifiers, used by Alembic.
 revision = "df1b3dd317d0"
@@ -28,12 +28,26 @@ class Customer(Base):
     __tablename__ = "customer"
     id = Column(types.Integer, primary_key=True)
     internal_id = Column(types.String(32), unique=True, nullable=False)
-    name = Column(types.String(32), unique=False, nullable=False)
+    is_trusted = Column(type_=types.Boolean)
+
+
+TRUSTED_CUSTOMERS = ["cust144"]
 
 
 def upgrade():
-    op.add_column(table_name="customer", column=Column())
+    op.add_column(table_name="customer", column=Column(name="is_trusted", type_=types.Boolean))
+    bind: Connection = op.get_bind()
+    session: Session = sa.orm.Session(bind=bind)
+    for customer in session.query(Customer):
+        if customer.internal_id in TRUSTED_CUSTOMERS:
+            customer.is_trusted = True
+            continue
+        customer.is_trusted = False
+    session.commit()
+    op.alter_column(
+        table_name="customer", column_name="is_trusted", nullable=False, existing_type=types.Boolean
+    )
 
 
 def downgrade():
-    pass
+    op.drop_column(table_name="customer", column_name="is_trusted")
