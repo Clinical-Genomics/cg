dd4dd6e37a47159c964494938494f340f73ae7d3
seallard <sebastian.allard@scilifelab.se>
2023-04-13 09:29:10 +0200

    Fix naming

--- cg/server/api.py
+++ cg/server/api.py
@@ -35,7 +35,7 @@ BLUEPRINT = Blueprint("api", __name__, url_prefix="/api/v1")
 cache = TTLCache(maxsize=1, ttl=3600)
 
 
-def extract_certificate_expiration_in_seconds(response_data):
+def get_certificate_ttl(response_data):
     expires_header = response_data.headers.get("Expires")
     expires = datetime.strptime(expires_header, "%a, %d %b %Y %H:%M:%S %Z")
     ttl = int((expires - datetime.utcnow().replace(tzinfo=timezone.utc)).total_seconds())
@@ -43,30 +43,30 @@ def extract_certificate_expiration_in_seconds(response_data):
     return ttl
 
 
-def extract_certificate(response_data):
+def get_certificate(response_data):
     cert_key = next(iter(response_data))
     certificate = response_data[cert_key]
     return certificate
 
 
-def update_certificate_cache():
+def fetch_and_cache_google_oauth2_certificate():
     url = "https://www.googleapis.com/oauth2/v1/certs"
     response = requests.get(url)
     response.raise_for_status()
     data = response.json()
 
-    ttl = extract_certificate_expiration_in_seconds(response_data=data)
-    cert = extract_certificate(response_data=data)
+    ttl = get_certificate_ttl(response_data=data)
+    cert = get_certificate(response_data=data)
     cache.set("cert", cert, ttl=ttl)
 
     return data
 
 
-def get_certificate_from_cache():
+def get_google_oauth2_certificate():
     cert = cache.get("cert")
 
     if not cert:
-        cert = update_certificate_cache()
+        cert = fetch_and_cache_google_oauth2_certificate()
 
     return cert
 
@@ -107,7 +107,7 @@ def before_request():
 
     jwt_token = auth_header.split("Bearer ")[-1]
     try:
-        user_data = jwt.decode(jwt_token, certs=get_certificate_from_cache())
+        user_data = jwt.decode(jwt_token, certs=get_google_oauth2_certificate())
     except ValueError:
         return abort(
             make_response(
