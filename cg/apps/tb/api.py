73b0b46a7f00378ebadc805a8cc5cde72552f8c4
Eva Caceres <eva.caceres@scilifelab.se>
2023-04-17 14:32:10 +0200

    refactor use_tower by task_manager

--- cg/apps/tb/api.py
+++ cg/apps/tb/api.py
@@ -151,7 +151,7 @@ class TrailblazerAPI:
         email: str = None,
         data_analysis: Pipeline = None,
         ticket: str = None,
-        use_tower: bool = False,
+        task_manager: str = "NF_tower",
     ) -> TrailblazerAnalysis:
         request_body = {
             "case_id": case_id,
@@ -162,7 +162,7 @@ class TrailblazerAPI:
             "priority": slurm_quality_of_service,
             "data_analysis": str(data_analysis).upper(),
             "ticket": ticket,
-            "use_tower": use_tower,
+            "task_manager": task_manager,
         }
         LOG.debug("Submitting job to Trailblazer: %s", request_body)
         response = self.query_trailblazer(command="add-pending-analysis", request_body=request_body)
