f0e0b7b79018306db07434f560da839f4d668fc5
ChristianOertlin <christian.oertlin@scilifelab.se>
2023-04-17 16:39:51 +0200

    refactor function names (#1939) (patch)
    ## Description
    renaming of pre-fix for for flow cell filter functions from get to filter

--- tests/store/filters/test_status_flow_cell_filters.py
+++ tests/store/filters/test_status_flow_cell_filters.py
@@ -6,9 +6,9 @@ from cg.constants import FlowCellStatus
 from cg.store import Store
 from cg.store.models import Flowcell, Family, Sample
 from cg.store.filters.status_flow_cell_filters import (
-    get_flow_cell_by_id,
-    get_flow_cells_with_statuses,
-    get_flow_cells_by_case,
+    filter_flow_cell_by_id,
+    filter_flow_cells_with_statuses,
+    filter_flow_cells_by_case,
     filter_flow_cell_by_name_pattern,
 )
 from tests.store_helpers import StoreHelpers
@@ -30,7 +30,7 @@ def test_get_flow_cells_by_case(
     # GIVEN a flow cell Query
 
     # WHEN getting flow cell
-    returned_flow_cell: Optional[List[Flowcell]] = get_flow_cells_by_case(
+    returned_flow_cell: Optional[List[Flowcell]] = filter_flow_cells_by_case(
         flow_cells=base_store._get_join_flow_cell_sample_links_query(), case=case_obj
     )
 
@@ -54,7 +54,7 @@ def test_get_flow_cells_by_case_when_no_flow_cell_for_case(
     # GIVEN a flow cell Query
 
     # WHEN getting flow cell
-    returned_flow_cell: Optional[List[Flowcell]] = get_flow_cells_by_case(
+    returned_flow_cell: Optional[List[Flowcell]] = filter_flow_cells_by_case(
         flow_cells=base_store._get_join_flow_cell_sample_links_query(), case=case_obj
     )
 
@@ -71,7 +71,7 @@ def test_get_flow_cell_by_id(base_store: Store, helpers: StoreHelpers, flow_cell
     # GIVEN a flow cell Query
 
     # WHEN getting flow cell
-    returned_flow_cell: Flowcell = get_flow_cell_by_id(
+    returned_flow_cell: Flowcell = filter_flow_cell_by_id(
         flow_cells=base_store._get_query(table=Flowcell), flow_cell_id=flow_cell_id
     )
 
@@ -111,7 +111,7 @@ def test_get_flow_cells_with_statuses(base_store: Store, helpers: StoreHelpers, 
     # GIVEN a flow cell Query
 
     # WHEN getting flow cell
-    returned_flow_cell_query: Query = get_flow_cells_with_statuses(
+    returned_flow_cell_query: Query = filter_flow_cells_with_statuses(
         flow_cells=base_store._get_query(table=Flowcell),
         flow_cell_statuses=[FlowCellStatus.ON_DISK, FlowCellStatus.PROCESSING],
     )
