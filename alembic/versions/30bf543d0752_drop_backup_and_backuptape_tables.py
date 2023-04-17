0e1a2bc62627f31cb977d9e835bf46459ca3c416
seallard <sebastian.allard@scilifelab.se>
2023-04-17 12:47:52 +0200

    Remove unused backup models

---
+++ alembic/versions/30bf543d0752_drop_backup_and_backuptape_tables.py
@@ -0,0 +1,24 @@
+"""Drop Backup and Backuptape tables
+
+Revision ID: 30bf543d0752
+Revises: 554140bc13e4
+Create Date: 2023-04-17 12:43:32.128432
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '30bf543d0752'
+down_revision = '554140bc13e4'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
