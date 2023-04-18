23b6ccdd13aaefbc95826fde19641c0238c464ee
Vincent Janvid <vincent.janvid@scilifelab.se>
2023-04-18 15:49:46 +0200

    Added print to debug

--- cg/apps/lims/order.py
+++ cg/apps/lims/order.py
@@ -19,6 +19,7 @@ class OrderHandler:
         """Post the data to the server."""
         data = etree.tostring(document, xml_declaration=True)
         LOG.debug(data)
+        print(data)
         result = self.post(uri, data)
         return result
 
