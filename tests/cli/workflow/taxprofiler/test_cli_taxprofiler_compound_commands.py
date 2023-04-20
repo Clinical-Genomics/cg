41755f4fa62c87863e7be3e14355905272059279
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-04-20 10:56:32 +0200

    Remove test

--- tests/cli/workflow/taxprofiler/test_cli_taxprofiler_compound_commands.py
+++ tests/cli/workflow/taxprofiler/test_cli_taxprofiler_compound_commands.py
@@ -2,10 +2,6 @@ import logging
 
 from _pytest.logging import LogCaptureFixture
 from click.testing import CliRunner
-
-from cg.apps.hermes.hermes_api import HermesApi
-from cg.apps.hermes.models import CGDeliverables
-from cg.apps.housekeeper.hk import HousekeeperAPI
 from cg.cli.workflow.taxprofiler.base import (
     taxprofiler,
 )
@@ -15,7 +11,7 @@ from cg.models.cg_config import CGConfig
 
 
 def test_taxprofiler_no_args(cli_runner: CliRunner, taxprofiler_context: CGConfig):
-    """Test to see that running BALSAMIC without options prints help and doesn't result in an error."""
+    """Test to see that running Taxprofiler without options prints help and doesn't result in an error."""
     # GIVEN no arguments or options besides the command call
 
     # WHEN running command
@@ -25,5 +21,5 @@ def test_taxprofiler_no_args(cli_runner: CliRunner, taxprofiler_context: CGConfi
     print(result.output)
 
     assert result.exit_code == EXIT_SUCCESS
-    # THEN help should be printed
+    ## THEN help should be printed
     assert "help" in result.output
