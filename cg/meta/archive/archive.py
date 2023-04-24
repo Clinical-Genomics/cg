3558b039fca4896e0ca51588a44537b58ba8abc4
Vincent Janvid <vincent.janvid@scilifelab.se>
2023-04-24 10:35:51 +0200

    re-wrote archive_sample function

--- cg/meta/archive/archive.py
+++ cg/meta/archive/archive.py
@@ -11,14 +11,19 @@ class ArchiveAPI:
         self.housekeeper_api: HousekeeperAPI = None
         self.ddn_api: DDNDataFlowApi = None
 
-    def archive_sample(self, sample_id: str) -> None:
-        files_to_archive: List[Path] = self.housekeeper_api.get_file_paths(
-            bundle=sample_id, tags=[SequencingFileTag.SPRING]
-        )
+    def archive_samples(self, sample_ids: List[str]) -> None:
+        files_to_archive: List[Path] = []
+        for sample_id in sample_ids:
+            files_to_archive.extend(
+                self.housekeeper_api.get_file_paths(
+                    bundle=sample_id, tags=[SequencingFileTag.SPRING]
+                )
+            )
         task_id: int = self.ddn_api.archive_folders(
             {file: Path(sample_id) for file in files_to_archive}
         )
-        self.housekeeper_api.update_file_task_id(files_to_archive, task_id)
+        self.housekeeper_api.add_archive(files_to_archive, task_id)
+        self.housekeeper_api.set_file_archive
 
     def retrieve_sample(self) -> None:
         pass
