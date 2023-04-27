8ff1ed310aadc98f8e6b57915c8d3233b5b24e40
Eva Caceres <eva.caceres@scilifelab.se>
2023-04-27 16:46:49 +0200

    update metrics conditions for rnafusion

--- cg/constants/rnafusion.py
+++ cg/constants/rnafusion.py
@@ -6,7 +6,12 @@ from cg.constants.nextflow import NFX_SAMPLESHEET_HEADERS
 RNAFUSION_ACCEPTED_STRANDEDNESS = ["forward", "reverse", "unstranded"]
 RNAFUSION_STRANDEDNESS_HEADER = "strandedness"
 RNAFUSION_SAMPLESHEET_HEADERS = NFX_SAMPLESHEET_HEADERS + [RNAFUSION_STRANDEDNESS_HEADER]
-RNAFUSION_METRIC_CONDITIONS: dict = {"percent_duplicates": {"norm": "lt", "threshold": 40}}
+RNAFUSION_METRIC_CONDITIONS: dict = {
+    "uniquely_mapped_percent": {"norm": "gt", "threshold": 60},
+    "PCT_MRNA_BASES": {"norm": "gt", "threshold": 80},
+    "PCT_RIBOSOMAL_BASES": {"norm": "lt", "threshold": 5},
+    "PERCENT_DUPLICATION": {"norm": "lt", "threshold": 0.7},
+}
 
 
 class RnafusionDefaults:
