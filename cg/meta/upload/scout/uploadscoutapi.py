b19672220853178c22405f6ab96013e53b250f7b
peterpru <peter.pruisscher@scilifelab.se>
2023-04-28 10:36:50 +0200

    remove customer check when adding cases

--- cg/meta/upload/scout/uploadscoutapi.py
+++ cg/meta/upload/scout/uploadscoutapi.py
@@ -461,7 +461,6 @@ class UploadScoutAPI:
             if (
                 case_object.data_analysis
                 in [Pipeline.MIP_DNA, Pipeline.BALSAMIC, Pipeline.BALSAMIC_UMI]
-                and case_object.customer_id == rna_sample.customer_id
             ):
                 rna_dna_sample_case_map[rna_sample.internal_id][dna_sample.name].append(
                     case_object.internal_id
