c975891e70ccc3f855c22e3c64f565c2b52ad867
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-05-03 17:28:55 +0200

    feat(refactor):

--- tests/meta/backup/conftest.py
+++ tests/meta/backup/conftest.py
@@ -3,33 +3,27 @@ from pathlib import Path
 import pytest
 
 
-@pytest.fixture(name="spring_file_path")
-def fixture_spring_file_path() -> Path:
-    """Return spring file path"""
-    return Path("/path/to/spring_file.spring")
-
-
 @pytest.fixture(name="backup_file_path")
 def fixture_backup_file_path() -> str:
-    """Return path to a file used in the backup process"""
+    """Return path to a file used in the backup process."""
     return "/path/to/backup_file.extension"
 
 
 @pytest.fixture(name="archived_flow_cell")
 def fixture_archived_flow_cell() -> Path:
-    """Path of archived flow cell"""
-    return Path("/path/to/archived/flow_cell.tar.gz.gpg")
+    """Path of archived flow cell."""
+    return Path("path", "to", "archived", "flow_cell.tar.gz.gpg")
 
 
 @pytest.fixture(name="archived_key")
 def fixture_archived_key() -> Path:
-    """Path of archived key"""
-    return Path("/path/to/archived/encryption_key.key.gpg")
+    """Path of archived key."""
+    return Path("path", "to", "archived", "encryption_key.key.gpg")
 
 
 @pytest.fixture(name="pdc_query")
 def fixture_pdc_query() -> list:
-    """Result of a PDC query"""
+    """Result of a PDC query."""
     return [
         "IBM Tivoli Storage Manager",
         "Command Line Backup-Archive Client Interface",
