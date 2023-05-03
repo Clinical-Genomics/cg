04fca656addd527625c177b8ce16822e1c67b921
Christian Oertlin <c.oertlin@gmail.com>
2023-05-03 14:09:16 +0200

    initial commit

--- cg/store/models.py
+++ cg/store/models.py
@@ -753,3 +753,21 @@ class User(Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class SequencingStats(Model):
+    __tablename__ = "sequencing_stats"
+    id = Column(types.Integer, primary_key=True)
+    sample_internal_id = Column(
+        ForeignKey("sample.sample_internal_id", ondelete="CASCADE"), nullable=False
+    )
+    flow_cell_id = Column(ForeignKey("flowcell.id", ondelete="CASCADE"), nullable=False)
+    lane = Column(types.Integer)
+    yield_mb = Column(types.Integer)
+    passed_filter_pct = Column(types.Numeric(10, 5))
+    read_counts = Column(types.Integer)
+    raw_clusters_per_lane_pct = Column(types.Numeric(10, 5))
+    perfect_index_reads_pct = Column(types.Numeric(10, 5))
+    q30_bases_pct = Column(types.Numeric(10, 5))
+    mean_quality_score = Column(types.Numeric(10, 5))
+    time = Column(types.DateTime)
