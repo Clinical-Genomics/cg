bf90bcb801f05fc9513e641aacf6c424378224ed
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-05-03 11:08:33 +0200

    Mantain run_parameters, create flowcell mode function, general refactoring

--- cg/models/demultiplex/run_parameters.py
+++ cg/models/demultiplex/run_parameters.py
@@ -10,7 +10,7 @@ LOG = logging.getLogger(__name__)
 
 
 class RunParameters:
-    """Class to handle the run parameters from a sequencing run"""
+    """Class to handle the run parameters from a sequencing run."""
 
     def __init__(self, run_parameters_path: Path):
         self.path: Path = run_parameters_path
@@ -19,14 +19,16 @@ class RunParameters:
 
     @property
     def index_length(self) -> int:
-        index_one_length: int = self.index_read_one()
-        index_two_length: int = self.index_read_two()
+        """Return the length of the indexes if they are equal, raise an error otherwise."""
+        index_one_length: int = self.get_index1_cycles()
+        index_two_length: int = self.get_index2_cycles()
         if index_one_length != index_two_length:
             raise FlowCellError("Index lengths are not the same!")
         return index_one_length
 
     @property
     def control_software_version(self) -> str:
+        """Return the control software version."""
         node_name = ".ApplicationVersion"
         xml_node: Optional[ElementTree.Element] = self.tree.find(node_name)
         self.node_not_found(node=xml_node, name="control software version")
@@ -34,6 +36,7 @@ class RunParameters:
 
     @property
     def reagent_kit_version(self) -> str:
+        """Return the reagent kit version if existent, return 'unknown' otherwise."""
         node_name = "./RfidsInfo/SbsConsumableVersion"
         xml_node: Optional[ElementTree.Element] = self.tree.find(node_name)
         if xml_node is None:
@@ -44,7 +47,7 @@ class RunParameters:
 
     @property
     def flowcell_type(self) -> Literal["novaseq", "hiseq"]:
-        """Fetch the flowcell type from the run parameters"""
+        """Fetch the flowcell type from the run parameters."""
         # First try with the node name for hiseq
         node_name = "./Setup/ApplicationName"
         xml_node = self.tree.find(node_name)
@@ -61,56 +64,68 @@ class RunParameters:
         raise FlowCellError(message)
 
     @property
+    def flow_cell_mode(self) -> Optional[str]:
+        """Return the flow cell mode."""
+        node_name = "/RfidsInfo/FlowCellMode"
+        xml_node = self.tree.find(node_name)
+        if xml_node is None:
+            LOG.warning("Could not determine flow cell mode")
+            LOG.info("Set flow cell mode to None")
+            return
+        return xml_node.text
+
+    @property
     def run_type(self) -> Literal["wgs", "fluffy"]:
-        """Fetch what type of run the parameters is"""
+        """Return the run type of the sequencing."""
         if self.index_length == 8:
             return "fluffy"
         return "wgs"
 
     @staticmethod
     def node_not_found(node: Optional[ElementTree.Element], name: str) -> None:
-        """Raise exception if node if not found"""
+        """Raise exception if the given node is not found."""
         if node is None:
             message = f"Could not determine {name}"
             LOG.warning(message)
             raise FlowCellError(message)
 
     def get_node_integer_value(self, node_name: str, name: str) -> int:
+        """Return the value of the node as an integer."""
         xml_node = self.tree.find(node_name)
         self.node_not_found(node=xml_node, name=name)
         return int(xml_node.text)
 
-    def index_read_one(self) -> int:
-        """Get the value for index read one"""
+    def get_index1_cycles(self) -> int:
+        """Return the number of cycles in the first index read."""
         node_name = "./IndexRead1NumberOfCycles"
         return self.get_node_integer_value(node_name=node_name, name="length of index one")
 
-    def index_read_two(self) -> int:
-        """Get the value for index read one"""
+    def get_index2_cycles(self) -> int:
+        """Return the number of cycles in the second index read."""
         node_name = "./IndexRead2NumberOfCycles"
         return self.get_node_integer_value(node_name=node_name, name="length of index two")
 
-    def read_one_nr_cycles(self) -> int:
-        """Get the nr of cycles for read one"""
+    def get_read1_cycles(self) -> int:
+        """Return the number of cycles in the first read."""
         node_name = "./Read1NumberOfCycles"
         return self.get_node_integer_value(node_name=node_name, name="length of reads one")
 
-    def read_two_nr_cycles(self) -> int:
-        """Get the nr of cycles for read one"""
+    def get_read2_cycles(self) -> int:
+        """Return the number of cycles in the second read."""
         node_name = "./Read2NumberOfCycles"
         return self.get_node_integer_value(node_name=node_name, name="length of reads two")
 
-    def base_mask(self) -> str:
-        """create the bcl2fastq basemask for novaseq flowcells
+    def get_base_mask(self) -> str:
+        """Create the bcl2fastq basemask for novaseq flow cells.
 
         Basemask is used in this comma format as an argument to bcl2fastq.
-        When creating the unaligned path the commas are stripped
+        When creating the unaligned path the commas are stripped.
         """
         return (
-            f"Y{self.read_one_nr_cycles()},"
-            f"I{self.index_read_one()},"
-            f"I{self.read_two_nr_cycles()},"
-            f"Y{self.index_read_two()}"
+            f"Y{self.get_read1_cycles()},"
+            f"I{self.get_index1_cycles()},"
+            f"I{self.get_index2_cycles()},"
+            f"Y{self.get_read2_cycles()}"
         )
 
     def __str__(self):
