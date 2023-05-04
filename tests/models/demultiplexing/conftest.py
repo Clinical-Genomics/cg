2066db865d8bdd50a2050c317686b92e463dd21d
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-05-04 14:09:44 +0200

    Remove repeated fixture and updated usages

--- tests/models/demultiplexing/conftest.py
+++ tests/models/demultiplexing/conftest.py
@@ -12,14 +12,9 @@ def fixture_flow_cell_name(flow_cell_full_name: str) -> str:
     return flow_cell_full_name.split("_")[-1][1:]
 
 
-@pytest.fixture(name="flow_cell_runs")
-def fixture_flow_cell_runs(demultiplex_fixtures: Path) -> Path:
-    return Path(demultiplex_fixtures, "flowcell-runs")
-
-
 @pytest.fixture(name="flow_cell_path")
-def fixture_flowcell_path(flow_cell_runs: Path, flow_cell_full_name: str) -> Path:
-    return Path(flow_cell_runs, flow_cell_full_name)
+def fixture_flowcell_path(demux_run_dir: Path, flow_cell_full_name: str) -> Path:
+    return Path(demux_run_dir, flow_cell_full_name)
 
 
 @pytest.fixture(name="flow_cell")
@@ -35,8 +30,8 @@ def fixture_dragen_flow_cell_full_name() -> str:
 
 
 @pytest.fixture(name="dragen_flow_cell_path")
-def fixture_dragen_flow_cell_path(flow_cell_runs: Path, dragen_flow_cell_full_name: str) -> Path:
-    return Path(flow_cell_runs, dragen_flow_cell_full_name)
+def fixture_dragen_flow_cell_path(demux_run_dir: Path, dragen_flow_cell_full_name: str) -> Path:
+    return Path(demux_run_dir, dragen_flow_cell_full_name)
 
 
 @pytest.fixture(name="demultiplexed_dragen_flow_cell")
