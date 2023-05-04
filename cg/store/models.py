f129112161d282d8ce566a4d01248bda1fdb9cbe
Christian Oertlin <c.oertlin@gmail.com>
2023-05-04 14:17:39 +0200

    table-relationships

--- cg/store/models.py
+++ cg/store/models.py
@@ -519,7 +519,7 @@ class Flowcell(Model):
     updated_at = Column(types.DateTime, onupdate=dt.datetime.now)
 
     samples = orm.relationship("Sample", secondary=flowcell_sample, backref="flowcells")
-    sequencing_stats = orm.relationship("SequencingStats", back_populates="flowcell")
+    sequencing_stats = orm.relationship("SequencingStats", back_populates="flow_cells")
 
     def __str__(self):
         return self.name
@@ -639,7 +639,7 @@ class Sample(Model, PriorityMixin):
     sequenced_at = Column(types.DateTime)
     sex = Column(types.Enum(*SEX_OPTIONS), nullable=False)
     subject_id = Column(types.String(128))
-    sequencing_stat = orm.relationship("SequencingStats", back_populates="sample")
+    sequencing_stats = orm.relationship("SequencingStats", back_populates="samples")
 
     def __str__(self) -> str:
         return f"{self.internal_id} ({self.name})"
@@ -773,5 +773,5 @@ class SequencingStats(Model):
     q30_bases_pct = Column(types.Numeric(10, 5))
     mean_quality_score = Column(types.Numeric(10, 5))
     time = Column(types.DateTime)
-    sample = orm.relationship("Sample", back_populates="sequencing_stats")
-    flowcell = orm.relationship("Flowcell", back_populates="sequencing_stats")
+    samples = orm.relationship("Sample", back_populates="sequencing_stats")
+    flow_cells = orm.relationship("Flowcell", back_populates="sequencing_stats")
