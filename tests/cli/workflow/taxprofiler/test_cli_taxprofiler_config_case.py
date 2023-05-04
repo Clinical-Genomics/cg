3fcb0c8f3c21c8a51da83ccbbdc93a11e1df1c18
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-05-04 10:57:18 +0200

    Test config_case with missing case

--- tests/cli/workflow/taxprofiler/test_cli_taxprofiler_config_case.py
+++ tests/cli/workflow/taxprofiler/test_cli_taxprofiler_config_case.py
@@ -24,24 +24,33 @@ def test_without_options(cli_runner: CliRunner, taxprofiler_context: CGConfig):
     assert "Missing argument" in result.output
 
 
-# def test_with_missing_case(
-#    cli_runner: CliRunner,
-#    taxprofiler_context: CGConfig,
-#    caplog: LogCaptureFixture,
-#    case_id_does_not_exist: str,
-# ):
-#    """Test command with invalid case to start with."""
-#    caplog.set_level(logging.ERROR)
-#    # GIVEN case_id not in database
-#    assert not taxprofiler_context.status_db.get_case_by_internal_id(
-#        internal_id=case_id_does_not_exist
-#    )
-#    # WHEN running
-#    result = cli_runner.invoke(config_case, [case_id_does_not_exist], obj=taxprofiler_context)
-#    # THEN command should NOT successfully call the command it creates
-#    assert result.exit_code != EXIT_SUCCESS
-#    # THEN ERROR log should be printed containing invalid case_id
-#    #assert "could not be found in StatusDB!" in caplog.text
+def test_with_missing_case(
+    cli_runner: CliRunner,
+    taxprofiler_context: CGConfig,
+    caplog: LogCaptureFixture,
+    case_id_does_not_exist: str,
+):
+    """Test command with invalid case to start with."""
+    caplog.set_level(logging.ERROR)
+    logger = logging.getLogger("your.logger.name")
+    logger.setLevel(logging.ERROR)
+    handler = logging.handlers.BufferingHandler(capacity=1)
+    logger.addHandler(handler)
+    # GIVEN case_id not in database
+    assert not taxprofiler_context.status_db.get_case_by_internal_id(
+        internal_id=case_id_does_not_exist
+    )
+    # WHEN running
+    result = cli_runner.invoke(config_case, [case_id_does_not_exist], obj=taxprofiler_context)
+    # THEN command should NOT successfully call the command it creates
+    assert result.exit_code != EXIT_SUCCESS
+    # THEN ERROR log should be printed containing invalid case_id
+    # assert "could not be found in StatusDB!" in caplog.text
+    for record in handler.buffer:
+        if "could not be found in StatusDB!" in record.getMessage():
+            break
+    else:
+        assert False, "Log message not found"
 
 
 # def test_without_samples(
