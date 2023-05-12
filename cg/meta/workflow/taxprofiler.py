af8f4e18a3516d87701af87a6408a450f1e6a4cd
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-05-12 11:50:33 +0200

    Add samplesheet content

--- cg/meta/workflow/taxprofiler.py
+++ cg/meta/workflow/taxprofiler.py
@@ -72,14 +72,14 @@ class TaxprofilerAnalysisAPI(AnalysisAPI):
         """Write sample sheet for taxprofiler analysis in case folder."""
         case_obj = self.status_db.get_case_by_internal_id(internal_id=case_id)
         for link in case_obj.links:
-            #    sample_metadata: List[str] = self.gather_file_metadata_for_sample(link.sample)
-            #    fastq_r1: List[str] = NextflowAnalysisAPI.extract_read_files(1, sample_metadata)
-            #    fastq_r2: List[str] = NextflowAnalysisAPI.extract_read_files(2, sample_metadata)
-            #    samplesheet_content: Dict[str, List[str]] = self.build_samplesheet_content(
-            #        case_id, fastq_r1, fastq_r2
-            #    )
-            #    LOG.info("Content" + samplesheet_content)
-            #    LOG.info(samplesheet_content)
+            sample_metadata: List[str] = self.gather_file_metadata_for_sample(link.sample)
+            fastq_r1: List[str] = NextflowAnalysisAPI.extract_read_files(1, sample_metadata)
+            fastq_r2: List[str] = NextflowAnalysisAPI.extract_read_files(2, sample_metadata)
+            samplesheet_content: Dict[str, List[str]] = self.build_samplesheet_content(
+                case_id, fastq_r1, fastq_r2
+            )
+            LOG.info("Content" + samplesheet_content)
+            LOG.info(samplesheet_content)
             # if dry_run:
             #     continue
             NextflowAnalysisAPI.create_samplesheet_csv(
