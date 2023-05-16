b0fe38d8e69561945c3106ac8e35140efa3e90ee
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-05-16 10:26:16 +0200

    Pass empty fasta argument

--- cg/cli/workflow/taxprofiler/base.py
+++ cg/cli/workflow/taxprofiler/base.py
@@ -50,7 +50,9 @@ def config_case(context: CGConfig, case_id: str, instrument_platform: str) -> No
     analysis_api.verify_case_id_in_statusdb(case_id=case_id)
     try:
         analysis_api.verify_case_id_in_statusdb(case_id=case_id)
-        analysis_api.config_case(case_id=case_id, instrument_platform=instrument_platform)
+        analysis_api.config_case(
+            case_id=case_id, instrument_platform=instrument_platform, fasta=None
+        )
 
     except CgError as error:
         LOG.error(f"Could not create sample sheet: {error}")
