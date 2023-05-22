3007d27f421bb78eeea9e47c7f5a60d4fdc57ad5
seallard <sebastian.allard@scilifelab.se>
2023-05-22 15:52:51 +0200

    Fix tests

--- tests/apps/sequencing_metrics_parser/test_sequencing_metrics_calculator.py
+++ tests/apps/sequencing_metrics_parser/test_sequencing_metrics_calculator.py
@@ -8,28 +8,31 @@ from cg.apps.sequencing_metrics_parser.sequencing_metrics_calculator import (
 )
 
 
-def test_calculate_lane_yield_in_megabases():
-    assert yield_in_megabases(total_bases=5000000) == 5
+def test_calculate_yield_in_megabases():
+    yield_metric: int = yield_in_megabases(total_bases=5000000)
+    assert yield_metric == 5
 
 
-def test_calculate_passed_filter_percent():
+def test_calculate_pass_filter_ratio():
     passed_filter_percent: float = pass_filter_ratio(clusters_passed=100, total_clusters=200)
-
     assert passed_filter_percent == 0.5
 
 
-def test_calculate_raw_clusters_per_lane_percent():
-    assert average_clusters_per_lane(total_clusters=100, lane_count=10) == 10
+def test_average_clusters_per_lane():
+    avg_clusters_per_lane: float = average_clusters_per_lane(total_clusters=100, lane_count=10)
+    assert avg_clusters_per_lane == 10
 
 
-def test_calculate_bases_with_q30_percent():
-    assert q30_ratio(q30_yield=100, total_yield=200) == 0.5
+def test_calculate_bases_with_q30_ratio():
+    q30_ratio_metric = q30_ratio(q30_yield=100, total_yield=200)
+    assert q30_ratio_metric == 0.5
 
 
 def test_calculate_lane_mean_quality_score():
-    assert average_quality_score(total_quality_score=100, total_yield=200) == 0.5
+    avg_quality_score: float = average_quality_score(total_quality_score=100, total_yield=200)
+    assert avg_quality_score == 0.5
 
 
-def test_calculate_perfect_index_reads_percent():
-    perfect_index_reads_percent: float = perfect_reads_ratio(perfect_reads=100, total_reads=200)
-    assert perfect_index_reads_percent == 50
+def test_calculate_perfect_index_reads_ratio():
+    perfect_index_reads: float = perfect_reads_ratio(perfect_reads=100, total_reads=200)
+    assert perfect_index_reads == 50
