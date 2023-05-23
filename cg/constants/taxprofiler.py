5d13a49e45833cedaa13b6f3b67955a1a0c336bf
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-05-23 10:35:34 +0200

    Remove type hint

--- cg/constants/taxprofiler.py
+++ cg/constants/taxprofiler.py
@@ -6,7 +6,7 @@ from cg.constants.sequencing import SequencingPlatform
 
 TAXPROFILER_RUN_ACCESSION: str = "run_accession"
 TAXPROFILER_INSTRUMENT_PLATFORM: str = "instrument_platform"
-TAXPROFILER_FASTA_HEADER: str = "fasta"
+TAXPROFILER_FASTA_HEADER = "fasta"
 TAXPROFILER_SAMPLE_SHEET_HEADERS: list[str] = [
     NFX_SAMPLE_HEADER,
     TAXPROFILER_RUN_ACCESSION,
