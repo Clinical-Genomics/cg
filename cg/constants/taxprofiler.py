528d617f5047f044d856649fe5bedbe7989268f6
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-05-23 10:53:11 +0200

    Test type hints

--- cg/constants/taxprofiler.py
+++ cg/constants/taxprofiler.py
@@ -4,9 +4,9 @@
 from cg.constants.nextflow import NFX_SAMPLE_HEADER, NFX_SAMPLESHEET_READS_HEADERS
 from cg.constants.sequencing import SequencingPlatform
 
-TAXPROFILER_RUN_ACCESSION = "run_accession"
-TAXPROFILER_INSTRUMENT_PLATFORM = "instrument_platform"
-TAXPROFILER_FASTA_HEADER = "fasta"
+TAXPROFILER_RUN_ACCESSION: str = "run_accession"
+TAXPROFILER_INSTRUMENT_PLATFORM: str = "instrument_platform"
+TAXPROFILER_FASTA_HEADER: str = "fasta"
 TAXPROFILER_SAMPLE_SHEET_HEADERS = [
     NFX_SAMPLE_HEADER,
     TAXPROFILER_RUN_ACCESSION,
