ed58db010b4c717ecad4d3272958cdc1e86bb795
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-05-23 15:35:52 +0200

    Import list from typing

--- cg/constants/taxprofiler.py
+++ cg/constants/taxprofiler.py
@@ -2,12 +2,12 @@
 
 
 from cg.constants.nextflow import NFX_SAMPLE_HEADER, NFX_SAMPLESHEET_READS_HEADERS
-from cg.constants.sequencing import SequencingPlatform
+from typing import List
 
 TAXPROFILER_RUN_ACCESSION: str = "run_accession"
 TAXPROFILER_INSTRUMENT_PLATFORM: str = "instrument_platform"
 TAXPROFILER_FASTA_HEADER: str = "fasta"
-TAXPROFILER_SAMPLE_SHEET_HEADERS: list[str] = [
+TAXPROFILER_SAMPLE_SHEET_HEADERS: List[str] = [
     NFX_SAMPLE_HEADER,
     TAXPROFILER_RUN_ACCESSION,
     TAXPROFILER_INSTRUMENT_PLATFORM,
