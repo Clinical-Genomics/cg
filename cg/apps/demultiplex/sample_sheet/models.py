7b551da673e87363917057c4d890ed632d833432
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-05-24 09:31:14 +0200

    Make NovaSeqSample more similar to Lims sample

--- cg/apps/demultiplex/sample_sheet/models.py
+++ cg/apps/demultiplex/sample_sheet/models.py
@@ -11,16 +11,20 @@ class NovaSeqSample(BaseModel):
     """This model is used when parsing/validating existing sample sheets."""
 
     flow_cell: str = Field(..., alias=SampleSheetHeaderColumnNames.FLOW_CELL_ID.value)
-    lane: int = Field(..., alias="Lane")
+    lane: int = Field(..., alias=SampleSheetHeaderColumnNames.LANE.value)
     sample_id: str = Field(..., alias="SampleID")
-    reference: str = Field(..., alias="SampleRef")
+    sample_ref: str = Field(..., alias="SampleRef")
     index: str = Field(..., alias="index")
+    index2: str = ""
     sample_name: str = Field(..., alias="SampleName")
-    control: str = Field(..., alias="Control")
-    recipe: str = Field(..., alias="Recipe")
+    control: str = Field("N", alias="Control")
+    recipe: str = Field("R1", alias="Recipe")
     operator: str = Field(..., alias="Operator")
     project: str = Field(..., alias="Project")
-    second_index: str = Field(..., alias="index2")
+    second_index: str = Field("script", alias="index2")
+
+    class Config:
+        allow_population_by_field_name = True
 
 
 class SampleBcl2Fastq(NovaSeqSample):
