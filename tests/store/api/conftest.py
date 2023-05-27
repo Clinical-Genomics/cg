260a9824e1541ed49452388247c49545cf80ac19
seallard <sebastian.allard@scilifelab.se>
2023-05-27 12:43:12 +0200

    Remove unused fixtures

--- tests/store/api/conftest.py
+++ tests/store/api/conftest.py
@@ -176,12 +176,6 @@ def fixture_expected_number_of_not_archived_applications() -> int:
     return 4
 
 
-@pytest.fixture(name="EXPECTED_NUMBER_OF_APPLICATIONS_WITH_PREP_CATEGORY")
-def fixture_expected_number_of_applications_with_prep_category() -> int:
-    """Return the number of expected number of applications with prep category"""
-    return 7
-
-
 @pytest.fixture(name="EXPECTED_NUMBER_OF_APPLICATIONS")
 def fixture_expected_number_of_applications() -> int:
     """Return the number of expected number of applications with prep category"""
