5958057c59cf2045225ea128a97d3919a943387c
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-05-30 10:41:26 +0200

    Refactor hiseq fixture files

--- tests/conftest.py
+++ tests/conftest.py
@@ -749,9 +749,9 @@ def fixture_dragen_flow_cell_path(
 
 
 @pytest.fixture(name="hiseq_dir")
-def fixture_hiseq_dir(demultiplex_fixtures: Path) -> Path:
+def fixture_hiseq_dir(flow_cell_runs_dir: Path) -> Path:
     """Return the path to the hiseq demultiplex fixtures."""
-    return Path(demultiplex_fixtures, "hiseq_run")
+    return Path(flow_cell_runs_dir, "hiseq_run")
 
 
 @pytest.fixture(name="run_parameters_dir")
