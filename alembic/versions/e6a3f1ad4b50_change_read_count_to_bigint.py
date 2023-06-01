16f3a500f000ac935fc6ed74c5f24e30ce5519cf
seallard <sebastian.allard@scilifelab.se>
2023-06-01 16:53:41 +0200

    Fix formatting

--- alembic/versions/e6a3f1ad4b50_change_read_count_to_bigint.py
+++ alembic/versions/e6a3f1ad4b50_change_read_count_to_bigint.py
@@ -10,17 +10,21 @@ import sqlalchemy as sa
 from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
-revision = 'e6a3f1ad4b50'
-down_revision = 'f5e0db62a5a7'
+revision = "e6a3f1ad4b50"
+down_revision = "f5e0db62a5a7"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
     # Alter the column to use BigInteger
-    op.alter_column('sample_lane_sequencing_metrics', 'sample_total_reads_in_lane', type_=sa.BigInteger)
+    op.alter_column(
+        "sample_lane_sequencing_metrics", "sample_total_reads_in_lane", type_=sa.BigInteger
+    )
 
 
 def downgrade():
     # Alter the column to use Integer
-    op.alter_column('sample_lane_sequencing_metrics', 'sample_total_reads_in_lane', type_=sa.Integer)
+    op.alter_column(
+        "sample_lane_sequencing_metrics", "sample_total_reads_in_lane", type_=sa.Integer
+    )
