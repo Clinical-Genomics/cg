644c24a77f03be548d0d24deaab64905400fcee0
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-06-14 11:47:28 +0200

    Fix error in the argument

--- cg/meta/workflow/taxprofiler.py
+++ cg/meta/workflow/taxprofiler.py
@@ -83,7 +83,7 @@ class TaxprofilerAnalysisAPI(AnalysisAPI):
         return sample_sheet_content
 
     def write_sample_sheet(
-        self, sample_id: str, instrument_platform: SequencingPlatform.ILLUMINA, fasta: Optional[str]
+        self, case_id: str, instrument_platform: SequencingPlatform.ILLUMINA, fasta: Optional[str]
     ) -> None:
         """Write sample sheet for taxprofiler analysis in case folder."""
         case: Family = self.status_db.get_case_by_internal_id(internal_id=case_id)
@@ -93,7 +93,7 @@ class TaxprofilerAnalysisAPI(AnalysisAPI):
             fastq_r1: List[str] = NextflowAnalysisAPI.extract_read_files(1, sample_metadata)
             fastq_r2: List[str] = NextflowAnalysisAPI.extract_read_files(2, sample_metadata)
             sample_sheet_content: Dict[str, List[str]] = self.build_sample_sheet_content(
-                sample_id=sample_id,
+                case_id=sample_id,
                 fastq_r1=fastq_r1,
                 fastq_r2=fastq_r2,
                 instrument_platform=instrument_platform,
