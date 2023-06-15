b4a2a2bf11b74b964b81af96752b294e83c85fca
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-06-15 11:20:02 +0200

    Print sample metadata

--- cg/meta/workflow/taxprofiler.py
+++ cg/meta/workflow/taxprofiler.py
@@ -101,7 +101,7 @@ class TaxprofilerAnalysisAPI(AnalysisAPI):
         for link in case.links:
             print("Link" + link)
             sample_metadata: List[str] = self.gather_file_metadata_for_sample(link.sample)
-            print("Sample metadata" + str(sample_metadata))
+            print("sample_metadata: List[str] =", sample_metadata)
             fastq_r1: List[str] = NextflowAnalysisAPI.extract_read_files(1, sample_metadata)
             fastq_r2: List[str] = NextflowAnalysisAPI.extract_read_files(2, sample_metadata)
             sample_sheet_content: Dict[str, List[str]] = self.build_sample_sheet_content(
