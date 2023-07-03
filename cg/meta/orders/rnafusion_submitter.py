06c0be98aebab1da37df07ba9365439e1b91b3e2
Vincent Janvid <vincent.janvid@scilifelab.se>
2023-07-03 16:10:02 +0200

    Add one sample per case validation for RNAFUSION orders

--- cg/meta/orders/rnafusion_submitter.py
+++ cg/meta/orders/rnafusion_submitter.py
@@ -1,5 +1,19 @@
+from typing import List
+
+from cg.exc import OrderError
 from cg.meta.orders.case_submitter import CaseSubmitter
+from cg.models.orders.order import OrderIn
+from cg.models.orders.samples import Of1508Sample
 
 
 class RnafusionSubmitter(CaseSubmitter):
-    pass
+    def validate_order(self, order: OrderIn) -> None:
+        """Validates that the order is correct."""
+        CaseSubmitter.validate_order(self, order)
+        self._validate_one_sample_per_case(order.samples)
+
+    @staticmethod
+    def _validate_one_sample_per_case(samples: List[Of1508Sample]) -> None:
+        """Validates that all samples have a unique case."""
+        if len({sample.family_name for sample in samples}) != len(samples):
+            raise OrderError("RNAFUSION orders only accept one sample per case.")
