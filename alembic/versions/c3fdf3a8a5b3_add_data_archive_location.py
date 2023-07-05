6374727dac19b8546f518a5f97df0a1c5d9e1228
islean <isak.ohlsson.angnell@gmail.com>
2023-07-05 11:31:48 +0200

    Add default value "PDC" and make non-nullable

--- alembic/versions/c3fdf3a8a5b3_add_data_archive_location.py
+++ alembic/versions/c3fdf3a8a5b3_add_data_archive_location.py
@@ -8,6 +8,8 @@ Create Date: 2023-07-04 15:29:48.507215
 import sqlalchemy as sa
 from alembic import op
 
+from cg.store.models import Customer
+
 # revision identifiers, used by Alembic.
 revision = "c3fdf3a8a5b3"
 down_revision = "ffb9f8ab8e62"
@@ -18,6 +20,17 @@ depends_on = None
 def upgrade():
     op.add_column(
         table_name="customer", column=sa.column("data_archive_location", sa.String(length=32))
+    )
+    bind = op.get_bind()
+    session = sa.orm.Session(bind=bind)
+    for customer in session.query(Customer):
+        customer.data_archive_location = "PDC"
+    session.commit()
+    op.alter_column(
+        table_name="customer",
+        column_name="data_archive_location",
+        existing_type=sa.String(length=32),
+        nullable=False
     )
 
 
