7598afdbaf72b3899431596b4c179f831b360b44
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-07-07 16:21:38 +0200

    Add review suggestions

--- cg/meta/workflow/taxprofiler.py
+++ cg/meta/workflow/taxprofiler.py
@@ -98,7 +98,7 @@ class TaxprofilerAnalysisAPI(AnalysisAPI):
             sample_metadata: List[str] = self.gather_file_metadata_for_sample(link.sample)
             fastq_r1: List[str] = NextflowAnalysisAPI.extract_read_files(1, sample_metadata)
             fastq_r2: List[str] = NextflowAnalysisAPI.extract_read_files(2, sample_metadata)
-            sample_content = self.build_sample_sheet_content(
+            sample_content: Dict[str, List[str]] = self.build_sample_sheet_content(
                 sample_name=sample_name,
                 fastq_r1=fastq_r1,
                 fastq_r2=fastq_r2,
