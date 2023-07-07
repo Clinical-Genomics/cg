4e8e98e310fa9599ac7e0a9a5e27c78247a42b70
Christian Oertlin <c.oertlin@gmail.com>
2023-07-07 16:56:31 +0200

    reviews

--- cg/utils/files.py
+++ cg/utils/files.py
@@ -7,16 +7,12 @@ import os
 def get_file_in_directory(directory: Path, file_name: str) -> Path:
     """Get a file in a directory and subdirectories.
     Raises:
-        FileNotFoundError: If the file is not found in the directory.
         FileNotFoundError: If the directory does not exist.
     """
     if not directory.is_dir() or not directory.exists():
         raise FileNotFoundError(f"Directory {directory} does not exist")
-    path_to_file: Optional[Path] = None
     for directory_path, _, files in os.walk(directory):
         for file in files:
-            if file_name in file:
+            if file_name == file:
                 path_to_file = Path(directory_path, file)
                 return path_to_file
-    if not path_to_file:
-        raise FileNotFoundError(f"File {file_name} not found in {directory}")
