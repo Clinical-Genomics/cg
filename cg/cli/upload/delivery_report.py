9a04fe221b1855ca259b78b6fa2f204afbf316a0
Vadym <vadym.ivanchuk@scilifelab.se>
2023-07-17 10:57:44 +0200

    review: re-upload option

--- cg/cli/upload/delivery_report.py
+++ cg/cli/upload/delivery_report.py
@@ -16,11 +16,16 @@ LOG = logging.getLogger(__name__)
 @click.command("delivery-report-to-scout")
 @ARGUMENT_CASE_ID
 @click.option(
-    "-d", "--dry-run", is_flag=True, default=False, help="Run command without uploading to scout"
+    "-r", "--re-upload", is_flag=True, default=False, help="Re-upload existing delivery report"
+)
+@click.option(
+    "-d", "--dry-run", is_flag=True, default=False, help="Run command without uploading to Scout"
 )
 @click.pass_context
-def upload_delivery_report_to_scout(context: click.Context, case_id: str, dry_run: bool) -> None:
-    """Fetches a delivery report from housekeeper and uploads it to scout."""
+def upload_delivery_report_to_scout(
+    context: click.Context, case_id: str, re_upload: bool, dry_run: bool
+) -> None:
+    """Fetches a delivery report from Housekeeper and uploads it to Scout."""
     click.echo(click.style("--------------- DELIVERY REPORT UPLOAD ---------------"))
     case: Family = get_report_case(context, case_id)
     report_api: ReportAPI = get_report_api(context, case)
@@ -30,7 +35,7 @@ def upload_delivery_report_to_scout(context: click.Context, case_id: str, dry_ru
     )
     if delivery_report and not dry_run:
         report_api.scout_api.upload_delivery_report(
-            report_path=delivery_report, case_id=case.internal_id, update=True
+            report_path=delivery_report, case_id=case.internal_id, update=re_upload
         )
         return
     LOG.error("Delivery report not uploaded to Scout")
