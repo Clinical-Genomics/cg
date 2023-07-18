0a06eca79d80ab6555283d982f65c508cbdaffc8
Vadym <vadym.ivanchuk@scilifelab.se>
2023-07-18 16:50:35 +0200

    feat(delivery-report): Upload forced generated report (#2256)
    ### Fixed:
    - Upload command to skip report generation for reports already in HK

--- tests/cli/generate/report/test_cli_delivery_report.py
+++ tests/cli/generate/report/test_cli_delivery_report.py
@@ -1,24 +1,26 @@
-"""Tests the cli for generating delivery reports"""
-
+"""Tests the cli for generating delivery reports."""
 import logging
 from datetime import datetime
 
+from _pytest.logging import LogCaptureFixture
+from click.testing import Result, CliRunner
+
 from cg.cli.generate.report.base import generate_delivery_report
 from cg.constants import EXIT_SUCCESS, EXIT_FAIL
+from cg.models.cg_config import CGConfig
 
 
-def test_delivery_report_invalid_case(mip_dna_context, cli_runner, caplog):
-    """Tests the delivery report command for an invalid case"""
-
+def test_delivery_report_invalid_case(
+    mip_dna_context: CGConfig, cli_runner: CliRunner, caplog: LogCaptureFixture
+):
+    """Tests the delivery report command for an invalid case."""
     caplog.set_level(logging.INFO)
 
     # GIVEN a MIP DNA context object
 
     # WHEN calling delivery_report with an invalid case name
-    result = cli_runner.invoke(
-        generate_delivery_report,
-        ["not a case"],
-        obj=mip_dna_context,
+    result: Result = cli_runner.invoke(
+        generate_delivery_report, ["not a case"], obj=mip_dna_context
     )
 
     # THEN the command should fail due to an invalid case ID
@@ -27,18 +29,17 @@ def test_delivery_report_invalid_case(mip_dna_context, cli_runner, caplog):
     assert result.exit_code == EXIT_FAIL
 
 
-def test_delivery_report_dry_run(mip_dna_context, cli_runner, case_id, caplog):
-    """Tests the delivery report command with a dry run option"""
-
+def test_delivery_report_dry_run(
+    mip_dna_context: CGConfig, cli_runner: CliRunner, case_id: str, caplog: LogCaptureFixture
+):
+    """Tests the delivery report command with a dry run option."""
     caplog.set_level(logging.INFO)
 
     # GIVEN a MIP DNA context object
 
     # WHEN calling delivery_report with a dry option
-    result = cli_runner.invoke(
-        generate_delivery_report,
-        [case_id, "--dry-run"],
-        obj=mip_dna_context,
+    result: Result = cli_runner.invoke(
+        generate_delivery_report, [case_id, "--dry-run"], obj=mip_dna_context
     )
 
     # THEN the command should be invoked successfully
@@ -49,20 +50,18 @@ def test_delivery_report_dry_run(mip_dna_context, cli_runner, case_id, caplog):
     assert result.exit_code == EXIT_SUCCESS
 
 
-def test_delivery_report(mip_dna_context, cli_runner, case_id, caplog):
-    """Tests the delivery report command expecting a rendered html file"""
-
+def test_delivery_report(
+    mip_dna_context: CGConfig, cli_runner: CliRunner, case_id: str, caplog: LogCaptureFixture
+):
+    """Tests the delivery report command expecting a rendered html file."""
     caplog.set_level(logging.INFO)
 
     # GIVEN a MIP DNA context object
 
     # WHEN calling delivery_report with a dry option
-    cli_runner.invoke(
-        generate_delivery_report,
-        [case_id],
-        obj=mip_dna_context,
-    )
+    result: Result = cli_runner.invoke(generate_delivery_report, [case_id], obj=mip_dna_context)
 
     # THEN the report html should have been created and added to HK
     assert "create_delivery_report_file" in caplog.text
     assert "add_delivery_report_to_hk" in caplog.text
+    assert result.exit_code == EXIT_SUCCESS
