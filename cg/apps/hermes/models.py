cab909793caf00bbe87901ca09e69856d3a1100c
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-07-19 09:28:20 +0200

    Refactor hermes models

--- cg/apps/hermes/models.py
+++ cg/apps/hermes/models.py
@@ -3,7 +3,7 @@ import logging
 from pathlib import Path
 from typing import List, Optional
 
-from pydantic import field_validator, BaseModel, validator
+from pydantic import field_validator, BaseModel
 
 LOG = logging.getLogger(__name__)
 
@@ -23,19 +23,16 @@ class CGDeliverables(BaseModel):
     bundle_id: str
     files: List[CGTag]
 
-    # TODO[pydantic]: We couldn't refactor the `validator`, please replace it by `field_validator` manually.
-    # Check https://docs.pydantic.dev/dev-v2/migration/#changes-to-validators for more information.
-    @validator("files", each_item=True)
-    def check_mandatory_exists(cls, file):
-        if file.mandatory:
-            assert Path(file.path).exists(), f"Mandatory file cannot be found at {file.path}"
-            return file
-        if not Path(file.path).exists():
-            LOG.info("Optional file %s not found, removing from bundle", file.path)
-            return
-        return file
+    @field_validator("files")
+    def check_mandatory_exists(cls, value):
+        for file in value:
+            if file.mandatory:
+                assert Path(file.path).exists(), f"Mandatory file cannot be found at {file.path}"
+            if not Path(file.path).exists():
+                LOG.info("Optional file %s not found, removing from bundle", file.path)
+                value.pop(file)
+        return value
 
     @field_validator("files")
-    @classmethod
     def remove_invalid(cls, value):
         return [item for item in value if item]
