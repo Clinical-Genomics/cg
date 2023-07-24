1d208cbc48076379fdfca22b7d31ed3de23712b0
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-07-24 11:55:03 +0200

    Remove commented lines

--- tests/cli/workflow/taxprofiler/test_cli_taxprofiler_run.py
+++ tests/cli/workflow/taxprofiler/test_cli_taxprofiler_run.py
@@ -21,32 +21,3 @@ def test_without_options(cli_runner: CliRunner, taxprofiler_context: CGConfig):
     assert "Missing argument" in result.output
 
 
-# def test_with_config_use_nextflow(
-#    cli_runner: CliRunner,
-#    taxprofiler_context: CGConfig,
-#    caplog: LogCaptureFixture,
-#    taxprofiler_case_id: str,
-#    taxprofiler_config,
-# ):
-#    """Test command with case_id and config file using nextflow."""
-#    caplog.set_level(logging.INFO)
-#    # GIVEN case-id
-#    case_id: str = taxprofiler_case_id
-
-#    # GIVEN a mocked config for taxprofiler
-
-#    # WHEN dry running with dry specified
-#    result = cli_runner.invoke(
-#        run, [case_id, "--dry-run", "--use-nextflow"], obj=taxprofiler_context
-#    )
-
-#    # THEN command should execute successfully
-#    assert result.exit_code == EXIT_SUCCESS
-
-#    # THEN command should use nextflow
-#    assert "using nextflow" in caplog.text
-#    assert "path/to/bin/nextflow" in caplog.text
-#    assert "-work-dir" in caplog.text
-
-#    # THEN command should include resume flag
-#    assert "-resume" in caplog.text
