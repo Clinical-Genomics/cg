1cdc96dc1f38f084aee0060cb427c6571619b21b
seallard <sebastian.allard@scilifelab.se>
2023-08-21 13:06:16 +0200

    Update parameter naming

--- cg/cli/demultiplex/copy_novaseqx_data.py
+++ cg/cli/demultiplex/copy_novaseqx_data.py
@@ -22,8 +22,8 @@ def is_in_demultiplexed_runs(flow_cell: Path, demultiplexed_runs: Path) -> bool:
     return Path(demultiplexed_runs, flow_cell.name).exists()
 
 
-def get_latest_analysis_directory(flow_cell: Path) -> Optional[Path]:
-    analysis_path = Path(flow_cell, DemultiplexingDirsAndFiles.ANALYSIS)
+def get_latest_analysis_directory(flow_cell_dir: Path) -> Optional[Path]:
+    analysis_path = Path(flow_cell_dir, DemultiplexingDirsAndFiles.ANALYSIS)
 
     if not analysis_path.exists():
         return None
@@ -40,39 +40,39 @@ def get_sorted_analysis_versions(analysis_path: Path) -> List[Path]:
     return sorted_versions
 
 
-def is_queued_for_post_processing(flow_cell: Path) -> bool:
-    return Path(flow_cell, DemultiplexingDirsAndFiles.QUEUED_FOR_POST_PROCESSING).exists()
+def is_queued_for_post_processing(flow_cell_dir: Path) -> bool:
+    return Path(flow_cell_dir, DemultiplexingDirsAndFiles.QUEUED_FOR_POST_PROCESSING).exists()
 
 
-def copy_flow_cell_analysis_data(flow_cell: Path, destination: Path) -> None:
-    analysis: Path = get_latest_analysis_directory(flow_cell)
+def copy_flow_cell_analysis_data(flow_cell_dir: Path, destination: Path) -> None:
+    analysis: Path = get_latest_analysis_directory(flow_cell_dir)
     analysis_data = Path(analysis, DemultiplexingDirsAndFiles.DATA)
 
     hardlink_tree(src=analysis_data, dst=destination)
 
 
-def mark_as_demultiplexed(flow_cell: Path) -> None:
-    Path(flow_cell, DemultiplexingDirsAndFiles.DEMUX_COMPLETE).touch()
+def mark_as_demultiplexed(flow_cell_dir: Path) -> None:
+    Path(flow_cell_dir, DemultiplexingDirsAndFiles.DEMUX_COMPLETE).touch()
 
 
-def mark_flow_cell_as_queued_for_post_processing(flow_cell: Path) -> None:
-    Path(flow_cell, DemultiplexingDirsAndFiles.QUEUED_FOR_POST_PROCESSING).touch()
+def mark_flow_cell_as_queued_for_post_processing(flow_cell_dir: Path) -> None:
+    Path(flow_cell_dir, DemultiplexingDirsAndFiles.QUEUED_FOR_POST_PROCESSING).touch()
 
 
 def hardlink_tree(src: Path, dst: Path) -> None:
     shutil.copytree(src=src, dst=dst, copy_function=os.link)
 
 
-def is_ready_for_post_processing(flow_cell: Path, demultiplexed_runs: Path) -> bool:
-    analysis: Path = get_latest_analysis_directory(flow_cell)
+def is_ready_for_post_processing(flow_cell_dir: Path, demultiplexed_runs: Path) -> bool:
+    analysis: Path = get_latest_analysis_directory(flow_cell_dir)
 
     if not analysis:
         return False
 
     copy_completed = is_copied(analysis)
     analysis_completed = is_analyzed(analysis)
-    in_demultiplexed_runs = is_in_demultiplexed_runs(flow_cell, demultiplexed_runs)
-    post_processed = is_queued_for_post_processing(flow_cell)
+    in_demultiplexed_runs = is_in_demultiplexed_runs(flow_cell_dir, demultiplexed_runs)
+    post_processed = is_queued_for_post_processing(flow_cell_dir)
 
     return (
         copy_completed and analysis_completed and not in_demultiplexed_runs and not post_processed
