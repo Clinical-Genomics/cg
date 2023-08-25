0b88ee2b1cab25b29a2fafd394fa75344c1dfba3
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-08-25 16:56:45 +0200

    Fix another buig

--- cg/meta/demultiplex/status_db_storage_functions.py
+++ cg/meta/demultiplex/status_db_storage_functions.py
@@ -130,7 +130,7 @@ def update_sample_read_counts_in_status_db(
     """Update samples in status db with the sum of all read counts for the sample in the sequencing metrics table."""
     q30_threshold: int = get_q30_threshold(flow_cell_data.sequencer_type)
     sample_internal_ids: List[str] = get_sample_internal_ids_from_sample_sheet(
-        sample_sheet_path=flow_cell_data.sample_sheet_path,
+        sample_sheet_path=flow_cell_data.get_sample_sheet_path_hk(),
         flow_cell_sample_type=flow_cell_data.sample_type,
     )
     for sample_id in sample_internal_ids:
