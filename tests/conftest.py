9e1c6a06154be5ceeb7e162b6352d251ba3a9ab7
Sebastian Allard <sebastian.allard@scilifelab.se>
2023-08-25 15:11:15 +0200

    Store and use q30 metrics as percentages (#2352)(patch)
    - Consistently use percentages for the q30 metrics

--- tests/conftest.py
+++ tests/conftest.py
@@ -2900,7 +2900,7 @@ def store_with_sequencing_metrics(
         flow_cell_name_,
         flow_cell_lane_number,
         sample_total_reads_in_lane,
-        sample_base_fraction_passing_q30,
+        sample_base_percentage_passing_q30,
         sample_base_mean_quality_score,
     ) in sample_sequencing_metrics_details:
         helpers.add_sample_lane_sequencing_metrics(
@@ -2909,7 +2909,7 @@ def store_with_sequencing_metrics(
             flow_cell_name=flow_cell_name_,
             flow_cell_lane_number=flow_cell_lane_number,
             sample_total_reads_in_lane=sample_total_reads_in_lane,
-            sample_base_fraction_passing_q30=sample_base_fraction_passing_q30,
+            sample_base_percentage_passing_q30=sample_base_percentage_passing_q30,
             sample_base_mean_quality_score=sample_base_mean_quality_score,
         )
 
