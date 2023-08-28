0b034a507aff5a07fbba82ce0d80fb412970b8d7
seallard <sebastian.allard@scilifelab.se>
2023-08-28 16:23:23 +0200

    Replace deprecated parse_obj on model

--- cg/apps/tb/validators.py
+++ cg/apps/tb/validators.py
@@ -1,6 +1,6 @@
 import datetime as dt
 from pathlib import Path
-from typing import Optional
+from typing import Dict, Optional
 
 from dateutil.parser import parse as parse_datestr
 
@@ -10,7 +10,7 @@ def parse_str_to_datetime(value: str) -> Optional[dt.datetime]:
         return parse_datestr(value)
 
 
-def inherit_family_value(value: str, values: dict) -> Optional[str]:
+def inherit_family_value(value: str, values: Dict) -> Optional[str]:
     return values.get("family")
 
 
