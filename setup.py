4b718f703ca2fc1c483aac3ed9bc6a3fb68d1006
Vincent Janvid <vincent.janvid@scilifelab.se>
2023-08-29 11:07:37 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -52,7 +52,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     packages=find_packages(exclude=("tests*", "docs", "examples")),
-    entry_points={"console_scripts": ["cg=cg.cli:base"]},
+    entry_points={"console_scripts": ["cg=cg.cli.base:base"]},
     install_requires=parse_requirements(),
     tests_require=parse_requirements("requirements-dev.txt"),
     classifiers=[
