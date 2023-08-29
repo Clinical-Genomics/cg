045ed4fc9331c74fdd3622ad084fadec699e379f
seallard <sebastian.allard@scilifelab.se>
2023-08-29 09:25:21 +0200

    Fix default values

--- cg/apps/tb/models.py
+++ cg/apps/tb/models.py
@@ -1,33 +1,37 @@
 from typing import Optional
 
-from pydantic import AfterValidator, BaseModel, ConfigDict
+from pydantic import AfterValidator, BaseModel, ConfigDict, FieldValidationInfo, field_validator
 from typing_extensions import Annotated
 
-from cg.apps.tb.validators import inherit_family_value, parse_str_to_datetime, parse_str_to_path
+from cg.apps.tb.validators import parse_str_to_datetime, parse_str_to_path
 
 
 class TrailblazerAnalysis(BaseModel):
     id: int
     family: str
-    case_id: Annotated[Optional[str], AfterValidator(inherit_family_value)]
 
-    version: Optional[str]
+    @field_validator("case_id")
+    def inherit_family_value(cls, value: str, info: FieldValidationInfo) -> str:
+        return info.data.get("family")
+
+    version: Optional[str] = None
     logged_at: Annotated[Optional[str], AfterValidator(parse_str_to_datetime)]
     started_at: Annotated[Optional[str], AfterValidator(parse_str_to_datetime)]
     completed_at: Annotated[Optional[str], AfterValidator(parse_str_to_datetime)]
-    status: Optional[str]
-    priority: Optional[str]
+    status: Optional[str] = None
+    priority: Optional[str] = None
     out_dir: Annotated[Optional[str], AfterValidator(parse_str_to_path)]
     config_path: Annotated[Optional[str], AfterValidator(parse_str_to_path)]
-    comment: Optional[str]
-    is_deleted: Optional[bool]
-    is_visible: Optional[bool]
-    type: Optional[str]
-    user_id: Optional[int]
+    comment: Optional[str] = None
+    is_deleted: Optional[bool] = None
+    is_visible: Optional[bool] = None
+    type: Optional[str] = None
+    user_id: Optional[int] = None
     progress: Optional[float] = 0.0
-    data_analysis: Optional[str]
-    ticket: Optional[str]
-    uploaded_at: Optional[str]
+    data_analysis: Optional[str] = None
+    ticket: Optional[str] = None
+    uploaded_at: Optional[str] = None
+
     model_config = ConfigDict(
         arbitrary_types_allowed=True,
         validate_default=True,
--- cg/apps/tb/validators.py
+++ cg/apps/tb/validators.py
@@ -1,6 +1,6 @@
 import datetime as dt
 from pathlib import Path
-from typing import Dict, Optional
+from typing import Optional
 
 from dateutil.parser import parse as parse_datestr
 
@@ -8,10 +8,6 @@ from dateutil.parser import parse as parse_datestr
 def parse_str_to_datetime(value: str) -> Optional[dt.datetime]:
     if value:
         return parse_datestr(value)
-
-
-def inherit_family_value(value: str, values: Dict) -> Optional[str]:
-    return values.get("family")
 
 
 def parse_str_to_path(value: str) -> Optional[Path]:
