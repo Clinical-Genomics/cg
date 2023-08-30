33bf5dcf58695dade876fba6d6574b04017d42d3
Eva Caceres <eva.caceres@scilifelab.se>
2023-08-30 13:17:39 +0200

    docstring

--- cg/io/csv.py
+++ cg/io/csv.py
@@ -9,7 +9,7 @@ from cg.io.validate_path import validate_file_suffix
 
 
 def read_csv(file_path: Path, read_to_dict: bool = False) -> Union[List[List[str]], List[dict]]:
-    """Read content in a CSV file to a list of list, list of dict or a single string."""
+    """Read content in a CSV file to a list of list or list of dict."""
     validate_file_suffix(path_to_validate=file_path, target_suffix=FileExtensions.CSV)
     with open(file_path, "r") as file:
         csv_reader = csv.DictReader(file) if read_to_dict else csv.reader(file)
