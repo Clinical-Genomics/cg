b33e1ccc34e6dd895331bb1ccc50936378dafd49
Eva Caceres <eva.caceres@scilifelab.se>
2023-08-31 10:35:27 +0200

    pytest

--- tests/cli/workflow/rnafusion/test_cli_rnafusion_compound_commands.py
+++ tests/cli/workflow/rnafusion/test_cli_rnafusion_compound_commands.py
@@ -1,4 +1,5 @@
 import logging
+from typing import List
 
 from _pytest.logging import LogCaptureFixture
 from click.testing import CliRunner
@@ -10,6 +11,7 @@ from cg.cli.workflow.rnafusion.base import rnafusion, start, start_available, st
 from cg.constants import EXIT_SUCCESS
 from cg.meta.workflow.rnafusion import RnafusionAnalysisAPI
 from cg.models.cg_config import CGConfig
+from cg.models.rnafusion.rnafusion import RnafusionDeliverables
 
 
 def test_rnafusion_no_args(cli_runner: CliRunner, rnafusion_context: CGConfig):
@@ -66,48 +68,69 @@ def test_store_success(
     hermes_deliverables: dict,
     mocker,
     rnafusion_case_id: str,
+    deliverables_template_content: List[dict],
 ):
     """Test to ensure all parts of store command are run successfully given ideal conditions."""
     caplog.set_level(logging.INFO)
 
-    # GIVEN case-id for which we created a config file, deliverables file, and analysis_finish file
-    case_id: str = rnafusion_case_id
+    # GIVEN a valid and successful case
+
+    # GIVEN a mocked deliverables template
+    mocker.patch.object(
+        RnafusionDeliverables,
+        "get_deliverables_template",
+        return_value=deliverables_template_content,
+    )
 
     # Set Housekeeper to an empty real Housekeeper store
     rnafusion_context.housekeeper_api_: HousekeeperAPI = real_housekeeper_api
     rnafusion_context.meta_apis["analysis_api"].housekeeper_api = real_housekeeper_api
 
     # Make sure the bundle was not present in hk
-    assert not rnafusion_context.housekeeper_api.bundle(case_id)
+    assert not rnafusion_context.housekeeper_api.bundle(rnafusion_case_id)
 
     # Make sure analysis not already stored in status_db
-    assert not rnafusion_context.status_db.get_case_by_internal_id(internal_id=case_id).analyses
+    assert not rnafusion_context.status_db.get_case_by_internal_id(
+        internal_id=rnafusion_case_id
+    ).analyses
 
     # GIVEN that HermesAPI returns a deliverables output
     mocker.patch.object(HermesApi, "convert_deliverables")
     HermesApi.convert_deliverables.return_value = CGDeliverables(**hermes_deliverables)
 
     # WHEN running command
-    result = cli_runner.invoke(store, [case_id], obj=rnafusion_context)
+    result = cli_runner.invoke(store, [rnafusion_case_id], obj=rnafusion_context)
     # THEN bundle should be successfully added to HK and STATUSDB
     assert result.exit_code == EXIT_SUCCESS
     assert "Analysis successfully stored in Housekeeper" in caplog.text
     assert "Analysis successfully stored in StatusDB" in caplog.text
-    assert rnafusion_context.status_db.get_case_by_internal_id(internal_id=case_id).analyses
-    assert rnafusion_context.housekeeper_api.bundle(case_id)
+    assert rnafusion_context.status_db.get_case_by_internal_id(
+        internal_id=rnafusion_case_id
+    ).analyses
+    assert rnafusion_context.housekeeper_api.bundle(rnafusion_case_id)
 
 
 def test_store_fail(
     cli_runner: CliRunner,
+    rnafusion_case_id: str,
     rnafusion_context: CGConfig,
     real_housekeeper_api: HousekeeperAPI,
+    deliverables_template_content: List[dict],
     caplog: LogCaptureFixture,
+    mocker,
 ):
     """Test store command fails when a case did not finish."""
     caplog.set_level(logging.INFO)
 
     # GIVEN CASE ID where analysis finish is not mocked
-    case_id_fail: str = "rnafusion_case_not_finished"
+    case_id_fail: str = rnafusion_case_id
+
+    # GIVEN a mocked deliverables template
+    mocker.patch.object(
+        RnafusionDeliverables,
+        "get_deliverables_template",
+        return_value=deliverables_template_content,
+    )
 
     # Set Housekeeper to an empty real Housekeeper store
     rnafusion_context.housekeeper_api_: HousekeeperAPI = real_housekeeper_api
@@ -121,36 +144,6 @@ def test_store_fail(
 
     # THEN bundle exist status should be
     assert result_fail.exit_code != EXIT_SUCCESS
-
-
-def test_start_available(
-    cli_runner: CliRunner,
-    rnafusion_context: CGConfig,
-    caplog: LogCaptureFixture,
-    mocker,
-    rnafusion_case_id: str,
-):
-    """Test to ensure all parts of compound start-available command are executed given ideal conditions
-    Test that start-available picks up eligible cases and does not pick up ineligible ones."""
-    caplog.set_level(logging.INFO)
-
-    # GIVEN CASE ID of sample where read counts pass threshold
-    case_id_success: str = rnafusion_case_id
-
-    # GIVEN a mocked config
-
-    # GIVEN decompression is not needed
-    mocker.patch.object(RnafusionAnalysisAPI, "resolve_decompression")
-    RnafusionAnalysisAPI.resolve_decompression.return_value = None
-
-    # WHEN running command
-    result = cli_runner.invoke(start_available, ["--dry-run"], obj=rnafusion_context)
-
-    # THEN command exits with 0
-    assert result.exit_code == EXIT_SUCCESS
-
-    # THEN it should successfully identify the one case eligible for auto-start
-    assert case_id_success in caplog.text
 
 
 def test_store_available(
@@ -171,6 +164,13 @@ def test_store_available(
 
     # GIVEN CASE ID of sample where read counts pass threshold
     case_id_success: str = rnafusion_case_id
+
+    # GIVEN a mocked deliverables template
+    mocker.patch.object(
+        RnafusionDeliverables,
+        "get_deliverables_template",
+        return_value=deliverables_template_content,
+    )
 
     # GIVEN that HermesAPI returns a deliverables output
     mocker.patch.object(HermesApi, "convert_deliverables")
@@ -208,3 +208,33 @@ def test_store_available(
 
     # THEN bundle added successfully and action set to None
     assert rnafusion_context.status_db.get_case_by_internal_id(case_id_success).action is None
+
+
+def test_start_available(
+    cli_runner: CliRunner,
+    rnafusion_context: CGConfig,
+    caplog: LogCaptureFixture,
+    mocker,
+    rnafusion_case_id: str,
+):
+    """Test to ensure all parts of compound start-available command are executed given ideal conditions
+    Test that start-available picks up eligible cases and does not pick up ineligible ones."""
+    caplog.set_level(logging.INFO)
+
+    # GIVEN CASE ID of sample where read counts pass threshold
+    case_id_success: str = rnafusion_case_id
+
+    # GIVEN a mocked config
+
+    # GIVEN decompression is not needed
+    mocker.patch.object(RnafusionAnalysisAPI, "resolve_decompression")
+    RnafusionAnalysisAPI.resolve_decompression.return_value = None
+
+    # WHEN running command
+    result = cli_runner.invoke(start_available, ["--dry-run"], obj=rnafusion_context)
+
+    # THEN command exits with 0
+    assert result.exit_code == EXIT_SUCCESS
+
+    # THEN it should successfully identify the one case eligible for auto-start
+    assert case_id_success in caplog.text
