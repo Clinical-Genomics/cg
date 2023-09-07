3efe4e6a011db2eae71bcbe683d5f2c2c1aad4a9
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-09-07 17:33:50 +0200

    Fix code smell

--- cg/cli/workflow/taxprofiler/base.py
+++ cg/cli/workflow/taxprofiler/base.py
@@ -203,7 +203,7 @@ def start_available(context: click.Context, dry_run: bool = False) -> None:
     exit_code: int = EXIT_SUCCESS
     for case in analysis_api.get_cases_to_analyze():
         try:
-            context.invoke(start, case_id=case_obj.internal_id, dry_run=dry_run)
+            context.invoke(start, case_id=case.internal_id, dry_run=dry_run)
         except CgError as error:
             LOG.error(error)
             exit_code = EXIT_FAIL
