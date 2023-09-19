da4a3793d3b27a538739b84d5f55bc0954d9b069
seallard <sebastian.allard@scilifelab.se>
2023-09-19 14:56:34 +0200

    Improve test for bcl2fastq undetermined metrics

--- tests/apps/sequencing_metrics_parser/parsers/test_bcl2fastq_to_sequencing_statistics.py
+++ tests/apps/sequencing_metrics_parser/parsers/test_bcl2fastq_to_sequencing_statistics.py
@@ -10,9 +10,9 @@ from cg.store.models import SampleLaneSequencingMetrics
 
 def test_create_sequencing_statistics_from_bcl2fastq_flow_cell(bcl2fastq_flow_cell_path: Path):
     """Test creating metrics for undetermined reads on a flow cell demultiplexed with bcl2fastq."""
-    # GIVEN a flow cell demultiplexed with bcl2fastq
+    # GIVEN a flow cell demultiplexed with bcl2fastq with one lane, one sample and two undetermined reads
 
-    # WHEN creating undetermined metrics for an existing lane on the flow cell
+    # WHEN creating undetermined metrics for the lane and sample
     metrics: List[SampleLaneSequencingMetrics] = create_bcl2fastq_undetermined_metrics(
         bcl2fastq_flow_cell_path=bcl2fastq_flow_cell_path,
         non_pooled_lane_sample_pairs=[(1, "sample_id")],
@@ -21,9 +21,14 @@ def test_create_sequencing_statistics_from_bcl2fastq_flow_cell(bcl2fastq_flow_ce
     # THEN a list of metrics is returned
     assert isinstance(metrics, list)
     assert all(isinstance(item, SampleLaneSequencingMetrics) for item in metrics)
+    assert len(metrics) == 1
 
-    # THEN the metrics has been assigned the correct sample id
+    # THEN the undetermined metrics have been assigned the correct sample id and lane number
     assert metrics[0].sample_internal_id == "sample_id"
+    assert metrics[0].flow_cell_lane_number == 1
+
+    # THEN the two undetermined paired reads in each tile were combined
+    assert metrics[0].sample_total_reads_in_lane == 4
 
 
 def test_create_undetermined_metrics_for_invalid_lane(bcl2fastq_flow_cell_path: Path):
