e3575f59c99bf52aebbbe4bd9153d8e906436aef
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2023-09-21 14:31:09 +0200

    Remove get_cases_to_store from microsalt api

--- tests/meta/workflow/test_microsalt.py
+++ tests/meta/workflow/test_microsalt.py
@@ -1,12 +1,11 @@
 """Tests for MicroSALT analysis."""
+import logging
+from pathlib import Path
 from typing import List
 
 from cg.apps.tb import TrailblazerAPI
 from cg.meta.workflow.microsalt import MicrosaltAnalysisAPI
 from cg.models.cg_config import CGConfig
-from pathlib import Path
-import logging
-
 from cg.models.orders.sample_base import ControlEnum
 from cg.store import Store
 from cg.store.models import Family
@@ -107,88 +106,6 @@ def test_qc_check_negative_control_fail(
     assert not qc_pass
     assert "failed" in caplog.text
     assert "Negative control sample" in caplog.text
-
-
-def test_get_cases_to_store_pass(
-    qc_microsalt_context: CGConfig,
-    caplog,
-    mocker,
-    microsalt_qc_pass_lims_project: str,
-    microsalt_case_qc_pass: str,
-    microsalt_qc_pass_run_dir_path: Path,
-):
-    """Test get cases to store for a microsalt case that passes QC."""
-
-    caplog.set_level(logging.INFO)
-    store = qc_microsalt_context.status_db
-    microsalt_api: MicrosaltAnalysisAPI = qc_microsalt_context.meta_apis["analysis_api"]
-    mocker.patch.object(MicrosaltAnalysisAPI, "create_qc_done_file")
-    mocker.patch.object(TrailblazerAPI, "set_analysis_status")
-    mocker.patch.object(TrailblazerAPI, "add_comment")
-
-    # GIVEN a store with a QC ready microsalt case that will pass QC
-    microsalt_pass_case: Family = store.get_case_by_internal_id(internal_id=microsalt_case_qc_pass)
-    microsalt_pass_case.samples[1].control = "negative"
-    microsalt_pass_case.samples[1].reads = 1100000
-
-    mocker.patch.object(
-        MicrosaltAnalysisAPI,
-        "get_completed_cases",
-        return_value=[microsalt_pass_case],
-    )
-    mocker.patch.object(
-        MicrosaltAnalysisAPI, "get_project", return_value=microsalt_qc_pass_lims_project
-    )
-
-    mocker.patch.object(
-        MicrosaltAnalysisAPI, "get_latest_case_path", return_value=microsalt_qc_pass_run_dir_path
-    )
-
-    # WHEN get cases to store
-    cases_to_store: List[Family] = microsalt_api.get_cases_to_store()
-
-    # THEN it should be stored
-    assert microsalt_pass_case in cases_to_store
-
-
-def test_get_cases_to_store_fail(
-    qc_microsalt_context: CGConfig,
-    caplog,
-    mocker,
-    microsalt_qc_fail_lims_project: str,
-    microsalt_case_qc_fail: str,
-    microsalt_qc_fail_run_dir_path: Path,
-):
-    """Test get cases to store for a microsalt case that fails QC."""
-
-    caplog.set_level(logging.INFO)
-    store = qc_microsalt_context.status_db
-    microsalt_api: MicrosaltAnalysisAPI = qc_microsalt_context.meta_apis["analysis_api"]
-    mocker.patch.object(MicrosaltAnalysisAPI, "create_qc_done_file")
-    mocker.patch.object(TrailblazerAPI, "set_analysis_status")
-    mocker.patch.object(TrailblazerAPI, "add_comment")
-
-    # GIVEN a store with a QC ready microsalt case that will fail QC
-    microsalt_fail_case: Family = store.get_case_by_internal_id(internal_id=microsalt_case_qc_fail)
-
-    mocker.patch.object(
-        MicrosaltAnalysisAPI,
-        "get_completed_cases",
-        return_value=[microsalt_fail_case],
-    )
-    mocker.patch.object(
-        MicrosaltAnalysisAPI, "get_project", return_value=microsalt_qc_fail_lims_project
-    )
-
-    mocker.patch.object(
-        MicrosaltAnalysisAPI, "get_latest_case_path", return_value=microsalt_qc_fail_run_dir_path
-    )
-
-    # WHEN get case to store
-    cases_to_store: List[Family] = microsalt_api.get_cases_to_store()
-
-    # Then it should not be stored
-    assert microsalt_fail_case not in cases_to_store
 
 
 def test_get_latest_case_path(
