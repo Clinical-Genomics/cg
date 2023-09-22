d72e08b78c2e82972a5f0f2560d1ff097563d82d
Vincent Janvid <vincent.janvid@scilifelab.se>
2023-09-22 13:10:49 +0200

    Replace per field validator with model validator

--- cg/meta/upload/gisaid/models.py
+++ cg/meta/upload/gisaid/models.py
@@ -1,7 +1,13 @@
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Any
 from typing_extensions import Annotated
-from pydantic import BaseModel, BeforeValidator, FieldValidationInfo, field_validator
+from pydantic import (
+    BaseModel,
+    BeforeValidator,
+    FieldValidationInfo,
+    field_validator,
+    model_validator,
+)
 from datetime import datetime
 
 from cg.meta.upload.gisaid.constants import AUTHORS
@@ -58,22 +64,35 @@ class GisaidSample(BaseModel):
     covv_subm_lab_addr: Optional[str] = "171 76 Stockholm, Sweden"
     covv_authors: Optional[str] = " ,".join(AUTHORS)
 
-    @field_validator("covv_location")
+    @model_validator(mode="before")
     @classmethod
-    def parse_location(cls, _, info: FieldValidationInfo):
-        region: str = info.data.get("region")
-        return f"Europe/Sweden/{region}"
+    def set_generated_fields(cls, data: Any):
+        if isinstance(data, dict):
+            data.setdefault("covv_location", _generate_covv_location(data.get("region")))
+            data["covv_subm_sample_id"] = _generate_covv_subm_sample_id(
+                subm_sample_id=data.get(
+                    "covv_subm_sample_id",
+                ),
+                region_code=data.get("region_code"),
+            )
+            data.setdefault(
+                "covv_virus_name",
+                _generate_covv_virus_name(
+                    covv_subm_sample_id=data.get("covv_subm_sample_id"),
+                    covv_collection_date=data.get("covv_collection_date"),
+                ),
+            )
+        return data
 
-    @field_validator("covv_subm_sample_id")
-    @classmethod
-    def parse_subm_sample_id(cls, v: str, info: FieldValidationInfo):
-        region_code = info.data.get("region_code")
-        return f"{region_code}_SE100_{v}"
 
-    @field_validator("covv_virus_name")
-    @classmethod
-    def parse_virus_name(cls, _, info: FieldValidationInfo):
-        sample_name = info.data.get("covv_subm_sample_id")
-        date = info.data.get("covv_collection_date")
-        datetime_date = datetime.strptime(date, "%Y-%m-%d")
-        return f"hCoV-19/Sweden/{sample_name}/{datetime_date.year}"
+def _generate_covv_location(region):
+    return f"Europe/Sweden/{region}"
+
+
+def _generate_covv_subm_sample_id(subm_sample_id, region_code):
+    return f"{region_code}_SE100_{subm_sample_id}"
+
+
+def _generate_covv_virus_name(covv_subm_sample_id, covv_collection_date):
+    datetime_date = datetime.strptime(covv_collection_date, "%Y-%m-%d")
+    return f"hCoV-19/Sweden/{covv_subm_sample_id}/{datetime_date.year}"
