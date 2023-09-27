54add34a2d540bc05aea57078b774b455c18fac7
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-09-27 14:07:20 +0200

    feat(refactor):

--- cg/cli/backup.py
+++ cg/cli/backup.py
@@ -64,7 +64,7 @@ def encrypt_flow_cell(context: CGConfig, dry_run: bool):
         flow_cell_encrypt_dir = Path(encrypt_dir, flow_cell.id)
         flow_cell_encrypt_file_path_prefix = Path(flow_cell_encrypt_dir, flow_cell.id)
         pending_file_path: Path = flow_cell_encrypt_file_path_prefix.with_suffix(".pending")
-        if flow_cell_encrypt_dir.exists() and pending_file_path.exists():
+        if pending_file_path.exists():
             LOG.debug(f"Encryption already started for flow cell: {flow_cell.id}")
             continue
 
