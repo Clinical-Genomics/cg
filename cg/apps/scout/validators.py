ef6d091d1acf6f431c14189e53bc20acdf5d379b
islean <isak.ohlsson.angnell@gmail.com>
2023-10-09 16:12:41 +0200

    Added test

--- cg/apps/scout/validators.py
+++ cg/apps/scout/validators.py
@@ -4,7 +4,7 @@ from cg.constants.subject import Gender, PlinkGender, RelationshipStatus
 
 
 def set_parent_if_missing(parent: Optional[str]) -> str:
-    return str(RelationshipStatus.HAS_NO_PARENT) if parent is None else parent
+    return str(RelationshipStatus.HAS_NO_PARENT.value) if parent is None else parent
 
 
 def set_gender_if_other(gender: Optional[str]) -> str:
