c4f9a2dc5ce84e1a04b9488477df2d4e4dede0cc
Vincent Janvid <vincent.janvid@scilifelab.se>
2023-10-23 11:15:28 +0200

    Moved code to other PR

--- cg/cli/demultiplex/demux.py
+++ cg/cli/demultiplex/demux.py
@@ -59,7 +59,6 @@ def demultiplex_all(context: CGConfig, flow_cells_directory: click.Path, dry_run
             continue
 
         if not dry_run:
-            demultiplex_api.remove_demultiplexing_output_directory(flow_cell)
             slurm_job_id: int = demultiplex_api.start_demultiplexing(flow_cell=flow_cell)
             tb_api: TrailblazerAPI = context.trailblazer_api
             demultiplex_api.add_to_trailblazer(
@@ -165,7 +164,14 @@ def delete_flow_cell(
         f"Run-dir={True if status_db else run_dir}\nStatusdb={status_db}\n"
         f"\nSample-lane-sequencing-metrics={True if sample_lane_sequencing_metrics else sample_lane_sequencing_metrics}"
     ):
-        delete_demux_api.delete_flow_cell()
+        delete_demux_api.delete_flow_cell(
+            demultiplexing_dir=demultiplexing_dir,
+            housekeeper=housekeeper,
+            init_files=init_files,
+            sample_lane_sequencing_metrics=sample_lane_sequencing_metrics,
+            run_dir=run_dir,
+            status_db=status_db,
+        )
 
 
 @click.command(name="copy-completed-flow-cell")
