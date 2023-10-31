2a23567af687db79acb52917a56757c5775bc80d
seallard <sebastian.allard@scilifelab.se>
2023-10-31 15:25:00 +0100

    Update down revision

--- alembic/versions/2023_10_31_fce8a2ca0fd1_add_is_compressible.py
+++ alembic/versions/2023_10_31_fce8a2ca0fd1_add_is_compressible.py
@@ -1,17 +1,16 @@
 """Add is_compressible
 
 Revision ID: fce8a2ca0fd1
-Revises: b6f00cc615cf
+Revises: 9073c61bc72b
 Create Date: 2023-10-31 10:18:11.450637
 
 """
 from alembic import op
 import sqlalchemy as sa
-from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
 revision = "fce8a2ca0fd1"
-down_revision = "b6f00cc615cf"
+down_revision = "9073c61bc72b"
 branch_labels = None
 depends_on = None
 
