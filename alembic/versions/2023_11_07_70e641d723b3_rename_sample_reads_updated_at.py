47366714568dd549404f36f5c440dbb9410ccdfb
seallard <sebastian.allard@scilifelab.se>
2023-11-07 10:07:27 +0100

    Add revision for renaming column

---
+++ alembic/versions/2023_11_07_70e641d723b3_rename_sample_reads_updated_at.py
@@ -0,0 +1,24 @@
+"""Rename sample reads_updated_at
+
+Revision ID: 70e641d723b3
+Revises: fce8a2ca0fd1
+Create Date: 2023-11-07 10:05:05.324260
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
+
+# revision identifiers, used by Alembic.
+revision = "70e641d723b3"
+down_revision = "fce8a2ca0fd1"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column("sample", "reads_updated_at", new_column_name="last_sequenced_at")
+
+
+def downgrade():
+    op.alter_column("sample", "last_sequenced_at", new_column_name="reads_updated_at")
