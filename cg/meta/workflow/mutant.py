a1aca641f273b4001f2a82397a88682dbfcde745
beatrizsavinhas <beatriz.sa.vinhas@scilifelab.se>
2023-11-07 14:09:47 +0100

    Add structure for logic

--- cg/meta/workflow/mutant.py
+++ cg/meta/workflow/mutant.py
@@ -189,10 +189,18 @@ class MutantAnalysisAPI(AnalysisAPI):
     def get_cases_to_store(self) -> list[Case]:
         """Return cases where analysis has a deliverables file,
         and is ready to be stored in Housekeeper."""
+
+        # Gather running cases with deliverables
+        cases = case in self.status_db.get_running_cases_in_pipeline(pipeline=self.pipeline) if Path(self.get_deliverables_file_path(case_id=case.internal_id)).exists()
+
+        # send each case to qc check
+        for case in cases:
+            qc_check(case)
+
+        # Then return them to be stored
         return [
             case
-            for case in self.status_db.get_running_cases_in_pipeline(pipeline=self.pipeline)
-            if Path(self.get_deliverables_file_path(case_id=case.internal_id)).exists()
+            for case in cases
         ]
 
     def get_metadata_for_nanopore_sample(self, sample_obj: Sample) -> list[dict]:
