3195d339166a9dca5717daad98e0520dfcb07b36
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-11-21 18:06:40 +0100

    Black format

--- cg/meta/workflow/nf_analysis.py
+++ cg/meta/workflow/nf_analysis.py
@@ -310,7 +310,7 @@ class NfAnalysisAPI(AnalysisAPI):
         for key in multiqc_json.report_general_stats_data:
             if case_id in key:
                 metrics_values.update(list(key.values())[0])
-        for key, value in multiqc_json['report_general_stats_data'].items():
+        for key, value in multiqc_json["report_general_stats_data"].items():
             LOG.info(f"{key}: {value}")
         return [
             MetricsBase(
