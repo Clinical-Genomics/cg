5932c74db717d8a7c44176ba013bfb8203b9ca5a
Eva Caceres <eva.caceres@scilifelab.se>
2023-11-28 13:22:01 +0100

    add logo in report

--- cg/meta/report/rnafusion.py
+++ cg/meta/report/rnafusion.py
@@ -69,8 +69,8 @@ class RnafusionReportAPI(ReportAPI):
     def get_report_accreditation(
         self, samples: list[SampleModel], analysis_metadata: AnalysisModel
     ) -> bool:
-        """Checks if the report is accredited or not. Rnafusion is not an accredited workflow."""
-        return False
+        """Checks if the report is accredited or not. Rnafusion is an accredited workflow."""
+        return True
 
     def get_scout_uploaded_file_from_hk(self, case_id: str, scout_tag: str) -> Optional[str]:
         """Return file path of the uploaded to Scout file given its tag."""
