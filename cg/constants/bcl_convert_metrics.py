1c89442f3a2b973d1890af869cb28728bf5d6940
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-11-28 12:00:08 +0100

    feat(0663): Demux

--- cg/constants/bcl_convert_metrics.py
+++ cg/constants/bcl_convert_metrics.py
@@ -1,6 +1,6 @@
 """This module contains constants for the BCLConvert metrics files."""
 
-from enum import Enum
+from enum import StrEnum
 
 from cg.constants.demultiplexing import (
     BclConverter,
@@ -9,7 +9,7 @@ from cg.constants.demultiplexing import (
 )
 
 
-class BclConvertQualityMetricsColumnNames(Enum):
+class BclConvertQualityMetricsColumnNames(StrEnum):
     """Column names for the BCLConvert quality metrics file."""
 
     LANE: str = "Lane"
@@ -18,7 +18,7 @@ class BclConvertQualityMetricsColumnNames(Enum):
     Q30_BASES_PERCENT: str = "% Q30"
 
 
-SAMPLE_SHEET_HEADER = {
+SAMPLE_SHEET_HEADER: dict[[str, str]] = {
     BclConverter.BCL2FASTQ: ",".join(
         [
             column
@@ -43,7 +43,7 @@ SAMPLE_SHEET_HEADER = {
 }
 
 
-class BclConvertDemuxMetricsColumnNames(Enum):
+class BclConvertDemuxMetricsColumnNames(StrEnum):
     """Column names for the BCLConvert demultiplexing metrics file."""
 
     LANE: str = "Lane"
@@ -51,6 +51,6 @@ class BclConvertDemuxMetricsColumnNames(Enum):
     READ_PAIR_COUNT: str = "# Reads"
 
 
-DEMUX_METRICS_FILE_NAME = "Demultiplex_Stats.csv"
-QUALITY_METRICS_FILE_NAME = "Quality_Metrics.csv"
-ADAPTER_METRICS_FILE_NAME = "Adapter_Metrics.csv"
+DEMUX_METRICS_FILE_NAME: str = "Demultiplex_Stats.csv"
+QUALITY_METRICS_FILE_NAME: str = "Quality_Metrics.csv"
+ADAPTER_METRICS_FILE_NAME: str = "Adapter_Metrics.csv"
