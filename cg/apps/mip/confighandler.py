ab474b6d42bcb0cb7fcef472b827ecb7c567f8e9
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-11-30 09:26:51 +0100

    feat(json): Coerce

--- cg/apps/mip/confighandler.py
+++ cg/apps/mip/confighandler.py
@@ -81,10 +81,10 @@ class ConfigHandler:
             data_copy["samples"][0]["phenotype"] = "unaffected"
         for sample_data in data_copy["samples"]:
             sample_data["mother"]: str = (
-                sample_data.get("mother") or RelationshipStatus.HAS_NO_PARENT
+                sample_data.get("mother") or RelationshipStatus.HAS_NO_PARENT.value
             )
             sample_data["father"]: str = (
-                sample_data.get("father") or RelationshipStatus.HAS_NO_PARENT
+                sample_data.get("father") or RelationshipStatus.HAS_NO_PARENT.value
             )
             if sample_data["analysis_type"] == "wgs" and sample_data.get("capture_kit") is None:
                 sample_data["capture_kit"] = DEFAULT_CAPTURE_KIT
