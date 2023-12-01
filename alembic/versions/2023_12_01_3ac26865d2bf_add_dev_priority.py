94857b3b957ff60a17f917bc6059ec72b9eae03e
seallard <sebastian.allard@scilifelab.se>
2023-12-01 13:03:00 +0100

    Auto format

--- alembic/versions/2023_12_01_3ac26865d2bf_add_dev_priority.py
+++ alembic/versions/2023_12_01_3ac26865d2bf_add_dev_priority.py
@@ -8,13 +8,19 @@ Create Date: 2023-12-01 11:39:35.273991
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = '3ac26865d2bf'
-down_revision = 'b105b426af99'
+revision = "3ac26865d2bf"
+down_revision = "b105b426af99"
 branch_labels = None
 depends_on = None
 
+
 def upgrade():
-    op.execute("ALTER TABLE `case` MODIFY COLUMN priority ENUM('research', 'standard', 'priority', 'express', 'clinical_trials', 'development')")
+    op.execute(
+        "ALTER TABLE `case` MODIFY COLUMN priority ENUM('research', 'standard', 'priority', 'express', 'clinical_trials', 'development')"
+    )
+
 
 def downgrade():
-    op.execute("ALTER TABLE `case` MODIFY COLUMN priority ENUM('research', 'standard', 'priority', 'express', 'clinical_trials')")
+    op.execute(
+        "ALTER TABLE `case` MODIFY COLUMN priority ENUM('research', 'standard', 'priority', 'express', 'clinical_trials')"
+    )
