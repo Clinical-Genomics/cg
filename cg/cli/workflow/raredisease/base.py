fb15cb7f74a9565218551c8fb9238c34808691da
peterpru <peter.pruisscher@scilifelab.se>
2023-12-01 12:41:29 +0100

    add raredisease config

--- cg/cli/workflow/raredisease/base.py
+++ cg/cli/workflow/raredisease/base.py
@@ -3,10 +3,14 @@
 import logging
 
 import click
+from pydantic.v1 import ValidationError
 
-from cg.constants.constants import MetaApis
+from cg.constants.constants import DRY_RUN, MetaApis
 from cg.meta.workflow.analysis import AnalysisAPI
+from cg.cli.workflow.commands import ARGUMENT_CASE_ID
 from cg.meta.workflow.raredisease import RarediseaseAnalysisAPI
+from cg.models.cg_config import CGConfig
+from cg.exc import CgError
 
 LOG = logging.getLogger(__name__)
 
@@ -19,3 +23,19 @@ def raredisease(context: click.Context) -> None:
     context.obj.meta_apis[MetaApis.ANALYSIS_API] = RarediseaseAnalysisAPI(
         config=context.obj,
     )
+
+
+@raredisease.command("config-case")
+@ARGUMENT_CASE_ID
+@DRY_RUN
+@click.pass_obj
+def config_case(context: CGConfig, case_id: str, dry_run: bool) -> None:
+    """Create sample sheet file and params file for a given case."""
+    analysis_api: RarediseaseAnalysisAPI = context.meta_apis[MetaApis.ANALYSIS_API]
+    LOG.info(f"Creating config files for {case_id}.")
+    try:
+        analysis_api.status_db.verify_case_exists(case_internal_id=case_id)
+        analysis_api.config_case(case_id=case_id, dry_run=dry_run)
+    except (CgError, ValidationError) as error:
+        LOG.error(f"Could not create config files for {case_id}: {error}")
+        raise click.Abort() from error
--- cg/meta/workflow/raredisease.py
+++ cg/meta/workflow/raredisease.py
@@ -19,3 +19,26 @@ class RarediseaseAnalysisAPI(NfAnalysisAPI):
         pipeline: Pipeline = Pipeline.RAREDISEASE,
     ):
         super().__init__(config=config, pipeline=pipeline)
+
+    def config_case(
+        self,
+        case_id: str,
+        dry_run: bool,
+    ) -> None:
+        """Create config files (parameters and sample sheet) for Raredisease analysis."""
+        self.create_case_directory(case_id=case_id, dry_run=dry_run)
+        sample_sheet_content: list[list[Any]] = self.get_sample_sheet_content(
+            case_id=case_id, strandedness=strandedness
+        )
+        pipeline_parameters: RarediseaseParameters = self.get_pipeline_parameters(
+            case_id=case_id, genomes_base=genomes_base
+        )
+        if dry_run:
+            LOG.info("Dry run: Config files will not be written")
+            return
+        self.write_sample_sheet(
+            content=sample_sheet_content,
+            file_path=self.get_sample_sheet_path(case_id=case_id),
+            header=RarediseaseSampleSheetEntry.headers(),
+        )
+        self.write_params_file(case_id=case_id, pipeline_parameters=pipeline_parameters.dict())
