039209733e35ab2aa2c8fc292ad4ee3811346955
Vincent Janvid <69356202+Vince-janv@users.noreply.github.com>
2023-12-04 14:24:51 +0100

    Feat(Orders) Merged metagenome orderform with others (#2734)(patched)
    ### Changed
    
    - Metagenome orders now go via the 1508 sheet

--- cg/constants/orderforms.py
+++ cg/constants/orderforms.py
@@ -9,9 +9,9 @@ CONTAINER_TYPES = ["Tube", "96 well plate"]
 SOURCE_TYPES = set().union(METAGENOME_SOURCES, ANALYSIS_SOURCES)
 
 CASE_PROJECT_TYPES = [
-    str(OrderType.MIP_DNA),
-    str(OrderType.BALSAMIC),
-    str(OrderType.MIP_RNA),
+    OrderType.MIP_DNA,
+    OrderType.BALSAMIC,
+    OrderType.MIP_RNA,
 ]
 
 
@@ -20,8 +20,8 @@ class Orderform(StrEnum):
     BALSAMIC_QC: str = "1508"
     BALSAMIC_UMI: str = "1508"
     FASTQ: str = "1508"
+    METAGENOME: str = "1508"
     FLUFFY: str = "1604"
-    METAGENOME: str = "1605"
     MICROSALT: str = "1603"
     MIP_DNA: str = "1508"
     MIP_RNA: str = "1508"
@@ -29,11 +29,13 @@ class Orderform(StrEnum):
     RML: str = "1604"
     SARS_COV_2: str = "2184"
 
-
-ORDERFORM_VERSIONS = {
-    Orderform.MIP_DNA: "29",
-    Orderform.RML: "17",
-    Orderform.METAGENOME: "11",
-    Orderform.MICROSALT: "11",
-    Orderform.SARS_COV_2: "7",
-}
+    @staticmethod
+    def get_current_orderform_version(order_form: str) -> str:
+        """Returns the current version of the given order form."""
+        current_order_form_versions = {
+            Orderform.MIP_DNA: "30",
+            Orderform.RML: "17",
+            Orderform.MICROSALT: "11",
+            Orderform.SARS_COV_2: "7",
+        }
+        return current_order_form_versions[order_form]
