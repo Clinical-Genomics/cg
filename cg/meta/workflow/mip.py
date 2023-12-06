f529af3893667bd2ab024aaa0f19cdf9ab24064e
Beatriz Vinhas <beatriz.sa.vinhas@gmail.com>
2023-12-06 15:28:47 +0100

    Update mip.py

--- cg/meta/workflow/mip.py
+++ cg/meta/workflow/mip.py
@@ -183,6 +183,7 @@ class MipAnalysisAPI(AnalysisAPI):
 
         # add OMIM to every panel choice
         all_panels.add(GenePanelMasterList.OMIM_AUTO)
+        # add PANELAPP-GREEN to every panel choice
         all_panels.add(GenePanelMasterList.PANELAPP_GREEN)
 
         return list(all_panels)
