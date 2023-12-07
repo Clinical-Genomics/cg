b90bde8dbf0e7ad8bac80677fa950a25b281e657
Sofia Stamouli <91951607+sofstam@users.noreply.github.com>
2023-12-07 18:29:51 +0100

    Update tests/cli/workflow/taxprofiler/test_cli_taxprofiler_metrics_deliver.py
    Co-authored-by: Henrik Stranneheim <henrik.stranneheim@scilifelab.se>

--- tests/cli/workflow/taxprofiler/test_cli_taxprofiler_metrics_deliver.py
+++ tests/cli/workflow/taxprofiler/test_cli_taxprofiler_metrics_deliver.py
@@ -14,7 +14,7 @@ def test_without_options(cli_runner: CliRunner, taxprofiler_context: CGConfig):
     """Test command without case_id argument."""
     # GIVEN no case_id
 
-    # WHEN dry running without anything specified
+    # WHEN using dry run without any options
     result = cli_runner.invoke(metrics_deliver, obj=taxprofiler_context)
 
     # THEN command should NOT execute successfully
