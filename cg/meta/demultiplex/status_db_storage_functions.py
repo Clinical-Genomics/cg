94a7395ee0f44c11faeeb1c1c3a21820a444790a
Vincent Janvid <vincent.janvid@scilifelab.se>
2023-12-07 16:14:41 +0100

    Add commit in function

--- cg/meta/demultiplex/status_db_storage_functions.py
+++ cg/meta/demultiplex/status_db_storage_functions.py
@@ -166,3 +166,4 @@ def delete_sequencing_metrics_from_statusdb(flow_cell_id: str, store: Store) -> 
     ] = store.get_sample_lane_sequencing_metrics_by_flow_cell_name(flow_cell_id)
     for metric in sequencing_metrics:
         store.session.delete(metric)
+    store.session.commit()
