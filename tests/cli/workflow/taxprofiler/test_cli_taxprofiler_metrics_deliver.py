5d28fd69fcb151e91fee2b86c0f7a3644cbd0630
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2023-12-11 14:53:14 +0100

    Add file is created and proper content in test

--- tests/cli/workflow/taxprofiler/test_cli_taxprofiler_metrics_deliver.py
+++ tests/cli/workflow/taxprofiler/test_cli_taxprofiler_metrics_deliver.py
@@ -1,13 +1,17 @@
 """Tests for the metrics-deliver CLI command."""
 
 import logging
+from pathlib import Path
 
 from _pytest.logging import LogCaptureFixture
 from click.testing import CliRunner
 
 from cg.cli.workflow.taxprofiler.base import metrics_deliver
+from cg.meta.workflow.taxprofiler import TaxprofilerAnalysisAPI
 from cg.constants import EXIT_SUCCESS
 from cg.models.cg_config import CGConfig
+from cg.io.controller import ReadFile
+from cg.constants.constants import FileFormat
 
 
 def test_without_options(cli_runner: CliRunner, taxprofiler_context: CGConfig):
@@ -54,15 +58,30 @@ def test_metrics_deliver(
     taxprofiler_case_id: str,
     sample_name: str,
     another_sample_name: str,
+    taxprofiler_metrics_deliverables: Path,
 ):
     """Test command with a case id and a finished analysis which should execute successfully."""
     caplog.set_level(logging.INFO)
+
+    analysis_api: TaxprofilerAnalysisAPI = taxprofiler_context.meta_apis["analysis_api"]
     # GIVEN case-id
+
+    metrics_content: str = "name: filtering_result_passed_filter_reads\n"
 
     result = cli_runner.invoke(metrics_deliver, [taxprofiler_case_id], obj=taxprofiler_context)
 
     # THEN command should execute successfully
     assert result.exit_code == EXIT_SUCCESS
+
+    # THEN metrics deliverable file should be generated
+    assert taxprofiler_metrics_deliverables.is_file()
+
+    # THEN assert metrics_content in metrics_deliverables
+    with open(taxprofiler_metrics_deliverables, "r") as file:
+        metrics_deliverables_content = file.read()
+
+    # Check if the expected content is present in the file
+    assert metrics_content in metrics_deliverables_content
 
     # THEN taxprofiler and case_id should be found in command string
     assert "Writing metrics deliverables file to" in caplog.text
