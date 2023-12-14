899af792127f0d790d58ad800f22fcb817377af2
seallard <sebastian.allard@scilifelab.se>
2023-12-14 16:33:32 +0100

    Add new lines

--- cg/meta/workflow/microsalt/quality_controller/result_logger.py
+++ cg/meta/workflow/microsalt/quality_controller/result_logger.py
@@ -8,7 +8,7 @@ class ResultLogger:
     @staticmethod
     def log_results(sample_results: list[QualityResult], case_result: CaseQualityResult):
         if case_result.passes_qc:
-            LOG.info("Quality control passed.")
+            LOG.info("Quality control passed.\n")
         else:
             message = get_case_fail_message(case_result)
             LOG.warning(message)
@@ -21,13 +21,13 @@ def get_case_fail_message(case_result: CaseQualityResult) -> str:
     fail_reasons = []
 
     if not case_result.control_passes_qc:
-        fail_reasons.append("The negative control sample failed quality control.")
+        fail_reasons.append("The negative control sample failed quality control.\n")
     if not case_result.urgent_passes_qc:
-        fail_reasons.append("The urgent samples failed quality control.")
+        fail_reasons.append("The urgent samples failed quality control.\n")
     if not case_result.non_urgent_passes_qc:
-        fail_reasons.append("The non-urgent samples failed quality control.")
+        fail_reasons.append("The non-urgent samples failed quality control.\n")
 
-    fail_message = "Quality control failed."
+    fail_message = "Quality control failed.\n"
 
     return fail_message + " ".join(fail_reasons)
 
@@ -40,7 +40,7 @@ def sample_result_message(sample_results: list[QualityResult]) -> str:
     passed_count: int = len(passed_samples)
     total_count = len(sample_results)
 
-    return f"Sample results: {failed_count} failed, {passed_count} passed, {total_count} total"
+    return f"Sample results: {failed_count} failed, {passed_count} passed, {total_count} total.\n"
 
 
 def get_failed_results(results: list[QualityResult]) -> list[str]:
