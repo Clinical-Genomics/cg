cce1969c70dbe60264c2ec3f635b2d5f7c09d7f9
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2023-12-28 10:01:41 +0100

    feat(sql): Refactor addhandler

--- cg/store/api/core.py
+++ cg/store/api/core.py
@@ -1,18 +1,18 @@
 import logging
 
-from cg.store.api.add import AddHandler
 from cg.store.api.delete import DeleteDataHandler
 from cg.store.api.find_basic_data import FindBasicDataHandler
 from cg.store.api.find_business_data import FindBusinessDataHandler
 from cg.store.api.status import StatusHandler
 from cg.store.api.update import UpdateHandler
+from cg.store.crud.create import CreateHandler
 from cg.store.database import get_session
 
 LOG = logging.getLogger(__name__)
 
 
 class CoreHandler(
-    AddHandler,
+    CreateHandler,
     DeleteDataHandler,
     FindBasicDataHandler,
     FindBusinessDataHandler,
