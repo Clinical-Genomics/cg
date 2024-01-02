43e9f55b44b7c4710d222e3a7f7e5bcbcb334fdd
Christian Oertlin <c.oertlin@gmail.com>
2024-01-02 09:40:24 +0100

    add default values

--- cg/apps/deliverables_metrics_parser/models/pipeline_metrics_deliverables.py
+++ cg/apps/deliverables_metrics_parser/models/pipeline_metrics_deliverables.py
@@ -3,19 +3,19 @@ from pydantic import BaseModel
 
 
 class MIPDNAMetricsDeliverables(BaseModel):
-    sample_id: str
-    gender: str
-    PCT_PF_READS_IMPROPER_PAIRS: list[str]
-    MEAN_TARGET_COVERAGE: str
-    AT_DROPOUT: str
-    raw_total_sequences: list[int]
-    fraction_duplicates: float
-    MEDIAN_TARGET_COVERAGE: int
-    percentage_mapped_reads: list[int]
-    PCT_TARGET_BASES_10X: float
-    reads_mapped: list[int]
-    GC_DROPOUT: float
-    MEAN_INSERT_SIZE: float
-    PCT_OFF_BAIT: float
-    PCT_TARGET_BASES_20X: float
-    FOLD_80_BASE_PENALTY: float
+    sample_id: str = None
+    gender: str = None
+    PCT_PF_READS_IMPROPER_PAIRS: list[str] = None
+    MEAN_TARGET_COVERAGE: str = None
+    AT_DROPOUT: str = None
+    raw_total_sequences: list[int] = None
+    fraction_duplicates: float = None
+    MEDIAN_TARGET_COVERAGE: int = None
+    percentage_mapped_reads: list[int] = None
+    PCT_TARGET_BASES_10X: float = None
+    reads_mapped: list[int] = None
+    GC_DROPOUT: float = None
+    MEAN_INSERT_SIZE: float = None
+    PCT_OFF_BAIT: float = None
+    PCT_TARGET_BASES_20X: float = None
+    FOLD_80_BASE_PENALTY: float = None
