c8fb9509693337c936f4bf51f00417a80051ccc6
islean <isak.ohlsson.angnell@gmail.com>
2024-01-04 16:44:19 +0100

    Rework metadata list

--- cg/meta/archive/ddn/utils.py
+++ cg/meta/archive/ddn/utils.py
@@ -4,10 +4,11 @@ from cg.store.models import Sample
 
 def get_metadata(sample: Sample) -> list[dict]:
     """Returns metadata generated from a sample."""
-    metadata: list[dict] = []
-    metadata.append({MetadataFields.CUSTOMER_NAME.value: sample.customer.name})
-    metadata.append({MetadataFields.PREP_CATEGORY.value: sample.prep_category})
-    metadata.append({MetadataFields.SAMPLE_NAME.value: sample.name})
-    metadata.append({MetadataFields.SEQUENCED_AT.value: sample.last_sequenced_at})
-    metadata.append({MetadataFields.TICKET_NUMBER.value: sample.original_ticket})
+    metadata: list[dict] = [
+        {"metadataName": MetadataFields.CUSTOMER_NAME.value, "value": sample.customer.name},
+        {"metadataName": MetadataFields.PREP_CATEGORY.value, "value": sample.prep_category},
+        {"metadataName": MetadataFields.SAMPLE_NAME.value, "value": sample.name},
+        {"metadataName": MetadataFields.SEQUENCED_AT.value, "value": sample.last_sequenced_at},
+        {"metadataName": MetadataFields.TICKET_NUMBER.value, "value": sample.original_ticket},
+    ]
     return metadata
