7fc295a692db08fecd2f3d02f5413f6e1e5e416f
islean <isak.ohlsson.angnell@gmail.com>
2024-01-04 16:35:05 +0100

    Use values

--- cg/meta/archive/ddn/utils.py
+++ cg/meta/archive/ddn/utils.py
@@ -5,9 +5,9 @@ from cg.store.models import Sample
 def get_metadata(sample: Sample) -> list[dict]:
     """Returns metadata generated from a sample."""
     metadata: list[dict] = []
-    metadata.append({MetadataFields.CUSTOMER_NAME: sample.customer.name})
-    metadata.append({MetadataFields.PREP_CATEGORY: sample.prep_category})
-    metadata.append({MetadataFields.SAMPLE_NAME: sample.name})
-    metadata.append({MetadataFields.SEQUENCED_AT: sample.last_sequenced_at})
-    metadata.append({MetadataFields.TICKET_NUMBER: sample.original_ticket})
+    metadata.append({MetadataFields.CUSTOMER_NAME.value: sample.customer.name})
+    metadata.append({MetadataFields.PREP_CATEGORY.value: sample.prep_category})
+    metadata.append({MetadataFields.SAMPLE_NAME.value: sample.name})
+    metadata.append({MetadataFields.SEQUENCED_AT.value: sample.last_sequenced_at})
+    metadata.append({MetadataFields.TICKET_NUMBER.value: sample.original_ticket})
     return metadata
