fc82c05de8edd20d7fa77daa05d91fba38e11d84
Vadym <vadym.ivanchuk@scilifelab.se>
2024-01-08 12:13:42 +0100

    Rename function get_report_accreditation to is_report_acreddited

--- tests/meta/report/test_balsamic_api.py
+++ tests/meta/report/test_balsamic_api.py
@@ -95,8 +95,8 @@ def test_get_variant_caller_version(report_api_balsamic, case_id):
     assert version == expected_version
 
 
-def test_get_report_accreditation(report_api_balsamic, case_id):
-    """Tests report accreditation for a specific BALSAMIC analysis."""
+def test_is_report_accredited(report_api_balsamic, case_id):
+    """Test report accreditation for a specific BALSAMIC analysis."""
 
     # GIVEN a mock metadata object and an accredited one
     balsamic_metadata = report_api_balsamic.analysis_api.get_latest_metadata(case_id)
@@ -105,10 +105,8 @@ def test_get_report_accreditation(report_api_balsamic, case_id):
     balsamic_accredited_metadata.config.panel.capture_kit = "gmsmyeloid"
 
     # WHEN performing the accreditation validation
-    unaccredited_report = report_api_balsamic.get_report_accreditation(None, balsamic_metadata)
-    accredited_report = report_api_balsamic.get_report_accreditation(
-        None, balsamic_accredited_metadata
-    )
+    unaccredited_report = report_api_balsamic.is_report_accredited(None, balsamic_metadata)
+    accredited_report = report_api_balsamic.is_report_accredited(None, balsamic_accredited_metadata)
 
     # THEN verify that only the panel "gmsmyeloid" reports are validated
     assert not unaccredited_report
