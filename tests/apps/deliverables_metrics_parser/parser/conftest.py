9e88e9cdf5e243cf4dc037ac5d4d3dab22bf357f
Christian Oertlin <c.oertlin@gmail.com>
2024-01-09 09:19:07 +0100

    typehints conftest

--- tests/apps/deliverables_metrics_parser/parser/conftest.py
+++ tests/apps/deliverables_metrics_parser/parser/conftest.py
@@ -5,35 +5,35 @@ import pytest
 
 
 @pytest.fixture
-def balsamic_metrics_deliverables_path(case_id) -> Path:
+def balsamic_metrics_deliverables_path(case_id: str) -> Path:
     return Path(
         f"/home/proj/production/cancer/cases/{case_id}/analysis/qc/{case_id}_metrics_deliverables.yaml"
     )
 
 
 @pytest.fixture()
-def mip_dna_metrics_deliverables_path(case_id) -> Path:
+def mip_dna_metrics_deliverables_path(case_id: str) -> Path:
     return Path(
         f"/home/proj/production/rare-disease/cases/{case_id}/analysis/{case_id}_metrics_deliverables.yaml"
     )
 
 
 @pytest.fixture()
-def mip_rna_metrics_deliverables_path(case_id) -> Path:
+def mip_rna_metrics_deliverables_path(case_id: str) -> Path:
     return Path(
         f"/home/proj/production/rare-disease/cases/{case_id}/analysis/{case_id}_metrics_deliverables.yaml"
     )
 
 
 @pytest.fixture()
-def mutant_metrics_deliverables_path(case_id) -> Path:
+def mutant_metrics_deliverables_path(case_id: str) -> Path:
     return Path(
         f"/home/proj/production/mutant/cases/{case_id}/results/{case_id}_metrics_deliverables.yaml"
     )
 
 
 @pytest.fixture()
-def rna_fusion_metrics_deliverables_path(case_id) -> Path:
+def rna_fusion_metrics_deliverables_path(case_id: str) -> Path:
     return Path(
         f"/home/proj/production/rnafusion/cases/{case_id}/{case_id}_metrics_deliverables.yaml"
     )
