844fb7806c302d3e57ac44c6d8713436c3d45d71
Vadym <vadym.ivanchuk@scilifelab.se>
2024-01-10 12:00:04 +0100

    Add CNV report to UMI

--- cg/constants/scout.py
+++ cg/constants/scout.py
@@ -57,6 +57,7 @@ BALSAMIC_CASE_TAGS = dict(
 BALSAMIC_UMI_CASE_TAGS = dict(
     sv_vcf={"vcf-sv-clinical"},
     snv_vcf={"vcf-umi-snv-clinical"},
+    cnv_report={"cnv-report"},
     multiqc_report={"multiqc-html"},
     delivery_report={"delivery-report"},
 )
