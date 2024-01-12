0756eec6183df39d508054fd239b84d29487d7bf
Sofia Stamouli <91951607+sofstam@users.noreply.github.com>
2024-01-12 16:45:34 +0100

    Update cg/constants/metric_conditions.py
    Co-authored-by: Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>

--- cg/constants/metric_conditions.py
+++ cg/constants/metric_conditions.py
@@ -2,7 +2,7 @@
 
 
 class MetricConditions:
-    RNAFUSION_METRIC_CONDITIONS: dict = {
+    RNAFUSION_METRIC_CONDITIONS: dict[str,dict[str, Any] = {
         "uniquely_mapped_percent": {"norm": "gt", "threshold": 60},
         "PCT_MRNA_BASES": {"norm": "gt", "threshold": 80},
         "PCT_RIBOSOMAL_BASES": {"norm": "lt", "threshold": 5},
