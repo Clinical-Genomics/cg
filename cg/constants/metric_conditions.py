463a7692b5e992b57c662a27e06a2bd09e24cb18
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2024-01-12 16:50:18 +0100

    Add type hinting

--- cg/constants/metric_conditions.py
+++ cg/constants/metric_conditions.py
@@ -1,8 +1,10 @@
 """NF-core pipelines related constants."""
 
+from typing import Any
+
 
 class MetricConditions:
-    RNAFUSION_METRIC_CONDITIONS: dict[str,dict[str, Any] = {
+    RNAFUSION_METRIC_CONDITIONS: dict[str, dict[str, Any]] = {
         "uniquely_mapped_percent": {"norm": "gt", "threshold": 60},
         "PCT_MRNA_BASES": {"norm": "gt", "threshold": 80},
         "PCT_RIBOSOMAL_BASES": {"norm": "lt", "threshold": 5},
