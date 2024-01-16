c37faeaa3ac880f8c1f3955f9c0eb5fa5378b3c9
seallard <sebastian.allard@scilifelab.se>
2024-01-16 12:55:41 +0100

    Add endpoint

--- cg/services/delivery_message/delivery_message_service.py
+++ cg/services/delivery_message/delivery_message_service.py
@@ -1,4 +1,5 @@
 from cg.exc import CaseNotFoundError
+from cg.server.dto.delivery_message_response import DeliveryMessageResponse
 from cg.services.delivery_message.utils import get_message
 from cg.store import Store
 
@@ -7,8 +8,9 @@ class DeliveryMessageService:
     def __init__(self, store: Store) -> None:
         self.store = store
 
-    def get_delivery_message(self, case_id: str) -> str:
+    def create_delivery_message(self, case_id: str) -> DeliveryMessageResponse:
         if case := self.store.get_case_by_internal_id(case_id):
-            return get_message(case)
+            message: str = get_message(case)
+            return DeliveryMessageResponse(message=message)
         else:
             raise CaseNotFoundError
