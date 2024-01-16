d157c757e60302f663069064038c98679e68fc09
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2024-01-16 14:51:09 +0100

    Parametrize metrics-deliver tests for rnafusion and taxprofiler

--- tests/cli/workflow/rnafusion/test_cli_rnafusion_metrics_deliver.py
+++ tests/cli/workflow/rnafusion/test_cli_rnafusion_metrics_deliver.py
@@ -10,45 +10,6 @@ from cg.constants import EXIT_SUCCESS
 from cg.models.cg_config import CGConfig
 
 
-def test_without_options(cli_runner: CliRunner, rnafusion_context: CGConfig):
-    """Test command without case_id argument."""
-    # GIVEN no case_id
-
-    # WHEN dry running without anything specified
-    result = cli_runner.invoke(metrics_deliver, obj=rnafusion_context)
-
-    # THEN command should NOT execute successfully
-    assert result.exit_code != EXIT_SUCCESS
-
-    # THEN command should mention argument
-    assert "Missing argument" in result.output
-
-
-def test_with_missing_case(
-    cli_runner: CliRunner,
-    rnafusion_context: CGConfig,
-    caplog: LogCaptureFixture,
-    case_id_does_not_exist: str,
-):
-    """Test command with invalid case to start with."""
-    caplog.set_level(logging.WARNING)
-
-    # GIVEN case_id not in database
-    assert not rnafusion_context.status_db.get_case_by_internal_id(
-        internal_id=case_id_does_not_exist
-    )
-
-    # WHEN running
-    result = cli_runner.invoke(metrics_deliver, [case_id_does_not_exist], obj=rnafusion_context)
-
-    # THEN command should NOT successfully call the command it creates
-    assert result.exit_code != EXIT_SUCCESS
-
-    # THEN ERROR log should be printed containing invalid case_id
-    assert case_id_does_not_exist in caplog.text
-    assert "could not be found" in caplog.text
-
-
 def test_without_samples(
     cli_runner: CliRunner,
     rnafusion_context: CGConfig,
--- tests/cli/workflow/taxprofiler/test_cli_taxprofiler_metrics_deliver.py
+++ tests/cli/workflow/taxprofiler/test_cli_taxprofiler_metrics_deliver.py
@@ -12,43 +12,6 @@ from cg.models.cg_config import CGConfig
 from cg.io.yaml import read_yaml
 
 
-def test_without_options(cli_runner: CliRunner, taxprofiler_context: CGConfig):
-    """Test command without 'case_id' argument."""
-    # GIVEN no case id
-
-    # WHEN using dry run without any options
-    result = cli_runner.invoke(metrics_deliver, obj=taxprofiler_context)
-
-    # THEN command should NOT execute successfully
-    assert result.exit_code != EXIT_SUCCESS
-
-    # THEN command should mention argument
-    assert "Missing argument" in result.output
-
-
-def test_with_missing_case(
-    cli_runner: CliRunner,
-    taxprofiler_context: CGConfig,
-    caplog: LogCaptureFixture,
-    case_id_does_not_exist: str,
-):
-    """Test metrics-deliver command with invalid case."""
-    caplog.set_level(logging.ERROR)
-    # GIVEN case id not in database
-    assert not taxprofiler_context.status_db.get_case_by_internal_id(
-        internal_id=case_id_does_not_exist
-    )
-
-    # WHEN running command with non-existing case-id
-    result = cli_runner.invoke(metrics_deliver, [case_id_does_not_exist], obj=taxprofiler_context)
-    # THEN command should NOT successfully call the command it creates
-
-    assert result.exit_code != EXIT_SUCCESS
-    # THEN ERROR log should be printed containing invalid case id
-    assert case_id_does_not_exist in caplog.text
-    assert f"{case_id_does_not_exist} could not be found" in caplog.text
-
-
 def test_metrics_deliver(
     cli_runner: CliRunner,
     taxprofiler_context: CGConfig,
