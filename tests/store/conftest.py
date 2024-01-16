0cf2d5463dc99f029f394b0b2992fb88cce39065
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2024-01-16 15:01:52 +0100

    feat(sql): Refactor test

--- tests/store/conftest.py
+++ tests/store/conftest.py
@@ -535,3 +535,40 @@ def rml_pool_store(
     )
 
     yield store
+
+
+@pytest.fixture
+def store_with_analyses_for_cases_not_uploaded_fluffy(
+    analysis_store: Store,
+    helpers: StoreHelpers,
+    timestamp_now: datetime,
+    timestamp_yesterday: datetime,
+) -> Store:
+    """Return a store with two analyses for two cases and pipeline."""
+    case_one = analysis_store.get_case_by_internal_id("yellowhog")
+    case_two = helpers.add_case(analysis_store, internal_id="test_case_1")
+
+    cases = [case_one, case_two]
+    for case in cases:
+        oldest_analysis = helpers.add_analysis(
+            analysis_store,
+            case=case,
+            started_at=timestamp_yesterday,
+            uploaded_at=timestamp_yesterday,
+            delivery_reported_at=None,
+            pipeline=Pipeline.FLUFFY,
+        )
+        helpers.add_analysis(
+            analysis_store,
+            case=case,
+            started_at=timestamp_now,
+            uploaded_at=None,
+            delivery_reported_at=None,
+            pipeline=Pipeline.FLUFFY,
+        )
+        sample = helpers.add_sample(analysis_store, delivered_at=timestamp_now)
+        link: CaseSample = analysis_store.relate_sample(
+            case=oldest_analysis.case, sample=sample, status=PhenotypeStatus.UNKNOWN
+        )
+        analysis_store.session.add(link)
+    return analysis_store
