16d6f94686603f139b8215f62166bc304d5f971f
seallard <sebastian.allard@scilifelab.se>
2024-01-16 13:39:08 +0100

    Naming

--- cg/services/delivery_message/delivery_message_service.py
+++ cg/services/delivery_message/delivery_message_service.py
@@ -8,7 +8,7 @@ class DeliveryMessageService:
     def __init__(self, store: Store) -> None:
         self.store = store
 
-    def create_delivery_message(self, case_id: str) -> DeliveryMessageResponse:
+    def get_delivery_message(self, case_id: str) -> DeliveryMessageResponse:
         if case := self.store.get_case_by_internal_id(case_id):
             message: str = get_message(case)
             return DeliveryMessageResponse(message=message)
