8e85cc06c483de8f0aeacead961397eb28ee5152
seallard <sebastian.allard@scilifelab.se>
2024-01-17 15:23:33 +0100

    Add statina link

--- cg/services/delivery_message/messages/statina_message.py
+++ cg/services/delivery_message/messages/statina_message.py
@@ -1,9 +1,9 @@
 from cg.services.delivery_message.messages.delivery_message import DeliveryMessage
-from cg.services.delivery_message.messages.utils import get_statina_batch_id
+from cg.services.delivery_message.messages.utils import get_statina_link
 from cg.store.models import Case
 
 
 class StatinaMessage(DeliveryMessage):
     def create_message(self, case: Case) -> str:
-        batch_id: str = get_statina_batch_id(case)
-        return f"Hello,\n\nBatch {batch_id} is now available in Statina."
+        statina_link: str = get_statina_link(case)
+        return f"Hello,\n\nBatch {statina_link} is now available in Statina."
