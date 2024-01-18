ed4315ef70bc216fcef3a98f4aac739387f47ae4
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-01-18 13:28:53 +0100

    Remove fwd slash, new line, single quote and underscore

--- cg/constants/__init__.py
+++ cg/constants/__init__.py
@@ -1,10 +1,6 @@
 """Import all constants here for easy access"""
 
-from cg.constants.compression import (
-    FASTQ_DELTA,
-    FASTQ_FIRST_READ_SUFFIX,
-    FASTQ_SECOND_READ_SUFFIX,
-)
+from cg.constants.compression import FASTQ_DELTA, FASTQ_FIRST_READ_SUFFIX, FASTQ_SECOND_READ_SUFFIX
 from cg.constants.constants import (
     CAPTUREKIT_CANCER_OPTIONS,
     CAPTUREKIT_OPTIONS,
@@ -18,15 +14,11 @@ from cg.constants.constants import (
     FlowCellStatus,
 )
 from cg.constants.gene_panel import GenePanelMasterList
-from cg.constants.housekeeper_tags import (
-    HK_FASTQ_TAGS,
-    HK_MULTIQC_HTML_TAG,
-    SequencingFileTag,
-)
+from cg.constants.housekeeper_tags import HK_FASTQ_TAGS, HK_MULTIQC_HTML_TAG, SequencingFileTag
 from cg.constants.paths import TMP_DIR
 from cg.constants.priority import Priority
 from cg.constants.process import EXIT_FAIL, EXIT_SUCCESS
 from cg.constants.report import *
 from cg.constants.sample_sources import ANALYSIS_SOURCES, METAGENOME_SOURCES
 from cg.constants.sequencing import FLOWCELL_Q30_THRESHOLD
-from cg.constants.symbols import SINGLE_QUOTE, SPACE
+from cg.constants.symbols import SPACE
--- cg/constants/symbols.py
+++ cg/constants/symbols.py
@@ -1,8 +1,4 @@
 """Symbol constants."""
 
 EMPTY_STRING = ""
-FWD_SLASH = "/"
-NEW_LINE = "\n"
-SINGLE_QUOTE = "'"
 SPACE = " "
-UNDERSCORE = "_"
