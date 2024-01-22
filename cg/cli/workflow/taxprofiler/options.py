6fc27b5ba44c4adf1f11f1480e99f87daa840996
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-01-22 17:01:33 +0100

    move option_from_start to common

--- cg/cli/workflow/taxprofiler/options.py
+++ cg/cli/workflow/taxprofiler/options.py
@@ -2,14 +2,6 @@ import click
 
 from cg.constants.sequencing import SequencingPlatform
 
-OPTION_FROM_START = click.option(
-    "--from-start",
-    is_flag=True,
-    default=False,
-    show_default=True,
-    help="Start pipeline from the start",
-)
-
 OPTION_INSTRUMENT_PLATFORM = click.option(
     "--instrument-platform",
     show_default=True,
