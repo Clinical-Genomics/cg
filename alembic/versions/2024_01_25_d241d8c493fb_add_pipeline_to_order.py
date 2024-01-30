6b6b777890d6378f04da00e41dcd3f28f327ab6d
Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>
2024-01-30 10:05:51 +0100

    Modify Order table (#2870) (patch)
    ### Added
    
    - Pipeline column to the order table.
    - Order-Customer relationship

---
+++ alembic/versions/2024_01_25_d241d8c493fb_add_pipeline_to_order.py
@@ -0,0 +1,31 @@
+"""Add pipeline to order
+
+Revision ID: d241d8c493fb
+Revises: de0f5b78dca4
+Create Date: 2024-01-25 16:18:35.740780
+
+"""
+import sqlalchemy as sa
+
+from alembic import op
+from cg.constants import Pipeline
+
+# revision identifiers, used by Alembic.
+revision = "d241d8c493fb"
+down_revision = "de0f5b78dca4"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        table_name="order",
+        column=sa.Column("workflow", sa.Enum(*tuple(Pipeline)), nullable=False),
+    )
+
+
+def downgrade():
+    op.drop_column(
+        table_name="order",
+        column_name="workflow",
+    )
