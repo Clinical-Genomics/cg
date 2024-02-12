7e47f569677fe20753e61b3d65915deac213a093
Vincent Janvid <69356202+Vince-janv@users.noreply.github.com>
2024-02-12 13:17:16 +0100

    patch(flow_cells) Refactor names to support introduction of new data sources (#2824)(minor))
    ### Changed
    
    - CGConfig.demultiplexed_flow_cells_dir to CGConfig.illumina_novaseq_demultiplexed_flow_cells_directory
    - Bound directories for bcl2fastq slurm jobs
    - CGConfig.flow_cells_dir to CGConfig.illumina_novaseq_flow_cells_directory
    - Renamed a lot of fixtures to match this

--- tests/cli/demultiplex/test_finish_demux.py
+++ tests/cli/demultiplex/test_finish_demux.py
@@ -1,7 +1,6 @@
 """Test finish demultiplexing CLI."""
 
 import logging
-from pathlib import Path
 
 from click import testing
 
@@ -14,7 +13,7 @@ def test_finish_all_cmd_dry_run(
     caplog,
     cli_runner: testing.CliRunner,
     demultiplex_context: CGConfig,
-    tmp_demultiplexed_runs_directory: Path,
+    tmp_illumina_demultiplexed_flow_cells_directory,
 ):
     caplog.set_level(logging.INFO)
 
@@ -37,7 +36,7 @@ def test_finish_flow_cell_dry_run(
     caplog,
     cli_runner: testing.CliRunner,
     demultiplex_context: CGConfig,
-    tmp_demultiplexed_runs_directory: Path,
+    tmp_illumina_demultiplexed_flow_cells_directory,
     bcl2fastq_flow_cell_full_name: str,
 ):
     caplog.set_level(logging.INFO)
