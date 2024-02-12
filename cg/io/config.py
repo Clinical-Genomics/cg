5df64719f6f391f9606e5b683b76d469dae0923e
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-02-12 17:01:26 +0100

    remove config read/write/concat that can be in txt io

--- cg/io/config.py
+++ cg/io/config.py
@@ -3,27 +3,6 @@
 from pathlib import Path
 from typing import Any
 
-
-def read_config(file_path: Path) -> Any:
-    """Read content in a config file"""
-    with open(file_path, "r") as file:
-        return file.read()
-
-
-def write_config(content: Any, file_path: Path) -> None:
-    """Write content to a config file"""
-    with open(file_path, "w") as file:
-        file.write(content)
-
-
-def concat_configs(file_paths: list[Path], target_file: Path, str_content: str = "") -> None:
-    """Concatenate config files and string content"""
-    content = str_content
-    for file_path in file_paths:
-        content = content + read_config(file_path)
-    write_config(content, target_file)
-
-
 def write_config_nextflow_style(content: dict[str, Any]) -> str:
     """Write content to stream accepted by Nextflow config files with non-quoted booleans and quoted strings."""
     string = ""
--- cg/io/txt.py
+++ cg/io/txt.py
@@ -15,3 +15,10 @@ def write_txt(content: list[str], file_path: Path) -> None:
     """Write content to a text file."""
     with open(file_path, "w") as file:
         file.writelines(content)
+
+def concat_txt(file_paths: list[Path], target_file: Path, str_content: str = "") -> None:
+    """Concatenate files and eventual string content"""
+    content = str_content
+    for file_path in file_paths:
+        content = content + read_txt(file_path)
+    write_txt(content, target_file)
