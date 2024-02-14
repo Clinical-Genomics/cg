63f75921d4b22c77d7dc046e9aa39bf6a4f81a1d
seallard <sebastian.allard@scilifelab.se>
2024-02-14 13:20:29 +0100

    Add fastq service

---
+++ cg/services/fastq_service/fastq_service.py
@@ -0,0 +1,8 @@
+from pathlib import Path
+
+
+class FastqService:
+
+    def concatenate(self, input_files: list[Path], output_file: Path) -> None:
+        """Concatenates multiple FASTQ files into a single output file."""
+        pass
