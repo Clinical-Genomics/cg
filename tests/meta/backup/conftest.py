bcb32b343a7aee03f2c7c93fe77f8616827e6300
Karl Nyrén <45558267+karlnyr@users.noreply.github.com>
2024-02-15 09:26:46 +0100

    Add CopyComplete.txt file creation in BackupAPI (#2913) (patch)
    ### Added
    
    - Add CopyComplete.txt file creation in BackupAPI

--- tests/meta/backup/conftest.py
+++ tests/meta/backup/conftest.py
@@ -5,7 +5,13 @@ from typing import Callable
 import pytest
 
 from cg.constants import FileExtensions
+from cg.meta.backup.backup import BackupAPI
+from cg.meta.backup.pdc import PdcAPI
+from cg.meta.encryption.encryption import EncryptionAPI
+from cg.meta.tar.tar import TarAPI
 from cg.models.cg_config import PDCArchivingDirectory
+from cg.models.cg_config import CGConfig
+from cg.store.store import Store
 
 
 @pytest.fixture
@@ -48,6 +54,29 @@ def archived_flow_cells(pdc_archiving_directory: PDCArchivingDirectory) -> list[
         f"{pdc_archiving_directory.nas}/old_flow_cell{FileExtensions.TAR}{FileExtensions.GZIP}{FileExtensions.GPG}",
         f"{pdc_archiving_directory.pre_nas}/ancient_flow_cell{FileExtensions.TAR}{FileExtensions.GZIP}{FileExtensions.GPG}",
     ]
+
+
+@pytest.fixture
+def backup_api(cg_context: CGConfig, illumina_flow_cells_directory: Path) -> BackupAPI:
+    """Return a BackupAPI instance."""
+    encryption_api: EncryptionAPI = EncryptionAPI(
+        binary_path=cg_context.encryption.binary_path, dry_run=True
+    )
+    store: Store = cg_context.status_db
+    tar_api: TarAPI = TarAPI(binary_path=cg_context.tar.binary_path, dry_run=True)
+    pdc_api: PdcAPI = PdcAPI(binary_path=cg_context.pdc.binary_path, dry_run=True)
+
+    pdc_archiving_directory: PDCArchivingDirectory = cg_context.backup.pdc_archiving_directory
+    _backup_api: BackupAPI = BackupAPI(
+        encryption_api=encryption_api,
+        status=store,
+        tar_api=tar_api,
+        pdc_api=pdc_api,
+        pdc_archiving_directory=pdc_archiving_directory,
+        flow_cells_dir=illumina_flow_cells_directory,
+        dry_run=True,
+    )
+    return _backup_api
 
 
 @pytest.fixture
