69a6777db40c83b8d69e3dd5dcf40dda9a7045d4
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-02-16 15:14:34 +0100

    update docstring

--- cg/meta/workflow/raredisease.py
+++ cg/meta/workflow/raredisease.py
@@ -92,7 +92,7 @@ class RarediseaseAnalysisAPI(NfAnalysisAPI):
         self,
         case_id: str,
     ) -> list[list[Any]]:
-        """Return sample sheet for Raredisease analysis in case folder."""
+        """Return Raredisease sample sheet content for a case."""
         case: Case = self.status_db.get_case_by_internal_id(internal_id=case_id)
         sample_sheet_content = []
         LOG.info("Getting sample sheet information")
