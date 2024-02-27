17d2a79652bf0e6da6fdd703d1ce9903f4b79fb0
Christian Oertlin <c.oertlin@gmail.com>
2024-02-27 09:36:32 +0100

    add metapi

---
+++ cg/meta/qc_metrics/collect_qc_metrics.py
@@ -0,0 +1,9 @@
+"""Module for the collect qc metrics meta api."""
+from cg.meta.meta import MetaAPI
+
+
+class CollectQCMetricsAPI(MetaAPI):
+    """Collect qc metrics API."""
+
+    def get_completed_cases(self):
+        completed_cases = self.trailblazer_api.g
