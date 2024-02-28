228ad2e078217fb97b9014bae79c099710536d01
Christian Oertlin <c.oertlin@gmail.com>
2024-02-28 11:04:48 +0100

    add more assertions

--- tests/meta/qc_metrics/test_collect_qc_metrics.py
+++ tests/meta/qc_metrics/test_collect_qc_metrics.py
@@ -2,8 +2,9 @@
 
 from housekeeper.store.models import File
 
-from cg.clients.janus.dto.create_qc_metrics_request import CreateQCMetricsRequest
+from cg.clients.janus.dto.create_qc_metrics_request import CreateQCMetricsRequest, FilePathAndTag
 from cg.meta.qc_metrics.collect_qc_metrics import CollectQCMetricsAPI
+from cg.store.models import Sample, Case
 
 
 def test_get_qc_metrics_file_paths_for_case(
@@ -25,7 +26,9 @@ def test_get_qc_metrics_file_paths_for_case(
 
 
 def test_create_qc_metrics_request(
-    collect_qc_metrics_api: CollectQCMetricsAPI, case_id_with_single_sample: str
+    collect_qc_metrics_api: CollectQCMetricsAPI,
+    case_id_with_single_sample: str,
+    file_with_right_tags,
 ):
     # GIVEN a collect qc metrics api
 
@@ -37,8 +40,16 @@ def test_create_qc_metrics_request(
     # THEN a qc metrics request is created
     assert qc_metrics_request
     assert qc_metrics_request.case_id == case_id_with_single_sample
-    sample_ids = collect_qc_metrics_api.status_db.get_sample_ids_by_case_id(
+    sample: Sample = collect_qc_metrics_api.status_db.get_samples_by_case_id(
+        case_id_with_single_sample
+    )[0]
+    for sample_id in qc_metrics_request.sample_ids:
+        assert sample_id == sample.internal_id
+    assert len(qc_metrics_request.files) == 1
+    file_path_and_tag: FilePathAndTag = qc_metrics_request.files[0]
+    assert file_path_and_tag.tag == "hsmetrics"
+    assert qc_metrics_request.prep_category == sample.prep_category
+    case: Case = collect_qc_metrics_api.status_db.get_case_by_internal_id(
         case_id_with_single_sample
     )
-    for sample_id in qc_metrics_request.sample_ids:
-        assert sample_id in sample_ids
+    assert qc_metrics_request.workflow == case.data_analysis
