8c13ce60ed7d0000c555710486ea8cdd515609a6
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2024-02-28 10:38:34 +0100

    Add test for get_formatted_file_deliverable function

--- tests/meta/workflow/test_nf_analysis.py
+++ tests/meta/workflow/test_nf_analysis.py
@@ -5,9 +5,11 @@ from typing import Any
 
 import pytest
 from _pytest.logging import LogCaptureFixture
+from pathlib import Path
 
 from cg.meta.workflow.nf_analysis import NfAnalysisAPI
 from cg.models.cg_config import CGConfig
+from tests.cli.workflow.conftest import deliverables_template_content
 
 
 @pytest.mark.parametrize(
@@ -55,3 +57,50 @@ def test_create_metrics_deliverables_content(
 
     # THEN assert that the content created is correct
     assert metrics_deliverables_content == metrics_deliverables
+
+
+@pytest.mark.parametrize(
+    ("context", "case_id"),
+    [
+        (
+            "rnafusion_context",
+            "rnafusion_case_id",
+        ),
+        (
+            "taxprofiler_context",
+            "taxprofiler_case_id",
+        ),
+    ],
+)
+def test_get_formatted_file_deliverable(
+    context: str,
+    case_id: str,
+    sample_id: str,
+    sample_name: str,
+    caplog: LogCaptureFixture,
+    deliverables_template_content,
+    request,
+):
+    """Test the formatted file deliverable with the case and sample attributes for Taxprofiler and Rnafusion."""
+
+    caplog.set_level(logging.INFO)
+
+    # GIVEN each fixture is being initialised
+    context: CGConfig = request.getfixturevalue(context)
+    case_id: str = request.getfixturevalue(case_id)
+
+    caplog.set_level(logging.INFO)
+    analysis_api: NfAnalysisAPI = context.meta_apis["analysis_api"]
+
+    # WHEN formatting the template
+    for field in deliverables_template_content:
+        formatted_file_deliverable = analysis_api.get_formatted_file_deliverable(
+            field,
+            case_id,
+            sample_id,
+            sample_name,
+            "get_case_path",
+        )
+
+        # THEN assert that the new path is correct
+        assert "get_case_path" in formatted_file_deliverable.path
