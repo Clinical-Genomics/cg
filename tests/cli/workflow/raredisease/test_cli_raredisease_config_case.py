abc900e32bb1a1644aeffbc774d1aca82e69d2f4
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-02-29 10:09:43 +0100

    debugging

--- tests/cli/workflow/raredisease/test_cli_raredisease_config_case.py
+++ tests/cli/workflow/raredisease/test_cli_raredisease_config_case.py
@@ -52,7 +52,7 @@ def test_config_case(
     # WHEN performing a dry-run
     result = cli_runner.invoke(config_case, [raredisease_case_id], obj=raredisease_context)
 
-    print(caplog.text)
+    assert caplog.text == "something"
     # THEN command should should exit successfully
     assert result.exit_code == EXIT_SUCCESS
 
