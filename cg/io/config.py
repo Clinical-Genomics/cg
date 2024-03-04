5aaf75a784763e485bf2c5903441a5b7a30d90b1
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-03-04 20:13:22 +0100

    key to parameters

--- cg/io/config.py
+++ cg/io/config.py
@@ -9,9 +9,9 @@ def write_config_nextflow_style(content: dict[str, Any] | None) -> str:
     """Write content to stream accepted by Nextflow config files with non-quoted booleans and quoted strings."""
     string = EMPTY_STRING
     double_quotes = '"'
-    for key, value in content.items():
+    for parameter, value in content.items():
         if isinstance(value, Path):
             value: str = value.as_posix()
         quotes = double_quotes if type(value) is str else EMPTY_STRING
-        string += f"params.{key} = {quotes}{value}{quotes}\n"
+        string += f"params.{parameter} = {quotes}{value}{quotes}\n"
     return string
