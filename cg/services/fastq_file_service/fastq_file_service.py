24f0374aebcdb9435f4eba1b3804c0ab1a5a824f
Vadym <vadym.ivanchuk@scilifelab.se>
2024-03-04 18:17:44 +0100

    Update FastqFileServices

--- cg/services/fastq_file_service/fastq_file_service.py
+++ cg/services/fastq_file_service/fastq_file_service.py
@@ -12,23 +12,32 @@ class FastqFileService:
 
     @staticmethod
     def concatenate(
-        fastq_directory: Path, forward_output: Path, reverse_output: Path, remove_raw: bool = False
-    ):
-        temp_forward: Path | None = concatenate_forward_reads(fastq_directory)
-        temp_reverse: Path | None = concatenate_reverse_reads(fastq_directory)
-
+        fastq_directory: Path, sample_name: str, remove_raw: bool = False
+    ) -> tuple[Path | None, Path | None]:
+        """Concatenates forward and reverse reads into single files from a provided directory."""
+        forward_out_path = Path(fastq_directory, f"{sample_name}_R1.fastq.gz")
+        reverse_out_path = Path(fastq_directory, f"{sample_name}_R2.fastq.gz")
+        temp_forward: Path | None = (
+            concatenate_forward_reads(fastq_directory)
+            if not forward_out_path.exists()
+            else forward_out_path
+        )
+        temp_reverse: Path | None = (
+            concatenate_reverse_reads(fastq_directory)
+            if not reverse_out_path.exists()
+            else reverse_out_path
+        )
         if remove_raw:
             remove_raw_fastqs(
                 fastq_directory=fastq_directory,
                 forward_file=temp_forward,
                 reverse_file=temp_reverse,
             )
-
         if temp_forward:
-            temp_forward.rename(forward_output)
-
+            temp_forward: Path = temp_forward.rename(forward_out_path)
         if temp_reverse:
-            temp_reverse.rename(reverse_output)
+            temp_reverse: Path = temp_reverse.rename(reverse_out_path)
+        return temp_forward, temp_reverse
 
     @staticmethod
     def is_concatenation_needed(case: Case) -> bool:
--- tests/services/fastq_file_service/conftest.py
+++ tests/services/fastq_file_service/conftest.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+
 import pytest
 
 from cg.services.fastq_file_service.fastq_file_service import FastqFileService
@@ -9,29 +10,40 @@ def fastq_file_service():
     return FastqFileService()
 
 
-def create_fastqs_directory(number_forward_reads, number_reverse_reads, tmp_path):
+def create_fastqs_directory(
+    sample_name: str, number_forward_reads: int, number_reverse_reads: int, tmp_path: Path
+) -> Path:
     fastq_dir = Path(tmp_path, "fastqs")
     fastq_dir.mkdir()
     for i in range(number_forward_reads):
-        file = Path(fastq_dir, f"sample_R1_{i}.fastq.gz")
+        file = Path(fastq_dir, f"{sample_name}_R1_{i}.fastq.gz")
         file.write_text(f"forward read {i}")
 
     for i in range(number_reverse_reads):
-        file = Path(fastq_dir, f"sample_R2_{i}.fastq.gz")
+        file = Path(fastq_dir, f"{sample_name}_R2_{i}.fastq.gz")
         file.write_text(f"reverse read {i}")
     return fastq_dir
 
 
 @pytest.fixture
-def fastqs_dir(tmp_path) -> Path:
+def fastqs_dir(tmp_path: Path, sample_name: str) -> Path:
     return create_fastqs_directory(
-        number_forward_reads=3, number_reverse_reads=3, tmp_path=tmp_path
+        sample_name=sample_name, number_forward_reads=3, number_reverse_reads=3, tmp_path=tmp_path
     )
 
 
 @pytest.fixture
-def fastqs_forward(tmp_path) -> Path:
+def fastqs_forward(tmp_path, sample_name: str) -> Path:
     """Return a directory with only forward reads."""
     return create_fastqs_directory(
-        number_forward_reads=3, number_reverse_reads=0, tmp_path=tmp_path
+        sample_name=sample_name, number_forward_reads=3, number_reverse_reads=0, tmp_path=tmp_path
     )
+
+
+@pytest.fixture
+def concatenated_fastqs_dir(fastqs_dir: Path, sample_name: str) -> Path:
+    """Return a directory with concatenated fastq files."""
+    for i in [1, 2]:
+        file = Path(fastqs_dir, f"{sample_name}_R{i}.fastq.gz")
+        file.write_text(f"Concatenated read {i}")
+    return fastqs_dir
