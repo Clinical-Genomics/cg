aaccda50124a2655bfccd97b79143b39d0cb69bd
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2024-03-06 13:06:52 +0100

    Test

--- tests/cli/workflow/taxprofiler/test_cli_taxprofiler_store_housekeeper.py
+++ tests/cli/workflow/taxprofiler/test_cli_taxprofiler_store_housekeeper.py
@@ -15,40 +15,24 @@ from cg.models.cg_config import CGConfig
 from cg.utils import Process
 
 
-def test_case_with_malformed_deliverables_file(
-    cli_runner,
-    mocker,
+def test_case_not_finished(
+    cli_runner: CliRunner,
     taxprofiler_context: CGConfig,
-    taxprofiler_malformed_hermes_deliverables: dict,
     caplog: LogCaptureFixture,
     taxprofiler_case_id: str,
 ):
-    """Test command with case_id and config file and analysis_finish but malformed deliverables output."""
-    caplog.set_level(logging.WARNING)
-    # GIVEN a malformed output from hermes
-    analysis_api: TaxprofilerAnalysisAPI = taxprofiler_context.meta_apis["analysis_api"]
+    """Test command with case_id and config file but no analysis_finish."""
+    caplog.set_level(logging.ERROR)
+    # GIVEN case-id
+    case_id: str = taxprofiler_case_id
 
-    # GIVEN that HermesAPI returns a malformed deliverables output
-    mocker.patch.object(Process, "run_command")
-    Process.run_command.return_value = WriteStream.write_stream_from_content(
-        content=taxprofiler_malformed_hermes_deliverables, file_format=FileFormat.JSON
-    )
+    # WHEN running
+    result = cli_runner.invoke(store_housekeeper, [case_id], obj=taxprofiler_context)
 
-    # GIVEN that the output is malformed
-    with pytest.raises(ValidationError):
-        analysis_api.hermes_api.convert_deliverables(
-            deliverables_file=Path("a_file"), workflow="taxprofiler"
-        )
+    # THEN command should NOT execute successfully
+    assert result.exit_code != EXIT_SUCCESS
 
-        # GIVEN case-id
-        case_id: str = taxprofiler_case_id
+    # THEN warning should be printed that no deliverables file has been found
+    assert "No deliverables file found for case" in caplog.text
 
-        # WHEN running
-        result = cli_runner.invoke(store_housekeeper, [case_id], obj=taxprofiler_context)
 
-        # THEN command should NOT execute successfully
-        assert result.exit_code != EXIT_SUCCESS
-
-        # THEN information that the file is malformed should be communicated
-        assert "Deliverables file is malformed" in caplog.text
-        assert "field required" in caplog.text
