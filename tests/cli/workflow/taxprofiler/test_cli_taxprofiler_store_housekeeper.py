1d8d645b93b2e5da091e681ea555be9d90e26437
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2024-03-06 13:44:21 +0100

    Separate tests for not finished analysis and malformatted

--- tests/cli/workflow/taxprofiler/test_cli_taxprofiler_store_housekeeper.py
+++ tests/cli/workflow/taxprofiler/test_cli_taxprofiler_store_housekeeper.py
@@ -34,3 +34,42 @@ def test_case_not_finished(
 
     # THEN warning should be printed that no deliverables file has been found
     assert "No deliverables file found for case" in caplog.text
+
+
+def test_case_with_malformed_deliverables_file(
+    cli_runner,
+    mocker,
+    taxprofiler_context: CGConfig,
+    taxprofiler_malformed_hermes_deliverables: dict,
+    caplog: LogCaptureFixture,
+    rnafusion_case_id: str,
+):
+    """Test command with case_id and config file and analysis_finish but malformed deliverables output."""
+    caplog.set_level(logging.WARNING)
+    # GIVEN a malformed output from hermes
+    analysis_api: TaxprofilerAnalysisAPI = taxprofiler_context.meta_apis["analysis_api"]
+
+    # GIVEN that HermesAPI returns a malformed deliverables output
+    mocker.patch.object(Process, "run_command")
+    Process.run_command.return_value = WriteStream.write_stream_from_content(
+        content=taxprofiler_malformed_hermes_deliverables, file_format=FileFormat.JSON
+    )
+
+    # GIVEN that the output is malformed
+    with pytest.raises(ValidationError):
+        analysis_api.hermes_api.convert_deliverables(
+            deliverables_file=Path("a_file"), workflow="rnafusion"
+        )
+
+        # GIVEN case-id
+        case_id: str = rnafusion_case_id
+
+        # WHEN running
+        result = cli_runner.invoke(store_housekeeper, [case_id], obj=taxprofiler_context)
+
+        # THEN command should NOT execute successfully
+        assert result.exit_code != EXIT_SUCCESS
+
+        # THEN information that the file is malformed should be communicated
+        assert "Deliverables file is malformed" in caplog.text
+        assert "field required" in caplog.text
