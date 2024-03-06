d9d2f86f4c5814e0b45110fa9f321f5ecb9e394e
Vadym <vadym.ivanchuk@scilifelab.se>
2024-03-06 19:20:24 +0100

    Fix fixtures

--- tests/fixture_plugins/delivery_fixtures/bundle_fixtures.py
+++ tests/fixture_plugins/delivery_fixtures/bundle_fixtures.py
@@ -1,6 +1,8 @@
 """Delivery API bundle fixtures."""
 
+from copy import deepcopy
 from pathlib import Path
+from typing import Any
 
 import pytest
 
@@ -9,14 +11,15 @@ from cg.constants import SequencingFileTag
 
 @pytest.fixture
 def hk_delivery_sample_bundle(
-    sample_hk_bundle_no_files: dict,
+    sample_hk_bundle_no_files: dict[str, Any],
     sample_id: str,
     spring_sample_file: Path,
     fastq_sample_file: Path,
 ) -> dict:
     """Return Housekeeper bundle for another sample."""
-    sample_hk_bundle_no_files["name"] = sample_id
-    sample_hk_bundle_no_files["files"] = [
+    sample_hk_bundle: dict[str, Any] = deepcopy(sample_hk_bundle_no_files)
+    sample_hk_bundle["name"] = sample_id
+    sample_hk_bundle["files"] = [
         {
             "archive": False,
             "path": fastq_sample_file.as_posix(),
@@ -28,19 +31,20 @@ def hk_delivery_sample_bundle(
             "tags": [SequencingFileTag.SPRING, sample_id],
         },
     ]
-    return sample_hk_bundle_no_files
+    return sample_hk_bundle
 
 
 @pytest.fixture
 def hk_delivery_another_sample_bundle(
-    sample_hk_bundle_no_files: dict,
+    sample_hk_bundle_no_files: dict[str, Any],
     another_sample_id: str,
     spring_another_sample_file: Path,
     fastq_another_sample_file: Path,
 ) -> dict:
     """Return Housekeeper bundle for another sample."""
-    sample_hk_bundle_no_files["name"] = another_sample_id
-    sample_hk_bundle_no_files["files"] = [
+    sample_hk_bundle: dict[str, Any] = deepcopy(sample_hk_bundle_no_files)
+    sample_hk_bundle["name"] = another_sample_id
+    sample_hk_bundle["files"] = [
         {
             "archive": False,
             "path": fastq_another_sample_file.as_posix(),
@@ -52,4 +56,4 @@ def hk_delivery_another_sample_bundle(
             "tags": [SequencingFileTag.SPRING, another_sample_id],
         },
     ]
-    return sample_hk_bundle_no_files
+    return sample_hk_bundle
