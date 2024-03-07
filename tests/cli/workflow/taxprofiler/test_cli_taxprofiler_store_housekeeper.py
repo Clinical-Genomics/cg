de7cb3c011591d6c8984e9172d503a02abb713d2
Sofia Stamouli <sofia.stamouli@scilifelab.se>
2024-03-07 13:59:47 +0100

    Troubleshoot for smell code

--- tests/cli/workflow/taxprofiler/test_cli_taxprofiler_store_housekeeper.py
+++ tests/cli/workflow/taxprofiler/test_cli_taxprofiler_store_housekeeper.py
@@ -18,33 +18,33 @@ from cg.utils import Process
 def test_case_with_malformed_deliverables_file(
     cli_runner,
     mocker,
-    rnafusion_context: CGConfig,
-    rnafusion_malformed_hermes_deliverables: dict,
+    taxprofiler_context: CGConfig,
+    taxprofiler_malformed_hermes_deliverables: dict,
     caplog: LogCaptureFixture,
     taxprofiler_case_id: str,
 ):
     """Test command with case_id and config file and analysis_finish but malformed deliverables output."""
     caplog.set_level(logging.WARNING)
     # GIVEN a malformed output from hermes
-    analysis_api: TaxprofilerAnalysisAPI = rnafusion_context.meta_apis["analysis_api"]
+    analysis_api: TaxprofilerAnalysisAPI = taxprofiler_context.meta_apis["analysis_api"]
 
     # GIVEN that HermesAPI returns a malformed deliverables output
     mocker.patch.object(Process, "run_command")
     Process.run_command.return_value = WriteStream.write_stream_from_content(
-        content=rnafusion_malformed_hermes_deliverables, file_format=FileFormat.JSON
+        content=taxprofiler_malformed_hermes_deliverables, file_format=FileFormat.JSON
     )
 
     # GIVEN that the output is malformed
     with pytest.raises(ValidationError):
         analysis_api.hermes_api.convert_deliverables(
-            deliverables_file=Path("a_file"), workflow="rnafusion"
+            deliverables_file=Path("a_file"), workflow="taxprofiler"
         )
 
         # GIVEN case-id
         case_id: str = taxprofiler_case_id
 
         # WHEN running
-        result = cli_runner.invoke(store_housekeeper, [case_id], obj=rnafusion_context)
+        result = cli_runner.invoke(store_housekeeper, [case_id], obj=taxprofiler_context)
 
         # THEN command should NOT execute successfully
         assert result.exit_code != EXIT_SUCCESS
