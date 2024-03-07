d6be77ba1fe7eba743770a67a76d96fac196df24
islean <isak.ohlsson.angnell@gmail.com>
2024-03-07 16:44:09 +0100

    Handle single cases

--- cg/services/delivery_message/messages/analysis_scout_message.py
+++ cg/services/delivery_message/messages/analysis_scout_message.py
@@ -6,15 +6,33 @@ from cg.services.delivery_message.messages.utils import (
 from cg.store.models import Case
 
 
+def get_case_message(scout_link: str, delivery_path: str):
+    return (
+        f"Hello,\n\n "
+        f"The results have been uploaded to Scout for the following case:\n\n"
+        f"{scout_link}\n\n"
+        f"The analysis files are currently being uploaded to your inbox on Caesar:\n\n"
+        f"{delivery_path}"
+    )
+
+
+def get_cases_message(scout_links: str, delivery_path: str):
+    return (
+        f"Hello,\n\n "
+        f"The results have been uploaded to Scout for the following cases:\n\n"
+        f"{scout_links}\n\n"
+        f"The analysis files are currently being uploaded to your inbox on Caesar:\n\n"
+        f"{delivery_path}"
+    )
+
+
 class AnalysisScoutMessage(DeliveryMessage):
     def create_message(self, cases: list[Case]) -> str:
         scout_links: list[str] = [get_scout_link(case) for case in cases]
         scout_links_row_separated: str = "\n".join(scout_links)
         delivery_path: str = get_fastq_delivery_path(cases[0])
-        return (
-            f"Hello,\n\n "
-            f"The results have been uploaded to Scout for the following cases:\n\n"
-            f"{scout_links_row_separated}\n\n"
-            f"The analysis files are currently being uploaded to your inbox on Caesar:\n\n"
-            f"{delivery_path}"
-        )
+        if len(cases) == 1:
+            return get_case_message(
+                scout_link=scout_links_row_separated, delivery_path=delivery_path
+            )
+        return get_cases_message(scout_links=scout_links_row_separated, delivery_path=delivery_path)
--- cg/services/delivery_message/messages/scout_message.py
+++ cg/services/delivery_message/messages/scout_message.py
@@ -3,8 +3,18 @@ from cg.services.delivery_message.messages.utils import get_scout_link
 from cg.store.models import Case
 
 
+def get_case_message(scout_link: str) -> str:
+    return f"Hello,\n\n The following case has been uploaded to Scout:\n\n {scout_link}"
+
+
+def get_cases_message(scout_links: str) -> str:
+    return f"Hello,\n\n The following cases has been uploaded to Scout:\n\n {scout_links}"
+
+
 class ScoutMessage(DeliveryMessage):
     def create_message(self, cases: list[Case]) -> str:
         scout_links: list[str] = [get_scout_link(case) for case in cases]
         scout_links_row_separated: str = "\n".join(scout_links)
-        return f"Hello,\n\n The following cases has been uploaded to Scout:\n\n {scout_links_row_separated}"
+        if len(cases) == 1:
+            return get_case_message(scout_links_row_separated)
+        return get_cases_message(scout_links_row_separated)
