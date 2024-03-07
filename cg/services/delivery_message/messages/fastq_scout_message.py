e72a9552f4b53f3c37f718dc8b9b7bc6ae6d3e2a
islean <isak.ohlsson.angnell@gmail.com>
2024-03-07 17:05:29 +0100

    Refactor

--- cg/services/delivery_message/messages/fastq_scout_message.py
+++ cg/services/delivery_message/messages/fastq_scout_message.py
@@ -6,7 +6,9 @@ from cg.services.delivery_message.messages.utils import (
 from cg.store.models import Case
 
 
-def get_case_message(scout_link: str, delivery_path) -> str:
+def get_case_message(case: Case) -> str:
+    scout_link: str = get_scout_link(case)
+    delivery_path: str = get_fastq_delivery_path(case)
     return (
         f"Hello,\n\n "
         f"The results have been uploaded to Scout for the following case:\n\n"
@@ -16,11 +18,14 @@ def get_case_message(scout_link: str, delivery_path) -> str:
     )
 
 
-def get_cases_message(scout_links: str, delivery_path) -> str:
+def get_cases_message(cases: list[Case]) -> str:
+    scout_links: list[str] = [get_scout_link(case) for case in cases]
+    scout_links_row_separated: str = "\n".join(scout_links)
+    delivery_path: str = get_fastq_delivery_path(cases[0])
     return (
         f"Hello,\n\n "
-        f"The results have been uploaded to Scout for the following case:\n\n"
-        f"{scout_links}\n\n"
+        f"The results have been uploaded to Scout for the following cases:\n\n"
+        f"{scout_links_row_separated}\n\n"
         f"The fastq files are currently being uploaded to your inbox on Caesar:\n\n"
         f"{delivery_path}"
     )
@@ -28,11 +33,6 @@ def get_cases_message(scout_links: str, delivery_path) -> str:
 
 class FastqScoutMessage(DeliveryMessage):
     def create_message(self, cases: list[Case]) -> str:
-        scout_links: list[str] = [get_scout_link(case) for case in cases]
-        scout_links_row_separated: str = "\n".join(scout_links)
-        delivery_path: str = get_fastq_delivery_path(cases[0])
         if len(cases) == 1:
-            return get_case_message(
-                scout_link=scout_links_row_separated, delivery_path=delivery_path
-            )
-        return get_cases_message(scout_links=scout_links_row_separated, delivery_path=delivery_path)
+            return get_case_message(cases[0])
+        return get_cases_message(cases)
