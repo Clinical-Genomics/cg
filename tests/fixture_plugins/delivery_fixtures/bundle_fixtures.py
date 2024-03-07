88cf76ec1cc8205ea24aa20374e11fc5a97efa33
Vadym <vadym.ivanchuk@scilifelab.se>
2024-03-07 17:22:42 +0100

    Rename fixtures

--- tests/fixture_plugins/delivery_fixtures/bundle_fixtures.py
+++ tests/fixture_plugins/delivery_fixtures/bundle_fixtures.py
@@ -14,20 +14,20 @@ from cg.constants.housekeeper_tags import HK_DELIVERY_REPORT_TAG, AlignmentFileT
 def hk_delivery_sample_bundle(
     sample_hk_bundle_no_files: dict[str, Any],
     sample_id: str,
-    fastq_sample_file: Path,
-    spring_sample_file: Path,
+    delivery_fastq_file: Path,
+    delivery_spring_file: Path,
 ) -> dict:
     sample_hk_bundle: dict[str, Any] = deepcopy(sample_hk_bundle_no_files)
     sample_hk_bundle["name"] = sample_id
     sample_hk_bundle["files"] = [
         {
             "archive": False,
-            "path": fastq_sample_file.as_posix(),
+            "path": delivery_fastq_file.as_posix(),
             "tags": [SequencingFileTag.FASTQ, sample_id],
         },
         {
             "archive": False,
-            "path": spring_sample_file.as_posix(),
+            "path": delivery_spring_file.as_posix(),
             "tags": [SequencingFileTag.SPRING, sample_id],
         },
     ]
@@ -38,20 +38,20 @@ def hk_delivery_sample_bundle(
 def hk_delivery_another_sample_bundle(
     sample_hk_bundle_no_files: dict[str, Any],
     another_sample_id: str,
-    fastq_another_sample_file: Path,
-    spring_another_sample_file: Path,
+    delivery_another_fastq_file: Path,
+    delivery_another_spring_file: Path,
 ) -> dict:
     sample_hk_bundle: dict[str, Any] = deepcopy(sample_hk_bundle_no_files)
     sample_hk_bundle["name"] = another_sample_id
     sample_hk_bundle["files"] = [
         {
             "archive": False,
-            "path": fastq_another_sample_file.as_posix(),
+            "path": delivery_another_fastq_file.as_posix(),
             "tags": [SequencingFileTag.FASTQ, another_sample_id],
         },
         {
             "archive": False,
-            "path": spring_another_sample_file.as_posix(),
+            "path": delivery_another_spring_file.as_posix(),
             "tags": [SequencingFileTag.SPRING, another_sample_id],
         },
     ]
@@ -64,26 +64,26 @@ def hk_delivery_case_bundle(
     case_id: str,
     sample_id: str,
     another_sample_id: str,
-    case_report_file: Path,
-    sample_cram_file: Path,
-    another_sample_cram_file: Path,
+    delivery_report_file: Path,
+    delivery_cram_file: Path,
+    delivery_another_cram_file: Path,
 ) -> dict:
     case_hk_bundle: dict[str, Any] = deepcopy(case_hk_bundle_no_files)
     case_hk_bundle["name"] = case_id
     case_hk_bundle["files"] = [
         {
             "archive": False,
-            "path": case_report_file.as_posix(),
+            "path": delivery_report_file.as_posix(),
             "tags": [HK_DELIVERY_REPORT_TAG, case_id],
         },
         {
             "archive": False,
-            "path": sample_cram_file.as_posix(),
+            "path": delivery_cram_file.as_posix(),
             "tags": [AlignmentFileTag.CRAM, sample_id],
         },
         {
             "archive": False,
-            "path": another_sample_cram_file.as_posix(),
+            "path": delivery_another_cram_file.as_posix(),
             "tags": [AlignmentFileTag.CRAM, another_sample_id],
         },
     ]
--- tests/fixture_plugins/delivery_fixtures/path_fixtures.py
+++ tests/fixture_plugins/delivery_fixtures/path_fixtures.py
@@ -6,49 +6,49 @@ import pytest
 
 
 @pytest.fixture
-def fastq_sample_file(tmp_path: Path, sample_id: str) -> Path:
+def delivery_fastq_file(tmp_path: Path, sample_id: str) -> Path:
     file = Path(tmp_path, f"{sample_id}_R1.fastq.gz")
     file.touch()
     return file
 
 
 @pytest.fixture
-def fastq_another_sample_file(tmp_path: Path, another_sample_id: str) -> Path:
+def delivery_another_fastq_file(tmp_path: Path, another_sample_id: str) -> Path:
     file = Path(tmp_path, f"{another_sample_id}_R1.fastq.gz")
     file.touch()
     return file
 
 
 @pytest.fixture
-def spring_sample_file(tmp_path: Path, sample_id: str) -> Path:
+def delivery_spring_file(tmp_path: Path, sample_id: str) -> Path:
     file = Path(tmp_path, f"{sample_id}_S1.spring")
     file.touch()
     return file
 
 
 @pytest.fixture
-def spring_another_sample_file(tmp_path: Path, another_sample_id: str) -> Path:
+def delivery_another_spring_file(tmp_path: Path, another_sample_id: str) -> Path:
     file = Path(tmp_path, f"{another_sample_id}_S1.spring")
     file.touch()
     return file
 
 
 @pytest.fixture
-def case_report_file(tmp_path: Path, case_id: str) -> Path:
+def delivery_report_file(tmp_path: Path, case_id: str) -> Path:
     file = Path(tmp_path, f"{case_id}_delivery-report.html")
     file.touch()
     return file
 
 
 @pytest.fixture
-def sample_cram_file(tmp_path: Path, sample_id: str) -> Path:
+def delivery_cram_file(tmp_path: Path, sample_id: str) -> Path:
     file = Path(tmp_path, f"{sample_id}.cram")
     file.touch()
     return file
 
 
 @pytest.fixture
-def another_sample_cram_file(tmp_path: Path, another_sample_id: str) -> Path:
+def delivery_another_cram_file(tmp_path: Path, another_sample_id: str) -> Path:
     file = Path(tmp_path, f"{another_sample_id}.cram")
     file.touch()
     return file
