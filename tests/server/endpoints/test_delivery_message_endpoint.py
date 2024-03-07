3f88f6aec04affb170c00953452e48c7e12b4588
islean <isak.ohlsson.angnell@gmail.com>
2024-03-07 13:55:19 +0100

    Fix test

--- tests/server/endpoints/test_delivery_message_endpoint.py
+++ tests/server/endpoints/test_delivery_message_endpoint.py
@@ -5,11 +5,11 @@ from flask.testing import FlaskClient
 from cg.store.models import Case, Order
 
 
-def test_get_delivery_message_single_case(client: FlaskClient, case: Case, order: Order):
+def test_get_delivery_message_single_case(client: FlaskClient, server_case: Case, order: Order):
     # GIVEN a case
 
     # WHEN a request is made to get a delivery message for the case
-    response = client.get(f"/api/v1/cases/{case.internal_id}/delivery_message")
+    response = client.get(f"/api/v1/cases/{server_case.internal_id}/delivery_message")
 
     # THEN the response should be successful
     assert response.status_code == HTTPStatus.OK
