e35936be2791e6ae7a643c387b12c9e0646177eb
Vadym <vadym.ivanchuk@scilifelab.se>
2024-03-08 12:34:15 +0100

    Bottom up

--- cg/meta/delivery/delivery.py
+++ cg/meta/delivery/delivery.py
@@ -34,98 +34,23 @@ class DeliveryAPI:
         self.housekeeper_api: HousekeeperAPI = housekeeper_api
         self.store: Store = store
 
-    def link_delivery_files_to_inbox(self) -> None:
-        """Link files from Housekeeper bundle to the customer's inbox."""
-        raise NotImplementedError
+    @staticmethod
+    def get_analysis_case_tags_for_workflow(workflow: Workflow) -> list[set[str]]:
+        return PIPELINE_ANALYSIS_TAG_MAP[workflow]["case_tags"]
 
-    def get_delivery_files(self, case: Case, force: bool = False) -> list[DeliveryFile]:
-        """Return a complete list of files to be delivered to the customer's inbox."""
-        delivery_files: list[DeliveryFile] = []
-        if DataDelivery.FASTQ in case.data_delivery:
-            fastq_files: list[DeliveryFile] = self.get_fastq_delivery_files(case=case, force=force)
-            delivery_files.extend(fastq_files)
-        if DataDelivery.ANALYSIS_FILES in case.data_delivery:
-            analysis_case_files: list[DeliveryFile] = self.get_analysis_case_delivery_files(case)
-            analysis_sample_files: list[DeliveryFile] = self.get_analysis_sample_delivery_files(
-                case=case
-            )
-            delivery_files.extend(analysis_case_files + analysis_sample_files)
-        return delivery_files
+    @staticmethod
+    def get_analysis_sample_tags_for_workflow(workflow: Workflow) -> list[set[str]]:
+        return PIPELINE_ANALYSIS_TAG_MAP[workflow]["sample_tags"]
 
-    def get_fastq_delivery_files(self, case: Case, force: bool = False) -> list[DeliveryFile]:
-        """Return a complete list of fastq sample files to be delivered."""
-        delivery_files: list[DeliveryFile] = []
-        for sample in case.samples:
-            fastq_delivery_files: list[DeliveryFile] = self.get_fastq_delivery_files_by_sample(
-                case=case, sample=sample, force=force
-            )
-            delivery_files.extend(fastq_delivery_files)
-        return delivery_files
-
-    def get_fastq_delivery_files_by_sample(
-        self, case: Case, sample: Sample, force: bool = False
-    ) -> list[DeliveryFile]:
-        """Return a list of fastq files to be delivered for a specific sample."""
-        delivery_files: list[DeliveryFile] = []
-        fastq_tags: list[set[str]] = self.get_analysis_sample_tags_for_workflow(Workflow.FASTQ)
-        if not self.is_sample_deliverable(sample=sample, force=force):
-            LOG.warning(f"Sample {sample.internal_id} is not deliverable")
-            return delivery_files
-        fastq_files: list[File] = self.housekeeper_api.get_files_from_latest_version_by_tags(
-            bundle_name=sample.internal_id, tags=fastq_tags
-        )
-        delivery_files: list[DeliveryFile] = self.convert_files_to_delivery_files(
-            files=fastq_files,
-            case=case,
-            internal_id=sample.internal_id,
-            external_id=sample.name,
-        )
-        return delivery_files
-
-    def get_analysis_case_delivery_files(self, case: Case) -> list[DeliveryFile]:
+    @staticmethod
+    def is_sample_deliverable(sample: Sample, force: bool = False) -> bool:
         """
-        Return a complete list of analysis case files to be delivered and ignore analysis sample
-        files.
+        Return whether the sample is deliverable or not. A sample is deliverable if it passes qc
+        or is external. The force parameter can be used to override checks.
         """
-        case_tags: list[set[str]] = self.get_analysis_case_tags_for_workflow(case.data_analysis)
-        sample_ids: list[set[str]] = [{sample_id} for sample_id in case.sample_ids]
-        case_files: list[File] = self.housekeeper_api.get_files_from_latest_version_by_tags(
-            bundle_name=case.internal_id, tags=case_tags, excluded_tags=sample_ids
-        )
-        delivery_files: list[DeliveryFile] = self.convert_files_to_delivery_files(
-            files=case_files, case=case, internal_id=case.internal_id, external_id=case.name
-        )
-        return delivery_files
-
-    def get_analysis_sample_delivery_files(self, case: Case) -> list[DeliveryFile]:
-        """Return a complete list of analysis sample files to be delivered."""
-        delivery_files: list[DeliveryFile] = []
-        for sample in case.samples:
-            sample_delivery_files: list[DeliveryFile] = (
-                self.get_analysis_sample_delivery_files_by_sample(case=case, sample=sample)
-            )
-            delivery_files.extend(sample_delivery_files)
-        return delivery_files
-
-    def get_analysis_sample_delivery_files_by_sample(
-        self, case: Case, sample: Sample
-    ) -> list[DeliveryFile]:
-        """Return a list of analysis files to be delivered for a specific sample."""
-        sample_tags: list[set[str]] = self.get_analysis_sample_tags_for_workflow(case.data_analysis)
-        sample_tags_with_sample_id: list[set[str]] = [
-            tag | {sample.internal_id} for tag in sample_tags
-        ]
-        sample_files: list[File] = self.housekeeper_api.get_files_from_latest_version_by_tags(
-            bundle_name=case.internal_id, tags=sample_tags_with_sample_id
-        )
-        delivery_files: list[DeliveryFile] = self.convert_files_to_delivery_files(
-            files=sample_files,
-            case=case,
-            internal_id=sample.internal_id,
-            external_id=sample.name,
-            analysis_sample_files=True,
-        )
-        return delivery_files
+        is_external: bool = sample.application_version.application.is_external
+        qc_pass: bool = sample.sequencing_qc
+        return is_external or qc_pass or force
 
     def convert_files_to_delivery_files(
         self,
@@ -152,20 +77,95 @@ class DeliveryAPI:
             delivery_files.append(delivery_file)
         return delivery_files
 
-    @staticmethod
-    def get_analysis_case_tags_for_workflow(workflow: Workflow) -> list[set[str]]:
-        return PIPELINE_ANALYSIS_TAG_MAP[workflow]["case_tags"]
+    def get_analysis_sample_delivery_files_by_sample(
+        self, case: Case, sample: Sample
+    ) -> list[DeliveryFile]:
+        """Return a list of analysis files to be delivered for a specific sample."""
+        sample_tags: list[set[str]] = self.get_analysis_sample_tags_for_workflow(case.data_analysis)
+        sample_tags_with_sample_id: list[set[str]] = [
+            tag | {sample.internal_id} for tag in sample_tags
+        ]
+        sample_files: list[File] = self.housekeeper_api.get_files_from_latest_version_by_tags(
+            bundle_name=case.internal_id, tags=sample_tags_with_sample_id
+        )
+        delivery_files: list[DeliveryFile] = self.convert_files_to_delivery_files(
+            files=sample_files,
+            case=case,
+            internal_id=sample.internal_id,
+            external_id=sample.name,
+            analysis_sample_files=True,
+        )
+        return delivery_files
 
-    @staticmethod
-    def get_analysis_sample_tags_for_workflow(workflow: Workflow) -> list[set[str]]:
-        return PIPELINE_ANALYSIS_TAG_MAP[workflow]["sample_tags"]
+    def get_analysis_sample_delivery_files(self, case: Case) -> list[DeliveryFile]:
+        """Return a complete list of analysis sample files to be delivered."""
+        delivery_files: list[DeliveryFile] = []
+        for sample in case.samples:
+            sample_delivery_files: list[DeliveryFile] = (
+                self.get_analysis_sample_delivery_files_by_sample(case=case, sample=sample)
+            )
+            delivery_files.extend(sample_delivery_files)
+        return delivery_files
 
-    @staticmethod
-    def is_sample_deliverable(sample: Sample, force: bool = False) -> bool:
+    def get_analysis_case_delivery_files(self, case: Case) -> list[DeliveryFile]:
         """
-        Return whether the sample is deliverable or not. A sample is deliverable if it passes qc
-        or is external. The force parameter can be used to override checks.
+        Return a complete list of analysis case files to be delivered and ignore analysis sample
+        files.
         """
-        is_external: bool = sample.application_version.application.is_external
-        qc_pass: bool = sample.sequencing_qc
-        return is_external or qc_pass or force
+        case_tags: list[set[str]] = self.get_analysis_case_tags_for_workflow(case.data_analysis)
+        sample_ids: list[set[str]] = [{sample_id} for sample_id in case.sample_ids]
+        case_files: list[File] = self.housekeeper_api.get_files_from_latest_version_by_tags(
+            bundle_name=case.internal_id, tags=case_tags, excluded_tags=sample_ids
+        )
+        delivery_files: list[DeliveryFile] = self.convert_files_to_delivery_files(
+            files=case_files, case=case, internal_id=case.internal_id, external_id=case.name
+        )
+        return delivery_files
+
+    def get_fastq_delivery_files_by_sample(
+        self, case: Case, sample: Sample, force: bool = False
+    ) -> list[DeliveryFile]:
+        """Return a list of fastq files to be delivered for a specific sample."""
+        delivery_files: list[DeliveryFile] = []
+        fastq_tags: list[set[str]] = self.get_analysis_sample_tags_for_workflow(Workflow.FASTQ)
+        if not self.is_sample_deliverable(sample=sample, force=force):
+            LOG.warning(f"Sample {sample.internal_id} is not deliverable")
+            return delivery_files
+        fastq_files: list[File] = self.housekeeper_api.get_files_from_latest_version_by_tags(
+            bundle_name=sample.internal_id, tags=fastq_tags
+        )
+        delivery_files: list[DeliveryFile] = self.convert_files_to_delivery_files(
+            files=fastq_files,
+            case=case,
+            internal_id=sample.internal_id,
+            external_id=sample.name,
+        )
+        return delivery_files
+
+    def get_fastq_delivery_files(self, case: Case, force: bool = False) -> list[DeliveryFile]:
+        """Return a complete list of fastq sample files to be delivered."""
+        delivery_files: list[DeliveryFile] = []
+        for sample in case.samples:
+            fastq_delivery_files: list[DeliveryFile] = self.get_fastq_delivery_files_by_sample(
+                case=case, sample=sample, force=force
+            )
+            delivery_files.extend(fastq_delivery_files)
+        return delivery_files
+
+    def get_delivery_files(self, case: Case, force: bool = False) -> list[DeliveryFile]:
+        """Return a complete list of files to be delivered to the customer's inbox."""
+        delivery_files: list[DeliveryFile] = []
+        if DataDelivery.FASTQ in case.data_delivery:
+            fastq_files: list[DeliveryFile] = self.get_fastq_delivery_files(case=case, force=force)
+            delivery_files.extend(fastq_files)
+        if DataDelivery.ANALYSIS_FILES in case.data_delivery:
+            analysis_case_files: list[DeliveryFile] = self.get_analysis_case_delivery_files(case)
+            analysis_sample_files: list[DeliveryFile] = self.get_analysis_sample_delivery_files(
+                case=case
+            )
+            delivery_files.extend(analysis_case_files + analysis_sample_files)
+        return delivery_files
+
+    def link_delivery_files_to_inbox(self) -> None:
+        """Link files from Housekeeper bundle to the customer's inbox."""
+        raise NotImplementedError
