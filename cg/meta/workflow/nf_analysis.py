6d614616d30a154276fe65d8fb3c2b0ad84f3341
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-03-08 12:48:15 +0100

    change try statement to avoid running when trailblazer fails

--- cg/meta/workflow/nf_analysis.py
+++ cg/meta/workflow/nf_analysis.py
@@ -295,17 +295,33 @@ class NfAnalysisAPI(AnalysisAPI):
     def run_nextflow_analysis(
         self, case_id: str, use_nextflow: bool, command_args=NfCommandArgs, dry_run: bool = False
     ) -> None:
+        # try:
+        #     self.verify_sample_sheet_exists(case_id=case_id, dry_run=dry_run)
+        #     self.check_analysis_ongoing(case_id=case_id)
+        #     LOG.info(f"Running analysis for {case_id}")
+        #     self.run_analysis(
+        #         case_id=case_id,
+        #         command_args=command_args,
+        #         use_nextflow=use_nextflow,
+        #         dry_run=dry_run,
+        #     )
+        #     self.set_statusdb_action(case_id=case_id, action=CaseActions.RUNNING, dry_run=dry_run)
+        # except FileNotFoundError as error:
+        #     LOG.error(f"Could not resume analysis: {error}")
+        #     raise click.Abort() from error
+        # except (CgError, ValueError) as error:
+        #     LOG.error(f"Could not run analysis: {error}")
+        #     raise click.Abort() from error
+        # except Exception as error:
+        #     LOG.error(f"Could not run analysis: {error}")
+        #     raise click.Abort() from error
+        # if not dry_run:
+        #     self.add_pending_trailblazer_analysis(case_id=case_id)
+
+
         try:
             self.verify_sample_sheet_exists(case_id=case_id, dry_run=dry_run)
             self.check_analysis_ongoing(case_id=case_id)
-            LOG.info(f"Running analysis for {case_id}")
-            self.run_analysis(
-                case_id=case_id,
-                command_args=command_args,
-                use_nextflow=use_nextflow,
-                dry_run=dry_run,
-            )
-            self.set_statusdb_action(case_id=case_id, action=CaseActions.RUNNING, dry_run=dry_run)
         except FileNotFoundError as error:
             LOG.error(f"Could not resume analysis: {error}")
             raise click.Abort() from error
@@ -315,8 +331,19 @@ class NfAnalysisAPI(AnalysisAPI):
         except Exception as error:
             LOG.error(f"Could not run analysis: {error}")
             raise click.Abort() from error
+
+        LOG.info(f"Running analysis for {case_id}")
+        self.run_analysis(
+                case_id=case_id,
+                command_args=command_args,
+                use_nextflow=use_nextflow,
+                dry_run=dry_run,
+            )
+        self.set_statusdb_action(case_id=case_id, action=CaseActions.RUNNING, dry_run=dry_run)
         if not dry_run:
             self.add_pending_trailblazer_analysis(case_id=case_id)
+
+
 
     def run_analysis(
         self,
