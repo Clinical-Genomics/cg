ff71dfc95e34308d5d32850d8c435ed32aac2e43
Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>
2024-03-11 16:04:18 +0100

    (Streamline delivery) Add delivery message endpoint for order (#3030) (minor)
    ### Added
    
    - Endpoint for getting the delivery message for an order

--- cg/services/delivery_message/messages/scout_message.py
+++ cg/services/delivery_message/messages/scout_message.py
@@ -5,13 +5,15 @@ from cg.store.models import Case
 
 def get_case_message(case: Case) -> str:
     scout_link: str = get_scout_link(case)
-    return f"Hello,\n\n The following case has been uploaded to Scout:\n\n {scout_link}"
+    return f"Hello,\n\nThe following case has been uploaded to Scout:\n\n{scout_link}"
 
 
 def get_cases_message(cases: list[Case]) -> str:
     scout_links: list[str] = [get_scout_link(case) for case in cases]
     scout_links_row_separated: str = "\n".join(scout_links)
-    return f"Hello,\n\n The following cases has been uploaded to Scout:\n\n {scout_links_row_separated}"
+    return (
+        f"Hello,\n\nThe following cases has been uploaded to Scout:\n\n{scout_links_row_separated}"
+    )
 
 
 class ScoutMessage(DeliveryMessage):
