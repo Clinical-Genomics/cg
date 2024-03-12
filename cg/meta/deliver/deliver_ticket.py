401219fef5c2d0b1adbddf79fe53e22a3e984606
Karl Nyrén <45558267+karlnyr@users.noreply.github.com>
2024-03-12 16:13:22 +0100

    Update cg/meta/deliver/deliver_ticket.py
    Co-authored-by: Vadym <vadym.ivanchuk@scilifelab.se>

--- cg/meta/deliver/deliver_ticket.py
+++ cg/meta/deliver/deliver_ticket.py
@@ -1,5 +1,6 @@
-"""Module for deliver and rsync customer inbox on the HPC to customer inbox on the delivery
-server """
+"""
+Module for deliver and rsync customer inbox on the HPC to customer inbox on the delivery server.
+"""
 
 import logging
 import os
