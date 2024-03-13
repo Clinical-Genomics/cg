1b2378680df99557b847e2dc5f4babcc156df555
Vadym <vadym.ivanchuk@scilifelab.se>
2024-03-13 17:47:12 +0100

    Fix access empty list

--- cg/apps/housekeeper/hk.py
+++ cg/apps/housekeeper/hk.py
@@ -164,7 +164,9 @@ class HousekeeperAPI:
     def get_file_from_version(version: Version, tags: set[str]) -> File | None:
         """Return the first file matching the given tags."""
         files: list[File] = HousekeeperAPI.get_files_from_version(version=version, tags=tags)
-        return None if not files else files[0]
+        if not files:
+            return None
+        return files[0]
 
     @staticmethod
     def get_latest_file_from_version(version: Version, tags: set[str]) -> File | None:
