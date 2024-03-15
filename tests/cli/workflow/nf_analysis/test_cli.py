ffae8ef55b72a7c3566cf563305058cf048a339f
Felix Lenner <felix.lenner@igp.uu.se>
2024-03-15 10:48:30 +0100

    Keep style consistent with other refactored tests

--- tests/cli/workflow/nf_analysis/test_cli.py
+++ tests/cli/workflow/nf_analysis/test_cli.py
@@ -1,35 +1,29 @@
-import pytest
-from enum import Enum
-from click.testing import CliRunner
-from cg.cli.workflow.raredisease.base import raredisease
-from cg.cli.workflow.taxprofiler.base import taxprofiler
-from cg.cli.workflow.rnafusion.base import rnafusion
+"""Tests CLI common methods that Nextflow workflows print help and exits without error."""
 
-from cg.constants import EXIT_SUCCESS
+import pytest
+from _pytest.fixtures import FixtureRequest
+from click.testing import CliRunner
+
+from cg.cli.workflow.base import workflow as workflow_cli
+from cg.constants import EXIT_SUCCESS, Workflow
 from cg.models.cg_config import CGConfig
 
 @pytest.mark.parametrize(
-    "pipeline, context",
-    [
-        (rnafusion, "rnafusion_context"),
-        (taxprofiler, "taxprofiler_context"),
-        (raredisease, "raredisease_context"),
-    ]
+    "workflow",
+    [Workflow.RAREDISEASE, Workflow.RNAFUSION, Workflow.TAXPROFILER],
+
 )
 
 def test_workflow_no_args(
-        cli_runner: CliRunner,
-        pipeline,
-        context: CGConfig,
-        request
+        cli_runner: CliRunner, workflow: Workflow, request: FixtureRequest
 ):
-    """Test to assess that Nextflow workflows without options successfully print help and exist without error."""
-    context = request.getfixturevalue(context)
+    """Test to assess that Nextflow workflows without options successfully print help and exits without error."""
+    context: CGConfig = request.getfixturevalue(f"{workflow}_context")
 
     # GIVEN no arguments or options besides the command call
 
     # WHEN running command
-    result = cli_runner.invoke(pipeline, [], obj=context)
+    result = cli_runner.invoke(workflow_cli, [], obj=context)
 
     # THEN command runs successfully
     assert result.exit_code == EXIT_SUCCESS
