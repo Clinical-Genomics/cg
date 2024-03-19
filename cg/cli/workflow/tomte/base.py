4dadabe2de15aa0b060518d87552d6631b6c0254
Eva Caceres <eva.caceres@scilifelab.se>
2024-03-19 17:52:46 +0100

    add config-case for tomte

--- cg/cli/workflow/tomte/base.py
+++ cg/cli/workflow/tomte/base.py
@@ -4,6 +4,8 @@ import logging
 
 import click
 
+from cg.cli.workflow.commands import resolve_compression
+from cg.cli.workflow.nf_analysis import config_case
 from cg.constants.constants import MetaApis
 from cg.meta.workflow.analysis import AnalysisAPI
 from cg.meta.workflow.tomte import TomteAnalysisAPI
@@ -17,3 +19,7 @@ def tomte(context: click.Context) -> None:
     """gms/tomte analysis workflow."""
     AnalysisAPI.get_help(context)
     context.obj.meta_apis[MetaApis.ANALYSIS_API] = TomteAnalysisAPI(config=context.obj)
+
+
+tomte.add_command(resolve_compression)
+tomte.add_command(config_case)
