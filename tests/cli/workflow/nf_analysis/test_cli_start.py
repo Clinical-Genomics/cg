cbe1f5cf9b7f1e8f1bb9a39b710528e3ad425848
Eva Caceres <eva.caceres@scilifelab.se>
2024-03-20 21:15:03 +0100

    fix pytests

--- tests/cli/workflow/nf_analysis/test_cli_start.py
+++ tests/cli/workflow/nf_analysis/test_cli_start.py
@@ -1,39 +1,43 @@
 import logging
 
+import pytest
+from _pytest.fixtures import FixtureRequest
 from _pytest.logging import LogCaptureFixture
 from click.testing import CliRunner
 
-from cg.apps.hermes.hermes_api import HermesApi
-from cg.apps.hermes.models import CGDeliverables
-from cg.apps.housekeeper.hk import HousekeeperAPI
 from cg.cli.workflow.base import workflow as workflow_cli
-from cg.cli.workflow.rnafusion.base import start, start_available, store, store_available
 from cg.constants import EXIT_SUCCESS, Workflow
-from cg.meta.workflow.rnafusion import RnafusionAnalysisAPI
+from cg.meta.workflow.nf_analysis import NfAnalysisAPI
 from cg.models.cg_config import CGConfig
 from tests.cli.workflow.conftest import mock_analysis_flow_cell
 
 
+@pytest.mark.parametrize(
+    "workflow",
+    [Workflow.RNAFUSION, Workflow.TAXPROFILER],
+)
 def test_start(
     cli_runner: CliRunner,
-    rnafusion_context: CGConfig,
+    workflow: Workflow,
     caplog: LogCaptureFixture,
-    rnafusion_case_id: str,
     mock_analysis_flow_cell,
+    request: FixtureRequest,
+    mocker,
 ):
     """Test to ensure all parts of start command will run successfully given ideal conditions."""
-    caplog.set_level(logging.INFO)
+    caplog.set_level(logging.DEBUG)
+    context: CGConfig = request.getfixturevalue(f"{workflow}_context")
 
-    # GIVEN case id
-    case_id: str = rnafusion_case_id
+    # GIVEN a case id
+    case_id: str = request.getfixturevalue(f"{workflow}_case_id")
 
     # GIVEN a mocked config
 
     # GIVEN decompression is not needed
-    RnafusionAnalysisAPI.resolve_decompression.return_value = None
+    mocker.patch.object(NfAnalysisAPI, "resolve_decompression", return_value=None)
 
-    # WHEN dry running with dry specified
-    result = cli_runner.invoke(start, [case_id, "--dry-run"], obj=rnafusion_context)
+    # WHEN invoking the command with dry-run specified
+    result = cli_runner.invoke(workflow_cli, [workflow, "start", case_id, "--dry-run"], obj=context)
 
     # THEN command should execute successfully
     assert result.exit_code == EXIT_SUCCESS
@@ -43,66 +47,44 @@ def test_start(
     assert "-resume" not in caplog.text
 
 
+@pytest.mark.parametrize(
+    "workflow",
+    [Workflow.RNAFUSION, Workflow.TAXPROFILER],
+)
 def test_start_available(
     cli_runner: CliRunner,
-    rnafusion_context: CGConfig,
+    workflow: Workflow,
     caplog: LogCaptureFixture,
     mocker,
-    rnafusion_case_id: str,
     mock_analysis_flow_cell,
-):
-    """Test to ensure all parts of compound start-available command are executed given ideal conditions
-    Test that start-available picks up eligible cases and does not pick up ineligible ones."""
-    caplog.set_level(logging.INFO)
-
-    # GIVEN CASE ID of sample where read counts pass threshold
-    case_id_success: str = rnafusion_case_id
-
-    # GIVEN a mocked config
-
-    # GIVEN decompression is not needed
-    mocker.patch.object(RnafusionAnalysisAPI, "resolve_decompression")
-    RnafusionAnalysisAPI.resolve_decompression.return_value = None
-
-    # WHEN running command
-    result = cli_runner.invoke(start_available, ["--dry-run"], obj=rnafusion_context)
-
-    # THEN command exits with 0
-    assert result.exit_code == EXIT_SUCCESS
-
-    # THEN it should successfully identify the one case eligible for auto-start
-    assert case_id_success in caplog.text
-
-
-
-def test_start_available(
-    cli_runner: CliRunner,
-    rnafusion_context: CGConfig,
-    caplog: LogCaptureFixture,
-    mocker,
-    rnafusion_case_id: str,
+    request: FixtureRequest,
     case_id_not_enough_reads: str,
 ):
     """Test to ensure all parts of compound start-available command are executed given ideal conditions
     Test that start-available picks up eligible cases and does not pick up ineligible ones."""
-    caplog.set_level(logging.INFO)
+    caplog.set_level(logging.DEBUG)
+    context: CGConfig = request.getfixturevalue(f"{workflow}_context")
+
+    # GIVEN CASE ID of sample where read counts pass threshold
+    case_id: str = request.getfixturevalue(f"{workflow}_case_id")
 
     # GIVEN a case passing read counts threshold and another one not passing
 
     # GIVEN a mocked config
 
     # GIVEN decompression is not needed
-    mocker.patch.object(RnafusionAnalysisAPI, "resolve_decompression")
-    RnafusionAnalysisAPI.resolve_decompression.return_value = None
+    mocker.patch.object(NfAnalysisAPI, "resolve_decompression", return_value=None)
 
-    # WHEN running command
-    result = cli_runner.invoke(start_available, ["--dry-run"], obj=rnafusion_context)
+    # WHEN invoking the command with dry-run specified
+    result = cli_runner.invoke(
+        workflow_cli, [workflow, "start-available", "--dry-run"], obj=context
+    )
 
     # THEN command exits with 0
     assert result.exit_code == EXIT_SUCCESS
 
     # THEN it should successfully identify the one case eligible for auto-start
-    assert rnafusion_case_id in caplog.text
+    assert case_id in caplog.text
 
     # THEN the case without enough reads should not start
     assert case_id_not_enough_reads not in caplog.text
