6a0764a928a0c8a80f2efbb5a7eef1471a1eb76a
karlnyr <kallejohan211@hotmail.com>
2024-03-20 09:58:28 +0100

    Update analysis messages in delivery_message module

--- cg/services/delivery_message/messages/scout_message.py
+++ cg/services/delivery_message/messages/scout_message.py
@@ -5,14 +5,14 @@ from cg.store.models import Case
 
 def get_case_message(case: Case) -> str:
     scout_link: str = get_scout_link(case)
-    return f"Hello,\n\nThe following case has been uploaded to Scout:\n\n{scout_link}"
+    return f"Hello,\n\nThe analysis has been uploaded to Scout for the following case:\n\n{scout_link}"
 
 
 def get_cases_message(cases: list[Case]) -> str:
     scout_links: list[str] = [get_scout_link(case) for case in cases]
     scout_links_row_separated: str = "\n".join(scout_links)
     return (
-        f"Hello,\n\nThe following cases have been uploaded to Scout:\n\n{scout_links_row_separated}"
+        f"Hello,\n\nThe analyses of the following cases have been uploaded to Scout:\n\n{scout_links_row_separated}"
     )
 
 
