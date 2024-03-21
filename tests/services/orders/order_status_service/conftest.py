c3fab6fcf402ae42b1fdabcf296404c05b1334e8
Sebastian Allard <seallards@gmail.com>
2024-03-21 13:41:26 +0100

    Improve fixtures

--- tests/services/orders/order_status_service/conftest.py
+++ tests/services/orders/order_status_service/conftest.py
@@ -4,7 +4,7 @@ from cg.apps.tb.api import TrailblazerAPI
 from cg.apps.tb.dto.summary_response import AnalysisSummary
 
 from cg.services.orders.order_status_service.order_summary_service import OrderSummaryService
-from cg.store.models import Customer, Order
+from cg.store.models import Customer, Order, Sample
 from cg.store.store import Store
 from tests.store_helpers import StoreHelpers
 
@@ -33,49 +33,62 @@ def analysis_summary(order: Order) -> AnalysisSummary:
         failed=5,
     )
 
+
 @pytest.fixture
-def order_with_case_in_preparation(store: Store, helpers: StoreHelpers) -> Order:
-    customer: Customer = helpers.ensure_customer(store)
-    order: Order = helpers.add_order(store=store, customer_id=customer.id, ticket_id=1)
-    case = helpers.ensure_case(store=store, customer=customer, order=order)
-    sample = helpers.add_sample(
+def sample_not_received(store: Store, helpers: StoreHelpers) -> Sample:
+    return helpers.add_sample(
+        store=store,
+        internal_id="in_prep",
+        received_at=None,
+        prepared_at=None,
+        last_sequenced_at=None,
+    )
+
+
+@pytest.fixture
+def sample_in_preparation(store: Store, helpers: StoreHelpers) -> Sample:
+    return helpers.add_sample(
         store=store,
         internal_id="in_prep",
         received_at=datetime.now(),
         prepared_at=None,
         last_sequenced_at=None,
     )
-    helpers.add_relationship(store=store, sample=sample, case=case)
-    return order
 
 
 @pytest.fixture
-def order_with_case_not_received(store: Store, helpers: StoreHelpers) -> Order:
-    customer: Customer = helpers.ensure_customer(store)
-    order: Order = helpers.add_order(store=store, customer_id=customer.id, ticket_id=1)
-    case = helpers.ensure_case(store=store, customer=customer, order=order)
-    sample = helpers.add_sample(
+def sample_in_sequencing(store: Store, helpers: StoreHelpers) -> Sample:
+    return helpers.add_sample(
         store=store,
-        internal_id="not_received",
-        received_at=None,
-        prepared_at=None,
-        last_sequenced_at=None,
-    )
-    helpers.add_relationship(store=store, sample=sample, case=case)
-    return order
-
-
-@pytest.fixture
-def order_with_case_in_sequencing(store: Store, helpers: StoreHelpers) -> Order:
-    customer: Customer = helpers.ensure_customer(store)
-    order: Order = helpers.add_order(store=store, customer_id=customer.id, ticket_id=1)
-    case = helpers.ensure_case(store=store, customer=customer, order=order)
-    sample = helpers.add_sample(
-        store=store,
-        internal_id="in_sequencing",
+        internal_id="in_prep",
         received_at=datetime.now(),
         prepared_at=datetime.now(),
         last_sequenced_at=None,
     )
-    helpers.add_relationship(store=store, sample=sample, case=case)
+
+
+@pytest.fixture
+def order_with_case_in_preparation(
+    store: Store, helpers: StoreHelpers, order: Order, sample_in_preparation: Sample
+) -> Order:
+    case = helpers.ensure_case(store=store, customer=order.customer, order=order)
+    helpers.add_relationship(store=store, sample=sample_in_preparation, case=case)
+    return order
+
+
+@pytest.fixture
+def order_with_case_not_received(
+    store: Store, helpers: StoreHelpers, order: Order, sample_not_received: Sample
+) -> Order:
+    case = helpers.ensure_case(store=store, customer=order.customer, order=order)
+    helpers.add_relationship(store=store, sample=sample_not_received, case=case)
+    return order
+
+
+@pytest.fixture
+def order_with_case_in_sequencing(
+    store: Store, helpers: StoreHelpers, order: Order, sample_in_sequencing: Sample
+) -> Order:
+    case = helpers.ensure_case(store=store, customer=order.customer, order=order)
+    helpers.add_relationship(store=store, sample=sample_in_sequencing, case=case)
     return order
