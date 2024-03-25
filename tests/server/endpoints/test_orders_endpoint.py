2c75b690026d7847aca899e152b8acb6fbe33fba
seallard <sebastian.allard@scilifelab.se>
2024-03-25 14:17:09 +0100

    Readd test

--- tests/server/endpoints/test_orders_endpoint.py
+++ tests/server/endpoints/test_orders_endpoint.py
@@ -2,10 +2,56 @@ from http import HTTPStatus
 
 import mock.mock
 from flask.testing import FlaskClient
+import pytest
 
 from cg.apps.tb import TrailblazerAPI
 from cg.apps.tb.dto.summary_response import AnalysisSummary
+from cg.constants.constants import Workflow
 from cg.store.models import Order
+
+
+@pytest.mark.parametrize(
+    "limit, workflow, expected_orders",
+    [
+        (None, Workflow.MIP_DNA, 2),
+        (1, Workflow.MIP_DNA, 1),
+        (2, Workflow.MIP_DNA, 2),
+        (None, Workflow.BALSAMIC, 1),
+        (None, Workflow.FLUFFY, 0),
+        (None, None, 3),
+    ],
+)
+def test_orders_endpoint(
+    client: FlaskClient,
+    order: Order,
+    order_another: Order,
+    order_balsamic: Order,
+    limit: int | None,
+    workflow: str,
+    expected_orders: int,
+    analysis_summary,
+):
+    """Tests that orders are returned from the orders endpoint"""
+    # GIVEN a store with three orders, two of which are MIP-DNA and the last is BALSAMIC
+
+    # WHEN a request is made to get all orders
+    endpoint: str = "/api/v1/orders"
+    with mock.patch.object(
+        TrailblazerAPI,
+        "get_summaries",
+        return_value=[
+            AnalysisSummary(order_id=order.id),
+            AnalysisSummary(order_id=order_another.id),
+            AnalysisSummary(order_id=order_balsamic.id),
+        ],
+    ):
+        response = client.get(endpoint, query_string={"pageSize": limit, "workflow": workflow})
+
+    # THEN the response should be successful
+    assert response.status_code == HTTPStatus.OK
+
+    # THEN the response contains the correct number of orders
+    assert len(response.json["orders"]) == expected_orders
 
 
 def test_order_endpoint(
