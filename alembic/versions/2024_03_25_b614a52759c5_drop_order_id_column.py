4a067ae9c0c2c13f492aa272feb86064370b2f92
islean <isak.ohlsson.angnell@gmail.com>
2024-03-25 16:01:17 +0100

    Drop order_id column

---
+++ alembic/versions/2024_03_25_b614a52759c5_drop_order_id_column.py
@@ -0,0 +1,30 @@
+"""Drop order id column
+
+Revision ID: b614a52759c5
+Revises: 24c1e9e1476d
+Create Date: 2024-03-25 15:52:29.658272
+
+"""
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "b614a52759c5"
+down_revision = "24c1e9e1476d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.drop_column("case", "order_id")
+
+
+def downgrade():
+    op.add_column(
+        "case",
+        sa.Column(
+            "order_id", sa.Integer(), sa.ForeignKey("order.id", name="case_ibfk_2"), nullable=True
+        ),
+    )
