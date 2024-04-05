d0cdee0cb1500d38eb05d276f8ea508a60d0e617
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-04-05 15:55:44 +0200

    add raredisease start and start-available

--- cg/cli/workflow/raredisease/base.py
+++ cg/cli/workflow/raredisease/base.py
@@ -6,7 +6,12 @@ import click
 
 from cg.cli.utils import echo_lines
 from cg.cli.workflow.commands import ARGUMENT_CASE_ID, OPTION_DRY, resolve_compression
-from cg.cli.workflow.nf_analysis import config_case, run
+from cg.cli.workflow.nf_analysis import (
+    config_case,
+    run,
+    start,
+    start_available
+)
 from cg.constants.constants import MetaApis
 from cg.meta.workflow.analysis import AnalysisAPI
 from cg.meta.workflow.raredisease import RarediseaseAnalysisAPI
@@ -26,7 +31,8 @@ def raredisease(context: click.Context) -> None:
 raredisease.add_command(resolve_compression)
 raredisease.add_command(config_case)
 raredisease.add_command(run)
-
+raredisease.add_command(start)
+raredisease.add_command(start_available)
 
 @raredisease.command("panel")
 @OPTION_DRY
