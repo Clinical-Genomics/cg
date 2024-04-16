87ad615c6be7b4f72423047c33dda7c427cd5801
seallard <sebastian.allard@scilifelab.se>
2024-04-16 18:54:31 +0200

    Formatting

--- alembic/versions/2024_04_16_dd3fd72df1ae_remove_order_date_from_pool.py
+++ alembic/versions/2024_04_16_dd3fd72df1ae_remove_order_date_from_pool.py
@@ -5,19 +5,21 @@ Revises: ac5a804a9f47
 Create Date: 2024-04-16 18:51:33.598555
 
 """
+
 from alembic import op
 import sqlalchemy as sa
 from sqlalchemy.dialects import mysql
 
 # revision identifiers, used by Alembic.
-revision = 'dd3fd72df1ae'
-down_revision = 'ac5a804a9f47'
+revision = "dd3fd72df1ae"
+down_revision = "ac5a804a9f47"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.drop_column('pool', 'ordered_at')
+    op.drop_column("pool", "ordered_at")
+
 
 def downgrade():
-    op.add_column('pool', sa.Column('ordered_at', mysql.DATETIME(), nullable=False))
+    op.add_column("pool", sa.Column("ordered_at", mysql.DATETIME(), nullable=False))
