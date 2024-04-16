c4af08de785453ddeb82f4efd86d435ef208d8ba
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2024-04-16 08:36:41 +0200

    feat(linters): Update

--- alembic/versions/2024_03_27_9cf71c905539_remove_delivery_table.py
+++ alembic/versions/2024_03_27_9cf71c905539_remove_delivery_table.py
@@ -5,12 +5,14 @@ Revises: b614a52759c5
 Create Date: 2024-03-27 11:51:51.148871
 
 """
-from alembic import op
+
 import sqlalchemy as sa
 
+from alembic import op
+
 # revision identifiers, used by Alembic.
-revision = '9cf71c905539'
-down_revision = 'b614a52759c5'
+revision = "9cf71c905539"
+down_revision = "b614a52759c5"
 branch_labels = None
 depends_on = None
 
@@ -20,6 +22,7 @@ def upgrade():
     op.drop_table(table_name="delivery")
     op.drop_column(table_name="pool", column_name="deliveries")
     op.drop_column(table_name="sample", column_name="deliveries")
+
 
 def downgrade():
 
@@ -31,9 +34,10 @@ def downgrade():
         sa.Column("id", sa.Integer, nullable=False, primary_key=True),
         sa.Column("delivered_at", sa.DateTime, nullable=True),
         sa.Column("removed_at", sa.DateTime, nullable=True),
-        sa.Column("destination", sa.Enum(*("caesar", "pdc", "uppmax", "mh", "custom")), default="caesar"),
+        sa.Column(
+            "destination", sa.Enum(*("caesar", "pdc", "uppmax", "mh", "custom")), default="caesar"
+        ),
         sa.ForeignKeyConstraint(["sample_id"], ["sample.id"], ondelete="CASCADE"),
         sa.ForeignKeyConstraint(["pool_id"], ["pool.id"], ondelete="CASCADE"),
         sa.Column("comment", sa.types.TEXT, nullable=True),
     )
-
