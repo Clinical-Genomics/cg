64ed1f8eafba58a6a91fc0d913b325c9b91a0911
Vadym <vadym.ivanchuk@scilifelab.se>
2024-04-22 15:39:15 +0200

    Remove outdated lims check

--- cg/meta/report/tomte.py
+++ cg/meta/report/tomte.py
@@ -34,19 +34,11 @@ class TomteReportAPI(ReportAPI):
     ) -> TomteSampleMetadataModel:
         """Return Tomte sample metadata to include in the delivery report."""
         sample_metrics: TomteQCMetrics = analysis_metadata.sample_metrics[sample.internal_id]
-
-        # Skip LIMS data collection if down sampled
-        input_amount = None
-        rin = None
-        if not sample.downsampled_to:
-            input_amount = self.lims_api.get_latest_rna_input_amount(sample_id=sample.internal_id)
-            rin = self.lims_api.get_sample_rin(sample_id=sample.internal_id)
-
         return TomteSampleMetadataModel(
             bias_5_3=sample_metrics.median_5prime_to_3prime_bias,
             duplicates=sample_metrics.pct_duplication,
             gc_content=sample_metrics.after_filtering_gc_content,
-            input_amount=input_amount,
+            input_amount=self.lims_api.get_latest_rna_input_amount(sample.internal_id),
             mean_length_r1=sample_metrics.after_filtering_read1_mean_length,
             million_read_pairs=get_million_read_pairs(
                 reads=sample_metrics.before_filtering_total_reads
@@ -59,7 +51,7 @@ class TomteReportAPI(ReportAPI):
             q20_rate=sample_metrics.after_filtering_q20_rate,
             q30_rate=sample_metrics.after_filtering_q30_rate,
             ribosomal_bases=sample_metrics.pct_ribosomal_bases,
-            rin=rin,
+            rin=self.lims_api.get_sample_rin(sample.internal_id),
             uniquely_mapped_reads=sample_metrics.uniquely_mapped_percent,
         )
 
