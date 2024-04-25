528da05bfae10021f968f05cada311c36ef3cd69
Samuel Lampa <samuel.lampa@scilifelab.se>
2024-04-25 13:32:58 +0200

    Add add jasen analysis option alembic script

---
+++ alembic/versions/2024_04_25_0fda0f2746d6_add_jasen_analysis_option.py
@@ -0,0 +1,24 @@
+"""Add Jasen analysis option
+
+Revision ID: 0fda0f2746d6
+Revises: ac5a804a9f47
+Create Date: 2024-04-25 12:17:39.250700
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '0fda0f2746d6'
+down_revision = 'ac5a804a9f47'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
