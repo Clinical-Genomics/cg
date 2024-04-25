1ade6ef935c6ff4a0f66fbceeecff238bbdbad5b
Samuel Lampa <samuel.lampa@scilifelab.se>
2024-04-25 16:06:51 +0200

    WIP: Create draft of alembic script for adding Jasen
    Requires #3179 to be fixed first

--- alembic/versions/2024_04_25_0fda0f2746d6_add_jasen_analysis_option.py
+++ alembic/versions/2024_04_25_0fda0f2746d6_add_jasen_analysis_option.py
@@ -7,18 +7,49 @@ Create Date: 2024-04-25 12:17:39.250700
 """
 from alembic import op
 import sqlalchemy as sa
+from sqlalchemy.dialects import mysql
 
 
 # revision identifiers, used by Alembic.
-revision = '0fda0f2746d6'
-down_revision = 'ac5a804a9f47'
+revision = "0fda0f2746d6"
+down_revision = "ac5a804a9f47"
 branch_labels = None
 depends_on = None
 
+old_options = (
+    "balsamic",
+    "balsamic-pon",
+    "balsamic-qc",
+    "balsamic-umi",
+    "demultiplex",
+    "fastq",
+    "fluffy",
+    "microsalt",
+    "mip-dna",
+    "mip-rna",
+    "mutant",
+    "raredisease",
+    "rnafusion",
+    "rsync",
+    "spring",
+    "taxprofiler",
+    "tomte",
+)
+new_options = sorted(old_options + ("jasen",))
+
+old_enum = mysql.ENUM(*old_options)
+new_enum = mysql.ENUM(*new_options)
+
 
 def upgrade():
-    pass
+    op.alter_column("analysis", "workflow", type_=new_enum)
+    op.alter_column("application_limitations", "workflow", type_=new_enum)
+    op.alter_column("case", "data_analysis", type_=new_enum)
+    op.alter_column("order", "workflow", type_=new_enum)
 
 
 def downgrade():
-    pass
+    op.alter_column("analysis", "workflow", type_=old_enum)
+    op.alter_column("application_limitations", "workflow", type_=old_enum)
+    op.alter_column("case", "data_analysis", type_=old_enum)
+    op.alter_column("order", "workflow", type_=old_enum)
