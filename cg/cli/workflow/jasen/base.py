d9abb3760e88f327a34dce3fca2a8887f5ae9980
Samuel Lampa <samuel.lampa@scilifelab.se>
2024-04-29 15:26:14 +0200

    Start fleshing out config-case command

--- cg/cli/workflow/jasen/base.py
+++ cg/cli/workflow/jasen/base.py
@@ -4,6 +4,9 @@ import logging
 
 import click
 
+from cg.cli.workflow.nf_analysis import (
+    config_case,
+)
 from cg.constants.constants import MetaApis
 from cg.meta.workflow.analysis import AnalysisAPI
 from cg.meta.workflow.jasen import JasenAnalysisAPI
@@ -17,3 +20,5 @@ def jasen(context: click.Context) -> None:
     """GMS/Jasen analysis workflow."""
     AnalysisAPI.get_help(context)
     context.obj.meta_apis[MetaApis.ANALYSIS_API] = JasenAnalysisAPI(config=context.obj)
+
+jasen.add_command(config_case)
