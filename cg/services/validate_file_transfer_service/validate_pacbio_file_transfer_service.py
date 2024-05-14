2057b3112c31c57bb622c7ad74c0b229ead4383c
Christian Oertlin <c.oertlin@gmail.com>
2024-05-14 13:59:21 +0200

    add file writer

--- cg/services/validate_file_transfer_service/validate_pacbio_file_transfer_service.py
+++ cg/services/validate_file_transfer_service/validate_pacbio_file_transfer_service.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import logging
 from cg.constants.constants import FileFormat
+from cg.io.controller import ReadFile, WriteFile
 from cg.models.cg_config import CGConfig
 from cg.services.validate_file_transfer_service.validate_file_transfer_service import (
     ValidateFileTransferService,
@@ -31,23 +32,21 @@ class ValidatePacbioFileTransferService(ValidateFileTransferService):
     def get_manifest_file_path(flow_cell_path: Path) -> Path:
         return get_file_in_directory_with_pattern(directory=flow_cell_path, pattern="transferdone")
 
-    def validate_transfer_done(self, run_id: str) -> bool:
-        flow_cell_paths: list[Path] = self.get_flow_cell_paths(run_id)
-        for flow_cell_path in flow_cell_paths:
-            try:
-                manifest_file: Path = self.get_manifest_file_path(flow_cell_path)
-            except FileNotFoundError:
-                LOG.error(
-                    f"Manifest file not found for run {run_id} and flow cell {flow_cell_path.name}"
-                )
-                return False
-            if not self.validate_by_manifest_file(
-                manifest_file=manifest_file,
-                source_dir=flow_cell_path,
-                manifest_file_format=FileFormat.TXT,
-            ):
-                LOG.error(f"Transfer not done for run {run_id} and flow cell {flow_cell_path.name}")
-                return False
+    def validate_transfer_done(self, run_id: str, flow_cell_path: Path) -> bool:
+        try:
+            manifest_file: Path = self.get_manifest_file_path(flow_cell_path)
+        except FileNotFoundError:
+            LOG.error(
+                f"Manifest file not found for run {run_id} and flow cell {flow_cell_path.name}"
+            )
+            return False
+        if not self.validate_by_manifest_file(
+            manifest_file=manifest_file,
+            source_dir=flow_cell_path,
+            manifest_file_format=FileFormat.TXT,
+        ):
+            LOG.error(f"Transfer not done for run {run_id} and flow cell {flow_cell_path.name}")
+            return False
         return True
 
     def get_run_ids(self) -> list[str]:
@@ -58,7 +57,22 @@ class ValidatePacbioFileTransferService(ValidateFileTransferService):
                 run_ids.append(run_dir.name)
         return run_ids
 
+    def write_flow_cell_path_to_trigger(self, flow_cell_path: Path):
+        reader = ReadFile()
+        trigger_file: str = self.config.pacbio_data_directory + "validated_flow_cell_runs"
+        content: any = reader.get_content_from_file(
+            file_path=Path(trigger_file), file_format=FileFormat.TXT
+        )
+        content.append(flow_cell_path.name)
+        writer = WriteFile()
+        writer.write_file_from_content(
+            file_path=Path(trigger_file), file_format=FileFormat.TXT, content=content
+        )
+
     def validate_all_transfer_done(self) -> bool:
         run_ids: list[str] = self.get_run_ids()
         for run_id in run_ids:
-            return self.validate_transfer_done(run_id)
+            for flow_cell_path in self.get_flow_cell_paths(run_id):
+                if self.validate_transfer_done(run_id=run_id, flow_cell_path=flow_cell_path):
+                    LOG.info(f"Transfer done for run {run_id} and flow cell {flow_cell_path.name}")
+                    self.write_flow_cell_path_to_trigger(flow_cell_path)
