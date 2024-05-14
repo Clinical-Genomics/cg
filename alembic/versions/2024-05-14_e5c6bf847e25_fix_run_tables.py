8da7bb97405e0cd331220e560b417e79208adacf
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-05-14 15:38:03 +0200

    updated migration

--- alembic/versions/2024-05-14_e5c6bf847e25_fix_run_tables.py
+++ alembic/versions/2024-05-14_e5c6bf847e25_fix_run_tables.py
@@ -1,8 +1,8 @@
-"""fix_new_tech_tables
+"""fix_run_tables
 
-Revision ID: 078884a6b2d2
-Revises: 9b188aee9577
-Create Date: 2024-05-13 11:36:49.628610
+Revision ID: e5c6bf847e25
+Revises: 4766684153e3
+Create Date: 2024-05-14 15:34:50.944774
 
 """
 
@@ -11,17 +11,13 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-revision = "078884a6b2d2"
-down_revision = "9b188aee9577"
+revision = "e5c6bf847e25"
+down_revision = "4766684153e3"
 branch_labels = None
 depends_on = None
 
 
 def upgrade():
-    op.create_primary_key(constraint_name="pk_run_device", table_name="run_device", columns=["id"])
-    op.create_primary_key(
-        constraint_name="pk_run_metrics", table_name="run_metrics", columns=["id"]
-    )
     op.drop_column(table_name="run_device", column_name="device_id")
     op.add_column(
         table_name="run_metrics",
@@ -41,4 +37,3 @@ def downgrade():
         table_name="run_device",
         column=sa.Column("device_id", sa.Integer(), nullable=False),
     )
-    op.drop_constraint(constraint_name="pk_run_device", table_name="run_device", type_="primary")
