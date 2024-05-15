7c5ff3d027ce696f1c6442e2758b70043acf842e
beatrizsavinhas <beatriz.sa.vinhas@scilifelab.se>
2024-05-15 14:31:58 +0200

    Add parser for internal_negative_control.

--- cg/meta/workflow/mutant/metadata_parser/metadata_parser.py
+++ cg/meta/workflow/mutant/metadata_parser/metadata_parser.py
@@ -6,7 +6,7 @@ from cg.meta.workflow.mutant.metadata_parser.utils import (
     is_sample_external_negative_control,
 )
 from cg.models.cg_config import CGConfig
-from cg.store.api.core import Store
+from cg.store.store import Store
 from cg.store.models import Case, Sample
 
 
@@ -17,7 +17,9 @@ class MetadataParser:
 
     def parse_metadata(self, case_internal_id: str) -> SamplesMetadata:
         metadata_for_case = self.parse_metadata_for_case(case_internal_id)
-        metadata_for_internal_negative_control = self.parse_metadata_for_internal_negative_control(metadata_for_case)
+        metadata_for_internal_negative_control = self.parse_metadata_for_internal_negative_control(
+            metadata_for_case
+        )
 
         metadata = metadata_for_case | metadata_for_internal_negative_control
 
@@ -44,33 +46,53 @@ class MetadataParser:
 
         return metadata_for_case
 
-    def parse_metadata_for_internal_negative_control(self, metadata_for_case: SamplesMetadata) -> dict[str, SampleMetadata]:
-        sample_internal_id = metadata_for_case.keys()[0] 
+    def parse_metadata_for_internal_negative_control(
+        self, metadata_for_case: SamplesMetadata
+    ) -> dict[str, SampleMetadata]:
+        sample_internal_id = metadata_for_case.keys()[0]
 
-        internal_negative_control_id = get_internal_negative_control_id_from_lims(self, sample_internal_id)
+        internal_negative_control_id = self.get_internal_negative_control_id_from_lims(
+            self, sample_internal_id
+        )
 
-        #return dict[internal_negative_control_id, get_internal_negative_control_metadata]
+        internal_negative_control: Sample = self.status_db.get_sample_by_internal_id(
+            internal_negative_control_id
+        )
 
-        pass
+        if not internal_negative_control:
+            return None  # TODO: How should this exception be handled.
+        else:
+            internal_negative_control_metadata = SampleMetadata(
+                sample_internal_id=internal_negative_control.internal_id,
+                sample_name=internal_negative_control.name,
+                is_external_negative_control=False,
+                is_internal_negative_control=True,
+                reads=internal_negative_control.reads,
+                target_reads=internal_negative_control.application_version.application.target_reads,
+                percent_reads_guaranteed=internal_negative_control.application_version.application.percent_reads_guaranteed,
+            )
 
-def get_negative_controls_from_list(samples: list[Sample]) -> list[Sample]:
-        """Filter and return a list of internal negative controls from a given sample list."""
-        negative_controls = []
-        for sample in samples:
-            if sample.udf.get("Control") == "negative" and sample.udf.get("customer") == "cust000":
-                negative_controls.append(sample)
-        return negative_controls
+            return dict[internal_negative_control_id, internal_negative_control_metadata]
+
+    # Do we ever run the risk of having several controls in one covid pool artifact?
+    # def get_negative_controls_from_list(samples: list[Sample]) -> list[Sample]:
+    #     """Filter and return a list of internal negative controls from a given sample list."""
+    #     negative_controls = []
+    #     for sample in samples:
+    #         if sample.udf.get("Control") == "negative" and sample.udf.get("customer") == "cust000":
+    #             negative_controls.append(sample)
+    #     return negative_controls
 
     def get_internal_negative_control_id_from_lims(self, sample_internal_id) -> str:
-
-    # LimsAPI
-        artifact = self.lims.get_latest_artifact_for_sample(LimsProcess.COVID_POOLING_STEP, LimsArtifactTypes.ANALYTE, sample_internal_id)
+        artifact = self.lims.get_latest_artifact_for_sample(
+            LimsProcess.COVID_POOLING_STEP, LimsArtifactTypes.ANALYTE, sample_internal_id
+        )
         if not artifact:
             return None
-        negative_controls = get_negative_controls_from_list(samples=artifact[0].samples)
-        if not negative_controls:
-            return None
-        return negative_controls[0].id
 
+        samples = artifact[0].samples
 
-    
+        for sample in samples:
+            if sample.udf.get("Control") == "negative" and sample.udf.get("customer") == "cust000":
+                internal_negative_control = sample
+                return internal_negative_control.id
