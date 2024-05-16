39e7bc5fb0fd3a60f769cc19dd7490966a2f516a
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2024-05-16 12:22:58 +0200

    feat(status): Use phenotype

--- cg/apps/mip/confighandler.py
+++ cg/apps/mip/confighandler.py
@@ -4,7 +4,7 @@ from copy import deepcopy
 from marshmallow import Schema, fields, validate
 
 from cg.constants import DEFAULT_CAPTURE_KIT
-from cg.constants.subject import RelationshipStatus
+from cg.constants.subject import PhenotypeStatus, RelationshipStatus
 from cg.exc import PedigreeConfigError
 
 LOG = logging.getLogger(__name__)
@@ -28,7 +28,9 @@ class SampleSchema(Schema):
     mother = fields.Str(dump_default=RelationshipStatus.HAS_NO_PARENT)
     phenotype = fields.Str(
         required=True,
-        validate=validate.OneOf(choices=["affected", "unaffected", "unknown"]),
+        validate=validate.OneOf(
+            choices=[PhenotypeStatus.AFFECTED, PhenotypeStatus.UNAFFECTED, PhenotypeStatus.UNKNOWN]
+        ),
     )
     sex = fields.Str(required=True, validate=validate.OneOf(choices=["female", "male", "unknown"]))
     expected_coverage = fields.Float()
