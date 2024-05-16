f61006b972b02ed1c32daebbf7c270d56ef91f72
islean <isak.ohlsson.angnell@gmail.com>
2024-05-16 14:56:58 +0200

    Docstring

--- cg/cli/utils.py
+++ cg/cli/utils.py
@@ -9,5 +9,6 @@ def echo_lines(lines: list[str]) -> None:
 
 
 def is_case_name_allowed(name: str) -> bool:
+    """Returns true if the given name consists only of letters, numbers and dashes."""
     allowed_pattern: re.Pattern = re.compile("^[A-Za-z0-9-]+$")
     return bool(allowed_pattern.fullmatch(name))
