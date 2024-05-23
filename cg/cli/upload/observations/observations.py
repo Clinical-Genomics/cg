cfc10b50e7ef4dae5a896fc48f1be469b2732542
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-05-23 17:30:30 +0200

    black

--- cg/cli/upload/observations/observations.py
+++ cg/cli/upload/observations/observations.py
@@ -28,9 +28,9 @@ def upload_observations_to_loqusdb(context: CGConfig, case_id: str | None, dry_r
     """Upload observations from an analysis to Loqusdb."""
     click.echo(click.style("----------------- OBSERVATIONS -----------------"))
     try:
-        observations_api: RarediseaseObservationsAPI |MipDNAObservationsAPI | BalsamicObservationsAPI = get_observations_api(
-            context=context, case_id=case_id, upload=True
-        )
+        observations_api: (
+            RarediseaseObservationsAPI | MipDNAObservationsAPI | BalsamicObservationsAPI
+        ) = get_observations_api(context=context, case_id=case_id, upload=True)
         if dry_run:
             LOG.info(f"Dry run. Would upload observations for {case_id}.")
             return
