13f4825809355380b73d1279266c9e862d307827
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-05-23 17:28:59 +0200

    update rd observation APO

--- cg/meta/observations/raredisease_observations_api.py
+++ cg/meta/observations/raredisease_observations_api.py
@@ -5,11 +5,12 @@ import logging
 from housekeeper.store.models import File, Version
 
 from cg.apps.loqus import LoqusdbAPI
+from cg.constants.constants import CustomerId, SampleType
 from cg.constants.observations import (
     LOQUSDB_ID,
-    LOQUSDB_RAREDISEASE_SEQUENCING_METHODS,
+    LOQUSDB_RARE_DISEASE_SEQUENCING_METHODS,
     LoqusdbInstance,
-    LoqusdbRarediseaseCustomers,
+    LOQUSDB_RARE_DISEASE_CUSTOMERS,
     RarediseaseLoadParameters,
     RarediseaseObservationsAnalysisTag,
 )
@@ -29,33 +30,60 @@ LOG = logging.getLogger(__name__)
 
 
 class RarediseaseObservationsAPI(ObservationsAPI):
-    """API to manage MIP-DNA observations."""
+    """API to manage RAREDISEASE observations."""
 
     def __init__(self, config: CGConfig, sequencing_method: SequencingMethod):
         super().__init__(config)
         self.sequencing_method: SequencingMethod = sequencing_method
         self.loqusdb_api: LoqusdbAPI = self.get_loqusdb_api(self.get_loqusdb_instance())
 
-    def get_loqusdb_instance(self) -> LoqusdbInstance:
-        """Return the Loqusdb instance associated to the sequencing method."""
-        if self.sequencing_method not in LOQUSDB_RAREDISEASE_SEQUENCING_METHODS:
-            LOG.error(
-                f"Sequencing method {self.sequencing_method} is not supported by Loqusdb. Cancelling upload."
-            )
-            raise LoqusdbUploadCaseError
+    @property
+    def loqusdb_customers(self) -> list[CustomerId]:
+        """Customers that are eligible for rare disease Loqusdb uploads."""
+        return LOQUSDB_RARE_DISEASE_CUSTOMERS
 
+    @property
+    def loqusdb_sequencing_methods(self) -> list[str]:
+        """Sequencing methods that are eligible for cancer Loqusdb uploads."""
+        return LOQUSDB_RARE_DISEASE_SEQUENCING_METHODS
+
+    @staticmethod
+    def is_sample_type_eligible_for_observations_upload(case: Case) -> bool:
+        """Return whether a rare disease case is free of tumor samples."""
+        if case.tumour_samples:
+            LOG.error(f"Sample type {SampleType.TUMOR} is not supported for Loqusdb uploads")
+            return False
+        return True
+
+    def is_case_eligible_for_observations_upload(self, case: Case) -> bool:
+        """Return whether a rare disease case is eligible for observations upload."""
+        return all(
+            [
+                self.is_customer_eligible_for_observations_upload(case.customer.internal_id),
+                self.is_sequencing_method_eligible_for_observations_upload(case.internal_id),
+                self.is_sample_type_eligible_for_observations_upload(case),
+                self.is_sample_source_eligible_for_observations_upload(case.internal_id),
+            ]
+        )
+
+    def set_loqusdb_instance(self, case_id: str) -> None:
+        """Return the Loqusdb instance associated to the sequencing method."""
+        sequencing_method: SequencingMethod = self.analysis_api.get_data_analysis_type(case_id)
         loqusdb_instances: dict[SequencingMethod, LoqusdbInstance] = {
             SequencingMethod.WGS: LoqusdbInstance.WGS,
             SequencingMethod.WES: LoqusdbInstance.WES,
         }
-        return loqusdb_instances[self.sequencing_method]
+        self.loqusdb_api = self.get_loqusdb_api(loqusdb_instances[sequencing_method])
 
-    def load_observations(self, case: Case, input_files: RarediseaseObservationsInputFiles) -> None:
-        """Load observation counts to Loqusdb for a raredisease case."""
-        if case.tumour_samples:
-            LOG.error(f"Case {case.internal_id} has tumour samples. Cancelling upload.")
-            raise LoqusdbUploadCaseError
+    def load_observations(self, case: Case) -> None:
+        """
+        Load observation counts to Loqusdb for a MIP-DNA case.
 
+        Raises:
+            LoqusdbDuplicateRecordError: If case has already been uploaded.
+        """
+        self.set_loqusdb_instance(case.internal_id)
+        input_files: RarediseaseObservationsInputFiles = self.get_observations_input_files(case)
         if self.is_duplicate(
             case=case,
             loqusdb_api=self.loqusdb_api,
@@ -81,10 +109,10 @@ class RarediseaseObservationsAPI(ObservationsAPI):
         self.update_statusdb_loqusdb_id(samples=case.samples, loqusdb_id=loqusdb_id)
         LOG.info(f"Uploaded {load_output['variants']} variants to {repr(self.loqusdb_api)}")
 
-    def extract_observations_files_from_hk(
-        self, hk_version: Version
+    def get_observations_files_from_hk(
+        self, hk_version: Version, case_id: str
     ) -> RarediseaseObservationsInputFiles:
-        """Extract observations files given a housekeeper version for rare diseases."""
+        """Return observations files given a Housekeeper version for rare diseases."""
         input_files: dict[str, File] = {
             "snv_vcf_path": self.housekeeper_api.files(
                 version=hk_version.id, tags=[RarediseaseObservationsAnalysisTag.SNV_VCF]
@@ -93,7 +121,7 @@ class RarediseaseObservationsAPI(ObservationsAPI):
                 self.housekeeper_api.files(
                     version=hk_version.id, tags=[RarediseaseObservationsAnalysisTag.SV_VCF]
                 ).first()
-                if self.sequencing_method == SequencingMethod.WGS
+                if self.analysis_api.get_data_analysis_type(case_id) == SequencingMethod.WGS
                 else None
             ),
             "profile_vcf_path": self.housekeeper_api.files(
@@ -105,18 +133,13 @@ class RarediseaseObservationsAPI(ObservationsAPI):
         }
         return RarediseaseObservationsInputFiles(**get_full_path_dictionary(input_files))
 
-    def delete_case(self, case: Case) -> None:
-        """Delete raredisease case observations from Loqusdb."""
-        if not self.loqusdb_api.get_case(case.internal_id):
-            LOG.error(
-                f"Case {case.internal_id} could not be found in Loqusdb. Skipping case deletion."
-            )
+    def delete_case(self, case_id: str) -> None:
+        """Delete rare disease case observations from Loqusdb."""
+        case: Case = self.store.get_case_by_internal_id(internal_id=case_id)
+        self.set_loqusdb_instance(case_id)
+        if not self.loqusdb_api.get_case(case_id):
+            LOG.error(f"Case {case_id} could not be found in Loqusdb. Skipping case deletion.")
             raise CaseNotFoundError
-
-        self.loqusdb_api.delete_case(case.internal_id)
+        self.loqusdb_api.delete_case(case_id)
         self.update_statusdb_loqusdb_id(samples=case.samples, loqusdb_id=None)
-        LOG.info(f"Removed observations for case {case.internal_id} from {repr(self.loqusdb_api)}")
-
-    def get_loqusdb_customers(self) -> LoqusdbRarediseaseCustomers:
-        """Returns the customers that are entitled to Rare Disease Loqusdb uploads."""
-        return LoqusdbRarediseaseCustomers
+        LOG.info(f"Removed observations for case {case_id} from {repr(self.loqusdb_api)}")
