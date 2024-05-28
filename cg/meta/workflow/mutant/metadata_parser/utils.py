f4af7d3710b0f7ade2bdda15b550ac7908d53cb9
beatrizsavinhas <beatriz.sa.vinhas@scilifelab.se>
2024-05-28 15:47:39 +0200

    Add log warning when several internal controls are found.

--- cg/meta/workflow/mutant/metadata_parser/utils.py
+++ cg/meta/workflow/mutant/metadata_parser/utils.py
@@ -3,35 +3,38 @@ from cg.meta.workflow.mutant.metadata_parser.models import SampleMetadata
 from cg.store.models import Sample
 from cg.apps.lims.api import LimsAPI
 from cg.constants.lims import LimsArtifactTypes, LimsProcess
+from genologics.entities import Artifact, Sample
+from cg.models.cg_config import LOG
 
 
 def is_sample_external_negative_control(sample: Sample) -> bool:
     return sample.control == ControlOptions.NEGATIVE
 
 
-# TODO: Do we ever run the risk of having several controls in one covid pool artifact?
-# def get_negative_controls_from_list(samples: list[Sample]) -> list[Sample]:
-#     """Filter and return a list of internal negative controls from a given sample list."""
-#     negative_controls = []
-#     for sample in samples:
-#         if sample.udf.get("Control") == "negative" and sample.udf.get("customer") == "cust000":
-#             negative_controls.append(sample)
-#     return negative_controls
+def get_negative_controls_from_list(samples: list[Sample]) -> list[Sample]:
+    """Filter and return a list of internal negative controls from a given sample list."""
+    negative_controls = []
+    for sample in samples:
+        if sample.udf.get("Control") == "negative" and sample.udf.get("customer") == "cust000":
+            negative_controls.append(sample)
+    return negative_controls
 
 
 def get_internal_negative_control_id_from_lims(lims: LimsAPI, sample_internal_id: str) -> str:
-    artifact = lims.get_latest_artifact_for_sample(
+    artifact: Artifact = lims.get_latest_artifact_for_sample(
         LimsProcess.COVID_POOLING_STEP, LimsArtifactTypes.ANALYTE, sample_internal_id
     )
     if not artifact:
         return None
-
     samples = artifact[0].samples
 
-    for sample in samples:
-        if sample.udf.get("Control") == "negative" and sample.udf.get("customer") == "cust000":
-            internal_negative_control = sample
-            return internal_negative_control.id
+    negative_controls: list = get_negative_controls_from_list(samples=samples)
+
+    if len(negative_controls) > 1:
+        sample_ids = [sample.id for sample in negative_controls]
+        LOG.warning(f"Several internal negative control samples found: {' '.join(sample_ids)}")
+    else:
+        return negative_controls[0].id
 
 
 def get_internal_negative_control_id(lims: LimsAPI, metadata_for_case: SampleMetadata) -> str:
