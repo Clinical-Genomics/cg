48a7193f8aa997ff0a8ec633ebf7b7ad416f0ff8
eliott <eliottob6@gmail.com>
2024-05-29 16:11:44 +0200

    Import shutil and def click_context_setting_max_content_width

--- cg/cli/utils.py
+++ cg/cli/utils.py
@@ -2,6 +2,8 @@ import re
 
 import click
 
+import shutil
+
 
 def echo_lines(lines: list[str]) -> None:
     for line in lines:
@@ -12,3 +14,7 @@ def is_case_name_allowed(name: str) -> bool:
     """Returns true if the given name consists only of letters, numbers and dashes."""
     allowed_pattern: re.Pattern = re.compile("^[A-Za-z0-9-]+$")
     return bool(allowed_pattern.fullmatch(name))
+
+
+def click_context_setting_max_content_width():
+    return {"max_content_width": shutil.get_terminal_size().columns - 10}
