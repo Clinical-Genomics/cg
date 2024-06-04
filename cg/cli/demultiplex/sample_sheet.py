df9a4b1bc07d0c2e60bd16933c607fcd5a277975
eliott <eliottob6@gmail.com>
2024-06-04 10:12:32 +0200

    Add import and context, demultiplex.sample_sheet.py

--- cg/cli/demultiplex/sample_sheet.py
+++ cg/cli/demultiplex/sample_sheet.py
@@ -6,13 +6,14 @@ from pydantic import ValidationError
 
 from cg.apps.demultiplex.sample_sheet.api import SampleSheetAPI
 from cg.constants.constants import DRY_RUN
+from cg.cli.utils import click_context_setting_max_content_width
 from cg.exc import SampleSheetError
 from cg.models.cg_config import CGConfig
 
 LOG = logging.getLogger(__name__)
 
 
-@click.group(name="samplesheet")
+@click.group(name="samplesheet", context_settings=click_context_setting_max_content_width())
 def sample_sheet_commands():
     """Command group for the sample sheet commands."""
 
