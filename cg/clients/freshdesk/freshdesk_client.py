da180ac2966a990835c10c8a9c37f81639171aa2
Vincent Janvid <vincent.janvid@scilifelab.se>
2024-06-10 15:48:31 +0200

    Self-review

--- cg/clients/freshdesk/freshdesk_client.py
+++ cg/clients/freshdesk/freshdesk_client.py
@@ -1,8 +1,7 @@
 import logging
 
-import requests
 from pydantic import ValidationError
-from requests import RequestException
+from requests import RequestException, Response, Session
 from requests.adapters import HTTPAdapter
 from urllib3 import Retry
 
@@ -24,17 +23,17 @@ class FreshdeskClient:
         self.api_key = api_key
         self.session = self._configure_session()
 
-    def __enter__(self):
+    def __enter__(self) -> "FreshdeskClient":
         return self
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, exc_type, exc_value, traceback) -> None:
         self.session.close()
 
     def create_ticket(self, ticket: TicketCreate) -> TicketResponse:
         """Create a ticket."""
         LOG.debug(ticket.model_dump_json())
         try:
-            response = self.session.post(
+            response: Response = self.session.post(
                 url=f"{self.base_url}{EndPoints.TICKETS}",
                 json=ticket.model_dump(exclude_none=True),
             )
@@ -48,15 +47,17 @@ class FreshdeskClient:
             raise FreshdeskModelException(error) from error
         return ticket_response
 
-    def _configure_session(self):
-        session = requests.Session()
+    def _configure_session(self) -> Session:
+        """Configures and sets a session to be used for requests."""
+        session = Session()
         self._configure_retries(session)
         session.auth = (self.api_key, "X")
         session.headers.update({"Content-Type": "application/json"})
         return session
 
     @staticmethod
-    def _configure_retries(session: requests.Session) -> None:
+    def _configure_retries(session: Session) -> None:
+        """Configures retries for the session."""
         retry_strategy = Retry(
             total=5,
             status_forcelist=[TO_MANY_REQUESTS_CODE],
@@ -65,4 +66,3 @@ class FreshdeskClient:
         )
         adapter = HTTPAdapter(max_retries=retry_strategy)
         session.mount("https://", adapter)
-        session.mount("http://", adapter)
