1c08dec5ee96c045d8445f8ba6558b9fc60447d1
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-06-12 19:28:38 +0200

    Make test for illumina flow cell filter use correct store

--- tests/store/filters/test_status_illumina_flow_cell_filters.py
+++ tests/store/filters/test_status_illumina_flow_cell_filters.py
@@ -12,18 +12,18 @@ from cg.store.store import Store
 
 def test_filter_illumina_flow_cell_by_internal_id(
     illumina_flow_cell_dto: IlluminaFlowCellDTO,
-    illumina_flow_cell_internal_id: str,
-    store: Store,
+    novaseq_x_flow_cell_id: str,
+    store_with_illumina_sequencing_data: Store,
 ):
-    # GIVEN an Illumina flow cell in store
-    store.add_illumina_flow_cell(illumina_flow_cell_dto)
+    # GIVEN a store with the canonical Illumina flow cells
+    flow_cells: Query = store_with_illumina_sequencing_data._get_query(table=IlluminaFlowCell)
+    assert flow_cells.count() == 7
 
     # WHEN filtering an Illumina flow cell by internal id
-    flow_cells: Query = store._get_query(table=IlluminaFlowCell)
     flow_cell: Query = filter_illumina_flow_cell_by_internal_id(
-        flow_cells=flow_cells, internal_id=illumina_flow_cell_internal_id
+        flow_cells=flow_cells, internal_id=novaseq_x_flow_cell_id
     )
 
     # THEN a query with the correct flow cell should be returned
-    assert isinstance(flow_cell, Query)
-    assert flow_cell.first().internal_id == illumina_flow_cell_internal_id
+    assert flow_cell.count() == 1
+    assert flow_cell.first().internal_id == novaseq_x_flow_cell_id
