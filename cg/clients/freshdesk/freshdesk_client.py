b7433f856af7f30553a2c5047dec4fad93b6d619
Vincent Janvid <vincent.janvid@scilifelab.se>
2024-06-14 13:39:10 +0200

    Address review commments

--- cg/clients/freshdesk/freshdesk_client.py
+++ cg/clients/freshdesk/freshdesk_client.py
@@ -22,27 +22,24 @@ class FreshdeskClient:
     def __init__(self, base_url: str, api_key: str):
         self.base_url = base_url
         self.api_key = api_key
+        self.session = self._get_session()
 
     def create_ticket(self, ticket: TicketCreate) -> TicketResponse:
         """Create a ticket."""
         LOG.debug(ticket.model_dump_json())
-        session: Session = self._get_session()
         try:
-            response: Response = session.post(
+            response: Response = self.session.post(
                 url=f"{self.base_url}{EndPoints.TICKETS}",
                 json=ticket.model_dump(exclude_none=True),
             )
             response.raise_for_status()
-            ticket_response: TicketResponse = TicketResponse.model_validate(response.json())
-            return ticket_response
+            return TicketResponse.model_validate(response.json())
         except RequestException as error:
             LOG.error(f"Could not create ticket: {error}")
             raise FreshdeskAPIException(error) from error
         except ValidationError as error:
             LOG.error(f"Response from Freshdesk does not fit model: {TicketResponse}.\n{error}")
             raise FreshdeskModelException(error) from error
-        finally:
-            session.close()
 
     def _get_session(self) -> Session:
         """Configures and sets a session to be used for requests."""
@@ -57,8 +54,13 @@ class FreshdeskClient:
         """Configures retries for the session."""
         retry_strategy = Retry(
             total=5,
-            status_forcelist=[HTTPStatus.TOO_MANY_REQUESTS],
-            allowed_methods=None,
+            status_forcelist=[
+                HTTPStatus.TOO_MANY_REQUESTS,
+                HTTPStatus.INTERNAL_SERVER_ERROR,
+                HTTPStatus.BAD_GATEWAY,
+                HTTPStatus.SERVICE_UNAVAILABLE,
+                HTTPStatus.GATEWAY_TIMEOUT,
+            ],
             backoff_factor=2,
         )
         adapter = HTTPAdapter(max_retries=retry_strategy)
