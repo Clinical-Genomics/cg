76ebf485de30c540c9b38d6aca2d75b7507d09cb
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-06-24 12:10:06 +0200

    output more

--- cg/meta/upload/gt.py
+++ cg/meta/upload/gt.py
@@ -155,6 +155,7 @@ class UploadGenotypesAPI(object):
         qcmetrics_raw: dict = ReadFile.get_content_from_file(
             file_format=FileFormat.YAML, file_path=qc_metrics
         )
+        LOG.info(qcmetrics_raw)
         return [MetricsBase(**metric) for metric in qcmetrics_raw]
 
     def upload(self, data: dict, replace: bool = False):
