05cafd5e78dc49fbf095107e084e7186815cf571
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2024-06-25 08:19:34 +0200

    feat(rm_pandas): WIP

--- tests/models/fohm/test_reports.py
+++ tests/models/fohm/test_reports.py
@@ -1,13 +1,13 @@
 from cg.models.fohm.reports import FohmComplementaryReport
 
 
-def test_instantiate_fohm_report(fohm_reports_raw: dict[str, str]):
+def test_instantiate_fohm_report(fohm_complementary_reports_raw: dict[str, str]):
     """Tests report dict against a pydantic FohmComplementaryReport."""
 
     # GIVEN report dicts
 
     # WHEN instantiating a FOHM report object
-    fohm_report = FohmComplementaryReport.model_validate(fohm_reports_raw)
+    fohm_report = FohmComplementaryReport.model_validate(fohm_complementary_reports_raw)
 
     # THEN assert that it was successfully created
     assert isinstance(fohm_report, FohmComplementaryReport)
