6c3f66569a37f0f05c93b6f5065c71868ea6fcc5
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-06-26 08:55:45 +0200

    Update cg/meta/upload/gt.py
    Co-authored-by: Vadym <vadym.ivanchuk@scilifelab.se>

--- cg/meta/upload/gt.py
+++ cg/meta/upload/gt.py
@@ -150,7 +150,7 @@ class UploadGenotypesAPI(object):
 
     @staticmethod
     def get_parsed_qc_metrics_data_raredisease(qc_metrics: Path) -> MetricsBase:
-        """Parse the information from a qc metrics file"""
+        """Parse the information from a QC metrics file."""
         qcmetrics_raw: dict = ReadFile.get_content_from_file(
             file_format=FileFormat.YAML, file_path=qc_metrics
         )
