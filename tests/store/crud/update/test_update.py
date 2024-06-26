0890a51c36a7b375cafe544cda3b7412d562066a
Christian Oertlin <c.oertlin@gmail.com>
2024-06-26 13:09:25 +0200

    add store filters

--- tests/store/crud/update/test_update.py
+++ tests/store/crud/update/test_update.py
@@ -1,3 +1,38 @@
-from cg.store.models import Flowcell
+from cg.constants import FlowCellStatus
+from cg.store.models import Flowcell, IlluminaSequencingRun
 from cg.store.store import Store
 from tests.store_helpers import StoreHelpers
+
+
+def test_update_illumina_sequencing_run_availability(store_with_illumina_sequencing_data: Store):
+    # GIVEN a store with Illumina Sequencing Runs that have data availability ON_DISK
+    sequencing_run: IlluminaSequencingRun = (
+        store_with_illumina_sequencing_data.get_illumina_sequencing_runs_by_availability(
+            [FlowCellStatus.ON_DISK]
+        )[0]
+    )
+    assert sequencing_run.data_availability == FlowCellStatus.ON_DISK
+
+    # WHEN updating the data availability of a sequencing run
+    store_with_illumina_sequencing_data.update_illumina_sequencing_run_availability(
+        sequencing_run=sequencing_run, data_availability=FlowCellStatus.REQUESTED
+    )
+
+    # THEN the data availability of the sequencing run is updated
+    assert sequencing_run.data_availability == FlowCellStatus.REQUESTED
+
+
+def test_update_illumina_sequencing_run_has_backup(store_with_illumina_sequencing_data: Store):
+    # GIVEN a store with Illumina Sequencing Runs that have backup status False
+    sequencing_run: IlluminaSequencingRun = store_with_illumina_sequencing_data._get_query(
+        IlluminaSequencingRun
+    ).all()[0]
+    assert sequencing_run.has_backup is False
+
+    # WHEN updating the backup status of a sequencing run
+    store_with_illumina_sequencing_data.update_illumina_sequencing_run_has_backup(
+        sequencing_run=sequencing_run, has_backup=True
+    )
+
+    # THEN the backup status of the sequencing run is updated
+    assert sequencing_run.has_backup is True
