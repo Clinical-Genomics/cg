e8b0fe0afbb0440dc2b300a257949e6d98273a33
islean <isak.ohlsson.angnell@gmail.com>
2024-06-26 10:04:00 +0200

    Same message for multiple cases

--- cg/services/delivery_message/messages/analysis_message.py
+++ cg/services/delivery_message/messages/analysis_message.py
@@ -3,15 +3,6 @@ from cg.services.delivery_message.messages.utils import get_caesar_delivery_path
 from cg.store.models import Case
 
 
-def get_case_message(case: Case) -> str:
-    delivery_path: str = get_caesar_delivery_path(case)
-    return (
-        f"Hello,\n\n"
-        f"The analysis files are currently being uploaded to your inbox on Caesar:\n\n"
-        f"{delivery_path}"
-    )
-
-
 def get_cases_message(cases: list[Case]) -> str:
     delivery_path: str = get_caesar_delivery_path(cases[0])
     return (
@@ -23,6 +14,4 @@ def get_cases_message(cases: list[Case]) -> str:
 
 class AnalysisMessage(DeliveryMessage):
     def create_message(self, cases: list[Case]) -> str:
-        if len(cases) == 1:
-            return get_case_message(cases[0])
         return get_cases_message(cases)
