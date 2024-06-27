518fc1bf8d2f66214d21f0ba65895c0a430df477
islean <isak.ohlsson.angnell@gmail.com>
2024-06-27 11:46:32 +0200

    Fix tests

--- tests/meta/orders/test_meta_orders_status.py
+++ tests/meta/orders/test_meta_orders_status.py
@@ -121,7 +121,7 @@ def test_sarscov2_samples_to_status(sarscov2_order_to_submit):
     data = SarsCov2Submitter.order_to_status(order=order)
 
     # THEN it should pick out samples and relevant information
-    assert len(data["samples"]) == 5
+    assert len(data["samples"]) == 6
     assert data["customer"] == "cust002"
     assert data["order"] == "Sars-CoV-2 samples"
     assert data["comment"] == "Order comment"
