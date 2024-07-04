e7a879c3a36363a5d2b2799c4c984147652cc3ab
seallard <sebastian.allard@scilifelab.se>
2024-07-04 13:54:02 +0200

    Name var

--- cg/cli/sequencing_qc/sequencing_qc.py
+++ cg/cli/sequencing_qc/sequencing_qc.py
@@ -10,5 +10,5 @@ LOG = logging.getLogger(__name__)
 @click.command(name="sequencing-qc", help="Perform sequencing QC")
 @click.pass_obj
 def sequencing_qc(context: CGConfig):
-    sequencing_qc_service: SequencingQCService = context.sequencing_qc_service
-    sequencing_qc_service.run_sequencing_qc()
+    service: SequencingQCService = context.sequencing_qc_service
+    service.run_sequencing_qc()
