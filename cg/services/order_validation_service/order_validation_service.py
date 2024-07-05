65fb4581691e83320552690cc4de175f46d977cf
seallard <sebastian.allard@scilifelab.se>
2024-07-05 16:47:35 +0200

    Pass tomte service to base validation service

--- cg/services/order_validation_service/order_validation_service.py
+++ cg/services/order_validation_service/order_validation_service.py
@@ -1,5 +1,8 @@
+from workflows.tomte.validation_service import TomteOrderValidationService
+
+
 class OrderValidationService:
     """Service to orchestrate the order validation."""
 
-    def __init__(self):
-        pass
+    def __init__(self, tomte_service: TomteOrderValidationService):
+        self.tomte_service = tomte_service
---
+++ cg/services/order_validation_service/workflows/tomte/validation_service.py
@@ -0,0 +1,5 @@
+
+class TomteOrderValidationService:
+
+    def __init__(self):
+        pass
