87209866102ab7119840678ffe84f3067cde5e9f
seallard <sebastian.allard@scilifelab.se>
2024-07-10 13:45:11 +0200

    Add base case model

---
+++ cg/services/order_validation_service/models/case.py
@@ -0,0 +1,11 @@
+from pydantic import BaseModel, Field
+from cg.constants.constants import DataDelivery
+from cg.constants.priority import PriorityTerms
+from cg.models.orders.sample_base import NAME_PATTERN
+
+
+class Case(BaseModel):
+    data_delivery: DataDelivery
+    internal_id: str | None = None
+    name: str = Field(pattern=NAME_PATTERN, min_length=2, max_length=128)
+    priority: PriorityTerms = PriorityTerms.STANDARD
--- cg/services/order_validation_service/workflows/tomte/models/case.py
+++ cg/services/order_validation_service/workflows/tomte/models/case.py
@@ -4,17 +4,14 @@ from cg.constants import DataDelivery, GenePanelMasterList
 from cg.constants.priority import PriorityTerms
 from cg.models.orders.orderform_schema import OrderCase
 from cg.models.orders.sample_base import NAME_PATTERN
+from cg.services.order_validation_service.models.case import Case
 from cg.services.order_validation_service.workflows.tomte.constants import TomteDeliveryType
 from cg.services.order_validation_service.workflows.tomte.models.sample import TomteSample
 
 
-class TomteCase(OrderCase):
+class TomteCase(Case):
     cohorts: list[str] | None = None
     data_delivery: TomteDeliveryType
     panels: list[GenePanelMasterList]
     synopsis: str | None = None
-    data_delivery: DataDelivery
-    internal_id: str | None = None
-    name: str = Field(pattern=NAME_PATTERN, min_length=2, max_length=128)
-    priority: PriorityTerms = PriorityTerms.STANDARD
     samples: list[TomteSample]
