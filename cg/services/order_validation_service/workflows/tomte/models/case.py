ef421fb9fddfae6c74e8da15a7dc0732b64c3977
seallard <sebastian.allard@scilifelab.se>
2024-07-10 13:47:08 +0200

    Fix imports

--- cg/services/order_validation_service/workflows/tomte/models/case.py
+++ cg/services/order_validation_service/workflows/tomte/models/case.py
@@ -1,9 +1,4 @@
-from pydantic import Field
-
-from cg.constants import DataDelivery, GenePanelMasterList
-from cg.constants.priority import PriorityTerms
-from cg.models.orders.orderform_schema import OrderCase
-from cg.models.orders.sample_base import NAME_PATTERN
+from cg.constants import GenePanelMasterList
 from cg.services.order_validation_service.models.case import Case
 from cg.services.order_validation_service.workflows.tomte.constants import TomteDeliveryType
 from cg.services.order_validation_service.workflows.tomte.models.sample import TomteSample
