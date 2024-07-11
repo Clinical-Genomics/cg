57d037feac9b357008232b7aa96886e319955a80
Christian Oertlin <c.oertlin@gmail.com>
2024-07-11 11:41:37 +0200

    add type

---
+++ alembic/versions/2024_07_11_0808675ad136_add_sample_fk.py
@@ -0,0 +1,33 @@
+"""add_sample_fk
+
+Revision ID: 0808675ad136
+Revises: 2a2c618967af
+Create Date: 2024-07-11 09:31:16.561316
+
+"""
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = "0808675ad136"
+down_revision = "2a2c618967af"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.create_foreign_key(
+        constraint_name="fk_sample_id",
+        source_table="sample_run_metrics",
+        referent_table="sample",
+        local_cols=["sample_id"],
+        remote_cols=["id"],
+    )
+
+
+def downgrade():
+    op.drop_constraint(
+        constraint_name="fk_sample_id", table_name="sample_run_metrics", type_="foreignkey"
+    )
