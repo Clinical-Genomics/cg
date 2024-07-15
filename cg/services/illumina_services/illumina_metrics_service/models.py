a3ecbcf8ce5f5a132d0c046e997227f55e00d151
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-07-15 09:15:47 +0200

    add validation to percent attributes of illumina dtos (#3425)(patch)
    ## Description
    Closes https://github.com/Clinical-Genomics/add-new-tech/issues/52
    
    ### Added
    
    - Field validators
    - Fixtures and tests
    
    ### Changed
    
    - Added validate upon assignment in DTO config

--- cg/services/illumina_services/illumina_metrics_service/models.py
+++ cg/services/illumina_services/illumina_metrics_service/models.py
@@ -6,6 +6,7 @@ from cg.constants import SequencingRunDataAvailability
 from cg.constants.devices import DeviceType
 from cg.constants.metrics import DemuxMetricsColumnNames, QualityMetricsColumnNames
 from cg.constants.sequencing import Sequencers
+from cg.utils.calculations import fraction_to_percent
 
 
 class SequencingQualityMetrics(BaseModel):
@@ -72,8 +73,15 @@ class IlluminaSequencingRunDTO(BaseModel):
     demultiplexing_started_at: datetime | None
     demultiplexing_completed_at: datetime | None
 
+    @field_validator("percent_undetermined_reads", "percent_q30", mode="before")
+    def validate_percent_fields(cls, value: float | None) -> float | None:
+        if value is not None:
+            return fraction_to_percent(value)
+        return value
+
     class Config:
         arbitrary_types_allowed = True
+        validate_assignment = True
 
 
 class IlluminaSampleSequencingMetricsDTO(BaseModel):
@@ -89,5 +97,12 @@ class IlluminaSampleSequencingMetricsDTO(BaseModel):
     yield_q30: float
     created_at: datetime
 
+    @field_validator("base_passing_q30_percent", mode="before")
+    def validate_percent_fields(cls, value: float | None) -> float | None:
+        if value is not None:
+            return fraction_to_percent(value)
+        return value
+
     class Config:
         arbitrary_types_allowed = True
+        validate_assignment = True
