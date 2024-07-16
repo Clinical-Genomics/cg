c64c8ee312a291b0a58767d22c46eb2bed38b4cc
seallard <sebastian.allard@scilifelab.se>
2024-07-16 12:13:24 +0200

    Extract helper method

--- cg/services/order_validation_service/validators/inter_field_validators.py
+++ cg/services/order_validation_service/validators/inter_field_validators.py
@@ -16,10 +16,14 @@ def validate_ticket_number_required_if_connected(order: Order, **kwargs) -> list
 
 def validate_name_required_for_new_order(order: Order, **kwargs) -> list[ValidationError]:
     errors: list[ValidationError] = []
-    if not order.connect_to_ticket and not order.name:
+    if _order_name_is_required(order):
         error = OrderNameRequiredError()
         errors.append(error)
     return errors
+
+
+def _order_name_is_required(order: Order) -> bool:
+    return not order.connect_to_ticket and not order.name
 
 
 def _ticket_number_is_missing(order: Order) -> bool:
