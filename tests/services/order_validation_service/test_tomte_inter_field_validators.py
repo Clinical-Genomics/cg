b02536c4196c583087c8f15c0f7e268f70b586a7
seallard <sebastian.allard@scilifelab.se>
2024-07-17 14:56:14 +0200

    Add negative test

--- tests/services/order_validation_service/test_tomte_inter_field_validators.py
+++ tests/services/order_validation_service/test_tomte_inter_field_validators.py
@@ -17,3 +17,13 @@ def test_multiple_samples_in_well_not_allowed(order_with_samples_in_same_well: T
 
     # THEN the error should be about the well
     assert isinstance(errors[0], OccupiedWellError)
+
+def test_order_without_multiple_samples_in_well(valid_order: TomteOrder):
+
+    # GIVEN a valid order with no samples in the same well
+
+    # WHEN validating the order
+    errors = validate_wells_contain_at_most_one_sample(valid_order)
+
+    # THEN no errors should be returned
+    assert not errors
