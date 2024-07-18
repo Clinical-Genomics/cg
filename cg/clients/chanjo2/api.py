1c74cf21397ccdd9f83b9b31492434683477a565
Vadym <vadym.ivanchuk@scilifelab.se>
2024-07-18 11:38:59 +0200

    Add Chanjo2APIClient

---
+++ cg/clients/chanjo2/api.py
@@ -0,0 +1,34 @@
+"""Coverage analysis API for human clinical sequencing data."""
+
+import logging
+from typing import Any
+
+import requests
+from pydantic import ValidationError
+
+from cg.clients.chanjo2.models import CoverageData, CoverageRequest
+
+LOG = logging.getLogger(__name__)
+
+
+class Chanjo2APIClient:
+    """
+    Chanjo2 API to communicate with a d4tools software in order to return coverage
+    over genomic intervals.
+    """
+
+    def __init__(self, config: dict[str, str]):
+        self.host = config["chanjo2"]["host"]
+
+    def get_coverage(self, coverage_request: CoverageRequest) -> CoverageData | None:
+        """Send a POST request to the coverage endpoint to retrieve gene coverage summary data."""
+        endpoint: str = f"{self.host}/coverage/d4/genes/summary"
+        post_data: dict[str, Any] = coverage_request.model_dump()
+        try:
+            response = requests.post(endpoint, data=post_data)
+            response.raise_for_status()
+            coverage_data = next(iter(response.json().values()), None)
+            return CoverageData(**coverage_data)
+        except (requests.RequestException, ValueError, ValidationError) as error:
+            LOG.error(f"Error during coverage POST request: {error}")
+            return None
---
+++ cg/clients/chanjo2/models.py
@@ -0,0 +1,29 @@
+"""Chanjo2 API pydantic models."""
+
+from pydantic import BaseModel
+
+from cg.constants.gene_panel import GenomeReferenceBuild
+
+
+class Sample(BaseModel):
+    """Chanjo2 sample model defined with an ID and a coverage file path."""
+
+    coverage_path: str
+    id: str
+
+
+class CoverageRequest(BaseModel):
+    """Chanjo2 coverage POST request model."""
+
+    build: GenomeReferenceBuild
+    coverage_threshold: int
+    gene_ids: list[int]
+    interval_type: str
+    samples: list[Sample]
+
+
+class CoverageData(BaseModel):
+    """Coverage data model returned from the POST request."""
+
+    mean_coverage: float
+    coverage_completeness_percent: float
