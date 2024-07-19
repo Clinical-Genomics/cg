a3b541628c8ccb2d5d1c1f071480d6d341fd5e3c
Vadym <vadym.ivanchuk@scilifelab.se>
2024-07-19 16:47:12 +0200

    Support is_report_accredited

--- cg/meta/report/raredisease.py
+++ cg/meta/report/raredisease.py
@@ -2,7 +2,9 @@
 
 from cg.meta.report.report_api import ReportAPI
 from cg.meta.workflow.raredisease import RarediseaseAnalysisAPI
+from cg.models.analysis import AnalysisModel
 from cg.models.cg_config import CGConfig
+from cg.models.report.sample import SampleModel
 
 
 class RarediseaseReportAPI(ReportAPI):
@@ -10,3 +12,12 @@ class RarediseaseReportAPI(ReportAPI):
 
     def __init__(self, config: CGConfig, analysis_api: RarediseaseAnalysisAPI):
         super().__init__(config=config, analysis_api=analysis_api)
+
+    def is_report_accredited(
+        self, samples: list[SampleModel], analysis_metadata: AnalysisModel = None
+    ) -> bool:
+        """
+        Return whether the Raredisease delivery report is accredited.
+        This method evaluates the accreditation status of each sample's application.
+        """
+        return all(sample.application.accredited for sample in samples)
