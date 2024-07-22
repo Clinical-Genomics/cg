4b24f21daebca017c94f80f7ce2f470d1e6d7e5f
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-07-22 15:17:26 +0200

    rename decorator for clarity

--- cg/services/pacbio/metrics/utils.py
+++ cg/services/pacbio/metrics/utils.py
@@ -17,7 +17,7 @@ from cg.services.pacbio.metrics.models import (
 from cg.utils.files import get_file_in_directory
 
 
-def handle_missing_metrics_file(func):
+def handle_pac_bio_parsing_errors(func):
     """Decorator to catch any metrics parsing error to raise a PacBioMetricsParsingError instead."""
 
     @wraps(func)
@@ -32,7 +32,7 @@ def handle_missing_metrics_file(func):
     return wrapper
 
 
-@handle_missing_metrics_file
+@handle_pac_bio_parsing_errors
 def parse_report_to_model(report_file: Path, data_model: Type[BaseMetrics]) -> BaseMetrics:
     """Parse the metrics report to a data model."""
     parsed_json: dict = ReadFile.read_file[FileFormat.JSON](file_path=report_file)
@@ -41,7 +41,7 @@ def parse_report_to_model(report_file: Path, data_model: Type[BaseMetrics]) -> B
     return data_model.model_validate(data, from_attributes=True)
 
 
-@handle_missing_metrics_file
+@handle_pac_bio_parsing_errors
 def parse_dataset_metrics(report_dir: Path) -> SmrtlinkDatasetsMetrics:
     file_name = PacBioDirsAndFiles.SMRTLINK_DATASETS_REPORT
     report: Path = get_file_in_directory(directory=report_dir, file_name=file_name)
@@ -50,28 +50,28 @@ def parse_dataset_metrics(report_dir: Path) -> SmrtlinkDatasetsMetrics:
     return SmrtlinkDatasetsMetrics.model_validate(data, from_attributes=True)
 
 
-@handle_missing_metrics_file
+@handle_pac_bio_parsing_errors
 def parse_hifi_metrics(report_dir: Path) -> HiFiMetrics:
     file_name = PacBioDirsAndFiles.BASECALLING_REPORT
     report: Path = get_file_in_directory(directory=report_dir, file_name=file_name)
     return parse_report_to_model(report_file=report, data_model=HiFiMetrics)
 
 
-@handle_missing_metrics_file
+@handle_pac_bio_parsing_errors
 def parse_control_metrics(report_dir: Path) -> ControlMetrics:
     file_name = PacBioDirsAndFiles.CONTROL_REPORT
     report: Path = get_file_in_directory(directory=report_dir, file_name=file_name)
     return parse_report_to_model(report_file=report, data_model=ControlMetrics)
 
 
-@handle_missing_metrics_file
+@handle_pac_bio_parsing_errors
 def parse_productivity_metrics(report_dir: Path) -> ProductivityMetrics:
     file_name = PacBioDirsAndFiles.LOADING_REPORT
     report: Path = get_file_in_directory(directory=report_dir, file_name=file_name)
     return parse_report_to_model(report_file=report, data_model=ProductivityMetrics)
 
 
-@handle_missing_metrics_file
+@handle_pac_bio_parsing_errors
 def parse_polymerase_metrics(report_dir: Path) -> PolymeraseMetrics:
     file_name = PacBioDirsAndFiles.RAW_DATA_REPORT
     report: Path = get_file_in_directory(directory=report_dir, file_name=file_name)
