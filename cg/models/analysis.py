89e2f9f052008e3b3912c82feff4264f5af0e951
Vadym <vadym.ivanchuk@scilifelab.se>
2024-07-22 12:44:05 +0200

    Rollback QCMetrics typehinting

--- cg/models/analysis.py
+++ cg/models/analysis.py
@@ -1,6 +1,9 @@
 from pydantic.v1 import BaseModel
 
-from cg.models.qc_metrics import QCMetrics
+from cg.models.raredisease.raredisease import RarediseaseQCMetrics
+from cg.models.rnafusion.rnafusion import RnafusionQCMetrics
+from cg.models.taxprofiler.taxprofiler import TaxprofilerQCMetrics
+from cg.models.tomte.tomte import TomteQCMetrics
 
 
 class AnalysisModel(BaseModel):
@@ -10,4 +13,6 @@ class AnalysisModel(BaseModel):
 class NextflowAnalysis(AnalysisModel):
     """Nextflow's analysis results model."""
 
-    sample_metrics: dict[str, QCMetrics]
+    sample_metrics: dict[
+        str, RarediseaseQCMetrics | RnafusionQCMetrics | TaxprofilerQCMetrics | TomteQCMetrics
+    ]
