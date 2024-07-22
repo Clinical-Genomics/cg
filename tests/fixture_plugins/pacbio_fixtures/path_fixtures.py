11f6ab6f316fe36dd2ef36901f593c58e531e733
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-07-22 14:01:16 +0200

    remove unused fixtures

--- tests/fixture_plugins/pacbio_fixtures/path_fixtures.py
+++ tests/fixture_plugins/pacbio_fixtures/path_fixtures.py
@@ -4,8 +4,6 @@ from pathlib import Path
 
 import pytest
 
-from cg.constants.pacbio import PacBioDirsAndFiles
-
 
 # Directory fixtures
 @pytest.fixture
@@ -36,30 +34,3 @@ def pac_bio_smrt_cell_dir(pac_bio_test_run_dir: Path) -> Path:
 def pac_bio_run_statistics_dir(pac_bio_smrt_cell_dir: Path) -> Path:
     """Return the path to the PacBio SMRT cell statistics directory."""
     return Path(pac_bio_smrt_cell_dir, "statistics")
-
-
-# File fixtures
-
-
-@pytest.fixture
-def pac_bio_css_report(pac_bio_run_statistics_dir: Path) -> Path:
-    """Return the path to the PacBio CSS report."""
-    return Path(pac_bio_run_statistics_dir, PacBioDirsAndFiles.BASECALLING_REPORT)
-
-
-@pytest.fixture
-def pac_bio_control_report(pac_bio_run_statistics_dir: Path) -> Path:
-    """Return the path to the PacBio control report."""
-    return Path(pac_bio_run_statistics_dir, PacBioDirsAndFiles.CONTROL_REPORT)
-
-
-@pytest.fixture
-def pac_bio_loading_report(pac_bio_run_statistics_dir: Path) -> Path:
-    """Return the path to the PacBio loading report."""
-    return Path(pac_bio_run_statistics_dir, PacBioDirsAndFiles.LOADING_REPORT)
-
-
-@pytest.fixture
-def pac_bio_raw_data_report(pac_bio_run_statistics_dir: Path) -> Path:
-    """Return the path to the PacBio raw data report."""
-    return Path(pac_bio_run_statistics_dir, PacBioDirsAndFiles.RAW_DATA_REPORT)
