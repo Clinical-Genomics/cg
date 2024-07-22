a8b79ebedd74e22d669252aef243a8cad818ba51
Vadym <vadym.ivanchuk@scilifelab.se>
2024-07-22 12:11:20 +0200

    Fill RarediseaseQCMetrics

--- cg/models/raredisease/raredisease.py
+++ cg/models/raredisease/raredisease.py
@@ -8,8 +8,10 @@ from cg.models.qc_metrics import QCMetrics
 class RarediseaseQCMetrics(QCMetrics):
     """Raredisease QC metrics."""
 
+    mapped_reads: int
+    percent_duplicates: float
     predicted_sex_sex_check: SexOptions
-    # TODO: identify metrics of interest
+    total_reads: int
 
 
 class RarediseaseSampleSheetEntry(NextflowSampleSheetEntry):
