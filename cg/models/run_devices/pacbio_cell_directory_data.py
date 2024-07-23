6c51e37ee535d0bd0a84e2c3173d7d380b98597b
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-07-23 08:51:09 +0200

    add pacbio cell directory data

---
+++ cg/models/run_devices/pacbio_cell_directory_data.py
@@ -0,0 +1,20 @@
+from pathlib import Path
+
+from cg.constants.pacbio import PacBioDirsAndFiles
+from cg.utils.files import get_files_in_directory_with_pattern
+
+
+class PacBioCellDirectoryData:
+    """Class to collect information about PacBio sequencing run directories."""
+
+    def __init__(self, pac_bio_run_directory: Path):
+        self.path = pac_bio_run_directory
+        self.statistics_dir = Path(self.path, "statistics")
+
+    @property
+    def ccs_report(self) -> Path:
+        """Finds the json file finishing with ccs_report.json in the statistics dir."""
+        files: list[Path] = get_files_in_directory_with_pattern(
+            directory=self.statistics_dir, pattern=PacBioDirsAndFiles.CCS_REPORT_SUFFIX
+        )
+        return files[0]
