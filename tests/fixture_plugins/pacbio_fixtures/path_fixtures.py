1b7df22a37255c82a4a07663a8b00efbed26687f
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-07-24 14:21:47 +0200

    added tests for file manager

--- tests/fixture_plugins/pacbio_fixtures/path_fixtures.py
+++ tests/fixture_plugins/pacbio_fixtures/path_fixtures.py
@@ -39,6 +39,12 @@ def pac_bio_smrt_cell_dir_1_a01(pac_bio_test_run_dir: Path, pac_bio_smrt_cell_na
 
 
 @pytest.fixture
+def pac_bio_hifi_reads_dir(pac_bio_smrt_cell_dir_1_a01: Path) -> Path:
+    """Return the path to a PacBio HiFi reads directory."""
+    return Path(pac_bio_smrt_cell_dir_1_a01, PacBioDirsAndFiles.HIFI_READS_DIR)
+
+
+@pytest.fixture
 def pac_bio_run_statistics_dir(pac_bio_smrt_cell_dir_1_a01: Path) -> Path:
     """Return the path to the PacBio SMRT cell statistics directory."""
     return Path(pac_bio_smrt_cell_dir_1_a01, PacBioDirsAndFiles.STATISTICS_DIR)
@@ -54,3 +60,78 @@ def pac_bio_run_reports_dir(pac_bio_run_statistics_dir: Path) -> Path:
 def pac_bio_wrong_metrics_file(pac_bio_wrong_metrics_dir: Path) -> Path:
     """Return the path to a temporary PacBio statistics directory."""
     return Path(pac_bio_wrong_metrics_dir, "metrics.json")
+
+
+# File fixtures
+
+
+@pytest.fixture
+def pac_bio_ccs_report_file(
+    pac_bio_run_statistics_dir: Path, pac_bio_1_a01_cell_full_name: str
+) -> Path:
+    """Return the path to the PacBio CCS report file."""
+    return Path(
+        pac_bio_run_statistics_dir,
+        f"{pac_bio_1_a01_cell_full_name}.{PacBioDirsAndFiles.CCS_REPORT_SUFFIX}",
+    )
+
+
+@pytest.fixture
+def pac_bio_basecalling_report_file(pac_bio_run_reports_dir: Path) -> Path:
+    """Return the path to the PacBio basecalling report file."""
+    return Path(pac_bio_run_reports_dir, PacBioDirsAndFiles.BASECALLING_REPORT)
+
+
+@pytest.fixture
+def pac_bio_control_report_file(pac_bio_run_reports_dir: Path) -> Path:
+    """Return the path to the PacBio control report file."""
+    return Path(pac_bio_run_reports_dir, PacBioDirsAndFiles.CONTROL_REPORT)
+
+
+@pytest.fixture
+def pac_bio_loading_report_file(pac_bio_run_reports_dir: Path) -> Path:
+    """Return the path to the PacBio loading report file."""
+    return Path(pac_bio_run_reports_dir, PacBioDirsAndFiles.LOADING_REPORT)
+
+
+@pytest.fixture
+def pac_bio_raw_data_report_file(pac_bio_run_reports_dir: Path) -> Path:
+    """Return the path to the PacBio raw data report file."""
+    return Path(pac_bio_run_reports_dir, PacBioDirsAndFiles.RAW_DATA_REPORT)
+
+
+@pytest.fixture
+def pac_bio_smrtlink_datasets_report_file(pac_bio_run_reports_dir: Path) -> Path:
+    """Return the path to the PacBio SMRTLink datasets report file."""
+    return Path(pac_bio_run_reports_dir, PacBioDirsAndFiles.SMRTLINK_DATASETS_REPORT)
+
+
+@pytest.fixture
+def pac_bio_report_files_to_parse(
+    pac_bio_ccs_report_file: Path,
+    pac_bio_basecalling_report_file: Path,
+    pac_bio_control_report_file: Path,
+    pac_bio_loading_report_file: Path,
+    pac_bio_raw_data_report_file: Path,
+    pac_bio_smrtlink_datasets_report_file: Path,
+) -> list[Path]:
+    """Return the list of PacBio report files to parse."""
+    return [
+        pac_bio_ccs_report_file,
+        pac_bio_basecalling_report_file,
+        pac_bio_control_report_file,
+        pac_bio_loading_report_file,
+        pac_bio_raw_data_report_file,
+        pac_bio_smrtlink_datasets_report_file,
+    ]
+
+
+@pytest.fixture
+def pac_bio_hifi_files(
+    pac_bio_hifi_reads_dir: Path, pac_bio_1_a01_cell_full_name: str
+) -> list[Path]:
+    """Return the list of PacBio HiFi read files."""
+    return [
+        Path(pac_bio_hifi_reads_dir, f"{pac_bio_1_a01_cell_full_name}.hifi_reads.bam"),
+        Path(pac_bio_hifi_reads_dir, f"{pac_bio_1_a01_cell_full_name}.hifi_reads.bam.pbi"),
+    ]
---
+++ tests/services/post_processing/pacbio/run_file_manager/test_pacbio_run_file_manager.py
@@ -0,0 +1,42 @@
+from pathlib import Path
+
+from cg.services.post_processing.pacbio.run_data_generator.run_data import PacBioRunData
+from cg.services.post_processing.pacbio.run_file_manager.run_file_manager import (
+    PacBioRunFileManager,
+)
+
+
+def test_get_files_to_parse(
+    expected_pac_bio_run_data: PacBioRunData, pac_bio_report_files_to_parse: list[Path]
+):
+    """Test that the report file paths are returned."""
+
+    # GIVEN a run data object
+
+    # GIVEN a PacBio run file manager
+    file_manager = PacBioRunFileManager()
+
+    # WHEN getting the files to parse
+    files: list[Path] = file_manager.get_files_to_parse(expected_pac_bio_run_data)
+
+    # THEN the correct files are returned
+    assert files == pac_bio_report_files_to_parse
+
+
+def test_get_files_to_store(
+    expected_pac_bio_run_data: PacBioRunData,
+    pac_bio_report_files_to_parse: list[Path],
+    pac_bio_hifi_files: list[Path],
+):
+    """Test that the files to be stored are returned"""
+
+    # GIVEN a run data object
+
+    # GIVEN a PacBio run file manager
+    file_manager = PacBioRunFileManager()
+
+    # WHEN getting the files to store
+    files: list[Path] = file_manager.get_files_to_store(expected_pac_bio_run_data)
+
+    # THEN the correct files are returned
+    assert files == pac_bio_hifi_files + pac_bio_report_files_to_parse
