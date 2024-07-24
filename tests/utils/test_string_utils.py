814eff0d2155273df19009eb311f617544531acb
Christian Oertlin <c.oertlin@gmail.com>
2024-07-24 11:35:29 +0200

    add string error handling

--- tests/utils/test_string_utils.py
+++ tests/utils/test_string_utils.py
@@ -1,5 +1,8 @@
 """Test for the string utilities,"""
 
+import pytest
+
+from cg.exc import CgError
 from cg.utils.string import get_element_from_split
 
 
@@ -13,3 +16,15 @@ def test_get_element_from_split():
 
     # THEN the expected element is returned
     assert element == "two"
+
+
+def test_get_element_from_split_error():
+
+    # GIVEN a string with a seperator
+    separated_string: str = "zero_one_two_three"
+
+    # WHEN getting an element divided by a separator based on the position that is out of bounds
+    with pytest.raises(CgError):
+        get_element_from_split(value=separated_string, element_position=12, split="_")
+
+        # THEN an error is raised
