6851e5dc984c5e7e195f114964ca8b72e9acd5e3
Christian Oertlin <c.oertlin@gmail.com>
2024-07-24 11:29:53 +0200

    fix test

--- tests/utils/test_string_utils.py
+++ tests/utils/test_string_utils.py
@@ -6,7 +6,7 @@ from cg.utils.string import get_element_from_split
 def test_get_element_from_split():
 
     # GIVEN a string with a seperator
-    separated_string: str = "one_two_three_four"
+    separated_string: str = "zero_one_two_three"
 
     # WHEN getting an element divided by a separator based on the position
     element: str = get_element_from_split(value=separated_string, element_position=2, split="_")
