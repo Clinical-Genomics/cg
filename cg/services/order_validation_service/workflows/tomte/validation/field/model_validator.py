20c24feafead07a7ea879f02c936e72ab8604a5b
islean <isak.ohlsson.angnell@gmail.com>
2024-07-29 14:11:38 +0200

    Refactor validators and utils

--- cg/services/order_validation_service/workflows/tomte/validation/field/model_validator.py
+++ cg/services/order_validation_service/workflows/tomte/validation/field/model_validator.py
@@ -5,11 +5,10 @@ from cg.services.order_validation_service.utils import convert_errors
 from cg.services.order_validation_service.workflows.tomte.models.order import TomteOrder
 
 
-class TomteFieldValidator:
+class TomteModelValidator:
 
     def validate(self, order_json: str) -> TomteOrder | ValidationErrors:
         try:
             return TomteOrder.model_validate_json(order_json)
         except ValidationError as error:
-            order_errors: ValidationErrors = convert_errors(error.errors())
-            return order_errors
+            return convert_errors(error.errors())
--- cg/services/order_validation_service/workflows/tomte/validation/inter_field/utils.py
+++ cg/services/order_validation_service/workflows/tomte/validation/inter_field/utils.py
@@ -7,12 +7,6 @@ from cg.services.order_validation_service.workflows.tomte.models.sample import (
 )
 
 
-def validate_wells_contain_at_most_one_sample(order: TomteOrder) -> list[OccupiedWellError]:
-    samples_with_cases = _get_plate_samples(order)
-    samples = _get_excess_samples(samples_with_cases)
-    return _get_errors(samples)
-
-
 def _get_errors(colliding_samples: list[tuple[TomteSample, TomteCase]]) -> list[OccupiedWellError]:
     errors = []
     for sample, case in colliding_samples:
