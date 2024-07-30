96a4e6af1dc97493a709556d5c8e5379f859428d
seallard <sebastian.allard@scilifelab.se>
2024-07-30 16:53:46 +0200

    Fix field name for error

--- cg/services/order_validation_service/models/errors.py
+++ cg/services/order_validation_service/models/errors.py
@@ -71,10 +71,10 @@ class RepeatedSampleNameError(CaseSampleError):
 
 
 class InvalidFatherSexError(CaseSampleError):
-    field: str = "sex"
+    field: str = "father"
     message: str = "Father must be male"
 
 
 class InvalidMotherSexError(CaseSampleError):
-    field: str = "sex"
+    field: str = "mother"
     message: str = "Mother must be female"
