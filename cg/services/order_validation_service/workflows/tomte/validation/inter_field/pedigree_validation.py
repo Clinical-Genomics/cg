0e20b7ef613e93cffbb7083702ddecab17b95cb9
seallard <sebastian.allard@scilifelab.se>
2024-07-31 14:26:42 +0200

    Implement cycle detection

--- cg/services/order_validation_service/workflows/tomte/validation/inter_field/pedigree_validation.py
+++ cg/services/order_validation_service/workflows/tomte/validation/inter_field/pedigree_validation.py
@@ -1,5 +1,9 @@
 from pydantic import BaseModel
-from cg.services.order_validation_service.models.errors import PedigreeError
+from cg.services.order_validation_service.models.errors import (
+    PedigreeError,
+    SampleHasOffspringAsParent,
+    SampleIsOwnParentError,
+)
 from cg.services.order_validation_service.workflows.tomte.models.case import TomteCase
 from cg.services.order_validation_service.workflows.tomte.models.sample import TomteSample
 
@@ -10,6 +14,7 @@ class Node:
         self.father = father
         self.mother = mother
         self.visited = False
+        self.in_stack = False
 
 
 class Pedigree:
@@ -26,8 +31,45 @@ class Pedigree:
         node = Node(sample=sample, mother=mother, father=father)
         self.pedigree[sample.name] = node
 
-    def validate(self) -> list[str]:
-        return []
+    def validate(self) -> list[PedigreeError]:
+        errors = []
+        for node in self.pedigree.values():
+            if not node.visited:
+                self._dfs(node, errors)
+        return errors
+
+    def _dfs(self, node: Node, errors: list[PedigreeError]) -> None:
+        node.visited = True
+        node.in_stack = True
+
+        for parent in [node.mother, node.father]:
+
+            if not parent:
+                continue
+
+            parent_node: Node = self.pedigree.get(parent.name)
+            if parent_node:
+                if parent_node.in_stack:
+                    if parent_node == node:
+                        errors.append(
+                            SampleIsOwnParentError(
+                                field="error",
+                                sample_name=node.sample.name,
+                                case_name=self.case.name,
+                            )
+                        )
+                    else:
+                        errors.append(
+                            SampleHasOffspringAsParent(
+                                field="error",
+                                sample_name=node.sample.name,
+                                case_name=self.case.name,
+                            )
+                        )
+                elif not parent_node.visited:
+                    self._dfs(parent_node, errors)
+
+        node.in_stack = False
 
 
 def get_pedigree_errors(case: TomteCase) -> list[PedigreeError]:
