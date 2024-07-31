cfecb3843177721b7690578fecd7754661d425f9
Christian Oertlin <c.oertlin@gmail.com>
2024-07-31 14:04:40 +0200

    remove docstring

--- tests/services/post_processing/pacbio/run_file_manager/test_pacbio_run_file_manager.py
+++ tests/services/post_processing/pacbio/run_file_manager/test_pacbio_run_file_manager.py
@@ -13,8 +13,6 @@ from cg.services.post_processing.pacbio.run_file_manager.run_file_manager import
 def test_get_files_to_parse(
     expected_pac_bio_run_data: PacBioRunData, pac_bio_report_files_to_parse: list[Path]
 ):
-    """Test that the report file paths are returned."""
-
     # GIVEN a run data object
 
     # GIVEN a PacBio run file manager
@@ -32,8 +30,6 @@ def test_get_files_to_store(
     pac_bio_report_files_to_parse: list[Path],
     pac_bio_hifi_files: list[Path],
 ):
-    """Test that the files to be stored are returned"""
-
     # GIVEN a run data object
 
     # GIVEN a PacBio run file manager
@@ -50,8 +46,6 @@ def test_get_files_to_store(
 def test_get_files_to_store_error(
     expected_pac_bio_run_data: PacBioRunData,
 ):
-    """Test that the files to be stored are returned."""
-
     # GIVEN a run data object
 
     # GIVEN a PacBio run file manager
@@ -71,8 +65,6 @@ def test_get_files_to_store_error(
 def test_get_files_to_parse_error(
     expected_pac_bio_run_data: PacBioRunData,
 ):
-    """Test that the files to be stored are returned."""
-
     # GIVEN a run data object
 
     # GIVEN a PacBio run file manager
