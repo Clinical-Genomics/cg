741c62f980670eb5e670c990fbec7aa9609c6ea0
seallard <sebastian.allard@scilifelab.se>
2024-08-01 16:06:02 +0200

    Separate traversal logic

--- cg/services/order_validation_service/workflows/tomte/validation/inter_field/pedigree/models.py
+++ cg/services/order_validation_service/workflows/tomte/validation/inter_field/pedigree/models.py
@@ -1,24 +1,18 @@
-from cg.services.order_validation_service.models.errors import (
-    DescendantAsFatherError,
-    DescendantAsMotherError,
-    PedigreeError,
-    SampleIsOwnFatherError,
-    SampleIsOwnMotherError,
-)
 from cg.services.order_validation_service.workflows.tomte.models.case import TomteCase
 from cg.services.order_validation_service.workflows.tomte.models.sample import TomteSample
 
 
 class Node:
-    def __init__(self, sample: TomteSample):
+    def __init__(self, sample: TomteSample, case_name: str):
         self.sample = sample
+        self.case_name = case_name
         self.father: Node | None = None
         self.mother: Node | None = None
         self.visited = False
         self.in_current_path = False
 
 
-class Pedigree:
+class FamilyTree:
     def __init__(self, case: TomteCase):
         self.graph: dict[str, Node] = {}
         self.case = case
@@ -27,7 +21,7 @@ class Pedigree:
 
     def _add_nodes(self) -> None:
         for sample in self.case.samples:
-            node = Node(sample=sample)
+            node = Node(sample=sample, case_name=self.case.name)
             self.graph[sample.name] = node
 
     def add_edges(self) -> None:
@@ -41,44 +35,3 @@ class Pedigree:
     @property
     def nodes(self) -> list[Node]:
         return self.graph.values()
-
-    def validate(self) -> list[PedigreeError]:
-        errors = []
-        for node in self.nodes:
-            if not node.visited:
-                self._detect_cycles(node, errors)
-        return errors
-
-    def _detect_cycles(self, node: Node, errors: list[PedigreeError]) -> None:
-        """Detect cycles in the pedigree graph using depth-first search"""
-        node.visited = True
-        node.in_current_path = True
-
-        parents = {"mother": node.mother, "father": node.father}
-
-        for parent_type, parent in parents.items():
-            if parent and parent.in_current_path:
-                error = self._get_error(node=node, parent_type=parent_type)
-                errors.append(error)
-            elif parent and not parent.visited:
-                self._detect_cycles(parent, errors)
-
-        node.in_current_path = False
-
-    def _get_error(self, node: Node, parent_type: str) -> PedigreeError:
-        if parent_type == "mother":
-            return self._get_mother_error(node)
-        if parent_type == "father":
-            return self._get_father_error(node)
-
-    def _get_mother_error(self, node: Node) -> PedigreeError:
-        sample = node.sample
-        if sample.name == sample.mother:
-            return SampleIsOwnMotherError(sample_name=sample.name, case_name=self.case.name)
-        return DescendantAsMotherError(sample_name=sample.name, case_name=self.case.name)
-
-    def _get_father_error(self, node: Node) -> PedigreeError:
-        sample = node.sample
-        if sample.name == sample.father:
-            return SampleIsOwnFatherError(sample_name=sample.name, case_name=self.case.name)
-        return DescendantAsFatherError(sample_name=sample.name, case_name=self.case.name)
--- cg/services/order_validation_service/workflows/tomte/validation/inter_field/pedigree/validate_pedigree.py
+++ cg/services/order_validation_service/workflows/tomte/validation/inter_field/pedigree/validate_pedigree.py
@@ -1,10 +1,13 @@
 from cg.services.order_validation_service.models.errors import PedigreeError
 from cg.services.order_validation_service.workflows.tomte.models.case import TomteCase
 from cg.services.order_validation_service.workflows.tomte.validation.inter_field.pedigree.models import (
-    Pedigree,
+    FamilyTree,
+)
+from cg.services.order_validation_service.workflows.tomte.validation.inter_field.pedigree.utils import (
+    validate_tree,
 )
 
 
 def get_pedigree_errors(case: TomteCase) -> list[PedigreeError]:
-    pedigree = Pedigree(case)
-    return pedigree.validate()
+    pedigree = FamilyTree(case)
+    return validate_tree(pedigree)
