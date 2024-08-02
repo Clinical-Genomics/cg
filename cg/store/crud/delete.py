1f3529c70ec5ec766b197f7bb248f296cae3ce62
seallard <sebastian.allard@scilifelab.se>
2024-08-02 15:57:34 +0200

    Remove unused method

--- cg/store/crud/delete.py
+++ cg/store/crud/delete.py
@@ -13,13 +13,6 @@ class DeleteDataHandler(BaseHandler):
         super().__init__(session=session)
         self.session = session
 
-    def delete_relationships_sample(self, sample: Sample) -> None:
-        """Delete relationships between all cases and the provided sample."""
-        if sample and sample.links:
-            for case_sample in sample.links:
-                self.session.delete(case_sample)
-            self.session.commit()
-
     def delete_cases_without_samples(self, case_internal_ids: list[str]) -> None:
         """Delete any cases specified in case_ids without samples."""
         for case_internal_id in case_internal_ids:
