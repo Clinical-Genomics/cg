b55a1531017125cc87c53ecbc59f189dca00096e
seallard <sebastian.allard@scilifelab.se>
2024-08-02 13:51:17 +0200

    Formatting

--- cg/services/sample_service/utils.py
+++ cg/services/sample_service/utils.py
@@ -7,9 +7,9 @@ def get_cancel_comment(user_name: str) -> str:
 
 
 def get_confirmation_message(sample_ids: list[str], remaining_cases: list[str]) -> str:
-    samples: str = "sample" if len(sample_ids) == 1 else "samples"
-    message: str = f"Cancelled {len(sample_ids)} {samples}. "
- 
+    samples = "sample" if len(sample_ids) == 1 else "samples"
+    message = f"Cancelled {len(sample_ids)} {samples}. "
+
     if remaining_cases:
         cases = "case" if len(remaining_cases) == 1 else "cases"
         case_message = ", ".join(remaining_cases)
