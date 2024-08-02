be12a73c20c8ae4cfc2eee3805dd9ee1f27f07d1
seallard <sebastian.allard@scilifelab.se>
2024-08-02 13:47:09 +0200

    Simplify

--- cg/services/sample_service/utils.py
+++ cg/services/sample_service/utils.py
@@ -6,20 +6,15 @@ def get_cancel_comment(user_name: str) -> str:
     return f"Cancelled {date} by {user_name}"
 
 
-def get_confirmation_message(sample_ids: list[str], remaining_cases: list[str]):
+def get_confirmation_message(sample_ids: list[str], remaining_cases: list[str]) -> str:
     samples: str = "sample" if len(sample_ids) == 1 else "samples"
-    cases: str = "case" if len(remaining_cases) == 1 else "cases"
-
     message: str = f"Cancelled {len(sample_ids)} {samples}. "
-    case_message: str = ""
-
-    for case_id in remaining_cases:
-        case_message = f"{case_message} {case_id},"
-
-    case_message = case_message.strip(",")
-
+ 
     if remaining_cases:
-        case_count = len(remaining_cases)
-        message += f"Found {case_count} {cases} with additional samples: {case_message}."
+        cases = "case" if len(remaining_cases) == 1 else "cases"
+        case_message = ", ".join(remaining_cases)
+        message += f"Found {len(remaining_cases)} {cases} with additional samples: {case_message}."
     else:
         message += "No case contained additional samples."
+
+    return message
