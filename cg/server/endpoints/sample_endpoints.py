58e2835688475c2d8f517af45fd7b07d6d87186c
seallard <sebastian.allard@scilifelab.se>
2024-08-03 15:31:21 +0200

    Fix parsing request params

--- cg/server/endpoints/sample_endpoints.py
+++ cg/server/endpoints/sample_endpoints.py
@@ -13,6 +13,6 @@ SAMPLES_BLUEPRINT = Blueprint("samples", __name__, url_prefix="/api/v1")
 
 @SAMPLES_BLUEPRINT.route("/samples_in_collaboration")
 def get_samples_in_collaboration():
-    data = CollaboratorSamplesRequest.model_validate(request.args)
+    data = CollaboratorSamplesRequest.model_validate(request.values.to_dict())
     response: SamplesResponse = sample_service.get_collaborator_samples(data)
     return jsonify(response.model_dump()), HTTPStatus.OK
