e72cc59fdc48df65c217483848ab4e4d0b204b51
seallard <sebastian.allard@scilifelab.se>
2024-08-04 13:26:06 +0200

    Simplify

--- cg/services/sample_service/sample_service.py
+++ cg/services/sample_service/sample_service.py
@@ -15,10 +15,8 @@ class SampleService:
         self.store = store
 
     def cancel_sample(self, sample_id: int, user_email: str) -> None:
-        case_ids: list[str] = self.store.get_case_ids_with_sample(sample_id)
         self.store.mark_sample_as_cancelled(sample_id)
         self.store.decouple_sample_from_cases(sample_id)
-        self.store.delete_cases_without_samples(case_ids)
         self._add_cancel_comment(sample_id=sample_id, user_email=user_email)
 
     def _add_cancel_comment(self, sample_id: int, user_email: str) -> None:
