68ea91e389e79b7f0606d5e9c7ddf6d8f57c5401
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-08-07 14:35:58 +0200

    black

--- tests/meta/upload/test_upload_genotypes_api.py
+++ tests/meta/upload/test_upload_genotypes_api.py
@@ -26,7 +26,10 @@ def test_get_analysis_sex_mip(
     # GIVEN an AnalysisAPI and some qcmetrics data
     mip_analysis_api = MipDNAAnalysisAPI(config=cg_context)
     # WHEN fetching the predicted sex by the analysis
-    sex: dict = MipDNAAnalysisAPI._get_analysis_sex(qc_metrics_file=case_qc_metrics_deliverables, sample_id=mip_analysis_api.sample_id_in_single_case)
+    sex: dict = MipDNAAnalysisAPI._get_analysis_sex(
+        qc_metrics_file=case_qc_metrics_deliverables,
+        sample_id=mip_analysis_api.sample_id_in_single_case,
+    )
 
     # THEN assert that the the predicted sex per sample_id is returned
     assert sex == genotype_analysis_sex
