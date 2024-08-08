0445c14fdf7650e7b25a0d11891af0a71b4ef23f
seallard <sebastian.allard@scilifelab.se>
2024-08-08 17:07:14 +0200

    Split functions

--- cg/services/order_validation_service/workflows/tomte/validation/field/utils.py
+++ cg/services/order_validation_service/workflows/tomte/validation/field/utils.py
@@ -26,12 +26,29 @@ def convert_sample_errors(error_details: list[ErrorDetails], order: dict) -> lis
     errors: list[SampleError] = []
     sample_details = get_sample_error_details(error_details)
     for error in sample_details:
-        sample_name = order["samples"][error["loc"][1]]["name"]
-        field_name = error["loc"][2]
-        message = error["msg"]
-        error = SampleError(sample_name=sample_name, field=field_name, message=message)
+        error = create_sample_error(error=error, order=order)
         errors.append(error)
     return errors
+
+
+def create_sample_error(error: ErrorDetails, order: dict) -> SampleError:
+    sample_name = get_sample_name(error=error, order=order)
+    field_name = get_sample_field_name(error)
+    message = get_error_message(error)
+    error = SampleError(sample_name=sample_name, field=field_name, message=message)
+    return error
+
+
+def get_sample_name(error: ErrorDetails, order: dict) -> str:
+    return order["samples"][error["loc"][1]]["name"]
+
+
+def get_sample_field_name(error: ErrorDetails) -> str:
+    return error["loc"][2]
+
+
+def get_error_message(error: ErrorDetails) -> str:
+    return error["msg"]
 
 
 def get_sample_error_details(error_details: list[ErrorDetails]) -> list[ErrorDetails]:
@@ -46,23 +63,40 @@ def convert_order_errors(error_details: list[ErrorDetails]) -> list[OrderError]:
     errors: list[OrderError] = []
     order_details = get_order_error_details(error_details)
     for error in order_details:
-        field_name = error["loc"][0]
-        message = error["msg"]
+        field_name = get_order_field_name(error)
+        message = get_error_message(error)
         error = OrderError(field=field_name, message=message)
         errors.append(error)
     return errors
+
+
+def get_order_field_name(error: ErrorDetails) -> str:
+    return error["loc"][0]
 
 
 def convert_case_errors(error_details: list[ErrorDetails], order: dict) -> list[CaseError]:
     errors: list[CaseError] = []
     case_details = get_case_error_details(error_details)
     for error in case_details:
-        case_name = order["cases"][error["loc"][1]]["name"]
-        field_name = error["loc"][2]
-        message = error["msg"]
-        error = CaseError(case_name=case_name, field=field_name, message=message)
+        error = create_case_error(error=error, order=order)
         errors.append(error)
     return errors
+
+
+def create_case_error(error: ErrorDetails, order: dict) -> CaseError:
+    case_name = get_case_name(error=error, order=order)
+    field_name = get_case_field_name(error)
+    message = get_error_message(error)
+    error = CaseError(case_name=case_name, field=field_name, message=message)
+    return error
+
+
+def get_case_name(error: ErrorDetails, order: dict) -> str:
+    return order["cases"][error["loc"][1]]["name"]
+
+
+def get_case_field_name(error: ErrorDetails) -> str:
+    return error["loc"][2]
 
 
 def convert_case_sample_errors(
@@ -71,15 +105,35 @@ def convert_case_sample_errors(
     errors: list[CaseSampleError] = []
     case_sample_details = get_case_sample_error_details(error_details)
     for error in case_sample_details:
-        case_name = order["cases"][error["loc"][1]]["name"]
-        sample_name = order["cases"][error["loc"][1]]["samples"][error["loc"][3]]["name"]
-        field_name = error["loc"][4]
-        message = error["msg"]
-        error = CaseSampleError(
-            case_name=case_name, sample_name=sample_name, field_name=field_name, message=message
-        )
+        error = create_case_sample_error(error=error, order=order)
         errors.append(error)
     return errors
+
+
+def create_case_sample_error(error: ErrorDetails, order: dict) -> CaseSampleError:
+    case_name = get_case_name(error=error, order=order)
+    sample_name = get_case_sample_name(error=error, order=order)
+    field_name = get_case_sample_field_name(error)
+    message = get_error_message(error)
+    error = CaseSampleError(
+        case_name=case_name,
+        sample_name=sample_name,
+        field_name=field_name,
+        message=message,
+    )
+    return error
+
+
+def get_case_name(error: ErrorDetails, order: dict) -> str:
+    return order["cases"][error["loc"][1]]["name"]
+
+
+def get_case_sample_name(error: ErrorDetails, order: dict) -> str:
+    return order["cases"][error["loc"][1]]["samples"][error["loc"][3]]["name"]
+
+
+def get_case_sample_field_name(error: ErrorDetails) -> str:
+    return error["loc"][4]
 
 
 def get_case_sample_error_details(error_details: list[ErrorDetails]) -> list[ErrorDetails]:
