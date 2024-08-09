e23cbbb0c28a38432f41fa7455376b04c6e9e5b1
seallard <sebastian.allard@scilifelab.se>
2024-08-09 10:34:02 +0200

    Extract flow cell endpoints

--- cg/server/api.py
+++ cg/server/api.py
@@ -13,23 +13,6 @@ BLUEPRINT = Blueprint("api", __name__, url_prefix="/api/v1")
 BLUEPRINT.before_request(before_request)
 
 
-@BLUEPRINT.route("/flowcells/<flow_cell_name>/sequencing_metrics", methods=["GET"])
-def get_sequencing_metrics(flow_cell_name: str):
-    """Return sample lane sequencing metrics for a flow cell."""
-    if not flow_cell_name:
-        return jsonify({"error": "Invalid or missing flow cell id"}), HTTPStatus.BAD_REQUEST
-    sequencing_metrics: list[IlluminaSampleSequencingMetrics] = (
-        db.get_illumina_sequencing_run_by_device_internal_id(flow_cell_name).sample_metrics
-    )
-    if not sequencing_metrics:
-        return (
-            jsonify({"error": f"Sequencing metrics not found for flow cell {flow_cell_name}."}),
-            HTTPStatus.NOT_FOUND,
-        )
-    metrics_dtos: list[SequencingMetricsRequest] = parse_metrics_into_request(sequencing_metrics)
-    return jsonify([metric.model_dump() for metric in metrics_dtos])
-
-
 @BLUEPRINT.route("/analyses")
 def get_analyses():
     """Return analyses."""
