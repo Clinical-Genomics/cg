6c8d3fad28f62bfd0c14ccf63dee1d1ac127d1c8
seallard <sebastian.allard@scilifelab.se>
2024-08-09 12:40:14 +0200

    Fix test

---
+++ tests/services/flow_cell_service/test_flow_cell_service.py
@@ -0,0 +1,11 @@
+from cg.services.flow_cell_service.flow_cell_service import FlowCellService
+
+
+def test_flow_cell_service(flow_cell_service: FlowCellService, novaseq_x_flow_cell_id: str):
+    # GIVEN an existing flow cell with sequencing metrics
+
+    # WHEN fetching the metrics for the flow cell
+    metrics = flow_cell_service.get_sequencing_metrics(novaseq_x_flow_cell_id)
+
+    # THEN the metrics should be returned
+    assert metrics
