6dee27a4e36dc720eb176181de342b36ca1e3f8e
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2024-08-11 14:25:00 +0200

    feat(ensure_rna): WIP

--- cg/constants/sequencing.py
+++ cg/constants/sequencing.py
@@ -55,6 +55,10 @@ class SequencingMethod(StrEnum):
     WGS: str = "wgs"
     WTS: str = "wts"
 
+    @property
+    def dna(self) -> set[str]:
+        return {self.WGS, self.WES, self.TGS}
+
 
 class Variants(StrEnum):
     """Type of variants."""
