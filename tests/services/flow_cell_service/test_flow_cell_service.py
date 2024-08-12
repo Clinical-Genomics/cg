e2394629dc5fa26f545ad05233cb8f7bad0b9656
seallard <sebastian.allard@scilifelab.se>
2024-08-12 09:54:46 +0200

    Formatting

--- tests/services/flow_cell_service/test_flow_cell_service.py
+++ tests/services/flow_cell_service/test_flow_cell_service.py
@@ -1,7 +1,10 @@
 from cg.services.flow_cell_service.sample_run_metrics_service import SampleRunMetricsService
 
 
-def test_get_metrics_for_flow_cell(flow_cell_service: SampleRunMetricsService, novaseq_x_flow_cell_id: str):
+def test_get_metrics_for_flow_cell(
+    flow_cell_service: SampleRunMetricsService,
+    novaseq_x_flow_cell_id: str,
+):
     # GIVEN an existing flow cell with sequencing metrics
 
     # WHEN fetching the metrics for the flow cell
