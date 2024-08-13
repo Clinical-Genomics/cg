5371dfda9870b9d11f584adf1c82aab747a428a5
Christian Oertlin <c.oertlin@gmail.com>
2024-08-13 09:51:07 +0200

    remove organism related fields

--- cg/models/orders/samples.py
+++ cg/models/orders/samples.py
@@ -271,19 +271,13 @@ class MicrobialSample(OrderInSample):
 class MicrobialFastqSample(OrderInSample):
     _suitable_project = OrderType.MICROBIAL_FASTQ
 
-    # "These fields are required"
-    organism: constr(max_length=Organism.internal_id.property.columns[0].type.length)
-    reference_genome: constr(max_length=Sample.reference_genome.property.columns[0].type.length)
     elution_buffer: str
     container: ContainerEnum
     # "Required if Plate"
     container_name: str | None
     well_position: str | None
-    # "Required if "Other" is chosen in column "Species""
-    organism_other: constr(max_length=Organism.internal_id.property.columns[0].type.length) | None
     # "These fields are not required"
     concentration_sample: float | None
-    verified_organism: bool | None  # sent to LIMS
     control: str | None
 
 
