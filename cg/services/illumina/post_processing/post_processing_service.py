f4d5d2aeb4a1de84410421a64dc8f2b3b2559265
karlnyr <kallejohan211@hotmail.com>
2024-08-13 13:56:40 +0200

    Handle EntryNotFoundError when deleting Illumina flow cell

--- cg/services/illumina/post_processing/post_processing_service.py
+++ cg/services/illumina/post_processing/post_processing_service.py
@@ -260,6 +260,6 @@ class IlluminaPostProcessingService:
         """Delete sequencing run entries from Housekeeper and StatusDB."""
         try:
             self.status_db.delete_illumina_flow_cell(flow_cell_id)
-        except ValueError:
+        except EntryNotFoundError:
             LOG.warning(f"Flow cell {flow_cell_id} not found in StatusDB.")
         delete_sequencing_data_from_housekeeper(flow_cell_id=flow_cell_id, hk_api=self.hk_api)
