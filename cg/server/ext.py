1f046afe323c35ecc82fc83ee55f6efbda03fc55
AhmedAmin-ki <ahmed.amin@scilifelab.sei>
2024-08-13 10:08:41 +0200

    rename FreshDesk client variable

--- cg/server/ext.py
+++ cg/server/ext.py
@@ -85,7 +85,7 @@ delivery_message_service = DeliveryMessageService(store=db, trailblazer_api=anal
 summary_service = OrderSummaryService(store=db, analysis_client=analysis_client)
 order_service = OrderService(store=db, status_service=summary_service)
 sample_service = SampleService(db)
-freshdesk_client = FreshdeskClient(
+ticket_client = FreshdeskClient(
     base_url=app_config.freshdesk_url,
     api_key=app_config.freshdesk_api_key,
     order_email_id=app_config.freshdesk_order_email_id,
