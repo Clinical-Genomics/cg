a487886f38a052fa0e3e029aa29f5848e9b975b7
AhmedAmin-ki <ahmed.amin@scilifelab.sei>
2024-08-13 14:50:25 +0200

    initialize ticket handler and OrdersAPI

--- cg/server/ext.py
+++ cg/server/ext.py
@@ -8,6 +8,10 @@ from flask_wtf.csrf import CSRFProtect
 from cg.apps.lims import LimsAPI
 from cg.apps.osticket import OsTicket
 from cg.apps.tb.api import TrailblazerAPI
+from cg.clients.freshdesk.freshdesk_client import FreshdeskClient
+from cg.meta.orders import OrdersAPI
+from cg.meta.orders.ticket_handler import TicketHandler
+from cg.server.app_config import app_config
 from cg.services.delivery_message.delivery_message_service import DeliveryMessageService
 from cg.services.orders.order_service.order_service import OrderService
 from cg.services.orders.order_summary_service.order_summary_service import (
@@ -16,8 +20,6 @@ from cg.services.orders.order_summary_service.order_summary_service import (
 from cg.services.sample_service.sample_service import SampleService
 from cg.store.database import initialize_database
 from cg.store.store import Store
-from cg.clients.freshdesk.freshdesk_client import FreshdeskClient
-from cg.server.app_config import app_config
 
 
 class FlaskLims(LimsAPI):
@@ -85,9 +87,18 @@ delivery_message_service = DeliveryMessageService(store=db, trailblazer_api=anal
 summary_service = OrderSummaryService(store=db, analysis_client=analysis_client)
 order_service = OrderService(store=db, status_service=summary_service)
 sample_service = SampleService(db)
-ticket_service = FreshdeskClient(
+ticket_client = FreshdeskClient(
     base_url=app_config.freshdesk_url,
     api_key=app_config.freshdesk_api_key,
     order_email_id=app_config.freshdesk_order_email_id,
     env=app_config.freshdesk_environment,
+)
+ticket_handler = TicketHandler(
+    client=ticket_client,
+    status_db=db,
+)
+orders_api = OrdersAPI(
+    lims=lims,
+    status=db,
+    ticket_handler=ticket_handler,
 )
