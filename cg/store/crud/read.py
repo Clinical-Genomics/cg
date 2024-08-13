1fc6837ceafe4d27c5aa904f1f883a8677ef7e2c
Karl Nyrén <45558267+karlnyr@users.noreply.github.com>
2024-08-13 13:38:49 +0200

    Handle EntryNotFoundError when retrieving Illumina sequencing run by device internal ID (#3556) (patch)
    ### Added
    
    - Error handler when we do not have an entry in statusdb

--- cg/store/crud/read.py
+++ cg/store/crud/read.py
@@ -383,7 +383,7 @@ class ReadHandler(BaseHandler):
         self, device_internal_id: str
     ) -> IlluminaSequencingRun:
         """Get Illumina sequencing run entry by device internal id."""
-        sequencing_run: IlluminaSequencingRun = apply_illumina_sequencing_run_filter(
+        sequencing_run: IlluminaSequencingRun | None = apply_illumina_sequencing_run_filter(
             runs=self._get_query(table=IlluminaSequencingRun),
             filter_functions=[IlluminaSequencingRunFilter.BY_DEVICE_INTERNAL_ID],
             device_internal_id=device_internal_id,
