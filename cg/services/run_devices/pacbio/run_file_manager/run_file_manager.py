33e9de11bba75faec7531af7f6e9b81f61a22566
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-08-15 17:23:52 +0200

    Commented about the mocks

--- cg/services/run_devices/pacbio/run_file_manager/run_file_manager.py
+++ cg/services/run_devices/pacbio/run_file_manager/run_file_manager.py
@@ -19,9 +19,7 @@ class PacBioRunFileManager(RunFileManager):
         """Get the file paths required by the PacBioMetricsParser."""
         run_path: Path = run_data.full_path
         validate_files_or_directories_exist([run_path])
-        files_to_parse: list[Path] = self._get_report_files(run_path)
-        files_to_parse.append(self._get_ccs_report_file(run_path))
-        return files_to_parse
+        return self._get_report_files(run_path)
 
     @handle_post_processing_errors(
         to_except=(FileNotFoundError,), to_raise=PostProcessingRunFileManagerError
@@ -29,9 +27,7 @@ class PacBioRunFileManager(RunFileManager):
     def get_files_to_store(self, run_data: PacBioRunData) -> list[Path]:
         """Get the files to store for the PostProcessingHKService."""
         run_path: Path = run_data.full_path
-        files_to_store: list[Path] = self.get_files_to_parse(run_data)
-        files_to_store.extend(self._get_hifi_read_file(run_path))
-        return files_to_store
+        return self.get_files_to_parse(run_data) + self._get_hifi_read_files(run_path)
 
     @staticmethod
     def _get_ccs_report_file(run_path: Path) -> Path:
@@ -44,8 +40,7 @@ class PacBioRunFileManager(RunFileManager):
             raise FileNotFoundError(f"No CCS report file found in {statistics_dir}")
         return files[0]
 
-    @staticmethod
-    def _get_report_files(run_path: Path) -> list[Path]:
+    def _get_report_files(self, run_path: Path) -> list[Path]:
         """Return the paths to the unzipped report files."""
         unzipped_dir: Path = Path(
             run_path, PacBioDirsAndFiles.STATISTICS_DIR, PacBioDirsAndFiles.UNZIPPED_REPORTS_DIR
@@ -55,12 +50,13 @@ class PacBioRunFileManager(RunFileManager):
             Path(unzipped_dir, PacBioDirsAndFiles.LOADING_REPORT),
             Path(unzipped_dir, PacBioDirsAndFiles.RAW_DATA_REPORT),
             Path(unzipped_dir, PacBioDirsAndFiles.SMRTLINK_DATASETS_REPORT),
+            self._get_ccs_report_file(run_path),
         ]
         validate_files_or_directories_exist(report_files)
         return report_files
 
     @staticmethod
-    def _get_hifi_read_file(run_path: Path) -> list[Path]:
+    def _get_hifi_read_files(run_path: Path) -> list[Path]:
         """Return the path to the HiFi read file."""
         hifi_dir = Path(run_path, PacBioDirsAndFiles.HIFI_READS)
         bam_files: list[Path] = get_files_matching_pattern(
