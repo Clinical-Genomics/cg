56d675ac6e7a5abf54cb20ad0616f6eb33cac35a
islean <isak.ohlsson@scilifelab.se>
2024-08-15 13:36:20 +0200

    Start change of endpoints

--- cg/server/endpoints/orders.py
+++ cg/server/endpoints/orders.py
@@ -30,7 +30,7 @@ from cg.models.orders.order import OrderIn, OrderType
 from cg.models.orders.orderform_schema import Orderform
 from cg.server.dto.delivery_message.delivery_message_response import DeliveryMessageResponse
 from cg.server.dto.orders.order_delivery_update_request import OrderDeliveredUpdateRequest
-from cg.server.dto.orders.order_patch_request import OrderDeliveredPatch
+from cg.server.dto.orders.order_patch_request import OrderDeliveredPatch, OrderOpenPatch
 from cg.server.dto.orders.orders_request import OrdersRequest
 from cg.server.dto.orders.orders_response import Order, OrdersResponse
 from cg.server.endpoints.utils import before_request
@@ -75,21 +75,21 @@ def get_order(order_id: int):
 @ORDERS_BLUEPRINT.route("/orders/<order_id>/open", methods=["PATCH"])
 def set_order_delivered(order_id: int):
     try:
-        request_data = OrderDeliveredPatch.model_validate(request.json)
-        delivered: bool = request_data.delivered
-        response_data: Order = order_service.set_open(order_id=order_id, delivered=delivered)
+        request_data = OrderOpenPatch.model_validate(request.json)
+        is_open: bool = request_data.is_open
+        response_data: Order = order_service.set_open(order_id=order_id, open=is_open)
         return jsonify(response_data.model_dump()), HTTPStatus.OK
     except OrderNotFoundError as error:
         return jsonify(error=str(error)), HTTPStatus.NOT_FOUND
 
 
-@ORDERS_BLUEPRINT.route("/orders/<order_id>/update-delivery-status", methods=["POST"])
+@ORDERS_BLUEPRINT.route("/orders/<order_id>/update-open-status", methods=["POST"])
 def update_order_delivered(order_id: int):
-    """Update the delivery status of an order based on the number of delivered analyses."""
+    """Update the openness status of an order based on the number of delivered analyses."""
     try:
         request_data = OrderDeliveredUpdateRequest.model_validate(request.json)
         delivered_analyses: int = request_data.delivered_analyses_count
-        order_service.update_delivered(order_id=order_id, delivered_analyses=delivered_analyses)
+        order_service.update_is_open(order_id=order_id, delivered_analyses=delivered_analyses)
     except OrderNotFoundError as error:
         return jsonify(error=str(error)), HTTPStatus.NOT_FOUND
 
