dcf513d990680263d36d686530c2bc77419ede8d
Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>
2024-08-16 15:33:11 +0200

    Source not required for existing samples (#3582) (patch)
    ### Fixed
    
    - Mothers are required in the same case even for existing TomteSamples

--- cg/services/order_validation_service/workflows/tomte/models/case.py
+++ cg/services/order_validation_service/workflows/tomte/models/case.py
@@ -24,4 +24,4 @@ class TomteCase(Case):
         return [(sample, index) for index, sample in self.enumerated_new_samples if sample.father]
 
     def get_samples_with_mother(self) -> list[tuple[TomteSample, int]]:
-        return [(sample, index) for index, sample in self.enumerated_new_samples if sample.mother]
+        return [(sample, index) for index, sample in self.enumerated_samples if sample.mother]
