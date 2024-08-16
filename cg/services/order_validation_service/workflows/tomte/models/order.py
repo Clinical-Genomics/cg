d475aa09daa83f9f623a233be4d307ea9ba54a76
islean <isak.ohlsson.angnell@gmail.com>
2024-08-16 15:51:37 +0200

    Remove delivery type from Case

--- cg/services/order_validation_service/workflows/tomte/models/order.py
+++ cg/services/order_validation_service/workflows/tomte/models/order.py
@@ -1,9 +1,13 @@
 from cg.services.order_validation_service.models.order import Order
+from cg.services.order_validation_service.workflows.tomte.constants import (
+    TomteDeliveryType,
+)
 from cg.services.order_validation_service.workflows.tomte.models.case import TomteCase
 
 
 class TomteOrder(Order):
     cases: list[TomteCase]
+    delivery_type: TomteDeliveryType
 
     @property
     def enumerated_cases(self) -> enumerate[TomteCase]:
