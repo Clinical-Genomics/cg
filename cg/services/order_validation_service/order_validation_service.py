3fb1a339bdbd8ab27952c1a7a9f9fea242393dc9
islean <isak.ohlsson.angnell@gmail.com>
2024-08-19 13:28:45 +0200

    Fix blueprint method name

--- cg/services/order_validation_service/order_validation_service.py
+++ cg/services/order_validation_service/order_validation_service.py
@@ -1,9 +1,7 @@
 from abc import ABC, abstractmethod
 
-from cg.services.order_validation_service.models.errors import ValidationErrors
-
 
 class OrderValidationService(ABC):
     @abstractmethod
-    def _get_errors(self, order_json: str) -> dict:
+    def validate(self, order_json: str) -> dict:
         pass
