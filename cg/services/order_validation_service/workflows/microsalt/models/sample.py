53c5633d4e716faf52b79e5f1df0dd80514a4126
seallard <sebastian.allard@scilifelab.se>
2024-08-19 14:41:35 +0200

    Remove unused field

--- cg/services/order_validation_service/workflows/microsalt/models/sample.py
+++ cg/services/order_validation_service/workflows/microsalt/models/sample.py
@@ -1,10 +1,6 @@
 from pydantic import Field
 
-from cg.models.orders.sample_base import (
-    NAME_PATTERN,
-    ControlEnum,
-    PriorityEnum,
-)
+from cg.models.orders.sample_base import ControlEnum, PriorityEnum
 from cg.services.order_validation_service.models.sample import Sample
 
 
@@ -17,4 +13,3 @@ class MicroSaltSample(Sample):
     quantity: int | None = None
     reference_genome: str | None = Field(default=None, max_length=255)
     sample_concentration: float | None = None
-    subject_id: str = Field(pattern=NAME_PATTERN, max_length=128)
