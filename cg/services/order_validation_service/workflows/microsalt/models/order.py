de0e83c59f02bf5c97bb810677a023ff9a6582c2
islean <isak.ohlsson.angnell@gmail.com>
2024-08-19 14:56:12 +0200

    Add order validation rules for microsalt

--- cg/services/order_validation_service/workflows/microsalt/models/order.py
+++ cg/services/order_validation_service/workflows/microsalt/models/order.py
@@ -1,6 +1,10 @@
 from cg.models.orders.samples import MicrosaltSample
 from cg.services.order_validation_service.models.order import Order
+from cg.services.order_validation_service.workflows.microsalt.constants import (
+    MicrosaltDeliveryType,
+)
 
 
 class MicroSaltOrder(Order):
+    delivery_type: MicrosaltDeliveryType
     samples: list[MicrosaltSample]
