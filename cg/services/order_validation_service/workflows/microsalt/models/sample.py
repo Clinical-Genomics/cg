2ec4fd91c63183e98cc7467364ed79f1099619b5
seallard <sebastian.allard@scilifelab.se>
2024-08-19 14:29:45 +0200

    Add microsalt fields

--- cg/services/order_validation_service/workflows/microsalt/models/sample.py
+++ cg/services/order_validation_service/workflows/microsalt/models/sample.py
@@ -1,13 +1,20 @@
 from pydantic import Field
 
-from cg.constants.constants import GenomeVersion
-from cg.models.orders.sample_base import NAME_PATTERN, ControlEnum, SexEnum, StatusEnum
-from cg.services.order_validation_service.constants import TissueBlockEnum
+from cg.models.orders.sample_base import (
+    NAME_PATTERN,
+    ControlEnum,
+    PriorityEnum,
+)
 from cg.services.order_validation_service.models.sample import Sample
 
 
 class MicroSaltSample(Sample):
     control: ControlEnum | None = None
     elution_buffer: str | None = None
+    extraction_method: str | None = None
+    organism: str | None = Field(default=None, max_length=32)
+    priority: PriorityEnum | None = None
+    quantity: int | None = None
+    reference_genome: str | None = Field(default=None, max_length=255)
+    sample_concentration: float | None = None
     subject_id: str = Field(pattern=NAME_PATTERN, max_length=128)
-    concentration_ng_ul: float | None = None
