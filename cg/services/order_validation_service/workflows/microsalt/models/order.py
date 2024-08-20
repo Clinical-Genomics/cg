ebe26f1dbdb91e5cae89a27ea465c1164bacfc87
Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>
2024-08-20 09:46:57 +0200

    (Improve order flow) Add sample application exists validation (#3600) (patch)
    ### Added
    
    - MicroSalt orders are ensured to have existing applications

--- cg/services/order_validation_service/workflows/microsalt/models/order.py
+++ cg/services/order_validation_service/workflows/microsalt/models/order.py
@@ -1,10 +1,24 @@
-from cg.models.orders.samples import MicrosaltSample
 from cg.services.order_validation_service.models.order import Order
 from cg.services.order_validation_service.workflows.microsalt.constants import (
-    MicrosaltDeliveryType,
+    MicroSaltDeliveryType,
+)
+from cg.services.order_validation_service.workflows.microsalt.models.sample import (
+    MicroSaltSample,
 )
 
 
 class MicroSaltOrder(Order):
-    delivery_type: MicrosaltDeliveryType
-    samples: list[MicrosaltSample]
+    delivery_type: MicroSaltDeliveryType
+    samples: list[MicroSaltSample]
+
+    @property
+    def enumerated_samples(self) -> enumerate[MicroSaltSample]:
+        return enumerate(self.samples)
+
+    @property
+    def enumerated_new_samples(self) -> list[tuple[int, MicroSaltSample]]:
+        samples: list[tuple[int, MicroSaltSample]] = []
+        for sample_index, sample in self.enumerated_samples:
+            if sample.is_new:
+                samples.append((sample_index, sample))
+        return samples
---
+++ cg/services/order_validation_service/workflows/microsalt/validation/data/rules.py
@@ -0,0 +1,18 @@
+from cg.services.order_validation_service.errors.sample_errors import (
+    ApplicationNotValidError,
+)
+from cg.services.order_validation_service.workflows.microsalt.models.order import (
+    MicroSaltOrder,
+)
+from cg.store.store import Store
+
+
+def validate_application_exists(
+    order: MicroSaltOrder, store: Store, **kwargs
+) -> list[ApplicationNotValidError]:
+    errors: list[ApplicationNotValidError] = []
+    for sample_index, sample in order.enumerated_new_samples:
+        if not store.get_application_by_tag(sample.application):
+            error = ApplicationNotValidError(sample_index=sample_index)
+            errors.append(error)
+    return errors
