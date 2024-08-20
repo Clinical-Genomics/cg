fd985258e5fbe0ea164225c3f99e40ef0f859ac4
ahdamin <ahmed.amin@scilifelab.se>
2024-08-20 13:46:42 +0200

    remove irelevant multipart data conversion

--- cg/clients/freshdesk/freshdesk_client.py
+++ cg/clients/freshdesk/freshdesk_client.py
@@ -21,39 +21,25 @@ class FreshdeskClient:
     def __init__(self, base_url: str, api_key: str, order_email_id: int):
         self.base_url = base_url
         self.api_key = api_key
-        self.order_email_id = order_email_id
         self.session = self._get_session()
 
     @handle_client_errors
-    def create_ticket(self, ticket: TicketCreate, attachments: list[Path] = None) -> TicketResponse:
+    def create_ticket(
+        self, multipart_data: List[Tuple[str, Union[str, int]]], attachments: List[Path] = None
+    ) -> TicketResponse:
         """Create a ticket with multipart form data."""
-        ticket_data = self._convert_ticket_to_multipart_data(ticket)
-        files = prepare_attachments(attachments) if attachments else None
+        files = []
+        if attachments:
+            files = [
+                ("attachments[]", (attachment.name, attachment.open("rb")))
+                for attachment in attachments
+            ]
 
-        LOG.info(ticket_data)
-        response: Response = self.session.post(
-            url=f"{self.base_url}{EndPoints.TICKETS}", data=ticket_data, files=files
+        response = self.session.post(
+            url=f"{self.base_url}{EndPoints.TICKETS}", data=multipart_data, files=files
         )
         response.raise_for_status()
         return TicketResponse.model_validate(response.json())
-
-    def _convert_ticket_to_multipart_data(
-        self, ticket: TicketCreate
-    ) -> List[Tuple[str, Union[str, int]]]:
-        """Convert the TicketCreate model into a flat dictionary for multipart form data."""
-        multipart_data = []
-
-        for field, value in ticket.model_dump(exclude_none=True).items():
-            if isinstance(value, list) and field == "tags":
-                multipart_data.extend([("tags[]", tag) for tag in value])
-            elif isinstance(value, dict) and field == "custom_fields":
-                multipart_data.extend(
-                    [(f"custom_fields[{key}]", val) for key, val in value.items()]
-                )
-            else:
-                multipart_data.append((field, value))
-
-        return multipart_data
 
     @handle_client_errors
     def _get_session(self) -> Session:
