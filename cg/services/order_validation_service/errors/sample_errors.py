d094592e28624f9d0fbebddb8ab0c0b8120e6fd1
seallard <sebastian.allard@scilifelab.se>
2024-08-20 08:56:35 +0200

    Add app compatibility valdiation for microsalt

--- cg/services/order_validation_service/errors/sample_errors.py
+++ cg/services/order_validation_service/errors/sample_errors.py
@@ -8,3 +8,8 @@ class SampleError(OrderError):
 class ApplicationNotValidError(SampleError):
     field: str = "application"
     message: str = "Chosen application does not exist"
+
+
+class ApplicationNotCompatibleError(SampleError):
+    field: str = "application"
+    message: str = "Chosen application is not compatible with workflow"
---
+++ cg/services/order_validation_service/workflows/microsalt/validation/inter_field/rules.py
@@ -0,0 +1,28 @@
+from cg.constants.constants import PrepCategory, Workflow
+from cg.services.order_validation_service.constants import WORKFLOW_PREP_CATEGORIES
+from cg.services.order_validation_service.errors.sample_errors import ApplicationNotCompatibleError
+from cg.services.order_validation_service.validators.inter_field.utils import (
+    _is_application_not_compatible,
+)
+from cg.services.order_validation_service.workflows.microsalt.models.order import MicroSaltOrder
+from cg.store.store import Store
+
+
+def validate_application_compatibility(
+    order: MicroSaltOrder,
+    store: Store,
+    **kwargs,
+) -> list[ApplicationNotCompatibleError]:
+    errors: list[ApplicationNotCompatibleError] = []
+    workflow: Workflow = order.workflow
+    allowed_prep_categories: list[PrepCategory] = WORKFLOW_PREP_CATEGORIES[workflow]
+    for sample_index, sample in order.enumerated_new_samples:
+        incompatible: bool = _is_application_not_compatible(
+            allowed_prep_categories=allowed_prep_categories,
+            application_tag=sample.application,
+            store=store,
+        )
+        if incompatible:
+            error = ApplicationNotCompatibleError(sample_index=sample_index)
+            errors.append(error)
+    return errors
