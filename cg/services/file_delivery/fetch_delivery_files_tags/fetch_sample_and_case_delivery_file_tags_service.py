63438de0d47b35011a9c4a9add263d9df62deef1
Christian Oertlin <c.oertlin@gmail.com>
2024-08-20 15:17:11 +0200

    add tag fethcing tests

--- cg/services/file_delivery/fetch_delivery_files_tags/fetch_sample_and_case_delivery_file_tags_service.py
+++ cg/services/file_delivery/fetch_delivery_files_tags/fetch_sample_and_case_delivery_file_tags_service.py
@@ -1,8 +1,6 @@
 from cg.constants import Workflow
 from cg.constants.delivery import PIPELINE_ANALYSIS_TAG_MAP
-from cg.services.file_delivery.fetch_delivery_files_tags.error_handling import (
-    handle_tag_fetch_errors,
-)
+from cg.services.file_delivery.fetch_delivery_files_tags.error_handling import handle_tag_errors
 from cg.services.file_delivery.fetch_delivery_files_tags.fetch_delivery_file_tags_service import (
     FetchDeliveryFileTagsService,
 )
@@ -11,7 +9,7 @@ from cg.services.file_delivery.fetch_delivery_files_tags.models import DeliveryF
 
 class FetchSampleAndCaseDeliveryFileTagsService(FetchDeliveryFileTagsService):
 
-    @handle_tag_fetch_errors
+    @handle_tag_errors
     def fetch_tags(self, workflow: Workflow) -> DeliveryFileTags:
         """Get the case tags for the files that need to be delivered for a workflow."""
         self._validate_workflow(workflow)
--- tests/services/file_delivery/fetch_delivery_file_tags_service/test_fetch_analysis_delivery_file_tags_service.py
+++ tests/services/file_delivery/fetch_delivery_file_tags_service/test_fetch_analysis_delivery_file_tags_service.py
@@ -2,21 +2,47 @@
 
 from cg.constants import Workflow
 from cg.constants.delivery import PIPELINE_ANALYSIS_TAG_MAP
+from cg.services.file_delivery.fetch_delivery_files_tags.exc import FetchDeliveryFileTagsError
 from cg.services.file_delivery.fetch_delivery_files_tags.fetch_sample_and_case_delivery_file_tags_service import (
     FetchSampleAndCaseDeliveryFileTagsService,
 )
 from cg.services.file_delivery.fetch_delivery_files_tags.models import DeliveryFileTags
+import pytest
 
 
-def test_fetch_tags():
+@pytest.mark.parametrize(
+    "workflow",
+    [
+        Workflow.BALSAMIC,
+        Workflow.FASTQ,
+        Workflow.MIP_DNA,
+        Workflow.MIP_RNA,
+        Workflow.BALSAMIC_QC,
+        Workflow.MICROSALT,
+        Workflow.TOMTE,
+    ],
+)
+def test_fetch_tags(workflow: Workflow):
     """Test to fetch tags for the files to deliver."""
-    # GIVEN a workflow
+    # GIVEN a workflow and a tag fetcher
+    tag_fetcher = FetchSampleAndCaseDeliveryFileTagsService()
 
     # WHEN fetching the tags for the files to deliver
-    tags: DeliveryFileTags = FetchSampleAndCaseDeliveryFileTagsService().fetch_tags(
-        Workflow.BALSAMIC
-    )
+    tags: DeliveryFileTags = tag_fetcher.fetch_tags(workflow)
 
     # THEN assert that the tags are fetched
-    assert tags.case_tags == PIPELINE_ANALYSIS_TAG_MAP[Workflow.BALSAMIC]["case_tags"]
-    assert tags.sample_tags == PIPELINE_ANALYSIS_TAG_MAP[Workflow.BALSAMIC]["sample_tags"]
+    assert tags.case_tags == PIPELINE_ANALYSIS_TAG_MAP[workflow]["case_tags"]
+    assert tags.sample_tags == PIPELINE_ANALYSIS_TAG_MAP[workflow]["sample_tags"]
+
+
+def test_fetch_tags_unsupported_workflow():
+    """Test to fetch tags for the files to deliver."""
+    # GIVEN an unsupported workflow and a tag fetcher
+    unsupported_workflow: str = "workflow_not_supported"
+    tag_fetcher = FetchSampleAndCaseDeliveryFileTagsService()
+
+    # WHEN fetching the tags for the files to deliver
+
+    # THEN a FetchDeliveryFileTagsError should be raised
+    with pytest.raises(FetchDeliveryFileTagsError):
+        tag_fetcher.fetch_tags(unsupported_workflow)
