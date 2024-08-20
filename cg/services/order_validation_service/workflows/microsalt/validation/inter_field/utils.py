7c86ba5a188a625df472f0c74d0992f06f440623
seallard <sebastian.allard@scilifelab.se>
2024-08-20 11:56:02 +0200

    Validate well is provided

--- cg/services/order_validation_service/workflows/microsalt/validation/inter_field/utils.py
+++ cg/services/order_validation_service/workflows/microsalt/validation/inter_field/utils.py
@@ -1,4 +1,5 @@
 from cg.models.orders.sample_base import ContainerEnum
+from cg.services.order_validation_service.errors.case_sample_errors import WellPositionMissingError
 from cg.services.order_validation_service.errors.sample_errors import OccupiedWellError
 from cg.services.order_validation_service.workflows.microsalt.models.order import MicrosaltOrder
 from cg.services.order_validation_service.workflows.microsalt.models.sample import MicrosaltSample
@@ -8,11 +9,13 @@ class PlateSamples:
 
     def __init__(self, order: MicrosaltOrder):
         self.wells: dict = {}
-        self._initialize_map(order)
+        self.plate_samples: list[tuple[MicrosaltSample, int]] = []
+        self._initialize_plate(order)
 
-    def _initialize_map(self, order: MicrosaltOrder):
+    def _initialize_plate(self, order: MicrosaltOrder):
         for sample_index, sample in order.enumerated_new_samples:
             if is_sample_on_plate(sample):
+                self.plate_samples.append((sample_index, sample))
                 key: tuple[str, str] = (sample.container_name, sample.well_position)
                 if not self.wells.get(key):
                     self.wells[key] = []
@@ -26,9 +29,21 @@ class PlateSamples:
                 conflicting_samples.extend(samples_indices[1:])
         return get_well_errors(conflicting_samples)
 
+    def get_well_position_missing_errors(self) -> list[OccupiedWellError]:
+        """Get errors for samples missing well positions."""
+        samples_missing_wells: list[int] = []
+        for sample, sample_index in self.plate_samples:
+            if not sample.well_position:
+                samples_missing_wells.append(sample_index)
+        return get_missing_well_errors(samples_missing_wells)
+
 
 def get_well_errors(sample_indices: list[int]) -> list[OccupiedWellError]:
     return [OccupiedWellError(sample_index=sample_index) for sample_index in sample_indices]
+
+
+def get_missing_well_errors(sample_indices: list[int]) -> list[OccupiedWellError]:
+    return [WellPositionMissingError(sample_index=sample_index) for sample_index in sample_indices]
 
 
 def is_sample_on_plate(sample: MicrosaltSample) -> bool:
