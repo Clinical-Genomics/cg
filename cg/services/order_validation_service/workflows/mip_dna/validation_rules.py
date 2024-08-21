14ddd83638de2ffe6be077298ef62bea6713c02a
seallard <sebastian.allard@scilifelab.se>
2024-08-21 16:03:24 +0200

    Add order rules

--- cg/services/order_validation_service/workflows/mip_dna/validation_rules.py
+++ cg/services/order_validation_service/workflows/mip_dna/validation_rules.py
@@ -1,4 +1,19 @@
-ORDER_RULES: list[callable] = []
+from cg.services.order_validation_service.validators.data.rules import (
+    validate_customer_can_skip_reception_control,
+    validate_customer_exists,
+    validate_user_belongs_to_customer,
+)
+from cg.services.order_validation_service.validators.inter_field.rules import (
+    validate_ticket_number_required_if_connected,
+)
+
+
+ORDER_RULES: list[callable] = [
+    validate_customer_can_skip_reception_control,
+    validate_customer_exists,
+    validate_ticket_number_required_if_connected,
+    validate_user_belongs_to_customer,
+]
 
 CASE_RULES: list[callable] = []
 
