c63ba1ac2363e9303aac0adc9b3b3e530e58c94e
ahdamin <ahmed.amin@scilifelab.se>
2024-08-21 12:55:21 +0200

    update TicketHandler FreshdeskClient  parameters

--- tests/meta/orders/conftest.py
+++ tests/meta/orders/conftest.py
@@ -133,7 +133,7 @@ def tomte_status_data(tomte_order_to_submit: dict):
 @pytest.fixture
 def freshdesk_client():
     return FreshdeskClient(
-        base_url="https://example.com", api_key="dummy_api_key", order_email_id=12345
+        base_url="https://example.com", api_key="dummy_api_key"
     )
 
 
@@ -144,4 +144,4 @@ def orders_api(base_store: Store, lims_api: MockLimsAPI, ticket_handler: TicketH
 
 @pytest.fixture
 def ticket_handler(store: Store, freshdesk_client: FreshdeskClient):
-    return TicketHandler(status_db=store, client=freshdesk_client, env="test")
+    return TicketHandler(status_db=store, client=freshdesk_client, system_email_id=1, env="test")
