dd0b7910e9da391f5a6bd738d6cd29363d40d42d
islean <isak.ohlsson.angnell@gmail.com>
2024-08-21 13:03:46 +0200

    Add extraction method validation

--- cg/services/order_validation_service/workflows/microsalt/models/sample.py
+++ cg/services/order_validation_service/workflows/microsalt/models/sample.py
@@ -1,14 +1,15 @@
 from pydantic import Field
 
 from cg.models.orders.sample_base import ControlEnum, PriorityEnum
+from cg.services.order_validation_service.constants import ExtractionMethod
 from cg.services.order_validation_service.models.sample import Sample
 
 
 class MicrosaltSample(Sample):
     control: ControlEnum | None = None
     elution_buffer: str | None = None
-    extraction_method: str | None = None
-    organism: str | None = Field(default=None, max_length=32)
+    extraction_method: ExtractionMethod | None = None
+    organism: str | None = None
     priority: PriorityEnum | None = None
     quantity: int | None = None
     reference_genome: str | None = Field(default=None, max_length=255)
