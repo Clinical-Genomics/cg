e20ccb0ad5d8e59ce2862fbf971a7389b3e21e76
Christian Oertlin <c.oertlin@gmail.com>
2024-08-27 15:54:26 +0200

    add builder test

--- cg/services/file_delivery/fetch_file_service/fetch_fastq_analysis_files_service.py
+++ cg/services/file_delivery/fetch_file_service/fetch_fastq_analysis_files_service.py
@@ -1,4 +1,7 @@
 from cg.apps.housekeeper.hk import HousekeeperAPI
+from cg.services.file_delivery.fetch_delivery_files_tags.fetch_delivery_file_tags_service import (
+    FetchDeliveryFileTagsService,
+)
 from cg.services.file_delivery.fetch_delivery_files_tags.fetch_sample_and_case_delivery_file_tags_service import (
     FetchSampleAndCaseDeliveryFileTagsService,
 )
@@ -17,30 +20,34 @@ from cg.store.store import Store
 
 
 class FetchFastqAndAnalysisDeliveryFilesService(FetchDeliveryFilesService):
-    def __init__(self, status_db: Store, hk_api: HousekeeperAPI):
+
+    def __init__(
+        self, status_db: Store, hk_api: HousekeeperAPI, tags_fetcher: FetchDeliveryFileTagsService
+    ):
         self.status_db = status_db
         self.hk_api = hk_api
+        self.tags_fetcher = tags_fetcher
 
     def get_files_to_deliver(self, case_id: str) -> DeliveryFiles:
-        case: Case = self.status_db.get_case_by_internal_id(internal_id=case_id)
-        fetch_fastq_service = FetchFastqDeliveryFilesService(
-            self.status_db,
-            self.hk_api,
-            tags_fetcher=FetchSampleAndCaseDeliveryFileTagsService(),
-        )
-        fetch_analysis_service = FetchAnalysisDeliveryFilesService(
-            self.status_db,
-            self.hk_api,
-            tags_fetcher=FetchSampleAndCaseDeliveryFileTagsService(),
-        )
+        case = self._get_case(case_id)
+        fastq_files = self._fetch_files(FetchFastqDeliveryFilesService, case_id)
+        analysis_files = self._fetch_files(FetchAnalysisDeliveryFilesService, case_id)
 
-        fastq_files: DeliveryFiles = fetch_fastq_service.get_files_to_deliver(case_id)
-        analysis_files: DeliveryFiles = fetch_analysis_service.get_files_to_deliver(case_id)
         delivery_data = DeliveryMetaData(
             customer_internal_id=case.customer.internal_id, ticket_id=case.latest_ticket
         )
+
         return DeliveryFiles(
             delivery_data=delivery_data,
             case_files=analysis_files.case_files,
             sample_files=analysis_files.sample_files + fastq_files.sample_files,
         )
+
+    def _get_case(self, case_id: str) -> Case:
+        """Fetch the case from the database."""
+        return self.status_db.get_case_by_internal_id(internal_id=case_id)
+
+    def _fetch_files(self, service_class: type, case_id: str) -> DeliveryFiles:
+        """Fetch files using the provided service class."""
+        service = service_class(self.status_db, self.hk_api, tags_fetcher=self.tags_fetcher)
+        return service.get_files_to_deliver(case_id)
