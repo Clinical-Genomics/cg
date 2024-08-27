25e8e15fdcc1591c1872e517a431fa6fc4510875
Christian Oertlin <c.oertlin@gmail.com>
2024-08-27 14:57:45 +0200

    add factory

--- cg/services/file_delivery/deliver_files_service/deliver_files_service_factory.py
+++ cg/services/file_delivery/deliver_files_service/deliver_files_service_factory.py
@@ -1,9 +1,12 @@
 """Module for the factory of the deliver files service."""
 
 from cg.apps.housekeeper.hk import HousekeeperAPI
-from cg.constants import Workflow
+from cg.constants import Workflow, DataDelivery
 from cg.services.fastq_concatenation_service.fastq_concatenation_service import (
     FastqConcatenationService,
+)
+from cg.services.file_delivery.deliver_files_service.deliver_files_service import (
+    DeliverFilesService,
 )
 from cg.services.file_delivery.fetch_delivery_files_tags.fetch_delivery_file_tags_service import (
     FetchDeliveryFileTagsService,
@@ -23,14 +26,15 @@ from cg.services.file_delivery.fetch_file_service.fetch_fastq_analysis_files_ser
 from cg.services.file_delivery.fetch_file_service.fetch_fastq_files_service import (
     FetchFastqDeliveryFilesService,
 )
-from cg.services.file_delivery.file_formatter_service.concatenation_delivery_file_formatter_service import (
-    ConcatenationDeliveryFileFormatter,
+from cg.services.file_delivery.file_formatter_service.delivery_file_formatter import (
+    DeliveryFileFormatter,
 )
+
 from cg.services.file_delivery.file_formatter_service.delivery_file_formatting_service import (
     DeliveryFileFormattingService,
 )
-from cg.services.file_delivery.file_formatter_service.generic_delivery_file_formatter_service import (
-    GenericDeliveryFileFormatter,
+from cg.services.file_delivery.file_formatter_service.utils.case_file_formatter import (
+    CaseFileFormatter,
 )
 from cg.services.file_delivery.file_formatter_service.utils.sample_file_concatenation_formatter import (
     SampleFileConcatenationFormatter,
@@ -38,53 +42,63 @@ from cg.services.file_delivery.file_formatter_service.utils.sample_file_concaten
 from cg.services.file_delivery.file_formatter_service.utils.sample_file_formatter import (
     SampleFileFormatter,
 )
+from cg.services.file_delivery.move_files_service.move_delivery_files_service import (
+    MoveDeliveryFilesService,
+)
 from cg.store.store import Store
 
 
-def get_file_tag_fetcher(delivery_type: str) -> FetchDeliveryFileTagsService:
-    """Get the file tag fetcher based on the delivery type."""
-    service_map = {
-        "fastq": FetchSampleAndCaseDeliveryFileTagsService,
-        "analysis": FetchSampleAndCaseDeliveryFileTagsService,
-        "analysis_fastq": FetchSampleAndCaseDeliveryFileTagsService,
-    }
-    return service_map[delivery_type]
+class DeliveryServiceBuilder:
+    """Class to build the delivery services based on workflow and delivery type."""
 
+    def __init__(self, store: Store, hk_api: HousekeeperAPI):
+        self.store = store
+        self.hk_api = hk_api
 
-def get_fetch_file_service(delivery_type: str) -> FetchDeliveryFilesService:
-    """Get the file tag fetcher based on the delivery type."""
-    service_map = {
-        "fastq": FetchFastqDeliveryFilesService,
-        "analysis": FetchAnalysisDeliveryFilesService,
-        "analysis_fastq": FetchFastqAndAnalysisDeliveryFilesService,
-    }
-    return service_map[delivery_type]
+    @staticmethod
+    def _get_file_tag_fetcher(delivery_type: DataDelivery) -> FetchDeliveryFileTagsService:
+        """Get the file tag fetcher based on the delivery type."""
+        service_map = {
+            DataDelivery.FASTQ: FetchSampleAndCaseDeliveryFileTagsService,
+            DataDelivery.ANALYSIS_FILES: FetchSampleAndCaseDeliveryFileTagsService,
+            DataDelivery.FASTQ_ANALYSIS: FetchSampleAndCaseDeliveryFileTagsService,
+        }
+        return service_map[delivery_type]()
 
+    def _get_file_fetcher(self, delivery_type: DataDelivery) -> FetchDeliveryFilesService:
+        """Get the file fetcher based on the delivery type."""
+        service_map = {
+            DataDelivery.FASTQ: FetchFastqDeliveryFilesService,
+            DataDelivery.ANALYSIS_FILES: FetchAnalysisDeliveryFilesService,
+            DataDelivery.FASTQ_ANALYSIS: FetchFastqAndAnalysisDeliveryFilesService,
+        }
+        file_tag_fetcher = self._get_file_tag_fetcher(delivery_type)
+        return service_map[delivery_type](
+            status_db=self.store,
+            hk_api=self.hk_api,
+            tags_fetcher=file_tag_fetcher,
+        )
 
-def get_deliver_file_formatter_service(workflow: Workflow) -> DeliveryFileFormattingService:
-    """Get the file formatter service based on the workflow."""
-    service_map = {
-        Workflow.BALSAMIC: GenericDeliveryFileFormatter,
-        Workflow.BALSAMIC_QC: GenericDeliveryFileFormatter,
-        Workflow.BALSAMIC_UMI: GenericDeliveryFileFormatter,
-        Workflow.MIP_DNA: GenericDeliveryFileFormatter,
-        Workflow.MIP_RNA: GenericDeliveryFileFormatter,
-        Workflow.FASTQ: GenericDeliveryFileFormatter,
-        Workflow.RAREDISEASE: GenericDeliveryFileFormatter,
-        Workflow.RNAFUSION: GenericDeliveryFileFormatter,
-        Workflow.TAXPROFILER: GenericDeliveryFileFormatter,
-        Workflow.TOMTE: GenericDeliveryFileFormatter,
-        Workflow.MICROSALT: ConcatenationDeliveryFileFormatter,
-        Workflow.MUTANT: ConcatenationDeliveryFileFormatter,
-    }
-    return service_map[workflow]
-
-
-def get_sample_file_formatter_service(
-    delivery_file_foramtter: DeliveryFileFormattingService,
-) -> SampleFileFormatter | SampleFileConcatenationFormatter:
-    """Get the sample file formatter service based on the delivery file formatter."""
-    if isinstance(delivery_file_foramtter, GenericDeliveryFileFormatter):
+    @staticmethod
+    def _get_sample_file_formatter(
+        workflow: Workflow,
+    ) -> SampleFileFormatter | SampleFileConcatenationFormatter:
+        """Get the file formatter service based on the workflow."""
+        if workflow in [Workflow.MICROSALT, Workflow.MUTANT]:
+            return SampleFileConcatenationFormatter(FastqConcatenationService())
         return SampleFileFormatter()
-    concatenation_service = FastqConcatenationService()
-    return SampleFileConcatenationFormatter(concatenation_service)
+
+    def build_delivery_service(
+        self, workflow: Workflow, delivery_type: DataDelivery
+    ) -> DeliverFilesService:
+        """Build a delivery service based on the workflow and delivery type."""
+        file_fetcher = self._get_file_fetcher(delivery_type)
+        sample_file_formatter = self._get_sample_file_formatter(workflow)
+        file_formatter: DeliveryFileFormattingService = DeliveryFileFormatter(
+            case_file_formatter=CaseFileFormatter(), sample_file_formatter=sample_file_formatter
+        )
+        return DeliverFilesService(
+            delivery_file_manager_service=file_fetcher,
+            move_file_service=MoveDeliveryFilesService(),
+            file_formatter_service=file_formatter,
+        )
