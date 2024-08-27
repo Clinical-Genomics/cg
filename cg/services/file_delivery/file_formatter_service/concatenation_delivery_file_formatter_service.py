db99c052fa4324cc888b6cd2bf8fed9923a49e39
Christian Oertlin <c.oertlin@gmail.com>
2024-08-27 14:11:33 +0200

    make super call

--- cg/services/file_delivery/file_formatter_service/concatenation_delivery_file_formatter_service.py
+++ cg/services/file_delivery/file_formatter_service/concatenation_delivery_file_formatter_service.py
@@ -1,22 +1,14 @@
 from pathlib import Path
-
-from cg.constants.delivery import INBOX_NAME
-from cg.services.fastq_concatenation_service.fastq_concatenation_service import (
-    FastqConcatenationService,
-)
 from cg.services.file_delivery.fetch_file_service.models import DeliveryFiles
+from cg.services.file_delivery.file_formatter_service.generic_delivery_file_formatter_service import (
+    GenericDeliveryFileFormatter,
+)
 from cg.services.file_delivery.file_formatter_service.models import FormattedFiles, FormattedFile
 from cg.services.file_delivery.file_formatter_service.utils.case_file_formatter import (
     CaseFileFormatter,
 )
-from cg.services.file_delivery.file_formatter_service.delivery_file_formatting_service import (
-    DeliveryFileFormattingService,
-)
 from cg.services.file_delivery.file_formatter_service.utils.sample_file_concatenation_formatter import (
     SampleFileConcatenationFormatter,
-)
-from cg.services.file_delivery.file_formatter_service.utils.sample_file_formatter import (
-    SampleFileFormatter,
 )
 from cg.services.file_delivery.file_formatter_service.utils.utils import (
     get_ticket_dir_path,
@@ -24,7 +16,7 @@ from cg.services.file_delivery.file_formatter_service.utils.utils import (
 )
 
 
-class ConcatenationDeliveryFileFormatter(DeliveryFileFormattingService):
+class ConcatenationDeliveryFileFormatter(GenericDeliveryFileFormatter):
     """
     Reformat the files to be delivered and concatenate fastq files.
     Expected structure:
@@ -39,23 +31,6 @@ class ConcatenationDeliveryFileFormatter(DeliveryFileFormattingService):
         case_file_formatter: CaseFileFormatter,
         sample_file_formatter: SampleFileConcatenationFormatter,
     ):
-        self.case_file_formatter = case_file_formatter
-        self.sample_file_formatter = sample_file_formatter
-
-    def format_files(self, delivery_files: DeliveryFiles) -> FormattedFiles:
-        """Format the files to be delivered and reutrn the formatted in the concatenated format."""
-        ticket_dir_path: Path = get_ticket_dir_path(delivery_files.sample_files[0].file_path)
-        create_ticket_dir(ticket_dir_path)
-        formatted_files: list[FormattedFile] = []
-        formatted_sample_files: list[FormattedFile] = self.sample_file_formatter.format_files(
-            moved_files=delivery_files.sample_files,
-            ticket_dir_path=ticket_dir_path,
+        super().__init__(
+            case_file_formatter=case_file_formatter, sample_file_formatter=sample_file_formatter
         )
-        formatted_files.extend(formatted_sample_files)
-        if delivery_files.case_files:
-            formatted_case_file: list[FormattedFile] = self.case_file_formatter.format_files(
-                moved_files=delivery_files.case_files,
-                ticket_dir_path=ticket_dir_path,
-            )
-            formatted_files.extend(formatted_case_file)
-        return FormattedFiles(files=formatted_files)
