87208e7f63fafabd5e14f8d6451e3b698a60bfe2
seallard <sebastian.allard@scilifelab.se>
2024-08-28 12:52:14 +0200

    flint my flife

--- cg/services/order_validation_service/workflows/mutant/models/order.py
+++ cg/services/order_validation_service/workflows/mutant/models/order.py
@@ -1,5 +1,6 @@
 from cg.services.order_validation_service.models.order import Order
 from cg.services.order_validation_service.workflows.mutant.models.sample import MutantSample
 
+
 class MutantOrder(Order):
     samples: list[MutantSample]
