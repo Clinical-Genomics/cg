a68f98c091a10f5703c0716e5811f3a3ce39d776
seallard <sebastian.allard@scilifelab.se>
2024-08-28 14:11:00 +0200

    Add rule

--- cg/services/order_validation_service/workflows/mutant/rules/sample/rules.py
+++ cg/services/order_validation_service/workflows/mutant/rules/sample/rules.py
@@ -1,1 +1,23 @@
-SAMPLE_RULES = []
+from cg.services.order_validation_service.rules.sample.rules import (
+    validate_application_compatibility,
+    validate_application_exists,
+    validate_applications_not_archived,
+    validate_buffer_required,
+    validate_organism_exists,
+    validate_sample_names_unique,
+    validate_volume_interval,
+    validate_well_positions_required,
+    validate_wells_contain_at_most_one_sample,
+)
+
+SAMPLE_RULES: list[callable] = [
+    validate_application_compatibility,
+    validate_application_exists,
+    validate_applications_not_archived,
+    validate_buffer_required,
+    validate_organism_exists,
+    validate_sample_names_unique,
+    validate_volume_interval,
+    validate_well_positions_required,
+    validate_wells_contain_at_most_one_sample,
+]
