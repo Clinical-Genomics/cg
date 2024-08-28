1dbef5992bc659bcb92521be273930a2856b6e29
islean <isak.ohlsson.angnell@gmail.com>
2024-08-28 13:27:09 +0200

    Add MutantSample model

--- cg/services/order_validation_service/workflows/mutant/models/sample.py
+++ cg/services/order_validation_service/workflows/mutant/models/sample.py
@@ -1,5 +1,32 @@
+from datetime import date
+
+from cg.models.orders.sample_base import ControlEnum, PriorityEnum
+from cg.services.order_validation_service.constants import (
+    ElutionBuffer,
+    ExtractionMethod,
+)
 from cg.services.order_validation_service.models.sample import Sample
+from cg.services.order_validation_service.workflows.mutant.constants import (
+    OriginalLab,
+    PreProcessingMethod,
+    Primer,
+    Region,
+    SelectionCriteria,
+)
 
 
 class MutantSample(Sample):
-    pass
+    collection_date: date
+    concentration_sample: float | None = None
+    control: ControlEnum
+    elution_buffer: ElutionBuffer
+    extraction_method: ExtractionMethod
+    organism: str
+    original_lab: OriginalLab
+    pre_processing_method: PreProcessingMethod
+    primer: Primer
+    priority: PriorityEnum
+    quantity: int | None = None
+    reference_genome: str
+    region: Region
+    selection_criteria: SelectionCriteria
