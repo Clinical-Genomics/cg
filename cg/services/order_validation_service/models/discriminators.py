8eebe886e496e4a278f1c577107bb4b37d4636e6
islean <isak.ohlsson.angnell@gmail.com>
2024-08-29 15:42:48 +0200

    Make sure internal_id None is disregarded

--- cg/services/order_validation_service/models/discriminators.py
+++ cg/services/order_validation_service/models/discriminators.py
@@ -3,5 +3,5 @@ from typing import Any
 
 def has_internal_id(v: Any) -> str:
     if isinstance(v, dict):
-        return "existing" if "internal_id" in v.keys() else "new"
+        return "existing" if v.get("internal_id") else "new"
     return "existing" if getattr(v, "internal_id", None) else "new"
