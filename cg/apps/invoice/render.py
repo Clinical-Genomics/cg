1a402c5a50f9b1e08c75d3ea10d0a8f03449e7ab
henrikstranneheim <henrik.stranneheim@scilifelab.se>
2024-09-02 08:54:22 +0200

    feat(render): Move one level up for rendering

--- cg/apps/invoice/render.py
+++ cg/apps/invoice/render.py
@@ -37,11 +37,13 @@ def render_xlsx(data: dict) -> Workbook:
             }]
         }
     """
-    project_root_dir = get_project_root_dir()
+    project_root_dir_parent = get_project_root_dir().parent
     sample_type = "pool" if data["pooled_samples"] else "sample"
     costcenter = data["cost_center"]
     template_path = Path(
-        project_root_dir, "templates", f"{costcenter}_{sample_type}_invoice{FileExtensions.XLSX}"
+        project_root_dir_parent,
+        "templates",
+        f"{costcenter}_{sample_type}_invoice{FileExtensions.XLSX}",
     )
     workbook = load_workbook(template_path.as_posix())
     if data["pooled_samples"]:
