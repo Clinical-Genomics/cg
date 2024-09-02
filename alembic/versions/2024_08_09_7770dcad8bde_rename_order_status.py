bfc916010a67c91053efe86f5f547842d210a028
islean <isak.ohlsson.angnell@gmail.com>
2024-09-02 15:40:36 +0200

    Fix revision

--- alembic/versions/2024_08_09_7770dcad8bde_rename_order_status.py
+++ alembic/versions/2024_08_09_7770dcad8bde_rename_order_status.py
@@ -6,6 +6,7 @@ Create Date: 2024-08-09 09:47:47.814700
 
 import sqlalchemy as sa
 from sqlalchemy import orm
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from alembic import op
 
@@ -16,21 +17,34 @@ branch_labels = None
 depends_on = None
 
 
+class Base(DeclarativeBase):
+    pass
+
+
+class Order(Base):
+    __tablename__ = "order"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    is_open: Mapped[str]
+
+
 def upgrade():
     op.alter_column(
         table_name="order", column_name="is_delivered", type_=sa.Boolean, new_column_name="is_open"
     )
     bind = op.get_bind()
     session = orm.Session(bind=bind)
-    for order in session.query("order").all():
+    for order in session.query(Order).all():
         order.is_open = not order.is_open
+    session.commit()
 
 
 def downgrade():
+    bind = op.get_bind()
+    session = orm.Session(bind=bind)
+    for order in session.query(Order).all():
+        order.is_open = not order.is_open
+    session.commit()
     op.alter_column(
         table_name="order", new_column_name="is_delivered", type_=sa.Boolean, column_name="is_open"
     )
-    bind = op.get_bind()
-    session = orm.Session(bind=bind)
-    for order in session.query("order").all():
-        order.is_delivered = not order.is_delivered
