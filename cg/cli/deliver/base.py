d25acab5e7bb60dfa48d445c44f1c39914ffc619
Christian Oertlin <c.oertlin@gmail.com>
2024-09-03 07:38:49 +0200

    fix delivery type add rsync

--- cg/cli/deliver/base.py
+++ cg/cli/deliver/base.py
@@ -11,6 +11,9 @@ from cg.constants.cli_options import DRY_RUN
 from cg.constants.delivery import PIPELINE_ANALYSIS_OPTIONS
 from cg.meta.rsync.rsync_api import RsyncAPI
 from cg.models.cg_config import CGConfig
+from cg.services.file_delivery.deliver_files_service.deliver_files_service import (
+    DeliverFilesService,
+)
 from cg.services.file_delivery.deliver_files_service.deliver_files_service_factory import (
     DeliveryServiceFactory,
 )
@@ -52,7 +55,6 @@ def rsync(context: CGConfig, ticket: str, dry_run: bool):
 
 
 @deliver.command(name="case")
-@DELIVERY_TYPE
 @click.option(
     "-c",
     "--case-id",
@@ -60,47 +62,64 @@ def rsync(context: CGConfig, ticket: str, dry_run: bool):
     help="Deliver the files for a specific case",
 )
 @click.pass_obj
+@DRY_RUN
 def deliver_analysis(
     context: CGConfig,
     case_id: str,
-    delivery_type: list[str],
+    dry_run: bool,
 ):
     """
     Deliver all case files based on delivery type to the customer inbox on the HPC
     """
     inbox: str = context.delivery_path
     service_builder = DeliveryServiceFactory(
-        store=context.status_db, hk_api=context.housekeeper_api
+        store=context.status_db,
+        hk_api=context.housekeeper_api,
     )
     case: Case = context.status_db.get_case_by_internal_id(internal_id=case_id)
-    for delivery in delivery_type:
-        delivery_service = service_builder.build_delivery_service(
-            delivery_type=delivery, workflow=case.workflow
-        )
-        delivery_service.deliver_files_for_case(
-            case_id=case.internal_id, delivery_base_path=Path(inbox)
-        )
+    delivery_service: DeliverFilesService = service_builder.build_delivery_service(
+        delivery_type=case.data_delivery,
+        workflow=case.workflow,
+    )
+    delivery_service.deliver_files_for_case(
+        case_id=case.internal_id, delivery_base_path=Path(inbox)
+    )
+    tb_api: TrailblazerAPI = context.trailblazer_api
+    rsync_api: RsyncAPI = RsyncAPI(config=context)
+    slurm_id: int = rsync_api.run_rsync_on_slurm(ticket=case.latest_ticket, dry_run=dry_run)
+    LOG.info(f"Rsync to the delivery server running as job {slurm_id}")
+    rsync_api.add_to_trailblazer_api(
+        tb_api=tb_api, slurm_job_id=slurm_id, ticket=case.latest_ticket, dry_run=dry_run
+    )
 
 
 @deliver.command(name="ticket")
-@DELIVERY_TYPE
 @TICKET_ID_ARG
 @click.pass_obj
+@DRY_RUN
 def deliver_ticket(
     context: CGConfig,
-    delivery_type: list[str],
     ticket: str,
+    dry_run: bool,
 ):
     """
     Deliver all case files based on delivery type to the customer inbox on the HPC for cases connected to a ticket.
     """
     inbox: str = context.delivery_path
     service_builder = DeliveryServiceFactory(
-        store=context.status_db, hk_api=context.housekeeper_api
+        store=context.status_db,
+        hk_api=context.housekeeper_api,
     )
     case: Case = context.status_db.get_cases_by_ticket_id(ticket_id=ticket)[0]
-    for delivery in delivery_type:
-        delivery_service = service_builder.build_delivery_service(
-            delivery_type=delivery, workflow=case.workflow
-        )
-        delivery_service.deliver_files_for_ticket(ticket_id=ticket, delivery_base_path=Path(inbox))
+    delivery_service: DeliverFilesService = service_builder.build_delivery_service(
+        delivery_type=case.data_delivery,
+        workflow=case.workflow,
+    )
+    delivery_service.deliver_files_for_ticket(ticket_id=ticket, delivery_base_path=Path(inbox))
+    tb_api: TrailblazerAPI = context.trailblazer_api
+    rsync_api: RsyncAPI = RsyncAPI(config=context)
+    slurm_id: int = rsync_api.run_rsync_on_slurm(ticket=ticket, dry_run=dry_run)
+    LOG.info(f"Rsync to the delivery server running as job {slurm_id}")
+    rsync_api.add_to_trailblazer_api(
+        tb_api=tb_api, slurm_job_id=slurm_id, ticket=ticket, dry_run=dry_run
+    )
