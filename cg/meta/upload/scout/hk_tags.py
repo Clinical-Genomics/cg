471e6a45f529c814051812cd745ad289babe3c5c
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-09-04 15:15:44 +0200

    add sample tag and hk tag

--- cg/meta/upload/scout/hk_tags.py
+++ cg/meta/upload/scout/hk_tags.py
@@ -72,7 +72,7 @@ class SampleTags(BaseModel):
     alignment_file: set[str] | None = None
     alignment_path: set[str] | None = None
     vcf2cytosure: set[str] | None = None
-    eklipse: set[str] | None = None
+    eklipse_path: set[str] | None = None
     mt_bam: set[str] | None = None
     chromograph_autozyg: set[str] | None = None
     chromograph_coverage: set[str] | None = None
