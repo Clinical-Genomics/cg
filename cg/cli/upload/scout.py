dc43cd026296d929cd9304048bcd6c21ac5a2934
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-09-04 20:54:32 +0200

    logging

--- cg/cli/upload/scout.py
+++ cg/cli/upload/scout.py
@@ -85,7 +85,7 @@ def create_scout_load_config(context: CGConfig, case_id: str, print_console: boo
     except SyntaxError as error:
         LOG.warning(repr(error))
         raise click.Abort from error
-    LOG.info(f"Found load config {scout_load_config}")
+    LOG.info(f"Found load config: {scout_load_config}")
     root_dir: str = context.meta_apis["upload_api"].analysis_api.root
     LOG.info(f"Set root dir to {root_dir}")
     file_path: Path = Path(root_dir, case_id, "scout_load.yaml")
--- cg/io/yaml.py
+++ cg/io/yaml.py
@@ -25,6 +25,7 @@ def write_yaml(content: Any, file_path: Path) -> None:
 
 def write_yaml_stream(content: Any) -> str:
     """Write content to a yaml stream"""
+    print(content)
     return yaml.dump(content)
 
 
