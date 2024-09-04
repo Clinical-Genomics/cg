dc462fc5fb824038592c778bc15d0e67cb8c7b41
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-09-04 22:19:28 +0200

    testing

--- cg/cli/upload/scout.py
+++ cg/cli/upload/scout.py
@@ -86,6 +86,8 @@ def create_scout_load_config(context: CGConfig, case_id: str, print_console: boo
         LOG.warning(repr(error))
         raise click.Abort from error
     LOG.info(f"Found load config: {scout_load_config}")
+    LOG.info(f"Found load config: {scout_load_config.dict()}")
+
     root_dir: str = context.meta_apis["upload_api"].analysis_api.root
     LOG.info(f"Set root dir to {root_dir}")
     file_path: Path = Path(root_dir, case_id, "scout_load.yaml")
