b5860d93ed90975e792b4f44addc37dd72c60ee0
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-09-05 11:10:44 +0200

    define enum in alembic script

--- alembic/versions/2024_09_04_18dbadd8c436_rename_fastq_to_raw_data.py
+++ alembic/versions/2024_09_04_18dbadd8c436_rename_fastq_to_raw_data.py
@@ -6,7 +6,11 @@ Create Date: 2024-09-04 13:15:11.876822
 
 """
 
-from sqlalchemy import orm
+from enum import StrEnum
+
+import sqlalchemy as sa
+from sqlalchemy import orm, types
+from sqlalchemy.dialects import mysql
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
 from alembic import op
@@ -18,6 +22,53 @@ branch_labels = None
 depends_on = None
 
 
+old_options = (
+    "balsamic",
+    "balsamic-pon",
+    "balsamic-qc",
+    "balsamic-umi",
+    "demultiplex",
+    "fluffy",
+    "jasen",
+    "microsalt",
+    "mip-dna",
+    "mip-rna",
+    "mutant",
+    "raredisease",
+    "raw-data",
+    "rnafusion",
+    "rsync",
+    "spring",
+    "taxprofiler",
+    "tomte",
+)
+new_options = sorted(old_options + ("taxprofiler",))
+
+old_enum = mysql.ENUM(*old_options)
+new_enum = mysql.ENUM(*new_options)
+
+
+class Workflow(StrEnum):
+    BALSAMIC: str = "balsamic"
+    BALSAMIC_PON: str = "balsamic-pon"
+    BALSAMIC_QC: str = "balsamic-qc"
+    BALSAMIC_UMI: str = "balsamic-umi"
+    DEMULTIPLEX: str = "demultiplex"
+    FLUFFY: str = "fluffy"
+    JASEN: str = "jasen"
+    MICROSALT: str = "microsalt"
+    MIP_DNA: str = "mip-dna"
+    MIP_RNA: str = "mip-rna"
+    MUTANT: str = "mutant"
+    RAREDISEASE: str = "raredisease"
+    RAW_DATA: str = "raw-data"
+    RNAFUSION: str = "rnafusion"
+    RSYNC: str = "rsync"
+    SPRING: str = "spring"
+    TAXPROFILER: str = "taxprofiler"
+    TOMTE: str = "tomte"
+
+
 class Base(DeclarativeBase):
     pass
 
@@ -25,14 +76,18 @@ class Base(DeclarativeBase):
 class Case(Base):
     __tablename__ = "case"
     id: Mapped[int] = mapped_column(primary_key=True)
-    data_analysis: Mapped[str | None]
+    data_analysis: Mapped[str | None] = mapped_column(
+        types.Enum(*(workflow.value for workflow in Workflow))
+    )
 
 
 class Analysis(Base):
     __tablename__ = "analysis"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    workflow: Mapped[str | None]
+    workflow: Mapped[str | None] = mapped_column(
+        types.Enum(*(workflow.value for workflow in Workflow))
+    )
 
 
 def upgrade():
@@ -41,10 +96,10 @@ def upgrade():
 
     try:
         session.query(Case).filter(Case.data_analysis == "fastq").update(
-            {"data_analysis": "raw_data"}, synchronize_session="evaluate"
+            {"data_analysis": Workflow.RAW_DATA}, synchronize_session="evaluate"
         )
         session.query(Analysis).filter(Analysis.workflow == "fastq").update(
-            {"workflow": "raw_data"}, synchronize_session="evaluate"
+            {"workflow": Workflow.RAW_DATA}, synchronize_session="evaluate"
         )
         session.commit()
     finally:
@@ -55,10 +110,10 @@ def downgrade():
     bind = op.get_bind()
     session = orm.Session(bind=bind)
     try:
-        session.query(Case).filter(Case.data_analysis == "raw_data").update(
+        session.query(Case).filter(Case.data_analysis == Workflow.RAW_DATA).update(
             {"data_analysis": "fastq"}, synchronize_session="evaluate"
         )
-        session.query(Analysis).filter(Analysis.workflow == "raw_data").update(
+        session.query(Analysis).filter(Analysis.workflow == Workflow.RAW_DATA).update(
             {"workflow": "fastq"}, synchronize_session="evaluate"
         )
         session.commit()
