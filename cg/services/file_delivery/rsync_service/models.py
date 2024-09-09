29053bc53a7d6b0cd26058802f8536b7ebc64c5e
Christian Oertlin <c.oertlin@gmail.com>
2024-09-09 13:23:39 +0200

    remove unused var

--- cg/services/file_delivery/rsync_service/models.py
+++ cg/services/file_delivery/rsync_service/models.py
@@ -5,7 +5,6 @@ class RsyncDeliveryConfig(BaseModel):
     account: str
     base_path: str
     covid_destination_path: str
-    covid_source_path: str
     covid_report_path: str
     destination_path: str
     mail_user: str
