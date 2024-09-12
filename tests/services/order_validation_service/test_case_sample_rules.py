e7d13111c1e301f003026ffe0a884c31886c2842
eliott <eliottob6@gmail.com>
2024-09-12 15:26:34 +0200

    Correct assert statement

--- tests/services/order_validation_service/test_case_sample_rules.py
+++ tests/services/order_validation_service/test_case_sample_rules.py
@@ -32,22 +32,18 @@ def test_validate_well_position_format(valid_order: OrderWithCases):
 
 
 def test_validate_tube_container_name_unique(valid_order: OrderWithCases):
-    import pdb
 
-    pdb.set_trace()
     # GIVEN an order with two samples with the same tube container name
     valid_order.cases[0].samples[0].container = ContainerEnum.tube
     valid_order.cases[0].samples[1].container = ContainerEnum.tube
     valid_order.cases[0].samples[0].container_name = "tube_name"
     valid_order.cases[0].samples[1].container_name = "tube_name"
-    breakpoint()
 
     # WHEN validating the tube container name uniqueness
     errors = validate_tube_container_name_unique(order=valid_order)
 
     # THEN an error should be returned
     assert errors
-    # breakpoint()
     # THEN the error should concern the non-unique tube container name
     assert isinstance(errors[0], ContainerNameRepeatedError)
-    assert errors[0].sample_index == 1 and errors[0].case_index == 0
+    assert errors[0].sample_index == 0 and errors[0].case_index == 0
