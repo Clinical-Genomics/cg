1b11952465b610b673d1038436f37571384c4d61
Christian Oertlin <c.oertlin@gmail.com>
2024-09-13 11:36:16 +0200

    add debug logs

--- cg/services/analysis_service/analysis_service.py
+++ cg/services/analysis_service/analysis_service.py
@@ -1,3 +1,4 @@
+import logging
 from datetime import datetime
 
 from cg.apps.tb.api import TrailblazerAPI
@@ -5,6 +6,8 @@ from cg.apps.tb.models import TrailblazerAnalysis
 from cg.constants import Workflow
 from cg.store.models import Case, Analysis
 from cg.store.store import Store
+
+LOG = logging.getLogger(__name__)
 
 
 class AnalysisService:
@@ -25,12 +28,14 @@ class AnalysisService:
         analysis_to_upload: list[Analysis] = []
         analyses: list[Analysis] = self.status_db.get_analyses_to_upload(workflow=workflow)
         for analysis in analyses:
+            LOG.debug(f"Checking if analysis {analysis.case.internal_id} should be uploaded")
             if self._has_uploaded_started(analysis) and self._is_analysis_completed(analysis):
                 self.status_db.update_analysis_uploaded_at(
                     analysis_id=analysis.id, uploaded_at=datetime.now()
                 )
             if not self._is_analysis_uploaded(analysis) and self._is_analysis_completed(analysis):
                 analysis_to_upload.append(analysis)
+                LOG.debug(f"Added analysis for {analysis.case.internal_id} to upload list.")
         return analysis_to_upload
 
     @staticmethod
