e99c0cdf0030dab6650ff437b6785d883bdeefb0
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-09-17 14:06:52 +0200

    fix iteration

--- cg/utils/get_genome_build.py
+++ cg/utils/get_genome_build.py
@@ -9,7 +9,7 @@ def get_genome_build(case: Case) -> GenomeVersion:
     """Return reference genome version for a case.
     Raises CgError if this information is missing or inconsistent for the samples linked to a case.
     """
-    reference_genome: set[str] = {sample.reference_genome for sample in case}
+    reference_genome: set[str] = {sample.reference_genome for sample in case.samples}
     if len(reference_genome) == 1:
         return reference_genome.pop()
     if len(reference_genome) > 1:
