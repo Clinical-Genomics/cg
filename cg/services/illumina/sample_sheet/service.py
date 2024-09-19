696e128da0bf51009623cd7ce1a6ff3f35ea5ad5
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-09-19 15:58:20 +0200

    add a bunch of todos

--- cg/services/illumina/sample_sheet/service.py
+++ cg/services/illumina/sample_sheet/service.py
@@ -1,14 +1,19 @@
 import logging
 from pathlib import Path
 
+from cg.apps.demultiplex.sample_sheet.utils import (
+    add_and_include_sample_sheet_path_to_housekeeper,
+    delete_sample_sheet_from_housekeeper,
+)
 from cg.apps.housekeeper.hk import HousekeeperAPI
 from cg.constants.constants import FileFormat
-from cg.io.controller import WriteFile
+from cg.io.controller import ReadFile, WriteFile
 from cg.models.run_devices.illumina_run_directory_data import IlluminaRunDirectoryData
 from cg.services.illumina.sample_sheet.creator import SampleSheetCreator
 from cg.services.illumina.sample_sheet.parser import SampleSheetParser
+from cg.services.illumina.sample_sheet.translator import SampleSheetTranslator
 from cg.services.illumina.sample_sheet.validator import SampleSheetValidator
-from cg.utils.files import get_directories_in_path
+from cg.utils.files import get_directories_in_path, link_or_overwrite_file
 
 LOG = logging.getLogger(__name__)
 
@@ -20,18 +25,25 @@ class IlluminaSampleSheetService:
         hk_api: HousekeeperAPI,
         creator: SampleSheetCreator,
         parser: SampleSheetParser,
+        translator: SampleSheetTranslator,
         validator: SampleSheetValidator,
     ):
         self.sequencing_dir = Path(sequencing_dir)
         self.hk_api = hk_api
         self.creator = creator
         self.parser = parser
+        self.translator = translator
         self.validator = validator
 
     # TODO: Add fetching from HK and FC dir
     def create(self, sequencing_run_name: str) -> None:
         """Write a sample sheet for a flow cell."""
         run_dir: IlluminaRunDirectoryData = self._get_illumina_run_dir_data(sequencing_run_name)
+        if self._use_sample_sheet_from_hk(run_dir_data=run_dir):
+            return
+        if self._use_sample_sheet_from_sequencing_dir(run_dir_data=run_dir):
+            return
+        LOG.debug(f"Creating a new sample sheet for sequencing run {sequencing_run_name}")
         sample_sheet_content: list[list[str]] = self.creator.create(run_dir).get_content()
         WriteFile.write_file_from_content(
             content=sample_sheet_content,
@@ -41,15 +53,29 @@ class IlluminaSampleSheetService:
 
     def create_all(self):
         for run_dir_path in get_directories_in_path(self.sequencing_dir):
-            LOG.info(f"Getting a valid sample sheet for flow cell {run_dir_path.name}")
+            LOG.info(f"Getting a valid sample sheet for run {run_dir_path.name}")
             try:
                 self.create(run_dir_path.name)
+                LOG.info(f"Got a valid sample sheet for run {run_dir_path.name}")
             except Exception as error:
-                LOG.error(f"Could not create sample sheet for {run_dir_path.name}: {error}")
+                LOG.error(
+                    f"Could not find or create valid sample sheet for {run_dir_path.name}: {error}"
+                )
                 continue
 
-    def translate(self):
-        pass
+    def translate(self, sequencing_run_name: str) -> None:
+        run_dir_data: IlluminaRunDirectoryData = self._get_illumina_run_dir_data(
+            sequencing_run_name
+        )
+        if not self._are_necessary_files_in_flow_cell(run_dir_data):
+            raise MissingFilesError("Missing necessary files in run directory for translation")
+        content: list[list[str]] = ReadFile.get_content_from_file(
+            file_format=FileFormat.CSV, file_path=run_dir_data.sample_sheet_path
+        )
+        if not self.validator.is_sample_sheet_bcl2fastq():
+            LOG.error("Sample sheet is already in version 2")
+            return
+        self.translator.translate_content()
 
     def validate(self, sample_sheet_path: str):
         self.validator.validate_file(file_path=Path(sample_sheet_path))
@@ -67,3 +93,42 @@ class IlluminaSampleSheetService:
             raise Exception(message)
         flow_cell = IlluminaRunDirectoryData(run_path)
         return flow_cell
+
+    # TODO add decorator to catch HousekeeperFileMissingError and validation errors, add logs and return False
+    def _use_sample_sheet_from_hk(self, run_dir_data: IlluminaRunDirectoryData) -> bool:
+        """Get the sample sheet content from Housekeeper."""
+
+        sample_sheet_path: Path = self.hk_api.get_sample_sheet_path(run_dir_data.id)
+        run_dir_data.set_sample_sheet_path_hk(sample_sheet_path)
+        self.validator.validate_file(file_path=sample_sheet_path)
+        if self.dry_run:
+            LOG.info(
+                "Sample sheet from Housekeeper is valid, "
+                "would have copied it to sequencing run directory"
+            )
+            return True
+        LOG.info("Sample sheet from Housekeeper is valid. Copying it to sequencing run directory")
+        link_or_overwrite_file(src=sample_sheet_path, dst=run_dir_data.sample_sheet_path)
+        return True
+
+    # TODO add decorator to catch HousekeeperFileMissingError and validation errors, add logs and return False
+    def _use_sample_sheet_from_sequencing_dir(self, run_dir_data: IlluminaRunDirectoryData) -> bool:
+        """Get the sample sheet content from the flow cell directory."""
+        self.validator.validate_file(run_dir_data.sample_sheet_path)
+        if self.dry_run:
+            LOG.info(
+                "Sample sheet from sequencing run directory is valid, "
+                "would have added it to Housekeeper"
+            )
+            return True
+        LOG.info("Sample sheet from sequencing run directory is valid. Adding it to Housekeeper")
+        try:
+            delete_sample_sheet_from_housekeeper(flow_cell_id=run_dir_data.id, hk_api=self.hk_api)
+        except HousekeeperFileMissingError:
+            pass
+        add_and_include_sample_sheet_path_to_housekeeper(
+            flow_cell_directory=run_dir_data.path,
+            flow_cell_name=run_dir_data.id,
+            hk_api=self.hk_api,
+        )
+        return True
--- cg/services/illumina/sample_sheet/validator.py
+++ cg/services/illumina/sample_sheet/validator.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from cg.constants.constants import FileFormat
+from cg.constants.demultiplexing import SampleSheetBcl2FastqSections
 from cg.io.controller import ReadFile
 from cg.services.illumina.sample_sheet.parser import SampleSheetParser
 
@@ -10,18 +11,29 @@ class SampleSheetValidator:
     def __init__(self, parser: SampleSheetParser) -> None:
         self.parser = parser
 
+    @staticmethod
+    def is_sample_sheet_bcl2fastq(content: list[list[str]]) -> bool:
+        """Validate that the sample sheet is not BCL2Fastq."""
+        try:
+            content.index([SampleSheetBcl2FastqSections.Data.HEADER])
+            return True
+        # TODO: Add custom exception and error message
+        except ValueError:
+            return False
+
     def validate_file(self, file_path: Path) -> None:
         """Validate a sample sheet given the path to the file."""
         content: list[list[str]] = ReadFile.get_content_from_file(
             file_format=FileFormat.CSV, file_path=file_path
         )
+        if self.is_sample_sheet_bcl2fastq(content=content):
+            raise ValueError("Sample sheet is BCL2Fastq")
         self.validate_content(content=content)
 
-    # TODO: Implement the hidden methods
     def validate_content(self, content: list[list[str]]) -> None:
         """Validate the content of a sample sheet."""
-        sample_sheet = self.parser.parse(content)
-        self._validate_header(sample_sheet.header)
-        self._validate_reads(sample_sheet.reads)
-        self._validate_settings(sample_sheet.settings)
-        self._validate_data(sample_sheet.data)
+        try:
+            self.parser.parse(content)
+        # TODO: Add custom exception and improve error message
+        except ValueError as error:
+            raise ValueError(f"Invalid sample sheet content: {error}")
