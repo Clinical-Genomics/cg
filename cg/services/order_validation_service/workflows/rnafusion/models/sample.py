930e94db28704e274465ba3274265e57fdd5672b
islean <isak.ohlsson.angnell@gmail.com>
2024-09-19 16:44:34 +0200

    Add RNAFusion models

--- cg/services/order_validation_service/workflows/rnafusion/models/sample.py
+++ cg/services/order_validation_service/workflows/rnafusion/models/sample.py
@@ -1,25 +1,20 @@
 from pydantic import Field
 
-from cg.constants.constants import GenomeVersion
-from cg.models.orders.sample_base import NAME_PATTERN, ControlEnum, SexEnum, StatusEnum
+from cg.models.orders.sample_base import NAME_PATTERN, ControlEnum, SexEnum
 from cg.services.order_validation_service.constants import TissueBlockEnum
 from cg.services.order_validation_service.models.sample import Sample
 
 
-class TomteSample(Sample):
+class RnafusionSample(Sample):
     age_at_sampling: float | None = None
     concentration_ng_ul: float | None = None
     control: ControlEnum | None
     elution_buffer: str | None = None
-    father: str | None = Field(None, pattern=NAME_PATTERN)
     formalin_fixation_time: int | None = None
-    mother: str | None = Field(None, pattern=NAME_PATTERN)
     phenotype_groups: list[str] | None = None
     phenotype_terms: list[str] | None = None
     post_formalin_fixation_time: int | None = None
-    reference_genome: GenomeVersion
     sex: SexEnum
     source: str
-    status: StatusEnum
     subject_id: str = Field(pattern=NAME_PATTERN, min_length=1, max_length=128)
     tissue_block_size: TissueBlockEnum | None = None
