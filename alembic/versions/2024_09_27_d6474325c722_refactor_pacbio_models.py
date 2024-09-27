a53c76696d11acb137eb91ee39ac379386b07747
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-09-27 16:05:00 +0200

    add downgrade

--- alembic/versions/2024_09_27_d6474325c722_refactor_pacbio_models.py
+++ alembic/versions/2024_09_27_d6474325c722_refactor_pacbio_models.py
@@ -20,41 +20,46 @@ depends_on = None
 def upgrade():
     # PacBioSequencingRun
     op.add_column(
-        table_name="pacbio_sequencing_run", column=sa.Column("barcoded_hifi_reads", sa.INTEGER)
+        table_name="pacbio_sequencing_run",
+        column=sa.Column("barcoded_hifi_reads", sa.INTEGER, nullable=True),
     )
     op.add_column(
         table_name="pacbio_sequencing_run",
-        column=sa.Column("barcoded_hifi_reads_percentage", sa.FLOAT),
-    )
-    op.add_column(
-        table_name="pacbio_sequencing_run", column=sa.Column("barcoded_hifi_yield", sa.INTEGER)
+        column=sa.Column("barcoded_hifi_reads_percentage", sa.FLOAT, nullable=True),
     )
     op.add_column(
         table_name="pacbio_sequencing_run",
-        column=sa.Column("barcoded_hifi_yield_percentage", sa.FLOAT),
+        column=sa.Column("barcoded_hifi_yield", sa.INTEGER, nullable=True),
     )
     op.add_column(
         table_name="pacbio_sequencing_run",
-        column=sa.Column("barcoded_hifi_mean_read_length", sa.INTEGER),
-    )
-    op.add_column(
-        table_name="pacbio_sequencing_run", column=sa.Column("unbarcoded_hifi_reads", sa.INTEGER)
-    )
-    op.add_column(
-        table_name="pacbio_sequencing_run", column=sa.Column("unbarcoded_hifi_yield", sa.INTEGER)
+        column=sa.Column("barcoded_hifi_yield_percentage", sa.FLOAT, nullable=True),
     )
     op.add_column(
         table_name="pacbio_sequencing_run",
-        column=sa.Column("unbarcoded_hifi_mean_read_length", sa.INTEGER),
+        column=sa.Column("barcoded_hifi_mean_read_length", sa.INTEGER, nullable=True),
+    )
+    op.add_column(
+        table_name="pacbio_sequencing_run",
+        column=sa.Column("unbarcoded_hifi_reads", sa.INTEGER, nullable=True),
+    )
+    op.add_column(
+        table_name="pacbio_sequencing_run",
+        column=sa.Column("unbarcoded_hifi_yield", sa.INTEGER, nullable=True),
+    )
+    op.add_column(
+        table_name="pacbio_sequencing_run",
+        column=sa.Column("unbarcoded_hifi_mean_read_length", sa.INTEGER, nullable=True),
     )
 
     # PacBioSampleSequencingMetrics
     op.add_column(
         table_name="pacbio_sample_run_metrics",
-        column=sa.Column("polymerase_mean_read_length", sa.INTEGER),
+        column=sa.Column("polymerase_mean_read_length", sa.INTEGER, nullable=True),
     )
     op.add_column(
-        table_name="pacbio_sample_run_metrics", column=sa.Column("barcode", sa.VARCHAR(length=32))
+        table_name="pacbio_sample_run_metrics",
+        column=sa.Column("barcode", sa.VARCHAR(length=32), nullable=True),
     )
     op.drop_column(table_name="pacbio_sample_run_metrics", column_name="percent_reads_passing_q30")
     op.drop_column(table_name="pacbio_sample_run_metrics", column_name="failed_reads")
@@ -63,4 +68,32 @@ def upgrade():
 
 
 def downgrade():
-    pass
+    # PacBioSequencingRun
+    op.drop_column(table_name="pacbio_sequencing_run", column_name="barcoded_hifi_reads")
+    op.drop_column(table_name="pacbio_sequencing_run", column_name="barcoded_hifi_reads_percentage")
+    op.drop_column(table_name="pacbio_sequencing_run", column_name="barcoded_hifi_yield")
+    op.drop_column(table_name="pacbio_sequencing_run", column_name="barcoded_hifi_yield_percentage")
+    op.drop_column(table_name="pacbio_sequencing_run", column_name="barcoded_hifi_mean_read_length")
+    op.drop_column(table_name="pacbio_sequencing_run", column_name="unbarcoded_hifi_reads")
+    op.drop_column(table_name="pacbio_sequencing_run", column_name="unbarcoded_hifi_yield")
+    op.drop_column(table_name="pacbio_sequencing_run", column_name="unbarcoded_hifi_mean_read_length")
+
+    # PacBioSampleSequencingMetrics
+    op.drop_column(table_name="pacbio_sample_run_metrics", column_name="polymerase_mean_read_length")
+    op.drop_column(table_name="pacbio_sample_run_metrics", column_name="barcode")
+    op.add_column(
+        table_name="pacbio_sample_run_metrics",
+        column=sa.Column("percent_reads_passing_q30", sa.FLOAT, nullable=False),
+    )
+    op.add_column(
+        table_name="pacbio_sample_run_metrics",
+        column=sa.Column("failed_reads", sa.INTEGER, nullable=False),
+    )
+    op.add_column(
+        table_name="pacbio_sample_run_metrics",
+        column=sa.Column("failed_yield", sa.INTEGER, nullable=False),
+    )
+    op.add_column(
+        table_name="pacbio_sample_run_metrics",
+        column=sa.Column("failed_mean_read_length", sa.INTEGER, nullable=False),
+    )
