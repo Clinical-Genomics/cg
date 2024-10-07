39ef9ab196ec599f3928826cbe1957b20a06e46b
eliott <eliottob6@gmail.com>
2024-10-07 14:03:43 +0200

    Add dockstrings to are_all_samples_control

--- cg/meta/workflow/utils/are_all_samples_control.py
+++ cg/meta/workflow/utils/are_all_samples_control.py
@@ -3,6 +3,8 @@ from cg.store.models import Case
 
 
 def are_all_samples_control(case: Case) -> bool:
+    """Check if all samples in a case are controls."""
+
     return all(
         sample.control in [ControlOptions.NEGATIVE, ControlOptions.POSITIVE]
         for sample in case.samples
