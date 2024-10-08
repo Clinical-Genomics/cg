e5424edeeba26753d3c5829bad0a9460a1b60d65
eliott <eliottob6@gmail.com>
2024-10-08 15:18:54 +0200

    Refactor test_get_slurm_qos_for_case to work with are_all_samples_control

--- tests/meta/workflow/test_analysis.py
+++ tests/meta/workflow/test_analysis.py
@@ -34,15 +34,26 @@ from tests.store_helpers import StoreHelpers
         (Priority.express, SlurmQos.EXPRESS),
     ],
 )
-def test_get_slurm_qos_for_case(mocker, case_id: str, priority, expected_slurm_qos):
+def test_get_slurm_qos_for_case(
+    store_with_non_control_samples: Store,
+    base_store: Store,
+    case_id: str,
+    priority,
+    expected_slurm_qos,
+    mip_analysis_api: MipDNAAnalysisAPI,
+):
     """Test get Quality of service (SLURM QOS) from the case priority"""
 
-    # GIVEN a case that has a priority
-    mocker.patch.object(AnalysisAPI, "get_priority_for_case")
-    AnalysisAPI.get_priority_for_case.return_value = priority
+    # GIVEN a store with a case with a specific priority
+    case: Case = base_store.get_case_by_internal_id(case_id)
+    case.priority = priority
 
-    # When getting the SLURM QOS for the priority
-    slurm_qos = AnalysisAPI.get_slurm_qos_for_case(AnalysisAPI, case_id=case_id)
+    with mock.patch.object(
+        mip_analysis_api.status_db, "get_case_by_internal_id", return_value=case
+    ):
+
+        # WHEN getting the SLURM QOS for the priority
+        slurm_qos = mip_analysis_api.get_slurm_qos_for_case(case_id=case_id)
 
     # THEN the expected slurm QOS should be returned
     assert slurm_qos is expected_slurm_qos
