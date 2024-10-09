cb402c9567a3f75babc828c6751a055d11d6a831
Christian Oertlin <c.oertlin@gmail.com>
2024-10-09 15:08:45 +0200

    fix attributes persisted to store

--- tests/services/orders/test_validate_order_service/conftest.py
+++ tests/services/orders/test_validate_order_service/conftest.py
@@ -14,7 +14,6 @@ from cg.store.store import Store
 def pacbio_sample() -> PacBioSample:
     return PacBioSample(
         application="WGSPCFC060",
-        capture_kit=None,
         data_analysis=Workflow.RAW_DATA,
         data_delivery=DataDelivery.NO_DELIVERY,
         name="PacbioSample",
@@ -24,6 +23,7 @@ def pacbio_sample() -> PacBioSample:
         buffer="buffer",
         source="source",
         subject_id="subject_id",
+        container="container",
     )
 
 
