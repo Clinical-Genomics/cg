4f9d332a56339e95e3b48372f48a2e2cfc8292c7
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-10-14 11:19:23 +0200

    fix pacbio spelling in self.instrument

--- cg/services/run_devices/pacbio/post_processing_service.py
+++ cg/services/run_devices/pacbio/post_processing_service.py
@@ -39,7 +39,7 @@ class PacBioPostProcessingService(PostProcessingService):
         self.hk_service: PacBioHousekeeperService = hk_service
         self.store_service: PacBioStoreService = store_service
         self.sequencing_dir: str = sequencing_dir
-        self.instrument: str = "PacBio"
+        self.instrument: str = "Pacbio"
 
     @handle_post_processing_errors(
         to_except=(
