8c0b4bcc976745387b15f128b1b0a8c4f31ddff8
islean <isak.ohlsson.angnell@gmail.com>
2024-10-15 10:46:49 +0200

    Add details

--- alembic/versions/2024_10_14_fd7c9d246255_add_ordertype_application_table.py
+++ alembic/versions/2024_10_14_fd7c9d246255_add_ordertype_application_table.py
@@ -39,14 +39,22 @@ class OrderTypes(Enum):
     TOMTE: str = "tomte"
 
 
-order_type_column = sa.Column("order_type", sa.Enum(OrderTypes), index=True)
+order_type_column = sa.Column("order_type", sa.Enum(OrderTypes), index=True, nullable=False)
 application_column = sa.Column(
-    "application_id", sa.Integer(), foreign_key=sa.ForeignKey("application.id")
+    "application_id",
+    sa.Integer(),
+    sa.ForeignKey("application.id", ondelete="CASCADE"),
+    nullable=False,
 )
 
 
 def upgrade():
-    op.create_table("order_type_application", order_type_column, application_column)
+    op.create_table(
+        "order_type_application",
+        order_type_column,
+        application_column,
+        sa.PrimaryKeyConstraint("order_type", "application_id"),
+    )
 
 
 def downgrade():
