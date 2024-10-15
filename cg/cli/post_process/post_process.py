49500761869af96dd03831ac7916b9163dcb2349
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-10-15 08:50:58 +0200

    fix help message

--- cg/cli/post_process/post_process.py
+++ cg/cli/post_process/post_process.py
@@ -44,7 +44,7 @@ def post_process_run(context: CGConfig, run_name: str, dry_run: bool) -> None:
 @click.option(
     "--instrument",
     type=click.Choice(["pacbio", "all"]),
-    help="The instrument for which the runs will be post-processed. Choose 'all' to post-process runs from all devices.",
+    help="The instrument for which the runs will be post-processed. Choose 'all' to post-process runs from all instruments.",
     required=True,
 )
 @click.pass_obj
