8c34afe4ae0b6a5482a8d2a131684399d16d9419
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-10-16 12:58:27 +0200

    update test with self info

--- tests/meta/upload/test_upload_genotypes_api.py
+++ tests/meta/upload/test_upload_genotypes_api.py
@@ -25,13 +25,13 @@ def test_get_analysis_sex(case_qc_metrics_deliverables: Path, genotype_analysis_
 
 
 def test_get_analysis_sex_raredisease(
-    case_qc_metrics_deliverables_raredisease: Path, genotype_analysis_sex: dict, sample_id: str
+    case_qc_metrics_deliverables_raredisease: Path, sample_id: str
 ):
     """Test to get the predicted sex from a MIP run using the upload Genotypes API"""
     # GIVEN a UploadGenotypesAPI some qcmetrics data
 
     # WHEN fetching the predicted sex by the analysis
-    sex: str = UploadGenotypesAPI._get_analysis_sex_raredisease(
+    sex: str = UploadGenotypesAPI._get_analysis_sex_raredisease(UploadGenotypesAPI,
         qc_metrics_file=case_qc_metrics_deliverables_raredisease, sample_id=sample_id
     )
 
