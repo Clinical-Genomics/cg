d294268aa1484a63bb1d19d21ef27ac048ffd6f2
Annick Renevey <47788523+rannick@users.noreply.github.com>
2024-10-16 10:27:52 +0200

    formatting

--- tests/cli/upload/test_cli_upload_genotype.py
+++ tests/cli/upload/test_cli_upload_genotype.py
@@ -9,6 +9,7 @@ from cg.cli.upload.genotype import upload_genotypes as upload_genotypes_cmd
 from cg.models.cg_config import CGConfig
 from cg.store.store import Store
 
+
 def test_upload_genotype(
     upload_context: CGConfig,
     case_id: str,
