53f9f24934435e2a0621de70214fa1ae59043d70
Christian Oertlin <c.oertlin@gmail.com>
2024-10-22 13:34:12 +0200

    add message

---
+++ cg/services/delivery_message/messages/bam_message.py
@@ -0,0 +1,14 @@
+from cg.services.delivery_message.messages.delivery_message import DeliveryMessage
+from cg.services.delivery_message.messages.utils import get_caesar_delivery_path
+from cg.store.models import Case
+
+
+class BamMessage(DeliveryMessage):
+
+    def create_message(self, cases: list[Case]) -> str:
+        delivery_path: str = get_caesar_delivery_path(cases[0])
+        return (
+            f"Hello,\n\n"
+            f"The bam files for this order have been uploaded to your inbox on Caesar at:\n\n"
+            f"{delivery_path}"
+        )
