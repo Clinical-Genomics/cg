3cb7bf852b8cf8427560b9c29073d41a7bdec6bd
Christian Oertlin <c.oertlin@gmail.com>
2024-10-30 09:14:47 +0100

    review

--- cg/cli/delete/illumina_sequencing_run.py
+++ cg/cli/delete/illumina_sequencing_run.py
@@ -7,20 +7,14 @@ from cg.services.illumina.post_processing.housekeeper_storage import (
 
 
 @click.command("illumina-run")
-@DRY_RUN
-@click.option(
-    "--flow-cell-id",
+@click.argument(
     "flow_cell_id",
     type=str,
-    multiple=False,
     help="Give the flow cell id of the run to delete from statusdb and housekeeper.",
 )
 @click.pass_context
-def delete_illumina_run(context: click.Context, dry_run: bool, flow_cell_id: str):
+def delete_illumina_run(context: click.Context, flow_cell_id: str):
     """Delete an Illumina sequencing run"""
-    if dry_run:
-        click.echo("Dry run: Would delete the Illumina sequencing run")
-        return
     delete_sequencing_data_from_housekeeper(
         flow_cell_id=flow_cell_id,
         hk_api=context.obj.housekeeper_api,
