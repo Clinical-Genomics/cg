b891340cfdbabf69c9bb784f568279046bcaaa3e
Christian Oertlin <c.oertlin@gmail.com>
2024-10-30 09:21:28 +0100

    fix

--- cg/cli/delete/illumina_sequencing_run.py
+++ cg/cli/delete/illumina_sequencing_run.py
@@ -10,11 +10,15 @@ from cg.services.illumina.post_processing.housekeeper_storage import (
 @click.argument(
     "flow_cell_id",
     type=str,
-    help="Give the flow cell id of the run to delete from statusdb and housekeeper.",
 )
 @click.pass_context
 def delete_illumina_run(context: click.Context, flow_cell_id: str):
-    """Delete an Illumina sequencing run"""
+    """
+    Delete an Illumina sequencing run.
+
+    Args:
+        flow_cell_id (str): Give the flow cell id of the run to delete from statusdb and housekeeper.
+    """
     delete_sequencing_data_from_housekeeper(
         flow_cell_id=flow_cell_id,
         hk_api=context.obj.housekeeper_api,
