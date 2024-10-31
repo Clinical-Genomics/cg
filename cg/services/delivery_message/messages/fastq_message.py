4e295496a3f6940dc5471b73d7b59421f749f48b
islean <isak.ohlsson.angnell@gmail.com>
2024-10-31 15:38:15 +0100

    Add case names to the delivery messages

--- cg/services/delivery_message/messages/fastq_message.py
+++ cg/services/delivery_message/messages/fastq_message.py
@@ -6,8 +6,12 @@ from cg.store.models import Case
 class FastqMessage(DeliveryMessage):
     def create_message(self, cases: list[Case]) -> str:
         delivery_path: str = get_caesar_delivery_path(cases[0])
+        case_names: str = "\n".join([case.name for case in cases])
+        case_s_: str = "case" if len(cases) == 1 else "cases"
         return (
             f"Hello,\n\n"
-            f"The fastq files for this order have been uploaded to your inbox on Caesar at:\n\n"
+            f"The fastq files for the following {case_s_} have been uploaded to your inbox on Caesar:\n\n"
+            f"{case_names}\n"
+            "Available under: \n\n"
             f"{delivery_path}"
         )
