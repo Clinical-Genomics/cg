0d8524f8277d3edfc42effb2d49b269dfbc22b40
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-11-05 09:48:54 +0100

    fix elution buffer

--- cg/services/order_validation_service/workflows/fastq/models/sample.py
+++ cg/services/order_validation_service/workflows/fastq/models/sample.py
@@ -1,10 +1,11 @@
 from cg.models.orders.sample_base import PriorityEnum, SexEnum
+from cg.services.order_validation_service.constants import ElutionBuffer
 from cg.services.order_validation_service.models.sample import Sample
 
 
 class FastqSample(Sample):
     concentration_ng_ul: float | None = None
-    elution_buffer: ElutionBufferEnum
+    elution_buffer: ElutionBuffer
     priority: PriorityEnum
     quantity: int | None = None
     require_qc_ok: bool
