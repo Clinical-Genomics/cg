935296f394afadf721680c374ee8b3195fe9cd1c
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-11-05 10:02:59 +0100

    add fastq delivery types

--- cg/services/order_validation_service/workflows/fastq/models/order.py
+++ cg/services/order_validation_service/workflows/fastq/models/order.py
@@ -1,8 +1,8 @@
-from cg.constants import DataDelivery
 from cg.services.order_validation_service.models.order_with_samples import OrderWithNonHumanSamples
+from cg.services.order_validation_service.workflows.fastq.constants import FastqDeliveryType
 from cg.services.order_validation_service.workflows.fastq.models.sample import FastqSample
 
 
 class FastqOrder(OrderWithNonHumanSamples):
-    delivery_type: DataDelivery.FASTQ
+    delivery_type: FastqDeliveryType
     samples: list[FastqSample]
