f1e936696162d94eae7acead5a66983a13b9500b
eliott <eliottob6@gmail.com>
2024-11-05 11:44:01 +0100

    Move MutantFileFormatter and update SampleFileFormatter

--- cg/services/deliver_files/file_formatter/utils/sample_service.py
+++ cg/services/deliver_files/file_formatter/utils/sample_service.py
@@ -1,15 +1,8 @@
 import os
 from pathlib import Path
 
-from cg.apps.lims import LimsAPI
 from cg.services.deliver_files.file_fetcher.models import SampleFile
 from cg.services.deliver_files.file_formatter.models import FormattedFile
-from cg.services.deliver_files.file_formatter.utils.sample_concatenation_service import (
-    SampleFileConcatenationFormatter,
-)
-from cg.services.fastq_concatenation_service.fastq_concatenation_service import (
-    FastqConcatenationService,
-)
 
 
 class SampleFileFormatter:
@@ -24,7 +17,9 @@ class SampleFileFormatter:
         """Format the sample files to deliver and return the formatted files."""
         sample_names: set[str] = self._get_sample_names(moved_files)
         self._create_sample_folders(ticket_dir_path=ticket_dir_path, sample_names=sample_names)
-        return self._format_sample_files(moved_files)
+        formatted_files: list[FormattedFile] = self._get_formatted_files(moved_files)
+
+        return self._format_sample_files(formatted_files)
 
     @staticmethod
     def _get_sample_names(sample_files: list[SampleFile]) -> set[str]:
@@ -36,8 +31,7 @@ class SampleFileFormatter:
             sample_dir_path = Path(ticket_dir_path, sample_name)
             sample_dir_path.mkdir(exist_ok=True)
 
-    def _format_sample_files(self, sample_files: list[SampleFile]) -> list[FormattedFile]:
-        formatted_files: list[FormattedFile] = self._get_formatted_files(sample_files)
+    def _format_sample_files(self, formatted_files: list[FormattedFile]) -> list[FormattedFile]:
         for formatted_file in formatted_files:
             os.rename(src=formatted_file.original_path, dst=formatted_file.formatted_path)
         return formatted_files
@@ -63,40 +57,4 @@ class SampleFileFormatter:
                 )
             )
         return formatted_files
-
-
-class MutantFileFormatter(SampleFileConcatenationFormatter):
-    def __init__(self, lims_api: LimsAPI, concatenation_service: FastqConcatenationService):
-        self.lims_api: LimsAPI = lims_api
-        super().__init__(concatenation_service = concatenation_service)
-    
-    def format_files(self, moved_files: list[SampleFile], ticket_dir_path: Path
-    ) -> list[FormattedFile]:
-        formatted_files: list[FormattedFile] = super().format_files(
-            moved_files=moved_files, ticket_dir_path=ticket_dir_path
-        )
-        formatted_files = self._add_lims_metadata(formatted_files)
-        return self._format_sample_files(formatted_files)
-    
-    def _get_lims_naming_metadata(self, sample_id: str)-> str:
-
-        region_code = self.lims_api.get_sample_attribute(lims_id=sample_id, key="region_code").split(" ")[0]
-        lab_code = self.lims_api.get_sample_attribute(lims_id=sample_id, key="lab_code").split(" ")[0]
-    
-        return f"{region_code}_{lab_code}"
-
-    def _add_lims_metadata(self, formatted_files: list[FormattedFile], sample_files: list[SampleFile]) -> list[FormattedFile]:
-        for formatted_file in formatted_files:
-            lims_meta_data = self._get_lims_naming_metadata(sample_id = self._get_sample_id_by_original_path(formatted_file.original_path, sample_files))
-            formatted_file.original_path = formatted_file.formatted_path
-            formatted_file.formatted_path = lims_meta_data + formatted_file.formatted_path
-        return formatted_files
-    
-    def _get_sample_id_by_original_path(original_path: Path, sample_files: list[SampleFile])-> str:
-        for sample_file in sample_files:
-            if sample_file.file_path == original_path:
-                return sample_file.sample_id
-        raise ValueError(f"Could not find sample file with path {original_path}")
-
-
 
