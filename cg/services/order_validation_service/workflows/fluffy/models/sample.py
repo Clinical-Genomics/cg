b03e3172d426e722d2ce98f6d7722a6feb333a7d
ahdamin <ahmed.amin@scilifelab.se>
2024-11-13 15:17:24 +0100

    add fluffyIndexEnum

--- cg/services/order_validation_service/workflows/fluffy/models/sample.py
+++ cg/services/order_validation_service/workflows/fluffy/models/sample.py
@@ -2,13 +2,16 @@ from pydantic import Field
 
 from cg.models.orders.sample_base import ControlEnum, PriorityEnum
 from cg.services.order_validation_service.models.sample import Sample
+from cg.services.order_validation_service.workflows.fluffy.constants import (
+    fluffyIndexEnum,
+)
 
 
 class fluffySample(Sample):
     control: ControlEnum | None = None
     priority: PriorityEnum
-    index: str
+    index: fluffyIndexEnum
     index_number: str | None
     pool: str | None
     pool_concentration: float | None
-    sample_concentration: float | None = None
+    concentration_sample: float | None = None
