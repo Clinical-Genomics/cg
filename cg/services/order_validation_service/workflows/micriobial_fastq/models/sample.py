4f57b8ac32cec9f56723c6c5d242fbd4f6fb914a
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-11-18 13:25:44 +0100

    add models and constants

---
+++ cg/services/order_validation_service/workflows/micriobial_fastq/models/sample.py
@@ -0,0 +1,17 @@
+from pydantic import Field
+
+from cg.models.orders.sample_base import NAME_PATTERN, PriorityEnum, SexEnum
+from cg.services.order_validation_service.constants import ElutionBuffer
+from cg.services.order_validation_service.models.sample import Sample
+
+
+class MicrobialFastqSample(Sample):
+    concentration_ng_ul: float | None = None
+    elution_buffer: ElutionBuffer
+    priority: PriorityEnum
+    quantity: int | None = None
+    require_qc_ok: bool
+    sex: SexEnum
+    source: str
+    subject_id: str = Field(pattern=NAME_PATTERN, max_length=128)
+    tumour: bool = False
