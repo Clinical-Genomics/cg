222d41005748b94a2acde6718dc1f7ba6b77c0dc
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-11-19 10:13:12 +0100

    patch microbial fastq sample (#3954)

--- cg/services/order_validation_service/workflows/micriobial_fastq/models/sample.py
+++ cg/services/order_validation_service/workflows/micriobial_fastq/models/sample.py
@@ -1,11 +1,12 @@
-from cg.models.orders.sample_base import PriorityEnum
+from cg.models.orders.sample_base import ControlEnum, PriorityEnum
 from cg.services.order_validation_service.constants import ElutionBuffer
 from cg.services.order_validation_service.models.sample import Sample
 
 
 class MicrobialFastqSample(Sample):
-    concentration_ng_ul: float | None = None
+    control: ControlEnum | None = None
     elution_buffer: ElutionBuffer
     priority: PriorityEnum
     quantity: int | None = None
     require_qc_ok: bool
+    volume: int
