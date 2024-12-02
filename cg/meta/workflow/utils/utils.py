92731c327514b0864d2416b8a05f1a473b73d22c
eliott <eliottob6@gmail.com>
2024-12-02 10:31:24 +0100

    Define map_to_trailblazer_priority

--- cg/meta/workflow/utils/utils.py
+++ cg/meta/workflow/utils/utils.py
@@ -1,4 +1,5 @@
 from cg.constants.constants import ControlOptions
+from cg.constants.priority import TrailblazerPriority
 from cg.store.models import Case
 
 
@@ -9,3 +10,13 @@ def are_all_samples_control(case: Case) -> bool:
         sample.control in [ControlOptions.NEGATIVE, ControlOptions.POSITIVE]
         for sample in case.samples
     )
+
+
+def map_to_trailblazer_priority(priority: int) -> TrailblazerPriority:
+    """Map a priority to a Trailblazer priority."""
+    return {
+        0: TrailblazerPriority.RESEARCH,
+        1: TrailblazerPriority.STANDARD,
+        2: TrailblazerPriority.PRIORITY,
+        3: TrailblazerPriority.EXPRESS,
+    }[priority]
