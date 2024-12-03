fb1e0336214e71ca2f53464e5aaa5dbd321b9f03
ahdamin <ahmed.amin@scilifelab.se>
2024-12-03 21:53:30 +0100

    Correct STATINA reference

--- cg/services/order_validation_service/workflows/fluffy/constants.py
+++ cg/services/order_validation_service/workflows/fluffy/constants.py
@@ -4,5 +4,5 @@ from cg.constants import DataDelivery
 
 
 class FluffyDeliveryType(Enum):
-    SCOUT = DataDelivery.STATINA
+    STATINA = DataDelivery.STATINA
     NO_DELIVERY = DataDelivery.NO_DELIVERY
