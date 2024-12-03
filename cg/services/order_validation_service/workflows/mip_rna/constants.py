0b4ad23d9141aaf9191eb1e2cda710d2bae51e50
ahdamin <ahmed.amin@scilifelab.se>
2024-12-03 14:58:14 +0100

    Use StrEnum

--- cg/services/order_validation_service/workflows/mip_rna/constants.py
+++ cg/services/order_validation_service/workflows/mip_rna/constants.py
@@ -1,9 +1,9 @@
-from enum import Enum
+from enum import StrEnum
 
 from cg.constants import DataDelivery
 
 
-class MipRnaDeliveryType(Enum):
+class MipRnaDeliveryType(StrEnum):
     ANALYSIS = DataDelivery.ANALYSIS_FILES
     ANALYSIS_SCOUT = DataDelivery.ANALYSIS_SCOUT
     SCOUT = DataDelivery.SCOUT
