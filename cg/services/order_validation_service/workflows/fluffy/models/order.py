11c2a7093000814b4d9184695ec988486dfe2a9d
ahdamin <ahmed.amin@scilifelab.se>
2024-12-03 14:37:45 +0100

    Correct class name

--- cg/services/order_validation_service/workflows/fluffy/models/order.py
+++ cg/services/order_validation_service/workflows/fluffy/models/order.py
@@ -2,17 +2,17 @@ from cg.services.order_validation_service.models.order_with_samples import (
     OrderWithSamples,
 )
 from cg.services.order_validation_service.workflows.fluffy.constants import (
-    fluffyDeliveryType,
+    FluffyDeliveryType,
 )
 from cg.services.order_validation_service.workflows.fluffy.models.sample import (
-    fluffySample,
+    FluffySample,
 )
 
 
-class fluffyOrder(OrderWithSamples):
-    delivery_type: fluffyDeliveryType
-    samples: list[fluffySample]
+class FluffyOrder(OrderWithSamples):
+    delivery_type: FluffyDeliveryType
+    samples: list[FluffySample]
 
     @property
-    def enumerated_samples(self) -> enumerate[fluffySample]:
+    def enumerated_samples(self) -> enumerate[FluffySample]:
         return enumerate(self.samples)
