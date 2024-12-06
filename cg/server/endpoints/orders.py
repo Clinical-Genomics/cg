5acb73df6aeb0c4c1d6d330a2cab9810c7ea47d2
islean <isak.ohlsson.angnell@gmail.com>
2024-12-06 10:22:24 +0100

    Add required fields

--- cg/server/endpoints/orders.py
+++ cg/server/endpoints/orders.py
@@ -175,6 +175,8 @@ def submit_order(order_type: OrderType):
                 content=request_json, file_format=FileFormat.JSON
             ),
         )
+        request_json["workflow"] = order_type
+        request_json["user_id"] = g.current_user.id
 
         result: dict = api.submit(
             raw_order=request_json,
