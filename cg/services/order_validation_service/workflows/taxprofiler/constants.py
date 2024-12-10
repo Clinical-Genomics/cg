b46087d5a78ffd20d49cfeaadd84635b2ad00e9f
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-12-10 10:27:42 +0100

    fix delivery types

--- cg/services/order_validation_service/workflows/taxprofiler/constants.py
+++ cg/services/order_validation_service/workflows/taxprofiler/constants.py
@@ -4,5 +4,6 @@ from cg.constants import DataDelivery
 
 
 class TaxprofilerDeliveryType(Enum):
-    FASTQ = DataDelivery.FASTQ
+    ANALYSIS = DataDelivery.ANALYSIS_FILES
+    FASTQ_ANALYSIS = DataDelivery.FASTQ_ANALYSIS
     NO_DELIVERY = DataDelivery.NO_DELIVERY
