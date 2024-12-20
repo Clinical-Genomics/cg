eb3ea5df702a3ff1bacceefc96928055b0a98d30
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2024-12-20 09:40:00 +0100

    refactor fastq and microbial fastq storing services (#4037)
    Refactored the existing new storing services using a new template: implementing the addition of samples, cases and order in different hidden methods within the service. This improves readability and separation of concerns

--- cg/services/orders/store_order_services/store_microbial_fastq_order_service.py
+++ cg/services/orders/store_order_services/store_microbial_fastq_order_service.py
@@ -36,7 +36,12 @@ class StoreMicrobialFastqOrderService(StoreOrderService):
 
     def store_order_data_in_status_db(self, order: MicrobialFastqOrder) -> list[Sample]:
         """
-        Store order, cases, samples and relationships in the status database. Return the samples.
+        Store all order data in the Status database for a Microbial FASTQ order. Return the samples.
+        The stored data objects are:
+        - Order
+        - Samples
+        - For each Sample, a Case
+        - For each Sample, a relationship between the Sample and its Case
         """
         db_order = self._create_db_order(order=order)
         new_samples = []
@@ -54,16 +59,25 @@ class StoreMicrobialFastqOrderService(StoreOrderService):
                 case_sample: CaseSample = self.status_db.relate_sample(
                     case=case, sample=db_sample, status=StatusEnum.unknown
                 )
-                db_order.cases.append(case)
                 self.status_db.add_multiple_items_to_store([case, db_sample, case_sample])
+                db_order.cases.append(case)
                 new_samples.append(db_sample)
         self.status_db.add_item_to_store(db_order)
         self.status_db.commit_to_store()
         return new_samples
 
+    def _create_db_order(self, order: MicrobialFastqOrder) -> Order:
+        """Return an Order database object."""
+        ticket_id: int = order._generated_ticket_id
+        customer: Customer = self.status_db.get_customer_by_internal_id(
+            customer_internal_id=order.customer
+        )
+        return self.status_db.add_order(customer=customer, ticket_id=ticket_id)
+
     def _create_db_case_for_sample(
         self, sample: MicrobialFastqSample, customer: Customer, ticket_id: str
     ) -> Case:
+        """Return a Case database object for a MicrobialFastqSample."""
         case_name: str = f"{sample.name}-case"
         case: Case = self.status_db.add_case(
             data_analysis=Workflow.RAW_DATA,
@@ -75,13 +89,6 @@ class StoreMicrobialFastqOrderService(StoreOrderService):
         case.customer = customer
         return case
 
-    def _create_db_order(self, order: MicrobialFastqOrder) -> Order:
-        ticket_id: int = order._generated_ticket_id
-        customer: Customer = self.status_db.get_customer_by_internal_id(
-            customer_internal_id=order.customer
-        )
-        return self.status_db.add_order(customer=customer, ticket_id=ticket_id)
-
     def _create_db_sample(
         self,
         sample: MicrobialFastqSample,
@@ -89,10 +96,14 @@ class StoreMicrobialFastqOrderService(StoreOrderService):
         ticket_id: str,
         customer: Customer,
     ) -> Sample:
-        """Create a sample in the status database."""
-        db_sample: Sample = self.status_db.add_sample(
+        """Return a Sample database object."""
+        application_version: ApplicationVersion = (
+            self.status_db.get_current_application_version_by_tag(tag=sample.application)
+        )
+        return self.status_db.add_sample(
             name=sample.name,
             customer=customer,
+            application_version=application_version,
             sex=SexOptions.UNKNOWN,
             comment=sample.comment,
             internal_id=sample._generated_lims_id,
@@ -101,8 +112,3 @@ class StoreMicrobialFastqOrderService(StoreOrderService):
             original_ticket=ticket_id,
             priority=sample.priority,
         )
-        application_version: ApplicationVersion = (
-            self.status_db.get_current_application_version_by_tag(tag=sample.application)
-        )
-        db_sample.application_version = application_version
-        return db_sample
