a87e79b8e4a3c2090475834a12b34a92d8b83275
islean <isak.ohlsson.angnell@gmail.com>
2025-01-03 10:39:04 +0100

    Rework metagenome storing service

--- cg/services/order_validation_service/workflows/metagenome/constants.py
+++ cg/services/order_validation_service/workflows/metagenome/constants.py
@@ -1,8 +1,8 @@
-from enum import Enum
+from enum import StrEnum
 
 from cg.constants import DataDelivery
 
 
-class MetagenomeDeliveryType(Enum):
+class MetagenomeDeliveryType(StrEnum):
     FASTQ = DataDelivery.FASTQ
     NO_DELIVERY = DataDelivery.NO_DELIVERY
