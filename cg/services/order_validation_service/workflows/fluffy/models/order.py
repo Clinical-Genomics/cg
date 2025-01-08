12d4c23b1017608ccdd0bfc0fcc15262cb005822
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-01-08 09:55:27 +0100

    add pools property to orders

--- cg/services/order_validation_service/workflows/fluffy/models/order.py
+++ cg/services/order_validation_service/workflows/fluffy/models/order.py
@@ -1,12 +1,7 @@
-from cg.services.order_validation_service.models.order_with_samples import (
-    OrderWithSamples,
-)
-from cg.services.order_validation_service.workflows.fluffy.constants import (
-    FluffyDeliveryType,
-)
-from cg.services.order_validation_service.workflows.fluffy.models.sample import (
-    FluffySample,
-)
+from cg.constants import Workflow
+from cg.services.order_validation_service.models.order_with_samples import OrderWithSamples
+from cg.services.order_validation_service.workflows.fluffy.constants import FluffyDeliveryType
+from cg.services.order_validation_service.workflows.fluffy.models.sample import FluffySample
 
 
 class FluffyOrder(OrderWithSamples):
@@ -16,3 +11,22 @@ class FluffyOrder(OrderWithSamples):
     @property
     def enumerated_samples(self) -> enumerate[FluffySample]:
         return enumerate(self.samples)
+
+    @property
+    def pools_with_samples(self) -> dict[str, dict]:
+        """Return a dictionary representation of the order pools, where the dictionary key is the
+        pool name and the values are relevant pool attributes."""
+        pools: dict[str, dict] = {}
+        for sample in self.samples:
+            if sample.pool not in pools:
+                pool: dict = {
+                    "application": sample.application,
+                    "data_analysis": Workflow.FLUFFY,
+                    "data_delivery": self.delivery_type,
+                    "priority": sample.priority,
+                    "samples": [sample],
+                }
+                pools[sample.pool] = pool
+            else:
+                pools[sample.pool]["samples"].append(sample)
+        return pools
--- cg/services/order_validation_service/workflows/rml/models/order.py
+++ cg/services/order_validation_service/workflows/rml/models/order.py
@@ -1,3 +1,4 @@
+from cg.constants import Workflow
 from cg.services.order_validation_service.models.order_with_samples import OrderWithSamples
 from cg.services.order_validation_service.workflows.rml.constants import RmlDeliveryType
 from cg.services.order_validation_service.workflows.rml.models.sample import RmlSample
@@ -10,3 +11,22 @@ class RmlOrder(OrderWithSamples):
     @property
     def enumerated_samples(self) -> enumerate[RmlSample]:
         return enumerate(self.samples)
+
+    @property
+    def pools_with_samples(self) -> dict[str, dict]:
+        """Return a dictionary representation of the order pools, where the dictionary key is the
+        pool name and the values are relevant pool attributes."""
+        pools: dict[str, dict] = {}
+        for sample in self.samples:
+            if sample.pool not in pools:
+                pool: dict = {
+                    "application": sample.application,
+                    "data_analysis": Workflow.RAW_DATA,
+                    "data_delivery": self.delivery_type,
+                    "priority": sample.priority,
+                    "samples": [sample],
+                }
+                pools[sample.pool] = pool
+            else:
+                pools[sample.pool]["samples"].append(sample)
+        return pools
