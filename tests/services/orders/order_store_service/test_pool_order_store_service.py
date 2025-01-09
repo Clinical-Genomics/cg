146c2cefbb31b9758a095467827dab634b648920
islean <isak.ohlsson.angnell@gmail.com>
2025-01-09 15:06:23 +0100

    Reenable tests

--- tests/services/orders/order_store_service/test_pool_order_store_service.py
+++ tests/services/orders/order_store_service/test_pool_order_store_service.py
@@ -1,92 +1,82 @@
-import datetime as dt
-
 import pytest
 
-from cg.constants import DataDelivery, Workflow
-from cg.models.orders.order import OrderIn, OrderType
+from cg.constants import Workflow
+from cg.constants.sequencing import SeqLibraryPrepCategory
+from cg.services.order_validation_service.models.sample_aliases import OrderWithIndexedSamples
 from cg.services.orders.store_order_services.implementations.store_pool_order import (
     StorePoolOrderService,
 )
-from cg.store.models import Case, Pool, Sample
+from cg.store.models import ApplicationVersion, Case, CaseSample, Order, Pool, Sample
 from cg.store.store import Store
+from tests.store_helpers import StoreHelpers
 
 
-@pytest.mark.xfail(reason="New validation to be implemented")
-def test_pools_to_status(
-    rml_order_to_submit: dict, store_pool_order_service: StorePoolOrderService
-):
-    # GIVEN a rml order with three samples in one pool
-    order = OrderIn.parse_obj(rml_order_to_submit, OrderType.RML)
-
-    # WHEN parsing for status
-    data = store_pool_order_service.order_to_status(order=order)
-
-    # THEN it should pick out the general information
-    assert data["customer"] == "cust000"
-    assert data["order"] == "#123456"
-    assert data["comment"] == "order comment"
-
-    # ... and information about the pool(s)
-    assert len(data["pools"]) == 2
-    pool = data["pools"][0]
-    assert pool["name"] == "pool-1"
-    assert pool["application"] == "RMLP05R800"
-    assert pool["data_analysis"] == Workflow.RAW_DATA
-    assert pool["data_delivery"] == str(DataDelivery.FASTQ)
-    assert len(pool["samples"]) == 2
-    sample = pool["samples"][0]
-    assert sample["name"] == "sample1"
-    assert sample["comment"] == "test comment"
-    assert pool["priority"] == "research"
-    assert sample["control"] == "negative"
-
-
-@pytest.mark.xfail(reason="New validation to be implemented")
-def test_store_rml(
-    base_store: Store,
-    rml_status_data: dict,
+@pytest.mark.parametrize(
+    "order_fixture, workflow",
+    [("valid_rml_order", Workflow.RAW_DATA), ("valid_fluffy_order", Workflow.FLUFFY)],
+    ids=["RML", "Fluffy"],
+)
+def test_store_order_data_in_status_db(
+    store_with_rml_applications: Store,
+    order_fixture: str,
     ticket_id: str,
     store_pool_order_service: StorePoolOrderService,
+    workflow: Workflow,
+    helpers: StoreHelpers,
+    request: pytest.FixtureRequest,
 ):
-    # GIVEN a basic store with no samples and a rml order
-    assert base_store._get_query(table=Pool).count() == 0
-    assert base_store._get_query(table=Case).count() == 0
-    assert not base_store._get_query(table=Sample).first()
+    """Test that a Fluffy or RML order is stored in the database."""
+    # GIVEN a valid Fluffy or RML order
+    order: OrderWithIndexedSamples = request.getfixturevalue(order_fixture)
+
+    # GIVEN a store with no samples, pools, cases nor orders
+    assert store_with_rml_applications._get_query(table=Sample).count() == 0
+    assert store_with_rml_applications._get_query(table=Pool).count() == 0
+    assert store_with_rml_applications._get_query(table=Case).count() == 0
+    assert store_with_rml_applications._get_query(table=CaseSample).count() == 0
+    assert store_with_rml_applications._get_query(table=Order).count() == 0
+
+    # GIVEN that the store has application versions for the RML workflow
+    apps: list[ApplicationVersion] = store_with_rml_applications._get_query(
+        table=ApplicationVersion
+    ).all()
+    assert len(apps) == 4
+    for app in apps:
+        assert app.application.prep_category == SeqLibraryPrepCategory.READY_MADE_LIBRARY
 
     # WHEN storing the order
-    new_pools = store_pool_order_service.store_items_in_status(
-        customer_id=rml_status_data["customer"],
-        order=rml_status_data["order"],
-        ordered=dt.datetime.now(),
-        ticket_id=ticket_id,
-        items=rml_status_data["pools"],
-    )
+    new_pools: list[Pool] = store_pool_order_service.store_order_data_in_status_db(order=order)
 
-    # THEN it should update the database with new pools
-    assert len(new_pools) == 2
+    # THEN it should return the pools
+    assert len(new_pools) == 4
+    assert isinstance(new_pools[0], Pool)
 
-    assert base_store._get_query(table=Pool).count() == base_store._get_query(table=Case).count()
-    assert len(base_store._get_query(table=Sample).all()) == 4
+    # THEN the pools should be stored in the database
+    db_pools: list[Pool] = store_with_rml_applications._get_query(table=Pool).all()
+    assert len(db_pools) == 4
+    assert set(new_pools) == set(db_pools)
 
-    # ASSERT that there is one negative sample
-    negative_samples = 0
-    for sample in base_store._get_query(table=Sample).all():
-        if sample.control == "negative":
-            negative_samples += 1
-    assert negative_samples == 1
+    # THEN the database pools should be invoiced, have a RML application and the correct ticket id
+    for pool in db_pools:
+        assert not pool.no_invoice
+        assert pool.application_version.application.tag.startswith("RML")
+        assert pool.ticket == ticket_id
 
-    new_pool = base_store._get_query(table=Pool).order_by(Pool.created_at.desc()).first()
-    assert new_pool == new_pools[1]
+    # THEN the order should be stored, have the correct ticket id
+    new_order = store_with_rml_applications._get_query(table=Order).first()
+    assert new_order.ticket_id == int(ticket_id)
 
-    assert new_pool.name == "pool-2"
-    assert new_pool.application_version.application.tag == "RMLP05R800"
-    assert not hasattr(new_pool, "data_analysis")
+    # THEN it should store the samples and create a case for each sample
+    new_samples: list[Sample] = store_with_rml_applications._get_query(table=Sample).all()
+    new_cases: list[Case] = store_with_rml_applications._get_query(table=Case).all()
+    assert len(new_samples) == 4
+    assert len(new_cases) == 4
+    assert store_with_rml_applications._get_query(table=CaseSample).count() == 4
 
-    new_case = base_store.get_cases()[0]
-    assert new_case.data_analysis == Workflow.RAW_DATA
-    assert new_case.data_delivery == str(DataDelivery.FASTQ)
+    # THEN the samples are not set for invoicing
+    for sample in new_samples:
+        assert sample.no_invoice
 
-    # and that the pool is set for invoicing but not the samples of the pool
-    assert not new_pool.no_invoice
-    for link in new_case.links:
-        assert link.sample.no_invoice
+    # THEN the cases should have the correct data analysis
+    for case in new_cases:
+        assert case.data_analysis == workflow
