2e4734bd6405f8c7e20ba8877cace7779aabe108
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-01-13 10:24:35 +0100

    add new mutant validation rule

---
+++ cg/services/order_validation_service/workflows/mutant/utils.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+from cg.models.orders.sample_base import ControlEnum
+from cg.services.order_validation_service.workflows.mutant.models.order import MutantOrder
+
+
+def get_indices_for_repeated_non_control_sample_names(order: MutantOrder) -> list[int]:
+    counter = Counter(
+        [sample.name for sample in order.samples if sample.control == ControlEnum.not_control]
+    )
+    indices: list[int] = []
+    for index, sample in order.enumerated_samples:
+        if counter.get(sample.name) > 1:
+            indices.append(index)
+    return indices
--- cg/services/order_validation_service/workflows/mutant/validation_rules.py
+++ cg/services/order_validation_service/workflows/mutant/validation_rules.py
@@ -5,7 +5,6 @@ from cg.services.order_validation_service.rules.sample.rules import (
     validate_container_name_required,
     validate_organism_exists,
     validate_sample_names_available,
-    validate_sample_names_unique,
     validate_tube_container_name_unique,
     validate_volume_interval,
     validate_volume_required,
@@ -13,16 +12,19 @@ from cg.services.order_validation_service.rules.sample.rules import (
     validate_well_positions_required,
     validate_wells_contain_at_most_one_sample,
 )
+from cg.services.order_validation_service.workflows.mutant.rules import (
+    validate_non_control_samples_are_unique,
+)
 
 MUTANT_SAMPLE_RULES: list[callable] = [
     validate_application_compatibility,
     validate_application_exists,
     validate_applications_not_archived,
     validate_container_name_required,
+    validate_non_control_samples_are_unique,
     validate_organism_exists,
     validate_volume_required,
     validate_sample_names_available,
-    validate_sample_names_unique,
     validate_tube_container_name_unique,
     validate_volume_interval,
     validate_well_position_format,
