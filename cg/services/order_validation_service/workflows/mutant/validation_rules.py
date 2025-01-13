6bfd230686c761257f58b52a6536be1d95706659
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-01-13 16:15:05 +0100

    add modified version of names available for control samples

--- cg/services/order_validation_service/workflows/mutant/validation_rules.py
+++ cg/services/order_validation_service/workflows/mutant/validation_rules.py
@@ -3,8 +3,8 @@ from cg.services.order_validation_service.rules.sample.rules import (
     validate_application_exists,
     validate_applications_not_archived,
     validate_container_name_required,
+    validate_non_control_sample_names_available,
     validate_organism_exists,
-    validate_sample_names_available,
     validate_sample_names_unique,
     validate_tube_container_name_unique,
     validate_volume_interval,
@@ -13,19 +13,15 @@ from cg.services.order_validation_service.rules.sample.rules import (
     validate_well_positions_required,
     validate_wells_contain_at_most_one_sample,
 )
-from cg.services.order_validation_service.workflows.mutant.rules import (
-    validate_non_control_samples_are_unique,
-)
 
 MUTANT_SAMPLE_RULES: list[callable] = [
     validate_application_compatibility,
     validate_application_exists,
     validate_applications_not_archived,
     validate_container_name_required,
-    validate_non_control_samples_are_unique,
+    validate_non_control_sample_names_available,
     validate_organism_exists,
     validate_volume_required,
-    validate_sample_names_available,
     validate_sample_names_unique,
     validate_tube_container_name_unique,
     validate_volume_interval,
