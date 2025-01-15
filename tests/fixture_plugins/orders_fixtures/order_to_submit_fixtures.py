605ef457e4e1328a187ce89ff065c02c521c0b8a
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-01-15 11:37:21 +0100

    remove pool old validation

--- tests/fixture_plugins/orders_fixtures/order_to_submit_fixtures.py
+++ tests/fixture_plugins/orders_fixtures/order_to_submit_fixtures.py
@@ -10,10 +10,12 @@ from cg.models.orders.constants import OrderType
 from cg.services.order_validation_service.models.order import Order
 from cg.services.order_validation_service.workflows.balsamic.models.order import BalsamicOrder
 from cg.services.order_validation_service.workflows.fastq.models.order import FastqOrder
+from cg.services.order_validation_service.workflows.fluffy.models.order import FluffyOrder
 from cg.services.order_validation_service.workflows.metagenome.models.order import MetagenomeOrder
 from cg.services.order_validation_service.workflows.mip_dna.models.order import MipDnaOrder
 from cg.services.order_validation_service.workflows.mip_rna.models.order import MipRnaOrder
 from cg.services.order_validation_service.workflows.pacbio_long_read.models.order import PacbioOrder
+from cg.services.order_validation_service.workflows.rml.models.order import RmlOrder
 
 
 @pytest.fixture(scope="session")
@@ -138,7 +140,7 @@ def all_orders_to_submit(
     return {
         OrderType.BALSAMIC: BalsamicOrder.model_validate(balsamic_order_to_submit),
         OrderType.FASTQ: FastqOrder.model_validate(fastq_order_to_submit),
-        # OrderType.FLUFFY: FluffyOrder.model_validate(rml_order_to_submit),
+        OrderType.FLUFFY: FluffyOrder.model_validate(fluffy_order_to_submit),
         OrderType.METAGENOME: MetagenomeOrder.model_validate(metagenome_order_to_submit),
         # OrderType.MICROSALT: OrderIn.parse_obj(
         #    microbial_order_to_submit, project=OrderType.MICROSALT
@@ -146,7 +148,7 @@ def all_orders_to_submit(
         OrderType.MIP_DNA: MipDnaOrder.model_validate(mip_dna_order_to_submit),
         OrderType.MIP_RNA: MipRnaOrder.model_validate(mip_rna_order_to_submit),
         OrderType.PACBIO_LONG_READ: PacbioOrder.model_validate(pacbio_order_to_submit),
-        # OrderType.RML: OrderIn.parse_obj(rml_order_to_submit, project=OrderType.RML),
+        OrderType.RML: RmlOrder.model_validate(rml_order_to_submit),
         # OrderType.RNAFUSION: RnaFusionOrder.model_validate(rnafusion_order_to_submit),
         # OrderType.SARS_COV_2: OrderIn.parse_obj(
         #    sarscov2_order_to_submit, project=OrderType.SARS_COV_2
