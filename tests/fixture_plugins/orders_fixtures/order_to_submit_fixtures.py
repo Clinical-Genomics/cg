06bdd39eec01dfe5e250a179b119ce490146c44b
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-01-15 09:36:45 +0100

    delete old validation

--- tests/fixture_plugins/orders_fixtures/order_to_submit_fixtures.py
+++ tests/fixture_plugins/orders_fixtures/order_to_submit_fixtures.py
@@ -12,6 +12,7 @@ from cg.services.order_validation_service.workflows.balsamic.models.order import
 from cg.services.order_validation_service.workflows.fastq.models.order import FastqOrder
 from cg.services.order_validation_service.workflows.mip_dna.models.order import MipDnaOrder
 from cg.services.order_validation_service.workflows.mip_rna.models.order import MipRnaOrder
+from cg.services.order_validation_service.workflows.pacbio_long_read.models.order import PacbioOrder
 
 
 @pytest.fixture(scope="session")
@@ -145,9 +146,7 @@ def all_orders_to_submit(
         # ),
         OrderType.MIP_DNA: MipDnaOrder.model_validate(mip_dna_order_to_submit),
         OrderType.MIP_RNA: MipRnaOrder.model_validate(mip_rna_order_to_submit),
-        OrderType.PACBIO_LONG_READ: OrderIn.parse_obj(
-            pacbio_order_to_submit, project=OrderType.PACBIO_LONG_READ
-        ),
+        OrderType.PACBIO_LONG_READ: PacbioOrder.model_validate(pacbio_order_to_submit),
         # OrderType.RML: OrderIn.parse_obj(rml_order_to_submit, project=OrderType.RML),
         # OrderType.RNAFUSION: RnaFusionOrder.model_validate(rnafusion_order_to_submit),
         # OrderType.SARS_COV_2: OrderIn.parse_obj(
