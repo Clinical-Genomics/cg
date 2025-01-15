3a59e4c95708e4edcb83cb3145415dc7b36256d7
Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>
2025-01-15 10:43:10 +0100

    (Improve order flow) Remove old microbial validation (#4090) (patch)
    ### Fixed
    
    - Removed old microbial validation service

--- tests/fixture_plugins/orders_fixtures/order_to_submit_fixtures.py
+++ tests/fixture_plugins/orders_fixtures/order_to_submit_fixtures.py
@@ -11,8 +11,13 @@ from cg.services.order_validation_service.models.order import Order
 from cg.services.order_validation_service.workflows.balsamic.models.order import BalsamicOrder
 from cg.services.order_validation_service.workflows.fastq.models.order import FastqOrder
 from cg.services.order_validation_service.workflows.metagenome.models.order import MetagenomeOrder
+from cg.services.order_validation_service.workflows.microbial_fastq.models.order import (
+    MicrobialFastqOrder,
+)
+from cg.services.order_validation_service.workflows.microsalt.models.order import MicrosaltOrder
 from cg.services.order_validation_service.workflows.mip_dna.models.order import MipDnaOrder
 from cg.services.order_validation_service.workflows.mip_rna.models.order import MipRnaOrder
+from cg.services.order_validation_service.workflows.mutant.models.order import MutantOrder
 from cg.services.order_validation_service.workflows.pacbio_long_read.models.order import PacbioOrder
 
 
@@ -127,6 +132,7 @@ def all_orders_to_submit(
     fluffy_order_to_submit: dict,
     metagenome_order_to_submit: dict,
     microbial_order_to_submit: dict,
+    microbial_fastq_order_to_submit: dict,
     mip_dna_order_to_submit: dict,
     mip_rna_order_to_submit: dict,
     pacbio_order_to_submit: dict,
@@ -140,15 +146,14 @@ def all_orders_to_submit(
         OrderType.FASTQ: FastqOrder.model_validate(fastq_order_to_submit),
         # OrderType.FLUFFY: FluffyOrder.model_validate(rml_order_to_submit),
         OrderType.METAGENOME: MetagenomeOrder.model_validate(metagenome_order_to_submit),
-        # OrderType.MICROSALT: OrderIn.parse_obj(
-        #    microbial_order_to_submit, project=OrderType.MICROSALT
-        # ),
+        OrderType.MICROBIAL_FASTQ: MicrobialFastqOrder.model_validate(
+            microbial_fastq_order_to_submit
+        ),
+        OrderType.MICROSALT: MicrosaltOrder.model_validate(microbial_order_to_submit),
         OrderType.MIP_DNA: MipDnaOrder.model_validate(mip_dna_order_to_submit),
         OrderType.MIP_RNA: MipRnaOrder.model_validate(mip_rna_order_to_submit),
         OrderType.PACBIO_LONG_READ: PacbioOrder.model_validate(pacbio_order_to_submit),
         # OrderType.RML: OrderIn.parse_obj(rml_order_to_submit, project=OrderType.RML),
         # OrderType.RNAFUSION: RnaFusionOrder.model_validate(rnafusion_order_to_submit),
-        # OrderType.SARS_COV_2: OrderIn.parse_obj(
-        #    sarscov2_order_to_submit, project=OrderType.SARS_COV_2
-        # ),
+        OrderType.SARS_COV_2: MutantOrder.model_validate(sarscov2_order_to_submit),
     }
