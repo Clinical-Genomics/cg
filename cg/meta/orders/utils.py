2f0dfa60c8498b6c487a384f0b5136a743ddbe79
eliott <eliottob6@gmail.com>
2025-01-20 11:45:51 +0100

    Add utility functions for order data validation and ticket tag generation

---
+++ cg/meta/orders/utils.py
@@ -0,0 +1,26 @@
+from cg.models.orders.constants import OrderType
+from cg.services.orders.constants import ORDER_TYPE_WORKFLOW_MAP
+from cg.services.orders.validation.models.case import Case
+from cg.services.orders.validation.models.order import Order
+from cg.services.orders.validation.models.order_with_cases import OrderWithCases
+
+
+def is_order_containing_existing_data(order: OrderWithCases) -> bool:
+    """Check if the order contains any existing data"""
+
+    for enumerated_case in order.enumerated_cases:
+        case: Case = enumerated_case[1]
+        if case.enumerated_existing_samples:
+            return True
+    return False
+
+
+def get_ticket_tags(order: Order, order_type: OrderType) -> list[str]:
+    """Generate ticket tags based on the order and order type"""
+    tags: list[str] = []
+    tags.append([ORDER_TYPE_WORKFLOW_MAP[order_type]])
+
+    if is_order_containing_existing_data(order=order):
+        tags.append("existing-data")
+
+    return tags
