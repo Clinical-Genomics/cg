6aa6eedc086d73853119275091ce503a2aded917
Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>
2025-01-21 11:12:50 +0100

    Make panels optional for existing cases (#4131) (patch)
    ### Fixed
    
    - Panels are optional for existing cases.

--- cg/services/orders/validation/models/existing_case.py
+++ cg/services/orders/validation/models/existing_case.py
@@ -3,7 +3,7 @@ from pydantic import BaseModel
 
 class ExistingCase(BaseModel):
     internal_id: str
-    panels: list[str]
+    panels: list[str] | None = None
 
     @property
     def is_new(self) -> bool:
