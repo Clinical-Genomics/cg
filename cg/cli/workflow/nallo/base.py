67169de33a0c87d7b997614f8c20803e333c66aa
Annick Renevey <47788523+rannick@users.noreply.github.com>
2025-01-30 09:48:54 +0100

    add report-deliver

--- cg/cli/workflow/nallo/base.py
+++ cg/cli/workflow/nallo/base.py
@@ -5,9 +5,7 @@ import logging
 import rich_click as click
 
 from cg.cli.utils import CLICK_CONTEXT_SETTINGS
-
-from cg.cli.workflow.nf_analysis import config_case, run, start
-
+from cg.cli.workflow.nf_analysis import config_case, report_deliver, run, start
 from cg.constants.constants import MetaApis
 from cg.meta.workflow.analysis import AnalysisAPI
 from cg.meta.workflow.nallo import NalloAnalysisAPI
@@ -24,5 +22,6 @@ def nallo(context: click.Context) -> None:
 
 
 nallo.add_command(config_case)
+nallo.add_command(report_deliver)
 nallo.add_command(run)
 nallo.add_command(start)
