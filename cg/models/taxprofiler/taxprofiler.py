d1601e78660c89c0ea3c586f1d64c194c7af8504
Sofia Stamouli <91951607+sofstam@users.noreply.github.com>
2025-02-03 12:42:19 +0100

    Add parameters to the nextflow config (#3853)(minor)
    ### Added
    Parameters to the nextflow config instead of the params file.

--- cg/models/taxprofiler/taxprofiler.py
+++ cg/models/taxprofiler/taxprofiler.py
@@ -21,29 +21,7 @@ class TaxprofilerQCMetrics(QCMetrics):
 
 
 class TaxprofilerParameters(WorkflowParameters):
-    """Model for Taxprofiler parameters."""
-
-    databases: Path
-    save_preprocessed_reads: bool = True
-    perform_shortread_qc: bool = True
-    perform_shortread_complexityfilter: bool = True
-    save_complexityfiltered_reads: bool = True
-    perform_shortread_hostremoval: bool = True
-    hostremoval_reference: Path
-    save_hostremoval_index: bool = True
-    save_hostremoval_mapped: bool = True
-    save_hostremoval_unmapped: bool = True
-    perform_runmerging: bool = True
-    run_kraken2: bool = True
-    kraken2_save_reads: bool = True
-    kraken2_save_readclassifications: bool = True
-    run_bracken: bool = True
-    run_centrifuge: bool = True
-    centrifuge_save_reads: bool = True
-    run_krona: bool = True
-    run_profile_standardisation: bool = True
-    clusterOptions: str = Field(alias="cluster_options")
-    priority: str
+    """Taxprofiler parameters."""
 
 
 class TaxprofilerSampleSheetEntry(NextflowSampleSheetEntry):
