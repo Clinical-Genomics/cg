2b965ae7122241e8c4391c5cf80818bf2ca9189f
islean <isak.ohlsson.angnell@gmail.com>
2025-02-06 16:16:28 +0100

    Add no entry found test

--- cg/server/endpoints/sequencing_run/pacbio_sequencing_run.py
+++ cg/server/endpoints/sequencing_run/pacbio_sequencing_run.py
@@ -5,11 +5,13 @@ from cg.server.endpoints.sequencing_run.dtos import PacbioSequencingRunsResponse
 from cg.server.endpoints.utils import before_request
 from cg.server.ext import pacbio_sequencing_runs_service
 
-PACBIO_SEQUENCING_RUN_BLUEPRINT = Blueprint("pacbio_sequencing_run", __name__, url_prefix="/api/v1")
+PACBIO_SEQUENCING_RUN_BLUEPRINT = Blueprint(
+    "pacbio_sequencing_run", __name__, url_prefix="/api/v1/pacbio_sequencing_run"
+)
 PACBIO_SEQUENCING_RUN_BLUEPRINT.before_request(before_request)
 
 
-@PACBIO_SEQUENCING_RUN_BLUEPRINT.route("/pacbio_sequencing_run/<run_name>", methods=["GET"])
+@PACBIO_SEQUENCING_RUN_BLUEPRINT.route("/<run_name>", methods=["GET"])
 @handle_missing_entries
 def get_sequencing_metrics(run_name: str):
     response: PacbioSequencingRunsResponse = (
--- tests/services/run_devices/pacbio/test_pacbio_sequencing_runs_service.py
+++ tests/services/run_devices/pacbio/test_pacbio_sequencing_runs_service.py
@@ -1,5 +1,8 @@
+import pytest
+
 from cg.server.endpoints.sequencing_run.dtos import PacbioSequencingRunsResponse
 from cg.services.run_devices.pacbio.sequencing_runs_service import PacbioSequencingRunsService
+from cg.store.exc import EntryNotFoundError
 from cg.store.models import PacbioSequencingRun
 
 
@@ -21,3 +24,21 @@ def test_get_pacbio_sequencing_runs_by_name(
 
     # THEN all the returned runs have the correct run name
     assert all(run.run_name == pacbio_run_name_to_fetch for run in runs.runs)
+
+
+def test_get_pacbio_sequencing_runs_by_name_no_matches(
+    pacbio_sequencing_runs_service: PacbioSequencingRunsService,
+):
+    """Tests getting Pacbio runs from the store filtered on a non-existent run name."""
+
+    # GIVEN that there are no Pacbio sequencing runs with a specific run name
+    all_runs: list[PacbioSequencingRun] = pacbio_sequencing_runs_service.store._get_query(
+        table=PacbioSequencingRun
+    ).all()
+    assert all(run.run_name != "Non-existent run" for run in all_runs)
+
+    # WHEN fetching sequencing runs filtered by run name
+
+    # THEN an EntryNotFoundError should be raised when trying to get sequencing runs filtered on that name
+    with pytest.raises(EntryNotFoundError):
+        pacbio_sequencing_runs_service.get_sequencing_runs_by_name("Non-existent run")
