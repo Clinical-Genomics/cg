b7dcaf6de247ac5893f19759f0b5fd6b091d7acf
islean <isak.ohlsson.angnell@gmail.com>
2025-02-20 11:38:56 +0100

    Add skeleton for seqera platform client + submitter

---
+++ cg/services/analysis_starter/submitters/seqera_platform/client.py
@@ -0,0 +1,12 @@
+from cg.constants import Workflow
+
+
+class SeqeraPlatformClient:
+    def get_workflow_config(self, workflow: Workflow):
+        pass
+
+    def run_case(self, request):
+        pass
+
+    def get_session_id(self, workflow_id: str):
+        pass
