85f2f07c747594fa0a20e8d839673525cff90dbf
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-02-26 15:41:14 +0100

    fix signature of write function

--- cg/services/analysis_starter/configurator/utils.py
+++ cg/services/analysis_starter/configurator/utils.py
@@ -8,8 +8,8 @@ from cg.meta.workflow.utils.utils import are_all_samples_control
 from cg.store.models import Case
 
 
-def write_content_to_json_or_stdout(content: str, file_path: Path, dry_run: bool = False) -> None:
-    """Write content to a JSON file if dry-run is False, otherwise print to stdout."""
+def write_content_to_file_or_stdout(content: str, file_path: Path, dry_run: bool = False) -> None:
+    """Write content to a file if dry-run is False, otherwise print to stdout."""
     if dry_run:
         click.echo(content)
         return
