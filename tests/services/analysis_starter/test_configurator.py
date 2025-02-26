26969f65f0176b5390378ed30895e66569544556
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-02-26 15:20:52 +0100

    create new test and parametrise existent ones

--- tests/services/analysis_starter/test_configurator.py
+++ tests/services/analysis_starter/test_configurator.py
@@ -23,7 +23,7 @@ def test_create_config(
     dummy_work_dir_path: str,
     request: pytest.FixtureRequest,
 ):
-    """Test creating a case config."""
+    """Test creating the case config for all pipelines."""
     # GIVEN a configurator and a case id
     configurator: Configurator = request.getfixturevalue(configurator_fixture)
     case_id: str = request.getfixturevalue(case_id_fixture)
@@ -42,18 +42,50 @@ def test_create_config(
     assert case_config == expected_case_config
 
 
-def test_get_nextflow_config_content(
-    raredisease_case_id: str,
-    raredisease_configurator: RarediseaseConfigurator,
-    expected_raredisease_config_content: str,
+@pytest.mark.parametrize(
+    "configurator_fixture, case_id_fixture",
+    [("raredisease_configurator", "raredisease_case_id")],
+    ids=["raredisease"],
+)
+def test_create_nextflow_config_file_exists(
+    configurator_fixture: str,
+    case_id_fixture: str,
+    request: pytest.FixtureRequest,
 ):
-    """Test getting nextflow config content."""
-    # GIVEN a configurator
+    """Test that a nextflow config file is created fro all Nextflow pipelines."""
+    # GIVEN a configurator and a case id
+    configurator: RarediseaseConfigurator = request.getfixturevalue(configurator_fixture)
+    case_id: str = request.getfixturevalue(case_id_fixture)
+
+    # GIVEN that a case directory exists
+    configurator._create_case_directory(case_id=case_id, dry_run=False)
+
+    # WHEN creating nextflow config
+    configurator._create_nextflow_config(case_id=case_id, dry_run=False)
+
+    # THEN the nextflow config is created
+    assert configurator._get_nextflow_config_path(case_id).exists()
+
+
+@pytest.mark.parametrize(
+    "configurator_fixture, case_id_fixture, expected_content_fixture",
+    [("raredisease_configurator", "raredisease_case_id", "expected_raredisease_config_content")],
+    ids=["raredisease"],
+)
+def test_get_nextflow_config_content(
+    configurator_fixture: str,
+    case_id_fixture: str,
+    expected_content_fixture: str,
+    request: pytest.FixtureRequest,
+):
+    """Test that the content of the nextflow config file is accurate for all Nextflow pipelines."""
+    # GIVEN a configurator and a case id
+    configurator: RarediseaseConfigurator = request.getfixturevalue(configurator_fixture)
+    case_id: str = request.getfixturevalue(case_id_fixture)
 
     # WHEN getting nextflow config content
-    nextflow_config_content: str = raredisease_configurator._get_nextflow_config_content(
-        case_id=raredisease_case_id
-    )
+    nextflow_config_content: str = configurator._get_nextflow_config_content(case_id=case_id)
 
     # THEN the expected content is returned
-    assert nextflow_config_content.rstrip() == expected_raredisease_config_content.rstrip()
+    expected_content: str = request.getfixturevalue(expected_content_fixture)
+    assert nextflow_config_content.rstrip() == expected_content.rstrip()
