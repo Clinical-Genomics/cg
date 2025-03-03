d1b8b178a729ae56d5f5db9f779688317596bca8
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-03-03 10:54:02 +0100

    update branch with QOS from Case

--- cg/services/analysis_starter/configurator/utils.py
+++ cg/services/analysis_starter/configurator/utils.py
@@ -2,10 +2,7 @@ from pathlib import Path
 
 import rich_click as click
 
-from cg.constants.priority import Priority, SlurmQos
 from cg.io.txt import write_txt
-from cg.meta.workflow.utils.utils import are_all_samples_control
-from cg.store.models import Case
 
 
 def write_content_to_file_or_stdout(content: str, file_path: Path, dry_run: bool = False) -> None:
@@ -14,10 +11,3 @@ def write_content_to_file_or_stdout(content: str, file_path: Path, dry_run: bool
         click.echo(content)
         return
     write_txt(content=content, file_path=file_path)
-
-
-def get_slurm_qos_for_case(case: Case) -> str:
-    """Get Quality of service (SLURM QOS) for the case."""
-    if are_all_samples_control(case=case):
-        return SlurmQos.EXPRESS
-    return Priority.priority_to_slurm_qos().get(case.priority)
