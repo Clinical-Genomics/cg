a926e8271cc3c14615a33746477f2b55e34be4f2
islean <isak.ohlsson.angnell@gmail.com>
2025-03-10 10:50:18 +0100

    Make interface

--- cg/services/analysis_starter/configurator/extensions/abstract.py
+++ cg/services/analysis_starter/configurator/extensions/abstract.py
@@ -1,6 +1,9 @@
+from abc import ABC, abstractmethod
 from pathlib import Path
 
 
-class PipelineExtension:
+class PipelineExtension(ABC):
+
+    @abstractmethod
     def configure(self, case_id: str, case_path: Path):
         pass
