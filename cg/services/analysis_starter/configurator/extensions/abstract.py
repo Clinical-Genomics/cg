cdab38c7b071bf217ed5014b809fa00fad8638ae
islean <isak.ohlsson.angnell@gmail.com>
2025-03-10 12:56:13 +0100

    Revert interface

--- cg/services/analysis_starter/configurator/extensions/abstract.py
+++ cg/services/analysis_starter/configurator/extensions/abstract.py
@@ -1,9 +1,8 @@
-from abc import ABC, abstractmethod
 from pathlib import Path
 
 
-class PipelineExtension(ABC):
-
-    @abstractmethod
+class PipelineExtension:
     def configure(self, case_id: str, case_path: Path):
+        """Intended for pipeline specific configurations. If none is needed, this bare class
+        can be used."""
         pass
