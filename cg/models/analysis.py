5d476871242635a947b7710bb569f07f736d610b
ChristianOertlin <christian.oertlin@scilifelab.se>
2025-03-17 09:26:32 +0100

    Revert "Add Nallo delivery report (#4198)" (#4292)
    This reverts commit ddbdcb88d7aa7f3771af53ba17fb6d5a6ee58964.

--- cg/models/analysis.py
+++ cg/models/analysis.py
@@ -1,6 +1,5 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
-from cg.models.nallo.nallo import NalloQCMetrics
 from cg.models.raredisease.raredisease import RarediseaseQCMetrics
 from cg.models.rnafusion.rnafusion import RnafusionQCMetrics
 from cg.models.taxprofiler.taxprofiler import TaxprofilerQCMetrics
@@ -17,9 +16,8 @@ class NextflowAnalysis(AnalysisModel):
     """Nextflow's analysis results model."""
 
     sample_metrics: (
-        dict[str, NalloQCMetrics]
-        | dict[str, RarediseaseQCMetrics]
+        dict[str, RarediseaseQCMetrics]
         | dict[str, RnafusionQCMetrics]
         | dict[str, TaxprofilerQCMetrics]
         | dict[str, TomteQCMetrics]
-    )
+    ) = Field(union_mode="left_to_right")
