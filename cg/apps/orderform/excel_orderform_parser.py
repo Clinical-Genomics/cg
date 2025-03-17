ee3c065c84ff701048eb20ae73ec65363763b7c0
Christian Oertlin <c.oertlin@gmail.com>
2025-03-17 15:09:11 +0100

    linting

--- cg/apps/orderform/excel_orderform_parser.py
+++ cg/apps/orderform/excel_orderform_parser.py
@@ -236,5 +236,5 @@ class ExcelOrderformParser(OrderformParser):
     @staticmethod
     def _transform_data_delivery(data_delivery: str) -> str:
         """Transforms the data-delivery parsed in the excel file, to the ones used in cg"""
-        
+
         return data_delivery.lower().replace(" + ", "-").replace(" ", "_")
