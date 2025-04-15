413824c8ecb554a64728448d9a5537c1b365f115
eliott <eliottob6@gmail.com>
2025-04-15 13:17:20 +0200

    Rename NALLODeliveryType to NalloDeliveryType

--- cg/services/orders/validation/order_types/nallo/constants.py
+++ cg/services/orders/validation/order_types/nallo/constants.py
@@ -3,7 +3,7 @@ from enum import StrEnum
 from cg.constants import DataDelivery
 
 
-class NALLODeliveryType(StrEnum):
+class NalloDeliveryType(StrEnum):
     ANALYSIS = DataDelivery.ANALYSIS_FILES
     ANALYSIS_SCOUT = DataDelivery.ANALYSIS_SCOUT
     NO_DELIVERY = DataDelivery.NO_DELIVERY
--- cg/services/orders/validation/order_types/nallo/models/order.py
+++ cg/services/orders/validation/order_types/nallo/models/order.py
@@ -4,7 +4,7 @@ from typing_extensions import Annotated
 from cg.services.orders.validation.models.discriminators import has_internal_id
 from cg.services.orders.validation.models.existing_case import ExistingCase
 from cg.services.orders.validation.models.order_with_cases import OrderWithCases
-from cg.services.orders.validation.order_types.nallo.constants import NALLODeliveryType
+from cg.services.orders.validation.order_types.nallo.constants import NalloDeliveryType
 from cg.services.orders.validation.order_types.nallo.models.case import NALLOCase
 
 NewCase = Annotated[NALLOCase, Tag("new")]
@@ -13,4 +13,4 @@ OldCase = Annotated[ExistingCase, Tag("existing")]
 
 class NALLOOrder(OrderWithCases):
     cases: list[Annotated[NewCase | OldCase, Discriminator(has_internal_id)]]
-    delivery_type: NALLODeliveryType
+    delivery_type: NalloDeliveryType
