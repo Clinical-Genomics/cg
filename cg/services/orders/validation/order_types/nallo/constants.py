425f99008573eed7a29e8b4818b7d262bbe08c8f
eliott <eliottob6@gmail.com>
2025-04-16 08:35:52 +0200

    Add new delivery types to NalloDeliveryType enum

--- cg/services/orders/validation/order_types/nallo/constants.py
+++ cg/services/orders/validation/order_types/nallo/constants.py
@@ -8,3 +8,6 @@ class NalloDeliveryType(StrEnum):
     ANALYSIS_SCOUT = DataDelivery.ANALYSIS_SCOUT
     NO_DELIVERY = DataDelivery.NO_DELIVERY
     SCOUT = DataDelivery.SCOUT
+    RAW_DATA_ANALYSIS = DataDelivery.RAW_DATA_ANALYSIS
+    RAW_DATA_ANALYSIS_SCOUT = DataDelivery.RAW_DATA_ANALYSIS_SCOUT
+    RAW_DATA_SCOUT = DataDelivery.RAW_DATA_SCOUT
