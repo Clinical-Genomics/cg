ce1cb399604a6ada4f33eca29e2fc943122914ba
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-04-17 11:25:16 +0200

    add nallo ordertype and alembic migration

---
+++ alembic/versions/2025_04_17_6362cfd4c61f_add_nallo_ordertype.py
@@ -0,0 +1,68 @@
+"""add Nallo ordertype
+
+Revision ID: 6362cfd4c61f
+Revises: 3a0250e5526d
+Create Date: 2025-04-17 10:42:20.943466
+
+"""
+
+from sqlalchemy.dialects import mysql
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "6362cfd4c61f"
+down_revision = "3a0250e5526d"
+branch_labels = None
+depends_on = None
+
+
+base_options = (
+    "balsamic",
+    "balsamic-umi",
+    "fastq",
+    "fluffy",
+    "metagenome",
+    "microbial-fastq",
+    "microsalt",
+    "mip-dna",
+    "mip-rna",
+    "pacbio-long-read",
+    "rml",
+    "rnafusion",
+    "sars-cov-2",
+    "taxprofiler",
+    "tomte",
+)
+
+old_options = sorted(base_options)
+new_options = sorted(base_options + ("nallo",))
+
+
+old_order_type_enum = mysql.ENUM(*old_options)
+new_order_type_enum = mysql.ENUM(*new_options)
+
+
+def upgrade():
+    # Add the new value to the enum
+    op.alter_column(
+        "order_type_application",
+        "order_type",
+        existing_type=old_order_type_enum,
+        type_=new_order_type_enum,
+        existing_nullable=False,
+    )
+    new_order_type_enum.create(op.get_bind(), checkfirst=False)
+
+
+def downgrade():
+    # Remove the new value from the enum
+    op.alter_column(
+        "order_type_application",
+        "order_type",
+        existing_type=new_order_type_enum,
+        type_=old_order_type_enum,
+        existing_nullable=False,
+    )
+    old_order_type_enum.create(op.get_bind(), checkfirst=False)
+    new_order_type_enum.drop(op.get_bind(), checkfirst=False)
