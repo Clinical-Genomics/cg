001136876b233ed22681e79677340e0f0c3ff3c7
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-04-17 10:16:44 +0200

    update fixtures

--- cg/services/delivery_message/messages/raw_data_scout_message.py
+++ cg/services/delivery_message/messages/raw_data_scout_message.py
@@ -1,8 +1,5 @@
 from cg.services.delivery_message.messages.delivery_message import DeliveryMessage
-from cg.services.delivery_message.messages.utils import (
-    get_caesar_delivery_path,
-    get_scout_link,
-)
+from cg.services.delivery_message.messages.utils import get_caesar_delivery_path, get_scout_link
 from cg.store.models import Case
 
 
