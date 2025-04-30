7d5e191666d1e16bbdc6a1522eb7981ad6fad3c0
islean <isak.ohlsson.angnell@gmail.com>
2025-04-30 11:03:24 +0200

    Add support for Nallo form and bump Pacbio long read

--- cg/apps/orderform/excel_orderform_parser.py
+++ cg/apps/orderform/excel_orderform_parser.py
@@ -26,6 +26,7 @@ class ExcelOrderformParser(OrderformParser):
     VALID_ORDERFORMS: list[str] = [
         f"{Orderform.MIP_DNA}:{Orderform.get_current_orderform_version(Orderform.MIP_DNA)}",  # Orderform MIP-DNA, Balsamic, sequencing only, MIP-RNA
         f"{Orderform.MICROSALT}:{Orderform.get_current_orderform_version(Orderform.MICROSALT)}",  # Microbial WHOLE_GENOME_SEQUENCING
+        f"{Orderform.NALLO}:{Orderform.get_current_orderform_version(Orderform.NALLO)}",  # Nallo
         f"{Orderform.RML}:{Orderform.get_current_orderform_version(Orderform.RML)}",  # Orderform Ready made libraries (RML)
         f"{Orderform.METAGENOME}:{Orderform.get_current_orderform_version(Orderform.METAGENOME)}",  # Microbial meta genomes
         f"{Orderform.SARS_COV_2}:{Orderform.get_current_orderform_version(Orderform.SARS_COV_2)}",  # Orderform SARS-CoV-2
