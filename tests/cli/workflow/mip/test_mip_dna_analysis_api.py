2d15d50dfb5b6b0394924c055ab42ec66f5e4acb
beatrizsavinhas <beatriz.sa.vinhas@scilifelab.se>
2025-05-08 17:10:05 +0200

    Last fixes.

--- tests/cli/workflow/mip/test_mip_dna_analysis_api.py
+++ tests/cli/workflow/mip/test_mip_dna_analysis_api.py
@@ -4,7 +4,7 @@ from cg.store.models import Case
 
 def test_get_cases_ready_for_analysis(mip_dna_analysis_api: MipDNAAnalysisAPI):
 
-    # GIVEN a balsamic_context with 3 case that is ready for analysis
+    # GIVEN a mip_dna_context with 3 case that are ready for analysis
 
     # WHEN running the command
     cases_to_analyse: list[Case] = mip_dna_analysis_api.get_cases_ready_for_analysis()
@@ -14,7 +14,7 @@ def test_get_cases_ready_for_analysis(mip_dna_analysis_api: MipDNAAnalysisAPI):
 
 
 def test_get_cases_ready_for_analysis_with_limit(mip_dna_analysis_api: MipDNAAnalysisAPI):
-    # GIVEN a balsamic_context with 1 case that is ready for analysis
+    # GIVEN a mip_dna_context with 3 case that are ready for analysis
 
     # WHEN running the command with limit=1
     cases_to_analyse: list[Case] = mip_dna_analysis_api.get_cases_ready_for_analysis(limit=1)
