388f1183aa4a43f9b7f77f9a16cc1d8c99e07c09
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-05-13 15:26:45 +0200

    fixed the test for number of days

---
+++ alembic/versions/2025_05_13_be6552c03d7f_add_top_up_case_action.py
@@ -0,0 +1,25 @@
+"""add top-up case action
+
+Revision ID: be6552c03d7f
+Revises: 8e0b9e03054d
+Create Date: 2025-05-13 15:25:24.572276
+
+"""
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "be6552c03d7f"
+down_revision = "8e0b9e03054d"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
