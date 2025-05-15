c7163975d55bd37351db7c3e6d34589536dbe043
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-05-15 10:25:37 +0200

    update migration with new case action value

--- alembic/versions/2025_05_13_be6552c03d7f_add_top_up_case_action.py
+++ alembic/versions/2025_05_13_be6552c03d7f_add_top_up_case_action.py
@@ -16,10 +16,49 @@ down_revision = "8e0b9e03054d"
 branch_labels = None
 depends_on = None
 
+Base = sa.orm.declarative_base()
+
+old_case_actions: list[str] = [
+    "analyze",
+    "hold",
+    "running",
+]
+
+new_case_actions: list[str] = old_case_actions.copy()
+new_case_actions.append("top-up")
+new_case_actions.sort()
+
+
+class Case(Base):
+    __tablename__ = "case"
+    id = sa.Column(sa.types.Integer, primary_key=True)
+    action = sa.Column(sa.types.Enum(*new_case_actions), nullable=True)
+
 
 def upgrade():
-    pass
+    op.alter_column(
+        table_name="case",
+        column_name="action",
+        existing_type=sa.Enum(*old_case_actions),
+        type_=sa.Enum(*new_case_actions),
+        nullable=True,
+    )
 
 
 def downgrade():
-    pass
+    # Remove incompatible entries
+    bind = op.get_bind()
+    session = sa.orm.Session(bind=bind)
+
+    for case in session.query(Case).filter(Case.action == "top-up"):
+        case.action = "running"
+
+    session.commit()
+
+    op.alter_column(
+        table_name="case",
+        column_name="action",
+        existing_type=sa.Enum(*new_case_actions),
+        type_=sa.Enum(*old_case_actions),
+        nullable=True,
+    )
