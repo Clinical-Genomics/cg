5ab6a35a078f0a7231ff543ce56e9276746165c1
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-06-05 13:16:18 +0200

    remove unused filter functions and aded tests for new ones

--- cg/store/filters/status_analysis_filters.py
+++ cg/store/filters/status_analysis_filters.py
@@ -75,11 +75,6 @@ def filter_analyses_started_before(analyses: Query, started_at_date: datetime, *
     return analyses.filter(Analysis.started_at < started_at_date)
 
 
-def filter_analyses_by_started_at(analyses: Query, started_at_date: datetime, **kwargs) -> Query:
-    """Return a query of analyses started at a certain date."""
-    return analyses.filter(Analysis.started_at == started_at_date)
-
-
 def filter_analyses_by_completed_at(
     analyses: Query, completed_at_date: datetime, **kwargs
 ) -> Query:
@@ -139,7 +134,6 @@ class AnalysisFilter(Enum):
     BY_CASE_ENTRY_ID: Callable = filter_analyses_by_case_entry_id
     IS_NOT_CLEANED: Callable = filter_analyses_not_cleaned
     STARTED_AT_BEFORE: Callable = filter_analyses_started_before
-    BY_STARTED_AT: Callable = filter_analyses_by_started_at
     BY_COMPLETED_AT: Callable = filter_analyses_by_completed_at
     CASE_ACTION_IS_NONE: Callable = filter_analysis_case_action_is_none
     ORDER_BY_UPLOADED_AT: Callable = order_analyses_by_uploaded_at_asc
