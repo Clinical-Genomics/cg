979efde9ef6bfc8ff7411d5cde25557edf7bc12d
peterpru <peter.pruisscher@scilifelab.se>
2025-06-11 14:04:09 +0200

    sex threshold equal

--- cg/constants/nf_analysis.py
+++ cg/constants/nf_analysis.py
@@ -25,7 +25,7 @@ NALLO_PARENT_PEDDY_METRIC_CONDITION: dict[str, dict[str, Any]] = {
 }
 
 NALLO_RAW_METRIC_CONDITIONS: dict[str, dict[str, Any]] = {
-    "sex": {"norm": "lt", "threshold": None},
+    "sex": {"norm": "eq", "threshold": None},
 }
 
 RAREDISEASE_PREDICTED_SEX_METRIC = "predicted_sex_sex_check"
