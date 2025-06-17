5ee5df1896504adc86b9664d755284fd6ac90ed3
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2025-06-17 15:31:20 +0200

    Fix indentation

--- cg/services/deliver_files/rsync/service.py
+++ cg/services/deliver_files/rsync/service.py
@@ -311,7 +311,7 @@ class DeliveryRsyncService:
         source_and_destination_paths: dict[str, Path],
         dry_run: bool = False,
     ) -> int:
-    """Submit the job that rsyncs case data to the delivery server and return the job id."""
+        """Submit the job that rsyncs case data to the delivery server and return the job id."""
         command: str = self.concatenate_rsync_commands(
             folder_list=folders_to_deliver,
             source_and_destination_paths=source_and_destination_paths,
