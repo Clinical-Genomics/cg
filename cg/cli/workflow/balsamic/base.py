0f7caa2be8714dece4cb3868514d92accbabe504
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-06-18 13:49:04 +0200

    add way to populate analysis hk version id in store command

--- cg/cli/workflow/balsamic/base.py
+++ cg/cli/workflow/balsamic/base.py
@@ -164,9 +164,11 @@ def store_housekeeper(
         analysis_api.status_db.verify_case_exists(case_internal_id=case_id)
         analysis_api.verify_case_config_file_exists(case_id=case_id, dry_run=dry_run)
         analysis_api.verify_deliverables_file_exists(case_id=case_id)
-        analysis_api.upload_bundle_housekeeper(case_id=case_id, dry_run=dry_run, force=force)
+        _, version = analysis_api.create_housekeeper_bundle(
+            case_id=case_id, dry_run=dry_run, force=force
+        )
         analysis_api.update_analysis_as_completed_statusdb(
-            case_id=case_id, comment=comment, dry_run=dry_run, force=force
+            case_id=case_id, hk_version_id=version.id, comment=comment, dry_run=dry_run, force=force
         )
         analysis_api.set_statusdb_action(case_id=case_id, action=None, dry_run=dry_run)
     except ValidationError as error:
