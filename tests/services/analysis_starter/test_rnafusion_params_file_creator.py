138bcf9141d08eba9e215593463da63fdc63188a
Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>
2025-06-30 15:44:31 +0200

    Add RNAFusion Params File Creator (#4478) (patch)
    ### Added
    
    - RNAFusionParamsFileCreator
    
    ### Changed
    
    - The ParamsFileCreator class implements the get_file_path method since it seems common for all NF pipelines

---
+++ tests/services/analysis_starter/test_rnafusion_params_file_creator.py
@@ -0,0 +1,36 @@
+from pathlib import Path
+
+from pytest_mock import MockerFixture
+
+from cg.models.cg_config import CGConfig
+from cg.services.analysis_starter.configurator.file_creators.nextflow.params_file import rnafusion
+from cg.services.analysis_starter.configurator.file_creators.nextflow.params_file.rnafusion import (
+    RNAFusionParamsFileCreator,
+)
+
+
+def test_rnafusion_params_file_creator_success(
+    rnafusion_context: CGConfig,
+    rnafusion_params_file_creator: RNAFusionParamsFileCreator,
+    mocker: MockerFixture,
+):
+    # GIVEN an RNAFusion Params file creator
+    write_mock = mocker.patch.object(rnafusion, "write_yaml_nextflow_style", return_value=None)
+    case_path = Path("case", "path")
+    sample_sheet_path = Path("some", "path")
+    case_id = "case_id"
+
+    # WHEN creating the RNAFusion params file
+    rnafusion_params_file_creator.create(
+        case_id=case_id, case_path=case_path, sample_sheet_path=sample_sheet_path
+    )
+
+    # THEN the file should have been created correctly
+    expected_params: dict = {"someparam": "something"} | {
+        "input": sample_sheet_path,
+        "outdir": case_path,
+    }
+    write_mock.assert_called_once_with(
+        file_path=rnafusion_params_file_creator.get_file_path(case_path=case_path, case_id=case_id),
+        content=expected_params,
+    )
