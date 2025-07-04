2b2cf1f2742c699857c2bcbcb75fbbc8f6e25e99
islean <isak.ohlsson.angnell@gmail.com>
2025-07-04 13:11:10 +0200

    Remove lims and housekeeper from configurator

--- tests/fixture_plugins/analysis_starter/configurator_fixtures.py
+++ tests/fixture_plugins/analysis_starter/configurator_fixtures.py
@@ -1,6 +1,5 @@
 import pytest
 
-from cg.apps.housekeeper.hk import HousekeeperAPI
 from cg.apps.lims import LimsAPI
 from cg.meta.workflow.fastq import MicrosaltFastqHandler
 from cg.models.cg_config import CGConfig
@@ -58,8 +57,6 @@ def raredisease_configurator(
     return NextflowConfigurator(
         store=raredisease_context.status_db,
         pipeline_config=raredisease_context.raredisease,
-        housekeeper_api=raredisease_context.housekeeper_api,
-        lims=raredisease_context.lims_api,
         config_file_creator=nextflow_config_file_creator,
         sample_sheet_creator=raredisease_sample_sheet_creator,
         params_file_creator=raredisease_params_file_creator,
@@ -72,16 +69,12 @@ def rnafusion_configurator(
     rnafusion_sample_sheet_creator: RNAFusionSampleSheetCreator,
     rnafusion_params_file_creator: RNAFusionParamsFileCreator,
     mock_store_for_rnafusion_sample_sheet_creator: Store,
-    housekeeper_api: HousekeeperAPI,
-    lims_api: LimsAPI,
     nextflow_config_file_creator: NextflowConfigFileCreator,
     rnafusion_context: CGConfig,
 ) -> NextflowConfigurator:
     return NextflowConfigurator(
         store=mock_store_for_rnafusion_sample_sheet_creator,
         config_file_creator=nextflow_config_file_creator,
-        housekeeper_api=housekeeper_api,
-        lims=lims_api,
         params_file_creator=rnafusion_params_file_creator,
         pipeline_config=rnafusion_context.rnafusion,
         sample_sheet_creator=rnafusion_sample_sheet_creator,
