34a82659ad30f9c0c6ee79d1d0cdf04e55cd5da1
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-07-14 09:23:12 +0200

    add dev commands for raredisease (#4500)(patch)
    ## Description
    Add dev commands for raredisease analysis.
    
    ### Added
    
    - dev_config_case
    - dev_run
    - dev_start
    - dev_start_available
    - Tests for these and rnafusion commands
    
    ### Changed
    
    - Remove special exception of `AnalysisRunningError` in AnalysisStarter `start_available`
    - Parametrised AnalysisStarter tests on workflow (second parameterisation)

--- tests/cli/workflow/microsalt/test_start_and_start_available.py
+++ tests/cli/workflow/microsalt/test_start_and_start_available.py
@@ -1,4 +1,4 @@
-from unittest.mock import create_autospec
+from unittest.mock import MagicMock
 
 import pytest
 from click.testing import CliRunner, Result
@@ -7,7 +7,6 @@ from pytest_mock import MockerFixture
 from cg.cli.workflow.microsalt.base import start, start_available
 from cg.constants import EXIT_FAIL, EXIT_SUCCESS
 from cg.models.cg_config import CGConfig
-from cg.services.analysis_starter.factories.starter_factory import AnalysisStarterFactory
 from cg.services.analysis_starter.service import AnalysisStarter
 
 
@@ -15,17 +14,14 @@ def test_start(cli_runner: CliRunner, cg_context: CGConfig, mocker: MockerFixtur
     # GIVEN a valid context and a case id
     case_id: str = "case_id"
 
-    # GIVEN a mocked AnalysisStarter
-    starter_mock: AnalysisStarter = create_autospec(AnalysisStarter)
-    mocker.patch.object(
-        AnalysisStarterFactory, "get_analysis_starter_for_case", return_value=starter_mock
-    )
+    # GIVEN a mocked AnalysisStarter that simulates the start method
+    service_call: MagicMock = mocker.patch.object(AnalysisStarter, "start")
 
     # WHEN running the start command
     result: Result = cli_runner.invoke(start, [case_id], obj=cg_context)
 
     # THEN the starter command should have been called with the specified case id
-    starter_mock.start.assert_called_once_with(case_id)
+    service_call.assert_called_once_with(case_id)
 
     # THEN the command should have executed without fail
     assert result.exit_code == EXIT_SUCCESS
@@ -46,23 +42,20 @@ def test_start_available(
     mocker: MockerFixture,
 ):
     """
-    Test that the microSALT start_available command succeeds when the starter succeeds and
-    aborts otherwise.
+    Test that the start_available command succeeds when the starter succeeds and aborts otherwise.
     """
     # GIVEN a valid context
 
-    # GIVEN a mocked AnalysisStarter and a store that returns a workflow
-    starter_mock: AnalysisStarter = create_autospec(AnalysisStarter)
-    starter_mock.start_available.return_value = succeeds
-    mocker.patch.object(
-        AnalysisStarterFactory, "get_analysis_starter_for_workflow", return_value=starter_mock
+    # GIVEN a mocked AnalysisStarter that simulates the start_available method
+    service_call: MagicMock = mocker.patch.object(
+        AnalysisStarter, "start_available", return_value=succeeds
     )
 
     # WHEN running the start_available command
     result: Result = cli_runner.invoke(start_available, obj=cg_context)
 
     # THEN the starter command should have been called
-    starter_mock.start_available.assert_called_once()
+    service_call.assert_called_once()
 
     # THEN the command should have executed as expected
     assert result.exit_code == exit_status
