bb8c069070280876d3a7efafaf80a8f6867212ff
Vincent Janvid <vincent.janvid@scilifelab.se>
2025-07-28 15:49:17 +0200

    Fix tests

--- tests/services/analysis_starter/test_configurator_factory.py
+++ tests/services/analysis_starter/test_configurator_factory.py
@@ -3,6 +3,7 @@ import pytest
 from cg.constants import Workflow
 from cg.models.cg_config import CGConfig
 from cg.services.analysis_starter.configurator.configurator import Configurator
+from cg.services.analysis_starter.configurator.implementations.balsamic import BalsamicConfigurator
 from cg.services.analysis_starter.configurator.implementations.microsalt import (
     MicrosaltConfigurator,
 )
@@ -16,6 +17,7 @@ from cg.services.analysis_starter.factories.configurator_factory import Configur
         (Workflow.MICROSALT, MicrosaltConfigurator),
         (Workflow.RAREDISEASE, NextflowConfigurator),
         (Workflow.RNAFUSION, NextflowConfigurator),
+        (Workflow.BALSAMIC, BalsamicConfigurator),
     ],
 )
 def test_configurator_factory_success(
@@ -35,7 +37,7 @@ def test_configurator_factory_success(
 
 def test_configurator_factory_failure(cg_context: CGConfig):
     # GIVEN a workflow we do not have support for
-    workflow = Workflow.BALSAMIC
+    workflow = Workflow.MIP_DNA
 
     # GIVEN a configurator factory
     configurator_factory = ConfiguratorFactory(cg_config=cg_context)
