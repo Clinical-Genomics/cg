72f2612d89972fad5104f0bacd51411409737c4e
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-07-28 14:51:28 +0200

    refactor Nextflow case config fixtures

--- tests/fixture_plugins/analysis_starter/case_config_fixtures.py
+++ tests/fixture_plugins/analysis_starter/case_config_fixtures.py
@@ -44,48 +44,13 @@ def get_nextflow_case_config_dict(
 
 
 @pytest.fixture
-def raredisease_case_config(
-    nextflow_case_id: str,
-    nextflow_case_path: Path,
-    nextflow_config_profiles: list[str],
-    nextflow_repository: str,
-    nextflow_pipeline_revision: str,
-) -> NextflowCaseConfig:
-
-    return NextflowCaseConfig(
-        case_id=nextflow_case_id,
-        config_profiles=nextflow_config_profiles,
-        workflow=Workflow.RAREDISEASE,
-        case_priority=SlurmQos.NORMAL,
-        nextflow_config_file=Path(
-            nextflow_case_path, f"{nextflow_case_id}_nextflow_config.json"
-        ).as_posix(),
-        params_file=Path(nextflow_case_path, f"{nextflow_case_id}_params_file.yaml").as_posix(),
-        pipeline_repository=nextflow_repository,
-        pre_run_script="",
-        revision=nextflow_pipeline_revision,
-        work_dir=Path(nextflow_case_path, "work").as_posix(),
-    )
+def raredisease_case_config(get_nextflow_case_config_dict: callable) -> NextflowCaseConfig:
+    return NextflowCaseConfig(**get_nextflow_case_config_dict(workflow=Workflow.RAREDISEASE))
 
 
 @pytest.fixture
-def rnafusion_case_config(nextflow_root: str, nextflow_case_id: str) -> NextflowCaseConfig:
-    return NextflowCaseConfig(
-        case_id=nextflow_case_id,
-        workflow=Workflow.RNAFUSION,
-        case_priority=SlurmQos.NORMAL,
-        config_profiles=["myprofile"],
-        nextflow_config_file=Path(
-            nextflow_root, nextflow_case_id, f"{nextflow_case_id}_nextflow_config.json"
-        ).as_posix(),
-        params_file=Path(
-            nextflow_root, nextflow_case_id, f"{nextflow_case_id}_params_file.yaml"
-        ).as_posix(),
-        pipeline_repository="https://some_url",
-        pre_run_script="",
-        revision="2.2.0",
-        work_dir=Path(nextflow_root, nextflow_case_id, "work").as_posix(),
-    )
+def rnafusion_case_config(get_nextflow_case_config_dict: callable) -> NextflowCaseConfig:
+    return NextflowCaseConfig(**get_nextflow_case_config_dict(workflow=Workflow.RNAFUSION))
 
 
 @pytest.fixture
