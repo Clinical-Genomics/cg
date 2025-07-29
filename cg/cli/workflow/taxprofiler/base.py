769e520ccf160e6c276a2ef1d69fd31ef62e9d9c
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2025-07-29 16:47:40 +0200

    Taxprofiler dev-start and dev-start-available

--- cg/cli/workflow/taxprofiler/base.py
+++ cg/cli/workflow/taxprofiler/base.py
@@ -5,7 +5,7 @@ import logging
 import rich_click as click
 
 from cg.cli.utils import CLICK_CONTEXT_SETTINGS
-from cg.cli.workflow.commands import resolve_compression
+from cg.cli.workflow.commands import ARGUMENT_CASE_ID, resolve_compression
 from cg.cli.workflow.nf_analysis import (
     config_case,
     metrics_deliver,
@@ -17,9 +17,12 @@ from cg.cli.workflow.nf_analysis import (
     store_available,
     store_housekeeper,
 )
-from cg.constants.constants import MetaApis
+from cg.constants.constants import MetaApis, Workflow
 from cg.meta.workflow.analysis import AnalysisAPI
 from cg.meta.workflow.taxprofiler import TaxprofilerAnalysisAPI
+from cg.models.cg_config import CGConfig
+from cg.services.analysis_starter.factories.starter_factory import AnalysisStarterFactory
+from cg.services.analysis_starter.service import AnalysisStarter
 
 LOG = logging.getLogger(__name__)
 
@@ -30,6 +33,32 @@ def taxprofiler(context: click.Context) -> None:
     """nf-core/taxprofiler analysis workflow."""
     AnalysisAPI.get_help(context)
     context.obj.meta_apis[MetaApis.ANALYSIS_API] = TaxprofilerAnalysisAPI(config=context.obj)
+
+
+@taxprofiler.command()
+@ARGUMENT_CASE_ID
+@click.pass_obj
+def dev_start(cg_config: CGConfig, case_id: str) -> None:
+    """Start a Taxprofiler case. Configures the case if needed."""
+    factory = AnalysisStarterFactory(cg_config)
+    analysis_starter: AnalysisStarter = factory.get_analysis_starter_for_workflow(
+        Workflow.TAXPROFILER
+    )
+    analysis_starter.start(case_id=case_id)
+
+
+@taxprofiler.command()
+@click.pass_obj
+def dev_start_available(cg_config: CGConfig) -> None:
+    """Starts all available Taxprofiler cases."""
+    LOG.info("Starting Taxprofiler workflow for all available cases.")
+    factory = AnalysisStarterFactory(cg_config)
+    analysis_starter: AnalysisStarter = factory.get_analysis_starter_for_workflow(
+        Workflow.TAXPROFILER
+    )
+    succeeded: bool = analysis_starter.start_available()
+    if not succeeded:
+        raise click.Abort
 
 
 taxprofiler.add_command(resolve_compression)
