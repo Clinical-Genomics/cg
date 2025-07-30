47d83bbf9c879365b416d98f8bf77246d63a9625
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2025-07-30 17:03:21 +0200

    Test passes with populated store

--- tests/integration/test_integration_cli_run.py
+++ tests/integration/test_integration_cli_run.py
@@ -1,31 +1,64 @@
+import traceback
 from unittest.mock import create_autospec
 
 import pytest
 from click.testing import CliRunner
+from pytest_httpserver import HTTPServer
 
 from cg.apps.tb.api import IDTokenCredentials
 from cg.cli.base import base
 from cg.cli.workflow.microsalt.base import run
 from cg.exc import CaseNotConfiguredError
-from cg.models.cg_config import CGConfig
 from cg.services.analysis_starter.tracker.tracker import Tracker
-from cg.store.models import Case, Sample
+from cg.store import database
+from cg.store.database import Session
+from cg.store.models import Case
 from cg.store.store import Store
 
 
-@pytest.mark.integration
-def test_run_raises_error_if_not_configured(mocker):
-
-    cli_runner = CliRunner()
-
-    # GIVEN a case to run
-    case_id = "some_case_id"
-
+@pytest.fixture(autouse=True)
+def valid_google_token(mocker):
     mocker.patch.object(
         IDTokenCredentials,
         "from_service_account_file",
         return_value=create_autospec(IDTokenCredentials, token="some token"),
     )
+
+
+@pytest.fixture(scope="session")
+def httpserver_listen_address():
+    return ("localhost", 8888)
+
+
+@pytest.fixture
+def store() -> Store:
+    database.initialize_database("sqlite:///file:cg?mode=memory&cache=shared&uri=true")
+    database.create_all_tables()
+    store = Store()
+    return store
+
+
+@pytest.mark.integration
+def test_run_raises_error_if_not_configured(microsalt_store: Store, httpserver: HTTPServer):
+
+    cli_runner = CliRunner()
+
+    # GIVEN a case to run
+    case_id = "microparakeet"
+
+    # GIVEN trailblazer returns an analysis for the case that is not complete
+    # TODO: should there even be an analysis at this stage?
+    httpserver.expect_request("/trailblazer/get-latest-analysis").respond_with_json(
+        {
+            "id": "12345",
+            "logged_at": "2025-07-30",
+            "started_at": "2025-07-30",
+            "completed_at": "",
+            "out_dir": "/out/dir",
+            "config_path": "/config/path",
+        }
+    )
+
     # GIVEN that the config file for a case does not exist
 
     # WHEN running the case
@@ -41,6 +74,11 @@ def test_run_raises_error_if_not_configured(mocker):
         ],
     )
 
-    assert result.exception.__context__ == "?"
+    traceback.print_exception(result.exception)
+
     # THEN an error should be raised
     assert isinstance(result.exception, CaseNotConfiguredError)
+
+    # THEN the case should not be running
+    case: Case | None = microsalt_store.get_case_by_internal_id(case_id)
+    assert case and case.action == None
