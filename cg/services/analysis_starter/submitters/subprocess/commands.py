d1a2b1b0a1140e2288e57f00652e72e773484998
Vincent Janvid <vincent.janvid@scilifelab.se>
2025-07-30 11:19:45 +0200

    Fix balsamic run CLI template

--- cg/services/analysis_starter/submitters/subprocess/commands.py
+++ cg/services/analysis_starter/submitters/subprocess/commands.py
@@ -3,7 +3,7 @@ from cg.constants import Workflow
 WORKFLOW_LAUNCH_COMMAND_MAP: dict[Workflow, str] = {
     Workflow.MICROSALT: "{conda_binary} run --name {environment} "
     "{binary} analyse {config_file} --input {fastq_directory}",
-    Workflow.BALSAMIC: "{conda_binary} run {binary} balsamic run --account {account} --mail-user {mail_user} "
+    Workflow.BALSAMIC: "{conda_binary} run {binary} run --account {account} --mail-user {mail_user} "
     "--qos {qos} --sample-config {sample_config} --cluster-config {cluster_config} --run-analysis "
     "--benchmark",
 }
