83247e1d4a6decd6fcc258fd203b6ecd552e05d7
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2025-08-01 16:36:31 +0200

    Exclude integration tests from default test runs

--- tests/integration/test_integration_cli_run.py
+++ tests/integration/test_integration_cli_run.py
@@ -16,11 +16,13 @@ from pytest_httpserver import HTTPServer
 from cg.apps.tb.api import IDTokenCredentials
 from cg.cli.base import base
 from cg.cli.workflow.microsalt.base import run
+from cg.cli.workflow.mip import base as mip_base
 from cg.constants.constants import Workflow
 from cg.constants.gene_panel import GenePanelMasterList
 from cg.constants.process import EXIT_SUCCESS
+from cg.constants.tb import AnalysisType
 from cg.store import database as cg_database
-from cg.store.models import Case, IlluminaFlowCell, IlluminaSequencingRun, Sample
+from cg.store.models import Case, IlluminaFlowCell, IlluminaSequencingRun, Order, Sample
 from cg.store.store import Store
 from cg.utils import commands
 from tests.store_helpers import StoreHelpers
@@ -76,8 +78,18 @@ def test_start_available_mip_dna(
 ):
     cli_runner = CliRunner()
 
-    case: Case = helpers.add_case(store=status_db, data_analysis=Workflow.MIP_DNA)
-    sample: Sample = helpers.add_sample(store=status_db, last_sequenced_at=datetime.now())
+    ticket_id = 12345
+    case: Case = helpers.add_case(
+        store=status_db, data_analysis=Workflow.MIP_DNA, ticket=str(ticket_id)
+    )
+    order: Order = helpers.add_order(
+        store=status_db, ticket_id=ticket_id, customer_id=case.customer_id
+    )
+    status_db.link_case_to_order(order_id=order.id, case_id=case.id)
+
+    sample: Sample = helpers.add_sample(
+        store=status_db, last_sequenced_at=datetime.now(), application_type=AnalysisType.WGS
+    )
     helpers.relate_samples(base_store=status_db, case=case, samples=[sample])
 
     flow_cell: IlluminaFlowCell = helpers.add_illumina_flow_cell(store=status_db)
@@ -120,8 +132,34 @@ def test_start_available_mip_dna(
         )
     )
 
-    httpserver.expect_request(f"/lims/api/v2/samples/{sample.internal_id}").respond_with_json(
-        {"what": "?"}
+    mocker.patch.object(mip_base, "environ_email", return_value="testuser@scilifelab.se")
+
+    httpserver.expect_request(
+        "/trailblazer/get-latest-analysis", data='{"case_id": "' + case.internal_id + '"}'
+    ).respond_with_json(
+        {
+            "id": "1",
+            "logged_at": "",
+            "started_at": "",
+            "completed_at": "",
+            "out_dir": "out/dir",
+            "config_path": "config/path",
+        }
+    )
+
+    httpserver.expect_request(
+        "/trailblazer/add-pending-analysis",
+        data=b'{"case_id": "%(case_id)s", "email": "linnealofdahl@scilifelab.se", "type": "wgs", "config_path": "tests/integration/config/workflows/mip-dna/cases/%(case_id)s/analysis/slurm_job_ids.yaml", "order_id": 1, "out_dir": "tests/integration/config/workflows/mip-dna/cases/%(case_id)s/analysis", "priority": "normal", "workflow": "MIP-DNA", "ticket": "%(ticket_id)s", "workflow_manager": "slurm", "tower_workflow_id": null, "is_hidden": true}'
+        % {b"case_id": case.internal_id.encode(), b"ticket_id": str(ticket_id).encode()},
+    ).respond_with_json(
+        {
+            "id": "1",
+            "logged_at": "",
+            "started_at": "",
+            "completed_at": "",
+            "out_dir": "out/dir",
+            "config_path": "config/path",
+        }
     )
 
     # WHEN running start available
@@ -135,8 +173,7 @@ def test_start_available_mip_dna(
             "start-available",
         ],
     )
-
-    subprocess_mock.run.assert_called_once_with(
+    subprocess_mock.run.assert_any_call(
         [
             "tests/integration/config/scout/binary",
             "--config",
@@ -155,13 +192,46 @@ def test_start_available_mip_dna(
         stderr=ANY,
     )
 
+    # TODO check that these files are created, switch to pytest temp dir:
+    # create mode 100644 tests/integration/config/workflows/mip-dna/cases/darlingdolphin/gene_panels.bed
+    # create mode 100644 tests/integration/config/workflows/mip-dna/cases/darlingdolphin/managed_variants.vcf
+    # create mode 100644 tests/integration/config/workflows/mip-dna/cases/invitingcrappie/gene_panels.bed
+    # create mode 100644 tests/integration/config/workflows/mip-dna/cases/invitingcrappie/managed_variants.vcf
+    # create mode 100644 tests/integration/config/workflows/mip-dna/cases/moralfinch/gene_panels.bed
+    # create mode 100644 tests/integration/config/workflows/mip-dna/cases/moralfinch/managed_variants.vcf
+    # create mode 100644 tests/integration/config/workflows/mip-dna/cases/mutualoriole/gene_panels.bed
+    # create mode 100644 tests/integration/config/workflows/mip-dna/cases/mutualoriole/managed_variants.vcf
+
     # The order of the bed arguments is not deterministic, so we need to look at them as a set
-    bed_args = subprocess_mock.run.call_args.args[0][6:9]
+    bed_args = subprocess_mock.run.call_args_list[0][0][0][6:9]
     assert set(bed_args) == {
         GenePanelMasterList.OMIM_AUTO,
         GenePanelMasterList.PANELAPP_GREEN,
         "panel_test",
     }
+
+    subprocess_mock.run.assert_any_call(
+        [
+            "tests/integration/config/scout/binary",
+            "--config",
+            "tests/integration/config/scout/config",
+            "export",
+            "managed",
+            "--build",
+            "37",
+        ],
+        check=False,
+        stdout=ANY,
+        stderr=ANY,
+    )
+
+    subprocess_mock.run.assert_any_call(
+        f"""tests/integration/config/workflows/mip-dna/conda_bin run --name S_mip12.1 tests/integration/config/workflows/mip-dna/bin analyse rd_dna --config tests/integration/config/workflows/mip-dna/config/mip12.1-dna-stage.yaml {case.internal_id} --slurm_quality_of_service normal --email testuser@scilifelab.se""",
+        check=False,
+        shell=True,
+        stdout=ANY,
+        stderr=ANY,
+    )
 
     assert result.exit_code == 0
     updated_case = status_db.get_case_by_internal_id(case.internal_id)
