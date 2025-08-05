2a9a94cfe127a4ef4eeb127fe4d211735a5356e5
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2025-08-05 16:23:00 +0200

    Refactor

--- tests/integration/test_integration_cli_run.py
+++ tests/integration/test_integration_cli_run.py
@@ -93,42 +93,31 @@ def test_start_available_mip_dna(
 ):
     cli_runner = CliRunner()
 
+    # GIVEN a mip root directory
     test_root_dir = tmp_path_factory.mktemp("test_start_available_mip_dna")
-    template_path = "tests/integration/config/cg-test.yaml"
-    with open(template_path) as f:
-        config_content = f.read()
+    # GIVEN a config file with valid database uris and directories
+    config_path = create_config(status_db_uri, housekeeper_db_uri, test_root_dir)
 
-    config_content = config_content.format(
-        test_root_dir=test_root_dir,
-        status_db_uri=status_db_uri,
-        housekeeper_db_uri=housekeeper_db_uri,
-    )
-
-    config_path = Path(test_root_dir, "cg-config.yaml")
-    with open(config_path, "w") as f:
-        f.write(config_content)
-
+    # GIVEN a case with existing qc files
     ticket_id = 12345
     case: Case = helpers.add_case(
         store=status_db, data_analysis=Workflow.MIP_DNA, ticket=str(ticket_id)
     )
+    create_qc_file(test_root_dir, case)
 
-    filepath = Path(
-        f"{test_root_dir}/mip-dna/cases/{case.internal_id}/analysis/{case.internal_id}_qc_sample_info.yaml"
-    )
-    filepath.parent.mkdir(parents=True, exist_ok=True)
-    shutil.copy2("tests/fixtures/apps/mip/dna/store/case_qc_sample_info.yaml", filepath)
-
+    # GIVEN an order associated with the case
     order: Order = helpers.add_order(
         store=status_db, ticket_id=ticket_id, customer_id=case.customer_id
     )
     status_db.link_case_to_order(order_id=order.id, case_id=case.id)
 
+    # GIVEN a sample associated with the case
     sample: Sample = helpers.add_sample(
         store=status_db, last_sequenced_at=datetime.now(), application_type=AnalysisType.WGS
     )
     helpers.relate_samples(base_store=status_db, case=case, samples=[sample])
 
+    # GIVEN a flow cell and sequencing run associated with the sample
     flow_cell: IlluminaFlowCell = helpers.add_illumina_flow_cell(store=status_db)
     sequencing_run: IlluminaSequencingRun = helpers.add_illumina_sequencing_run(
         store=status_db, flow_cell=flow_cell
@@ -137,8 +126,11 @@ def test_start_available_mip_dna(
         store=status_db, sample_id=sample.internal_id, sequencing_run=sequencing_run, lane=1
     )
 
+    # GIVEN sample bundle data directories exists
     bed_files_path: Path = tmp_path_factory.mktemp("bed_files")
     delivery_report_path: Path = tmp_path_factory.mktemp("delivery_report")
+
+    # GIVEN the bundle data exists in Housekeeper
     bundle_data = {
         "name": sample.internal_id,
         "created": datetime.now(),
@@ -162,12 +154,8 @@ def test_start_available_mip_dna(
     housekeeper_db.session.add(version)
     housekeeper_db.session.commit()
 
+    # GIVEN the scout command returns exported panel data
     subprocess_mock = mocker.patch.object(commands, "subprocess")
-    subprocess_mock.run = Mock(
-        return_value=create_autospec(
-            CompletedProcess, returncode=EXIT_SUCCESS, stdout=b"", stderr=b""
-        )
-    )
 
     def mock_run(*args, **kwargs):
         command = args[0]
@@ -180,8 +168,10 @@ def test_start_available_mip_dna(
 
     subprocess_mock.run = Mock(side_effect=mock_run)
 
+    # GIVEN an email adress can be determined from the environment
     mocker.patch.object(mip_base, "environ_email", return_value="testuser@scilifelab.se")
 
+    # GIVEN the trailblazer API returns an uncompleted analysis
     httpserver.expect_request(
         "/trailblazer/get-latest-analysis", data='{"case_id": "' + case.internal_id + '"}'
     ).respond_with_json(
@@ -195,6 +185,7 @@ def test_start_available_mip_dna(
         }
     )
 
+    # GIVEN a pending analysis can be added to the Trailblazer API
     httpserver.expect_request(
         "/trailblazer/add-pending-analysis",
         data=b'{"case_id": "%(case_id)s", "email": "linnealofdahl@scilifelab.se", "type": "wgs", "config_path": "%(test_root_dir)s/mip-dna/cases/%(case_id)s/analysis/slurm_job_ids.yaml", "order_id": 1, "out_dir": "%(test_root_dir)s/mip-dna/cases/%(case_id)s/analysis", "priority": "normal", "workflow": "MIP-DNA", "ticket": "%(ticket_id)s", "workflow_manager": "slurm", "tower_workflow_id": null, "is_hidden": true}'
@@ -214,7 +205,7 @@ def test_start_available_mip_dna(
         }
     )
 
-    # WHEN running start available
+    # WHEN running mip-dna start-available
     result: Result = cli_runner.invoke(
         base,
         [
@@ -225,6 +216,8 @@ def test_start_available_mip_dna(
             "start-available",
         ],
     )
+
+    # THEN a scout command is called to export panel beds
     subprocess_mock.run.assert_any_call(
         [
             f"{test_root_dir}/scout/binary",
@@ -252,6 +245,7 @@ def test_start_available_mip_dna(
         "panel_test",
     }
 
+    # THEN a scout command is called to export managed variants
     subprocess_mock.run.assert_any_call(
         [
             f"{test_root_dir}/scout/binary",
@@ -267,6 +261,7 @@ def test_start_available_mip_dna(
         stderr=ANY,
     )
 
+    # THEN a mip-dna analysis is started with the expected parameters
     subprocess_mock.run.assert_any_call(
         f"""{test_root_dir}/mip-dna/conda_bin run --name S_mip12.1 {test_root_dir}/mip-dna/bin analyse rd_dna --config {test_root_dir}/mip-dna/config/mip12.1-dna-stage.yaml {case.internal_id} --slurm_quality_of_service normal --email testuser@scilifelab.se""",
         check=False,
@@ -275,12 +270,43 @@ def test_start_available_mip_dna(
         stderr=ANY,
     )
 
+    # THEN a successful exit code is returned
     assert result.exit_code == 0
 
-    status_db.session.refresh(case)
+    # THEN an analysis has been created for the case
     assert len(case.analyses) == 1
+
+    # THEN the case action is set to running
+    status_db.session.refresh(case)
     assert case.action == CaseActions.RUNNING
 
+    # THEN gene_panels and managed_variant files has been created
     case_dir = Path(test_root_dir, "mip-dna", "cases", case.internal_id)
     assert Path(case_dir, "gene_panels.bed").exists()
     assert Path(case_dir, "managed_variants.vcf").exists()
+
+
+def create_qc_file(test_root_dir, case) -> Path:
+    filepath = Path(
+        f"{test_root_dir}/mip-dna/cases/{case.internal_id}/analysis/{case.internal_id}_qc_sample_info.yaml"
+    )
+    filepath.parent.mkdir(parents=True, exist_ok=True)
+    shutil.copy2("tests/fixtures/apps/mip/dna/store/case_qc_sample_info.yaml", filepath)
+    return filepath
+
+
+def create_config(status_db_uri, housekeeper_db_uri, test_root_dir):
+    template_path = "tests/integration/config/cg-test.yaml"
+    with open(template_path) as f:
+        config_content = f.read()
+
+    config_content = config_content.format(
+        test_root_dir=test_root_dir,
+        status_db_uri=status_db_uri,
+        housekeeper_db_uri=housekeeper_db_uri,
+    )
+
+    config_path = Path(test_root_dir, "cg-config.yaml")
+    with open(config_path, "w") as f:
+        f.write(config_content)
+    return config_path
