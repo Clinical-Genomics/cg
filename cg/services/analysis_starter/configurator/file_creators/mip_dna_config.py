bd6a262b2f9cf85b4de1ff3489bc332d9cc58418
islean <isak.ohlsson.angnell@gmail.com>
2025-08-06 13:54:52 +0200

    Sample data WIP

--- cg/services/analysis_starter/configurator/file_creators/mip_dna_config.py
+++ cg/services/analysis_starter/configurator/file_creators/mip_dna_config.py
@@ -1,9 +1,11 @@
 from pathlib import Path
 
 from cg.apps.lims import LimsAPI
-from cg.constants.constants import FileExtensions
+from cg.constants.constants import DEFAULT_CAPTURE_KIT, FileExtensions
+from cg.constants.tb import AnalysisType
+from cg.exc import CgError
 from cg.io.yaml import write_yaml
-from cg.store.models import BedVersion, Case
+from cg.store.models import BedVersion, Case, Sample
 from cg.store.store import Store
 
 
@@ -20,21 +22,42 @@ class MIPDNAConfigFileCreator:
     def _get_content(self, bed_file: str | None, case_id: str) -> dict:
         content = {"case": "", "default_gene_panels": [], "samples": []}
         case: Case = self.store.get_case_by_internal_id_strict(case_id)
-        bed_file = self._get_sample_bed_file()
+        samples_data = []
+        for sample in case.samples:
+            bed_file: str = self._get_sample_bed_file(bed_file_name=bed_file, case_id=case_id, sample=sample)
+            sample_content = self._get_sample_content(bed_file=bed_file, sample=sample)
+            samples_data.append(sample_content)
         return content
 
-    def _get_sample_bed_file(self):
-        return "bed_file.bed"
+    def _get_sample_bed_file(self, bed_file_name: str | None, case_id: str, sample: Sample) -> str:
+        bed_file: str
+        if sample.prep_category == AnalysisType.WGS:
+            bed_file = bed_file_name or DEFAULT_CAPTURE_KIT
+        else:
+            bed_file = bed_file_name or self._get_target_bed_from_lims(case_id)
+        return bed_file
 
     def _get_bed_file_name(self, bed_flag: str | None):
         if bed_flag is None:
             return None
         elif bed_flag.endswith(FileExtensions.BED):
             return bed_flag
-        bed_version: BedVersion | None = self.store.get_bed_version_by_short_name(
-            bed_version_short_name=bed_flag
-        )
-        if not bed_version:
-            raise CgError("Please provide a valid panel shortname or a path to panel.bed file!")
+        if bed_version := self.store.get_bed_version_by_short_name(bed_flag):
+            return bed_version.filename
+        raise CgError("Please provide a valid panel shortname or a path to panel.bed file!")
 
-        return bed_version.filename
+    def _get_target_bed_from_lims(self, case_id):
+        return ""
+
+    def _get_sample_content(self, bed_file: str, sample: Sample) -> dict:
+        return {
+            "analysis_type": "",
+            "capture_kit": bed_file,
+            "expected_coverage": "",
+            "father": "",
+            "mother": "",
+            "phenotype": "",
+            "sample_display_name": "",
+            "sample_id": "",
+            "sex": ""
+        }
--- tests/services/analysis_starter/test_mip_dna_config_file_creator.py
+++ tests/services/analysis_starter/test_mip_dna_config_file_creator.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from unittest.mock import Mock
+from unittest.mock import Mock, create_autospec
 
 from pytest_mock import MockerFixture
 
@@ -7,13 +7,24 @@ from cg.services.analysis_starter.configurator.file_creators import mip_dna_conf
 from cg.services.analysis_starter.configurator.file_creators.mip_dna_config import (
     MIPDNAConfigFileCreator,
 )
+from cg.store.models import Case, Sample
+from cg.store.store import Store
 
 
 def test_create(mocker: MockerFixture):
-    file_creator = MIPDNAConfigFileCreator(lims_api=Mock(), store=Mock())
+    # GIVEN a mocked store
+    sample = create_autospec(Sample)
+    case = create_autospec(Case, samples=[sample])
+    store = create_autospec(Store, get_case_by_internal_id_strict=case)
 
+    # GIVEN a MIPDNAConfigFileCreator
+    file_creator = MIPDNAConfigFileCreator(lims_api=Mock(), store=store)
+
+    # GIVEN a patched writer
     mock_write = mocker.patch.object(mip_dna_config, "write_yaml")
 
+    # WHEN creating the config file
     file_creator.create(case_id="case_id", bed_flag=None)
 
+    # THEN the writer is called with the correct content and file path
     mock_write.assert_called_once_with(content="expected_content", file_path=Path("expected_path"))
