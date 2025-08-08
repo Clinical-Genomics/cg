b807050e6ee845f49402a5b51b26c7e185bd55ce
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2025-08-08 14:35:59 +0200

    Move fixtures to conftest

--- tests/services/analysis_starter/mip_dna_config_file_creator/test_create.py
+++ tests/services/analysis_starter/mip_dna_config_file_creator/test_create.py
@@ -5,101 +5,15 @@ import pytest
 from pytest_mock import MockerFixture
 
 from cg.apps.lims import LimsAPI
-from cg.constants import GenePanelMasterList
 from cg.constants.constants import DEFAULT_CAPTURE_KIT, StatusOptions
 from cg.constants.tb import AnalysisType
 from cg.exc import BedVersionNotFoundError, LimsDataError
-from cg.models.orders.sample_base import SexEnum
 from cg.services.analysis_starter.configurator.file_creators import mip_dna_config
 from cg.services.analysis_starter.configurator.file_creators.mip_dna_config import (
     MIPDNAConfigFileCreator,
 )
-from cg.store.models import Application, ApplicationVersion, BedVersion, Case, CaseSample, Sample
+from cg.store.models import BedVersion, Case, CaseSample, Sample
 from cg.store.store import Store
-
-
-@pytest.fixture
-def expected_content_wgs() -> dict:
-    """Fixture to provide expected content for the MIP DNA config file."""
-    return {
-        "case": "case_id",
-        "default_gene_panels": [GenePanelMasterList.OMIM_AUTO],
-        "samples": [
-            {
-                "analysis_type": AnalysisType.WGS,
-                "capture_kit": "default.bed",
-                "expected_coverage": 26,
-                "father": "0",
-                "mother": "0",
-                "phenotype": StatusOptions.AFFECTED,
-                "sample_display_name": "sample_name",
-                "sample_id": "sample_id",
-                "sex": SexEnum.male,
-            },
-        ],
-    }
-
-
-@pytest.fixture
-def expected_content_wes() -> dict:
-    """Fixture to provide expected content for the MIP DNA config file."""
-    return {
-        "case": "case_id",
-        "default_gene_panels": [GenePanelMasterList.OMIM_AUTO, GenePanelMasterList.PANELAPP_GREEN],
-        "samples": [
-            {
-                "analysis_type": AnalysisType.WES,
-                "capture_kit": "mock_bed_version.bed",
-                "expected_coverage": 26,
-                "father": "0",
-                "mother": "mother_id",
-                "phenotype": StatusOptions.AFFECTED,
-                "sample_display_name": "sample_name",
-                "sample_id": "sample_id",
-                "sex": SexEnum.male,
-            },
-        ],
-    }
-
-
-@pytest.fixture
-def case_id() -> str:
-    return "case_id"
-
-
-@pytest.fixture
-def wgs_sample() -> Sample:
-    application: Application = create_autospec(Application, min_sequencing_depth=26)
-    application_version: ApplicationVersion = create_autospec(
-        ApplicationVersion, application=application
-    )
-    sample = create_autospec(
-        Sample,
-        internal_id="sample_id",
-        sex=SexEnum.male,
-        prep_category=AnalysisType.WGS,
-        application_version=application_version,
-    )
-    sample.name = "sample_name"
-    return sample
-
-
-@pytest.fixture
-def wgs_case_sample(wgs_sample: Sample) -> CaseSample:
-    return create_autospec(
-        CaseSample, father=None, mother=None, sample=wgs_sample, status=StatusOptions.AFFECTED
-    )
-
-
-@pytest.fixture
-def wgs_mock_store(case_id: str, wgs_case_sample: CaseSample) -> Store:
-    case: Case = create_autospec(
-        Case, internal_id=case_id, links=[wgs_case_sample], panels=[GenePanelMasterList.OMIM_AUTO]
-    )
-    wgs_case_sample.case = case
-    store: Store = create_autospec(Store)
-    store.get_case_by_internal_id_strict = Mock(return_value=case)
-    return store
 
 
 def test_create_wgs_no_bed_flag(
@@ -256,46 +170,6 @@ def test_create_wgs_only_one_sample_phenotype_unknown(
     mock_write.assert_called_once_with(
         content=expected_content_wgs, file_path=Path("root", case_id, "pedigree.yaml")
     )
-
-
-@pytest.fixture
-def wes_sample() -> Sample:
-    application: Application = create_autospec(Application, min_sequencing_depth=26)
-    application_version: ApplicationVersion = create_autospec(
-        ApplicationVersion, application=application
-    )
-    sample = create_autospec(
-        Sample,
-        internal_id="sample_id",
-        sex=SexEnum.male,
-        prep_category=AnalysisType.WES,
-        application_version=application_version,
-        from_sample=None,
-    )
-    sample.name = "sample_name"
-    return sample
-
-
-@pytest.fixture
-def wes_mock_store(case_id: str, wes_sample: Sample) -> Store:
-    mother = create_autospec(Sample, internal_id="mother_id")
-    case_sample: CaseSample = create_autospec(
-        CaseSample, father=None, mother=mother, sample=wes_sample, status=StatusOptions.AFFECTED
-    )
-    case: Case = create_autospec(
-        Case,
-        internal_id=case_id,
-        links=[case_sample],
-        panels=[GenePanelMasterList.OMIM_AUTO, GenePanelMasterList.PANELAPP_GREEN],
-        samples=[wes_sample],
-    )
-    case_sample.case = case
-    store: Store = create_autospec(Store)
-    store.get_case_by_internal_id_strict = Mock(return_value=case)
-
-    bed_version: BedVersion = create_autospec(BedVersion, filename="mock_bed_version.bed")
-    store.get_bed_version_by_short_name_strict = Mock(return_value=bed_version)
-    return store
 
 
 def test_create_wes_no_bed_flag(
