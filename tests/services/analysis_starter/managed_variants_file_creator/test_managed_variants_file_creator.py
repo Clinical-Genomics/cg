f999ca96d03510e05b874bb0a353debbe8860309
islean <isak.ohlsson.angnell@gmail.com>
2025-08-13 15:19:30 +0200

    Refactor tests

--- tests/services/analysis_starter/managed_variants_file_creator/test_managed_variants_file_creator.py
+++ tests/services/analysis_starter/managed_variants_file_creator/test_managed_variants_file_creator.py
@@ -1,29 +1,57 @@
+from pathlib import Path
+from unittest.mock import Mock, create_autospec
+
+import pytest
 from pytest_mock import MockerFixture
 
 from cg.apps.scout.scoutapi import ScoutAPI
+from cg.constants import FileExtensions, Workflow
 from cg.services.analysis_starter.configurator.file_creators import managed_variants
 from cg.services.analysis_starter.configurator.file_creators.managed_variants import (
     ManagedVariantsFileCreator,
 )
+from cg.store.models import Case
+from cg.store.store import Store
+
+
+@pytest.fixture
+def managed_variants_file_content() -> list[str]:
+    return ["variant_from_scout1", "variant_from_scout2"]
+
+
+@pytest.fixture
+def managed_variants_creator(
+    managed_variants_file_content: list[str],
+) -> ManagedVariantsFileCreator:
+
+    store: Store = create_autospec(Store)
+    case = create_autospec(Case, data_analysis=Workflow.RAREDISEASE)
+    store.get_case_by_internal_id = Mock(return_value=case)
+
+    scout_api: ScoutAPI = create_autospec(ScoutAPI)
+    scout_api.export_managed_variants = Mock(return_value=managed_variants_file_content)
+
+    return ManagedVariantsFileCreator(
+        store=store,
+        scout_api=scout_api,
+    )
 
 
 def test_managed_variants_content(
-    raredisease_managed_variants_creator: ManagedVariantsFileCreator,
-    raredisease_case_id: str,
-    raredisease_managed_variants_file_content: list[str],
+    managed_variants_creator: ManagedVariantsFileCreator,
+    managed_variants_file_content: list[str],
     mocker: MockerFixture,
 ):
     """Test that the managed variants file content is created correctly."""
-    # GIVEN a Raredisease managed variants file content creator and a case path
 
-    # GIVEN a mock of Scout variants
-    mocker.patch.object(managed_variants, "write_txt")
+    # GIVEN a mocked write_txt
+    mocked_write_txt = mocker.patch.object(managed_variants, "write_txt")
 
     # WHEN creating a managed variants file
-    expected_content_fixture: list[str] = raredisease_managed_variants_file_content
-    mocker.patch.object(ScoutAPI, "export_managed_variants", return_value=expected_content_fixture)
-
-    raredisease_managed_variants_creator.create(raredisease_case_id)
+    managed_variants_creator.create(case_id="case_id", case_path=Path("case_path"))
 
     # THEN the content of the file is the expected
-    assert content == expected_content_fixture
+    file_path = Path("case_path", "managed_variants").with_suffix(FileExtensions.VCF)
+    mocked_write_txt.assert_called_once_with(
+        file_path=file_path, content=managed_variants_file_content
+    )
