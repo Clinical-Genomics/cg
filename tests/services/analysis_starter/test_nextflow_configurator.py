3222729a4287ef25aae5b0f9d1b38e213162b4b6
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2025-08-19 14:45:35 +0200

    WIP

--- tests/services/analysis_starter/test_nextflow_configurator.py
+++ tests/services/analysis_starter/test_nextflow_configurator.py
@@ -1,9 +1,23 @@
 from pathlib import Path
+from unittest.mock import create_autospec
 
 import pytest
 from pytest_mock import MockerFixture
 
 from cg.constants import Workflow
+from cg.models.cg_config import RarediseaseConfig
+from cg.services.analysis_starter.configurator.extensions.abstract import PipelineExtension
+from cg.services.analysis_starter.configurator.file_creators.nextflow.config_file import (
+    NextflowConfigFileCreator,
+)
+from cg.services.analysis_starter.configurator.file_creators.nextflow.params_file.abstract import (
+    ParamsFileCreator,
+)
+from cg.services.analysis_starter.configurator.file_creators.nextflow.sample_sheet.creator import (
+    NextflowSampleSheetCreator,
+)
+from cg.services.analysis_starter.configurator.implementations.nextflow import NextflowConfigurator
+from cg.store.store import Store
 
 
 @pytest.mark.parametrize(
@@ -83,5 +97,26 @@ def test_get_case_config_none_flags(
     # THEN we should get back a case config without altering the pre-run-script
     assert case_config.pre_run_script is not None
 
-def test_MIPDNA_configure():
-    pass
+
+def test_raredisease_configure(mocker: MockerFixture):
+    pipeline_config = create_autospec(
+        RarediseaseConfig,
+        root="/root",
+        repository="http://repo.scilifelab.se",
+        revision="rev123",
+        profile="profile_raredisease",
+        pre_run_script="some_script.sh",
+    )
+
+    configurator = NextflowConfigurator(
+        config_file_creator=create_autospec(NextflowConfigFileCreator),
+        params_file_creator=create_autospec(ParamsFileCreator),
+        pipeline_config=pipeline_config,
+        sample_sheet_creator=create_autospec(NextflowSampleSheetCreator),
+        store=create_autospec(Store),
+        pipeline_extension=create_autospec(PipelineExtension),
+    )
+
+    mocker.patch.object(Path, "mkdir")
+
+    configurator.configure(case_id="case_id")
