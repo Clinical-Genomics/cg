88c24adbfc2702af5d5a1f24d1523c7db88ac0a5
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-08-28 14:05:07 +0200

    WIP MIP tracker

--- cg/services/analysis_starter/tracker/implementations/mip_dna.py
+++ cg/services/analysis_starter/tracker/implementations/mip_dna.py
@@ -1,3 +1,5 @@
+from pathlib import Path
+
 from cg.services.analysis_starter.configurator.models.mip_dna import MIPDNACaseConfig
 from cg.services.analysis_starter.tracker.tracker import Tracker
 
@@ -7,8 +9,8 @@ class MIPDNATracker(Tracker):
     def _workflow_manager(self):
         pass
 
-    def _get_job_ids_path(self, case_id: str):
-        pass
+    def _get_job_ids_path(self, case_id: str) -> Path:
+        return Path(self.workflow_root, case_id, "analysis", "slurm_job_ids.yaml")
 
     def _get_workflow_version(self, case_config: MIPDNACaseConfig) -> str:
         pass
--- tests/services/analysis_starter/tracker/test_mip_dna_tracker.py
+++ tests/services/analysis_starter/tracker/test_mip_dna_tracker.py
@@ -1,24 +1,36 @@
-from unittest.mock import create_autospec
+from unittest.mock import Mock, create_autospec
 
-from cg.services.analysis_starter.tracker.implementations.mip-dna import MIPDNATracker
+from cg.apps.tb import TrailblazerAPI
+from cg.services.analysis_starter.configurator.models.mip_dna import MIPDNACaseConfig
+from cg.services.analysis_starter.tracker.implementations.mip_dna import MIPDNATracker
+from cg.store.models import Case
 from cg.store.store import Store
-from tests.services.analysis_starter.test_mip_dna_configurator import mock_status_db
 
 
 def test_track():
     # GIVEN a StatusDB mock
     mock_status_db: Store = create_autospec(Store)
+    mock_status_db.get_case_by_internal_id = Mock(return_value=create_autospec(Case))
+
+    # GIVEN a mock TrailblazerAPI
+    mock_trailblazer_api: TrailblazerAPI = create_autospec(TrailblazerAPI)
 
     # GIVEN MIP-DNA tracker
-    tracker = MIPDNATracker(store=mock_status_db)
+    tracker = MIPDNATracker(
+        store=mock_status_db, trailblazer_api=mock_trailblazer_api, workflow_root="/some/root"
+    )
 
     # GIVEN MIP-DNA case config
+    case_config = MIPDNACaseConfig(
+        case_id="some_case",
+        email="some_email",
+        slurm_qos="some_qos",
+    )
+
     # WHEN calling track
-    tracker.track()
+    tracker.track(case_config=case_config)
 
     # THEN analysis object should have been created in StatusDB
     mock_status_db.add_analysis.assert_called_with()
 
-
     # THEN analysis object should have been created in Trailblazer
-
