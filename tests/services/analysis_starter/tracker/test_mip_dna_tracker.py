ea5b07c4edb2cca532680bcdea08a8ee709763e6
islean <isak.ohlsson.angnell@gmail.com>
2025-08-28 14:26:55 +0200

    Mock tb in test

--- tests/services/analysis_starter/tracker/test_mip_dna_tracker.py
+++ tests/services/analysis_starter/tracker/test_mip_dna_tracker.py
@@ -1,6 +1,8 @@
 from unittest.mock import Mock, create_autospec
 
 from cg.apps.tb import TrailblazerAPI
+from cg.apps.tb.models import TrailblazerAnalysis
+from cg.constants import Priority, Workflow
 from cg.services.analysis_starter.configurator.models.mip_dna import MIPDNACaseConfig
 from cg.services.analysis_starter.tracker.implementations.mip_dna import MIPDNATracker
 from cg.store.models import Case
@@ -8,12 +10,20 @@ from cg.store.store import Store
 
 
 def test_track():
+    # GIVEN a case
+    case: Case = create_autospec(Case, data_analysis=Workflow.MIP_DNA, priority=Priority.standard)
+
     # GIVEN a StatusDB mock
     mock_status_db: Store = create_autospec(Store)
-    mock_status_db.get_case_by_internal_id = Mock(return_value=create_autospec(Case))
+    mock_status_db.get_case_by_internal_id = Mock(return_value=case)
 
     # GIVEN a mock TrailblazerAPI
     mock_trailblazer_api: TrailblazerAPI = create_autospec(TrailblazerAPI)
+    mock_trailblazer_api.add_pending_analysis = Mock(
+        return_value=TrailblazerAnalysis(
+            id=1, logged_at=None, started_at=None, completed_at=None, out_dir=None, config_path=None
+        )
+    )
 
     # GIVEN MIP-DNA tracker
     tracker = MIPDNATracker(
