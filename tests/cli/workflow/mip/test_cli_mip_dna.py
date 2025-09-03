3c14ba303ef775c253a3c738e9730c08c167ea83
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2025-09-03 10:08:34 +0200

    deparameterize tests

--- tests/cli/workflow/mip/test_cli_mip_dna.py
+++ tests/cli/workflow/mip/test_cli_mip_dna.py
@@ -35,31 +35,7 @@ def cg_config() -> CGConfig:
     )
 
 
-@pytest.mark.parametrize(
-    "cli_args, start_after, start_with, use_bwa_mem",
-    [
-        (["case_id"], None, None, False),
-        (
-            [
-                "--start-with",
-                "dinkel_bread",
-                "--start-after",
-                "levain_bread",
-                "--use-bwa-mem",
-                "case_id",
-            ],
-            "levain_bread",
-            "dinkel_bread",
-            True,
-        ),
-    ],
-    ids=["No arguments", "All arguments"],
-)
-def test_mip_dna_dev_run(
-    cli_args: list[str],
-    start_after: str | None,
-    start_with: str | None,
-    use_bwa_mem: bool,
+def test_mip_dna_dev_run_no_flags(
     cg_config: CGConfig,
     mocker: MockerFixture,
 ):
@@ -73,8 +49,11 @@ def test_mip_dna_dev_run(
     )
     mock_run = mocker.patch.object(AnalysisStarter, "run")
 
+    # GIVEN no flags
+    flags: list[str] = ["case_id"]
+
     # WHEN invoking cg workflow mip-dna dev-run
-    result = cli_runner.invoke(dev_run, cli_args, obj=cg_config)
+    result = cli_runner.invoke(dev_run, flags, obj=cg_config)
 
     # THEN the command exits successfully
     assert result.exit_code == 0
@@ -82,39 +61,43 @@ def test_mip_dna_dev_run(
     # THEN the analysis starter should have been created and called
     get_analysis_starter_spy.assert_called_once_with(ANY, Workflow.MIP_DNA)
     mock_run.assert_called_once_with(
-        case_id="case_id", start_after=start_after, start_with=start_with, use_bwa_mem=use_bwa_mem
+        case_id="case_id", start_after=None, start_with=None, use_bwa_mem=False
     )
 
 
-@pytest.mark.parametrize(
-    "cli_args, panel_bed, start_after, start_with, use_bwa_mem",
-    [
-        (["case_id"], None, None, None, False),
-        (
-            [
-                "--panel-bed",
-                "panel.bed",
-                "--start-with",
-                "dinkel_bread",
-                "--start-after",
-                "levain_bread",
-                "--use-bwa-mem",
-                "case_id",
-            ],
-            "panel.bed",
-            "levain_bread",
-            "dinkel_bread",
-            True,
-        ),
-    ],
-    ids=["No arguments", "All arguments"],
-)
-def test_mip_dna_dev_start(
-    cli_args: list[str],
-    panel_bed: str | None,
-    start_after: str | None,
-    start_with: str | None,
-    use_bwa_mem: bool,
+def test_mip_dna_dev_run_all_flags(
+    cg_config: CGConfig,
+    mocker: MockerFixture,
+):
+    # GIVEN a CLI runner
+    cli_runner = CliRunner()
+
+    # GIVEN a CGConfig with configuration info for MIP-DNA
+
+    get_analysis_starter_spy = mocker.spy(
+        AnalysisStarterFactory, "get_analysis_starter_for_workflow"
+    )
+    mock_run = mocker.patch.object(AnalysisStarter, "run")
+
+    # GIVEN all possible flags
+    start_after = "levain_bread"
+    start_with = "dinkel_bread"
+    flags = ["--start-with", start_with, "--start-after", start_after, "--use-bwa-mem", "case_id"]
+
+    # WHEN invoking cg workflow mip-dna dev-run
+    result = cli_runner.invoke(dev_run, flags, obj=cg_config)
+
+    # THEN the command exits successfully
+    assert result.exit_code == 0
+
+    # THEN the analysis starter should have been created and called
+    get_analysis_starter_spy.assert_called_once_with(ANY, Workflow.MIP_DNA)
+    mock_run.assert_called_once_with(
+        case_id="case_id", start_after=start_after, start_with=start_with, use_bwa_mem=True
+    )
+
+
+def test_mip_dna_dev_start_no_flags(
     cg_config: CGConfig,
     mocker: MockerFixture,
 ):
@@ -128,10 +111,63 @@ def test_mip_dna_dev_start(
     )
     mock_start = mocker.patch.object(AnalysisStarter, "start")
 
+    # GIVEN no cli flags
+    flags: list[str] = ["case_id"]
+
     # WHEN invoking cg workflow mip-dna dev-start
     result = cli_runner.invoke(
         dev_start,
-        cli_args,
+        flags,
+        obj=cg_config,
+    )
+
+    # THEN the command exits successfully
+    assert result.exit_code == 0
+
+    # THEN the analysis starter should have been created and called
+    get_analysis_starter_spy.assert_called_once_with(ANY, Workflow.MIP_DNA)
+    mock_start.assert_called_once_with(
+        case_id="case_id",
+        panel_bed=None,
+        start_after=None,
+        use_bwa_mem=False,
+        start_with=None,
+    )
+
+
+def test_mip_dna_dev_start_all_flags(
+    cg_config: CGConfig,
+    mocker: MockerFixture,
+):
+    # GIVEN a cli runner
+    cli_runner = CliRunner()
+
+    # GIVEN a CGConfig with configuration info for MIP-DNA
+
+    get_analysis_starter_spy = mocker.spy(
+        AnalysisStarterFactory, "get_analysis_starter_for_workflow"
+    )
+    mock_start = mocker.patch.object(AnalysisStarter, "start")
+
+    # GIVEN all possible flags
+    panel_bed = "panel.bed"
+    start_after = "levain_bread"
+    start_with = "dinkel_bread"
+    flags: list[str] = [
+        "--panel-bed",
+        panel_bed,
+        "--start-with",
+        start_with,
+        "--start-after",
+        start_after,
+        "--use-bwa-mem",
+        "case_id",
+    ]
+
+    # WHEN invoking cg workflow mip-dna dev-start
+    result = cli_runner.invoke(
+        dev_start,
+        flags,
         obj=cg_config,
     )
 
@@ -144,7 +180,7 @@ def test_mip_dna_dev_start(
         case_id="case_id",
         panel_bed=panel_bed,
         start_after=start_after,
-        use_bwa_mem=use_bwa_mem,
+        use_bwa_mem=True,
         start_with=start_with,
     )
 
