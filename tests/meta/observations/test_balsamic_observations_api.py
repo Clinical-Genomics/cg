0fc54180a0b799a28bc51f4a302bda892f0f732b
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2025-09-23 10:29:15 +0200

    Create test test_panel_upload

--- tests/meta/observations/test_balsamic_observations_api.py
+++ tests/meta/observations/test_balsamic_observations_api.py
@@ -5,9 +5,11 @@ from unittest.mock import Mock, create_autospec
 
 import pytest
 from _pytest.logging import LogCaptureFixture
-from pytest_mock import MockFixture
+from pytest_mock import MockFixture, MockerFixture
 
+from cg.apps.lims import LimsAPI
 from cg.constants.constants import CancerAnalysisType
+from cg.constants.observations import BalsamicObservationPanels
 from cg.constants.sequencing import SeqLibraryPrepCategory
 from cg.exc import LoqusdbDuplicateRecordError
 from cg.meta.observations.balsamic_observations_api import BalsamicObservationsAPI
@@ -16,6 +18,9 @@ from cg.models.cg_config import CGConfig
 from cg.models.observations.input_files import BalsamicObservationsInputFiles
 from cg.store.models import Bed, BedVersion, Case, Sample
 from cg.store.store import Store
+from tests.cli.workflow.fluffy.conftest import sample
+from tests.integration.test_start_available_mip_dna import status_db
+from tests.meta.upload.scout.conftest import lims_api
 
 
 def test_is_analysis_type_eligible_for_observations_upload_eligible_wgs(
@@ -339,3 +344,26 @@ def test_load_cancer_observations(
 
     # THEN the observations should be loaded successfully
     assert f"Uploaded {number_of_loaded_variants} variants to Loqusdb" in caplog.text
+
+def test_panel_upload(mocker: MockerFixture,):
+    # GIVEN a panel case with a TGS sample
+    sample: Sample = create_autospec(Sample, prep_category=SeqLibraryPrepCategory.TARGETED_GENOME_SEQUENCING)
+    case: Case = create_autospec(Case, internal_id = "case_id", samples=[sample])
+
+    # GIVEN that a known panel is set in LIMS
+    lims_api: LimsAPI = create_autospec(LimsAPI)
+    lims_api.capture_kit = Mock(return_value=BalsamicObservationPanels.LYMPHOID)
+
+    # GIVEN balsamic observations API
+    balsamic_observations_api = BalsamicObservationsAPI(
+        create_autospec(CGConfig,
+                        lims_api=lims_api,
+                        ),
+    )
+
+    # WHEN uploading
+    balsamic_observations_api.load_observations(case)
+
+    # THEN the correct loqusDB instance is selected
+    # THEN the case is uploaded
+
