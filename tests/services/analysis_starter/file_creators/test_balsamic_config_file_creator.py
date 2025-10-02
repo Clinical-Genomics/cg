946afb01e168171c3995cf4e0ce472394d1517c1
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-10-02 13:50:11 +0200

    WIP test balsamic config file creator

--- tests/services/analysis_starter/file_creators/test_balsamic_config_file_creator.py
+++ tests/services/analysis_starter/file_creators/test_balsamic_config_file_creator.py
@@ -1,28 +1,39 @@
+from unittest.mock import Mock, create_autospec
+
+from cg.constants import SexOptions
+from cg.services.analysis_starter.configurator.file_creators.balsamic_config import (
+    BalsamicConfigFileCreator,
+)
+from cg.store.models import Application, ApplicationVersion, Case, Sample
+from cg.store.store import Store
+
+
 def test_create_wgs_tumor_only():
     # GIVEN a case with one tumor WGS sample
-    wgs_tumor_only_case: Mock[Case] = create_autospec(
-        Case, data_analysis="balsamic", internal_id="case_1"
-    )
-    application: Mock[Application] = create_autospec(Application, prep_category="wgs")
-    application_version: Mock[ApplicationVersion] = create_autospec(
+    application: Application = create_autospec(Application, prep_category="wgs")
+    application_version: ApplicationVersion = create_autospec(
         ApplicationVersion, application=application
     )
-    sample_1: Mock[Sample] = create_autospec(
+    sample_1: Sample = create_autospec(
         Sample,
         internal_id="sample_1",
         is_tumour=True,
         sex=SexOptions.FEMALE,
         application_version=application_version,
     )
-    wgs_tumor_only_case.samples = [sample_1]
-    balsamic_configurator.store.get_case_by_internal_id = Mock(return_value=wgs_tumor_only_case)
-
-    # WHEN building the CLI input
-    cli_input: BalsamicConfigInput = balsamic_configurator._build_cli_input(
-        case_id=wgs_tumor_only_case.internal_id
+    wgs_tumor_only_case: Case = create_autospec(
+        Case, data_analysis="balsamic", internal_id="case_1", samples=[sample_1]
     )
+    store: Store = create_autospec(Store)
+    store.get_case_by_internal_id = Mock(return_value=wgs_tumor_only_case)
 
-    # THEN pydantic model should be created (and validated)
+    # GIVEN a BalsamicConfigFileCreator
+    config_file_creator = BalsamicConfigFileCreator()
+
+    # WHEN creating the config file
+    config_file_creator.create()
+
+    # THEN
     assert isinstance(cli_input, BalsamicConfigInput)
 
     # THEN the correct normal sample name should be set
