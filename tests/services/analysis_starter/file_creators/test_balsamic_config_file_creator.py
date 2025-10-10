f340c9bece777d4119ecf47a5ec9cecce68573db
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2025-10-10 09:53:56 +0200

    WIP

--- tests/services/analysis_starter/file_creators/test_balsamic_config_file_creator.py
+++ tests/services/analysis_starter/file_creators/test_balsamic_config_file_creator.py
@@ -186,5 +186,44 @@ def test_create_wgs_tumor_only(
     )
 
 
-def test_create_wes_paired(expected_wes_paired_command: str, mocker: MockerFixture):
-    pass
+def test_create_wes_paired(
+    balsamic_config: BalsamicConfig, expected_wes_paired_command: str, mocker: MockerFixture
+):
+    # GIVEN a case with one tumor and one normal WGS samples
+    tumour_sample: Sample = create_autospec(
+        Sample,
+        internal_id="sample_tumour",
+        is_tumour=True,
+        sex=SexOptions.FEMALE,
+        prep_category=SeqLibraryPrepCategory.WHOLE_EXOME_SEQUENCING,
+    )
+    normal_sample: Sample = create_autospec(
+        Sample,
+        internal_id="sample_normal",
+        is_tumour=False,
+        sex=SexOptions.FEMALE,
+        prep_category=SeqLibraryPrepCategory.WHOLE_EXOME_SEQUENCING,
+    )
+    wgs_tumor_only_case: Case = create_autospec(
+        Case, data_analysis="balsamic", internal_id="case_1", samples=[tumour_sample, normal_sample]
+    )
+    store: Store = create_autospec(Store)
+    store.get_case_by_internal_id = Mock(return_value=wgs_tumor_only_case)
+
+    # GIVEN a Lims API
+
+    # GIVEN a BalsamicConfigFileCreator
+    config_file_creator = BalsamicConfigFileCreator(
+        status_db=store, lims_api=Mock(), cg_balsamic_config=balsamic_config
+    )
+
+    # GIVEN that the subprocess exits successfully
+    mock_runner = mocker.patch.object(creator.subprocess, "run")
+
+    # WHEN creating the config file
+    config_file_creator.create(case_id="case_1")
+
+    # THEN the expected command is called
+    mock_runner.assert_called_once_with(
+        args=expected_wes_paired_command, check=True, shell=True, stderr=-1, stdout=-1
+    )
