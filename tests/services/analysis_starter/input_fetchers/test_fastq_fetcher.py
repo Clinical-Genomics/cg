a5c88a019c121d757856454cef8bf280999c02e3
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2025-10-13 14:25:10 +0200

    WIP

--- tests/services/analysis_starter/input_fetchers/test_fastq_fetcher.py
+++ tests/services/analysis_starter/input_fetchers/test_fastq_fetcher.py
@@ -1,6 +1,9 @@
+from pathlib import Path
 from unittest.mock import Mock, create_autospec
 
 from cg.apps.housekeeper.hk import HousekeeperAPI
+from cg.meta.compress.compress import CompressAPI
+from cg.models.compression_data import CaseCompressionData, CompressionData, SampleCompressionData
 from cg.services.analysis_starter.input_fetcher.implementations.fastq_fetcher import FastqFetcher
 from cg.store.models import Case, Sample
 from cg.store.store import Store
@@ -14,6 +17,17 @@ def test_ensure_files_are_ready():
 
     housekeeper_api: HousekeeperAPI = create_autospec(HousekeeperAPI)
     housekeeper_api.get_archived_files_for_bundle = Mock(return_value=[])
+
+    compress_api: CompressAPI = create_autospec(CompressAPI)
+    compression_data = CompressionData(stub=Path("fastq_file"))
+    sample_compression_data = SampleCompressionData(
+        sample_id="sample_id", compression_objects=[compression_data]
+    )
+    compress_api.get_case_compression_data = Mock(
+        return_value=CaseCompressionData(
+            case_id="case_id", sample_compression_data=[sample_compression_data]
+        )
+    )
 
     fastq_fetcher = FastqFetcher(
         compress_api=Mock(),
