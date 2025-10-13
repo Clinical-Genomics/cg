21c0e7fab8254369451ff75cbd6a86367c7a7367
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-10-13 10:04:19 +0200

    Revert "Switch start commands for MIP-DNA (#4648)(major)"
    This reverts commit 5e3424d1c79707ee99e3383246f5438c45e5c5da.

---
+++ tests/cli/workflow/mip/test_cli_mip_dna_config_case.py
@@ -0,0 +1,51 @@
+""" Test the CLI for mip-dna config-case"""
+
+import logging
+
+from cg.cli.workflow.mip_dna.base import config_case
+
+
+def test_cg_workflow_mip_dna_config_case_dry_run(cli_runner, caplog, case_id, mip_dna_context):
+    """Test print the MIP DNA case config command to console."""
+
+    caplog.set_level(logging.INFO)
+
+    # GIVEN a cli function
+
+    # WHEN we run a case in dry run mode
+    result = cli_runner.invoke(config_case, ["--dry-run", case_id], obj=mip_dna_context)
+
+    # THEN the command should be printed
+    assert result.exit_code == 0
+
+
+def test_cg_workflow_mip_dna_config_case(cli_runner, caplog, case_id, mip_dna_context):
+    """Test print the MIP dna case config command to file"""
+
+    caplog.set_level(logging.INFO)
+
+    # GIVEN a cli function
+
+    # WHEN we run a case in dry run mode
+    result = cli_runner.invoke(config_case, [case_id], obj=mip_dna_context)
+
+    # THEN the command should be printed
+    assert result.exit_code == 0
+    assert f"Config file saved to" in caplog.text
+
+
+def test_cg_workflow_mip_dna_config_case_error(cli_runner, caplog, case_id, mip_dna_context):
+    """Test wrong case id with MIP DNA case config command."""
+
+    caplog.set_level(logging.INFO)
+
+    # GIVEN a cli function
+
+    # WHEN we run a case in dry run mode
+    result = cli_runner.invoke(config_case, ["not_a_case_id"], obj=mip_dna_context)
+
+    # THEN the command should return an exit fail code
+    assert result.exit_code == 1
+
+    # THEN an error should be logged
+    assert f"could not be found in Status DB" in caplog.text
