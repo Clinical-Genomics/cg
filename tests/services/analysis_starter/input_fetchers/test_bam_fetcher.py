d80647cd51e08a96453e35d8b637468143b5d2b9
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2025-11-03 11:51:24 +0100

    WIP

--- tests/services/analysis_starter/input_fetchers/test_bam_fetcher.py
+++ tests/services/analysis_starter/input_fetchers/test_bam_fetcher.py
@@ -1,15 +1,40 @@
-from unittest.mock import create_autospec
+from unittest.mock import Mock, create_autospec
 
+import pytest
+
+from cg.exc import AnalysisNotReadyError
 from cg.services.analysis_starter.input_fetcher.implementations.bam_fetcher import BamFetcher
+from cg.store.models import Case, Sample
 from cg.store.store import Store
 
 
 def test_ensure_files_are_ready_success():
     # GIVEN a case with all BAM files on disk
+    status_db: Store = create_autospec(Store)
+    sample: Sample = create_autospec(Sample)
+    case: Case = create_autospec(Case, samples=[sample])
+    status_db.get_case_by_internal_id_strict = Mock(return_value=case)
 
     # GIVEN a BamFetcher
-    bam_fetcher = BamFetcher(status_db=create_autospec(Store))
+    bam_fetcher = BamFetcher(status_db=status_db)
 
     # WHEN ensuring that all files are ready
+    bam_fetcher.ensure_files_are_ready("case_id")
+
     # THEN no error is raised
-    # THEN no error is raised
+
+
+def test_ensure_files_are_ready_failure():
+    # GIVEN a case with all BAM files on disk
+    status_db: Store = create_autospec(Store)
+    sample: Sample = create_autospec(Sample)
+    case: Case = create_autospec(Case, samples=[sample])
+    status_db.get_case_by_internal_id_strict = Mock(return_value=case)
+
+    # GIVEN a BamFetcher
+    bam_fetcher = BamFetcher(status_db=status_db)
+
+    # WHEN ensuring that all files are ready
+    # THEN an error is raised
+    with pytest.raises(AnalysisNotReadyError):
+        bam_fetcher.ensure_files_are_ready("case_id")
