f0a114f7bbfd2da1d3cb40f8d7c3801a81c1e4ef
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2025-11-03 13:52:37 +0100

    Green state

--- tests/services/analysis_starter/input_fetchers/test_bam_fetcher.py
+++ tests/services/analysis_starter/input_fetchers/test_bam_fetcher.py
@@ -2,34 +2,37 @@ from unittest.mock import Mock, create_autospec
 
 import pytest
 from housekeeper.store.models import File
+from pytest_mock import MockerFixture
 from sqlalchemy.orm import Query
 
 from cg.apps.housekeeper.hk import HousekeeperAPI
 from cg.exc import AnalysisNotReadyError
 from cg.services.analysis_starter.input_fetcher.implementations.bam_fetcher import BamFetcher
+from cg.services.analysis_starter.input_fetcher.implementations.bam_fetcher import Path as bam_path
 from cg.store.models import Case, Sample
 from cg.store.store import Store
 
 
-def test_ensure_files_are_ready_success():
+def test_ensure_files_are_ready_success(mocker: MockerFixture):
     # GIVEN a case with all BAM files on disk
     status_db: Store = create_autospec(Store)
     sample: Sample = create_autospec(Sample)
     case: Case = create_autospec(Case, samples=[sample])
     status_db.get_case_by_internal_id_strict = Mock(return_value=case)
 
-    # GIVEN a BamFetcher
-    bam_fetcher = BamFetcher(status_db=status_db, housekeeper_api=Mock())
-
     housekeeper_api: HousekeeperAPI = create_autospec(HousekeeperAPI)
     query = create_autospec(Query)
     query.all = Mock(
         return_value=[
-            create_autospec(File, full_path="non/existing/file1"),
-            create_autospec(File, full_path="non/existing/file2"),
+            create_autospec(File, full_path="non/existing/file"),
         ]
     )
     housekeeper_api.files = Mock(return_value=query)
+
+    mocker.patch.object(bam_path, "is_file", return_value=True)
+
+    # GIVEN a BamFetcher
+    bam_fetcher = BamFetcher(status_db=status_db, housekeeper_api=housekeeper_api)
 
     # WHEN ensuring that all files are ready
     bam_fetcher.ensure_files_are_ready("case_id")
@@ -37,7 +40,7 @@ def test_ensure_files_are_ready_success():
     # THEN no error is raised
 
 
-def test_ensure_files_are_ready_failure():
+def test_ensure_files_are_ready_failure(mocker: MockerFixture):
     # GIVEN a case with all BAM files on disk
     status_db: Store = create_autospec(Store)
     sample: Sample = create_autospec(Sample)
@@ -53,6 +56,7 @@ def test_ensure_files_are_ready_failure():
         ]
     )
     housekeeper_api.files = Mock(return_value=query)
+    mocker.patch.object(bam_path, "is_file", return_value=False)
 
     # GIVEN a BamFetcher
     bam_fetcher = BamFetcher(status_db=status_db, housekeeper_api=housekeeper_api)
