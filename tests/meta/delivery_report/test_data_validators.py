57961496916f5f9d79cd337fd884af663c0165c3
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2025-11-06 13:52:15 +0100

    Test fot Tomte

--- tests/meta/delivery_report/test_data_validators.py
+++ tests/meta/delivery_report/test_data_validators.py
@@ -19,16 +19,21 @@ from cg.meta.delivery_report.data_validators import (
 )
 from cg.meta.delivery_report.raredisease import RarediseaseDeliveryReportAPI
 from cg.meta.delivery_report.rnafusion import RnafusionDeliveryReportAPI
+from cg.meta.delivery_report.tomte import TomteDeliveryReportAPI
 from cg.meta.workflow.rnafusion import RnafusionAnalysisAPI
+from cg.meta.workflow.tomte import TomteAnalysisAPI
 from cg.models.analysis import NextflowAnalysis
 from cg.models.delivery.delivery import DeliveryFile
 from cg.models.delivery_report.report import ReportModel
 from cg.models.orders.sample_base import SexEnum
 from cg.models.rnafusion.rnafusion import RnafusionQCMetrics
+from cg.models.tomte.tomte import TomteQCMetrics
 from cg.store.models import Analysis, Application, ApplicationVersion, Case, CaseSample, Sample
 from cg.store.store import Store
 
 EXPECTED_RNAFUSION_QC_TABLE_WTS = """<table class="table w-auto text-center align-middle" aria-describedby="qc-metrics-table">\n    <thead class="table-secondary">\n      <tr>\n        <th scope="col">Kvalitetsmått</th>\n        \n          <th scope="col">sample_name (sample_id)</th>\n        \n        <th scope="col">Förklaringar</th>\n      </tr>\n    </thead>\n    <tbody>\n      \n      <tr>\n        <th scope="row">RNA Integrity Number (RIN)</th>\n        \n          <td>1.0</td>\n        \n        <td class="text-start">Mått på skala från 1 till 10 för kvalitet och integritet hos RNA-prover. N/A som värde är en varning om att inget tillförlitligt RIN är tillgängligt för det specifika provet.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">DV200</th>\n        \n          <td>1.0</td>\n        \n        <td class="text-start">Andelen RNA-fragment längre än 200 nukleotider.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Ingående mängd [ng]</th>\n        \n          <td>1.0</td>\n        \n        <td class="text-start">Mängd RNA som används för sekvensering.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Läspar [M]</th>\n        \n          <td>0.0</td>\n        \n        <td class="text-start">Antal sekvenseringsläsningar i miljoner läspar.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Mappade sekvenser [%]</th>\n        \n          <td>200.0</td>\n        \n        <td class="text-start">Procent sekvenser som matchar en eller flera positioner i referensgenomet.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Unikt mappade sekvenser [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Procentandel sekvenserade läsningar som endast matchar en position i referensgenomet.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Duplikat [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Sekvenseringsläsningar som är duplikat (kopior) och därmed ej unika sekvenser. Hög mängd duplikat kan tyda på dålig komplexitet av sekvenserat bibliotek eller djup sekvensering. Observera att dupliceringar är vanligt förekommande vid WTS.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Adapter [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Andel läsningar som innehåller adaptersekvenser.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">5&#39;-3&#39; bias</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Bias är förhållandet mellan antalet läsningar vid 3&#39;-änden och antalet läsningar vid 5&#39;-änden av fragmentet. Ett förhållande på 1 indikerar brist på bias, medan värden större eller mindre än 1 indikerar 3&#39;- eller 5&#39;-bias, respektive.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Sekvenser som klarar filter [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Procentandel läsningar som passerar kvalitetskontrollfilter.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Genomsnittlig läslängd efter filtrering [bp]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Genomsnittlig längd på läsningar som passerar kvalitetskontrollfilter.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Q20-baser [%]</th>\n        \n          <td>10.0</td>\n        \n        <td class="text-start">Andel baser med en Phred-poäng på minst 20.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Q30-baser [%]</th>\n        \n          <td>10.0</td>\n        \n        <td class="text-start">Andel baser med en Phred-poäng på minst 30.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">GC innehåll [%]</th>\n        \n          <td>10.0</td>\n        \n        <td class="text-start">Andel guanin- och cytosinbaser i ett prov, beräknat på trimmade läsningar.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">mRNA baser [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Andel baser i ett prov som härstammar från budbärar-RNA.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Ribosomala baser [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Andel baser i ett prov som härstammar från ribosomalt RNA.</td>\n      </tr>\n      \n    </tbody>\n  </table>\n\n\n"""
+
+EXPECTED_TOMTE_QC_TABLE_WTS = """<table class="table w-auto text-center align-middle" aria-describedby="qc-metrics-table">\n    <thead class="table-secondary">\n      <tr>\n        <th scope="col">Kvalitetsmått</th>\n        \n          <th scope="col">sample_name (sample_id)</th>\n        \n        <th scope="col">Förklaringar</th>\n      </tr>\n    </thead>\n    <tbody>\n      \n      <tr>\n        <th scope="row">RNA Integrity Number (RIN)</th>\n        \n          <td>1.0</td>\n        \n        <td class="text-start">Mått på skala från 1 till 10 för kvalitet och integritet hos RNA-prover. N/A som värde är en varning om att inget tillförlitligt RIN är tillgängligt för det specifika provet.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">DV200</th>\n        \n          <td>1.0</td>\n        \n        <td class="text-start">Andelen RNA-fragment längre än 200 nukleotider.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Ingående mängd [ng]</th>\n        \n          <td>1.0</td>\n        \n        <td class="text-start">Mängd RNA som används för sekvensering.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Läspar [M]</th>\n        \n          <td>0.0</td>\n        \n        <td class="text-start">Antal sekvenseringsläsningar i miljoner läspar.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Unikt mappade sekvenser [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Procentandel sekvenserade läsningar som endast matchar en position i referensgenomet.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Duplikat [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Sekvenseringsläsningar som är duplikat (kopior) och därmed ej unika sekvenser. Hög mängd duplikat kan tyda på dålig komplexitet av sekvenserat bibliotek eller djup sekvensering. Observera att dupliceringar är vanligt förekommande vid WTS.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Adapter [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Andel läsningar som innehåller adaptersekvenser.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">5&#39;-3&#39; bias</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Bias är förhållandet mellan antalet läsningar vid 3&#39;-änden och antalet läsningar vid 5&#39;-änden av fragmentet. Ett förhållande på 1 indikerar brist på bias, medan värden större eller mindre än 1 indikerar 3&#39;- eller 5&#39;-bias, respektive.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Sekvenser som klarar filter [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Procentandel läsningar som passerar kvalitetskontrollfilter.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Genomsnittlig läslängd efter filtrering [bp]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Genomsnittlig längd på läsningar som passerar kvalitetskontrollfilter.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Q20-baser [%]</th>\n        \n          <td>10.0</td>\n        \n        <td class="text-start">Andel baser med en Phred-poäng på minst 20.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Q30-baser [%]</th>\n        \n          <td>10.0</td>\n        \n        <td class="text-start">Andel baser med en Phred-poäng på minst 30.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">GC innehåll [%]</th>\n        \n          <td>10.0</td>\n        \n        <td class="text-start">Andel guanin- och cytosinbaser i ett prov, beräknat på trimmade läsningar.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">mRNA baser [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Andel baser i ett prov som härstammar från budbärar-RNA.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Ribosomala baser [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Andel baser i ett prov som härstammar från ribosomalt RNA.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Introniska baser [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Procent baser som ligger i intron. Om målet i den laboratoriska processen är att selektera transkript vars intron är borttagna är, så är ett högt värde avvikande.</td>\n      </tr>\n      \n      <tr>\n        <th scope="row">Intergeniska baser [%]</th>\n        \n          <td>0.1</td>\n        \n        <td class="text-start">Procent baser som anses ligga i regioner mellan gener. Om målet i den laboratoriska processen är att selektera ut kodande regioner i genomet, så är ett högt värde avvikande.</td>\n      </tr>\n      \n    </tbody>\n  </table>\n\n\n"""
 
 
 def test_get_empty_report_data(
@@ -168,6 +173,102 @@ def test_get_delivery_report_html_rnafusion(rnafusion_analysis: NextflowAnalysis
 
     # THEN the report is generated as expected
     assert EXPECTED_RNAFUSION_QC_TABLE_WTS in delivery_report
+
+
+@pytest.fixture
+def tomte_analysis() -> NextflowAnalysis:
+    return NextflowAnalysis(
+        sample_metrics={
+            "sample_id": TomteQCMetrics(
+                after_filtering_gc_content=0.1,
+                after_filtering_q20_rate=0.1,
+                after_filtering_q30_rate=0.1,
+                after_filtering_read1_mean_length=0.1,
+                before_filtering_total_reads=0.1,
+                median_5prime_to_3prime_bias=0.1,
+                pct_adapter=0.1,
+                pct_duplication=0.1,
+                pct_intergenic_bases=0.1,
+                pct_intronic_bases=0.1,
+                pct_mrna_bases=0.1,
+                pct_ribosomal_bases=0.1,
+                pct_surviving=0.1,
+                uniquely_mapped_percent=0.1,
+            )
+        },
+    )
+
+
+def test_get_delivery_report_html_tomte(tomte_analysis: NextflowAnalysis):
+    # GIVEN a store with a Tomte case linked to a sample
+    application_version = create_autospec(
+        ApplicationVersion, application=create_autospec(Application)
+    )
+    sample: Sample = create_autospec(
+        Sample,
+        sex=SexEnum.female,
+        internal_id="sample_id",
+        application_version=application_version,
+        is_tumour=False,
+    )
+    sample.name = "sample_name"
+    case: Case = create_autospec(
+        Case,
+        data_analysis=Workflow.TOMTE,
+        data_delivery=DataDelivery.ANALYSIS_FILES,
+        internal_id="case_id",
+        sample=[sample],
+    )
+
+    store: Store = create_autospec(Store)
+    store.get_case_by_internal_id = Mock(return_value=case)
+    case_sample = create_autospec(CaseSample, sample=sample, case=case)
+    store.get_case_samples_by_case_id = Mock(return_value=[case_sample])
+
+    # GIVEN a Delivery API
+    delivery_api: DeliveryAPI = create_autospec(DeliveryAPI)
+    delivery_api.is_analysis_delivery = Mock(return_value=True)
+    delivery_api.get_analysis_case_delivery_files = Mock(
+        return_value=[
+            create_autospec(
+                DeliveryFile, destination_path=Path("destination"), source_path=Path("source")
+            )
+        ]
+    )
+
+    # GIVEN a LIMS API
+    lims_api: LimsAPI = create_autospec(LimsAPI)
+    lims_api.has_sample_passed_initial_qc = Mock(return_value=True)
+
+    # GIVEN a Tomte Analysis API
+    analysis_api = create_autospec(
+        TomteAnalysisAPI,
+        chanjo_api=create_autospec(ChanjoAPI),
+        delivery_api=delivery_api,
+        housekeeper_api=create_autospec(HousekeeperAPI),
+        lims_api=lims_api,
+        scout_api=create_autospec(ScoutAPI),
+        status_db=store,
+        workflow=Workflow.TOMTE,
+    )
+    analysis_api.get_latest_metadata = Mock(return_value=tomte_analysis)
+
+    # GIVEN a RNAFusion delivery report API
+    delivery_report_api = TomteDeliveryReportAPI(analysis_api)
+
+    # WHEN generating a delivery report
+    delivery_report: str = delivery_report_api.get_delivery_report_html(
+        create_autospec(
+            Analysis,
+            comment="some comment",
+            workflow=Workflow.TOMTE,
+            workflow_version="1.0.0",
+        ),
+        force=False,
+    )
+
+    # THEN the report is generated as expected
+    assert EXPECTED_TOMTE_QC_TABLE_WTS in delivery_report
 
 
 def test_get_missing_report_data(
