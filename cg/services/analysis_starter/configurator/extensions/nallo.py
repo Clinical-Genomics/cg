1bce558f4816d22fcdaeed415bede4e71cd4557b
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-11-06 13:55:53 +0100

    address comment

--- cg/services/analysis_starter/configurator/extensions/nallo.py
+++ cg/services/analysis_starter/configurator/extensions/nallo.py
@@ -14,9 +14,14 @@ class NalloExtension(PipelineExtension):
     def configure(self, case_id: str, case_run_directory: Path) -> None:
         self.gene_panel_file_creator.create(
             case_id=case_id,
-            file_path=case_run_directory / ScoutExportFileName.PANELS_TSV,
+            file_path=self._get_gene_panel_file_path(case_run_directory),
             double_hashtag_filtering=True,
         )
 
     def do_required_files_exist(self, case_run_directory: Path) -> bool:
-        return case_run_directory.joinpath(ScoutExportFileName.PANELS_TSV).is_file()
+        gene_panel_file_path: Path = self._get_gene_panel_file_path(case_run_directory)
+        return gene_panel_file_path.is_file()
+
+    @staticmethod
+    def _get_gene_panel_file_path(case_run_directory: Path) -> Path:
+        return case_run_directory.joinpath(ScoutExportFileName.PANELS_TSV)
