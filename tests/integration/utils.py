7bcc4a591d91e5d4c4ec88127842b7af8e2e7ffb
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2025-11-10 14:46:24 +0100

    Anonymize sample data

--- tests/integration/utils.py
+++ tests/integration/utils.py
@@ -161,19 +161,19 @@ def expect_to_add_pending_analysis_to_trailblazer(
 
 def expect_lims_sample_request(lims_server: HTTPServer, sample: Sample, bed_name: str):
     lims_server.expect_request(f"/lims/api/v2/samples/{sample.internal_id}").respond_with_data(
-        f"""<smp:sample xmlns:udf="http://genologics.com/ri/userdefined" xmlns:ri="http://genologics.com/ri" xmlns:file="http://genologics.com/ri/file" xmlns:smp="http://genologics.com/ri/sample" uri="http://127.0.0.1:8000/api/v2/samples/ACC2351A1" limsid="ACC2351A1">
-<name>2016-02293</name>
-<date-received>2017-02-16</date-received>
-<project limsid="ACC2351" uri="http://127.0.0.1:8000/api/v2/projects/ACC2351"/>
+        f"""<smp:sample xmlns:udf="http://genologics.com/ri/userdefined" xmlns:ri="http://genologics.com/ri" xmlns:file="http://genologics.com/ri/file" xmlns:smp="http://genologics.com/ri/sample" uri="http://127.0.0.1:8000/api/v2/samples/ACC0001A1" limsid="ACC0001A1">
+<name>2016-00001</name>
+<date-received>2017-05-20</date-received>
+<project limsid="ACC0001" uri="http://127.0.0.1:8000/api/v2/projects/ACC0001"/>
 <submitter uri="http://127.0.0.1:8000/api/v2/researchers/3">
 <first-name>API</first-name>
 <last-name>Access</last-name>
 </submitter>
-<artifact limsid="ACC2351A1PA1" uri="http://127.0.0.1:8000/api/v2/artifacts/ACC2351A1PA1?state=55264"/>
+<artifact limsid="ACC0001A1PA1" uri="http://127.0.0.1:8000/api/v2/artifacts/ACC0001A1PA1?state=55264"/>
 <udf:field type="Boolean" name="Sample Delivered">true</udf:field>
 <udf:field type="String" name="Concentration (nM)">NA</udf:field>
-<udf:field type="String" name="customer">cust002</udf:field>
-<udf:field type="String" name="familyID">F0005063</udf:field>
+<udf:field type="String" name="customer">cust000</udf:field>
+<udf:field type="String" name="familyID">F0000001</udf:field>
 <udf:field type="String" name="Gender">M</udf:field>
 <udf:field type="String" name="priority">standard</udf:field>
 <udf:field type="String" name="Process only if QC OK">NA</udf:field>
