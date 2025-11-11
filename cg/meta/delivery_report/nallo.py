333568c293fc757ff50a61e94b8692921e1323ab
Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>
2025-11-11 16:20:52 +0100

    Add filter on case_id for Raredisease delivery report generation (#4701) (patch)
    ### Fixed
    
    - Filter on case_id as well to get the correct STR file into the delivery report for Raredisease.

--- cg/meta/delivery_report/nallo.py
+++ cg/meta/delivery_report/nallo.py
@@ -1,7 +1,9 @@
 """Nallo Delivery Report API."""
 
+from typing import cast
+
 from cg.clients.chanjo2.models import CoverageMetrics
-from cg.constants.housekeeper_tags import AnalysisTag, HermesFileTag
+from cg.constants.housekeeper_tags import AnalysisTag
 from cg.constants.report import (
     REQUIRED_APPLICATION_FIELDS,
     REQUIRED_CASE_FIELDS,
@@ -13,8 +15,6 @@ from cg.constants.report import (
     REQUIRED_SAMPLE_NALLO_FIELDS,
     REQUIRED_SAMPLE_TIMESTAMP_FIELDS,
 )
-from cg.constants.scout import ScoutUploadKey
-from cg.constants.subject import PlinkSex
 from cg.meta.delivery_report.data_validators import get_million_read_pairs
 from cg.meta.delivery_report.delivery_report_api import DeliveryReportAPI
 from cg.meta.workflow.nallo import NalloAnalysisAPI
@@ -36,7 +36,9 @@ class NalloDeliveryReportAPI(DeliveryReportAPI):
         self, case: Case, sample: Sample, analysis_metadata: NextflowAnalysis
     ) -> NalloSampleMetadataModel:
         """Return Nallo sample metadata to include in the report."""
-        sample_metrics: NalloQCMetrics = analysis_metadata.sample_metrics[sample.internal_id]
+        sample_metrics: NalloQCMetrics = cast(
+            NalloQCMetrics, analysis_metadata.sample_metrics[sample.internal_id]
+        )
         gene_ids: list[int] = self.analysis_api.get_gene_ids_from_scout(case.panels)
         coverage_metrics: CoverageMetrics | None = self.analysis_api.get_sample_coverage(
             case_id=case.internal_id, sample_id=sample.internal_id, gene_ids=gene_ids
@@ -65,23 +67,21 @@ class NalloDeliveryReportAPI(DeliveryReportAPI):
     def get_scout_variants_files(self, case_id: str) -> ScoutVariantsFiles:
         """Return Nallo files that will be uploaded to Scout."""
         return ScoutVariantsFiles(
-            snv_vcf=self.housekeeper_api.get_latest_file(
+            snv_vcf=self.housekeeper_api.get_latest_file_strict(
                 bundle=case_id,
                 tags=[
                     AnalysisTag.VCF_SNV_CLINICAL,
                 ],
             ).full_path,
-            sv_vcf=self.housekeeper_api.get_latest_file(
+            sv_vcf=self.housekeeper_api.get_latest_file_strict(
                 bundle=case_id,
                 tags=[
                     AnalysisTag.VCF_SV_CLINICAL,
                 ],
             ).full_path,
-            vcf_str=self.housekeeper_api.get_latest_file(
+            vcf_str=self.housekeeper_api.get_latest_file_strict(
                 bundle=case_id,
-                tags=[
-                    AnalysisTag.VCF_STR,
-                ],
+                tags=[AnalysisTag.VCF_STR],
             ).full_path,
         )
 
