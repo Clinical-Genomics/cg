13c7b14a919e38b24fe1ade67b624f99ca06e0ac
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2025-11-18 10:37:59 +0100

    Add session_id to analysis table

---
+++ alembic/versions/2025_11_18_3874118753ff_add_session_id_to_analysis.py
@@ -0,0 +1,25 @@
+"""add session_id to analysis
+
+Revision ID: 3874118753ff
+Revises: 2d63ba0d1154
+Create Date: 2025-11-18 10:36:08.049049
+
+"""
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "3874118753ff"
+down_revision = "2d63ba0d1154"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
