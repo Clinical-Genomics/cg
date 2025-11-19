695cc1090a9f4dff0ab2aa887f8545cce9d3df3f
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-11-19 09:28:14 +0100

    Apply suggestions from code review

--- cg/meta/workflow/fastq.py
+++ cg/meta/workflow/fastq.py
@@ -213,11 +213,11 @@ class BalsamicFastqHandler(FastqHandler):
         return f"{lane}_{date}_{flow_cell}_{sample}_{index}_R_{read_direction}{FileExtensions.FASTQ}{FileExtensions.GZIP}"
 
     def get_sample_fastq_destination_dir(self, case: Case, sample: Sample) -> Path:
-        """Get fastq paths for a Balsamic case (sample-independent)."""
+        """Get FASTQ file paths for a Balsamic case (sample-independent)."""
         return self.get_fastq_dir(case_id=case.internal_id)
 
     def get_fastq_dir(self, case_id: str) -> Path:
-        """Get fastq directory for a Balsamic case."""
+        """Get the FASTQ directory for a Balsamic case."""
         return Path(self.root_dir, case_id, "fastq")
 
 
