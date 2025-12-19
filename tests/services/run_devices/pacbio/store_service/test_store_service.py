e4e174f0e7465adbae485df07ed9ca111cb95b14
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2025-12-19 11:17:12 +0100

    green

--- tests/services/run_devices/pacbio/store_service/test_store_service.py
+++ tests/services/run_devices/pacbio/store_service/test_store_service.py
@@ -1,8 +1,7 @@
 """Module to test the PacBioStoreService."""
 
-from unittest import mock
-
 import pytest
+from pytest_mock import MockerFixture
 
 from cg.constants.devices import RevioNames
 from cg.services.run_devices.exc import (
@@ -33,6 +32,7 @@ def test_store_post_processing_data(
     pac_bio_dtos: PacBioDTOs,
     pacbio_barcoded_run_data: PacBioRunData,
     helpers: StoreHelpers,
+    mocker: MockerFixture,
 ):
     # GIVEN a PacBioStoreService and a data transfer service
 
@@ -51,11 +51,10 @@ def test_store_post_processing_data(
     # GIVEN a data transfer service that returns the correct DTOs
 
     # WHEN storing data for a PacBio instrument run
-    with mock.patch(
-        "cg.services.run_devices.pacbio.data_transfer_service.data_transfer_service.PacBioDataTransferService.get_post_processing_dtos",
-        return_value=pac_bio_dtos,
-    ):
-        pac_bio_store_service.store_post_processing_data(pacbio_barcoded_run_data)
+    mocker.patch.object(
+        PacBioDataTransferService, "get_post_processing_dtos", return_value=pac_bio_dtos
+    )
+    pac_bio_store_service.store_post_processing_data(pacbio_barcoded_run_data)
 
     # THEN the SMRT cell data is stored with the correct data
     smrt_cell: PacbioSMRTCell = pac_bio_store_service.store._get_query(PacbioSMRTCell).first()
@@ -101,56 +100,75 @@ def test_store_post_processing_data_error_database(
     pac_bio_store_service: PacBioStoreService,
     pac_bio_dtos: PacBioDTOs,
     pacbio_barcoded_run_data: PacBioRunData,
+    mocker: MockerFixture,
 ):
     # GIVEN a PacbioStoreService and a Pacbio run data object
 
     # GIVEN a store that raises an error when creating a PacBio SMRT cell
 
     # WHEN trying to store data for a Pacbio instrument run
-    with (
-        mock.patch(
-            "cg.services.run_devices.pacbio.data_transfer_service.data_transfer_service.PacBioDataTransferService.get_post_processing_dtos",
-            return_value=pac_bio_dtos,
-        ),
-        mock.patch.object(Store, "create_pac_bio_smrt_cell", side_effect=ValueError),
-    ):
-        # THEN a PostProcessingStoreDataError is raised
-        with pytest.raises(PostProcessingStoreDataError):
-            pac_bio_store_service.store_post_processing_data(pacbio_barcoded_run_data)
+    mocker.patch.object(
+        PacBioDataTransferService, "get_post_processing_dtos", return_value=pac_bio_dtos
+    )
+    mocker.patch.object(Store, "create_pac_bio_smrt_cell", side_effect=ValueError)
+
+    # THEN a PostProcessingStoreDataError is raised
+    with pytest.raises(PostProcessingStoreDataError):
+        pac_bio_store_service.store_post_processing_data(pacbio_barcoded_run_data)
 
 
 def test_store_post_processing_data_error_parser(
     pac_bio_store_service: PacBioStoreService,
     pac_bio_dtos: PacBioDTOs,
     pacbio_barcoded_run_data: PacBioRunData,
+    mocker: MockerFixture,
 ):
     # GIVEN a PacbioStoreService and a Pacbio run data object
 
     # GIVEN a data transfer service that raises an error when parsing data
 
     # WHEN trying to store data for a PacBio instrument ru
-    with (
-        mock.patch(
-            "cg.services.run_devices.pacbio.data_transfer_service.data_transfer_service.PacBioDataTransferService.get_post_processing_dtos",
-            return_value=pac_bio_dtos,
-        ),
-        mock.patch.object(
-            PacBioDataTransferService,
-            "get_post_processing_dtos",
-            side_effect=PostProcessingDataTransferError,
-        ),
-    ):
-        # THEN a PostProcessingStoreDataError is raised
-        with pytest.raises(PostProcessingStoreDataError):
-            pac_bio_store_service.store_post_processing_data(pacbio_barcoded_run_data)
+    mocker.patch.object(
+        PacBioDataTransferService, "get_post_processing_dtos", return_value=pac_bio_dtos
+    )
+    mocker.patch.object(
+        PacBioDataTransferService,
+        "get_post_processing_dtos",
+        side_effect=PostProcessingDataTransferError,
+    )
+
+    # THEN a PostProcessingStoreDataError is raised
+    with pytest.raises(PostProcessingStoreDataError):
+        pac_bio_store_service.store_post_processing_data(pacbio_barcoded_run_data)
 
 
-def test_store_post_processing_multiple_smrt_cells(pac_bio_store_service: PacBioStoreService):
-    # GIVEN a PacBioStoreService
-    
-    
+def test_store_post_processing_multiple_smrt_cells(
+    pac_bio_store_service: PacBioStoreService,
+    pac_bio_dtos: PacBioDTOs,
+    pacbio_barcoded_run_data: PacBioRunData,
+    helpers: StoreHelpers,
+    mocker: MockerFixture,
+):
+    # GIVEN a PacBioStoreService and a data transfer service
+
+    # GIVEN that the store has a sample
+    helpers.add_sample(
+        store=pac_bio_store_service.store,
+        internal_id=pac_bio_dtos.sample_sequencing_metrics[0].sample_internal_id,
+    )
+
     # GIVEN that the store already has a PacBio sequencing run with the same run name
-    
+    pac_bio_store_service.store.create_pacbio_sequencing_run(pac_bio_dtos.sequencing_run)
+    pac_bio_store_service.store.commit_to_store()
+
     # WHEN creating PacBio sequencing run with the run name
-    
+    mocker.patch.object(
+        PacBioDataTransferService, "get_post_processing_dtos", return_value=pac_bio_dtos
+    )
+    pac_bio_store_service.store_post_processing_data(run_data=pacbio_barcoded_run_data)
+
     # THEN no additional PacBio sequencing run should be added
+    sequencing_runs: list[PacbioSequencingRun] = pac_bio_store_service.store._get_query(
+        PacbioSequencingRun
+    ).all()
+    assert len(sequencing_runs) == 1
