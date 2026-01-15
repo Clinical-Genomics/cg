1c54bdaf04f02edf2e01162543beb85fcb96b3eb
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-01-15 10:32:19 +0100

    WIP

---
+++ tests/services/sequencing_qc_service/test_sequencing_qc_service.py
@@ -0,0 +1,28 @@
+from unittest.mock import Mock, create_autospec
+
+from pytest_mock import MockerFixture
+
+from cg.services.sequencing_qc_service import SequencingQCService
+from cg.services.sequencing_qc_service.quality_checks import checks
+from cg.store.models import Case
+from cg.store.store import Store
+
+
+def test_run_sequencing_qc_handles_exception(mocker: MockerFixture):
+    # GIVEN a store with a case ready for sequencing QC
+    store: Store = create_autospec(Store)
+    case: Case = create_autospec(Case)
+    store.get_cases_for_sequencing_qc = Mock(return_value=[case])
+
+    # GIVEN a sequencing qc service
+    service = SequencingQCService(store=store)
+
+    # GIVEN that an exception is raised during quality check
+    mocker.patch.object(
+        checks,
+        "get_sequencing_quality_check_for_case",
+        return_value=Mock(side_effect=Exception("BOOM!")),
+    )
+
+    # WHEN calling run_sequencing_qc
+    # THEN no exception was raised
