cddf09c9f8bbb614dc1fd16d67d691a5e7b1d7dc
Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>
2026-01-19 16:10:06 +0100

    Stop Balsamic mixed capture kit starts (#4823) (patch)
    ### Changed
    
    - BalsamicConfigurator.configure raises a MultipleCaptureKitsError when multiple different capture kits are found in Lims for the provided case

--- cg/services/analysis_starter/configurator/implementations/balsamic.py
+++ cg/services/analysis_starter/configurator/implementations/balsamic.py
@@ -2,8 +2,9 @@ import logging
 from pathlib import Path
 from typing import cast
 
+from cg.apps.lims.api import LimsAPI
 from cg.constants.priority import SlurmQos
-from cg.exc import CaseNotConfiguredError
+from cg.exc import CaseNotConfiguredError, MultipleCaptureKitsError
 from cg.meta.workflow.fastq import BalsamicFastqHandler
 from cg.models.cg_config import BalsamicConfig
 from cg.services.analysis_starter.configurator.configurator import Configurator
@@ -11,6 +12,7 @@ from cg.services.analysis_starter.configurator.file_creators.balsamic_config imp
     BalsamicConfigFileCreator,
 )
 from cg.services.analysis_starter.configurator.models.balsamic import BalsamicCaseConfig
+from cg.store.models import Case
 from cg.store.store import Store
 
 LOG = logging.getLogger(__name__)
@@ -22,6 +24,7 @@ class BalsamicConfigurator(Configurator):
         config: BalsamicConfig,
         config_file_creator: BalsamicConfigFileCreator,
         fastq_handler: BalsamicFastqHandler,
+        lims_api: LimsAPI,
         store: Store,
     ):
         self.store: Store = store
@@ -35,9 +38,11 @@ class BalsamicConfigurator(Configurator):
 
         self.fastq_handler: BalsamicFastqHandler = fastq_handler
         self.config_file_creator: BalsamicConfigFileCreator = config_file_creator
+        self.lims_api = lims_api
 
     def configure(self, case_id: str, **flags) -> BalsamicCaseConfig:
         LOG.info(f"Configuring case {case_id}")
+        self._ensure_consistent_capture_kits(case_id)
         self.fastq_handler.link_fastq_files(case_id)
         fastq_path: Path = self.fastq_handler.get_fastq_dir(case_id)
         self.config_file_creator.create(case_id=case_id, fastq_path=fastq_path, **flags)
@@ -60,6 +65,16 @@ class BalsamicConfigurator(Configurator):
 
     def _get_sample_config_path(self, case_id: str) -> Path:
         return Path(self.root_dir, case_id, f"{case_id}.json")
+
+    def _ensure_consistent_capture_kits(self, case_id):
+        case: Case = self.store.get_case_by_internal_id_strict(case_id)
+        capture_kits: set[str | None] = {
+            self.lims_api.capture_kit(sample.internal_id) for sample in case.samples
+        }
+        if len(capture_kits) > 1:
+            raise MultipleCaptureKitsError(
+                f"Multiple capture kits found for {case_id}: {capture_kits}"
+            )
 
     def _ensure_required_config_files_exist(self, config: BalsamicCaseConfig) -> None:
         if not config.sample_config.exists():
