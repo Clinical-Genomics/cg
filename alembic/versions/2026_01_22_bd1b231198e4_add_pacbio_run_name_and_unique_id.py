33d522b96b47a5f0c625d5096ff101766698a1ec
islean <isak.ohlsson.angnell@gmail.com>
2026-01-22 13:18:50 +0100

    Add New columns to Pacbio Sequencing Run

---
+++ alembic/versions/2026_01_22_bd1b231198e4_add_pacbio_run_name_and_unique_id.py
@@ -0,0 +1,39 @@
+"""Add Pacbio run name and unique id
+
+Revision ID: bd1b231198e4
+Revises: 35132fa0c4ea
+Create Date: 2026-01-22 13:09:47.820455
+
+"""
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "bd1b231198e4"
+down_revision = "35132fa0c4ea"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.alter_column(
+        table_name="pacbio_sequencing_run", column_name="run_name", new_column_name="internal_id"
+    )
+    op.add_column(
+        table_name="pacbio_sequencing_run",
+        column=sa.Column("name", sa.String(64), nullable=True, index=True),
+    )
+    op.add_column(
+        table_name="pacbio_sequencing_run",
+        column=sa.Column("unique_id", sa.String(64), nullable=True, unique=True),
+    )
+
+
+def downgrade():
+    op.drop_column(table_name="pacbio_sequencing_run", column_name="unique_id")
+    op.drop_column(table_name="pacbio_sequencing_run", column_name="name")
+    op.alter_column(
+        table_name="pacbio_sequencing_run", column_name="internal_id", new_column_name="run_name"
+    )
