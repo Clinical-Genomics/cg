3467ec6b069264c9ced4881f4889337f6e339fcb
islean <isak.ohlsson.angnell@gmail.com>
2026-01-27 10:13:00 +0100

    Rename stuff according to new names

--- cg/services/run_devices/pacbio/data_storage_service/pacbio_store_service.py
+++ cg/services/run_devices/pacbio/data_storage_service/pacbio_store_service.py
@@ -41,10 +41,8 @@ class PacBioStoreService(PostProcessingStoreService):
         try:
             return self.store.create_pacbio_sequencing_run(sequencing_run_dto)
         except PacbioSequencingRunAlreadyExistsError:
-            LOG.debug(f"Sequencing run {sequencing_run_dto.internal_id} already exists")
-            return self.store.get_pacbio_sequencing_run_by_internal_id(
-                sequencing_run_dto.internal_id
-            )
+            LOG.debug(f"Sequencing run {sequencing_run_dto.run_id} already exists")
+            return self.store.get_pacbio_sequencing_run_by_run_id(sequencing_run_dto.run_id)
 
     def _create_pacbio_smrt_cell_metrics(
         self,
@@ -110,5 +108,5 @@ class PacBioStoreService(PostProcessingStoreService):
                 f"Dry run, no entries will be added to database for SMRT cell {run_data.full_path}."
             )
             return
-        LOG.debug(f"Data stored in statusDB for run {run_data.run_internal_id}")
+        LOG.debug(f"Data stored in statusDB for run {run_data.run_id}")
         self.store.commit_to_store()
