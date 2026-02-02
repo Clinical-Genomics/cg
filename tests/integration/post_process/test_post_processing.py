7bc0f6def089f5d712481eb66bf9e36918d1d2e6
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2026-02-02 17:29:17 +0100

    zip includes all necessary files

--- tests/integration/post_process/test_post_processing.py
+++ tests/integration/post_process/test_post_processing.py
@@ -3,14 +3,20 @@ from pathlib import Path
 
 import pytest
 from click.testing import CliRunner, Result
+from housekeeper.store.store import Store as HousekeeperStore
 
 from cg.cli.base import base
+from cg.store.store import Store
 from tests.integration.utils import IntegrationTestPaths
+from tests.store_helpers import StoreHelpers
 
 
 @pytest.mark.xdist_group(name="integration")
 @pytest.mark.integration
 def test_post_processing(
+    status_db: Store,
+    helpers: StoreHelpers,
+    housekeeper_db: HousekeeperStore,
     test_run_paths: IntegrationTestPaths,
 ):
     cli_runner = CliRunner()
@@ -18,9 +24,12 @@ def test_post_processing(
     config_path: Path = test_run_paths.cg_config_file
     test_root_dir: Path = test_run_paths.test_root_dir
 
+    helpers.add_sample(store=status_db, internal_id="ACC15752A3")
+
     shutil.copytree(
         Path("tests/fixtures/devices/pacbio/SMRTcells/r84202_20240522_133539"),
         Path(test_root_dir, "pacbio_data_dir", "r84202_20240522_133539"),
+        ignore=lambda src, names: [".DS_Store"],
     )
 
     # WHEN running nallo start-available
@@ -35,4 +44,5 @@ def test_post_processing(
         catch_exceptions=False,
     )
 
+    assert result == "?"
     assert result == "?"
