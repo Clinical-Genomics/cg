388eddf06a6dcf4c1e7a03e292359f4cb1a0370a
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2026-02-04 16:45:16 +0100

    Command completes, assert sequencing run, smrt cell metrics and sample bundle

--- tests/integration/post_process/test_post_processing.py
+++ tests/integration/post_process/test_post_processing.py
@@ -6,6 +6,7 @@ from click.testing import CliRunner, Result
 from housekeeper.store.store import Store as HousekeeperStore
 
 from cg.cli.base import base
+from cg.store.models import PacbioSequencingRun, PacbioSMRTCellMetrics
 from cg.store.store import Store
 from tests.integration.utils import IntegrationTestPaths
 from tests.store_helpers import StoreHelpers
@@ -24,12 +25,15 @@ def test_post_processing(
     config_path: Path = test_run_paths.cg_config_file
     test_root_dir: Path = test_run_paths.test_root_dir
 
-    helpers.add_sample(store=status_db, internal_id="ACC15752A3")
+    sample_id = "ACC15752A3"
+
+    helpers.add_sample(store=status_db, internal_id=sample_id)
+    run_id = "r84202_20240522_133539"
 
     shutil.copytree(
-        Path("tests/fixtures/devices/pacbio/SMRTcells/r84202_20240522_133539"),
-        Path(test_root_dir, "pacbio_data_dir", "r84202_20240522_133539"),
-        ignore=lambda src, names: [".DS_Store"],
+        Path("tests/fixtures/devices/pacbio/SMRTcells/", run_id),
+        Path(test_root_dir, "pacbio_data_dir", run_id),
+        # ignore=lambda src, names: [".DS_Store"],
     )
 
     # WHEN running nallo start-available
@@ -44,5 +48,27 @@ def test_post_processing(
         catch_exceptions=False,
     )
 
-    assert result == "?"
-    assert result == "?"
+    assert result.exit_code == 0
+
+    # THEN a PacbioSequencingRun was persisted with the parsed data
+    created_sequencing_run: PacbioSequencingRun = status_db.get_pacbio_sequencing_run_by_run_id(
+        run_id
+    )
+
+    assert created_sequencing_run.run_id == run_id
+    assert created_sequencing_run.instrument_name == "Wilma"
+
+    # THEN a PacbioSMRTCellMetrics was persisted with the parsed data
+    smrt_cell_metrics: list[PacbioSMRTCellMetrics] = created_sequencing_run.smrt_cell_metrics
+
+    assert len(smrt_cell_metrics) == 1
+    assert smrt_cell_metrics[0].barcoded_hifi_mean_read_length == 14477
+    assert smrt_cell_metrics[0].device.samples[0].internal_id == sample_id
+
+    # THEN the BAM file was stored in the Sample housekeeper bundle
+    files = housekeeper_db.get_files(bundle_name=sample_id).all()
+    assert "m84202_240522_155607_s2.hifi_reads.bc2004.bam" in files[0].path
+
+    # TODO:
+    # assert smrtcell bundles
+    # assert post processing complete file was created
