c59efe82d54b1038ae7fc70d3dabeab1e674e68a
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-02-04 11:41:19 +0100

    Database change

---
+++ alembic/versions/2026_02_04_2c9a49ce5512_start_using_genome_column_in_bed_version.py
@@ -0,0 +1,39 @@
+"""start using genome column in bed version
+
+Revision ID: 2c9a49ce5512
+Revises: afe71f163d20
+Create Date: 2026-02-04 11:33:44.131245
+
+"""
+
+from enum import StrEnum
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "2c9a49ce5512"
+down_revision = "afe71f163d20"
+branch_labels = None
+depends_on = None
+
+
+class GenomeVersion(StrEnum):
+    hg19 = "hg19"
+    hg38 = "hg38"
+    cfam3 = "cfam3"
+
+
+def upgrade():
+    op.alter_column(
+        table_name="bed_version",
+        column_name="genome_version",
+        existing_type=sa.VARCHAR(length=32),
+        type_=GenomeVersion,
+        nullable=False,
+    )
+
+
+def downgrade():
+    pass
