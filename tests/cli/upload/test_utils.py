2d8272ce1a7764aa1de2bf2eba67c2bb4d11f734
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-02-11 10:26:59 +0100

    make raredisease get scout 38 for upload

--- tests/cli/upload/test_utils.py
+++ tests/cli/upload/test_utils.py
@@ -5,11 +5,25 @@ from cg.store.models import Case
 from tests.store_helpers import StoreHelpers
 
 
-def test_get_scout_api_by_case_38(upload_context: CGConfig, helpers: StoreHelpers):
+def test_get_scout_api_by_case_nallo(upload_context: CGConfig, helpers: StoreHelpers):
 
     # GIVEN a Nallo case
     nallo_case: Case = helpers.ensure_case(
         store=upload_context.status_db, data_analysis=Workflow.NALLO
+    )
+
+    # WHEN getting the corresponding ScoutAPI
+    scout_api = get_scout_api_by_case(cg_config=upload_context, case_id=nallo_case.internal_id)
+
+    # THEN the ScoutAPI should be towards the hg38 instance
+    assert scout_api == upload_context.scout_api_38
+
+
+def test_get_scout_api_by_case_raredisease(upload_context: CGConfig, helpers: StoreHelpers):
+
+    # GIVEN a Nallo case
+    nallo_case: Case = helpers.ensure_case(
+        store=upload_context.status_db, data_analysis=Workflow.RAREDISEASE
     )
 
     # WHEN getting the corresponding ScoutAPI
