bb197a14157754d4281a20e906bf32fd72d63e20
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-02-11 11:27:51 +0100

    WIP integration test

---
+++ tests/integration/upload/test_upload_raredisease.py
@@ -0,0 +1,37 @@
+from pathlib import Path
+
+import pytest
+from click.testing import CliRunner, Result
+from integration.utils import IntegrationTestPaths
+from store_helpers import StoreHelpers
+
+from cg.cli.base import base
+from cg.store.store import Store
+
+
+@pytest.mark.xdist_group(name="integration")
+@pytest.mark.integration
+def test_upload_raredisease_to_scout(
+    status_db: Store,
+    helpers: StoreHelpers,
+    test_run_paths: IntegrationTestPaths,
+):
+    cli_runner = CliRunner()
+
+    # GIVEN a config file with valid database URIs and directories
+    config_path: Path = test_run_paths.cg_config_file
+    # test_root_dir: Path = test_run_paths.test_root_dir
+
+    # WHEN running post-process all
+    result: Result = cli_runner.invoke(
+        base,
+        [
+            "--config",
+            config_path.as_posix(),
+            "upload",
+            "scout",
+        ],
+        catch_exceptions=False,
+    )
+
+    assert result.exit_code == 1
