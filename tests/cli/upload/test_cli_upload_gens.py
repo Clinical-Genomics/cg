7cefefc0cf8db49985d1af1a0d9ccc149d0c80af
kedhammar <alfred.kedhammar@scilifelab.se>
2026-02-18 11:31:09 +0100

    adress comments

--- tests/cli/upload/test_cli_upload_gens.py
+++ tests/cli/upload/test_cli_upload_gens.py
@@ -50,27 +50,26 @@ def test_upload_gens_genome_build(
     cli_runner: CliRunner,
     upload_gens_context: CGConfig,
 ):
-    # GIVEN a store
+    # GIVEN store and gens APIs
     status_db: Store = create_autospec(Store)
-
-    # GIVEN a Raredisease case with a sample
-    sample: Sample = create_autospec(Sample, internal_id="sample_id")
-    case: Case = create_autospec(Case, data_analysis=workflow, samples=[sample])
-
-    status_db.get_case_by_internal_id_strict = Mock(return_value=case)
     upload_gens_context.status_db_ = status_db
     gens_api: TypedMock[GensAPI] = create_typed_mock(GensAPI)
     upload_gens_context.gens_api_ = gens_api.as_type
 
+    # GIVEN a case with a sample
+    sample: Sample = create_autospec(Sample, internal_id="sample_id")
+    case: Case = create_autospec(Case, data_analysis=workflow, samples=[sample])
+    status_db.get_case_by_internal_id_strict = Mock(return_value=case)
+
     # GIVEN a HousekeeperAPI
     file_path = "/path/to/file"
-    housekeeper_api = create_autospec(HousekeeperAPI)
+    housekeeper_api: HousekeeperAPI = create_autospec(HousekeeperAPI)
     housekeeper_api.get_file_from_latest_version = Mock(
         return_value=create_autospec(File, full_path=file_path)
     )
     upload_gens_context.housekeeper_api_ = housekeeper_api
 
-    # WHEN uploading to Gens
+    # WHEN uploading to gens
     cli_runner.invoke(upload_gens_cmd, ["some_case"], obj=upload_gens_context)
 
     # THEN assert gens is called with correct genome build
