5acd9736fc256778ab77c565670e68201107bc00
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-02-18 11:38:15 +0100

    Change upload genome version for Gens (#4902)
    * wip
    
    * Start writing test
    
    * Golden green state
    
    * parametrize
    
    * fix type hint
    
    * adress comments
    
    ---------
    
    Co-authored-by: kedhammar <alfred.kedhammar@scilifelab.se>
    Co-authored-by: islean <isak.ohlsson.angnell@gmail.com>

--- tests/cli/upload/test_cli_upload_gens.py
+++ tests/cli/upload/test_cli_upload_gens.py
@@ -1,12 +1,21 @@
 """Test the Gens upload command."""
 
 import logging
+from unittest.mock import Mock, create_autospec
 
+import pytest
 from click.testing import CliRunner
+from housekeeper.store.models import File
 
+from cg.apps.gens import GensAPI
+from cg.apps.housekeeper.hk import HousekeeperAPI
 from cg.cli.upload.gens import upload_to_gens as upload_gens_cmd
-from cg.constants import EXIT_SUCCESS
+from cg.constants import EXIT_SUCCESS, Workflow
+from cg.constants.constants import GenomeBuild
 from cg.models.cg_config import CGConfig
+from cg.store.models import Case, Sample
+from cg.store.store import Store
+from tests.typed_mock import TypedMock, create_typed_mock
 
 
 def test_upload_gens(
@@ -24,3 +33,50 @@ def test_upload_gens(
     # THEN check that the command exits with success
     assert result.exit_code == EXIT_SUCCESS
     assert "Dry run" in caplog.text
+
+
+@pytest.mark.parametrize(
+    "workflow, genome_build",
+    [
+        (Workflow.BALSAMIC, GenomeBuild.hg19),
+        (Workflow.BALSAMIC_UMI, GenomeBuild.hg19),
+        (Workflow.MIP_DNA, GenomeBuild.hg19),
+        (Workflow.RAREDISEASE, GenomeBuild.hg38),
+    ],
+)
+def test_upload_gens_genome_build(
+    workflow: Workflow,
+    genome_build: GenomeBuild,
+    cli_runner: CliRunner,
+    upload_gens_context: CGConfig,
+):
+    # GIVEN store and gens APIs
+    status_db: Store = create_autospec(Store)
+    upload_gens_context.status_db_ = status_db
+    gens_api: TypedMock[GensAPI] = create_typed_mock(GensAPI)
+    upload_gens_context.gens_api_ = gens_api.as_type
+
+    # GIVEN a case with a sample
+    sample: Sample = create_autospec(Sample, internal_id="sample_id")
+    case: Case = create_autospec(Case, data_analysis=workflow, samples=[sample])
+    status_db.get_case_by_internal_id_strict = Mock(return_value=case)
+
+    # GIVEN a HousekeeperAPI
+    file_path = "/path/to/file"
+    housekeeper_api: HousekeeperAPI = create_autospec(HousekeeperAPI)
+    housekeeper_api.get_file_from_latest_version = Mock(
+        return_value=create_autospec(File, full_path=file_path)
+    )
+    upload_gens_context.housekeeper_api_ = housekeeper_api
+
+    # WHEN uploading to gens
+    cli_runner.invoke(upload_gens_cmd, ["some_case"], obj=upload_gens_context)
+
+    # THEN assert gens is called with correct genome build
+    gens_api.as_mock.load.assert_called_once_with(
+        baf_path=file_path,
+        case_id="some_case",
+        coverage_path=file_path,
+        genome_build=genome_build,
+        sample_id="sample_id",
+    )
