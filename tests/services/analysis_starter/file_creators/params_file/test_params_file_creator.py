85e6d87f9a08c9a247ed60b5397ff2823d46672e
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-02-20 10:25:10 +0100

    Green after first test refactoring

--- tests/services/analysis_starter/file_creators/params_file/test_params_file_creator.py
+++ tests/services/analysis_starter/file_creators/params_file/test_params_file_creator.py
@@ -147,24 +147,7 @@ def test_raredisease_params_file_creator_on_lims_capture_kit_availability(
     )
 
 
-# @pytest.mark.parametrize(
-#     "prep_category, expected_bed, expected_mu, expected_ud",
-#     [
-#         (
-#             SeqLibraryPrepCategory.WHOLE_GENOME_SEQUENCING,
-#             Path("some/sleeping_quarters.bed"),
-#             Path("some/cow.mu"),
-#             Path("some/department_of_external_affairs.UD"),
-#         ),
-#         (
-#             SeqLibraryPrepCategory.WHOLE_EXOME_SEQUENCING,
-#             Path("some/sleeping_quarters.exome.bed"),
-#             Path("some/cow.exome.mu"),
-#             Path("some/department_of_external_affairs.exome.UD"),
-#         ),
-#     ],
-# )
-def test_raredisease_params_file_creator_wgs1(
+def test_raredisease_params_file_creator_wgs(
     gcnvcaller_files: dict[str, GCNVCallerFiles],
     verifybamid_files_set: VerifybamidSvdFilesSet,
     mocker: MockerFixture,
@@ -230,6 +213,7 @@ def test_raredisease_params_file_creator_wgs1(
             "analysis_type": SeqLibraryPrepCategory.WHOLE_GENOME_SEQUENCING,
             "input": Path("root/samplesheet.csv"),
             "outdir": Path("some_path"),
+            "skip_germlinecnvcaller": True,
             "sample_id_map": Path("some_path/case_id_customer_internal_mapping.csv"),
             "save_mapped_as_cram": True,
             "target_bed_file": ANY,
@@ -323,11 +307,12 @@ def test_raredisease_params_file_creator_wes_gcnvcaller_included(
             "target_bed_file": ANY,
             "vcfanno_extra_resources": "some_path/managed_variants.vcf",
             "vep_filters_scout_fmt": "some_path/gene_panels.bed",
-            "verifybamid_svd_bed": ANY,
-            "verifybamid_svd_mu": ANY,
-            "verifybamid_svd_ud": ANY,
+            "verifybamid_svd_bed": Path("some/sleeping_quarters.exome.bed"),
+            "verifybamid_svd_mu": Path("some/cow.exome.mu"),
+            "verifybamid_svd_ud": Path("some/department_of_external_affairs.exome.UD"),
         },
     )
+
     # THEN auxiliary file is written and with the correct content
     write_csv_mock.assert_called_once_with(
         content=[["customer_id", "internal_id"], ["sample_name", "ACC"]],
@@ -398,86 +383,6 @@ def test_raredisease_params_file_creator_wes_gcnvcaller_bed_version_does_not_mat
         content={
             "Key": "Value",
             "analysis_type": SeqLibraryPrepCategory.WHOLE_EXOME_SEQUENCING,
-            "input": Path("root/samplesheet.csv"),
-            "outdir": Path("some_path"),
-            "sample_id_map": Path("some_path/case_id_customer_internal_mapping.csv"),
-            "save_mapped_as_cram": True,
-            "skip_germlinecnvcaller": True,
-            "target_bed_file": ANY,
-            "vcfanno_extra_resources": "some_path/managed_variants.vcf",
-            "vep_filters_scout_fmt": "some_path/gene_panels.bed",
-            "verifybamid_svd_bed": ANY,
-            "verifybamid_svd_mu": ANY,
-            "verifybamid_svd_ud": ANY,
-        },
-    )
-    # THEN auxiliary file is written and with the correct content
-    write_csv_mock.assert_called_once_with(
-        content=[["customer_id", "internal_id"], ["sample_name", "ACC"]],
-        file_path=Path("some_path", f"{case_id}_customer_internal_mapping.csv"),
-    )
-
-
-def test_raredisease_params_file_creator_wgs(
-    gcnvcaller_files: dict[str, GCNVCallerFiles],
-    verifybamid_files_set: VerifybamidSvdFilesSet,
-    mocker: MockerFixture,
-):
-    # GIVEN a file path
-    file_path = Path("some_path", "file_name.yaml")
-
-    # GIVEN a store containing a case with a linked sample. Also contains a bed version.
-    super_sample = create_autospec(
-        Sample,
-        from_sample=None,
-        internal_id="ACC",
-        prep_category=SeqLibraryPrepCategory.WHOLE_GENOME_SEQUENCING,
-    )
-    super_sample.name = "sample_name"
-    case = create_autospec(Case, samples=[super_sample])
-    store_mock: Store = create_autospec(Store)
-    store_mock.get_samples_by_case_id = Mock(return_value=[super_sample])
-    store_mock.get_case_by_internal_id_strict = Mock(return_value=case)
-
-    # GIVEN that Lims returns a bed shortname
-    lims = create_autospec(LimsAPI)
-    lims.capture_kit = Mock(return_value=None)
-
-    # GIVEN a params file creator
-    file_creator = RarediseaseParamsFileCreator(
-        gcnvcaller_files=gcnvcaller_files,
-        verifybamid_files_set=verifybamid_files_set,
-        store=store_mock,
-        lims=lims,
-        params="Path_to_file.yaml",
-    )
-
-    # GIVEN case id
-    case_id = "case_id"
-
-    # GIVEN sample sheet path
-    sample_sheet_path = Path("root", "samplesheet.csv")
-
-    # GIVEN IO is mocked
-    write_yaml_mock = mocker.patch.object(
-        raredisease_params_file_creator, "write_yaml_nextflow_style"
-    )
-    write_csv_mock = mocker.patch.object(raredisease_params_file_creator, "write_csv")
-    mocker.patch.object(raredisease_params_file_creator, "read_yaml", return_value={"Key": "Value"})
-
-    # WHEN creating the params file
-    file_creator.create(
-        case_id=case_id,
-        file_path=file_path,
-        sample_sheet_path=sample_sheet_path,
-    )
-
-    # THEN the file should have been written with the expected content
-    write_yaml_mock.assert_called_once_with(
-        file_path=file_path,
-        content={
-            "Key": "Value",
-            "analysis_type": SeqLibraryPrepCategory.WHOLE_GENOME_SEQUENCING,
             "input": Path("root/samplesheet.csv"),
             "outdir": Path("some_path"),
             "sample_id_map": Path("some_path/case_id_customer_internal_mapping.csv"),
