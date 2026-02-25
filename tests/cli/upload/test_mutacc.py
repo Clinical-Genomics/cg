a2a9c2551684afc659a208297ca7e365e5e8b594
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-02-25 14:58:48 +0100

    parametrized processed solved

--- tests/cli/upload/test_mutacc.py
+++ tests/cli/upload/test_mutacc.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from unittest.mock import ANY, Mock, create_autospec
 
+import pytest
 from click.testing import CliRunner
 from pytest_mock import MockerFixture
 
@@ -9,61 +10,22 @@ from cg.apps.scout.scout_export import ScoutExportCase
 from cg.apps.scout.scoutapi import ScoutAPI
 from cg.cli.upload import mutacc
 from cg.cli.upload.mutacc import process_solved
+from cg.constants.constants import BedVersionGenomeVersion
 from cg.constants.process import EXIT_SUCCESS
 from cg.meta.upload.mutacc import UploadToMutaccAPI
 from cg.models.cg_config import CGConfig, IlluminaConfig, MutaccAutoConfig, RunInstruments
 
 
-def test_process_solved_success(mocker: MockerFixture):
-    # GIVEN a case can be exported from Scout
-    scout_case = ScoutExportCase(
-        _id="1", analysis_date=datetime.now(), owner="owner", individuals=[]
-    )
-    scout_api: ScoutAPI = create_autospec(ScoutAPI)
-    scout_api.get_solved_cases = Mock(return_value=[scout_case])
-    get_scout_api_call = mocker.patch.object(
-        mutacc, "get_scout_api_by_genome_build", return_value=scout_api
-    )
-
-    mutacc_auto_init = mocker.spy(MutaccAutoAPI, "__init__")
-    extract_reads_mock = mocker.patch.object(UploadToMutaccAPI, "extract_reads")
-
-    # GIVEN a cg config
-    cg_config: CGConfig = create_autospec(
-        CGConfig,
-        delivery_path="delivery/path",
-        run_instruments=create_autospec(
-            RunInstruments,
-            illumina=create_autospec(IlluminaConfig, demultiplexed_runs_dir="some_dir"),
-        ),
-        mutacc_auto_hg19=MutaccAutoConfig(
-            binary_path="crazy_path",
-            config_path="a_path",
-            padding=1337,
-        ),
-    )
-
-    # GIVEN a cli_runner
-    cli_runner = CliRunner()
-
-    # WHEN running process_solved
-    result = cli_runner.invoke(
-        process_solved, ["--genome-version", "hg19", "--days-ago", "1"], obj=cg_config
-    )
-
-    # THEN the command exits successfully
-    assert result.exit_code == EXIT_SUCCESS
-
-    # THEN the correct Scout instance was used for the export
-    get_scout_api_call.assert_called_once_with(cg_config=cg_config, genome_build="hg19")
-    # THEN the mutacc auto api was used correctly
-    mutacc_auto_init.assert_called_once_with(ANY, config=cg_config.mutacc_auto_hg19)
-
-    # THEN the reads were extracted for the case returned by Scout
-    extract_reads_mock.assert_called_once_with(scout_case)
-
-
-def test_process_solved_success_hg38(mocker: MockerFixture):
+@pytest.mark.parametrize(
+    "genome_version, expected_mutacc_auto_config",
+    [
+        (BedVersionGenomeVersion.HG19, "mutacc_auto_hg19"),
+        (BedVersionGenomeVersion.HG38, "mutacc_auto_hg38"),
+    ],
+)
+def test_process_solved_success(
+    mocker: MockerFixture, genome_version: BedVersionGenomeVersion, expected_mutacc_auto_config: str
+):
     # GIVEN a case can be exported from Scout
     scout_case = ScoutExportCase(
         _id="1", analysis_date=datetime.now(), owner="owner", individuals=[]
@@ -102,16 +64,17 @@ def test_process_solved_success_hg38(mocker: MockerFixture):
 
     # WHEN running process_solved
     result = cli_runner.invoke(
-        process_solved, ["--genome-version", "hg38", "--days-ago", "1"], obj=cg_config
+        process_solved, ["--genome-version", genome_version, "--days-ago", "1"], obj=cg_config
     )
 
     # THEN the command exits successfully
     assert result.exit_code == EXIT_SUCCESS
 
     # THEN the correct Scout instance was used for the export
-    get_scout_api_call.assert_called_once_with(cg_config=cg_config, genome_build="hg38")
+    get_scout_api_call.assert_called_once_with(cg_config=cg_config, genome_build=genome_version)
     # THEN the mutacc auto api was used correctly
-    mutacc_auto_init.assert_called_once_with(ANY, config=cg_config.mutacc_auto_hg38)
-
+    mutacc_auto_init.assert_called_once_with(
+        ANY, config=getattr(cg_config, expected_mutacc_auto_config)
+    )
     # THEN the reads were extracted for the case returned by Scout
     extract_reads_mock.assert_called_once_with(scout_case)
