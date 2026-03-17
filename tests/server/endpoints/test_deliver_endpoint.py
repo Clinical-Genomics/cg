0fcd92e05e96267183afed22e154801037e106cb
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2026-03-17 10:54:52 +0100

    WIP

--- tests/server/endpoints/test_deliver_endpoint.py
+++ tests/server/endpoints/test_deliver_endpoint.py
@@ -72,3 +72,54 @@ def test_deliver_trailblazer_analysis(client: FlaskClient, mocker: MockerFixture
     analysis_client.mark_analyses_as_delivered.assert_called_once_with(
         trailblazer_ids=[trailblazer_id]
     )
+
+
+def test_deliver_mix_original_non_original_samples(client: FlaskClient, mocker: MockerFixture):
+    # GIVEN a trailblazer analysis id
+    trailblazer_id = 666666
+
+    # GIVEN an analysis linked to the case
+    analysis: Analysis = create_autospec(Analysis, case=case, trailblazer_id=trailblazer_id)
+
+    # GIVEN a new sample and an existing sample
+    sample_new: Sample = create_autospec(Sample, delivered_at=None)
+    sample_existing: Sample = create_autospec(Sample, delivered_at=None)
+
+    # GIVEN a case with the two samples
+    case: Case = create_autospec(Case, samples=[sample_new, sample_existing])
+
+    case_sample_new = create_autospec(CaseSample, case=case, sample=sample_new, is_original=True)
+    case_sample_existing = create_autospec(
+        CaseSample, case=case, sample=sample_existing, is_original=False
+    )
+    case.links = [case_sample_new, case_sample_existing]
+
+    # GIVEN a store
+    status_db: FlaskStore = create_autospec(FlaskStore)
+    status_db.get_analysis_by_trailblazer_id = Mock(return_value=analysis)
+    mocker.patch.object(deliver, "db", status_db)
+
+    # GIVEN a TrailblazerAPI
+    analysis_client = create_autospec(AnalysisClient)
+    mocker.patch.object(deliver, "analysis_client", analysis_client)
+
+    # WHEN calling the endpoint
+    response = client.post(f"/api/v1/deliver?trailblazer_id={trailblazer_id}")
+
+    # THEN the response should be successful
+    assert response.status_code == HTTPStatus.OK
+
+    # THEN the samples should have been delivered
+    assert sample_new.delivered_at is not None
+    assert sample_existing.delivered_at is not None
+
+    # THEN the delivered sample should have the same delivered timestamp as before
+    assert delivered_sample.delivered_at == yesterday
+
+    # THEN
+    assert existing_sample.delivered_at is None
+
+    # THEN endpoint in Trailblazer was called
+    analysis_client.mark_analyses_as_delivered.assert_called_once_with(
+        trailblazer_ids=[trailblazer_id]
+    )
