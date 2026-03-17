7d45ed5bd89aa8dd1a660d16d4a54ecc39b2fdd9
kedhammar <alfred.kedhammar@scilifelab.se>
2026-03-17 11:55:47 +0100

    add new test

--- cg/server/endpoints/deliver.py
+++ cg/server/endpoints/deliver.py
@@ -1,35 +1,34 @@
 from datetime import datetime
 from http import HTTPStatus
 
-from flask import Blueprint, jsonify, request
+from flask import Blueprint, Response, jsonify, request
 
 from cg.server.endpoints.utils import before_request
 from cg.server.ext import analysis_client, db
-from cg.store.models import Analysis, Case, Sample
+from cg.store.models import Analysis, Case
 
 DELIVER_BLUEPRINT = Blueprint("deliver", __name__, url_prefix="/api/v1")
 DELIVER_BLUEPRINT.before_request(before_request)
 
 
-@DELIVER_BLUEPRINT.route("/deliver", methods=["POST"])  # TODO
+@DELIVER_BLUEPRINT.route("/deliver", methods=["POST"])
 def deliver_analysis():
-    """TODO"""
-    # TODO
-    # - Filter samples, select the ones from the original case
-    # - Update the sample.delivered_at
-    # - Call trailblazer to mark the analysis as delivered
-    # - Commit changes to  StatusDb if TB call went well, else rollback
+    """TODO
+    - [ ] Filter samples, select the ones from the original case
+    - [ ] Update the sample.delivered_at
+    - [ ] Call trailblazer to mark the analysis as delivered
+    - [ ] Commit changes to  StatusDb if TB call went well, else rollback
+    """
 
     if trailblazer_id := request.args.get("trailblazer_id", type=int):
         analysis: Analysis = db.get_analysis_by_trailblazer_id(trailblazer_id)
         case: Case = analysis.case
-        samples: list[Sample] = case.samples
         for case_sample in case.links:
             # TODO group meditation on attribute name
             if case_sample.is_original and not case_sample.sample.delivered_at:
                 case_sample.sample.delivered_at = datetime.now()
         analysis_client.mark_analyses_as_delivered(trailblazer_ids=[trailblazer_id])
-        return jsonify({}), HTTPStatus.OK
+        return Response(status=HTTPStatus.NO_CONTENT)
     else:
         # TODO add test
         # TODO add error message shaming the bad request
--- tests/server/endpoints/test_deliver_endpoint.py
+++ tests/server/endpoints/test_deliver_endpoint.py
@@ -40,7 +40,7 @@ def test_deliver_trailblazer_analysis(client: FlaskClient, mocker: MockerFixture
     response = client.post(f"/api/v1/deliver?trailblazer_id={trailblazer_id}")
 
     # THEN the response should be successful
-    assert response.status_code == HTTPStatus.OK
+    assert response.status_code == HTTPStatus.NO_CONTENT
 
     # THEN the samples should have been delivered
     assert sample_1.delivered_at is not None
@@ -85,7 +85,7 @@ def test_deliver_mix_original_non_original_samples(client: FlaskClient, mocker: 
     response = client.post(f"/api/v1/deliver?trailblazer_id={trailblazer_id}")
 
     # THEN the response should be successful
-    assert response.status_code == HTTPStatus.OK
+    assert response.status_code == HTTPStatus.NO_CONTENT
 
     # THEN only the new sample should be delivered
     assert sample_new.delivered_at is not None
@@ -128,7 +128,7 @@ def test_deliver_rerun_case(client: FlaskClient, mocker: MockerFixture):
     response = client.post(f"/api/v1/deliver?trailblazer_id={trailblazer_id}")
 
     # THEN the response should be successful
-    assert response.status_code == HTTPStatus.OK
+    assert response.status_code == HTTPStatus.NO_CONTENT
 
     # THEN the delivered_at for both samples should be untouched
     assert sample_1.delivered_at is yesterday
@@ -171,7 +171,50 @@ def test_mixed_delivered_at_original_samples(client: FlaskClient, mocker: Mocker
     response = client.post(f"/api/v1/deliver?trailblazer_id={trailblazer_id}")
 
     # THEN the response should be successful
-    assert response.status_code == HTTPStatus.OK
+    assert response.status_code == HTTPStatus.NO_CONTENT
+
+    # THEN only the delivered_at of the undelivered sample is updated
+    assert sample_1.delivered_at is yesterday
+    assert sample_2.delivered_at is not None
+
+    # THEN endpoint in Trailblazer was called
+    analysis_client.mark_analyses_as_delivered.assert_called_once_with(
+        trailblazer_ids=[trailblazer_id]
+    )
+
+
+def test_partial_delivery(client: FlaskClient, mocker: MockerFixture):
+    # GIVEN a trailblazer analysis id
+    trailblazer_id = 666666
+
+    # GIVEN one delivered sample and one undelivered sample
+    yesterday = datetime.now() - timedelta(days=1)
+    sample_1: Sample = create_autospec(Sample, delivered_at=yesterday)
+    sample_2: Sample = create_autospec(Sample, delivered_at=None)
+
+    # GIVEN that the two samples originally belong to this given case
+    case: Case = create_autospec(Case, samples=[sample_1, sample_2])
+    case_sample_1 = create_autospec(CaseSample, case=case, sample=sample_1, is_original=True)
+    case_sample_2 = create_autospec(CaseSample, case=case, sample=sample_2, is_original=True)
+    case.links = [case_sample_1, case_sample_2]
+
+    # GIVEN an analysis linked to the case
+    analysis: Analysis = create_autospec(Analysis, case=case, trailblazer_id=trailblazer_id)
+
+    # GIVEN a store
+    status_db: FlaskStore = create_autospec(FlaskStore)
+    status_db.get_analysis_by_trailblazer_id = Mock(return_value=analysis)
+    mocker.patch.object(deliver, "db", status_db)
+
+    # GIVEN a TrailblazerAPI
+    analysis_client = create_autospec(AnalysisClient)
+    mocker.patch.object(deliver, "analysis_client", analysis_client)
+
+    # WHEN calling the endpoint
+    response = client.post(f"/api/v1/deliver?trailblazer_id={trailblazer_id}")
+
+    # THEN the response should be successful
+    assert response.status_code == HTTPStatus.NO_CONTENT
 
     # THEN only the delivered_at of the undelivered sample is updated
     assert sample_1.delivered_at is yesterday
