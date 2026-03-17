3e9f8a508150ad1d373aff3e24be577eeba8d01b
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-03-17 11:28:58 +0100

    finish tests for current logic

--- tests/server/endpoints/test_deliver_endpoint.py
+++ tests/server/endpoints/test_deliver_endpoint.py
@@ -18,27 +18,11 @@ def test_deliver_trailblazer_analysis(client: FlaskClient, mocker: MockerFixture
     sample_1: Sample = create_autospec(Sample, delivered_at=None)
     sample_2: Sample = create_autospec(Sample, delivered_at=None)
 
-    # GIVEN a sample that is already delivered
-    yesterday = datetime.now() - timedelta(days=1)
-    delivered_sample: Sample = create_autospec(Sample, delivered_at=yesterday)
-
-    # GIVEN a sample originating from a different case
-    existing_sample: Sample = create_autospec(Sample, delivered_at=None)
-
     # GIVEN a case to be delivered
-    case: Case = create_autospec(
-        Case, samples=[sample_1, sample_2, delivered_sample, existing_sample]
-    )
-
+    case: Case = create_autospec(Case, samples=[sample_1, sample_2])
     case_sample_1 = create_autospec(CaseSample, case=case, sample=sample_1, is_original=True)
     case_sample_2 = create_autospec(CaseSample, case=case, sample=sample_2, is_original=True)
-    delivered_case_sample = create_autospec(
-        CaseSample, case=case, sample=delivered_sample, is_original=True
-    )
-    case_sample_new: CaseSample = create_autospec(
-        CaseSample, case=case, sample=existing_sample, is_original=False
-    )
-    case.links = [case_sample_1, case_sample_2, delivered_case_sample, case_sample_new]
+    case.links = [case_sample_1, case_sample_2]
 
     # GIVEN an analysis linked to the case
     analysis: Analysis = create_autospec(Analysis, case=case, trailblazer_id=trailblazer_id)
@@ -61,12 +45,6 @@ def test_deliver_trailblazer_analysis(client: FlaskClient, mocker: MockerFixture
     # THEN the samples should have been delivered
     assert sample_1.delivered_at is not None
     assert sample_2.delivered_at is not None
-
-    # THEN the delivered sample should have the same delivered timestamp as before
-    assert delivered_sample.delivered_at == yesterday
-
-    # THEN
-    assert existing_sample.delivered_at is None
 
     # THEN endpoint in Trailblazer was called
     analysis_client.mark_analyses_as_delivered.assert_called_once_with(
@@ -152,9 +130,52 @@ def test_deliver_rerun_case(client: FlaskClient, mocker: MockerFixture):
     # THEN the response should be successful
     assert response.status_code == HTTPStatus.OK
 
-    # THEN only the new sample should be delivered
-    assert sample_1.delivered_at is not None
-    assert sample_2.delivered_at is None
+    # THEN the delivered_at for both samples should be untouched
+    assert sample_1.delivered_at is yesterday
+    assert sample_2.delivered_at is yesterday
+
+    # THEN endpoint in Trailblazer was called
+    analysis_client.mark_analyses_as_delivered.assert_called_once_with(
+        trailblazer_ids=[trailblazer_id]
+    )
+
+
+def test_mixed_delivered_at_original_samples(client: FlaskClient, mocker: MockerFixture):
+    # GIVEN a trailblazer analysis id
+    trailblazer_id = 666666
+
+    # GIVEN one delivered sample and one undelivered sample
+    yesterday = datetime.now() - timedelta(days=1)
+    sample_1: Sample = create_autospec(Sample, delivered_at=yesterday)
+    sample_2: Sample = create_autospec(Sample, delivered_at=None)
+
+    # GIVEN that the two samples originally belong to this given case
+    case: Case = create_autospec(Case, samples=[sample_1, sample_2])
+    case_sample_1 = create_autospec(CaseSample, case=case, sample=sample_1, is_original=True)
+    case_sample_2 = create_autospec(CaseSample, case=case, sample=sample_2, is_original=True)
+    case.links = [case_sample_1, case_sample_2]
+
+    # GIVEN an analysis linked to the case
+    analysis: Analysis = create_autospec(Analysis, case=case, trailblazer_id=trailblazer_id)
+
+    # GIVEN a store
+    status_db: FlaskStore = create_autospec(FlaskStore)
+    status_db.get_analysis_by_trailblazer_id = Mock(return_value=analysis)
+    mocker.patch.object(deliver, "db", status_db)
+
+    # GIVEN a TrailblazerAPI
+    analysis_client = create_autospec(AnalysisClient)
+    mocker.patch.object(deliver, "analysis_client", analysis_client)
+
+    # WHEN calling the endpoint
+    response = client.post(f"/api/v1/deliver?trailblazer_id={trailblazer_id}")
+
+    # THEN the response should be successful
+    assert response.status_code == HTTPStatus.OK
+
+    # THEN only the delivered_at of the undelivered sample is updated
+    assert sample_1.delivered_at is yesterday
+    assert sample_2.delivered_at is not None
 
     # THEN endpoint in Trailblazer was called
     analysis_client.mark_analyses_as_delivered.assert_called_once_with(
