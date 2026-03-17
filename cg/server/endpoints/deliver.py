8d8370852afe9c92d5fe8afa7db3d23c986d5eaa
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2026-03-17 13:57:31 +0100

    WIP

--- cg/server/endpoints/deliver.py
+++ cg/server/endpoints/deliver.py
@@ -4,9 +4,10 @@ from http import HTTPStatus
 from flask import Blueprint, Response, jsonify, request
 
 from cg.constants import Workflow
+from cg.exc import TrailblazerAPIHTTPError
 from cg.server.endpoints.utils import before_request
 from cg.server.ext import analysis_client, db
-from cg.store.models import Analysis, Case, CaseSample, Sample
+from cg.store.models import Analysis, Case, CaseSample
 
 DELIVER_BLUEPRINT = Blueprint("deliver", __name__, url_prefix="/api/v1")
 DELIVER_BLUEPRINT.before_request(before_request)
@@ -15,13 +16,14 @@ DELIVER_BLUEPRINT.before_request(before_request)
 @DELIVER_BLUEPRINT.route("/deliver", methods=["POST"])
 def deliver_analysis():
     """TODO
-    - [ ] Filter samples, select the ones from the original case
-    - [ ] Update the sample.delivered_at
-    - [ ] Call trailblazer to mark the analysis as delivered
+    - [x] Filter samples, select the ones from the original case
+    - [x] Update the sample.delivered_at
+    - [x] Call trailblazer to mark the analysis as delivered
     - [ ] Commit changes to  StatusDb if TB call went well, else rollback
     """
 
     if trailblazer_id := request.args.get("trailblazer_id", type=int):
+        # TODO: consider scenario when trailblazer id does not match an analysis
         analysis: Analysis = db.get_analysis_by_trailblazer_id(trailblazer_id)
         case: Case = analysis.case
         for case_sample in case.links:
@@ -32,7 +34,13 @@ def deliver_analysis():
                 and passes_on_reads(case_sample)
             ):
                 case_sample.sample.delivered_at = datetime.now()
-        analysis_client.mark_analyses_as_delivered(trailblazer_ids=[trailblazer_id])
+        try:
+            analysis_client.mark_analyses_as_delivered(trailblazer_ids=[trailblazer_id])
+        except TrailblazerAPIHTTPError:
+            db.rollback()
+            return Response(status=HTTPStatus.BAD_GATEWAY)
+        finally:
+            db.commit_to_store()
         return Response(status=HTTPStatus.NO_CONTENT)
     else:
         # TODO add test
--- tests/server/endpoints/test_deliver_endpoint.py
+++ tests/server/endpoints/test_deliver_endpoint.py
@@ -6,9 +6,11 @@ from flask.testing import FlaskClient
 from pytest_mock import MockerFixture
 
 from cg.constants import Workflow
+from cg.exc import TrailblazerAPIHTTPError
 from cg.server.endpoints import deliver
 from cg.server.ext import AnalysisClient, FlaskStore
 from cg.store.models import Analysis, Case, CaseSample, Sample
+from tests.typed_mock import TypedMock, create_typed_mock
 
 
 def test_deliver_trailblazer_analysis(client: FlaskClient, mocker: MockerFixture):
@@ -232,3 +234,45 @@ def test_partial_delivery(client: FlaskClient, mocker: MockerFixture):
     analysis_client.mark_analyses_as_delivered.assert_called_once_with(
         trailblazer_ids=[trailblazer_id]
     )
+
+
+def test_deliver_trailblazer_analysis_client_error(client: FlaskClient, mocker: MockerFixture):
+    # GIVEN a trailblazer analysis id
+    trailblazer_id = 666666
+
+    # GIVEN samples that should be delivered
+    sample_1: Sample = create_autospec(Sample, delivered_at=None)
+    sample_2: Sample = create_autospec(Sample, delivered_at=None)
+
+    # GIVEN a case to be delivered
+    case: Case = create_autospec(Case)
+    case_sample_1 = create_autospec(CaseSample, case=case, sample=sample_1, is_original=True)
+    case_sample_2 = create_autospec(CaseSample, case=case, sample=sample_2, is_original=True)
+    case.links = [case_sample_1, case_sample_2]
+
+    # GIVEN an analysis linked to the case
+    analysis: Analysis = create_autospec(Analysis, case=case, trailblazer_id=trailblazer_id)
+
+    # GIVEN a store
+    status_db: TypedMock[FlaskStore] = create_typed_mock(FlaskStore)
+    status_db.as_type.get_analysis_by_trailblazer_id = Mock(return_value=analysis)
+    mocker.patch.object(deliver, "db", status_db.as_type)
+
+    # GIVEN a TrailblazerAPI
+    analysis_client = create_autospec(AnalysisClient)
+    analysis_client.mark_analyses_as_delivered = Mock(side_effect=TrailblazerAPIHTTPError)
+    mocker.patch.object(deliver, "analysis_client", analysis_client)
+
+    # WHEN calling the endpoint
+    response = client.post(f"/api/v1/deliver?trailblazer_id={trailblazer_id}")
+
+    # THEN the response should be bad gateway
+    assert response.status_code == HTTPStatus.BAD_GATEWAY
+
+    # THEN endpoint in Trailblazer was called
+    analysis_client.mark_analyses_as_delivered.assert_called_once_with(
+        trailblazer_ids=[trailblazer_id]
+    )
+
+    # THEN the database changes were rolled back
+    status_db.as_mock.rollback.assert_called_once()
