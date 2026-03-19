ddbe6557a2ea80d1d6563614d12610c1f28e0b98
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-03-19 13:58:31 +0100

    add alembic migration

---
+++ alembic/versions/2026_03_19_ea37e15dd9c6_add_should_deliver_sample_column.py
@@ -0,0 +1,32 @@
+"""add should_deliver_sample column
+
+Revision ID: ea37e15dd9c6
+Revises: 2c9a49ce5512
+Create Date: 2026-03-19 13:43:58.661703
+
+"""
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "ea37e15dd9c6"
+down_revision = "2c9a49ce5512"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    op.add_column(
+        table_name="case_sample",
+        column=sa.Column(name="should_deliver_sample", type="boolean", nullable=False),
+    )
+    op.create_index("ix_analysis_trailblazer_id", "analysis", ["trailblazer_id"], unique=True)
+    op.create_unique_constraint("uq_analysis_trailblazer_id", "analysis", ["trailblazer_id"])
+
+
+def downgrade():
+    op.drop_constraint("uq_analysis_trailblazer_id", "analysis", type_="unique")
+    op.drop_index("ix_analysis_trailblazer_id", table_name="analysis")
+    op.drop_column(table_name="case_sample", column_name="should_deliver_sample")
