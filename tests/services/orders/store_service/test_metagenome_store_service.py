1ac90a65d0a38ae506234e7977d4aa6844247f4e
islean <isak.ohlsson.angnell@gmail.com>
2026-03-23 13:24:17 +0100

    Add metagenome storing service

--- tests/services/orders/store_service/test_metagenome_store_service.py
+++ tests/services/orders/store_service/test_metagenome_store_service.py
@@ -4,38 +4,30 @@ The function store_order_data_in_status_db is never expected to fail, as its inp
 have always been validated before calling the function.
 """
 
-import pytest
-
 from cg.services.orders.storing.implementations.metagenome_order_service import (
     StoreMetagenomeOrderService,
 )
 from cg.services.orders.validation.order_types.metagenome.models.order import MetagenomeOrder
-from cg.services.orders.validation.order_types.taxprofiler.models.order import TaxprofilerOrder
 from cg.store.models import Case, Sample
 from cg.store.store import Store
 
 
-@pytest.mark.parametrize(
-    "order_fixture",
-    ["metagenome_order", "taxprofiler_order"],
-    ids=["Metagenome", "Taxprofiler"],
-)
 def test_store_metagenome_order_data_in_status_db(
-    order_fixture: str,
+    metagenome_order: MetagenomeOrder,
     store_metagenome_order_service: StoreMetagenomeOrderService,
     store_to_submit_and_validate_orders: Store,
     ticket_id_as_int: int,
-    request: pytest.FixtureRequest,
 ):
-    # GIVEN an order
-    order: MetagenomeOrder | TaxprofilerOrder = request.getfixturevalue(order_fixture)
+    # GIVEN a metagenome order
 
     # GIVEN a store with no samples nor cases
     assert not store_to_submit_and_validate_orders._get_query(table=Sample).first()
     assert not store_to_submit_and_validate_orders.get_cases()
 
     # WHEN storing the order
-    new_samples: list[Sample] = store_metagenome_order_service.store_order_data_in_status_db(order)
+    new_samples: list[Sample] = store_metagenome_order_service.store_order_data_in_status_db(
+        metagenome_order
+    )
 
     # THEN the samples should have been stored
     db_samples: list[Sample] = store_to_submit_and_validate_orders._get_query(table=Sample).all()
