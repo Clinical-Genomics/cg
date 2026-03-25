cc3e827722701cca9fd64fff467005ac1bbc797d
Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>
2026-03-25 15:30:24 +0100

    Clean up validation models (#4967) (patch)
    ## Description
    
    This PR introduces TypeVars to enable better typing of the models in the validation flow. 
    
    ### Added
    
    - Generics for both the type of case and sample in the ordering flow to get better typing.

--- cg/services/orders/validation/models/order_with_samples.py
+++ cg/services/orders/validation/models/order_with_samples.py
@@ -1,10 +1,14 @@
+from typing import Generic, TypeVar
+
 from cg.services.orders.validation.models.order import Order
 from cg.services.orders.validation.models.sample import Sample
 
+SampleType = TypeVar("SampleType", bound=Sample)
 
-class OrderWithSamples(Order):
-    samples: list[Sample]
+
+class OrderWithSamples(Order, Generic[SampleType]):
+    samples: list[SampleType]
 
     @property
-    def enumerated_samples(self) -> enumerate[Sample]:
+    def enumerated_samples(self) -> enumerate[SampleType]:
         return enumerate(self.samples)
--- cg/services/orders/validation/order_types/mip_dna/models/order.py
+++ cg/services/orders/validation/order_types/mip_dna/models/order.py
@@ -1,7 +1,8 @@
 from cg.services.orders.validation.models.order_with_cases import OrderWithCases
 from cg.services.orders.validation.order_types.mip_dna.constants import MIPDNADeliveryType
 from cg.services.orders.validation.order_types.mip_dna.models.case import MIPDNACase
+from cg.services.orders.validation.order_types.mip_dna.models.sample import MIPDNASample
 
 
-class MIPDNAOrder(OrderWithCases[MIPDNACase]):
+class MIPDNAOrder(OrderWithCases[MIPDNACase, MIPDNASample]):
     delivery_type: MIPDNADeliveryType
