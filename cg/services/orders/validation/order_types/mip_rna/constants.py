d4c13e64669c5ece63618be97b6b47f51dd60ff3
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-04-20 16:39:55 +0200

    forgot to remove one option

--- cg/services/orders/validation/order_types/mip_rna/constants.py
+++ cg/services/orders/validation/order_types/mip_rna/constants.py
@@ -5,7 +5,6 @@ from cg.constants import DataDelivery
 
 class MIPRNADeliveryType(StrEnum):
     ANALYSIS = DataDelivery.ANALYSIS_FILES
-    ANALYSIS_SCOUT = DataDelivery.ANALYSIS_SCOUT
     FASTQ = DataDelivery.FASTQ
     FASTQ_ANALYSIS = DataDelivery.FASTQ_ANALYSIS
     NO_DELIVERY = DataDelivery.NO_DELIVERY
