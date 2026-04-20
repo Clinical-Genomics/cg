654982bac4d86c3e5c8db7524cad659d95a9cf09
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-04-20 16:13:19 +0200

    Remove unused MIP RNA delivery types from constants

--- cg/services/orders/validation/order_types/mip_rna/constants.py
+++ cg/services/orders/validation/order_types/mip_rna/constants.py
@@ -6,9 +6,6 @@ from cg.constants import DataDelivery
 class MIPRNADeliveryType(StrEnum):
     ANALYSIS = DataDelivery.ANALYSIS_FILES
     ANALYSIS_SCOUT = DataDelivery.ANALYSIS_SCOUT
-    SCOUT = DataDelivery.SCOUT
     FASTQ = DataDelivery.FASTQ
     FASTQ_ANALYSIS = DataDelivery.FASTQ_ANALYSIS
-    FASTQ_SCOUT = DataDelivery.FASTQ_SCOUT
-    FASTQ_ANALYSIS_SCOUT = DataDelivery.FASTQ_ANALYSIS_SCOUT
     NO_DELIVERY = DataDelivery.NO_DELIVERY
