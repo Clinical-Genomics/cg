ed059d2753f8cc273ebc4ec24252390619b33b9a
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2026-05-04 16:04:07 +0200

    Lazy load test scenarios for analysis starter (#5064) (patch)
    Change the scenario fixtures to return a function that will instantiate everything that is necessary for a particular workflow instead of the current eager loading which sets up all workflows for each version of the parameterized tests even though only one workflow is needed at a time.

--- tests/fixture_plugins/analysis_starter/configurator_fixtures.py
+++ tests/fixture_plugins/analysis_starter/configurator_fixtures.py
@@ -4,16 +4,7 @@ import pytest
 
 from cg.apps.lims import LimsAPI
 from cg.meta.workflow.fastq import BalsamicFastqHandler, MicrosaltFastqHandler
-from cg.models.cg_config import (
-    BalsamicConfig,
-    CGConfig,
-    NalloConfig,
-    RarediseaseConfig,
-    RnafusionConfig,
-    TaxprofilerConfig,
-    TomteConfig,
-)
-from cg.services.analysis_starter.configurator.extensions.tomte_extension import TomteExtension
+from cg.models.cg_config import BalsamicConfig, CGConfig, RarediseaseConfig
 from cg.services.analysis_starter.configurator.file_creators.balsamic_config import (
     BalsamicConfigFileCreator,
 )
@@ -23,35 +14,11 @@ from cg.services.analysis_starter.configurator.file_creators.microsalt_config im
 from cg.services.analysis_starter.configurator.file_creators.nextflow.config_file import (
     NextflowConfigFileCreator,
 )
-from cg.services.analysis_starter.configurator.file_creators.nextflow.params_file.nallo import (
-    NalloParamsFileCreator,
-)
 from cg.services.analysis_starter.configurator.file_creators.nextflow.params_file.raredisease import (
     RarediseaseParamsFileCreator,
 )
-from cg.services.analysis_starter.configurator.file_creators.nextflow.params_file.rnafusion import (
-    RNAFusionParamsFileCreator,
-)
-from cg.services.analysis_starter.configurator.file_creators.nextflow.params_file.taxprofiler import (
-    TaxprofilerParamsFileCreator,
-)
-from cg.services.analysis_starter.configurator.file_creators.nextflow.params_file.tomte_params_file_creator import (
-    TomteParamsFileCreator,
-)
-from cg.services.analysis_starter.configurator.file_creators.nextflow.sample_sheet.nallo_sample_sheet_creator import (
-    NalloSampleSheetCreator,
-)
 from cg.services.analysis_starter.configurator.file_creators.nextflow.sample_sheet.raredisease_sample_sheet_creator import (
     RarediseaseSampleSheetCreator,
-)
-from cg.services.analysis_starter.configurator.file_creators.nextflow.sample_sheet.rnafusion_sample_sheet_creator import (
-    RNAFusionSampleSheetCreator,
-)
-from cg.services.analysis_starter.configurator.file_creators.nextflow.sample_sheet.taxprofiler_sample_sheet_creator import (
-    TaxprofilerSampleSheetCreator,
-)
-from cg.services.analysis_starter.configurator.file_creators.nextflow.sample_sheet.tomte_sample_sheet_creator import (
-    TomteSampleSheetCreator,
 )
 from cg.services.analysis_starter.configurator.implementations.balsamic import BalsamicConfigurator
 from cg.services.analysis_starter.configurator.implementations.microsalt import (
@@ -93,20 +60,6 @@ def microsalt_configurator(
 
 
 @pytest.fixture
-def nallo_configurator(
-    mock_store_for_nallo_file_creators: Store, nallo_config_object: NalloConfig
-) -> NextflowConfigurator:
-    return NextflowConfigurator(
-        store=mock_store_for_nallo_file_creators,
-        config_file_creator=create_autospec(NextflowConfigFileCreator),
-        params_file_creator=create_autospec(NalloParamsFileCreator),
-        pipeline_config=nallo_config_object,
-        sample_sheet_creator=create_autospec(NalloSampleSheetCreator),
-        pipeline_extension=Mock(),
-    )
-
-
-@pytest.fixture
 def raredisease_configurator(
     mock_store_for_raredisease_file_creators: Store,
     raredisease_config_object: RarediseaseConfig,
@@ -118,49 +71,4 @@ def raredisease_configurator(
         pipeline_config=raredisease_config_object,
         sample_sheet_creator=create_autospec(RarediseaseSampleSheetCreator),
         pipeline_extension=Mock(),
-    )
-
-
-@pytest.fixture
-def rnafusion_configurator(
-    mock_store_for_rnafusion_file_creators: Store,
-    rnafusion_config_object: RnafusionConfig,
-) -> NextflowConfigurator:
-    return NextflowConfigurator(
-        store=mock_store_for_rnafusion_file_creators,
-        config_file_creator=create_autospec(NextflowConfigFileCreator),
-        params_file_creator=create_autospec(RNAFusionParamsFileCreator),
-        pipeline_config=rnafusion_config_object,
-        sample_sheet_creator=create_autospec(RNAFusionSampleSheetCreator),
-        pipeline_extension=Mock(),
-    )
-
-
-@pytest.fixture
-def taxprofiler_configurator(
-    mock_store_for_taxprofiler_file_creators: Store,
-    taxprofiler_config_object: TaxprofilerConfig,
-) -> NextflowConfigurator:
-    return NextflowConfigurator(
-        store=mock_store_for_taxprofiler_file_creators,
-        config_file_creator=create_autospec(NextflowConfigFileCreator),
-        params_file_creator=create_autospec(TaxprofilerParamsFileCreator),
-        pipeline_config=taxprofiler_config_object,
-        sample_sheet_creator=create_autospec(TaxprofilerSampleSheetCreator),
-        pipeline_extension=Mock(),
-    )
-
-
-@pytest.fixture
-def tomte_configurator(
-    mock_store_for_tomte_file_creators: Store,
-    tomte_config_object: TomteConfig,
-) -> NextflowConfigurator:
-    return NextflowConfigurator(
-        store=mock_store_for_tomte_file_creators,
-        config_file_creator=create_autospec(NextflowConfigFileCreator),
-        params_file_creator=create_autospec(TomteParamsFileCreator),
-        pipeline_config=tomte_config_object,
-        sample_sheet_creator=create_autospec(TomteSampleSheetCreator),
-        pipeline_extension=create_autospec(TomteExtension),
     )
