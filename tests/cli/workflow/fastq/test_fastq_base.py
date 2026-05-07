7d58a959de98ba066fc53d623209ee696f9eeef5
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-05-07 11:28:38 +0200

    test green

--- tests/cli/workflow/fastq/test_fastq_base.py
+++ tests/cli/workflow/fastq/test_fastq_base.py
@@ -4,7 +4,6 @@ from unittest.mock import Mock, create_autospec
 from cg.apps.tb.api import TrailblazerAPI
 from cg.apps.tb.models import TrailblazerAnalysis
 from cg.cli.workflow.raw_data.base import store_raw_data_analysis
-from cg.server.dto.orders.orders_response import Order
 from cg.store.models import Analysis, Case
 
 
@@ -19,16 +18,25 @@ def test_store_raw_data_analysis(another_case_id: str, cli_runner, raw_data_fast
             completed_at=None,
             out_dir=Path("great_path"),
             config_path=Path("great_config_path"),
-        ))
+        )
+    )
     raw_data_fastq_context.trailblazer_api_ = trailblazer_api
 
     # GIVEN a raw data fastq context
-    helpers.ensure_case(raw_data_fastq_context.status_db, case_id=another_case_id, order=create_autospec(Order))  # type: ignore pydantic model
+    order = helpers.add_order(
+        store=raw_data_fastq_context.status_db, customer_id=666, ticket_id=666
+    )
+    helpers.ensure_case(raw_data_fastq_context.status_db, case_id=another_case_id, order=order)
     case_obj: Case = raw_data_fastq_context.status_db.get_case_by_internal_id(another_case_id)
     assert not case_obj.analyses
 
     # WHEN a command is run to create an analysis for the case
-    cli_runner.invoke(store_raw_data_analysis, [another_case_id], obj=raw_data_fastq_context , catch_exceptions=False) 
+    cli_runner.invoke(
+        store_raw_data_analysis,
+        [another_case_id],
+        obj=raw_data_fastq_context,
+        catch_exceptions=False,
+    )
 
     # THEN the analysis is created
     assert (
