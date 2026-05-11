a0f7c5ab939e3bf08d00ecaee6c5f4c89d5274dd
islean <isak.ohlsson.angnell@gmail.com>
2026-05-11 15:54:58 +0200

    Add test for should skip sample

--- tests/services/analysis_starter/input_fetchers/test_fastq_fetcher.py
+++ tests/services/analysis_starter/input_fetchers/test_fastq_fetcher.py
@@ -4,7 +4,7 @@ import pytest
 from housekeeper.store.models import Archive, File
 
 from cg.apps.housekeeper.hk import HousekeeperAPI
-from cg.constants.constants import CaseActions
+from cg.constants.constants import CaseActions, Workflow
 from cg.exc import AnalysisNotReadyError
 from cg.meta.archive.archive import SpringArchiveAPI
 from cg.meta.compress.compress import CompressAPI
@@ -123,7 +123,7 @@ def test_ensure_files_are_ready_fetch_flow_cell():
 
     # GIVEN that there are raw data files
     housekeeper_api: HousekeeperAPI = create_autospec(HousekeeperAPI)
-    housekeeper_api.get_files_from_latest_version = Mock(return_value=create_autospec(File))
+    housekeeper_api.get_files_from_latest_version = Mock(return_value=[create_autospec(File)])
 
     # GIVEN that there are no files archived via DDN
     housekeeper_api.get_archived_files_for_bundle = Mock(return_value=[])
@@ -402,9 +402,10 @@ def test_ensure_files_are_ready_decompression_running():
 
 
 def test_ensure_files_are_ready_missing_raw_data_files():
-    # GIVEN a sample and a case in StatusDB
+    # GIVEN a sample
     sample: Sample = create_autospec(Sample)
-    case: Case = create_autospec(Case, samples=[sample])
+    # GIVEN a case with a workflow that does not do partial analyses
+    case: Case = create_autospec(Case, data_analysis=Workflow.RAREDISEASE, samples=[sample])
     status_db: Store = create_autospec(Store)
     status_db.get_case_by_internal_id = Mock(return_value=case)
 
@@ -441,3 +442,45 @@ def test_ensure_files_are_ready_missing_raw_data_files():
     # THEN an error is raised
     with pytest.raises(AnalysisNotReadyError):
         fastq_fetcher.ensure_files_are_ready("case_id")
+
+
+def test_ensure_files_are_ready_missing_raw_data_files_allowed_for_partial_analysis():
+    # GIVEN a sample with no reads
+    sample: Sample = create_autospec(Sample, has_reads=False)
+    # GIVEN a case with a workflow that does do partial analyses
+    case: Case = create_autospec(Case, data_analysis=Workflow.MICROSALT, samples=[sample])
+    status_db: Store = create_autospec(Store)
+    status_db.get_case_by_internal_id = Mock(return_value=case)
+
+    # GIVEN that the case is not down sampled nor external
+    status_db.is_case_down_sampled = Mock(return_value=False)
+    status_db.is_case_external = Mock(return_value=False)
+
+    # GIVEN that all Illumina runs are on disk
+    status_db.are_all_illumina_runs_on_disk = Mock(return_value=True)
+
+    # GIVEN that the sample is missing raw data files, both fastq and spring
+    housekeeper_api: HousekeeperAPI = create_autospec(HousekeeperAPI)
+    housekeeper_api.get_files_from_latest_version = Mock(return_value=[])
+
+    # GIVEN that there are no files archived via DDN
+    housekeeper_api.get_archived_files_for_bundle = Mock(return_value=[])
+
+    # GIVEN that all spring files are decompressed into FASTQ files
+    compress_api: CompressAPI = create_autospec(CompressAPI)
+    case_compression_data: CaseCompressionData = create_autospec(CaseCompressionData)
+    case_compression_data.is_spring_decompression_needed = Mock(return_value=False)
+    case_compression_data.is_spring_decompression_running = Mock(return_value=False)
+    compress_api.get_case_compression_data = Mock(return_value=case_compression_data)
+
+    # GIVEN a FastqFetcher
+    fastq_fetcher = FastqFetcher(
+        compress_api=compress_api,
+        housekeeper_api=housekeeper_api,
+        spring_archive_api=Mock(),
+        status_db=status_db,
+    )
+
+    # WHEN ensuring that the files are ready for analysis
+    # THEN no error is raised
+    fastq_fetcher.ensure_files_are_ready("case_id")
