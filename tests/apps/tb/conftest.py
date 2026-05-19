6ed359c3d85e3f86679e463370d1a75375afb3c1
kedhammar <alfred.kedhammar@scilifelab.se>
2026-05-19 15:27:42 +0200

    loose end

--- tests/apps/tb/conftest.py
+++ tests/apps/tb/conftest.py
@@ -1,64 +1,68 @@
+import json
+
 import pytest
 
 
 @pytest.fixture
 def response_with_one_rd_analysis_and_one_rsync_analysis() -> str:
-    return (
-        "{"
-        '"analyses": ['
-        "{"
-        '"case_id": "case_1",'
-        '"comment": null,'
-        '"completed_at": null,'
-        '"config_path": "/path/",'
-        '"delivered_by": null,'
-        '"delivered_date": null,'
-        '"failed_job": null,'
-        '"id": 1234,'
-        '"is_cancellable": false,'
-        '"is_delivered": false,'
-        '"is_visible": true,'
-        '"logged_at": "Sun, 10 May 2026 22:25:03 GMT",'
-        '"order_id": 12345,'
-        '"out_dir": "/some/path",'
-        '"priority": "high",'
-        '"progress": 0.0,'
-        '"started_at": "Sun, 10 May 2026 22:25:03 GMT",'
-        '"status": "pending",'
-        '"ticket_id": "1234",'
-        '"type": "other",'
-        '"uploaded_at": null,'
-        '"user_id": null,'
-        '"version": null,'
-        '"workflow": "RAREDISEASE",'
-        '"workflow_manager": "slurm"'
-        "},"
-        "{"
-        '"case_id": "case_2",'
-        '"comment": null,'
-        '"completed_at": null,'
-        '"config_path": "/path/",'
-        '"delivered_by": null,'
-        '"delivered_date": null,'
-        '"failed_job": null,'
-        '"id": 5678,'
-        '"is_cancellable": false,'
-        '"is_delivered": false,'
-        '"is_visible": true,'
-        '"logged_at": "Sun, 10 May 2026 22:25:03 GMT",'
-        '"order_id": 12345,'
-        '"out_dir": "/some/path",'
-        '"priority": "high",'
-        '"progress": 0.0,'
-        '"started_at": "Sun, 10 May 2026 22:25:03 GMT",'
-        '"status": "pending",'
-        '"ticket_id": "1234",'
-        '"type": "other",'
-        '"uploaded_at": null,'
-        '"user_id": null,'
-        '"version": null,'
-        '"workflow": "RSYNC",'
-        '"workflow_manager": "slurm"'
-        "}"
-        "]}"
-    )
+
+    response_dict = {
+        "analyses": [
+            {
+                "case_id": "case_1",
+                "comment": None,
+                "completed_at": None,
+                "config_path": "/path/",
+                "delivered_by": None,
+                "delivered_date": None,
+                "failed_job": None,
+                "id": 1234,
+                "is_cancellable": False,
+                "is_delivered": False,
+                "is_visible": True,
+                "logged_at": "Sun, 10 May 2026 22:25:03 GMT",
+                "order_id": 12345,
+                "out_dir": "/some/path",
+                "priority": "high",
+                "progress": 0.0,
+                "started_at": "Sun, 10 May 2026 22:25:03 GMT",
+                "status": "pending",
+                "ticket_id": "1234",
+                "type": "other",
+                "uploaded_at": None,
+                "user_id": None,
+                "version": None,
+                "workflow": "RAREDISEASE",
+                "workflow_manager": "slurm",
+            },
+            {
+                "case_id": "case_2",
+                "comment": None,
+                "completed_at": None,
+                "config_path": "/path/",
+                "delivered_by": None,
+                "delivered_date": None,
+                "failed_job": None,
+                "id": 5678,
+                "is_cancellable": False,
+                "is_delivered": False,
+                "is_visible": True,
+                "logged_at": "Sun, 10 May 2026 22:25:03 GMT",
+                "order_id": 12345,
+                "out_dir": "/some/path",
+                "priority": "high",
+                "progress": 0.0,
+                "started_at": "Sun, 10 May 2026 22:25:03 GMT",
+                "status": "pending",
+                "ticket_id": "1234",
+                "type": "other",
+                "uploaded_at": None,
+                "user_id": None,
+                "version": None,
+                "workflow": "RSYNC",
+                "workflow_manager": "slurm",
+            },
+        ]
+    }
+
+    return json.dumps(response_dict)
