09f98c0896c33a4cd55ed385008458259b86bc84
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-05-19 13:03:04 +0200

    Update tests/store/crud/read/test_read_sample.py
    Co-authored-by: Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>

--- tests/store/crud/read/test_read_sample.py
+++ tests/store/crud/read/test_read_sample.py
@@ -893,7 +893,7 @@ def test_get_paginated_unhandled_samples_search_sample(
     # GIVEN a searchable string
     perfect_searchable_string = "searchable"
 
-    # WHEN getting the unhandled samples in top-up using page 2 and page_size = 1
+    # WHEN getting the unhandled samples in top-up using page 1 and page_size = 2
     unhandled_samples, total = store.get_paginated_unhandled_samples(
         lims_status=LimsStatus.TOP_UP,
         page=1,
