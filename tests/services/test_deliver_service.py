742fd0a3d0305d9f3bb85fc74df9a65c1c6f11b9
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-05-28 15:58:39 +0200

    fix tests

--- tests/services/test_deliver_service.py
+++ tests/services/test_deliver_service.py
@@ -14,49 +14,33 @@ from tests.typed_mock import TypedMock, create_typed_mock
 
 
 def test_deliver_case_closes_order(mocker: MockerFixture):
-
-    # GIVEN a case with at least one uploaded analysis which is tied to an open order
-    status_db: Store = create_autospec(Store)
-    order: Order = create_autospec(Order, id=1, is_open=True)
-    not_uploaded_analysis = create_autospec(
+    # GIVEN a case with one analysis ready to be delivered
+    analysis = create_autospec(
         Analysis,
         trailblazer_id=1,
-        uploaded_at=None,
-        order=order,
-    )
-    uploaded_analysis = create_autospec(
-        Analysis,
-        trailblazer_id=2,
         uploaded_at=datetime.now(),
-        order=order,
     )
     case: Case = create_autospec(
         Case,
-        analyses=[not_uploaded_analysis, uploaded_analysis],
+        analyses=[analysis],
         internal_id="case_id",
     )
+
+    # GIVEN an open order with the case
+    order: Order = create_autospec(Order, id=1, is_open=True, cases=[case])
+    analysis.order = order
+    status_db: Store = create_autospec(Store)
     status_db.get_case_by_internal_id_strict = Mock(return_value=case)
-    order.cases = [case]
 
-    # GIVEN a Trailblazer API
+    # GIVEN a Trailblazer API with the analysis ready to be delivered
     trailblazer_api: TrailblazerAPI = create_autospec(TrailblazerAPI)
-
-    # GIVEN that there is an analysis to deliver for the case above
-    trailblazer_api.get_analyses_to_deliver_for_case = Mock(
-        return_value=[create_autospec(TrailblazerAnalysis, id=2, delivered=False)]
+    tb_analysis: TrailblazerAnalysis = create_autospec(
+        TrailblazerAnalysis, id=1, delivered=False, case_id="case_id"
     )
+    trailblazer_api.get_analyses_to_deliver_for_case = Mock(return_value=[tb_analysis])
 
-    # GIVEN that the TB analysis has been successfully delivered
-    trailblazer_api.get_delivered_analyses_for_order = Mock(
-        return_value=[
-            create_autospec(
-                TrailblazerAnalysis,
-                id=2,
-                delivered=True,
-                case_id="case_id",
-            )
-        ]
-    )
+    # GIVEN that the TB analysis is successfully delivered when delivering the case
+    trailblazer_api.get_delivered_analyses_for_order = Mock(return_value=[tb_analysis])
 
     # GIVEN a deliver service
     deliver_service = DeliverService(status_db=status_db, trailblazer_api=trailblazer_api)
@@ -65,14 +49,75 @@ def test_deliver_case_closes_order(mocker: MockerFixture):
     # WHEN delivering a case
     deliver_service.deliver_case(case_id="case_id", signature="CG")
 
+    # THEN the analysis of the case should be marked as delivered
+    mark_analyses_spy.assert_called_once_with(analyses=[analysis], signature="CG")
+
+    # THEN the order should have been closed
+    assert not order.is_open
+
+
+def test_deliver_case_order_is_not_closed(mocker: MockerFixture):
+    # GIVEN a case with an analysis ready to be delivered
+    uploaded_analysis = create_autospec(
+        Analysis,
+        id=2,
+        trailblazer_id=2,
+        uploaded_at=datetime.now(),
+    )
+    case_ready: Case = create_autospec(
+        Case,
+        analyses=[uploaded_analysis],
+        internal_id="case_ready",
+    )
+
+    # GIVEN another case with an analysis that has not been uploaded and thus is not ready to be delivered
+    not_uploaded_analysis = create_autospec(
+        Analysis,
+        id=1,
+        trailblazer_id=1,
+        uploaded_at=None,
+    )
+    case_not_ready: Case = create_autospec(
+        Case,
+        analyses=[not_uploaded_analysis],
+        internal_id="case_not_ready",
+    )
+
+    # GIVEN an open order with the two cases
+    order: Order = create_autospec(Order, id=1, is_open=True)
+    order.cases = [case_not_ready, case_ready]
+    not_uploaded_analysis.order = order
+    uploaded_analysis.order = order
+
+    # GIVEN a store for delivering the case that is ready to be delivered
+    status_db: Store = create_autospec(Store)
+    status_db.get_case_by_internal_id_strict = Mock(return_value=case_ready)
+
+    # GIVEN a Trailblazer API with the analysis of the case that is ready to be delivered
+    trailblazer_api: TrailblazerAPI = create_autospec(TrailblazerAPI)
+    tb_uploaded_analysis: TrailblazerAnalysis = create_autospec(
+        TrailblazerAnalysis, id=2, delivered=False, case_id="case_ready"
+    )
+    trailblazer_api.get_analyses_to_deliver_for_case = Mock(return_value=[tb_uploaded_analysis])
+
+    # GIVEN that the TB analysis is successfully delivered when delivering the case
+    trailblazer_api.get_delivered_analyses_for_order = Mock(return_value=[tb_uploaded_analysis])
+
+    # GIVEN a deliver service
+    deliver_service = DeliverService(status_db=status_db, trailblazer_api=trailblazer_api)
+    mark_analyses_spy = mocker.spy(deliver_service.mark_as_delivered_service, "mark_analyses")
+
+    # WHEN delivering the case that is ready to be delivered
+    deliver_service.deliver_case(case_id="case_ready", signature="CG")
+
     # THEN analysis that were not uploaded is filtered out
-    trailblazer_api.get_analyses_to_deliver_for_case.assert_called_once_with("case_id")
+    trailblazer_api.get_analyses_to_deliver_for_case.assert_called_once_with("case_ready")
 
     # THEN the analysis of the case should be marked as delivered
     mark_analyses_spy.assert_called_once_with(analyses=[uploaded_analysis], signature="CG")
 
-    # THEN the order should have been closed
-    assert not order.is_open
+    # THEN the order should have not been closed
+    assert order.is_open
 
 
 def test_deliver_case_more_than_one_found():
@@ -223,7 +268,6 @@ def test_deliver_all_cases_no_analyses_to_deliver(mocker: MockerFixture):
 
 
 def test_deliver_order_success(mocker: MockerFixture):
-    # TODO handle closure
     # GIVEN a store with an order
     status_db: TypedMock[Store] = create_typed_mock(Store)
     analysis_1 = create_autospec(Analysis, uploaded_at=datetime.now())
@@ -245,11 +289,17 @@ def test_deliver_order_success(mocker: MockerFixture):
 
     # GIVEN a Trailblazer API with two analyses to deliver for the order
     trailblazer_api: TypedMock[TrailblazerAPI] = create_typed_mock(TrailblazerAPI)
+    tb_analysis_1: TrailblazerAnalysis = create_autospec(
+        TrailblazerAnalysis, id=1, delivered=False, case_id="case_1"
+    )
+    tb_analysis_2: TrailblazerAnalysis = create_autospec(
+        TrailblazerAnalysis, id=2, delivered=False, case_id="case_2"
+    )
     trailblazer_api.as_mock.get_analyses_to_deliver = Mock(
-        return_value=[
-            create_autospec(TrailblazerAnalysis, id=1),
-            create_autospec(TrailblazerAnalysis, id=2),
-        ]
+        return_value=[tb_analysis_1, tb_analysis_2]
+    )
+    trailblazer_api.as_mock.get_delivered_analyses_for_order = Mock(
+        return_value=[tb_analysis_1, tb_analysis_2]
     )
 
     # GIVEN a Delivery Service
@@ -263,26 +313,21 @@ def test_deliver_order_success(mocker: MockerFixture):
     # WHEN delivering the order
     deliver_service.deliver_order(ticket_id=123, signature="CG")
 
-    # THEN the uploded analyses should have been marked as delivered
+    # THEN the uploaded analyses should have been marked as delivered
     mark_analyses_call.assert_called_once_with(analyses=[analysis_1, analysis_2], signature="CG")
 
-    # THEN the order should have been fetched from the database with the ticket id
-    status_db.as_mock.get_order_by_ticket_id_strict.assert_called_once_with(123)
-
-    # THEN uploaded analyses should have been fetched from the database with the trailblazer ids of the analyses to deliver
-    status_db.as_mock.get_uploaded_analyses.assert_called_once_with([1, 2])
-
-    # THEN the analyses to deliver should have been fetched from Trailblazer with the order id
-    trailblazer_api.as_mock.get_analyses_to_deliver.assert_called_once_with(1)
+    # THEN the order should have been closed
+    assert not order.is_open
 
 
 def test_deliver_order_without_analyses(mocker: MockerFixture):
-    # TODO handle closure
-    # GIVEN a store with an order without any analysis to deliver
+    # GIVEN a store with an open order without any analyses to deliver
+    order: Order = create_autospec(Order, is_open=True)
     status_db: Store = create_autospec(Store)
+    status_db.get_order_by_ticket_id_strict = Mock(return_value=order)
     status_db.get_uploaded_analyses = Mock(return_value=[])
 
-    # GIVEN a trailblazer api
+    # GIVEN a Trailblazer API
     trailblazer_api: TrailblazerAPI = create_autospec(TrailblazerAPI)
 
     # GIVEN a Delivery Service
@@ -296,6 +341,9 @@ def test_deliver_order_without_analyses(mocker: MockerFixture):
 
     # THEN we should not have marked any analysis as delivered
     mark_analyses_call.assert_not_called()
+
+    # THEN the order should have not been closed
+    assert order.is_open
 
 
 def test_deliver_order_invalid_ticket_id():
