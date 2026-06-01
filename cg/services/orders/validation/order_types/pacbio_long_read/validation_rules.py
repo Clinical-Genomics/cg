4c76c4ff66f36bb4c99dedc9844a0d7ff7f301c2
Moe Darrah <41540288+moedarrah@users.noreply.github.com>
2026-06-01 15:03:21 +0200

    Add warn_if_sex_unknown to PACBIO_LONG_READ_SAMPLE_RULES

--- cg/services/orders/validation/order_types/pacbio_long_read/validation_rules.py
+++ cg/services/orders/validation/order_types/pacbio_long_read/validation_rules.py
@@ -1,6 +1,5 @@
 from typing import Callable
 
-from cg.services.orders.validation.rules.case_sample.rules import warn_if_sex_unknown
 from cg.services.orders.validation.rules.sample.rules import (
     validate_application_compatibility,
     validate_application_exists,
@@ -14,6 +13,7 @@ from cg.services.orders.validation.rules.sample.rules import (
     validate_well_position_format,
     validate_well_positions_required,
     validate_wells_contain_at_most_one_sample,
+    warn_if_sex_unknown,
 )
 
 PACBIO_LONG_READ_SAMPLE_RULES: list[Callable] = [
