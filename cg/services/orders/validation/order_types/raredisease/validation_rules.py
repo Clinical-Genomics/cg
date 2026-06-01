8316e4a5e275d62cad7985af8b7a159e0de0ae37
Moe Darrah <41540288+moedarrah@users.noreply.github.com>
2026-06-01 15:12:39 +0200

    Add warn_if_sex_unknown to RAREDISEASE_CASE_SAMPLE_RULES

--- cg/services/orders/validation/order_types/raredisease/validation_rules.py
+++ cg/services/orders/validation/order_types/raredisease/validation_rules.py
@@ -41,6 +41,7 @@ from cg.services.orders.validation.rules.case_sample.rules import (
     validate_well_position_format,
     validate_well_positions_required,
     validate_wells_contain_at_most_one_sample,
+    warn_if_sex_unknown,
 )
 
 RAREDISEASE_CASE_RULES: list[Callable] = [
@@ -85,4 +86,5 @@ RAREDISEASE_CASE_SAMPLE_RULES: list[Callable] = [
     validate_well_positions_required,
     validate_wells_contain_at_most_one_sample,
     validate_well_position_format,
+    warn_if_sex_unknown,
 ]
