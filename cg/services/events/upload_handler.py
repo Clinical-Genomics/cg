51dd4c261fe067caf885fbf4e7beaba30a829ebd
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-06-02 13:34:50 +0200

    Add error handling and logging to completed handler for analysis uploads

--- cg/services/events/upload_handler.py
+++ cg/services/events/upload_handler.py
@@ -1,13 +1,19 @@
+import logging
 from datetime import datetime
 
 from cg.store.store import Store
 
+LOG = logging.getLogger(__name__)
+
 
 def completed(store: Store):
     def handler(message: dict):
-        store.update_analysis_uploaded_at(
-            analysis_id=message["cg.analysis_id"],
-            uploaded_at=datetime.strptime(message["uploaded_at"], "%Y-%m-%dT%H:%M:%SZ"),
-        )
+        try:
+            store.update_analysis_uploaded_at(
+                analysis_id=message["cg.analysis_id"],
+                uploaded_at=datetime.strptime(message["uploaded_at"], "%Y-%m-%dT%H:%M:%SZ"),
+            )
+        except Exception as error:
+            LOG.exception(error)
 
     return handler
--- tests/services/events/test_upload_handler.py
+++ tests/services/events/test_upload_handler.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from unittest.mock import create_autospec
+from unittest.mock import Mock, create_autospec
 
 from cg.services.events.upload_handler import completed
 from cg.store.store import Store
@@ -21,3 +21,18 @@ def test_completed_success():
     store.update_analysis_uploaded_at.assert_called_once_with(
         analysis_id=1, uploaded_at=expected_date
     )
+
+
+def test_completed_missing_analysis():
+    # GIVEN a message with a non-existing analysis id and a date
+    message = {"cg.analysis_id": 999, "uploaded_at": "2026-06-02T11:14:52Z"}
+
+    # GIVEN a store
+    store = create_autospec(Store)
+    store.update_analysis_uploaded_at = Mock(side_effect=Exception)
+
+    # WHEN a completed message is received
+    completed_handler = completed(store)
+    completed_handler(message)
+
+    # THEN it should not raise an exception
