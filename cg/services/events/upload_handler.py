9a54ab4756ebf51175a0712996a864411c13d5dc
islean <isak.ohlsson.angnell@gmail.com>
2026-06-02 13:43:26 +0200

    Added error handling

--- cg/services/events/upload_handler.py
+++ cg/services/events/upload_handler.py
@@ -8,12 +8,16 @@ LOG = logging.getLogger(__name__)
 
 def completed(store: Store):
     def handler(message: dict):
+        analysis_id = message["cg.analysis_id"]
+        uploaded_at = message["uploaded_at"]
         try:
             store.update_analysis_uploaded_at(
-                analysis_id=message["cg.analysis_id"],
-                uploaded_at=datetime.strptime(message["uploaded_at"], "%Y-%m-%dT%H:%M:%SZ"),
+                analysis_id=analysis_id,
+                uploaded_at=datetime.strptime(uploaded_at, "%Y-%m-%dT%H:%M:%SZ"),
             )
         except Exception as error:
+            LOG.error(f"Failed to update analysis uploaded_at for analysis with id {analysis_id}")
+            LOG.error(f"Failed to handle message {message}.")
             LOG.exception(error)
 
     return handler
