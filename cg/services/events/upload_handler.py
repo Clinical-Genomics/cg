8044ae44238f6e2f4927b8a5c52e1d83bbb210b2
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-06-02 13:08:07 +0200

    Refactor upload handler to parse uploaded_at date in completed handler

--- cg/services/events/upload_handler.py
+++ cg/services/events/upload_handler.py
@@ -1,11 +1,13 @@
+from datetime import datetime
+
 from cg.store.store import Store
 
 
 def completed(store: Store):
     def handler(message: dict):
-        # TODO parse date
         store.update_analysis_uploaded_at(
-            analysis_id=message["cg.analysis_id"], uploaded_at=message["uploaded_at"]
+            analysis_id=message["cg.analysis_id"],
+            uploaded_at=datetime.strptime(message["uploaded_at"], "%Y-%m-%dT%H:%M:%SZ"),
         )
 
     return handler
