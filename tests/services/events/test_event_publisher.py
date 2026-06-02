d3cf9496c5132953e5e831256e90fb9079fbecb7
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2026-06-02 09:06:52 +0200

    WIP started event publisher

---
+++ tests/services/events/test_event_publisher.py
@@ -0,0 +1,39 @@
+from pathlib import Path
+
+from cg.models.cg_config import NatsAuthentication, NatsConfig
+from cg.services.events import event_publisher
+
+
+def test_publish_command():
+    nats_config = NatsConfig(
+        server="nats://server",
+        publisher=NatsAuthentication(
+            ca_cert_path=Path("ca_cert"),
+            client_cert_path=Path("client_cert"),
+            client_key_path=Path("client_key"),
+            nats_binary_path=Path("nats_binary"),
+            token_path=Path("/token/path"),
+        ),
+        listener=NatsAuthentication(
+            ca_cert_path=Path("ca_cert"),
+            client_cert_path=Path("client_cert"),
+            client_key_path=Path("client_key"),
+            nats_binary_path=Path("nats_binary"),
+            token_path=Path("token/path"),
+        ),
+    )
+    subject = "cg.upload.completed"
+    data = {"analysis": "analysis_1", "uploaded_at": "$(date +%Y-%m-%dT%H:%M:%SZ)"}
+
+    command = event_publisher.publish_command(nats_config=nats_config, subject=subject, data=data)
+    expected = (
+        "nats_binary pub "
+        "--server nats://server "
+        "--tlsca ca_cert "
+        "--tlscert client_cert "
+        "--tlskey client_key "
+        "--token $(cat /token/path) "
+        'cg.upload.completed \'{"analysis": "analysis_1", "uploaded_at": "$(date +%Y-%m-%dT%H:%M:%SZ)"}\''
+    )
+
+    assert command == expected
