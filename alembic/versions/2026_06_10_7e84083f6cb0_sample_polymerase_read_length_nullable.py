2307097e723543010ad9103c76354f9ce50d404c
islean <isak.ohlsson.angnell@gmail.com>
2026-06-11 09:43:08 +0200

    Fix migration

--- alembic/versions/2026_06_10_7e84083f6cb0_sample_polymerase_read_length_nullable.py
+++ alembic/versions/2026_06_10_7e84083f6cb0_sample_polymerase_read_length_nullable.py
@@ -6,9 +6,10 @@ Create Date: 2026-06-10 11:38:36.405971
 
 """
 
+from typing import Annotated
+
 import sqlalchemy as sa
 from sqlalchemy import BigInteger, ForeignKey
-from sqlalchemy.dialects.mysql import BIGINT
 from sqlalchemy.orm import DeclarativeBase, Mapped, Session, mapped_column
 
 from alembic import op
@@ -19,14 +20,22 @@ down_revision = "8cd14469c35f"
 branch_labels = None
 depends_on = None
 
+BigInt = Annotated[int, None]
 
-class PacbioSampleSequencingMetrics(DeclarativeBase):
+
+class Base(DeclarativeBase):
+    type_annotation_map = {
+        BigInt: BigInteger,
+    }
+
+
+class PacbioSampleSequencingMetrics(Base):
     __tablename__ = "pacbio_sample_run_metrics"
 
     id: Mapped[int] = mapped_column(
         ForeignKey("sample_run_metrics.id", ondelete="CASCADE"), primary_key=True
     )
-    polymerase_mean_read_length: Mapped[BigInteger | None]
+    polymerase_mean_read_length: Mapped[BigInt]
 
 
 def upgrade():
@@ -47,7 +56,7 @@ def downgrade():
         .filter(PacbioSampleSequencingMetrics.polymerase_mean_read_length.is_(None))
         .all()
     ):
-        sample_metric.polymerase_mean_read_length = BIGINT(1)
+        sample_metric.polymerase_mean_read_length = 0
     session.commit()
     op.alter_column(
         table_name="pacbio_sample_run_metrics",
