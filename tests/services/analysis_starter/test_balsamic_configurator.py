d6af81718d0888c066d85bebf62648c4a4723bc3
islean <isak.ohlsson.angnell@gmail.com>
2026-06-12 10:50:15 +0200

    Set Balsamic default workflow profile

--- tests/services/analysis_starter/test_balsamic_configurator.py
+++ tests/services/analysis_starter/test_balsamic_configurator.py
@@ -14,7 +14,9 @@ from cg.models.cg_config import BalsamicConfig
 from cg.services.analysis_starter.configurator.file_creators.balsamic_config import (
     BalsamicConfigFileCreator,
 )
-from cg.services.analysis_starter.configurator.implementations.balsamic import BalsamicConfigurator
+from cg.services.analysis_starter.configurator.implementations.balsamic_configurator import (
+    BalsamicConfigurator,
+)
 from cg.services.analysis_starter.configurator.models.balsamic import BalsamicCaseConfig
 from cg.store.models import Case, Sample
 from cg.store.store import Store
@@ -33,7 +35,53 @@ def case_with_sample() -> Case:
 
 @pytest.mark.parametrize("workflow", [Workflow.BALSAMIC, Workflow.BALSAMIC_UMI])
 def test_get_config(
-    balsamic_configurator: BalsamicConfigurator, case_id: str, tmp_path: Path, workflow: Workflow
+    balsamic_configurator: BalsamicConfigurator,
+    case_id: str,
+    tmp_path: Path,
+    workflow: Workflow,
+    cg_balsamic_config: BalsamicConfig,
+):
+    """Tests that the get_config method returns a BalsamicCaseConfig and that fields can be overridden with flags."""
+    # GIVEN a Balsamic configurator with an existing config file
+    balsamic_configurator.root_dir = tmp_path
+    Path(balsamic_configurator.root_dir, case_id).mkdir()
+    Path(balsamic_configurator.root_dir, case_id, f"{case_id}.json").touch()
+
+    # GIVEN that the database returns a case with the provided case_id
+    case_to_configure: Case = create_autospec(
+        Case, internal_id=case_id, slurm_priority=SlurmQos.NORMAL
+    )
+    balsamic_configurator.store.get_case_by_internal_id_strict = Mock(
+        return_value=case_to_configure
+    )
+    balsamic_configurator.store.get_case_workflow = Mock(return_value=workflow)
+
+    # WHEN getting the config
+    config: BalsamicCaseConfig = balsamic_configurator.get_config(case_id=case_id)
+
+    # THEN the config should look as expected
+    expected_balsamic_case_config = BalsamicCaseConfig(
+        case_id=case_id,
+        account=cg_balsamic_config.slurm.account,
+        binary=cg_balsamic_config.binary_path,
+        conda_binary=cg_balsamic_config.conda_binary,
+        environment=cg_balsamic_config.conda_env,
+        head_job_partition=cg_balsamic_config.head_job_partition,
+        qos=SlurmQos.NORMAL,
+        sample_config=Path(balsamic_configurator.root_dir, case_id, f"{case_id}.json"),
+        workflow=workflow,
+        workflow_profile=cg_balsamic_config.workflow_profile,
+    )
+    assert config == expected_balsamic_case_config
+
+
+@pytest.mark.parametrize("workflow", [Workflow.BALSAMIC, Workflow.BALSAMIC_UMI])
+def test_get_config_with_flag(
+    balsamic_configurator: BalsamicConfigurator,
+    case_id: str,
+    tmp_path: Path,
+    workflow: Workflow,
+    cg_balsamic_config: BalsamicConfig,
 ):
     """Tests that the get_config method returns a BalsamicCaseConfig and that fields can be overridden with flags."""
     # GIVEN a Balsamic configurator with an existing config file
@@ -55,11 +103,20 @@ def test_get_config(
         case_id=case_id, qos=SlurmQos.EXPRESS
     )
 
-    # THEN the config should be a BalsamicCaseConfig
-    assert isinstance(config, BalsamicCaseConfig)
-
-    # THEN the qos should be set to the provided value
-    assert config.qos == SlurmQos.EXPRESS
+    # THEN the config should look as expected
+    expected_balsamic_case_config = BalsamicCaseConfig(
+        case_id=case_id,
+        account=cg_balsamic_config.slurm.account,
+        binary=cg_balsamic_config.binary_path,
+        conda_binary=cg_balsamic_config.conda_binary,
+        environment=cg_balsamic_config.conda_env,
+        head_job_partition=cg_balsamic_config.head_job_partition,
+        qos=SlurmQos.EXPRESS,
+        sample_config=Path(balsamic_configurator.root_dir, case_id, f"{case_id}.json"),
+        workflow=workflow,
+        workflow_profile=cg_balsamic_config.workflow_profile,
+    )
+    assert config == expected_balsamic_case_config
 
 
 def test_get_config_missing_config_file(
@@ -136,6 +193,7 @@ def test_configure(cg_balsamic_config: BalsamicConfig, workflow: Workflow, mocke
         qos=SlurmQos.NORMAL,
         sample_config=Path(cg_balsamic_config.root, "case_id", "case_id.json"),
         workflow=workflow,
+        workflow_profile=cg_balsamic_config.workflow_profile,
     )
 
 
