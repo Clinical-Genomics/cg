35a8965515a5d36acec5bdcfc85f1a5b259088ac
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-06-18 15:50:11 +0200

    Update delivery messages to reflect completed uploads

--- tests/fixture_plugins/delivery_message_fixtures/message_fixtures.py
+++ tests/fixture_plugins/delivery_message_fixtures/message_fixtures.py
@@ -10,7 +10,7 @@ def analysis_scout_message(mip_case_id: str, customer_id: str, ticket_id: str) -
         "Hello,\n\n"
         "The analysis has been uploaded to Scout for the following case:\n\n"
         f"https://scout.scilifelab.se/{customer_id}/{mip_case_id}\n\n"
-        "The analysis files are currently being uploaded to your inbox on Caesar:\n\n"
+        "The analysis files have been uploaded to your inbox on Caesar:\n\n"
         f"/home/{customer_id}/inbox/{ticket_id} \n\n"
         f"{REMINDER_TO_DOWNLOAD_MESSAGE}"
     )
@@ -21,9 +21,9 @@ def microsalt_message(customer_id: str, ticket_id: str) -> str:
     """Return the delivery message for a microSALT case."""
     return (
         "Hello,\n\n"
-        "The analysis is now complete and the fastq files are being uploaded to:\n\n"
+        "The analysis is now complete.\n\n"
+        " The fastq files and the QC and Typing reports have been uploaded to:\n\n"
         f"/home/{customer_id}/inbox/{ticket_id} \n\n"
-        "The QC and Typing reports can be found attached. \n\n"
         f"{REMINDER_TO_DOWNLOAD_MESSAGE}"
     )
 
@@ -34,7 +34,7 @@ def raw_data_analysis_message() -> str:
 
     return (
         "Hello,\n\n"
-        "The raw data and analysis files for the following case are currently being "
+        "The raw data and analysis files for the following case have been "
         "uploaded to your inbox on Caesar:\n\n"
         "{case_id}\n\n"
         "Available under: \n"
@@ -50,7 +50,7 @@ def raw_data_analysis_scout38_message() -> str:
         "Hello,\n\n"
         "The analysis has been uploaded to Scout for the following case:\n\n"
         "https://scout38.sys.scilifelab.se/{customer_id}/{case_id}\n\n"
-        "The raw data and analysis files are currently being uploaded to your inbox on Caesar:\n\n"
+        "The raw data and analysis files have been uploaded to your inbox on Caesar:\n\n"
         "/home/{customer_id}/inbox/{ticket_id} \n\n"
         f"{REMINDER_TO_DOWNLOAD_MESSAGE}"
     )
@@ -63,7 +63,7 @@ def raw_data_scout38_message() -> str:
         "Hello,\n\n"
         "The analysis has been uploaded to Scout for the following case:\n\n"
         "https://scout38.sys.scilifelab.se/{customer_id}/{case_id}\n\n"
-        "The raw data files are currently being uploaded to your inbox on Caesar:\n\n"
+        "The raw data files have been uploaded to your inbox on Caesar:\n\n"
         "/home/{customer_id}/inbox/{ticket_id} \n\n"
         f"{REMINDER_TO_DOWNLOAD_MESSAGE}"
     )
