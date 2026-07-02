aba10f582cf5909c65d29d83ddee78b807b274be
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-07-02 10:54:41 +0200

    patch linting and test

--- tests/server/endpoints/test_samples_endpoint.py
+++ tests/server/endpoints/test_samples_endpoint.py
@@ -497,24 +497,25 @@ def test_get_unhandled_samples_filter_on_unknown_workflow(
         lims_status=LimsStatus.TOP_UP,
         page=1,
         page_size=10,
+        priority=None,
         search=None,
         sort_by=None,
         sort_order=None,
         workflow="unknown",
     )
 
- 
+
 def test_get_unhandled_samples_filter_on_priority(client: FlaskClient, mocker: MockerFixture):
-      # GIVEN a store with unhandled samples in top-up
+    # GIVEN a store with unhandled samples in top-up
     status_db: TypedMock[Store] = create_typed_mock(Store)
     status_db.as_type.get_paginated_unhandled_samples = Mock(return_value=([], 0))
     mocker.patch.object(samples, "db", status_db.as_type)
-    
+
     # WHEN querying the unhandled samples endpoint with workflow Raredisease
     response = client.get(
-      path=f"/api/v1/unhandled_samples?lims_status=top-up&page=1&page_size=10&priority={TrailblazerPriority.EXPRESS}",
+        path=f"/api/v1/unhandled_samples?lims_status=top-up&page=1&page_size=10&priority={TrailblazerPriority.EXPRESS}",
     )
-    
+
     # THEN the response should be successful
     assert response.status_code == HTTPStatus.OK
 
@@ -529,7 +530,7 @@ def test_get_unhandled_samples_filter_on_priority(client: FlaskClient, mocker: M
         sort_order=None,
         workflow=None,
     )
-    
+
 
 def test_get_unhandled_samples_unknown_param(client: FlaskClient, mocker: MockerFixture):
     # GIVEN an unknown param in the request
