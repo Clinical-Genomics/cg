060dfcbceadd1f035b1f55be1c2854e3495a3848
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-07-08 14:55:08 +0200

    Correction

--- cg/apps/tb/api.py
+++ cg/apps/tb/api.py
@@ -268,8 +268,7 @@ class TrailblazerAPI:
             raise AnalysisNotCompletedError(f"The latest analysis for {case_id} has not completed.")
 
     def get_analyses_to_deliver_for_case(self, case_id: str) -> list[TrailblazerAnalysis]:
-        exclude_workflows = f"excludeWorkflow={Workflow.RSYNC.upper()}&excludeWorkflow={Workflow.DEMULTIPLEX.upper()}"
-        endpoint = f"analyses?case_id={case_id}&status[]={AnalysisStatus.COMPLETED}&delivered=false&holdDelivery=false&{exclude_workflows}"
+        endpoint = f"analyses?case_id={case_id}&status[]={AnalysisStatus.COMPLETED}&delivered=false&holdDelivery=false"
         raw_response = self.query_trailblazer(
             command=endpoint, request_body={}, method=APIMethods.GET
         )
@@ -277,9 +276,8 @@ class TrailblazerAPI:
         return validated_response.analyses
 
     def get_all_analyses_to_deliver(self) -> list[TrailblazerAnalysis]:
-        endpoint = (
-            f"analyses?status[]={AnalysisStatus.COMPLETED}&delivered=false&holdDelivery=false"
-        )
+        exclude_workflows = f"excludeWorkflow={Workflow.RSYNC.upper()}&excludeWorkflow={Workflow.DEMULTIPLEX.upper()}"
+        endpoint = f"analyses?status[]={AnalysisStatus.COMPLETED}&delivered=false&holdDelivery=false&{exclude_workflows}"
         raw_response = self.query_trailblazer(
             command=endpoint, request_body={}, method=APIMethods.GET
         )
