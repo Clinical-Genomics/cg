029db31cd0f192896b7a0b37d0cfc5fa62d210e2
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-07-30 11:33:48 +0200

    new listen command

---
+++ cg/cli/listen.py
@@ -0,0 +1,20 @@
+"""Standalone listen command."""
+
+import logging
+
+import rich_click as click
+
+from cg.server.app_config import AppConfig
+
+pass_app_config = click.make_pass_decorator(AppConfig, ensure=True)
+
+LOG = logging.getLogger(__name__)
+
+
+@click.command("listen")
+@pass_app_config
+def listen(app_config: AppConfig):
+    """Listen for incoming event messages."""
+    # Placeholder while AppConfig-driven listen behavior is implemented.
+    LOG.info(f"{app_config.freshdesk_url}")
+    LOG.info(f"{app_config.freshdesk_environment}")
