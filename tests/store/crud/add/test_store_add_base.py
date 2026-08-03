5e5b4c396947a9538460065165942e06c2b6b8d8
islean <isak.ohlsson.angnell@gmail.com>
2026-08-03 11:25:08 +0200

    Fix test

--- tests/store/crud/add/test_store_add_base.py
+++ tests/store/crud/add/test_store_add_base.py
@@ -128,7 +128,7 @@ def test_add_pool(rml_pool_store: Store):
         .filter(ApplicationVersion.application_id == application.id)
         .first()
     )
-    db_order: Order = rml_pool_store.add_order(customer=customer, ticket_id=123456)
+    db_order: Order = rml_pool_store.get_order_by_ticket_id(123456)
 
     # WHEN adding a new pool
     new_pool = rml_pool_store.add_pool(
