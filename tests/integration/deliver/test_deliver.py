b69b69048df2b0d4e0d76d14553d485bab64a2f4
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-08-06 15:58:09 +0200

    initial stash commit

---
+++ tests/integration/deliver/test_deliver.py
@@ -0,0 +1,47 @@
+from pathlib import Path
+
+import pytest
+from click.testing import CliRunner, Result
+from pytest_httpserver import HTTPServer
+
+from cg.cli.base import base
+from cg.constants.constants import Workflow
+from tests.integration.utils import (
+    IntegrationTestPaths,
+    expect_to_get_all_analyses_to_deliver_from_trailblazer,
+)
+
+
+@pytest.mark.xdist_group(name="integration")
+@pytest.mark.integration
+def test_deliver_all_available(httpserver: HTTPServer, test_run_paths: IntegrationTestPaths):
+    cli_runner = CliRunner()
+
+    # GIVEN a config file with valid database URIs and directories
+    config_path: Path = test_run_paths.cg_config_file
+
+    # GIVEN a database with orders ready for delivery
+    expect_to_get_all_analyses_to_deliver_from_trailblazer(
+        trailblazer_server=httpserver,
+        exclude_workflows=[
+            Workflow.MICROSALT,
+            Workflow.TAXPROFILER,
+            Workflow.DEMULTIPLEX,
+            Workflow.RSYNC,
+        ],
+    )
+
+    # WHEN running deliver all available
+    result: Result = cli_runner.invoke(
+        base,
+        [
+            "--config",
+            config_path.as_posix(),
+            "deliver",
+            "all-available",
+        ],
+        catch_exceptions=False,
+    )
+
+    # THEN the delivery was successful
+    assert result.exit_code == 0
