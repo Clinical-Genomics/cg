77544c87af73225b31df2f46b0375c08ad2a0097
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2026-08-20 11:30:13 +0200

    Test for parsing json

--- tests/cli/test_receive_event.py
+++ tests/cli/test_receive_event.py
@@ -1,4 +1,5 @@
-from unittest.mock import ANY, create_autospec
+from json import JSONDecodeError
+from unittest.mock import create_autospec
 
 from click.testing import CliRunner
 from pytest_mock import MockerFixture
@@ -19,19 +20,44 @@ def test_receive_event_success(mocker: MockerFixture):
     # WHEN calling the receive event command
     result = cli_runner.invoke(
         receive_event,
-        args=["cg-test.something-happened", "--data", "{'key': 'value'}"],
+        args=["cg-test.something-happened", "--data", '{"key": "value"}'],
         obj=cg_config,
-        catch_exceptions=False,
     )
 
     # THEN it calls the event handler
     handle_spy.assert_called_once_with(
-        config=cg_config, event_name="cg-test.something-happened", data=ANY
+        config=cg_config, event_name="cg-test.something-happened", data={"key": "value"}
     )
 
     # THEN the result exits successfully
     assert result.exit_code == 0
 
 
-def test_receive_event_json_parsing_fails():
+def test_receive_event_json_parsing_fails(mocker: MockerFixture):
+    # GIVEN a CliRunner
+    cli_runner = CliRunner()
+
+    handle_spy = mocker.spy(event_handler, "handle")
+
+    # GIVEN a CGConfig
+    cg_config = create_autospec(CGConfig)
+
+    # WHEN calling the receive event command with a malformed json
+    result = cli_runner.invoke(
+        receive_event,
+        args=["cg-test.something-happened", "--data", "this is a string"],
+        obj=cg_config,
+    )
+
+    # THEN it should not call the event handler
+    handle_spy.assert_not_called()
+
+    # THEN the result exits unsuccessfully
+    assert result.exit_code != 0
+
+    # THEN the error is because of the malformed json input
+    assert isinstance(result.exception, JSONDecodeError)
+
+
+def test_receive_event_no_data():
     pass
