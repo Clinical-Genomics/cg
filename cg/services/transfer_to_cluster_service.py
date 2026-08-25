677bdc119b182df11c1a385b2d65ab9ab7fb9a4f
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-08-25 14:28:30 +0200

    WIP

--- cg/services/transfer_to_cluster_service.py
+++ cg/services/transfer_to_cluster_service.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 
 from cg.apps.slurm.slurm_api import SlurmAPI
+from cg.constants.priority import SlurmQos
 from cg.models.cg_config import CGConfig
 from cg.models.slurm.sbatch import Sbatch
 from cg.services.deliver_files.rsync.sbatch_commands import (
@@ -13,11 +14,11 @@ def transfer_sample(cg_config: CGConfig, customer_internal_id: str, sample_name:
     # TODO submit a slurm job that calls rsync to transfer sample data from customer inbox to hasta
     slurm_api = SlurmAPI()
     sbatch_path = Path(cg_config.data_delivery.base_path, f"{customer_internal_id}_{sample_name}")
-    sbatch_content = ""
     command: str = RSYNC_CONTENTS_COMMAND.format(
         source_path=Path(cg_config.external.caesar % customer_internal_id, sample_name),
         destination_path=Path(cg_config.external.hasta % customer_internal_id, sample_name),
     )
+    # TODO add a publisher to the slurm job
     sbatch_parameters = Sbatch(
         job_name=f"{customer_internal_id}_{sample_name}_rsync_external_data",
         account=cg_config.data_delivery.account,
@@ -27,10 +28,10 @@ def transfer_sample(cg_config: CGConfig, customer_internal_id: str, sample_name:
         email=cg_config.data_delivery.mail_user,
         hours=24,
         commands=command,
-        error=ERROR_RSYNC_FUNCTION.format(),
+        error=ERROR_RSYNC_FUNCTION,
+        quality_of_service=SlurmQos.NORMAL,
     )
-    self.slurm_api.set_dry_run(dry_run=dry_run)
-    sbatch_content: str = self.slurm_api.generate_sbatch_content(sbatch_parameters)
+    sbatch_content: str = slurm_api.generate_sbatch_content(sbatch_parameters)
     slurm_api.submit_sbatch(sbatch_content=sbatch_content, sbatch_path=sbatch_path)
     # TODO publish an event when successful
     pass
--- tests/services/test_transfer_to_cluster_service.py
+++ tests/services/test_transfer_to_cluster_service.py
@@ -1,14 +1,56 @@
 from pathlib import Path
 from unittest.mock import create_autospec
 
+import pytest
 from pytest_mock import MockerFixture
 
 from cg.apps.slurm.slurm_api import SlurmAPI
-from cg.models.cg_config import CGConfig, DataDeliveryConfig
+from cg.models.cg_config import CGConfig, DataDeliveryConfig, ExternalConfig
 from cg.services import transfer_to_cluster_service
 
 
-def test_transfer_sample(mocker: MockerFixture):
+@pytest.fixture
+def expected_sbatch_content() -> str:
+    return """#! /bin/bash
+#SBATCH --job-name=cust000_sample-name_rsync_external_data
+#SBATCH --account=account
+#SBATCH --ntasks=1
+#SBATCH --mem=1G
+#SBATCH --error=/base/path/cust000_sample-name/cust000_sample-name_rsync_external_data.stderr
+#SBATCH --output=/base/path/cust000_sample-name/cust000_sample-name_rsync_external_data.stdout
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=mail@scilifelab.se
+#SBATCH --time=24:00:00
+#SBATCH --qos=normal
+
+
+set -eu -o pipefail
+
+log() {
+    NOW=$(date +"%Y-%m-%dT%H:%M:%S")
+    echo "[${NOW}] $*" 1>&2;
+}
+
+log "Running on: $(hostname)"
+
+
+error() {
+
+echo "Rsync failed"
+
+    exit 1
+}
+
+trap error ERR
+
+
+rsync -rvL /path/to/rome/cust000/sample-name/ /path/to/hasta/cust000/sample-name
+
+
+"""
+
+
+def test_transfer_sample(mocker: MockerFixture, expected_sbatch_content):
     # GIVEN a customer internal id and a sample name
     customer_internal_id = "cust000"
     sample_name = "sample-name"
@@ -24,6 +66,7 @@ def test_transfer_sample(mocker: MockerFixture):
             destination_path="/unknown",
             mail_user="mail@scilifelab.se",
         ),
+        external=ExternalConfig(hasta="/path/to/hasta/%s", caesar="/path/to/rome/%s"),
     )
 
     submit_sbatch_mock = mocker.patch.object(SlurmAPI, "submit_sbatch")
@@ -35,5 +78,6 @@ def test_transfer_sample(mocker: MockerFixture):
 
     # THEN the Slurm API should have been called with an SBATCH with the correct content
     submit_sbatch_mock.assert_called_once_with(
-        sbatch_path=Path("/base", "path", "cust000_sample-name")
+        sbatch_path=Path("/base", "path", "cust000_sample-name"),
+        sbatch_content=expected_sbatch_content,
     )
