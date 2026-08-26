853b89694dbad18e97b90d8bca3d653bd85447e2
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-08-26 11:53:11 +0200

    WIP

--- cg/services/events/event_handler.py
+++ cg/services/events/event_handler.py
@@ -14,8 +14,8 @@ EVENT_HANDLERS: dict = {
 
 def handle(config: CGConfig, event_name: str, data: dict, event_handlers: dict = EVENT_HANDLERS):
     """Select the appropriate handler for the given event name and call it with the provided data."""
-    handler: Callable | None = event_handlers.get(event_name)
-    if handler:
-        handler(config=config, data=data)
+    handle_function: Callable | None = event_handlers.get(event_name)
+    if handle_function:
+        handle_function(config=config, data=data)
     else:
         LOG.info(f"No handler for event {event_name}")
