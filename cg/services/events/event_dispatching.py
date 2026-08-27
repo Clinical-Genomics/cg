f8ee11d130b32f55cd5c36b9221a5d7bd70c46ab
islean <isak.ohlsson.angnell@gmail.com>
2026-08-27 15:52:37 +0200

    Use Protocol

--- cg/services/events/event_dispatching.py
+++ cg/services/events/event_dispatching.py
@@ -1,5 +1,5 @@
 import logging
-from typing import Callable
+from typing import Protocol
 
 from cg.models.cg_config import CGConfig
 from cg.services.events.event_handlers import external_sample_uploaded_handler
@@ -7,16 +7,23 @@ from cg.services.events.event_handlers import external_sample_uploaded_handler
 LOG = logging.getLogger(__name__)
 
 
-EVENT_HANDLERS: dict[str, Callable] = {
+class EventHandler(Protocol):
+    def __call__(self, config: CGConfig, event_payload: dict) -> None: ...
+
+
+EVENT_HANDLERS: dict[str, EventHandler] = {
     "external.customer_uploaded_sample": external_sample_uploaded_handler.handle
 }
 
 
 def dispatch(
-    config: CGConfig, event_name: str, event_payload: dict, event_handlers: dict = EVENT_HANDLERS
+    config: CGConfig,
+    event_name: str,
+    event_payload: dict,
+    event_handlers: dict[str, EventHandler] = EVENT_HANDLERS,
 ):
     """Select the appropriate handler for the given event name and call it with the provided data."""
-    handle_function: Callable | None = event_handlers.get(event_name)
+    handle_function: EventHandler | None = event_handlers.get(event_name)
     if handle_function:
         handle_function(config=config, event_payload=event_payload)
     else:
