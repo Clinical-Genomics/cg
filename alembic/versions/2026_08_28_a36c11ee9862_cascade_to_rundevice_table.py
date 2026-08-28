59a7542b615ffdd185c17e6599fc143e48c07514
islean <isak.ohlsson.angnell@gmail.com>
2026-08-28 10:08:02 +0200

    Created alembic revision

---
+++ alembic/versions/2026_08_28_a36c11ee9862_cascade_to_rundevice_table.py
@@ -0,0 +1,25 @@
+"""Cascade to RunDevice table
+
+Revision ID: a36c11ee9862
+Revises: 8a0dd32c2550
+Create Date: 2026-08-28 08:56:40.081418
+
+"""
+
+import sqlalchemy as sa
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision = "a36c11ee9862"
+down_revision = "8a0dd32c2550"
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
