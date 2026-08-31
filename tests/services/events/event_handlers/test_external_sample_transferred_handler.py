f739d58baa8f1fdc2f1c7789b9950683ac92bfe8
kedhammar <alfred.kedhammar@scilifelab.se>
2026-08-31 14:41:58 +0200

    wip tests

---
+++ tests/services/events/event_handlers/test_external_sample_transferred_handler.py
@@ -0,0 +1,20 @@
+from unittest.mock import create_autospec
+
+from cg.models.cg_config import CGConfig
+
+
+def test_handle():
+    # GIVEN a CG config
+    config: CGConfig = create_autospec(CGConfig)
+
+    # GIVEN a valid event payload
+    event_payload = {
+        "cg.sample_internal_id": "ACC123",
+        "cluster_location": "/path/to/home",
+        "transfer_completed_at": "2026-08-31T14:41:00",
+    }
+
+    # WHEN calling handle
+
+    # THEN
+    pass
