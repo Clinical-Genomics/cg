40e11471cf511a731c4954c3d600a4b72994157c
Moe Darrah <41540288+moedarrah@users.noreply.github.com>
2026-08-31 13:47:45 +0200

    WIP

--- cg/services/events/event_handlers/external_sample_transferred_handler.py
+++ cg/services/events/event_handlers/external_sample_transferred_handler.py
@@ -1,5 +1,21 @@
+from datetime import datetime
+from pathlib import Path
+
+from pydantic import BaseModel, Field
+
 from cg.models.cg_config import CGConfig
 
 
+class ExternalSampleTransferredEvent(BaseModel):
+    sample_internal_id: str = Field(alias="cg.sample_internal_id")
+    cluster_location: Path
+    transfer_completed_at: datetime
+
+
 def handle(config: CGConfig, event_payload: dict) -> None:
-    # TODO: Validate payload
+    event = ExternalSampleTransferredEvent.model_validate(event_payload)
+    # TODO: Get bundle name and tags
+
+    # TODO: Call Housekeeper API to add file
+
+    # TODO: Publish event that storing is complete
