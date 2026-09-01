14102c6c59679f344f7b64c0c8e3e471d504fcb2
Moe Darrah <41540288+moedarrah@users.noreply.github.com>
2026-09-01 10:50:44 +0200

    WIP

--- tests/services/events/event_handlers/test_external_sample_transferred_handler.py
+++ tests/services/events/event_handlers/test_external_sample_transferred_handler.py
@@ -1,9 +1,10 @@
 from unittest.mock import create_autospec
 
 from cg.models.cg_config import CGConfig
+from cg.services.events.event_handlers import external_sample_transferred_handler
 
 
-def test_handle():
+def test_handle_success():
     # GIVEN a CG config
     config: CGConfig = create_autospec(CGConfig)
 
@@ -15,6 +16,11 @@ def test_handle():
     }
 
     # WHEN calling handle
+    external_sample_transferred_handler.handle(config=config, event_payload=event_payload)
 
-    # THEN
+    # THEN the external sample transferred_at was set
+    # THEN a housekeeper bundle and version was created for the sample
+    # THEN all sequencing files were added to the bundle
+    # THEN an event was published saying the sample was stored
+
     pass
