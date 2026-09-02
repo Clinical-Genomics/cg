14e66de7f09233c2fa3f6a9cffbe16483a018d85
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-09-02 17:19:10 +0200

    Refactor sample transfer handling to integrate Housekeeper API and streamline file processing

--- cg/services/events/event_handlers/external_sample_transferred_handler.py
+++ cg/services/events/event_handlers/external_sample_transferred_handler.py
@@ -5,6 +5,7 @@ from pathlib import Path
 from housekeeper.store.models import Bundle, File, Version
 from pydantic import BaseModel, Field
 
+from cg.apps.housekeeper.hk import HousekeeperAPI
 from cg.exc import CgError
 from cg.models.cg_config import CGConfig
 from cg.services.events import event_publisher
@@ -21,6 +22,11 @@ class ExternalSampleTransferredEvent(BaseModel):
 
 
 def handle(config: CGConfig, event_payload: dict) -> None:
+    """
+    Add all sequencing files for the sample in the payload to the Housekeeper database
+    and copy them to the corresponding Housekeeper bundle directory. Also update the entry for the
+    sample in the ExternalSample table with the datetime of the transfer.
+    """
     event = ExternalSampleTransferredEvent.model_validate(event_payload)
 
     if not (event.cluster_location.glob("*.bam") or event.cluster_location.glob("*fastq.gz")):
@@ -32,8 +38,24 @@ def handle(config: CGConfig, event_payload: dict) -> None:
         customer_id=sample.customer_id,
         transferred_at=event.transfer_completed_at,
     )
+    LOG.info(
+        f"Updated transferred_at for ExternalSample {sample.name} of customer {sample.customer_id} "
+        f"to {event.transfer_completed_at}."
+    )
 
-    bundle: Bundle = config.housekeeper_api.add_new_bundle_and_version(event.sample_internal_id)
+    _add_sample_files_to_housekeeper(housekeeper_api=config.housekeeper_api, event=event)
+    config.status_db.commit_to_store()
+    event_publisher.publish(
+        nats_config=config.nats,
+        subject=f"{config.nats.stream}.{EXTERNAL_SAMPLE_STORED_SUBJECT}",
+        event_payload={"statusdb.sample_internal_id": event.sample_internal_id},
+    )
+
+
+def _add_sample_files_to_housekeeper(
+    housekeeper_api: HousekeeperAPI, event: ExternalSampleTransferredEvent
+):
+    bundle: Bundle = housekeeper_api.add_new_bundle_and_version(event.sample_internal_id)
     version: Version = bundle.versions[0]
 
     files: list[File] = []
@@ -46,14 +68,8 @@ def handle(config: CGConfig, event_payload: dict) -> None:
         else:
             LOG.info(f"Omitting storing for non-sequencing file {file_path}.")
             continue
-        file: File = config.housekeeper_api.add_file(
+        file: File = housekeeper_api.add_file(
             path=str(file_path.absolute()), version_obj=version, tags=tags
         )
         files.append(file)
-    config.housekeeper_api.finalize_file_transactions(files=files, version=version)
-    config.status_db.commit_to_store()
-    event_publisher.publish(
-        nats_config=config.nats,
-        subject=f"{config.nats.stream}.{EXTERNAL_SAMPLE_STORED_SUBJECT}",
-        event_payload={"statusdb.sample_internal_id": event.sample_internal_id},
-    )
+    housekeeper_api.finalize_file_transactions(files=files, version=version)
