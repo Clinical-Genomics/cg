72fee1e2135d05e064f6c3bc6055e6b2f48002dc
Moe Darrah <41540288+moedarrah@users.noreply.github.com>
2026-09-02 13:25:24 +0200

    WIP

--- cg/services/events/event_handlers/external_sample_stored_handler.py
+++ cg/services/events/event_handlers/external_sample_stored_handler.py
@@ -1,6 +1,10 @@
+from typing import cast
+
 from pydantic import BaseModel, Field
 
 from cg.models.cg_config import CGConfig
+from cg.services.analysis_starter.analysis_starter import AnalysisStarter
+from cg.services.analysis_starter.factories.starter_factory import AnalysisStarterFactory
 from cg.store.models import Case, Sample
 from cg.store.store import Store
 
@@ -28,8 +32,16 @@ def handle(config: CGConfig, event_payload: dict) -> None:
 
     # TODO: Check if all external samples in the case are stored
     sample: Sample = status_db.get_sample_by_internal_id_strict(event.sample_internal_id)
-    case: Case | None = sample.case_that_delivers
+    case: Case = cast(Case, sample.case_that_delivers)
+    if all(sample.is_external and sample.case_that_delivers == case for sample in case.samples):
+        # TODO: Check the samples had stored
+        analysis_starter_factory = AnalysisStarterFactory(config)
+        analysis_starter: AnalysisStarter = analysis_starter_factory.get_analysis_starter_for_case(
+            case.internal_id
+        )
+        analysis_starter.start(case.internal_id)
     # From case get samples
+
     # For every sample check if external and if so if it has hk bundle
 
     # TODO: Start the case if previous check return true else return
--- tests/services/events/event_handlers/test_external_sample_stored_handler.py
+++ tests/services/events/event_handlers/test_external_sample_stored_handler.py
@@ -1,11 +1,19 @@
 from unittest.mock import create_autospec
 
 from cg.apps.housekeeper.hk import HousekeeperAPI
+from cg.store.models import Case, Sample
+from cg.store.store import Store
 
 
 def test_handle_starts_case():
     # GIVEN a valid event payload
     event_payload: dict = {"status_db.sample_internal_id": "ACC123"}
+
+    # GIVEN that the sample belongs to a purely external case
+    status_db: Store = create_autospec(Store)
+    case: Case = create_autospec(Case)
+    sample: Sample = create_autospec(Sample, case_that_delivers=case, is_external=True)
+    case.samples = [sample]
 
     # GIVEN that all samples in the case are stored
     hk_api: HousekeeperAPI = create_autospec(HousekeeperAPI)
