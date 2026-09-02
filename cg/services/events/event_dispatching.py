1bff565e9fce2927c671f4962a865c261604d53d
Isak Ohlsson Ångnell <40887124+islean@users.noreply.github.com>
2026-09-02 15:33:02 +0200

    order-placed-handler (#5288)(patch)
    ## Description
    Implements handler for adding external samples when the customer submits the order.
    
    ### Added
    
    - `external.samples_ordered` event mapped to its handler to the handler map in the dispatcher
    - `ExternalSamplesOrderedEvent` that validates the payload for the `external.samples_ordered` event
    - `external_samples_ordered_handler` that triggers the transfer of teh external samples of an order if the customer has already uploaded them
    - CRUD methods `get_sample_by_customer_and_name_strict` and `get_external_sample`
    - tests
    
    
    ---------
    
    Co-authored-by: Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
    Co-authored-by: Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
    Co-authored-by: Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
    Co-authored-by: Moe Darrah <41540288+moedarrah@users.noreply.github.com>
    Co-authored-by: kedhammar <alfred.kedhammar@scilifelab.se>

--- cg/services/events/event_dispatching.py
+++ cg/services/events/event_dispatching.py
@@ -1,14 +1,22 @@
 import logging
-from typing import Callable
+from typing import Protocol
 
 from cg.models.cg_config import CGConfig
-from cg.services.events.event_handlers import external_sample_uploaded_handler
+from cg.services.events.event_handlers import (
+    external_sample_uploaded_handler,
+    external_samples_ordered_handler,
+)
 
 LOG = logging.getLogger(__name__)
 
 
-EVENT_HANDLERS: dict[str, Callable] = {
-    "external.customer_uploaded_sample": external_sample_uploaded_handler.handle
+class EventHandler(Protocol):
+    def __call__(self, config: CGConfig, event_payload: dict) -> None: ...
+
+
+EVENT_HANDLERS: dict[str, EventHandler] = {
+    "external.customer_uploaded_sample": external_sample_uploaded_handler.handle,
+    "external.samples_ordered": external_samples_ordered_handler.handle,
 }
 
 
@@ -18,7 +26,7 @@ def dispatch(
     """
     Select the appropriate handler for the given event name and call it with the provided payload.
     """
-    handler_function: Callable | None = event_handlers.get(event_name)
+    handler_function: EventHandler | None = event_handlers.get(event_name)
     if handler_function:
         LOG.debug(f"Dispatching event {event_name} to handler {handler_function.__name__}")
         handler_function(config=config, event_payload=event_payload)
