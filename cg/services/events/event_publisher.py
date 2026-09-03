561bec0341faff5f010c56aaf7a8ac1cb3a201dd
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-09-03 10:42:51 +0200

    debug statements

--- cg/services/events/event_publisher.py
+++ cg/services/events/event_publisher.py
@@ -1,5 +1,6 @@
 import asyncio
 import json
+import logging
 import ssl
 from pathlib import Path
 from ssl import Purpose, SSLContext, TLSVersion
@@ -7,6 +8,8 @@ from ssl import Purpose, SSLContext, TLSVersion
 import nats
 from nats.aio.client import Client
 from nats.js import JetStreamContext
+
+LOG = logging.getLogger(__name__)
 
 
 def publish_command(nats_config, subject: str, data: dict) -> str:
@@ -30,17 +33,21 @@ def publish(nats_config, subject: str, event_payload: dict) -> None:
 
 
 async def _publish_async(nats_config, subject: str, data: dict) -> None:
+    LOG.debug("Starting connection to the NATS server")
     nc: Client = await nats.connect(
         servers=nats_config.server,
         tls=_tls_context(nats_config=nats_config),
         token=Path(nats_config.token_path).read_text().strip(),
     )
+    LOG.debug("Connection to the NATS server established")
     try:
         js: JetStreamContext = nc.jetstream()
         payload: bytes = json.dumps(data).encode()
         await js.publish(subject=subject, payload=payload)
+        LOG.debug(f"Published event to NATS JetStream subject {subject} with payload {data}")
     finally:
         await nc.drain()
+        LOG.debug("Connection to the NATS server closed")
 
 
 def _tls_context(nats_config) -> SSLContext:
