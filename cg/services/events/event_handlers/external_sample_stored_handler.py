6346e2cec44344dd2ae478dc200256ffa9ed1d12
kedhammar <alfred.kedhammar@scilifelab.se>
2026-09-04 10:41:57 +0200

    resolve comments

--- cg/services/events/event_handlers/external_sample_stored_handler.py
+++ cg/services/events/event_handlers/external_sample_stored_handler.py
@@ -13,15 +13,6 @@ class ExternalSampleStoredEvent(BaseModel):
     sample_internal_id: str = Field(alias="status_db.sample_internal_id")
 
 
-def _are_all_samples_external_and_stored(case: Case, housekeeper_api: HousekeeperAPI) -> bool:
-    return all(
-        sample.is_external
-        and sample.case_that_delivers == case  # Ensures sample was originally ordered in this case
-        and housekeeper_api.bundle(sample.internal_id)  # Ensures it has been stored
-        for sample in case.samples
-    )
-
-
 def handle(config: CGConfig, event_payload: dict) -> None:
     event = ExternalSampleStoredEvent.model_validate(event_payload)
     status_db: Store = config.status_db
@@ -31,9 +22,22 @@ def handle(config: CGConfig, event_payload: dict) -> None:
     case: Case | None = sample.case_that_delivers
     if not case:
         raise CaseNotFoundError(f"No case found to deliver sample {sample.internal_id}")
-    if _are_all_samples_external_and_stored(case=case, housekeeper_api=housekeeper_api):
+    if _are_all_samples_new_external_and_stored(case=case, housekeeper_api=housekeeper_api):
         analysis_starter_factory = AnalysisStarterFactory(config)
         analysis_starter: AnalysisStarter = analysis_starter_factory.get_analysis_starter_for_case(
             case.internal_id
         )
         analysis_starter.start(case.internal_id)
+
+
+def _are_all_samples_new_external_and_stored(case: Case, housekeeper_api: HousekeeperAPI) -> bool:
+    """
+    Return True if all of the samples of the case are external, stored in Housekeeper
+    and are originally ordered with the case.
+    """
+    return all(
+        sample.is_external
+        and sample.case_that_delivers == case  # Ensures sample was originally ordered in this case
+        and housekeeper_api.bundle(sample.internal_id)  # Ensures it has been stored
+        for sample in case.samples
+    )
