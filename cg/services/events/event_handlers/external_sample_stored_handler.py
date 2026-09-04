a41130f1eb50c0adf857e2339e2e9b2e2b196b74
islean <isak.ohlsson.angnell@gmail.com>
2026-09-04 11:08:31 +0200

    Rework logging

--- cg/services/events/event_handlers/external_sample_stored_handler.py
+++ cg/services/events/event_handlers/external_sample_stored_handler.py
@@ -1,3 +1,5 @@
+import logging
+
 from pydantic import BaseModel, Field
 
 from cg.apps.housekeeper.hk import HousekeeperAPI
@@ -7,6 +9,8 @@ from cg.services.analysis_starter.analysis_starter import AnalysisStarter
 from cg.services.analysis_starter.factories.starter_factory import AnalysisStarterFactory
 from cg.store.models import Case, Sample
 from cg.store.store import Store
+
+LOG = logging.getLogger(__name__)
 
 
 class ExternalSampleStoredEvent(BaseModel):
@@ -23,11 +27,14 @@ def handle(config: CGConfig, event_payload: dict) -> None:
     if not case:
         raise CaseNotFoundError(f"No case found to deliver sample {sample.internal_id}")
     if _are_all_samples_new_external_and_stored(case=case, housekeeper_api=housekeeper_api):
+        LOG.info(f"Case {case.internal_id} is ready to be started.")
         analysis_starter_factory = AnalysisStarterFactory(config)
         analysis_starter: AnalysisStarter = analysis_starter_factory.get_analysis_starter_for_case(
             case.internal_id
         )
         analysis_starter.start(case.internal_id)
+    else:
+        LOG.info(f"Case {case.internal_id} is not ready to be started.")
 
 
 def _are_all_samples_new_external_and_stored(case: Case, housekeeper_api: HousekeeperAPI) -> bool:
@@ -35,9 +42,31 @@ def _are_all_samples_new_external_and_stored(case: Case, housekeeper_api: Housek
     Return True if all of the samples of the case are external, stored in Housekeeper
     and are originally ordered with the case.
     """
-    return all(
+    not_external: list[str] = []
+    not_new: list[str] = []
+    not_stored: list[str] = []
+    for sample in case.samples:
+        if not sample.is_external:
+            not_external.append(sample.internal_id)
+        if not sample.case_that_delivers == case:
+            not_new.append(sample.internal_id)
+        if not housekeeper_api.bundle(sample.internal_id):
+            not_stored.append(sample.internal_id)
+    if any([not_external, not_new, not_stored]):
+        LOG.info(
+            f"Could not start analysis because of the following samples:\n"
+            + f"Not external: {not_external}\n"
+            if not_external
+            else (
+                "" + f"Not new: {not_new}\n"
+                if not_new
+                else "" + f"Not stored: {not_stored}" if not_stored else ""
+            )
+        )
+
+    """return all(
         sample.is_external
         and sample.case_that_delivers == case  # Ensures sample was originally ordered in this case
         and housekeeper_api.bundle(sample.internal_id)  # Ensures it has been stored
         for sample in case.samples
-    )
+    )"""
