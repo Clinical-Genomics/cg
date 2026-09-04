7195121b04be27963372a15fe0732993d609402d
Sebastian Diaz <juan.sebastian.diaz.boada@scilifelab.se>
2026-09-04 13:37:08 +0200

    wip red

--- cg/services/orders/submitter/service.py
+++ cg/services/orders/submitter/service.py
@@ -7,6 +7,8 @@ document with all information about samples in the submission. The input will
 be validated and if passing all checks be accepted as new samples.
 """
 
+import logging
+
 from cg.models.cg_config import NatsConfig
 from cg.models.orders.constants import OrderType
 from cg.services.events import event_publisher
@@ -17,6 +19,8 @@ from cg.services.orders.validation.models.order import Order
 from cg.services.orders.validation.service import OrderValidationService
 from cg.store.models import User
 from cg.store.store import Store
+
+LOG = logging.getLogger(__name__)
 
 
 class OrderSubmitter:
@@ -52,14 +56,17 @@ class OrderSubmitter:
         order._generated_ticket_id = ticket_number
         serialized_order: dict = storing_service.store_order(order)
         if external_samples := order.external_samples(self.status_db):
-            sample_names = [sample.name for sample in external_samples]
+            sample_names: list[str] = [sample.name for sample in external_samples]
+            LOG.info(f"Order contains external samples {sample_names}")
             customer_internal_id: str = order.customer
             payload: dict = _get_payload_for_external_samples(
                 customer_internal_id=customer_internal_id, sample_names=sample_names
             )
+            subject = "external.samples_ordered"
+            LOG.info(f"Publishing event to subject {subject} with payload {payload}")
             event_publisher.publish(
                 nats_config=self.nats_config,
-                subject="external.samples_ordered",
+                subject=subject,
                 event_payload=payload,
             )
         return serialized_order
