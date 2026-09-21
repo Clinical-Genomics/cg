90d5a228d40b66ff57023331f2e2bd6270cb1f25
Linnéa Löfdahl <linnea.lofdahl@scilifelab.se>
2026-09-21 14:41:35 +0200

    Use constant for http status

--- cg/services/slack_notification_service.py
+++ cg/services/slack_notification_service.py
@@ -1,5 +1,6 @@
 import json
 import logging
+from http import HTTPStatus
 
 import requests
 
@@ -12,5 +13,5 @@ def notify(recipient: str, error: Exception):
         "Content-Type": "application/json",
     }
     response = requests.post(url=recipient, data=json.dumps({"text": str(error)}), headers=headers)
-    if response.status_code != 200:
+    if response.status_code != HTTPStatus.OK:
         LOG.error(f"Could not notify prod team: {response.status_code} - {response.text}")
