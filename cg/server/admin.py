313db7a6fd1d094ce288a8b85be20f803f9b08b8
Rasmus Burge <80392398+RasmusBurge-CG@users.noreply.github.com>
2026-09-22 16:25:01 +0200

    Admin view - Wider admin view (#5318) (minor)
    ### Added
    
    - Freeze the column header
    
    ### Changed
    
    - Tables will now use the full width of the web browser

--- cg/server/admin.py
+++ cg/server/admin.py
@@ -6,7 +6,7 @@ from flask import flash, redirect, request, session, url_for
 from flask_admin.actions import action
 from flask_admin.contrib.sqla import ModelView
 from flask_dance.contrib.google import google
-from markupsafe import Markup
+from markupsafe import Markup, escape
 from sqlalchemy import inspect
 from wtforms.form import Form
 
@@ -133,23 +133,16 @@ def view_pacbio_sample_sequencing_metrics_link(unused1, unused2, model, unused3)
     )
 
 
-def view_application_text_column(unused1, unused2, model, attribute_name):
-    """Column formatter to widen long text columns."""
+def view_cap_text_column_width(unused1, unused2, model, attribute_name):
+    """Column formatter to cap long text columns to a readable width."""
     del unused1, unused2
     text = getattr(model, attribute_name)
     return (
-        Markup(f"<div style='display: inline-block; min-width: 300px;'>{text}</div>")
+        Markup(
+            "<div style='max-width: 400px; white-space: normal; overflow-wrap: break-word;'>"
+            f"{escape(text)}</div>"
+        )
         if text
-        else ""
-    )
-
-
-def view_order_types(unused1, unused2, model, unused3):
-    del unused1, unused2, unused3
-    order_type_list = "<br>".join(model.order_types)
-    return (
-        Markup(f'<div style="display: inline-block; min-width: 200px;">{order_type_list}</div>')
-        if model.order_type_applications
         else ""
     )
 
@@ -319,14 +312,10 @@ class ApplicationView(BaseView):
     ]
     column_formatters = {
         "tag": view_application_version_link,
-        "order_types": view_order_types,
         "sample_concentration_minimum": view_sample_concentration_minimum,
         "sample_concentration_maximum": view_sample_concentration_maximum,
         "sample_concentration_minimum_cfdna": view_sample_concentration_minimum_cfdna,
         "sample_concentration_maximum_cfdna": view_sample_concentration_maximum_cfdna,
-        "limitations": view_application_text_column,
-        "comment": view_application_text_column,
-        "details": view_application_text_column,
     }
     column_filters = ["prep_category", "is_accredited", "is_archived", "read_type", "is_external"]
     column_searchable_list = ["tag", "prep_category", "description", "details"]
@@ -415,7 +404,10 @@ class ApplicationLimitationsView(BaseView):
         "created_at",
         "updated_at",
     )
-    column_formatters = {"application": ApplicationView.view_application_link}
+    column_formatters = {
+        "application": ApplicationView.view_application_link,
+        "limitations": view_cap_text_column_width,
+    }
     column_filters = ["application.tag", "workflow"]
     column_searchable_list = ["application.tag"]
     column_editable_list = ["comment"]
@@ -514,6 +506,21 @@ class CaseView(BaseView):
 
     column_default_sort = ("created_at", True)
     column_editable_list = ["action", "comment"]
+    column_list = [
+        "internal_id",
+        "name",
+        "customer",
+        "tickets",
+        "action",
+        "priority",
+        "ordered_at",
+        "aggregated_sequencing_qc",
+        "data_analysis",
+        "data_delivery",
+        "_panels",
+        "comment",
+        "is_compressible",
+    ]
     column_exclude_list = ["created_at", "_cohorts", "synopsis"]
     column_filters = [
         "customer.internal_id",
@@ -823,39 +830,38 @@ class PoolView(BaseView):
 
 
 class SampleView(BaseView):
-    """Admin view for Model.Sample"""
+    """Admin view for Model.Sample."""
 
     column_list = [
-        "application_version",
-        "customer",
-        "organism",
-        "invoice",
-        "is_cancelled",
-        "lims_status",
-        "capture_kit",
-        "comment",
-        "control",
-        "created_at",
-        "delivered_at",
-        "downsampled_to",
-        "from_sample",
         "internal_id",
-        "is_tumour",
-        "loqusdb_id",
         "name",
-        "no_invoice",
-        "order",
-        "ordered_at",
-        "original_ticket",
-        "prepared_at",
-        "priority",
-        "reads",
-        "hifi_yield",
-        "last_sequenced_at",
-        "received_at",
-        "reference_genome",
         "sex",
         "subject_id",
+        "customer",
+        "original_ticket",
+        "comment",
+        "is_cancelled",
+        "priority",
+        "application_version",
+        "capture_kit",
+        "is_tumour",
+        "reads",
+        "hifi_yield",
+        "control",
+        "organism",
+        "reference_genome",
+        "invoice",
+        "no_invoice",
+        "order",
+        "lims_status",
+        "from_sample",
+        "downsampled_to",
+        "loqusdb_id",
+        "ordered_at",
+        "received_at",
+        "prepared_at",
+        "last_sequenced_at",
+        "delivered_at",
     ]
     column_default_sort = ("created_at", True)
     column_editable_list = [
